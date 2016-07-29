3bbeecc52a85c18b2e379a983665bf3476a51e8e
Luis <lasote@gmail.com>
2016-07-29 18:43:42 +0200

    Fix 390 issue (#391)

--- conans/client/proxy.py
+++ conans/client/proxy.py
@@ -140,12 +140,13 @@ class ConanProxy(object):
         if not conan_reference:
             return 0
         read_manifest, _ = self._paths.conan_manifests(conan_reference)
-        try:  # get_conan_digest can fail, not in server
-            upstream_manifest = self.get_conan_digest(conan_reference)
-            if upstream_manifest.file_sums != read_manifest.file_sums:
-                return 1 if upstream_manifest.time > read_manifest.time else -1
-        except ConanException:
-            pass
+        if read_manifest:
+            try:  # get_conan_digest can fail, not in server
+                upstream_manifest = self.get_conan_digest(conan_reference)
+                if upstream_manifest.file_sums != read_manifest.file_sums:
+                    return 1 if upstream_manifest.time > read_manifest.time else -1
+            except ConanException:
+                pass
 
         return 0
 
