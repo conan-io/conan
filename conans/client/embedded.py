ee1c282f9d2e45b9ce4dbedd15fc01c40b7ea0c5
Loïc Dauphin <astralien3000@yahoo.fr>
2016-08-26 11:32:55 +0200

    External Embedded Settings proof of concept

--- conans/client/embedded.py
+++ conans/client/embedded.py
@@ -1,76 +1,81 @@
 from conans.errors import ConanException
+from conans.paths import conan_expand_user
+from conans.client.paths import ConanPaths
+from conans.client.output import ConanOutput
+from conans import tools
 
-class Target:
-    data = {}
-    def __init__(self, name, parents = []):
-        self.name = name;
-        self.parents = []
-        for p in parents:
-                self.parents += [Target.data[p]]
-        Target.data[self.name] = self
-    def groups(self):
-        ret = [self.name]
-        for p in self.parents:
-            ret += p.groups()
+import yaml
+import os
+import sys
+
+class EmbeddedSettingsParser:
+    def __init__(self, filename):
+        with open(filename, "r") as f:
+            self.data = yaml.load(f.read())
+        targets = set()
+        parents = set()
+        for (k, v) in self.data.items():
+            targets.add(k)
+            if v != None:
+                if v.has_key("parents"):
+                    parents = parents.union(set(v["parents"]))
+                if v.has_key("mcu"):
+                    parents.add(v["mcu"])
+        if not parents.issubset(targets):
+            raise ConanException("[ERROR] Undeclared parents : " + str(parents.difference(targets)))
+
+    def get_microcontroller(self, target):
+        target = str(target)
+        if not self.data.has_key(target):
+            return None
+        if not self.data[target].has_key("type"):
+            return None
+        if self.data[target]["type"] == "mcu":
+            return target
+        if self.data[target]["type"] == "board":
+            return self.data[target]["mcu"]
+        return None
+
+    def get_groups(self, target):
+        target = str(target)
+        if not self.data.has_key(target):
+            return set()
+        if self.data[target] == None:
+            return set()
+        ret = set()
+        if self.data[target].has_key("parents"):
+            for p in self.data[target]["parents"]:
+                ret.add(p)
+                ret = ret.union(self.get_groups(p))
+        if self.data[target].has_key("mcu"):
+            ret.add(self.data[target]["mcu"])
+            ret = ret.union(self.get_groups(self.data[target]["mcu"]))
         return ret
-    
-class Board(Target):
-    data = {}
-    def __init__(self, name, microcontroller, parents = []):
-        Target.__init__(self, name, parents)
-        self.microcontroller = Target.data[microcontroller]
-        Board.data[self.name] = self
-    def groups(self):
-        ret = Target.groups(self)
-        ret += self.microcontroller.groups()
-        return ret
-        
-class MCU(Target):
-    data = {}
-    def __init__(self, name, parents = []):
-        Target.__init__(self, name, parents)
-        MCU.data[self.name] = self
 
 class Embedded:
-    Target("avr")
-    Target("atmega", ["avr"])
-    Target("atmegaxx0_1", ["atmega"])
-    Target("atmegax8", ["atmega"])
-
-    Target("arm")
-    Target("cortex_m4", ["arm"])
-    
-    Target("stm32", ["arm"])
-    Target("stm32f4", ["stm32", "cortex_m4"])
-
-    MCU("atmega2560", ["atmegaxx0_1"])
-    MCU("atmega328p", ["atmegax8"])
-    MCU("stm32f407vg", ["stm32f4"])
-
-    Target("discovery")
-    Target("arduino")
-    
-    Board("arduino_mega", "atmega2560", ["arduino"])
-    Board("arduino_uno", "atmega328p", ["arduino"])
-    Board("stm32f4_discovery", "stm32f407vg", ["discovery"])
+    # VERY DIRTY !
+    user_folder = os.getenv("CONAN_USER_HOME", conan_expand_user("~"))
+    out = ConanOutput(sys.stdout, True)
+    paths = ConanPaths(user_folder, None, out)
+    if not os.path.exists(paths.embedded_settings_path):
+        tools.download("https://github.com/astralien3000/conan-embedded-settings/archive/master.zip", "embedded-settings.zip")
+        tools.unzip("embedded-settings.zip", paths.conan_folder)
+    settings = EmbeddedSettingsParser(os.path.join(paths.embedded_settings_path, "test.yml"))
 
     def __init__(self, settings):
         self.target = None
-        self.mcu = None
         if settings.target == "mcu":
-            self.target = MCU.data[str(settings.target.mcu)]
-            self.mcu = self.target
+            self.target = settings.target.mcu
         elif settings.target == "board":
-            self.target = Board.data[str(settings.target.board)]
-            self.mcu = self.target.microcontroller
+            self.target = settings.target.board
         else:
-            raise ConanException("Invalid target : " + str(settings.target))
+            raise ConanException("[ERROR] Invalid target : " + str(settings.target))
 
     def name(self):
-        return self.target.name
+        return self.target
 
     def microcontroller(self):
-        return self.mcu.name
+        return Embedded.settings.get_microcontroller(self.target)
 
     def groups(self):
-        return self.target.groups()
+        return Embedded.settings.get_groups(self.target)
