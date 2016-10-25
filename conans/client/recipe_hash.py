95a861b440713bdfe60af5d0e954ec40960c935d
Lasote <lasote@gmail.com>
2016-10-25 13:22:33 +0200

    Some functions to hash compute

---
+++ conans/client/recipe_hash.py
@@ -0,0 +1,22 @@
+import os
+from conans.util.files import load
+from conans.util.sha import sha1
+
+
+def normalize_crlfs(path):
+    contents = load(path, binary=True)
+    return contents.replace('\r\n', '\n').replace('\r', '\n')
+
+
+def get_file_digest(path):
+    return sha1(normalize_crlfs(path))
+
+
+def get_normalized_hash(folder):
+    paths = []
+    for root, _, files in os.walk(folder):
+        for name in files:
+            paths.append(os.path.join(root, name))
+    paths = sorted(paths)
+    digest_lines = "\n".join([get_file_digest(path) for path in paths])
+    return sha1(digest_lines)
---
+++ conans/test/recipe_hash_test.py
@@ -0,0 +1,24 @@
+import unittest
+from conans.util.files import save_files
+from conans.test.utils.test_files import temp_folder
+from conans.client.recipe_hash import get_normalized_hash
+from conans.util.sha import sha1
+
+
+class RecipeHashTest(unittest.TestCase):
+
+    def compute_hash_test(self):
+        tmp_dir = temp_folder()
+        save_files(tmp_dir, {"file1.txt": "Some contents\r\ncontents",
+                             "file2.txt":  "More contents\r\n",
+                             "subfolder/file3.txt": "more\nlines",
+                             "binary": '\x80abc'})
+
+        norm_hash = get_normalized_hash(tmp_dir)
+        expected_hashes_file = sha1(sha1("\x80abc") + "\n" +
+                                    sha1("Some contents\ncontents") + "\n" +
+                                    sha1("More contents\n") + "\n" +
+                                    sha1("more\nlines"))
+        self.assertEquals(norm_hash, expected_hashes_file)
+        self.assertEquals(norm_hash, "deb6fbeed96361c0c57afa1e34ebda60b97cb6cd")
+
