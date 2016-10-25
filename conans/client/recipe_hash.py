212023f38bbc8877912cf65af58bdbfb2c392e48
Lasote <lasote@gmail.com>
2016-10-25 13:43:13 +0200

    Encoding again py3

--- conans/client/recipe_hash.py
+++ conans/client/recipe_hash.py
@@ -3,13 +3,13 @@ from conans.util.files import load
 from conans.util.sha import sha1
 
 
-def normalize_crlfs(path):
+def _normalize_crlfs(path):
     contents = load(path, binary=True)
     return contents.replace(b'\r\n', b'\n').replace(b'\r', b'\n')
 
 
-def get_file_digest(path):
-    return sha1(normalize_crlfs(path))
+def _get_file_digest(path):
+    return sha1(_normalize_crlfs(path))
 
 
 def get_normalized_hash(folder):
@@ -18,5 +18,5 @@ def get_normalized_hash(folder):
         for name in files:
             paths.append(os.path.join(root, name))
     paths = sorted(paths)
-    digest_lines = "\n".join([get_file_digest(path) for path in paths])
+    digest_lines = "\n".join([_get_file_digest(path) for path in paths])
     return sha1(digest_lines.encode("utf-8"))
