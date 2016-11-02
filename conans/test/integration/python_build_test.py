c038f4e82ca76eeeef9e369f758f739cd9266027
memsharded <james@conan.io>
2016-11-02 12:36:19 +0100

    adding failing test for source command

--- conans/test/integration/python_build_test.py
+++ conans/test/integration/python_build_test.py
@@ -5,10 +5,7 @@ from conans.util.files import load
 import os
 
 
-class PythonBuildTest(unittest.TestCase):
-
-    def reuse_test(self):
-        conanfile = """from conans import ConanFile
+conanfile = """from conans import ConanFile
 
 class ConanToolPackage(ConanFile):
     name = "conantool"
@@ -22,7 +19,9 @@ class ConanToolPackage(ConanFile):
     def package_info(self):
         self.env_info.PYTHONPATH.append(self.package_folder)
 """
-        test = """def foo(output):
+
+
+test = """def foo(output):
     output.info("Hello Foo")
 def bar(output):
     output.info("Hello Bar")
@@ -31,11 +30,9 @@ def baz(output):
 def boom(output):
     output.info("Hello Boom")
 """
-        client = TestClient()
-        client.save({CONANFILE: conanfile, "__init__.py": "", "mytest.py": test})
-        client.run("export lasote/stable")
 
-        reuse = """from conans import ConanFile, tools
+
+reuse = """from conans import ConanFile, tools
 
 class ToolsTest(ConanFile):
     name = "Consumer"
@@ -63,6 +60,14 @@ class ToolsTest(ConanFile):
             import mytest
             mytest.bar(self.output)
 """
+
+class PythonBuildTest(unittest.TestCase):
+
+    def reuse_test(self):
+        client = TestClient()
+        client.save({CONANFILE: conanfile, "__init__.py": "", "mytest.py": test})
+        client.run("export lasote/stable")
+
         client.save({CONANFILE: reuse}, clean_first=True)
         client.run("install .")
         content = load(os.path.join(client.current_folder, "conanenv.txt"))
@@ -81,6 +86,19 @@ class ToolsTest(ConanFile):
                          lines)
 
         client.run("remove Consumer/0.1@lasote/stable -f")
+        client.run("export lasote/stable")
+        client.run("source Consumer/0.1@lasote/stable")
+        self.assertIn("Hello Baz", client.user_io.out)
+        self.assertNotIn("Hello Foo", client.user_io.out)
+        self.assertNotIn("Hello Bar", client.user_io.out)
+        self.assertNotIn("Hello Boom", client.user_io.out)
+
+    def source_test(self):
+        client = TestClient()
+        client.save({CONANFILE: conanfile, "__init__.py": "", "mytest.py": test})
+        client.run("export lasote/stable")
+
+        client.save({CONANFILE: reuse}, clean_first=True)
         client.run("export lasote/stable")
         client.run("source Consumer/0.1@lasote/stable")
         self.assertIn("Hello Baz", client.user_io.out)
