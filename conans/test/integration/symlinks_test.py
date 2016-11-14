6ff480f2b42a8cac0c00830fdb708e89b17c50b2
memsharded <james@conan.io>
2016-11-14 01:55:39 +0100

    added upload test, failing

--- conans/test/integration/symlinks_test.py
+++ conans/test/integration/symlinks_test.py
@@ -1,19 +1,11 @@
 import unittest
-from conans.test.tools import TestClient
+from conans.test.tools import TestClient, TestServer
 from conans.util.files import load, save
 from conans.model.ref import PackageReference
 import os
 import platform
 
-
-class SymLinksTest(unittest.TestCase):
-
-    def basic_test(self):
-        if platform.system() == "Windows":
-            return
-
-        client = TestClient()
-        conanfile = """
+conanfile = """
 from conans import ConanFile
 from conans.util.files import save
 import os
@@ -29,20 +21,22 @@ class HelloConan(ConanFile):
     def package(self):
         self.copy("*.txt*")
 """
-        test_conanfile = """[requires]
+
+test_conanfile = """[requires]
 Hello/0.1@lasote/stable
 
 [imports]
 ., * -> .
 """
-        client.save({"conanfile.py": conanfile,
-                     "conanfile.txt": test_conanfile})
-        client.run("export lasote/stable")
-        client.run("install . --build -f=conanfile.txt")
-        ref = PackageReference.loads("Hello/0.1@lasote/stable:"
-                                     "5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9")
 
-        for base in [client.paths.package(ref), client.paths.build(ref), client.current_folder]:
+
+class SymLinksTest(unittest.TestCase):
+
+    def _check(self, client, ref, build=True):
+        folders = [client.paths.package(ref), client.current_folder]
+        if build:
+            folders.append(client.paths.build(ref))
+        for base in folders:
             filepath = os.path.join(base, "file1.txt")
             link = os.path.join(base, "file1.txt.1")
             self.assertEqual(os.readlink(link), "file1.txt")
@@ -53,3 +47,37 @@ Hello/0.1@lasote/stable
             # Save any different string, random, or the base path
             save(filepath, base)
             self.assertEqual(load(link), base)
+
+    def basic_test(self):
+        if platform.system() == "Windows":
+            return
+
+        client = TestClient()
+        client.save({"conanfile.py": conanfile,
+                     "conanfile.txt": test_conanfile})
+        client.run("export lasote/stable")
+        client.run("install --build -f=conanfile.txt")
+        ref = PackageReference.loads("Hello/0.1@lasote/stable:"
+                                     "5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9")
+
+        self._check(client, ref)
+
+    def upload_test(self):
+        if platform.system() == "Windows":
+            return
+
+        test_server = TestServer()
+        servers = {"default": test_server}
+        client = TestClient(servers=servers, users={"default": [("lasote", "mypass")]})
+
+        client.save({"conanfile.py": conanfile,
+                     "conanfile.txt": test_conanfile})
+        client.run("export lasote/stable")
+        client.run("install --build -f=conanfile.txt")
+        ref = PackageReference.loads("Hello/0.1@lasote/stable:"
+                                     "5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9")
+
+        client.run("upload Hello/0.1@lasote/stable --all")
+        client.run('remove "*" -f')
+        client.run("install -f=conanfile.txt")
+        self._check(client, ref, build=False)
