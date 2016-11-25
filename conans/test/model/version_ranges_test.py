2f54b0d693da771059edd48be1cdb10b31a88f2a
Lasote <lasote@gmail.com>
2016-11-25 15:45:55 +0100

    Fixed test develop

--- conans/test/model/version_ranges_test.py
+++ conans/test/model/version_ranges_test.py
@@ -119,7 +119,8 @@ class VersionRangesTest(unittest.TestCase):
 
     def setUp(self):
         self.output = TestBufferConanOutput()
-        self.loader = ConanFileLoader(None, Settings.loads(""), OptionsValues.loads(""), Scopes())
+        self.loader = ConanFileLoader(None, Settings.loads(""), None, 
+                                      OptionsValues.loads(""), Scopes(), None, None)
         self.retriever = Retriever(self.loader, self.output)
         self.remote_search = MockSearchRemote()
         self.resolver = RequireResolver(self.output, self.retriever, self.remote_search)
