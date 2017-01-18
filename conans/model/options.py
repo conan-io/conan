b1530be6c3366066ae5853fbe244cefe89896342
memsharded <james@conan.io>
2017-01-18 12:30:35 +0100

    flattened PackageOptions

--- conans/model/options.py
+++ conans/model/options.py
@@ -136,18 +136,13 @@ class PackageValues(object):
 
 
 def bad_value_msg(name, value, value_range):
-    tip = ""
-    if "settings" in name:
-        tip = "\nCheck your settings ~/.conan/settings.yml or read the docs FAQ"
-
-    return ("'%s' is not a valid '%s' value.\nPossible values are %s%s"
-            % (value, name, value_range, tip))
+    return ("'%s' is not a valid 'options.%s' value.\nPossible values are %s"
+            % (value, name, value_range))
 
 
-def undefined_field(name, field, fields=None, value=None):
-    value_str = " for '%s'" % value if value else ""
-    result = ["'%s.%s' doesn't exist%s" % (name, field, value_str)]
-    result.append("'%s' possible configurations are %s" % (name, fields or "none"))
+def undefined_option(option_name, existing_options):
+    result = ["'options.%s' doesn't exist" % option_name]
+    result.append("Possible options are %s" % existing_options or "none")
     return "\n".join(result)
 
 
@@ -155,43 +150,19 @@ def undefined_value(name):
     return "'%s' value not defined" % name
 
 
-class ConfigItem(object):
-    def __init__(self, definition, name, cls):
+class PackageOption(object):
+    def __init__(self, possible_values, name):
         self._name = name
         self._value = None
-        self._cls = cls
-        self._definition = {}
-        if isinstance(definition, dict):
-            # recursive
-            for k, v in definition.items():
-                k = str(k)
-                self._definition[k] = cls(v, name, k)
-        elif definition == "ANY":
-            self._definition = "ANY"
+        if possible_values == "ANY":
+            self._possible_values = "ANY"
         else:
-            # list or tuple of possible values
-            self._definition = sorted([str(v) for v in definition])
-
-    def copy(self):
-        """ deepcopy, recursive
-        """
-        cls = type(self)
-        result = cls({}, name=self._name, cls=self._cls)
-        result._value = self._value
-        if self.is_final:
-            result._definition = self._definition[:]
-        else:
-            result._definition = {k: v.copy() for k, v in self._definition.items()}
-        return result
-
-    @property
-    def is_final(self):
-        return not isinstance(self._definition, dict)
+            self._possible_values = sorted(str(v) for v in possible_values)
 
     def __bool__(self):
         if not self._value:
             return False
-        return self._value.lower() not in ["false", "none", "0", "off"]
+        return self._value.lower() not in _falsey_options
 
     def __nonzero__(self):
         return self.__bool__()
@@ -203,61 +174,23 @@ class ConfigItem(object):
         if other is None:
             return self._value is None
         other = str(other)
-        if self._definition != "ANY" and other not in self.values_range:
-            raise ConanException(bad_value_msg(self._name, other, self.values_range))
+        if self._possible_values != "ANY" and other not in self._possible_values:
+            raise ConanException(bad_value_msg(self._name, other, self._possible_values))
         return other == self.__str__()
 
     def __ne__(self, other):
         return not self.__eq__(other)
 
-    def __delattr__(self, item):
-        """ This is necessary to remove libcxx subsetting from compiler in config()
-           del self.settings.compiler.stdlib
-        """
-        try:
-            self._get_child(self._value).remove(item)
-        except:
-            pass
-
     def remove(self, values):
+        if self._possible_values == "ANY":
+            return
         if not isinstance(values, (list, tuple, set)):
             values = [values]
-        for v in values:
-            v = str(v)
-            if isinstance(self._definition, dict):
-                self._definition.pop(v, None)
-            elif self._definition != "ANY":
-                if v in self._definition:
-                    self._definition.remove(v)
-        if self._value is not None and self._value not in self._definition:
-            raise ConanException(bad_value_msg(self._name, self._value, self.values_range))
+        values = [str(v) for v in values]
+        self._possible_values = [v for v in self._possible_values if v not in values]
 
-    def _get_child(self, item):
-        if not isinstance(self._definition, dict):
-            raise ConanException(undefined_field(self._name, item, None, self._value))
-        if self._value is None:
-            raise ConanException(undefined_value(self._name))
-        return self._definition[self._value]
-
-    def __getattr__(self, item):
-        item = str(item)
-        sub_config_dict = self._get_child(item)
-        return getattr(sub_config_dict, item)
-
-    def __setattr__(self, item, value):
-        if item[0] == "_" or item.startswith("value"):
-            return super(ConfigItem, self).__setattr__(item, value)
-
-        item = str(item)
-        sub_config_dict = self._get_child(item)
-        return setattr(sub_config_dict, item, value)
-
-    def __getitem__(self, value):
-        value = str(value)
-        try:
-            return self._definition[value]
-        except:
-            raise ConanException(bad_value_msg(self._name, value, self.values_range))
+        if self._value is not None and self._value not in self._possible_values:
+            raise ConanException(bad_value_msg(self._name, self._value, self._possible_values))
 
     @property
     def value(self):
@@ -266,69 +199,43 @@ class ConfigItem(object):
     @value.setter
     def value(self, v):
         v = str(v)
-        if self._definition != "ANY" and v not in self._definition:
-            raise ConanException(bad_value_msg(self._name, v, self.values_range))
+        if self._possible_values != "ANY" and v not in self._possible_values:
+            raise ConanException(bad_value_msg(self._name, v, self._possible_values))
         self._value = v
-
-    @property
-    def values_range(self):
-        try:
-            return sorted(list(self._definition.keys()))
-        except:
-            return self._definition
 
     @property
     def values_list(self):
         if self._value is None:
             return []
         result = []
-        partial_name = ".".join(self._name.split(".")[1:])
-        result.append((partial_name, self._value))
-        if isinstance(self._definition, dict):
-            sub_config_dict = self._definition[self._value]
-            result.extend(sub_config_dict.values_list)
+        result.append((self._name, self._value))
         return result
 
     def validate(self):
-        if self._value is None and "None" not in self._definition:
+        if self._value is None and "None" not in self._possible_values:
             raise ConanException(undefined_value(self._name))
-
-        if isinstance(self._definition, dict):
-            self._definition[self._value].validate()
 
 
 class PackageOptions(object):
-    """ Optional configuration of a package. Follows the same syntax as
-    settings and all values will be converted to strings
-    """
-    def __init__(self, definition, name="options", parent_value=None):
+    def __init__(self, definition):
         definition = definition or {}
-        self._name = name  # settings, settings.compiler
-        self._parent_value = parent_value  # gcc, x86
-        cls = type(self)
-        self._data = {str(k): ConfigItem(v, "%s.%s" % (name, k), cls)
+        self._data = {str(k): PackageOption(v, str(k))
                       for k, v in definition.items()}
         self._modified = {}
 
     def copy(self):
-        """ deepcopy, recursive
-        """
-        cls = type(self)
-        result = cls({}, name=self._name, parent_value=self._parent_value)
+        raise Exception("BOOM cannot copy PackageOptions")
+        result = PackageOptions({})
         for k, v in self._data.items():
             result._data[k] = v.copy()
         return result
 
-    @classmethod
-    def loads(cls, text):
-        name = cls.__name__.lower()
-        if name == "packageoptions":
-            name = "options"
-        return cls(yaml.load(text) or {})
+    @staticmethod
+    def loads(text):
+        return PackageOptions(yaml.load(text) or {})
 
     def validate(self):
-        for field in self.fields:
-            child = self._data[field]
+        for child in self._data.values():
             child.validate()
 
     @property
@@ -347,8 +254,7 @@ class PackageOptions(object):
 
     def _check_field(self, field):
         if field not in self._data:
-            raise ConanException(undefined_field(self._name, field, self.fields,
-                                                 self._parent_value))
+            raise ConanException(undefined_option(field, self.fields))
 
     def __getattr__(self, field):
         assert field[0] != "_", "ERROR %s" % field
@@ -401,7 +307,7 @@ class PackageOptions(object):
     def values(self, vals):
         assert isinstance(vals, PackageValues)
         self.values_list = vals.as_list()
-        
+
     def propagate_upstream(self, values, down_ref, own_ref, output):
         """ update must be controlled, to not override lower
         projects options
