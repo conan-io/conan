1c1d146d489a9be73eec6c085fb676fbf60b18bf
Daniel Schiffner <dschiffner@gdv.cs.uni-frankfurt.de>
2017-01-30 16:10:23 +0100

    Adding unit tests for htpasswd

--- conans/test/auth_test.py
+++ conans/test/auth_test.py
@@ -2,8 +2,9 @@ import unittest
 from conans.test.tools import TestServer, TestClient
 from conans.test.server.utils.dummy_server_conf import create_dummy_htpasswd
 from conans.paths import CONANFILE
-from conans.util.files import save
+from conans.util.files import save, mkdir
 from conans.model.ref import ConanFileReference
+from conans.test.utils.test_files import temp_folder
 import os
 
 conan_content = """
@@ -29,12 +30,13 @@ class AuthorizeTest(unittest.TestCase):
                                       users=users)  # exported users and passwords
         self.servers["default"] = self.test_server
         #create htpasswd remote
+        base_path = temp_folder()
+        mkdir(os.path.join(base_path,".conan_server"))
+        create_dummy_htpasswd(os.path.join(base_path,".conan_server", ".htpasswd"), users)
         self.test_server2 = TestServer([(str(self.conan_reference), "pepe")],  # read permissions
                                       [(str(self.conan_reference), "pepe")],  # write permissions
+                                      base_path=base_path,#custom base path here due to .htpasswd file
                                       authentication={"htpasswd": ".htpasswd"})
-        create_dummy_htpasswd(os.path.join(self.test_server2.config_path, ".htpasswd"), users)
-        if not os.path.exists(os.path.join(self.test_server2.config_path, ".htpasswd")):
-            raise Exception()
         self.servers["htpasswd"] = self.test_server2
         
 
@@ -77,14 +79,14 @@ class AuthorizeTest(unittest.TestCase):
                                                                        ("pepe", "pepepass")]})
         save(os.path.join(self.conan.current_folder, CONANFILE), conan_content)
         self.conan.run("export lasote")
-        errors = self.conan.run("upload %s" % str(self.conan_reference))
+        errors = self.conan.run("upload %s --remote htpasswd" % str(self.conan_reference))
         # Check that return was  ok
         self.assertFalse(errors)
         # Check that upload was granted
-        self.assertTrue(os.path.exists(self.test_server.paths.export(self.conan_reference)))
+        self.assertTrue(os.path.exists(self.test_server2.paths.export(self.conan_reference)))
 
         # Check that login failed two times before ok
-        self.assertEquals(self.conan.user_io.login_index["default"], 3)
+        self.assertEquals(self.conan.user_io.login_index["htpasswd"], 3)
 
     def max_retries_htpasswd_test(self):
         """Bad login 3 times"""
@@ -93,11 +95,11 @@ class AuthorizeTest(unittest.TestCase):
                                                                     ("baduser3", "badpass3")]})
         save(os.path.join(self.conan.current_folder, CONANFILE), conan_content)
         self.conan.run("export lasote -p ./ ")
-        errors = self.conan.run("upload %s" % str(self.conan_reference), ignore_error=True)
+        errors = self.conan.run("upload %s --remote htpasswd" % str(self.conan_reference), ignore_error=True)
         # Check that return was not ok
         self.assertTrue(errors)
         # Check that upload was not granted
         self.assertFalse(os.path.exists(self.test_server.paths.export(self.conan_reference)))
 
         # Check that login failed all times
-        self.assertEquals(self.conan.user_io.login_index["default"], 3)
+        self.assertEquals(self.conan.user_io.login_index["htpasswd"], 3)
