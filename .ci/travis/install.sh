17078d0bfb1c94b93336d5a6dba2c16c384fa6dc
Tomislav Ivek <tomislav.ivek@gmail.com>
2017-01-31 10:27:01 +0100

    fix setup.py and Travis CI install script

--- .ci/travis/install.sh
+++ .ci/travis/install.sh
@@ -40,8 +40,6 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     esac
     pyenv rehash
     pyenv activate conan
-else
-    pip install -r conans/requirements_linux.txt
 fi
 
 pip install -r conans/requirements_dev.txt
