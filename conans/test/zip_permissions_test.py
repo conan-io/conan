41f143f9fb8f09f987839e52ca2eacd1ceb3fdab
Lasote <lasote@gmail.com>
2017-02-20 13:02:26 +0100

    Not for windows

--- conans/test/zip_permissions_test.py
+++ conans/test/zip_permissions_test.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import stat
 import zipfile
 from stat import ST_MODE
@@ -15,19 +16,21 @@ from conans.util.files import save
 class ZipPermissionsTest(TestCase):
 
     def test_permissions(self):
-        tmp_dir = temp_folder()
-        file_path = os.path.join(tmp_dir, "a_file.txt")
-        save(file_path, "contents")
-        os.chmod(file_path, stat.S_IRWXU)
-        zf = zipfile.ZipFile(os.path.join(tmp_dir, 'zipfile.zip'), mode='w')
-        zf.write(file_path, basename(file_path))
-        zf.close()
+        if platform.system() != "Windows":
+            for perm_set in [stat.S_IRWXU, stat.S_IRUSR]:
+                tmp_dir = temp_folder()
+                file_path = os.path.join(tmp_dir, "a_file.txt")
+                save(file_path, "contents")
+                os.chmod(file_path, perm_set)
+                zf = zipfile.ZipFile(os.path.join(tmp_dir, 'zipfile.zip'), mode='w')
+                zf.write(file_path, basename(file_path))
+                zf.close()
 
-        # Unzip and check permissions are kept
-        dest_dir = temp_folder()
-        unzip(os.path.join(tmp_dir, 'zipfile.zip'), dest_dir)
+                # Unzip and check permissions are kept
+                dest_dir = temp_folder()
+                unzip(os.path.join(tmp_dir, 'zipfile.zip'), dest_dir)
 
-        dest_file = os.path.join(dest_dir, "a_file.txt")
-        self.assertEquals(stat.S_IMODE(os.stat(dest_file).st_mode), stat.S_IRWXU)
+                dest_file = os.path.join(dest_dir, "a_file.txt")
+                self.assertEquals(stat.S_IMODE(os.stat(dest_file).st_mode), perm_set)
 
 
