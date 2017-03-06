81595ea3ff81f77f1e8fd2f815aa2c2fb8c68b30
Lasote <lasote@gmail.com>
2017-03-06 17:57:09 +0100

    Removed tests

--- conans/test/integration/diamond_test.py
+++ conans/test/integration/diamond_test.py
@@ -72,6 +72,10 @@ class DiamondTest(unittest.TestCase):
         self._diamond_test(install=install, use_cmake=False)
 
     def _diamond_test(self, install="install", use_cmake=True, cmake_targets=False):
+
+        if not use_cmake and platform.system() == "SunOS":
+            return  # If is using sun-cc the gcc generator doesn't work
+
         self._export_upload("Hello0", "0.1", use_cmake=use_cmake, cmake_targets=cmake_targets)
         self._export_upload("Hello1", "0.1", ["Hello0/0.1@lasote/stable"], use_cmake=use_cmake,
                             cmake_targets=cmake_targets)
