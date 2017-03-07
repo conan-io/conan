35f1b858330fbfed1adffbb51547afc1a5201c76
Claas Diederichs <claas@cd-email.de>
2017-03-07 12:22:55 +0100

    Cleanup and xml tests

--- conans/client/generators/visualstudio.py
+++ conans/client/generators/visualstudio.py
@@ -7,8 +7,7 @@ class VisualStudioGenerator(Generator):
     template = '''<?xml version="1.0" encoding="utf-8"?>
 <Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
   <ImportGroup Label="PropertySheets" />
-  <PropertyGroup Label="UserMacros" />
-  {item_properties}
+  <PropertyGroup Label="UserMacros" />{item_properties}
   <PropertyGroup>
     <ExecutablePath>{bin_dirs}$(ExecutablePath)</ExecutablePath>
   </PropertyGroup>
@@ -16,7 +15,6 @@ class VisualStudioGenerator(Generator):
     <LocalDebuggerEnvironment>PATH=%PATH%;{bin_dirs}</LocalDebuggerEnvironment>
     <DebuggerFlavor>WindowsLocalDebugger</DebuggerFlavor>
   </PropertyGroup>
-  {item_properties}
   <ItemDefinitionGroup>
     <ClCompile>
       <AdditionalIncludeDirectories>{include_dirs}%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
@@ -41,49 +39,47 @@ class VisualStudioGenerator(Generator):
   #   <Conan.Defines.{name}>{defines}</Conan.Defines.{name}>
   #   <Conan.CompileDefinitions.{name}>{definitions}</Conan.CompileDefinitions.{name}>
   #   <Conan.CxxFlags.{name}>{cxx_flags}</Conan.CxxFlags.{name}>
-  #   <Conan.SharedLinkerFlags.{shared_linker_flags}>{include_dirs}</Conan.SharedLinkerFlags.{name}>
+  #   <Conan.SharedLinkerFlags.{name}>{include_dirs}</Conan.SharedLinkerFlags.{name}>
   #   <Conan.ExeLinkerFlags.{name}>{shared_exe_flags}</Conan.ExeLinkerFlags.{name}>
   #   <Conan.CFlags.{name}>{c_flags}</Conan.CFlags.{name}>
   # </PropertyGroup>'''
 
-    item_template = '''  <PropertyGroup Label="{name}">
-     <Conan.{name}.Root>{root_dir}</Conan.{name}.Root>
-     <Conan.IncludeDirs.{name}>{include_dirs}</Conan.IncludeDirs.{name}>
-     <Conan.LibDirs.{name}>{lib_dirs}</Conan.LibDirs.{name}>
-     <Conan.BinDirs.{name}>{bin_dirs}</Conan.BinDirs.{name}>
-     <Conan.Libs.{name}>{libs}</Conan.Libs.{name}>
-     <Conan.Defines.{name}>{definitions}</Conan.Defines.{name}>
-     <Conan.CompilerFlags.{name}>{compiler_flags}</Conan.CxxFlags.{name}>
-     <Conan.SharedLinkerFlags.{linker_flags}>{include_dirs}</Conan.SharedLinkerFlags.{name}>
-     <Conan.ExeLinkerFlags.{name}>{exe_flags}</Conan.ExeLinkerFlags.{name}>
-   </PropertyGroup>'''
+    item_template = '''
+  <PropertyGroup Label="Conan.{name}">
+    <Conan.{name}.Root>{root_dir}</Conan.{name}.Root>
+    <Conan.IncludeDirs.{name}>{include_dirs}</Conan.IncludeDirs.{name}>
+    <Conan.LibDirs.{name}>{lib_dirs}</Conan.LibDirs.{name}>
+    <Conan.BinDirs.{name}>{bin_dirs}</Conan.BinDirs.{name}>
+    <Conan.Libs.{name}>{libs}</Conan.Libs.{name}>
+    <Conan.Defines.{name}>{definitions}</Conan.Defines.{name}>
+    <Conan.CompilerFlags.{name}>{compiler_flags}</Conan.CompilerFlags.{name}>
+    <Conan.SharedLinkerFlags.{name}>{linker_flags}</Conan.SharedLinkerFlags.{name}>
+    <Conan.ExeLinkerFlags.{name}>{exe_flags}</Conan.ExeLinkerFlags.{name}>
+  </PropertyGroup>'''
 
-    def _format_template(self, template, dep_cpp_info, user_items=dict()):
-        items = user_items.copy()
-        items['bin_dirs'] = ";".join(dep_cpp_info.bin_paths)
-        items['include_dirs'] = "".join(dep_cpp_info.include_paths)
-        items['lib_dirs'] = "".join(dep_cpp_info.lib_paths)
-        items['libs'] = "".join(['%s.lib;' % lib if not lib.endswith(".lib")
+    def _add_common_fields(self, fields, dep_cpp_info):
+        fields['bin_dirs'] = "".join( "%s;" % p for p in dep_cpp_info.bin_paths)
+        fields['include_dirs'] = "".join( "%s;" % p for p in dep_cpp_info.include_paths)
+        fields['lib_dirs'] = "".join( "%s;" % p for p in dep_cpp_info.lib_paths)
+        fields['libs'] = "".join(['%s.lib;' % lib if not lib.endswith(".lib")
                              else '%s;' % lib for lib in dep_cpp_info.libs])
-        items['definitions'] = "".join("%s;" % d for d in dep_cpp_info.defines)
-        items['compiler_flags'] = " ".join(dep_cpp_info.cppflags + dep_cpp_info.cflags)
-        items['linker_flags'] = " ".join(dep_cpp_info.sharedlinkflags)
-        items['exe_flags'] = " ".join(dep_cpp_info.exelinkflags)
-
-        print(items)
-        line = template.format(**items)
-        print(line)
-        return line
+        fields['definitions'] = "".join("%s;" % d for d in dep_cpp_info.defines)
+        fields['compiler_flags'] = " ".join(dep_cpp_info.cppflags + dep_cpp_info.cflags)
+        fields['linker_flags'] = " ".join(dep_cpp_info.sharedlinkflags)
+        fields['exe_flags'] = " ".join(dep_cpp_info.exelinkflags)
 
     def _format_items(self, deps_cpp_info):
         sections = []
         for dep_name, dep_cpp_info in self.deps_build_info.dependencies:
-            items = {
+            fields = {
                 'root_dir': dep_cpp_info.rootpath,
                 'name': dep_name
             }
-            sections.append(self._format_template(self.item_template, dep_cpp_info, items))
-        return "\n".join(sections)
+            self._add_common_fields(fields, dep_cpp_info)
+            fields = {k: v.strip(";") for k, v in fields.items()}
+            section = self.item_template.format(**fields)
+            sections.append(section)
+        return "".join(sections)
 
     @property
     def filename(self):
@@ -92,5 +88,9 @@ class VisualStudioGenerator(Generator):
     @property
     def content(self):
         per_item_props = self._format_items(self._deps_build_info)
-        return self._format_template(self.template, self._deps_build_info, {'item_properties': per_item_props})
+        fields = {
+            'item_properties': per_item_props
+        }
+        self._add_common_fields(fields, self._deps_build_info)
+        return self.template.format(**fields)
 
--- conans/test/generators/visual_studio_test.py
+++ conans/test/generators/visual_studio_test.py
@@ -1,5 +1,6 @@
 import re
 import unittest
+import xml.etree.ElementTree
 
 from conans.client.generators import VisualStudioGenerator
 
@@ -11,8 +12,7 @@ from conans.model.ref import ConanFileReference
 
 
 class VisualStudioGeneratorTest(unittest.TestCase):
-
-    def variables_setup_test(self):
+    def _createInfo(self):
         conanfile = ConanFile(None, None, Settings({}), None)
         ref = ConanFileReference.loads("MyPkg/0.1@lasote/stables")
         cpp_info = CppInfo("dummy_root_folder1")
@@ -23,12 +23,24 @@ class VisualStudioGeneratorTest(unittest.TestCase):
         cpp_info.defines = ["MYDEFINE2"]
         conanfile.deps_cpp_info.update(cpp_info, ref)
         generator = VisualStudioGenerator(conanfile)
-        content = generator.content
-        lines = content.splitlines()
-        self.assertIn('<PropertyGroup Label="MyPkg">', content)
+        return generator.content
+
+    def valid_xml_test(self):
+        data = self._createInfo()
+        try:
+            xml.etree.ElementTree.fromstring(data)
+        except xml.etree.ElementTree.ParseError as err:
+            self.fail("Visual studio generated code is not valid! Error %s:\n%s " % (str(err), data))
+
+
+    def variables_setup_test(self):
+        content = self._createInfo()
+        self.assertIn('<PropertyGroup Label="Conan.MyPkg">', content)
         self.assertIn("<Conan.MyPkg.Root>dummy_root_folder1</Conan.MyPkg.Root>", content)
         self.assertIn("<Conan.Defines.MyPkg>MYDEFINE1</Conan.Defines.MyPkg>", content)
         self.assertIn("<Conan.Defines.MyPkg2>MYDEFINE2</Conan.Defines.MyPkg2>", content)
+        self.assertIn("<PreprocessorDefinitions>MYDEFINE2;MYDEFINE1;"
+                      "%(PreprocessorDefinitions)</PreprocessorDefinitions>", content)
 
     def multi_flag_test(self):
         conanfile = ConanFile(None, None, Settings({}), None)
@@ -43,6 +55,7 @@ class VisualStudioGeneratorTest(unittest.TestCase):
         conanfile.deps_cpp_info.update(cpp_info, ref)
         generator = VisualStudioGenerator(conanfile)
         content = generator.content
+        print(content)
         self.assertIn("<Conan.CompilerFlags.MyPkg>-DGTEST_USE_OWN_TR1_TUPLE=1 -DGTEST_LINKED_AS_SHARED_LIBRARY=1",
                       content)
         self.assertIn("<AdditionalOptions>-DGTEST_USE_OWN_TR1_TUPLE=1 -DGTEST_LINKED_AS_SHARED_LIBRARY=1"
