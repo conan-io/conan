ab5ee19a9326f76aeae440f024320f09e020b82f
Lasote <lasote@gmail.com>
2017-04-10 15:07:14 +0200

    Fixing test

--- conans/test/util/tools_test.py
+++ conans/test/util/tools_test.py
@@ -277,12 +277,12 @@ class ToolsTest(unittest.TestCase):
 
         conanfile = MockConanfile()
         tools.run_in_windows_bash(conanfile, "a_command.bat")
-        self.assertIn("bash --login -c", conanfile.output)
-        self.assertIn("^&^& a_command.bat ^", conanfile.output)
+        self.assertIn("bash --login -c", conanfile.command)
+        self.assertIn("^&^& a_command.bat ^", conanfile.command)
 
         with tools.environment_append({"CONAN_BASH_PATH": "path/to"}):
             tools.run_in_windows_bash(conanfile, "a_command.bat")
-            self.assertIn("path/to/bash --login -c", conanfile.output)
+            self.assertIn("path/to/bash --login -c", conanfile.command)
 
     @attr('slow')
     def build_vs_project_test(self):
