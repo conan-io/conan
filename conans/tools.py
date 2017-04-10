ceceb275849daa8e0f7054f453cf6c255db72af9
Lasote <lasote@gmail.com>
2017-04-10 14:28:38 +0200

    Custom bash path

--- conans/tools.py
+++ conans/tools.py
@@ -61,7 +61,8 @@ def run_in_windows_bash(conanfile, bashcmd, cwd=None):
         curdir = unix_path(cwd or os.path.abspath(os.path.curdir))
         # Needed to change to that dir inside the bash shell
         to_run = 'cd "%s" && %s ' % (curdir, bashcmd)
-        wincmd = 'bash --login -c %s' % escape_windows_cmd(to_run)
+        custom_bash_path = os.getenv("CONAN_BASH_PATH", "")
+        wincmd = '%s --login -c %s' % (os.path.join(custom_bash_path, "bash"), escape_windows_cmd(to_run))
         conanfile.output.info('run_in_windows_bash: %s' % wincmd)
         conanfile.run(wincmd)
 
