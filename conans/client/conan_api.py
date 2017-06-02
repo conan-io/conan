2e95e3a7447732afaaf3ab198f3b5d91ad592b31
Luis Martinez de Bartolome <lasote@gmail.com>
2017-06-02 16:13:24 +0200

    Factory new

--- conans/client/conan_api.py
+++ conans/client/conan_api.py
@@ -80,6 +80,59 @@ default_manifest_folder = '.conan_manifests'
 
 class ConanAPIV1(object):
 
+    @staticmethod
+    def new():
+        """Factory"""
+        def instance_remote_manager(client_cache):
+            requester = requests.Session()
+            requester.proxies = client_cache.conan_config.proxies
+            # Verify client version against remotes
+            version_checker_requester = VersionCheckerRequester(requester, Version(CLIENT_VERSION),
+                                                                Version(MIN_SERVER_COMPATIBLE_VERSION),
+                                                                out)
+            # To handle remote connections
+            put_headers = client_cache.read_put_headers()
+            rest_api_client = RestApiClient(out, requester=version_checker_requester, put_headers=put_headers)
+            # To store user and token
+            localdb = LocalDB(client_cache.localdb)
+            # Wraps RestApiClient to add authentication support (same interface)
+            auth_manager = ConanApiAuthManager(rest_api_client, user_io, localdb)
+            # Handle remote connections
+            remote_manager = RemoteManager(client_cache, auth_manager, out)
+            return remote_manager
+
+        use_color = get_env("CONAN_COLOR_DISPLAY", 1)
+        if use_color and hasattr(sys.stdout, "isatty") and sys.stdout.isatty():
+            import colorama
+            colorama.init()
+            color = True
+        else:
+            color = False
+        out = ConanOutput(sys.stdout, color)
+        user_io = UserIO(out=out)
+
+        user_folder = os.getenv("CONAN_USER_HOME", conan_expand_user("~"))
+
+        try:
+            client_cache = migrate_and_get_client_cache(user_folder, out)
+        except Exception as e:
+            out.error(str(e))
+            sys.exit(True)
+
+        with tools.environment_append(client_cache.conan_config.env_vars):
+            # Adjust CONAN_LOGGING_LEVEL with the env readed
+            conans.util.log.logger = configure_logger()
+
+            # Get the new command instance after migrations have been done
+            remote_manager = instance_remote_manager(client_cache)
+
+            # Get a search manager
+            search_adapter = DiskSearchAdapter()
+            search_manager = DiskSearchManager(client_cache, search_adapter)
+            conan = Conan(client_cache, user_io, get_conan_runner(), remote_manager, search_manager, os.getcwd())
+
+        return conan
+
     def __init__(self, client_cache, user_io, runner, remote_manager, search_manager, cwd):
         assert isinstance(user_io, UserIO)
         assert isinstance(client_cache, ClientCache)
@@ -470,59 +523,6 @@ def _parse_manifests_arguments(verify, manifests, manifests_interactive, referen
         manifest_verify = manifest_interactive = False
 
     return manifest_folder, manifest_interactive, manifest_verify
-
-def get_conan_api():
-
-    def instance_remote_manager(client_cache):
-        requester = requests.Session()
-        requester.proxies = client_cache.conan_config.proxies
-        # Verify client version against remotes
-        version_checker_requester = VersionCheckerRequester(requester, Version(CLIENT_VERSION),
-                                                            Version(MIN_SERVER_COMPATIBLE_VERSION),
-                                                            out)
-        # To handle remote connections
-        put_headers = client_cache.read_put_headers()
-        rest_api_client = RestApiClient(out, requester=version_checker_requester, put_headers=put_headers)
-        # To store user and token
-        localdb = LocalDB(client_cache.localdb)
-        # Wraps RestApiClient to add authentication support (same interface)
-        auth_manager = ConanApiAuthManager(rest_api_client, user_io, localdb)
-        # Handle remote connections
-        remote_manager = RemoteManager(client_cache, auth_manager, out)
-        return remote_manager
-
-    use_color = get_env("CONAN_COLOR_DISPLAY", 1)
-    if use_color and hasattr(sys.stdout, "isatty") and sys.stdout.isatty():
-        import colorama
-        colorama.init()
-        color = True
-    else:
-        color = False
-    out = ConanOutput(sys.stdout, color)
-    user_io = UserIO(out=out)
-
-    user_folder = os.getenv("CONAN_USER_HOME", conan_expand_user("~"))
-
-    try:
-        client_cache = migrate_and_get_client_cache(user_folder, out)
-    except Exception as e:
-        out.error(str(e))
-        sys.exit(True)
-
-    with tools.environment_append(client_cache.conan_config.env_vars):
-        # Adjust CONAN_LOGGING_LEVEL with the env readed
-        conans.util.log.logger = configure_logger()
-
-        # Get the new command instance after migrations have been done
-        remote_manager = instance_remote_manager(client_cache)
-
-        # Get a search manager
-        search_adapter = DiskSearchAdapter()
-        search_manager = DiskSearchManager(client_cache, search_adapter)
-        conan = Conan(client_cache, user_io, get_conan_runner(), remote_manager, search_manager, os.getcwd())
-
-    return conan
-
 
 def get_conan_runner():
     print_commands_to_output = get_env("CONAN_PRINT_RUN_COMMANDS", False)
