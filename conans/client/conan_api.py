3abf1ba86e3f22395c2579de8a8eede5d71a83df
Uilian Ries <uilianries@gmail.com>
2017-06-19 14:39:22 -0300

    #1399 Conan new generates Gitlab CI (#1401)
    - Added support to `conan new` creates GitLab file
    - GitLab supports Gcc and Clang jobs
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/client/conan_api.py
+++ conans/client/conan_api.py
@@ -145,7 +145,7 @@ class ConanAPIV1(object):
     @api_method
     def new(self, name, header=False, pure_c=False, test=False, exports_sources=False, bare=False, cwd=None,
             visual_versions=None, linux_gcc_versions=None, linux_clang_versions=None, osx_clang_versions=None,
-            shared=None, upload_url=None, gitignore=None):
+            shared=None, upload_url=None, gitignore=None, gitlab_gcc_versions=None, gitlab_clang_versions=None):
         from conans.client.new import get_files
         cwd = prepare_cwd(cwd)
         files = get_files(name, header=header, pure_c=pure_c, test=test,
@@ -154,7 +154,9 @@ class ConanAPIV1(object):
                           linux_gcc_versions=linux_gcc_versions,
                           linux_clang_versions=linux_clang_versions,
                           osx_clang_versions=osx_clang_versions, shared=shared,
-                          upload_url=upload_url, gitignore=gitignore)
+                          upload_url=upload_url, gitignore=gitignore,
+                          gitlab_gcc_versions=gitlab_gcc_versions,
+                          gitlab_clang_versions=gitlab_clang_versions)
 
         save_files(cwd, files)
         for f in sorted(files):
