39f74da37138d1922c4f35b8b34da321c7967880
Anton Matosov <anton.matosov@gmail.com>
2017-07-08 19:38:54 -0700

    Add Arduino as OS and AVR as Arch

--- conans/client/conf/__init__.py
+++ conans/client/conf/__init__.py
@@ -22,7 +22,8 @@ os:
         version: ["7.0", "7.1", "8.0", "8.1", "8.2", "8.3", "9.0", "9.1", "9.2", "9.3", "10.0", "10.1", "10.2", "10.3"]
     FreeBSD:
     SunOS:
-arch: [x86, x86_64, ppc64le, ppc64, armv6, armv7, armv7hf, armv8, sparc, sparcv9, mips, mips64]
+    Arduino:
+arch: [x86, x86_64, ppc64le, ppc64, armv6, armv7, armv7hf, armv8, sparc, sparcv9, mips, mips64, avr]
 compiler:
     sun-cc:
        version: ["5.10", "5.11", "5.12", "5.13", "5.14"]
