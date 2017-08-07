dec1dd6a3300861d35f4e139957913ba95378028
memsharded <james@conan.io>
2017-08-08 01:37:18 +0200

    add upload test

--- conans/test/functional/read_only_test.py
+++ conans/test/functional/read_only_test.py
@@ -1,5 +1,5 @@
 import unittest
-from conans.test.utils.tools import TestClient
+from conans.test.utils.tools import TestClient, TestServer
 from conans.util.files import load, save
 import os
 from conans.model.ref import ConanFileReference, PackageReference
@@ -8,7 +8,9 @@ from conans.model.ref import ConanFileReference, PackageReference
 class ReadOnlyTest(unittest.TestCase):
 
     def setUp(self):
-        client = TestClient()
+        test_server = TestServer()
+        client = TestClient(servers={"default": test_server},
+                            users={"default": [("lasote", "mypass")]})
         client.run("--version")
         conf_path = client.client_cache.conan_conf_path
         conf = load(conf_path)
@@ -22,11 +24,11 @@ class MyPkg(ConanFile):
 """
         client.save({"conanfile.py": conanfile,
                      "myheader.h": "my header"})
-        client.run("create Pkg/0.1@user/channel")
+        client.run("create Pkg/0.1@lasote/channel")
         self.client = client
 
     def basic_test(self):
-        ref = PackageReference(ConanFileReference.loads("Pkg/0.1@user/channel"),
+        ref = PackageReference(ConanFileReference.loads("Pkg/0.1@lasote/channel"),
                                "5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9")
         path = os.path.join(self.client.client_cache.package(ref), "myheader.h")
         with self.assertRaises(IOError):
@@ -36,6 +38,12 @@ class MyPkg(ConanFile):
 
     def remove_test(self):
         self.client.run("search")
-        self.assertIn("Pkg/0.1@user/channel", self.client.out)
+        self.assertIn("Pkg/0.1@lasote/channel", self.client.out)
         self.client.run("remove Pkg* -f")
-        self.assertNotIn("Pkg/0.1@user/channel", self.client.out)
+        self.assertNotIn("Pkg/0.1@lasote/channel", self.client.out)
+
+    def upload_test(self):
+        self.client.run("upload * --all --confirm")
+        self.client.run("remove Pkg* -f")
+        self.client.run("install Pkg/0.1@lasote/channel")
+        self.basic_test()
