4c168a4474371f962fc623be25b65b663fc7da9e
memsharded <james@conan.io>
2017-09-11 01:04:17 +0200

    creatinf folder

--- .ci/appveyor/install.bat
+++ .ci/appveyor/install.bat
@@ -2,9 +2,9 @@ if not exist "C:\mingw64" appveyor DownloadFile "https://s3-eu-west-1.amazonaws.
 if not exist "C:\mingw64" 7z x x86_64-6.3.0-release-posix-sjlj-rt_v5-rev1.7z -oc:\
 
 set CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.0-win64-x64.zip"
-appveyor DownloadFile %CMAKE_URL% -FileName cmake.zip
 mkdir C:\projects\deps
 cd C:\projects\deps
+appveyor DownloadFile %CMAKE_URL% -FileName cmake.zip
 7z x cmake.zip -oC:\projects\deps > nul
 move C:\projects\deps\cmake-* C:\projects\deps\cmake # Move to a version-agnostic directory
 set PATH=C:\projects\deps\cmake\bin;%PATH%
