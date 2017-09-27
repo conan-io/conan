5dc5aecb0bce7a41f399e7a8f4c5fb7e7ecaac95
SSE4 <tomskside@gmail.com>
2017-09-27 16:55:04 +0700

    - do not use sudo if we already running as root

--- conans/client/tools/system_pm.py
+++ conans/client/tools/system_pm.py
@@ -11,6 +11,8 @@ class SystemPackageTool(object):
     def __init__(self, runner=None, os_info=None, tool=None):
         env_sudo = os.environ.get("CONAN_SYSREQUIRES_SUDO", None)
         self._sudo = (env_sudo != "False" and env_sudo != "0")
+        if os.geteuid() == 0:
+            self._sudo = False
         os_info = os_info or OSInfo()
         self._is_up_to_date = False
         self._tool = tool or self._create_tool(os_info)
