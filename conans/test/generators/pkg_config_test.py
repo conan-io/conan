29a104ab12102cb0054e8b748cfc73ad2ae41336
memsharded <james@conan.io>
2017-10-06 17:41:06 +0200

    moved test

--- conans/test/generators/pkg_config_test.py
+++ conans/test/generators/pkg_config_test.py
@@ -1,16 +1,12 @@
 import unittest
 
-import six
-
 from conans.client.generators.pkg_config import PkgConfigGenerator
 from conans.model.settings import Settings
 from conans.model.conan_file import ConanFile
 from conans.model.build_info import CppInfo
 from conans.model.ref import ConanFileReference
-from nose.plugins.attrib import attr
 
 
-@attr('slow')
 class PkgGeneratorTest(unittest.TestCase):
 
     def variables_setup_test(self):
