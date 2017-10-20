e04a297339661224e538e35453a9e49552dcee15
Luis Martinez de Bartolome <lasote@gmail.com>
2017-10-20 11:28:37 +0200

    Complete help extracted from docs

--- conans/client/command.py
+++ conans/client/command.py
@@ -819,6 +819,16 @@ class Command(object):
 
     def upload(self, *args):
         """ Uploads a recipe and binary packages to a remote.
+
+            If you use the --force variable, it won't check the package date. It will override
+            the remote with the local package.
+            If you use a pattern instead of a conan recipe reference you can use the -c or
+            --confirm option to upload all the matching recipes.
+            If you use the --retry option you can specify how many times should conan try to upload
+            the packages in case of failure. The default is 2.
+            With --retry_wait you can specify the seconds to wait between upload attempts.
+            If no remote is specified, the first configured remote (by default conan.io, use
+            'conan remote list' to list the remotes) will be used.
         """
         parser = argparse.ArgumentParser(description=self.upload.__doc__,
                                          prog="conan upload")
