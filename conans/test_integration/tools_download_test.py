ad3e5bc8d3cf318a6845bd4b73ffa1b7322d0f53
memsharded <james@conan.io>
2017-10-30 16:59:38 +0100

    adding more tests

--- conans/test_integration/tools_download_test.py
+++ conans/test_integration/tools_download_test.py
@@ -15,3 +15,21 @@ class ToolsDownloadTest(unittest.TestCase):
                            "README.rst")
             content = load(os.path.join(tmp_folder, "README.rst"))
             self.assertIn(":alt: Conan develop coverage", content)
+
+    def test_download_zip(self):
+        tmp_folder = temp_folder()
+        with tools.chdir(tmp_folder):
+            tools.download("http://www.agentpp.com/download/snmp++-3.3.5.tar.gz",
+                           "agenpro")
+            tools.untargz("agenpro")
+            content = load(os.path.join(tmp_folder, "snmp++-3.3.5/include/snmp_pp/vb.h"))
+            self.assertIn("} // end of namespace Snmp_pp", content)
+
+    def test_download_small_zip(self):
+        tmp_folder = temp_folder()
+        with tools.chdir(tmp_folder):
+            tools.download("http://www.agentpp.com/download/vs2013.zip",
+                           "agenpro")
+            tools.unzip("agenpro")
+            content = load(os.path.join(tmp_folder, "vs2013/AgentX++/include/config.h"))
+            self.assertIn("/* define _SNMPv3 if you want to use SNMPv3 */", content)
