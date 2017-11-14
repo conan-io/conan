db23ada1cf1e68ccd8557f7fe52de5fbc1f7df4e
memsharded <james@conan.io>
2017-11-14 21:41:59 +0100

    compiler version quirks

--- conans/client/conf/__init__.py
+++ conans/client/conf/__init__.py
@@ -35,7 +35,10 @@ compiler:
         threads: [None, posix]
         libcxx: [libCstd, libstdcxx, libstlport, libstdc++]
     gcc:
-        version: ["4.1", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9", "5.1", "5.2", "5.3", "5.4", "6.1", "6.2", "6.3", "6.4", "7.1", "7.2"]
+        version: ["4.1", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9",
+                  "5", "5.1", "5.2", "5.3", "5.4",
+                  "6", "6.1", "6.2", "6.3", "6.4",
+                  "7", "7.1", "7.2"]
         libcxx: [libstdc++, libstdc++11]
         threads: [None, posix, win32] #  Windows MinGW
         exception: [None, dwarf2, sjlj, seh] # Windows MinGW
