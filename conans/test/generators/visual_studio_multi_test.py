3cf123ff256edb46e33f6487d1a090a5d222d4ac
jeroendv <fly.jdv@gmail.com>
2017-12-01 18:34:34 +0100

    Visual studio multi toolset (#2057)
    * add a VisualStudioToolsetMultiGenerator
    
    * create VsSetting objects for mapping conan to VS settings
    
    * create VsSetting objects for mapping conan to VS settings
    
    * fix unit test for changed whitespace in condition string
    
    * refactor Generator to create GeneratorHelper
    
    * add unit test
    
    * remove visualstudiotoolset_multi.py
    
    * some refactors to vs_multi generators
    
    * visual_studio_multi_toolset removed, made default

--- conans/test/generators/visual_studio_multi_test.py
+++ conans/test/generators/visual_studio_multi_test.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import unittest
 import os
+
 from nose.plugins.attrib import attr
+from nose_parameterized import parameterized
+
 from conans.model.settings import Settings
 from conans.model.conan_file import ConanFile
 from conans.model.build_info import CppInfo
 from conans.model.ref import ConanFileReference
 from conans.client.conf import default_settings_yml
-
 from conans.client.generators import VisualStudioMultiGenerator
 from conans.tools import chdir
 from conans.test.utils.test_files import temp_folder
@@ -17,15 +16,18 @@ from conans.test.utils.test_files import temp_folder
 
 @attr('visual_studio')
 class VisualStudioMultiGeneratorTest(unittest.TestCase):
-    def valid_xml_test(self):
 
+    @parameterized.expand([(False, ), (True, )])
+    def valid_xml_test(self, use_toolset):
         tempdir = temp_folder()
         with chdir(tempdir):
             settings = Settings.loads(default_settings_yml)
             settings.os = "Windows"
             settings.compiler = "Visual Studio"
-            settings.compiler.version = "12"
+            settings.compiler.version = "11"
             settings.compiler.runtime = "MD"
+            if use_toolset:
+                settings.compiler.toolset = "v110"
             conanfile = ConanFile(None, None, Settings({}), None)
 
             ref = ConanFileReference.loads("MyPkg/0.1@user/testing")
@@ -37,7 +39,6 @@ class VisualStudioMultiGeneratorTest(unittest.TestCase):
 
             settings.arch = "x86"
             settings.build_type = "Debug"
-            settings.compiler.version = "12"
             conanfile.settings = settings
 
             generator = VisualStudioMultiGenerator(conanfile)
@@ -46,20 +47,21 @@ class VisualStudioMultiGeneratorTest(unittest.TestCase):
 
             self.assertEqual(2, len(content))
             self.assertIn('conanbuildinfo_multi.props', content.keys())
-            self.assertIn('conanbuildinfo_debug_win32_12.props', content.keys())
+            self.assertIn('conanbuildinfo_debug_win32_v110.props', content.keys())
 
             content_multi = content['conanbuildinfo_multi.props']
-            self.assertIn("<Import Condition=\" '$(Configuration)' == 'Debug' "
+            self.assertIn("<Import Condition=\"'$(Configuration)' == 'Debug' "
                           "And '$(Platform)' == 'Win32' "
-                          "And '$(VisualStudioVersion)' == '12.0' \" "
-                          "Project=\"conanbuildinfo_debug_win32_12.props\"/>", content_multi)
+                          "And '$(PlatformToolset)' == 'v110'\" "
+                          "Project=\"conanbuildinfo_debug_win32_v110.props\"/>", content_multi)
 
             with open('conanbuildinfo_multi.props', 'w') as f:
                 f.write(content_multi)
 
             settings.arch = "x86_64"
             settings.build_type = "Release"
-            settings.compiler.version = "14"
+            settings.compiler.version = "15"
+            settings.compiler.toolset = "v141"
             conanfile.settings = settings
 
             generator = VisualStudioMultiGenerator(conanfile)
@@ -68,16 +70,16 @@ class VisualStudioMultiGeneratorTest(unittest.TestCase):
 
             self.assertEqual(2, len(content))
             self.assertIn('conanbuildinfo_multi.props', content.keys())
-            self.assertIn('conanbuildinfo_release_x64_14.props', content.keys())
+            self.assertIn('conanbuildinfo_release_x64_v141.props', content.keys())
 
             content_multi = content['conanbuildinfo_multi.props']
-            self.assertIn("<Import Condition=\" '$(Configuration)' == 'Debug' "
+            self.assertIn("<Import Condition=\"'$(Configuration)' == 'Debug' "
                           "And '$(Platform)' == 'Win32' "
-                          "And '$(VisualStudioVersion)' == '12.0' \" "
-                          "Project=\"conanbuildinfo_debug_win32_12.props\"/>", content_multi)
-            self.assertIn("<Import Condition=\" '$(Configuration)' == 'Release' "
+                          "And '$(PlatformToolset)' == 'v110'\" "
+                          "Project=\"conanbuildinfo_debug_win32_v110.props\"/>", content_multi)
+            self.assertIn("<Import Condition=\"'$(Configuration)' == 'Release' "
                           "And '$(Platform)' == 'x64' "
-                          "And '$(VisualStudioVersion)' == '14.0' \" "
-                          "Project=\"conanbuildinfo_release_x64_14.props\"/>", content_multi)
+                          "And '$(PlatformToolset)' == 'v141'\" "
+                          "Project=\"conanbuildinfo_release_x64_v141.props\"/>", content_multi)
 
             os.unlink('conanbuildinfo_multi.props')
