6c851fc644d885f30a1a89315da027b9d747804b
Luis Martinez de Bartolome <lasote@gmail.com>
2017-12-14 13:29:13 +0100

    Excluded meson tests python 2

--- conans/test_integration/meson_test.py
+++ conans/test_integration/meson_test.py
@@ -16,6 +16,11 @@ class PkgConfigGeneratorTest(unittest.TestCase):
 
     @parameterized.expand([(True, ), (False, )])
     def basic_test(self, no_copy_source):
+
+        # meson > py 3.4
+        if sys.version_info[0] < 3 or sys.version_info[1] < 5:
+            return
+
         client = TestClient()
         conanfile = """from conans import ConanFile, Meson, load
 import os
@@ -54,10 +59,9 @@ class Conan(ConanFile):
         self.assertEqual(load(os.path.join(build_folder, "package/include/header.h")), "//myheader.h")
 
     def test_base(self):
-        pyver = sys.version_info
 
-        # FIXME: Appveyor is not locating meson in py 3.4
-        if platform.system() == "Windows" and (pyver[0] < 3 or pyver[1] < 5):
+        # meson > py 3.4
+        if sys.version_info[0] < 3 or sys.version_info[1] < 5:
             return
 
         client = TestClient(path_with_spaces=False)
