15ff8fda2f9370674b729428b918414cd8212c91
danimtb <danimanzaneque@gmail.com>
2017-12-21 00:02:34 +0100

    Some test command tests for path

--- conans/test/command/test_package_test.py
+++ conans/test/command/test_package_test.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from conans.paths import CONANFILE
 from conans.test.utils.tools import TestClient
 
@@ -40,7 +41,7 @@ class TestConanLib(ConanFile):
         client.save({CONANFILE: conanfile,
                      "test_package/conanfile.py": test_conanfile})
         client.run("create lasote/stable")
-        client.run("test test_package  Hello/0.1@lasote/stable")
+        client.run("test test_package Hello/0.1@lasote/stable")
         self.assertNotIn("Exporting package recipe", client.out)
         self.assertNotIn("WARN: Forced build from source", client.out)
         self.assertNotIn("Package '5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9' created", client.out)
@@ -48,7 +49,7 @@ class TestConanLib(ConanFile):
         self.assertIn("Hello/0.1@lasote/stable: Already installed!", client.out)
 
         client.save({"test_package/conanfile.py": test_conanfile}, clean_first=True)
-        client.run("test test_package  Hello/0.1@lasote/stable")
+        client.run("test test_package Hello/0.1@lasote/stable")
         self.assertNotIn("Hello/0.1@lasote/stable: Configuring sources", client.out)
         self.assertNotIn("Hello/0.1@lasote/stable: Generated conaninfo.txt", client.out)
         self.assertIn("Hello/0.1@lasote/stable: Already installed!", client.out)
@@ -98,3 +99,19 @@ class ConanLib(ConanFile):
         client.run("create lasote/stable")
         self.assertIn("Hello/0.1@lasote/stable: Configuring sources", client.user_io.out)
         self.assertIn("Hello/0.1@lasote/stable: Generated conaninfo.txt", client.user_io.out)
+
+    def test_with_path_errors_test(self):
+        client = TestClient()
+        client.save({"conanfile.txt": "contents"}, clean_first=True)
+
+        # Path with conanfile.txt
+        error = client.run("test conanfile.txt other/0.2@user2/channel2", ignore_error = True)
+        self.assertTrue(error)
+        self.assertIn("A conanfile.py is needed (not valid conanfile.txt)", client.out)
+
+        # Path with wrong conanfile path
+        error = client.run("test not_real_dir/conanfile.py other/0.2@user2/channel2",
+                           ignore_error = True)
+        self.assertTrue(error)
+        self.assertIn("Conanfile not found: %s" % os.path.join(client.current_folder, "not_real_dir",
+                                                               "conanfile.py"), client.out)
