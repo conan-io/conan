1b827e04728bf1aaae362411eda0738c39883b34
danimtb <danimanzaneque@gmail.com>
2017-12-20 17:16:38 +0100

    Removed exception

--- conans/client/conan_api.py
+++ conans/client/conan_api.py
@@ -571,14 +571,12 @@ class ConanAPIV1(object):
         :return: Conanfile dirname path
         '"""
         if os.path.isfile(path):
-            path = os.path.dirname(path)
+            return os.path.dirname(path)
+
+        if os.path.exists(path):
+            return path
         else:
-            raise ConanException("Path is not a file path: %s" % path)
-
-        if not os.path.isdir(path):
-            raise ConanException("Dir not found: %s" % path)
-
-        return path
+            ConanException("Path not found: %s" % path)
 
     @api_method
     def imports(self, path, dest=None, filename=None, info_folder=None):
