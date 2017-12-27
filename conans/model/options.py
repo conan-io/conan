84fa97480ba2a5f60a2e5918c23856acaa0d7cad
Chandimal Hamparawa <chandimalh@lseg.com>
2017-12-27 23:34:18 +0530

    converting tabs -> spaces in commit
    Signed-off-by: Chandimal Hamparawa <chandimalh@lseg.com>

--- conans/model/options.py
+++ conans/model/options.py
@@ -441,9 +441,9 @@ class PackageOptions(object):
                     if name in self._data:
                         self._data[name].value = value
                         self._modified[name] = (value, down_ref)
-                else:
+                    else:
                         output.warn("%s tried to change %s option %s which is not available"
-                                    % (down_ref, own_ref, name))
+                                        % (down_ref, own_ref, name))
                 else:
                     self._check_field(name)
                     self._data[name].value = value
