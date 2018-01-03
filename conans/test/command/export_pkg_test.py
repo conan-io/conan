f230cd0a67ef2920e76d20f85d782f997f192a93
James <james@conan.io>
2018-01-03 12:32:52 +0100

    fix options reuse after install in export-pkg (#2246)

--- conans/test/command/export_pkg_test.py
+++ conans/test/command/export_pkg_test.py
@@ -2,7 +2,6 @@ import unittest
 import platform
 import os
 
-from conans.client import tools
 from conans.paths import CONANFILE
 from conans.test.utils.tools import TestClient
 from conans.model.ref import ConanFileReference, PackageReference
@@ -11,7 +10,59 @@ from conans.test.utils.conanfile import TestConanFile
 from nose_parameterized import parameterized
 
 
-class PackageFilesTest(unittest.TestCase):
+class ExportPkgTest(unittest.TestCase):
+
+    def test_options(self):
+        # https://github.com/conan-io/conan/issues/2242
+        conanfile = """from conans import ConanFile
+class HelloPythonConan(ConanFile):
+    name = "Hello"
+    options = { "optionOne": [True, False, 123] }
+    default_options = "optionOne=True"
+"""
+        client = TestClient()
+        client.save({CONANFILE: conanfile})
+        client.run("export-pkg . Hello/0.1@lasote/stable")
+        client.run("search Hello/0.1@lasote/stable")
+        self.assertIn("optionOne: True", client.out)
+        self.assertNotIn("optionOne: False", client.out)
+        self.assertNotIn("optionOne: 123", client.out)
+        client.run("export-pkg . Hello/0.1@lasote/stable -o optionOne=False")
+        client.run("search Hello/0.1@lasote/stable")
+        self.assertIn("optionOne: True", client.out)
+        self.assertIn("optionOne: False", client.out)
+        self.assertNotIn("optionOne: 123", client.out)
+        client.run("export-pkg . Hello/0.1@lasote/stable -o Hello:optionOne=123")
+        client.run("search Hello/0.1@lasote/stable")
+        self.assertIn("optionOne: True", client.out)
+        self.assertIn("optionOne: False", client.out)
+        self.assertIn("optionOne: 123", client.out)
+
+    def test_options_install(self):
+        # https://github.com/conan-io/conan/issues/2242
+        conanfile = """from conans import ConanFile
+class HelloPythonConan(ConanFile):
+    name = "Hello"
+    options = { "optionOne": [True, False, 123] }
+    default_options = "optionOne=True"
+"""
+        client = TestClient()
+        client.save({CONANFILE: conanfile})
+        client.run("install .")
+        client.run("export-pkg . Hello/0.1@lasote/stable")
+        client.run("search Hello/0.1@lasote/stable")
+        self.assertIn("optionOne: True", client.out)
+        client.run("install . -o optionOne=False")
+        client.run("export-pkg . Hello/0.1@lasote/stable")
+        client.run("search Hello/0.1@lasote/stable")
+        self.assertIn("optionOne: True", client.out)
+        self.assertIn("optionOne: False", client.out)
+        client.run("install . -o Hello:optionOne=123")
+        client.run("export-pkg . Hello/0.1@lasote/stable")
+        client.run("search Hello/0.1@lasote/stable")
+        self.assertIn("optionOne: True", client.out)
+        self.assertIn("optionOne: False", client.out)
+        self.assertIn("optionOne: 123", client.out)
 
     @parameterized.expand([(False, ), (True, )])
     def test_basic(self, short_paths):
