ba2dcbf2b41effb42ba73470b3904ec4799e282e
danimtb <danimanzaneque@gmail.com>
2018-01-05 17:24:31 +0100

    Rmove test steps not needed

--- conans/test/command/download_test.py
+++ conans/test/command/download_test.py
@@ -1,37 +1,33 @@
 import unittest
 import os
 from conans.test.utils.tools import TestClient, TestServer
-from conans.test.utils.cpp_test_files import cpp_hello_conan_files
 from conans.model.ref import ConanFileReference
 
 
 class DownloadTest(unittest.TestCase):
 
     def download_reference_without_packages_test(self):
-        client = TestClient()
-        conanfile = """from conans import ConanFile
-class Pkg(ConanFile):
-    settings = "os"
-"""
-        client.save({"conanfile.py": conanfile})
-
         server = TestServer()
         servers = {"default": server}
 
+        client = TestClient()
         client = TestClient(servers=servers, users={"default": [("lasote", "mypass")]})
-
-        files = cpp_hello_conan_files()
-        client.save(files)
-
+        conanfile = """from conans import ConanFile
+class Pkg(ConanFile):
+    name = "pkg"
+    version = "0.1"
+"""
+        client.save({"conanfile.py": conanfile})
         client.run("export . lasote/stable")
-        ref = ConanFileReference.loads("Hello/0.1@lasote/stable")
+
+        ref = ConanFileReference.loads("pkg/0.1@lasote/stable")
         self.assertTrue(os.path.exists(client.paths.conanfile(ref)))
 
-        client.run("upload Hello/0.1@lasote/stable")
-        client.run("remove Hello/0.1@lasote/stable -f")
+        client.run("upload pkg/0.1@lasote/stable")
+        client.run("remove pkg/0.1@lasote/stable -f")
         self.assertFalse(os.path.exists(client.paths.export(ref)))
 
-        client.run("download Hello/0.1@lasote/stable", ignore_error=True)
+        client.run("download pkg/0.1@lasote/stable", ignore_error=True)
         # Check 'No remote binary packages found' warning
         self.assertIn("WARN: No remote binary packages found in remote", client.out)
         # Check at least conanfile.py is downloaded
