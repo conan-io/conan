8d344f4e41f539cea55f0e2cfd583e060bffe99b
memsharded <james@conan.io>
2018-01-11 13:03:24 +0100

    test failing for exclude with quotes

--- conans/test/functional/imports_tests.py
+++ conans/test/functional/imports_tests.py
@@ -91,3 +91,30 @@ LibC/0.1@lasote/testing
                                                      "licenses/LibB/LICENSE.md")))
         self.assertEqual(load(os.path.join(client.current_folder, "licenses/LibC/license.txt")),
                          "LicenseC")
+
+    def error_imports_folders_txt_test(self):
+        # https://github.com/conan-io/conan/issues/2293
+        client = TestClient()
+        conanfile = """from conans import ConanFile
+class Pkg(ConanFile):
+    exports_sources = "*"
+    def package(self):
+        self.copy("*.dll", dst="bin")
+"""
+        client.save({"conanfile.py": conanfile,
+                     "a.dll": "",
+                     "Foo/b.dll": ""})
+        client.run("create . Pkg/0.1@user/testing")
+
+        conanfile = """[requires]
+Pkg/0.1@user/testing
+[imports]
+bin, *.dll ->  @ excludes=Foo/*.dll
+"""
+        client.save({"conanfile.txt": conanfile}, clean_first=True)
+        client.run("install . --build=missing")
+        print client.out
+        print client.current_folder
+        print os.listdir(client.current_folder)
+        self.assertTrue(os.path.exists(os.path.join(client.current_folder, "a.dll")))
+        self.assertFalse(os.path.exists(os.path.join(client.current_folder, "Foo/b.dll")))
