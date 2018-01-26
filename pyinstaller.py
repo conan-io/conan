61a8eda66ad2cfd94b981a6edf8561cceccdb9df
memsharded <james@conan.io>
2018-01-26 09:24:32 +0100

    fix win pyinstaller metadata

--- pyinstaller.py
+++ pyinstaller.py
@@ -4,6 +4,8 @@ import platform
 import subprocess
 import shutil
 from distutils import dir_util
+from conans import __version__
+from conans.util.files import save
 
 
 def _install_pyinstaller(pyinstaller_path):
@@ -11,6 +13,7 @@ def _install_pyinstaller(pyinstaller_path):
     # try to install pyinstaller if not installed
     if not os.path.exists(pyinstaller_path):
         os.mkdir(pyinstaller_path)
+
 
 def _run_bin(pyinstaller_path):
     # run the binary to test if working
@@ -22,11 +25,59 @@ def _run_bin(pyinstaller_path):
         raise Exception("Binary not working")
 
 
+def _windows_version_file(version):
+    template = """# UTF-8
+#
+# For more details about fixed file info 'ffi' see:
+# http://msdn.microsoft.com/en-us/library/ms646997.aspx
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
+    # Set not needed items to zero 0.
+    filevers={version_tuple},
+    prodvers={version_tuple},
+    # Contains a bitmask that specifies the valid bits 'flags'r
+    mask=0x3f,
+    # Contains a bitmask that specifies the Boolean attributes of the file.
+    flags=0x0,
+    # The operating system for which this file was designed.
+    # 0x4 - NT and there is no need to change it.
+    OS=0x4,
+    # The general type of file.
+    # 0x1 - the file is an application.
+    fileType=0x1,
+    # The function of the file.
+    # 0x0 - the function is not defined for this fileType
+    subtype=0x0,
+    # Creation date and time stamp.
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'000004b0',
+        [StringStruct(u'Comments', u'This executable was created with pyinstaller'),
+        StringStruct(u'CompanyName', u'JFrog'),
+        StringStruct(u'FileDescription', u'Conan C, C++ Open Source Package Manager'),
+        StringStruct(u'FileVersion', u'{version}'),
+        StringStruct(u'LegalCopyright', u'Copyright 2018 JFrog'),
+        StringStruct(u'ProductName', u'Conan'),
+        StringStruct(u'ProductVersion', u'{version}')])
+      ]),
+    VarFileInfo([VarStruct(u'Translation', [0, 1200])])
+  ]
+)"""
+    if "-" in version:
+        version, _ = version.split("-")
+    version_tuple = tuple([int(v) for v in version.split(".")] + [0])
+    return template.format(version=version, version_tuple=version_tuple)
+
+
 def pyinstall(source_folder):
     pyinstaller_path = os.path.join(os.getcwd(), 'pyinstaller')
     _install_pyinstaller(pyinstaller_path)
-    tmpdir = os.getcwd()
-    command = "pyinstaller" # "python pyinstaller.py"
+    command = "pyinstaller"  # "python pyinstaller.py"
 
     try:
         shutil.rmtree(os.path.join(pyinstaller_path))
@@ -39,21 +90,27 @@ def pyinstall(source_folder):
     hidden = "--hidden-import=glob --hidden-import=pylint.reporters.text"
     if platform.system() != "Windows":
         hidden += " --hidden-import=setuptools.msvc"
+        win_ver = ""
+    else:
+        win_ver_file = os.path.join(pyinstaller_path, 'windows-version-file')
+        content = _windows_version_file(__version__)
+        save(win_ver_file, content)
+        win_ver = "--version-file %s" % win_ver_file
 
     if not os.path.exists(pyinstaller_path):
         os.mkdir(pyinstaller_path)
-    subprocess.call('%s -y -p %s --console %s %s'
-                    % (command, source_folder, conan_path, hidden),
+    subprocess.call('%s -y -p %s --console %s %s %s'
+                    % (command, source_folder, conan_path, hidden, win_ver),
                     cwd=pyinstaller_path, shell=True)
 
     _run_bin(pyinstaller_path)
 
-    subprocess.call('%s -y -p %s --console %s'
-                    % (command, source_folder, conan_server_path),
+    subprocess.call('%s -y -p %s --console %s %s'
+                    % (command, source_folder, conan_server_path, win_ver),
                     cwd=pyinstaller_path, shell=True)
 
-    subprocess.call('%s -y -p %s --console %s -n conan_build_info'
-                    % (command, source_folder, conan_build_info_path),
+    subprocess.call('%s -y -p %s --console %s -n conan_build_info %s'
+                    % (command, source_folder, conan_build_info_path, win_ver),
                     cwd=pyinstaller_path, shell=True)
 
     conan_bin = os.path.join(pyinstaller_path, 'dist', 'conan')
@@ -70,6 +127,6 @@ def pyinstall(source_folder):
 if __name__ == "__main__":
     source_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
     output_folder = pyinstall(source_folder)
-    print("\n**************Conan binaries created!******************\n \
-    \nAppend this folder to your system PATH: '%s'\nFeel free to move the whole folder to another location." % output_folder)
-
+    print("\n**************Conan binaries created!******************\n"
+          "\nAppend this folder to your system PATH: '%s'\n"
+          "Feel free to move the whole folder to another location." % output_folder)
