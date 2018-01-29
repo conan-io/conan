ec53f769a183946c59c0838b7a9a02897546f46c
Luis Martinez de Bartolome <luism@jfrog.com>
2018-01-29 15:58:07 +0100

    Restored traceback

--- conans/client/command.py
+++ conans/client/command.py
@@ -1107,8 +1107,6 @@ class Command(object):
             errors = exc.code
         except ConanException as exc:
             errors = True
-            import traceback
-            print(traceback.format_exc())
             msg = exception_message_safe(exc)
             self._user_io.out.error(msg)
         except Exception as exc:
