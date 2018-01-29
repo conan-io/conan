d7740d5075cc10a1ad3d90fb933c6dbe91ad89b7
memsharded <james@conan.io>
2018-01-29 22:26:05 +0100

    fixed dict order for options

--- conans/model/options.py
+++ conans/model/options.py
@@ -527,13 +527,13 @@ class Options(object):
         option_values = PackageOptionValues()
         # First step is to accumulate all matching patterns, in sorted()=alphabetical order
         # except the exact match
-        for package_pattern, package_option_values in down_package_values.items():
-            if own_ref.name != package_pattern and fnmatch.fnmatch(own_ref.name, package_pattern):
+        down_options = down_package_values.pop(own_ref.name, None)
+        for package_pattern, package_option_values in sorted(down_package_values.items()):
+            if fnmatch.fnmatch(own_ref.name, package_pattern):
                 option_values.update(package_option_values)
-        # These are pattern options, shouldn't rais if not existing
+        # These are pattern options, shouldn't raise if not existing
         pattern_options = list(option_values.keys())
         # Now, update with the exact match, that has higher priority
-        down_options = down_package_values.get(own_ref.name)
         if down_options is not None:
             option_values.update(down_options)
 
