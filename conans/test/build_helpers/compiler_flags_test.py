08092033056d4665deb6f03fb050536d0feed4e6
SSE4 <tomskside@gmail.com>
2018-02-06 22:24:05 +0700

    - more helpers to format flags

--- conans/test/build_helpers/compiler_flags_test.py
+++ conans/test/build_helpers/compiler_flags_test.py
@@ -3,7 +3,9 @@
 
 import unittest
 
-from conans.client.build.compiler_flags import architecture_flags, libcxx_flags, CompilerFlags
+from conans.client.build.compiler_flags import architecture_flags, libcxx_flags, pic_flags, CompilerFlags, \
+    adjust_path, sysroot_flags, format_defines, format_include_paths, format_library_paths, format_libraries, \
+    build_type_flags
 
 
 class CompilerFlagsTest(unittest.TestCase):
@@ -131,3 +133,104 @@ class CompilerFlagsTest(unittest.TestCase):
         self.assertEquals(arch_flags.cxxflags, ['-library=stdcpp'])
         self.assertEquals(arch_flags.ldflags, [])
         self.assertEquals(arch_flags.defines, [])
+
+    def test_pic_flags(self):
+        flags = pic_flags()
+        self.assertEquals(flags.cflags, ['-fPIC'])
+        self.assertEquals(flags.cxxflags, ['-fPIC'])
+        self.assertEquals(flags.ldflags, [])
+        self.assertEquals(flags.defines, [])
+
+        flags = pic_flags(compiler='gcc')
+        self.assertEquals(flags.cflags, ['-fPIC'])
+        self.assertEquals(flags.cxxflags, ['-fPIC'])
+        self.assertEquals(flags.ldflags, [])
+        self.assertEquals(flags.defines, [])
+
+        flags = pic_flags(compiler='Visual Studio')
+        self.assertEquals(flags.cflags, [])
+        self.assertEquals(flags.cxxflags, [])
+        self.assertEquals(flags.ldflags, [])
+        self.assertEquals(flags.defines, [])
+
+    def test_build_type_flags(self):
+        flags = build_type_flags(compiler='Visual Studio', build_type='Debug')
+        self.assertEquals(flags.cflags, ['/Zi'])
+        self.assertEquals(flags.cxxflags, ['/Zi'])
+        self.assertEquals(flags.ldflags, [])
+        self.assertEquals(flags.defines, [])
+
+        flags = build_type_flags(compiler='Visual Studio', build_type='Release')
+        self.assertEquals(flags.cflags, [])
+        self.assertEquals(flags.cxxflags, [])
+        self.assertEquals(flags.ldflags, [])
+        self.assertEquals(flags.defines, [])
+
+        flags = build_type_flags(compiler='gcc', build_type='Debug')
+        self.assertEquals(flags.cflags, ['-g'])
+        self.assertEquals(flags.cxxflags, ['-g'])
+        self.assertEquals(flags.ldflags, [])
+        self.assertEquals(flags.defines, [])
+
+        flags = build_type_flags(compiler='gcc', build_type='Release')
+        self.assertEquals(flags.cflags, ['-s'])
+        self.assertEquals(flags.cxxflags, ['-s'])
+        self.assertEquals(flags.ldflags, [])
+        self.assertEquals(flags.defines, ['NDEBUG'])
+
+        flags = build_type_flags(compiler='clang', build_type='Debug')
+        self.assertEquals(flags.cflags, ['-g'])
+        self.assertEquals(flags.cxxflags, ['-g'])
+        self.assertEquals(flags.ldflags, [])
+        self.assertEquals(flags.defines, [])
+
+        flags = build_type_flags(compiler='clang', build_type='Release')
+        self.assertEquals(flags.cflags, [])
+        self.assertEquals(flags.cxxflags, [])
+        self.assertEquals(flags.cxxflags, [])
+        self.assertEquals(flags.ldflags, [])
+        self.assertEquals(flags.defines, [])
+
+    def test_adjust_path(self):
+        self.assertEquals('home/www', adjust_path('home\\www'))
+        self.assertEquals('home/www', adjust_path('home\\www', compiler='gcc'))
+        self.assertEquals('home\\www', adjust_path('home/www', compiler='Visual Studio'))
+        self.assertEquals('"home/www root"', adjust_path('home\\www root'))
+        self.assertEquals('"home/www root"', adjust_path('home\\www root', compiler='gcc'))
+        self.assertEquals('"home\\www root"', adjust_path('home/www root', compiler='Visual Studio'))
+        self.assertEquals('home/www', adjust_path('home\\www', compiler='Visual Studio', win_bash=True))
+        self.assertEquals('home/www', adjust_path('home/www', compiler='Visual Studio', win_bash=True))
+        self.assertEquals('"home/www root"', adjust_path('home\\www root', compiler='Visual Studio', win_bash=True))
+        self.assertEquals('"home/www root"', adjust_path('home/www root', compiler='Visual Studio', win_bash=True))
+
+    def test_sysroot_flag(self):
+        sysroot = sysroot_flags(sysroot=None)
+        self.assertEquals(sysroot.cflags, [])
+        self.assertEquals(sysroot.cxxflags, [])
+        self.assertEquals(sysroot.ldflags, [])
+        self.assertEquals(sysroot.defines, [])
+        sysroot = sysroot_flags(sysroot='sys/root', compiler='Visual Studio')
+        self.assertEquals(sysroot.cflags, [])
+        self.assertEquals(sysroot.cxxflags, [])
+        self.assertEquals(sysroot.ldflags, [])
+        self.assertEquals(sysroot.defines, [])
+        sysroot = sysroot_flags(sysroot='sys/root')
+        self.assertEquals(sysroot.cflags, ['--sysroot=sys/root'])
+        self.assertEquals(sysroot.cxxflags, ['--sysroot=sys/root'])
+        self.assertEquals(sysroot.ldflags, ['--sysroot=sys/root'])
+        self.assertEquals(sysroot.defines, [])
+
+    def test_format_defines(self):
+        self.assertEquals(['-DFOO', '-DBAR=1'], format_defines(['FOO', 'BAR=1']))
+
+    def test_format_include_paths(self):
+        self.assertEquals(['-Ipath1', '-I"with spaces"'], format_include_paths(['path1', 'with spaces']))
+
+    def test_format_library_paths(self):
+        self.assertEquals(['-Lpath1', '-L"with spaces"'], format_library_paths(['path1', 'with spaces']))
+        self.assertEquals(['-LIBPATH:path1', '-LIBPATH:"with spaces"'],
+                          format_library_paths(['path1', 'with spaces'], compiler='Visual Studio'))
+
+    def test_format_libraries(self):
+        self.assertEquals(['-llib1', '-llib2'], format_libraries(['lib1', 'lib2']))
+        self.assertEquals(['lib1', 'lib2'], format_libraries(['lib1', 'lib2'], compiler='Visual Studio'))
