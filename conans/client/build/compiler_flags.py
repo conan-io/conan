89671cb871b9c67e4e782be70a0aa154583743e0
SSE4 <tomskside@gmail.com>
2018-02-06 22:24:05 +0700

    - more helpers to format flags

--- conans/client/build/compiler_flags.py
+++ conans/client/build/compiler_flags.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from conans.tools import unix_path
+
 
 class CompilerFlags(object):
     def __init__(self, cflags=None, cxxflags=None, ldflags=None, defines=None):
@@ -69,3 +71,79 @@ def libcxx_flags(compiler=None, libcxx=None):
                                "libstlport": "-library=stlport4",
                                "libstdc++": "-library=stdcpp"}.get(libcxx, None))
     return flags
+
+
+def pic_flags(compiler=None):
+    """
+    returns PIC (position independent code) flags, such as -fPIC
+    """
+    if compiler == 'Visual Studio':
+        return CompilerFlags()
+    return CompilerFlags(cflags=['-fPIC'], cxxflags=['-fPIC'])
+
+
+def build_type_flags(compiler=None, build_type=None):
+    """
+    returns flags specific to the build type (Debug, Release, etc.)
+    usually definitions like DEBUG, _DEBUG, NDEBUG, or flags to
+    control debug information (-s, -g, /Zi, etc.)
+    """
+    flags = CompilerFlags()
+    if str(compiler) == 'Visual Studio':
+        if build_type == 'Debug':
+            flags.cflags.append('/Zi')
+            flags.cxxflags.append('/Zi')
+    else:
+        if build_type == 'Debug':
+            flags.cflags.append('-g')
+            flags.cxxflags.append('-g')
+        elif build_type == 'Release' and str(compiler) == 'gcc':
+            flags.cflags.append('-s')
+            flags.cxxflags.append('-s')
+            # TODO: why NDEBUG only for GCC?
+            flags.defines.append('NDEBUG')
+    return flags
+
+
+def adjust_path(path, win_bash=False, subsystem=None, compiler=None):
+    """
+    adjusts path to be safely passed to the compiler command line
+    for Windows bash, ensures path is in format according to the subsystem
+    for path with spaces, places double quotes around it
+    converts slashes to backslashes, or vice versa
+    """
+    if str(compiler) == 'Visual Studio':
+        path = path.replace('/', '\\')
+    else:
+        path = path.replace('\\', '/')
+    if win_bash:
+        path = unix_path(path, subsystem)
+    return '"%s"' % path if ' ' in path else path
+
+
+def sysroot_flags(sysroot, win_bash=False, subsystem=None, compiler=None):
+    if str(compiler) != 'Visual Studio' and sysroot:
+        sysroot = adjust_path(sysroot, win_bash=win_bash, subsystem=subsystem, compiler=compiler)
+        sysroot_flag = ['--sysroot=%s' % sysroot]
+        return CompilerFlags(cflags=sysroot_flag, cxxflags=sysroot_flag, ldflags=sysroot_flag)
+    return CompilerFlags()
+
+
+def format_defines(defines):
+    return ["-D%s" % define for define in defines]
+
+
+def format_include_paths(include_paths, win_bash=False, subsystem=None, compiler=None):
+    return ["-I%s" % adjust_path(include_path, win_bash=win_bash, subsystem=subsystem, compiler=compiler)
+            for include_path in include_paths]
+
+
+def format_library_paths(library_paths, win_bash=False, subsystem=None, compiler=None):
+    pattern = "-LIBPATH:%s" if str(compiler) == 'Visual Studio' else "-L%s"
+    return [pattern % adjust_path(library_path, win_bash=win_bash, subsystem=subsystem, compiler=compiler)
+            for library_path in library_paths]
+
+
+def format_libraries(libraries, compiler=None):
+    pattern = "%s" if str(compiler) == 'Visual Studio' else "-l%s"
+    return [pattern % library for library in libraries]
