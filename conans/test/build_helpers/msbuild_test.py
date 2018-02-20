c6a8981e1ca6dda657341920c327834fcaa90951
Luis Martinez de Bartolome <lasote@gmail.com>
2018-02-20 12:57:21 +0100

    Visual ok

--- conans/test/build_helpers/msbuild_test.py
+++ conans/test/build_helpers/msbuild_test.py
@@ -40,10 +40,10 @@ class HelloConan(ConanFile):
         files[CONANFILE] = conan_build_vs
 
         client.save(files)
-        error = client.run('create Hello/1.2.1@lasote/stable -s cppstd=11 -s '
+        error = client.run('create . Hello/1.2.1@lasote/stable -s cppstd=11 -s '
                            'compiler="Visual Studio" -s compiler.version=14', ignore_error=True)
         self.assertTrue(error)
-        client.run('create Hello/1.2.1@lasote/stable -s cppstd=17 '
+        client.run('create . Hello/1.2.1@lasote/stable -s cppstd=17 '
                    '-s compiler="Visual Studio" -s compiler.version=14')
         self.assertIn("Copied 1 '.exe' files: MyProject.exe", client.user_io.out)
 
