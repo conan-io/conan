e933d837feff401128e2a238dd67caa381f85778
memsharded <james@conan.io>
2018-03-14 15:08:05 +0100

    working

--- conans/client/cmd/export_linter.py
+++ conans/client/cmd/export_linter.py
@@ -2,13 +2,12 @@ import os
 import json
 import sys
 import six
-from six import StringIO
-
-from pylint.reporters.json import JSONReporter
-from pylint.lint import Run
 
 from conans.client.output import Color
 from conans.errors import ConanException
+from subprocess import PIPE, Popen
+from conans import tools
+from conans import __path__ as root_path
 
 
 def conan_linter(conanfile_path, out):
@@ -18,47 +17,42 @@ def conan_linter(conanfile_path, out):
     apply_lint = os.environ.get("CONAN_RECIPE_LINTER", True)
     if not apply_lint or apply_lint == "False":
         return
-    try:
-        dirname = os.path.dirname(conanfile_path)
-        sys.path.append(dirname)
-        py3_msgs = _lint_py3(conanfile_path)
-        if py3_msgs:
-            out.writeln("Python 3 incompatibilities\n    ERROR: %s"
-                        % "\n    ERROR: ".join(py3_msgs),
-                        front=Color.BRIGHT_MAGENTA)
-        msgs = _normal_linter(conanfile_path)
-        if msgs:
-            out.writeln("Linter warnings\n    WARN: %s" % "\n    WARN: ".join(msgs),
-                        front=Color.MAGENTA)
-        pylint_werr = os.environ.get("CONAN_PYLINT_WERR", None)
-        if pylint_werr and (py3_msgs or msgs):
-            raise ConanException("Package recipe has linter errors. Please fix them.")
-    finally:
-        sys.path.pop()
 
+    dirname = os.path.dirname(conanfile_path)
+    dir_path = os.path.dirname(root_path[0])
 
-class _WritableObject(object):
-    def __init__(self):
-        self.content = []
-
-    def write(self, st):
-        self.content.append(st)
+    with tools.environment_append({"PYTHONPATH": [dirname, dir_path]}):
+        try:
+            py3_msgs = _lint_py3(conanfile_path)
+            msgs = _normal_linter(conanfile_path)
+        except Exception as e:
+            out.warn("Failed pylint: %s" % e)
+        else:
+            if py3_msgs:
+                out.writeln("Python 3 incompatibilities\n    ERROR: %s"
+                            % "\n    ERROR: ".join(py3_msgs),
+                            front=Color.BRIGHT_MAGENTA)
+            if msgs:
+                out.writeln("Linter warnings\n    WARN: %s" % "\n    WARN: ".join(msgs),
+                            front=Color.MAGENTA)
+            pylint_werr = os.environ.get("CONAN_PYLINT_WERR", None)
+            if pylint_werr and (py3_msgs or msgs):
+                raise ConanException("Package recipe has linter errors. Please fix them.")
 
 
 def _runner(args):
-    try:
-        output = _WritableObject()
-        stdout_ = sys.stderr
-        stream = StringIO()
-        sys.stderr = stream
-        Run(args, reporter=JSONReporter(output), exit=False)
-    finally:
-        sys.stderr = stdout_
-    try:
-        output = "".join(output.content)
-        return json.loads(output)
-    except ValueError:
-        return []
+    command = "pylint -f json " + " ".join(args)
+    # This is a bit repeated from det
+    proc = Popen(command, shell=True, bufsize=1, stdout=PIPE, stderr=PIPE)
+    output_buffer = []
+    while True:
+        line = proc.stdout.readline()
+        if not line:
+            break
+        output_buffer.append(str(line))
+    proc.communicate()
+    output = "".join(output_buffer)
+    return json.loads(output) if output else {}
 
 
 def _lint_py3(conanfile_path):
