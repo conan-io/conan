67d00c5b7f080129f67b4984f4dd760108ad4f2e
memsharded <james@conan.io>
2018-03-14 15:05:36 +0100

    working in pylint removal

---
+++ kk/conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, CMake, tools
+
+
+class HelloConan(ConanFile):
+    name = "Hello"
+    version = "0.1"
+    license = "<Put the package license here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Hello here>"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def source(self):
+        pass
+
+    def build(self):
+        try:
+            pass
+        except:
+            pass
+        myvar = True
+
+    def package(self):
+        self.copy("*.h", dst="include", src="hello")
+        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
