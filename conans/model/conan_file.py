2ba5655f5937d5bc2b2fc96d6d8ec857f7feadd3
James <james@conan.io>
2018-03-16 10:50:46 +0100

    implemented build() stages arguments (#2600)
    * implemented build() stages arguments
    
    * applied to build_helpers
    
    * broken tests
    
    * proof of concept api with default=True
    
    * help messages

--- conans/model/conan_file.py
+++ conans/model/conan_file.py
@@ -162,11 +162,12 @@ class ConanFile(object):
         self._user = user
         self._channel = channel
 
-        # Are we in local cache? Suggest a better name
         self.in_local_cache = False
-
-        # Init a description
         self.description = None
+        # Vars to control the build steps (build(), package())
+        self.should_configure = True
+        self.should_build = True
+        self.should_install = True
 
     @property
     def env(self):
