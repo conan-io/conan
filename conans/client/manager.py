17c1b0d32ddbe84fdda47617f038be3be228bdee
Daniel <danimanzaneque@gmail.com>
2018-03-20 16:09:28 +0100

    conan download --recipe param and test (#2634)
    * conan download --only-recipe param and test
    
    * review
    
    * review 2
    
    * review 3

--- conans/client/manager.py
+++ conans/client/manager.py
@@ -206,11 +206,12 @@ class ConanManager(object):
             packager.create_package(conanfile, source_folder, build_folder, dest_package_folder,
                                     install_folder, package_output, local=True)
 
-    def download(self, reference, package_ids, remote):
+    def download(self, reference, package_ids, remote, recipe):
         """ Download conanfile and specified packages to local repository
         @param reference: ConanFileReference
         @param package_ids: Package ids or empty for download all
         @param remote: install only from that remote
+        @param only_recipe: download only the recipe
         """
         assert(isinstance(reference, ConanFileReference))
         remote_proxy = ConanProxy(self._client_cache, self._user_io, self._remote_manager, remote)
@@ -221,6 +222,10 @@ class ConanManager(object):
 
         # First of all download package recipe
         remote_proxy.get_recipe(reference)
+
+        if recipe:
+            return
+
         # Download the sources too, don't be lazy
         conan_file_path = self._client_cache.conanfile(reference)
         conanfile = load_conanfile_class(conan_file_path)
