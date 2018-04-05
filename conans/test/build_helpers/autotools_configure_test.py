e34fedfc775993f1f9356e231537d5f57802aa5f
danimtb <danimanzaneque@gmail.com>
2018-04-05 12:30:39 +0200

    fixed tests

--- conans/test/build_helpers/autotools_configure_test.py
+++ conans/test/build_helpers/autotools_configure_test.py
@@ -567,14 +567,17 @@ class HelloConan(ConanFile):
         conanfile = MockConanfile(MockSettings({"build_type": "Debug",
                                                 "arch": "x86",
                                                 "os": "Windows",
-                                                "os_build": "Windows",
-                                                "arch_build": "x86_64",
                                                 "compiler": "gcc",
                                                 "compiler.libcxx": "libstdc++"}),
                                   None, runner)
         ab = AutoToolsBuildEnvironment(conanfile)
-        self.assertIn("x86_64-w64-mingw32", ab.build)
         self.assertIn("i686-w64-mingw32", ab.host)
+        if platform.system() == "Windows":
+            self.assertIn("x86_64-w64-mingw32", ab.build)
+        elif platform.system() == "Linux":
+            self.assertIn("x86_64-linux-gnu", ab.build)
+        else:
+            self.assertIn("x86_64-apple-darwin", ab.build)
         ab.configure()
         self.assertIn("./configure  --build=x86_64-w64-mingw32 --host=i686-w64-mingw32", runner.command_called)
         ab.configure(build="fake_build_triplet", host="fake_host_triplet")
@@ -602,10 +605,16 @@ class HelloConan(ConanFile):
         # package folder defined
         conanfile.package_folder = "/package_folder"
         ab.configure()
-        self.assertIn("./configure --prefix=/package_folder", runner.command_called)
+        if platform.system() == "Windows":
+            self.assertIn("./configure --prefix=/package_folder", runner.command_called)
+        else:
+            self.assertIn("./configure '--prefix=/package_folder'", runner.command_called)
         # --prefix already used in args
         ab.configure(args=["--prefix=/my_package_folder"])
-        self.assertIn("./configure --prefix=/my_package_folder", runner.command_called)
+        if platform.system() == "Windows":
+            self.assertIn("./configure --prefix=/my_package_folder", runner.command_called)
+        else:
+            self.assertIn("./configure '--prefix=/my_package_folder'", runner.command_called)
 
     def autotools_get_triplets_test(self):
         runner = RunnerMock()
