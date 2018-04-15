3a3dcacfba748aab1abb69a22905596cadf8bd46
memsharded <james@conan.io>
2018-04-15 19:46:16 +0200

    cleaning

--- conans/client/python_requires.py
+++ conans/client/python_requires.py
@@ -1,17 +1,16 @@
 from conans.model.ref import ConanFileReference
 from conans.client.loader_parse import _parse_file
-import sys
 
-_retriever = None # proxy to retrieve recipes
-_modules = {} # Private dict for caching already parsed modules
+_retriever = None  # proxy to retrieve recipes
+_modules = {}  # Private dict for caching already parsed modules
+
 
 def conan_python_require(require):
     try:
         return _modules[require]
     except KeyError:
         r = ConanFileReference.loads(require)
-        path = _retriever.get_recipe()
+        path = _retriever.get_recipe(r)
         module, _ = _parse_file(path)
         _modules[require] = module
         return module
-
