e0935bd448528f5202c1ff1a1de426d5d6a15687
danimtb <danimanzaneque@gmail.com>
2018-05-18 14:55:51 +0200

    make sure which does not return a dir

--- conans/client/tools/files.py
+++ conans/client/tools/files.py
@@ -253,7 +253,7 @@ def collect_libs(conanfile, folder="lib"):
 def which(filename):
     """ same affect as posix which command or shutil.which from python3 """
     def verify(filepath):
-        if os.path.exists(filepath) and os.access(filepath, os.X_OK):
+        if os.path.exists(filepath) and os.access(filepath, os.X_OK) and os.path.isfile(filepath):
             return os.path.join(path, filename)
         return None
 
