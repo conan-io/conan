7e63ba5e8aa1ba7b5442fe72e9ae2ae855b6f592
Daniel <danimanzaneque@gmail.com>
2018-05-21 18:15:41 +0200

    Fix "Package '1' created" message with short_paths=True (#2922)
    * fix inconsistent cache with short_paths
    
    * added skip unless to test for windows
    
    * Added fix package created id output
    
    * review
    
    * reverted output package fix
    
    * Fixed package created output
    
    * fixed test with sorted
    
    * fixed test

--- conans/test/functional/short_paths_test.py
+++ conans/test/functional/short_paths_test.py
@@ -49,12 +49,14 @@ class TestConan(ConanFile):
         self.assertEqual(["source_file.cpp"], os.listdir(source_folder))
         self.assertIn("BUILD: source_file.cpp", client.out)
         self.assertIn("BUILD: artifact", client.out)
-        self.assertEqual(["artifact", "conanbuildinfo.txt", "conaninfo.txt", "source_file.cpp"],
-                         os.listdir(build_folder))
+        self.assertEqual(
+            sorted(["artifact", "conanbuildinfo.txt", "conaninfo.txt", "source_file.cpp"]),
+            sorted(os.listdir(build_folder)))
         self.assertIn("PACKAGE: source_file.cpp", client.out)
         self.assertIn("PACKAGE: artifact", client.out)
-        self.assertEqual(["artifact", "conaninfo.txt", "conanmanifest.txt", "source_file.cpp"],
-                         os.listdir(package_folder))
+        self.assertEqual(
+            sorted(["artifact", "conaninfo.txt", "conanmanifest.txt", "source_file.cpp"]),
+            sorted(os.listdir(package_folder)))
         client.save({"conanfile.py": conanfile.format("True")})
         client.run("create . danimtb/testing")
         self.assertIn("SOURCE: source_file.cpp", client.out)
@@ -65,3 +67,43 @@ class TestConan(ConanFile):
         self.assertIn("PACKAGE: source_file.cpp", client.out)
         self.assertIn("PACKAGE: artifact", client.out)
         self.assertEqual([".conan_link"], os.listdir(package_folder))
+
+    @unittest.skipUnless(platform.system() == "Windows", "Requires Windows")
+    def package_output_test(self):
+        conanfile = """
+import os
+from conans import ConanFile, tools
+
+
+class TestConan(ConanFile):
+    name = "test"
+    version = "1.0"
+    short_paths = True
+"""
+        client = TestClient()
+        client.save({"conanfile.py": conanfile,
+                     "source_file.cpp": ""})
+        client.run("create . danimtb/testing")
+        self.assertNotIn("test/1.0@danimtb/testing: Package '1' created", client.out)
+        self.assertIn(
+            "test/1.0@danimtb/testing: Package '5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9' created",
+            client.out)
+
+        # try local flow still works, but no pkg id available
+        client.run("install .")
+        client.run("package .")
+        self.assertIn("PROJECT: Package 'package' created", client.out)
+
+        # try export-pkg with package folder
+        client.run("remove test/1.0@danimtb/testing --force")
+        client.run("export-pkg . test/1.0@danimtb/testing --package-folder package")
+        self.assertIn(
+            "test/1.0@danimtb/testing: Package '5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9' created",
+            client.out)
+
+        # try export-pkg without package folder
+        client.run("remove test/1.0@danimtb/testing --force")
+        client.run("export-pkg . test/1.0@danimtb/testing --install-folder .")
+        self.assertIn(
+            "test/1.0@danimtb/testing: Package '5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9' created",
+            client.out)
