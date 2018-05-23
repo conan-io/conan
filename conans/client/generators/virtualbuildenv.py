586bd1775ab722ffade9300cf72faa070ee0c03a
danimtb <danimanzaneque@gmail.com>
2018-05-23 16:20:18 +0200

    Added deactivate test for generator

--- conans/client/generators/virtualbuildenv.py
+++ conans/client/generators/virtualbuildenv.py
@@ -10,11 +10,13 @@ class VirtualBuildEnvGenerator(VirtualEnvGenerator):
         super(VirtualBuildEnvGenerator, self).__init__(conanfile)
         self.venv_name = "conanbuildenv"
         compiler = conanfile.settings.get_safe("compiler")
-        if compiler != "Visual Studio":
-            self.env = AutoToolsBuildEnvironment(conanfile).vars_dict
-        else:
+        if compiler == "Visual Studio":
             self.env = VisualStudioBuildEnvironment(conanfile).vars_dict
-            self.env.update(vcvars_dict(conanfile.settings))
+            for key, value in vcvars_dict(conanfile.settings).items():
+                if not key.startswith("CONAN"):
+                    self.env[key] = value
+        else:
+            self.env = AutoToolsBuildEnvironment(conanfile).vars_dict
 
     @property
     def content(self):
---
+++ conans/test/generators/virtualbuildenv_test.py
@@ -0,0 +1,50 @@
+import os
+import platform
+import subprocess
+import unittest
+
+from conans import load
+from conans.test.utils.tools import TestClient
+
+
+class VirtualBuildEnvTest(unittest.TestCase):
+
+    def environment_deactivate_test(self):
+
+        def env_output_to_dict(out):
+            env = {}
+            for line in out.splitlines():
+                tmp = line.split("=")
+                env[tmp[0]] = tmp[1].replace("\\", "/")
+            return env
+
+        conanfile = """
+from conans import ConanFile
+
+class TestConan(ConanFile):
+    name = "test"
+    version = "1.0"
+    settings = "os", "compiler", "arch", "build_type"
+    generators = "virtualbuildenv"
+"""
+        client = TestClient(path_with_spaces=False)
+        client.save({"conanfile.py": conanfile})
+        client.run("install .")
+        env_cmd = "set" if platform.system() == "Windows" else "env"
+        extension = "bat" if platform.system() == "Windows" else "sh"
+        output = subprocess.check_output(env_cmd, shell=True)
+        normal_environment = env_output_to_dict(output)
+        client.run("install .")
+        activate_build_file = os.path.join(client.current_folder, "activate_build.%s" % extension)
+        deactivate_build_file = os.path.join(client.current_folder,
+                                             "deactivate_build.%s" % extension)
+        activate_build_content = load(activate_build_file)
+        deactivate_build_content = load(deactivate_build_file)
+        self.assertEqual(len(activate_build_content.splitlines()),
+                         len(deactivate_build_content.splitlines()))
+        output = subprocess.check_output(activate_build_file + " && %s" % env_cmd, shell=True)
+        activate_environment = env_output_to_dict(output)
+        self.assertNotEqual(normal_environment, activate_environment)
+        output = subprocess.check_output(deactivate_build_file + " && %s" % env_cmd, shell=True)
+        deactivate_environment = env_output_to_dict(output)
+        self.assertEqual(normal_environment, deactivate_environment)
