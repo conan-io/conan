fbfa9c0616255ce7cdd891c7410ab30cb72163b7
Luis Martinez de Bartolome Izquierdo <luism@jfrog.com>
2018-05-28 12:21:42 +0200

    Merged dev

--- conans/client/tools/win.py
+++ conans/client/tools/win.py
@@ -293,7 +293,8 @@ def vcvars_command(settings, arch=None, compiler_version=None, force=False):
     return command
 
 
-def vcvars_dict(settings, arch=None, compiler_version=None, force=False, filter_known_paths=False):
+def vcvars_dict(settings, arch=None, compiler_version=None, force=False, filter_known_paths=False,
+                only_diff=False):
     cmd = vcvars_command(settings, arch=arch,
                          compiler_version=compiler_version, force=force) + " && echo __BEGINS__ && set"
     ret = decode_text(subprocess.check_output(cmd, shell=True))
@@ -305,11 +306,13 @@ def vcvars_dict(settings, arch=None, compiler_version=None, force=False, filter_
             if "__BEGINS__" in line:
                 start_reached = True
             continue
+
         if line == "\n" or not line:
             continue
         try:
             name_var, value = line.split("=", 1)
-            new_env[name_var] = value
+            if not only_diff or os.environ.get(name_var) != value:
+                new_env[name_var] = value
         except ValueError:
             pass
 
