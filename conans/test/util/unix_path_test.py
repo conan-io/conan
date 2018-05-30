6f1adc8649a6cb9fa7da36d10d47bf70a5f5476d
memsharded <james@conan.io>
2018-05-31 00:19:41 +0200

    fixe get_cased_path

--- conans/test/util/unix_path_test.py
+++ conans/test/util/unix_path_test.py
@@ -3,6 +3,25 @@
 
 import unittest
 from conans import tools
+from conans.client.tools.win import get_cased_path
+from conans.test.utils.test_files import temp_folder
+import os
+import platform
+from conans.util.files import mkdir
+
+
+class GetCasedPath(unittest.TestCase):
+    @unittest.skipUnless(platform.system() == "Windows", "Requires Windows")
+    def test_case(self):
+        folder = temp_folder()
+        p1 = os.path.join(folder, "MyFolder", "Subfolder")
+        mkdir(p1)
+        p2 = get_cased_path(p1)
+        self.assertEqual(p1, p2)
+
+        p3 = os.path.join(folder, "myfolder", "subfolder")
+        p4 = get_cased_path(p3)
+        self.assertEqual(p1, p4)
 
 
 class UnixPathTest(unittest.TestCase):
