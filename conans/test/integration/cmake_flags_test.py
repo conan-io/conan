c8f91ee8313776d5e0df229f7e590839560ea715
danimtb <danimanzaneque@gmail.com>
2018-06-07 18:48:47 +0200

    fix windows test

--- conans/test/integration/cmake_flags_test.py
+++ conans/test/integration/cmake_flags_test.py
@@ -255,12 +255,7 @@ class MyLib(ConanFile):
             client.save({"conanfile.py": conanfile % settings_line})
             client.run("install .")
             error = client.run("build .", ignore_error=True)
-            if platform.system() == "Windows":
-                self.assertTrue(error)
-                self.assertIn("You must specify compiler, compiler.version and arch in "
-                              "your settings to use a CMake generator", client.user_io.out,)
-            else:
-                self.assertFalse(error)
+            self.assertFalse(error)
 
     def cmake_shared_flag_test(self):
         conanfile = """
