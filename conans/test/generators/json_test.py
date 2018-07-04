dd64b15e80414e40b0c2fef0754181a67f8ebe36
Tamás Szelei <szelei.t@gmail.com>
2018-07-04 13:24:16 +0200

    Added settings&options to json generator (#3163)

--- conans/test/generators/json_test.py
+++ conans/test/generators/json_test.py
@@ -69,3 +69,28 @@ class HelloConan(ConanFile):
         include_path = hello_data["include_paths"][0]
         self.assertTrue(os.path.isabs(include_path))
         self.assertTrue(os.path.exists(include_path))
+
+    def generate_json_info_settings_test(self):
+        conanfile_py = """from conans import ConanFile
+
+class HelloConan(ConanFile):
+    exports_sources = "*.h"
+    settings = "os", "arch"
+    def package(self):
+        self.copy("*.h", dst="include")
+    def package_info(self):
+        self.env_info.MY_ENV_VAR = "foo"
+        self.user_info.my_var = "my_value"
+"""
+        client = TestClient()
+        client.save({"conanfile.py": conanfile_py,
+                     "header.h": ""})
+        settings = "-sos=Linux -sarch=x86_64"
+        client.run("create . Hello/0.1@lasote/testing " + settings)
+        client.run("install Hello/0.1@lasote/testing -g json " + settings)
+        conan_json = os.path.join(client.current_folder, "conanbuildinfo.json")
+        with open(conan_json) as f:
+            data = json.load(f)
+        settings_data = data["settings"]
+        self.assertEqual(settings_data["os"], "Linux")
+        self.assertEqual(settings_data["arch"], "x86_64")
