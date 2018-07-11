59901c272a3dc3ab106a4648a1b4396ec69d2670
memsharded <james@conan.io>
2018-07-11 14:18:29 +0200

    fixing things in Linux

--- conans/test/integration/run_envronment_test.py
+++ conans/test/integration/run_envronment_test.py
@@ -74,8 +74,8 @@ class Pkg(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*/say_hello.exe", dst="bin", keep_path=False)
-        self.copy("*/say_hello", dst="bin", keep_path=False)
+        self.copy("*say_hello.exe", dst="bin", keep_path=False)
+        self.copy("*say_hello", dst="bin", keep_path=False)
         self.copy(pattern="*.dll", dst="bin", keep_path=False)
         self.copy(pattern="*.dylib", dst="lib", keep_path=False)
         self.copy(pattern="*.so", dst="lib", keep_path=False)
@@ -86,16 +86,21 @@ class Pkg(ConanFile):
                      "hello.cpp": hello_cpp,
                      "hello.h": hello_h})
         client.run("create . Pkg/0.1@user/testing")
-
+  
         reuse = '''from conans import ConanFile, RunEnvironment, tools
-
+import platform, os
 class HelloConan(ConanFile):
     requires = "Pkg/0.1@user/testing"
 
     def build(self):
         run_env = RunEnvironment(self)
         with tools.environment_append(run_env.vars):
-            self.run("say_hello")
+            if platform.system() == "Windows":
+                self.run("say_hello")
+            elif platform.system() == "Darwin":
+                self.run('DYLD_LIBRARY_PATH="%s" %s' % (os.environ.get('DYLD_LIBRARY_PATH', ''), "say_hello"))
+            else:
+                self.run('LD_LIBRARY_PATH="%s" %s' % (os.environ.get('LD_LIBRARY_PATH', ''), "say_hello"))
 '''
 
         client.save({"conanfile.py": reuse}, clean_first=True)
