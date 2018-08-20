fe4251cc302719a8858654bb86c9c589a1b4b20b
Luis Martinez de Bartolome Izquierdo <luism@jfrog.com>
2018-08-20 17:36:27 +0200

    Added server

---
+++ conans/server/revision_list.py
@@ -0,0 +1,49 @@
+import json
+
+import time
+
+from conans.errors import ConanException
+
+
+class RevisionList(object):
+
+    def __init__(self):
+        self._data = {"revisions": list()}
+
+    @staticmethod
+    def loads(contents):
+        ret = RevisionList()
+        ret._data = json.loads(contents)
+        return ret
+
+    def dumps(self):
+        return json.dumps(self._data)
+
+    def add_revision(self, revision_id):
+        if self.latest_revision() == revision_id:
+            # Each uploaded file calls to update the revision
+            return
+        if self._find_revision_index(revision_id) is not None:
+            raise ConanException("Revision already exists: %s" % revision_id)
+        now = time.time()
+        self._data["revisions"].append({"id": revision_id, "time": now})
+
+    def latest_revision(self):
+        if not self._data["revisions"]:
+            return None
+        return self._data["revisions"][-1]["id"]
+
+    def remove_revision(self, revision_id):
+        index = self._find_revision_index(revision_id)
+        if index is None:
+            return
+        self._data["revisions"].pop(index)
+
+    def _find_revision_index(self, revision_id):
+        for i, rev in enumerate(self._data["revisions"]):
+            if rev["id"] == revision_id:
+                return i
+        return None
+
+
+
---
+++ conans/server/store/server_store.py
@@ -0,0 +1,267 @@
+from os.path import normpath, join, relpath
+
+from conans.errors import NotFoundException, ConanException
+from conans.model.ref import PackageReference, ConanFileReference
+from conans.paths import SimplePaths, EXPORT_FOLDER, PACKAGES_FOLDER
+from conans.server.revision_list import RevisionList
+
+REVISIONS_FILE = "revisions.txt"
+
+
+class ServerStore(SimplePaths):
+
+    def __init__(self, revisions_enabled, storage_adapter):
+        super(ServerStore, self).__init__(storage_adapter.base_storage_folder())
+        self._revisions_enabled = revisions_enabled
+        self._storage_adapter = storage_adapter
+
+    # Methods to override some basics from paths to allow revisions paths (reading latest)
+    def conan(self, reference, resolve_latest=True):
+        reference = self.ref_with_rev(reference) if resolve_latest else reference
+        tmp = normpath(join(self.store, "/".join(reference)))
+        if reference.revision:
+            return join(tmp, reference.revision)
+        return tmp
+
+    def packages(self, reference):
+        reference = self.ref_with_rev(reference)
+        return join(self.conan(reference), PACKAGES_FOLDER)
+
+    def package(self, p_reference, short_paths=None):
+        p_reference = self._patch_package_ref(p_reference)
+        tmp = join(self.packages(p_reference.conan), p_reference.package_id)
+        if p_reference.revision:
+            return join(tmp, p_reference.revision)
+        return tmp
+
+    def export(self, reference):
+        return join(self.conan(reference), EXPORT_FOLDER)
+
+    # Methods to manage revisions
+    def get_last_revision(self, reference):
+        assert(isinstance(reference, ConanFileReference))
+        rev_file_path = self._recipe_revisions_file(reference)
+        return self._get_latest_revision(rev_file_path)
+
+    def get_last_package_revision(self, p_reference):
+        assert(isinstance(p_reference, PackageReference))
+        rev_file_path = self._package_revisions_file(p_reference)
+        return self._get_latest_revision(rev_file_path)
+
+    def update_last_revision(self, reference):
+        assert(isinstance(reference, ConanFileReference))
+        rev_file_path = self._recipe_revisions_file(reference)
+        self._update_last_revision(rev_file_path, reference)
+
+    def update_last_package_revision(self, p_reference):
+        assert(isinstance(p_reference, PackageReference))
+        rev_file_path = self._package_revisions_file(p_reference)
+        self._update_last_revision(rev_file_path, p_reference)
+
+    def _update_last_revision(self, rev_file_path, reference):
+        if self._storage_adapter.path_exists(rev_file_path):
+            rev_file = self._storage_adapter.read_file(rev_file_path,
+                                                       lock_file=rev_file_path + ".lock")
+            rev_list = RevisionList.loads(rev_file)
+        else:
+            rev_list = RevisionList()
+        if not reference.revision:
+            raise ConanException("Invalid revision for: %s" % reference.full_repr())
+        rev_list.add_revision(reference.revision)
+        self._storage_adapter.write_file(rev_file_path, rev_list.dumps(),
+                                         lock_file=rev_file_path + ".lock")
+
+    def _get_latest_revision(self, rev_file_path):
+        if self._storage_adapter.path_exists(rev_file_path):
+            rev_file = self._storage_adapter.read_file(rev_file_path,
+                                                       lock_file=rev_file_path + ".lock")
+            rev_list = RevisionList.loads(rev_file)
+            return rev_list.latest_revision()
+        else:
+            return None
+
+    def _recipe_revisions_file(self, reference):
+        recipe_folder = normpath(join(self._store_folder, "/".join(reference)))
+        return join(recipe_folder, REVISIONS_FILE)
+
+    def _package_revisions_file(self, p_reference):
+        tmp = normpath(join(self._store_folder, "/".join(p_reference.conan)))
+        revision = {None: ""}.get(p_reference.conan.revision, p_reference.conan.revision)
+        p_folder = join(tmp, revision, PACKAGES_FOLDER, p_reference.package_id)
+        return join(p_folder, REVISIONS_FILE)
+
+    def get_conanfile_file_path(self, reference, filename):
+        reference = self.ref_with_rev(reference)
+        abspath = join(self.export(reference), filename)
+        return abspath
+
+    def get_package_file_path(self, p_reference, filename):
+        p_reference = self._patch_package_ref(p_reference)
+        p_path = self.package(p_reference)
+        abspath = join(p_path, filename)
+        return abspath
+
+    def ref_with_rev(self, reference):
+        if not self._revisions_enabled:
+            return reference.copy_without_revision()
+        if reference.revision:
+            return reference
+
+        latest = self.get_last_revision(reference)
+        if not latest:
+            raise NotFoundException("Recipe not found: '%s'" % reference.full_repr())
+
+        reference.revision = latest
+        return reference
+
+    def _patch_package_ref(self, p_reference):
+        if not self._revisions_enabled:
+            return p_reference.copy_without_revision()
+        if p_reference.revision:
+            return p_reference
+
+        reference = self.ref_with_rev(p_reference.conan)
+        ret = PackageReference(reference, p_reference.package_id)
+
+        latest = self.get_last_package_revision(ret)
+        if not latest:
+            raise NotFoundException("Package not found: '%s'" % str(p_reference))
+
+        ret.revision = latest
+        return ret
+
+    def update_recipe_revision(self, reference):
+        if not self._revisions_enabled:
+            return
+
+        self.update_last_revision(reference)
+
+    def update_package_revision(self, p_reference):
+        if not self._revisions_enabled:
+            return
+
+        self.update_last_package_revision(p_reference)
+
+    def path_exists(self, path):
+        return self._storage_adapter.path_exists(path)
+
+    # ############ SNAPSHOTS (APIv1 and APIv2)
+    def get_conanfile_snapshot(self, reference):
+        """Returns a {filepath: md5} """
+        assert isinstance(reference, ConanFileReference)
+        return self._get_snapshot_of_files(self.export(reference))
+
+    def get_package_snapshot(self, p_reference):
+        """Returns a {filepath: md5} """
+        assert isinstance(p_reference, PackageReference)
+        p_reference = self._patch_package_ref(p_reference)
+        path = self.package(p_reference)
+        return self._get_snapshot_of_files(path)
+
+    def _get_snapshot_of_files(self, relative_path):
+        snapshot = self._storage_adapter.get_snapshot(relative_path)
+        snapshot = self._relativize_keys(snapshot, relative_path)
+        return snapshot
+
+    # ######### DELETE (APIv1 and APIv2)
+    def remove_conanfile(self, reference):
+        assert isinstance(reference, ConanFileReference)
+        result = self._storage_adapter.delete_folder(self.conan(reference, resolve_latest=False))
+        if reference.revision:
+            rev_file_path = self._recipe_revisions_file(reference)
+            self._remove_revision(rev_file_path, reference.revision)
+        self._storage_adapter.delete_empty_dirs([reference])
+        return result
+
+    def _remove_revision(self, rev_file_path, revision):
+        rev_file = self._storage_adapter.read_file(rev_file_path,
+                                                   lock_file=rev_file_path + ".lock")
+        rev_list = RevisionList.loads(rev_file)
+        rev_list.remove_revision(revision)
+        self._storage_adapter.write_file(rev_file_path, rev_list.dumps(),
+                                         lock_file=rev_file_path + ".lock")
+
+    def remove_packages(self, reference, package_ids_filter):
+        assert isinstance(reference, ConanFileReference)
+        assert isinstance(package_ids_filter, list)
+
+        if not package_ids_filter:  # Remove all packages
+            packages_folder = self.packages(reference)
+            self._storage_adapter.delete_folder(packages_folder)
+        else:
+            for package_id in package_ids_filter:
+                package_ref = PackageReference(reference, package_id)
+                package_folder = self.package(package_ref)
+                self._storage_adapter.delete_folder(package_folder)
+        self._storage_adapter.delete_empty_dirs([reference])
+        return
+
+    def remove_conanfile_files(self, reference, files):
+        subpath = self.export(reference)
+        for filepath in files:
+            path = join(subpath, filepath)
+            self._storage_adapter.delete_file(path)
+
+    def remove_package_files(self, package_reference, files):
+        subpath = self.package(package_reference)
+        for filepath in files:
+            path = join(subpath, filepath)
+            self._storage_adapter.delete_file(path)
+
+    # ONLY APIv1 URLS
+
+    # ############ DOWNLOAD URLS
+    def get_download_conanfile_urls(self, reference, files_subset=None, user=None):
+        """Returns a {filepath: url} """
+        assert isinstance(reference, ConanFileReference)
+        return self._get_download_urls(self.export(reference), files_subset, user)
+
+    def get_download_package_urls(self, package_reference, files_subset=None, user=None):
+        """Returns a {filepath: url} """
+        assert isinstance(package_reference, PackageReference)
+        return self._get_download_urls(self.package(package_reference), files_subset, user)
+
+    # ############ UPLOAD URLS
+    def get_upload_conanfile_urls(self, reference, filesizes, user):
+        """
+        :param reference: ConanFileReference
+        :param filesizes: {filepath: bytes}
+        :return {filepath: url} """
+        assert isinstance(reference, ConanFileReference)
+        assert isinstance(filesizes, dict)
+        return self._get_upload_urls(self.export(reference), filesizes, user)
+
+    def get_upload_package_urls(self, package_reference, filesizes, user):
+        """
+        :param reference: PackageReference
+        :param filesizes: {filepath: bytes}
+        :return {filepath: url} """
+        assert isinstance(package_reference, PackageReference)
+        assert isinstance(filesizes, dict)
+        return self._get_upload_urls(self.package(package_reference), filesizes, user)
+
+    def _get_download_urls(self, relative_path, files_subset=None, user=None):
+        """Get the download urls for the whole relative_path or just
+        for a subset of files. files_subset has to be a list with paths
+        relative to relative_path"""
+        relative_snap = self._storage_adapter.get_snapshot(relative_path, files_subset)
+        urls = self._storage_adapter.get_download_urls(list(relative_snap.keys()), user)
+        urls = self._relativize_keys(urls, relative_path)
+        return urls
+
+    def _get_upload_urls(self, relative_path, filesizes, user=None):
+        abs_paths = {}
+        for path, filesize in filesizes.items():
+            abs_paths[join(relative_path, path)] = filesize
+        urls = self._storage_adapter.get_upload_urls(abs_paths, user)
+        urls = self._relativize_keys(urls, relative_path)
+        return urls
+
+    @staticmethod
+    def _relativize_keys(the_dict, basepath):
+        """Relativize the keys in the dict relative to basepath"""
+        ret = {}
+        for old_key, value in the_dict.items():
+            new_key = relpath(old_key, basepath)
+            ret[new_key] = value
+        return ret
