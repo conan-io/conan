7b5e12d1beb60494a0b5dfc26eb0cebdf54cc3c5
Luis Martinez de Bartolome Izquierdo <luism@jfrog.com>
2018-08-30 09:39:34 +0200

    global and module

--- conans/test/__init__.py
+++ conans/test/__init__.py
@@ -1,3 +1,11 @@
 import os
 
 CONAN_TEST_FOLDER = os.getenv('CONAN_TEST_FOLDER', None)
+
+
+def setUpModule():
+    os.environ["CONAN_RECIPE_LINTER"] = "False"
+
+
+def tearDownModule():
+    del os.environ["CONAN_RECIPE_LINTER"]
