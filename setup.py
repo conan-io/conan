7a89a7af7fd1dc8c32a2685f1a17d9015fc600d7
Luis Martinez de Bartolome Izquierdo <luism@jfrog.com>
2018-08-30 10:09:11 +0200

    clean code

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
