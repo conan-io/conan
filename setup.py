3432873fbf03dcc0dc7e857e64bbdd88421b0a0e
Luis Martinez de Bartolome Izquierdo <luism@jfrog.com>
2018-08-30 09:31:13 +0200

    dummu change

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
+
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
