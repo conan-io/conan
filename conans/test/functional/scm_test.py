363f6cf949f6412ca498ff7104e962d0cfc30caf
Javier G. Sogo <jgsogo@gmail.com>
2018-09-03 12:00:51 +0200

    convert to str before calling .lower()

--- conans/test/functional/scm_test.py
+++ conans/test/functional/scm_test.py
@@ -494,7 +494,7 @@ class ConanLib(ConanFile):
         sources_dir = self.client.client_cache.scm_folder(self.reference)
         self.assertEquals(load(sources_dir), curdir)
         self.assertIn("Repo origin deduced by 'auto': {}".format(project_url).lower(),
-            self.client.out.lower())
+            str(self.client.out).lower())
         self.assertIn("Revision deduced by 'auto'", self.client.out)
         self.assertIn("Getting sources from folder: %s" % curdir, self.client.out)
         self.assertIn("My file is copied", self.client.out)
@@ -508,7 +508,7 @@ class ConanLib(ConanFile):
         self.assertNotIn("Repo origin deduced by 'auto'", self.client.out)
         self.assertNotIn("Revision deduced by 'auto'", self.client.out)
         self.assertIn("Getting sources from url: '{}'".format(project_url).lower(),
-                      self.client.out.lower())
+                      str(self.client.out).lower())
         self.assertIn("My file is copied", self.client.out)
 
     def test_auto_subfolder(self):
@@ -586,7 +586,7 @@ class ConanLib(ConanFile):
         self.assertFalse(os.path.exists(os.path.join(curdir, "source2", "myfile2.txt")))
         self.assertTrue(os.path.exists(os.path.join(curdir, "source2", "myfile.txt")))
         self.assertIn("Getting sources from url: '{}'".format(project_url).lower(),
-                      self.client.out.lower())
+                      str(self.client.out).lower())
         self.assertIn("SOURCE METHOD CALLED", self.client.out)
 
     def test_local_source_subfolder(self):
