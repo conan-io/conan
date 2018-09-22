035ff941ca7dc3714313f794d24ef22a70a922c3
Javier G. Sogo <jgsogo@gmail.com>
2018-09-22 21:51:06 +0200

    use SCM_Data class

--- conans/test/client/cmd/export_test.py
+++ conans/test/client/cmd/export_test.py
@@ -2,11 +2,13 @@
 import os
 import shutil
 import unittest
+from collections import namedtuple
 
 from conans.util.files import save, load
 from conans.client.cmd.export import _replace_scm_data_in_conanfile
 from conans.client.loader import _parse_file
 from conans.test.utils.test_files import temp_folder
+from conans.model.scm import SCMData
 
 
 class ReplaceSCMDataInConanfileTest(unittest.TestCase):
@@ -16,9 +18,9 @@ from conans import ConanFile
 class ConanLib(ConanFile):
     name = "lib"
     version = "0.1"
-    scm = {{'type': 'git',
-           'url': '{url}',
-           'revision': '{revision}'}}
+    scm = {{"revision": "{revision}",
+           "type": "git",
+           "url": "{url}"}}
     
     {after_scm}
 
@@ -41,6 +43,7 @@ class ConanLib(ConanFile):
         save(self.conanfile_path, content=self.conanfile.format(url='auto', revision='auto',
                                                                 after_scm=after_scm,
                                                                 after_recipe=after_recipe))
+        scm_data = SCMData(conanfile=namedtuple('_', 'scm')(scm=scm_data))
         _replace_scm_data_in_conanfile(self.conanfile_path, scm_data)
         self.assertEqual(load(self.conanfile_path), target_conanfile)
         _parse_file(self.conanfile_path)  # Check that the resulting file is valid python code.
