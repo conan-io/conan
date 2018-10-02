eb101e5758859b47b22eeb9a1574b9bdf35febb0
david A <david.allemant@gmail.com>
2018-10-03 00:09:13 +0200

    implement qmake_subdir minimal impl and unittest

--- conans/test/generators/qmake_subdirs_test.py
+++ conans/test/generators/qmake_subdirs_test.py
@@ -2,9 +2,9 @@ import re
 import unittest
 
 from conans.client.generators.qmake_subdirs import QmakeSubDirsGenerator
+from conans.paths import BUILD_INFO_QMAKE_SUBDIRS
 from conans.model.settings import Settings
 from conans.model.conan_file import ConanFile
-from conans.client.generators.cmake import CMakeGenerator
 from conans.model.build_info import CppInfo
 from conans.model.ref import ConanFileReference
 from conans.client.conf import default_settings_yml
@@ -16,24 +16,41 @@ from conans.model.env_info import EnvValues
 
 class QMakeSubDirsGeneratorTest(unittest.TestCase):
 
-
-    def test_variables_setup(self):
-        conanfile = ConanFile(None, None)
-        conanfile.initialize(Settings({}), EnvValues())
+    @classmethod
+    def setUpClass(cls):
+        cls.conanfile = ConanFile(None, None)
+        cls.conanfile.initialize(Settings({}), EnvValues())
         ref = ConanFileReference.loads("MyPkg/0.1@lasote/stables")
         cpp_info = CppInfo("dummy_root_folder1")
         cpp_info.defines = ["MYDEFINE1"]
-        conanfile.deps_cpp_info.update(cpp_info, ref.name)
+        cls.conanfile.deps_cpp_info.update(cpp_info, ref.name)
         ref = ConanFileReference.loads("MyPkg2/0.1@lasote/stables")
         cpp_info = CppInfo("dummy_root_folder2")
         cpp_info.defines = ["MYDEFINE2"]
-        conanfile.deps_cpp_info.update(cpp_info, ref.name)
-        conanfile.deps_user_info["LIB1"].myvar = "myvalue"
-        conanfile.deps_user_info["LIB1"].myvar2 = "myvalue2"
-        conanfile.deps_user_info["lib2"].MYVAR2 = "myvalue4"
-        generator = QmakeSubDirsGenerator(conanfile)
-        self.assertEquals( generator.filename, "conan_subdirs.pri")
+        cls.conanfile.deps_cpp_info.update(cpp_info, ref.name)
+        cls.conanfile.deps_user_info["LIB1"].myvar = "myvalue"
+        cls.conanfile.deps_user_info["LIB1"].myvar2 = "myvalue2"
+        cls.conanfile.deps_user_info["lib2"].MYVAR2 = "myvalue4"
+        cls.generator = QmakeSubDirsGenerator(cls.conanfile)
+        cls.content = cls.generator.content
 
-        content = generator.content
-        cmake_lines = content.splitlines()
-        self.assertIn('#todo', cmake_lines)
+    def test_qmake_subdirs_generates_multiple_files_test(self):
+
+        self.assertIsNone(self.generator.filename)
+        self.assertIsInstance(self.content,dict)
+        self.assertTrue( len(self.content) is 3)
+
+    def test_qmake_subdirs_generate_buildinfo_file_test(self):
+        self.assertTrue( BUILD_INFO_QMAKE_SUBDIRS in self.content)
+
+    def test_qmake_subdirs_generate_one_prf_file_per_dependancy_test(self):
+        self.assertTrue("MyPkg.prf" in self.content)
+        self.assertTrue("MyPkg2.prf" in self.content)
+
+    def test_qmake_subdirs_declare_defines_in_prf_files(self):
+        prf_lines = self.content["MyPkg.prf"].splitlines()
+        self.assertIn('DEFINES *= "MYDEFINE1"', prf_lines)
+
+        prf_lines = self.content["MyPkg2.prf"].splitlines()
+        self.assertIn('DEFINES *= "MYDEFINE2"', prf_lines)
+
