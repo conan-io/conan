6671847de5477db9e1ab3b73f511aaf9c38e7481
Luis Martinez de Bartolome Izquierdo <luism@jfrog.com>
2018-10-10 12:57:28 +0200

    Rename docker images

--- conans/test/command/new_test.py
+++ conans/test/command/new_test.py
@@ -138,7 +138,7 @@ class NewTest(unittest.TestCase):
         self.assertIn('- CONAN_REFERENCE: "MyPackage/1.3"', travis)
         self.assertIn('- CONAN_USERNAME: "myuser"', travis)
         self.assertIn('- CONAN_CHANNEL: "testing"', travis)
-        self.assertIn('env: CONAN_GCC_VERSIONS=5 CONAN_DOCKER_IMAGE=lasote/conangcc5',
+        self.assertIn('env: CONAN_GCC_VERSIONS=5 CONAN_DOCKER_IMAGE=conanio/gcc5',
                       travis)
 
         gitlab = load(os.path.join(root, ".gitlab-ci.yml"))
