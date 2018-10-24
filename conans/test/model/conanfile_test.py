cb10753882c9bc19c70057eece70b46c8229cabe
Javier G. Sogo <jgsogo@gmail.com>
2018-10-24 18:32:20 +0200

    implement env_var behavior at path_shortener level

--- conans/test/model/conanfile_test.py
+++ conans/test/model/conanfile_test.py
@@ -1,17 +1,10 @@
-import os
-import tempfile
+
 import unittest
-import shutil
-from parameterized import parameterized
 
 from conans.model.conan_file import ConanFile
 from conans.model.env_info import EnvValues
 from conans.model.settings import Settings
 from conans.test.utils.tools import TestClient
-from conans.client.loader import ConanFileLoader
-from conans.client.graph.python_requires import ConanPythonRequire
-from conans.client.tools.files import save
-from conans.client.tools.env import environment_append
 
 
 class ConanFileTest(unittest.TestCase):
@@ -48,72 +41,3 @@ class Pkg(ConanFile):
         client.save({"conanfile.py": conanfile.replace("pass",
                                                        "requires = 'PkgB/0.1@user/testing'")})
         client.run("create . PkgC/0.1@user/testing")
-
-
-class ConanFileShortPathsTests(unittest.TestCase):
-    conanfile = """from conans import ConanFile
-class Pkg(ConanFile):
-    {short_paths}
-    pass
-    """
-
-    def run(self, *args, **kwargs):
-        tmp_folder = tempfile.mkdtemp(suffix='_conans')
-        try:
-            self.tmp_conanfile = os.path.join(tmp_folder, 'conanfile.py')
-            super(ConanFileShortPathsTests, self).run(*args, **kwargs)
-        finally:
-            shutil.rmtree(tmp_folder, ignore_errors=True)
-
-    def setUp(self):
-        self.loader = ConanFileLoader(None, None, ConanPythonRequire(None, None))
-
-    def test_default_behavior(self):
-        save(self.tmp_conanfile, self.conanfile.format(short_paths=""))
-        conanfile = self.loader.load_class(self.tmp_conanfile)
-
-        self.assertEqual(False, conanfile.short_paths)
-
-    @parameterized.expand([(False,), (True,)])
-    def test_legacy_behaviour(self, short_paths):
-        """ Assigning to ConanFile::short_paths will override the property itself """
-        short_paths_str = "short_paths = True" if short_paths else "short_paths = False"
-        save(self.tmp_conanfile, self.conanfile.format(short_paths=short_paths_str))
-        conanfile = self.loader.load_class(self.tmp_conanfile)
-        self.assertEqual(short_paths, conanfile.short_paths)
-
-    @parameterized.expand([(False,), (True,)])
-    def test_use_always_short_paths_legacy_behavior(self, short_paths):
-        short_paths_str = "short_paths = True" if short_paths else "short_paths = False"
-
-        with environment_append({'CONAN_USE_ALWAYS_SHORT_PATHS': "True"}):
-            save(self.tmp_conanfile, self.conanfile.format(short_paths=short_paths_str))
-            conanfile = self.loader.load_class(self.tmp_conanfile)
-            self.assertEqual(True, conanfile.short_paths)
-
-        with environment_append({'CONAN_USE_ALWAYS_SHORT_PATHS': "False"}):
-            save(self.tmp_conanfile, self.conanfile.format(short_paths=short_paths_str))
-            conanfile = self.loader.load_class(self.tmp_conanfile)
-            self.assertEqual(short_paths, conanfile.short_paths)
-
-    @parameterized.expand([(False,), (True,)])
-    def test_conan_behaviour(self, short_paths):
-        short_paths_str = "_conan_short_paths = True" if short_paths else "_conan_short_paths = False"
-        save(self.tmp_conanfile, self.conanfile.format(short_paths=short_paths_str))
-        conanfile = self.loader.load_class(self.tmp_conanfile)
-        self.assertEqual(short_paths, conanfile.short_paths)
-
-    @parameterized.expand([(False,), (True,)])
-    def test_use_always_short_paths_conan_behaviour(self, short_paths):
-        short_paths_str = "_conan_short_paths = True" if short_paths else "_conan_short_paths = False"
-
-        with environment_append({'CONAN_USE_ALWAYS_SHORT_PATHS': "True"}):
-            save(self.tmp_conanfile, self.conanfile.format(short_paths=short_paths_str))
-            conanfile = self.loader.load_class(self.tmp_conanfile)
-            self.assertEqual(True, conanfile.short_paths)
-
-        with environment_append({'CONAN_USE_ALWAYS_SHORT_PATHS': "False"}):
-            save(self.tmp_conanfile, self.conanfile.format(short_paths=short_paths_str))
-            conanfile = self.loader.load_class(self.tmp_conanfile)
-            self.assertEqual(short_paths, conanfile.short_paths)
-
