a730b5b1b999d9f2b6d68d07937bc07b5feb0fec
Javier G. Sogo <jgsogo@gmail.com>
2018-11-05 10:12:55 +0100

    remove unused imports

--- conans/client/tools/net.py
+++ conans/client/tools/net.py
@@ -1,7 +1,5 @@
-import sys
-
 import os
-from conans.client.output import ConanOutput
+
 from conans.client.rest.uploader_downloader import Downloader
 from conans.client.tools.files import unzip, check_md5, check_sha1, check_sha256
 from conans.errors import ConanException
