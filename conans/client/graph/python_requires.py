cb77cb88028173cc32e1ffc8c260edb45de4c78a
Javier G. Sogo <jgsogo@gmail.com>
2018-11-19 14:15:21 +0100

    resolve aliased recipes

--- conans/client/graph/python_requires.py
+++ conans/client/graph/python_requires.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-from conans.client.loader import parse_conanfile
+from conans.client.loader import parse_conanfile, parse_module
 from conans.client.recorder.action_recorder import ActionRecorder
 from conans.model.ref import ConanFileReference
 from conans.model.requires import Requirement
@@ -34,7 +34,13 @@ class ConanPythonRequire(object):
             result = self._proxy.get_recipe(r, False, False, remote_name=None,
                                             recorder=ActionRecorder())
             path, _, _, reference = result
-            module, _ = parse_conanfile(path)
+            module, filename = parse_conanfile(path)
+
+            # Check for alias
+            conanfile = parse_module(module, filename)
+            if getattr(conanfile, "alias", None):
+                module = self.__call__(require=conanfile.alias)  # Will register also the aliased
+
             python_require = PythonRequire(reference, module)
             self._cached_requires[require] = python_require
         self._requires.append(python_require)
