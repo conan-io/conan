e58cd55878cddc63281ec87d732de90ede3b0680
Matt Lord <luckielordie@gmail.com>
2018-11-29 10:44:42 +0000

    Added the ability to output a binary log from MSBuild. (#3626)
    * Added the ability to output a binary log from MSBuild.
    
    * Added complex parameter for output_binary_log.
    
    * Added complex parameter for output_binary_log.
    
    * fixes from review
    
    * Moved tests, new get_version() with more tests
    
    * fix py27 test
    
    * build test
    
    * review
    
    * Remove quotes, make it simpler
    
    * revert and fix VS detect
    
    * try fix

--- conans/test/build_helpers/msbuild_test.py
+++ conans/test/build_helpers/msbuild_test.py
@@ -1,9 +1,14 @@
+import os
 import platform
 import unittest
 
+import mock
 from nose.plugins.attrib import attr
+from parameterized import parameterized
 
 from conans.client.build.msbuild import MSBuild
+from conans.errors import ConanException
+from conans.model.version import Version
 from conans.paths import CONANFILE
 from conans.test.utils.conanfile import MockSettings, MockConanfile
 from conans.test.utils.tools import TestClient
@@ -151,3 +156,114 @@ class HelloConan(ConanFile):
         client.save(files)
         client.run("create . danimtb/testing")
         self.assertIn("build() completed", client.out)
+
+    @unittest.skipUnless(platform.system() == "Windows", "Requires MSBuild")
+    def binary_logging_on_test(self):
+        settings = MockSettings({"build_type": "Debug",
+                                 "compiler": "Visual Studio",
+                                 "compiler.version": "15",
+                                 "arch": "x86_64",
+                                 "compiler.runtime": "MDd"})
+        conanfile = MockConanfile(settings)
+        msbuild = MSBuild(conanfile)
+        command = msbuild.get_command("dummy.sln", output_binary_log=True)
+        self.assertIn("/bl", command)
+
+    @unittest.skipUnless(platform.system() == "Windows", "Requires MSBuild")
+    def binary_logging_on_with_filename_test(self):
+        bl_filename = "a_special_log.log"
+        settings = MockSettings({"build_type": "Debug",
+                                 "compiler": "Visual Studio",
+                                 "compiler.version": "15",
+                                 "arch": "x86_64",
+                                 "compiler.runtime": "MDd"})
+        conanfile = MockConanfile(settings)
+        msbuild = MSBuild(conanfile)
+        command = msbuild.get_command("dummy.sln", output_binary_log=bl_filename)
+        expected_command = '/bl:"%s"' % bl_filename
+        self.assertIn(expected_command, command)
+
+    def binary_logging_off_explicit_test(self):
+        settings = MockSettings({"build_type": "Debug",
+                                 "compiler": "Visual Studio",
+                                 "compiler.version": "15",
+                                 "arch": "x86_64",
+                                 "compiler.runtime": "MDd"})
+        conanfile = MockConanfile(settings)
+        msbuild = MSBuild(conanfile)
+        command = msbuild.get_command("dummy.sln", output_binary_log=False)
+        self.assertNotIn("/bl", command)
+
+    def binary_logging_off_implicit_test(self):
+        settings = MockSettings({"build_type": "Debug",
+                                 "compiler": "Visual Studio",
+                                 "compiler.version": "15",
+                                 "arch": "x86_64",
+                                 "compiler.runtime": "MDd"})
+        conanfile = MockConanfile(settings)
+        msbuild = MSBuild(conanfile)
+        command = msbuild.get_command("dummy.sln")
+        self.assertNotIn("/bl", command)
+
+    @unittest.skipUnless(platform.system() == "Windows", "Requires MSBuild")
+    @mock.patch("conans.client.build.msbuild.MSBuild.get_version")
+    def binary_logging_not_supported_test(self, mock_get_version):
+        mock_get_version.return_value = Version("14")
+
+        mocked_settings = MockSettings({"build_type": "Debug",
+                                        "compiler": "Visual Studio",
+                                        "compiler.version": "15",
+                                        "arch": "x86_64",
+                                        "compiler.runtime": "MDd"})
+        conanfile = MockConanfile(mocked_settings)
+        except_text = "MSBuild version detected (14) does not support 'output_binary_log' ('/bl')"
+        msbuild = MSBuild(conanfile)
+
+        with self.assertRaises(ConanException) as exc:
+            msbuild.get_command("dummy.sln", output_binary_log=True)
+        self.assertIn(except_text, str(exc.exception))
+
+    @unittest.skipUnless(platform.system() == "Windows", "Requires MSBuild")
+    def get_version_test(self):
+        settings = MockSettings({"build_type": "Debug",
+                                 "compiler": "Visual Studio",
+                                 "compiler.version": "15",
+                                 "arch": "x86_64",
+                                 "compiler.runtime": "MDd"})
+        version = MSBuild.get_version(settings)
+        self.assertRegexpMatches(version, "(\d+\.){2,3}\d+")
+        self.assertGreater(version, "15.1")
+
+    @parameterized.expand([("True",), ("'my_log.binlog'",)])
+    @unittest.skipUnless(platform.system() == "Windows", "Requires MSBuild")
+    def binary_log_build_test(self, value):
+        conan_build_vs = """
+from conans import ConanFile, MSBuild
+
+class HelloConan(ConanFile):
+    name = "Hello"
+    version = "1.2.1"
+    exports = "*"
+    settings = "os", "build_type", "arch", "compiler"
+
+    def build(self):
+        msbuild = MSBuild(self)
+        msbuild.build("MyProject.sln", output_binary_log=%s)
+"""
+        client = TestClient()
+        files = get_vs_project_files()
+        files[CONANFILE] = conan_build_vs % value
+        client.save(files)
+        client.run("install . -s compiler=\"Visual Studio\" -s compiler.version=15")
+        client.run("build .")
+
+        if value == "'my_log.binlog'":
+            log_name = value[1:1]
+            flag = "/bl:%s" % log_name
+        else:
+            log_name = "msbuild.binlog"
+            flag = "/bl"
+
+        self.assertIn(flag, client.out)
+        log_path = os.path.join(client.current_folder, log_name)
+        self.assertTrue(os.path.exists(log_path))
