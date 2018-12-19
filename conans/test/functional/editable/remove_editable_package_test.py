997684fa90b4dcef096883d42863fc60000cbe6b
Javier G. Sogo <jgsogo@gmail.com>
2018-12-19 12:00:40 +0100

    check after unlink

--- conans/test/functional/editable/remove_editable_package_test.py
+++ conans/test/functional/editable/remove_editable_package_test.py
@@ -22,13 +22,14 @@ class RemoveEditablePackageTest(unittest.TestCase):
         self.t = TestClient()
         self.t.save(files={'conanfile.py': self.conanfile,
                            CONAN_PACKAGE_LAYOUT_FILE: "", })
-        self.t.run('export  . {}'.format(self.reference))  # No need to export, will create it on the fly
+        #self.t.run('export  . {}'.format(self.reference))  # No need to export, will create it on the fly
         self.t.run('link . {}'.format(self.reference))
         self.assertTrue(self.t.client_cache.installed_as_editable(self.reference))
 
     def test_unlink(self):
         self.t.run('link {} --remove'.format(self.reference))
         self.assertIn("Removed linkage for reference '{}'".format(self.reference), self.t.out)
+        self.assertFalse(self.t.client_cache.installed_as_editable(self.reference))
 
     def test_remove(self):
         self.t.run('remove {} --force'.format(self.reference), assert_error=True)
