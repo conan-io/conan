39cb234b10bd62435e6bb09d870b830e5e0435b7
danimtb <danimanzaneque@gmail.com>
2018-12-27 12:54:50 +0100

    New test to check for conan_basic_setup()

--- conans/test/unittests/client/generators/premake_test.py
+++ conans/test/unittests/client/generators/premake_test.py
@@ -1,4 +1,5 @@
 import os
+import textwrap
 import unittest
 
 from conans.client.generators import PremakeGenerator
@@ -14,20 +15,20 @@ from conans.test.utils.tools import TestBufferConanOutput
 
 class PremakeGeneratorTest(unittest.TestCase):
 
-    def variables_setup_test(self):
-        tmp_folder1 = temp_folder()
-        tmp_folder2 = temp_folder()
-        save(os.path.join(tmp_folder1, "include1", "file.h"), "")
-        save(os.path.join(tmp_folder2, "include2", "file.h"), "")
-        save(os.path.join(tmp_folder1, "lib1", "file.a"), "")
-        save(os.path.join(tmp_folder2, "lib2", "file.a"), "")
-        save(os.path.join(tmp_folder1, "bin1", "file.bin"), "")
-        save(os.path.join(tmp_folder2, "bin2", "file.bin"), "")
+    def setUp(self):
+        self.tmp_folder1 = temp_folder()
+        self.tmp_folder2 = temp_folder()
+        save(os.path.join(self.tmp_folder1, "include1", "file.h"), "")
+        save(os.path.join(self.tmp_folder2, "include2", "file.h"), "")
+        save(os.path.join(self.tmp_folder1, "lib1", "file.a"), "")
+        save(os.path.join(self.tmp_folder2, "lib2", "file.a"), "")
+        save(os.path.join(self.tmp_folder1, "bin1", "file.bin"), "")
+        save(os.path.join(self.tmp_folder2, "bin2", "file.bin"), "")
 
-        conanfile = ConanFile(TestBufferConanOutput(), None)
-        conanfile.initialize(Settings({}), EnvValues())
+        self.conanfile = ConanFile(TestBufferConanOutput(), None)
+        self.conanfile.initialize(Settings({}), EnvValues())
         ref = ConanFileReference.loads("MyPkg1/0.1@lasote/stables")
-        cpp_info = CppInfo(tmp_folder1)
+        cpp_info = CppInfo(self.tmp_folder1)
         cpp_info.defines = ["MYDEFINE1"]
         cpp_info.includedirs = ['include1']
         cpp_info.libdirs = ['lib1']
@@ -38,9 +39,9 @@ class PremakeGeneratorTest(unittest.TestCase):
         cpp_info.cppflags = ['-fPIE']
         cpp_info.sharedlinkflags = ['-framework Cocoa']
         cpp_info.exelinkflags = ['-framework QuartzCore']
-        conanfile.deps_cpp_info.update(cpp_info, ref.name)
+        self.conanfile.deps_cpp_info.update(cpp_info, ref.name)
         ref = ConanFileReference.loads("MyPkg2/3.2.3@lasote/stables")
-        cpp_info = CppInfo(tmp_folder2)
+        cpp_info = CppInfo(self.tmp_folder2)
         cpp_info.defines = ["MYDEFINE2"]
         cpp_info.includedirs = ['include2']
         cpp_info.libdirs = ['lib2']
@@ -51,28 +52,30 @@ class PremakeGeneratorTest(unittest.TestCase):
         cpp_info.cppflags = ['-march=native']
         cpp_info.sharedlinkflags = ['-framework AudioFoundation']
         cpp_info.exelinkflags = ['-framework VideoToolbox']
-        conanfile.deps_cpp_info.update(cpp_info, ref.name)
-        generator = PremakeGenerator(conanfile)
+        self.conanfile.deps_cpp_info.update(cpp_info, ref.name)
+
+    def test_variables_content(self):
+        generator = PremakeGenerator(self.conanfile)
         content = generator.content
 
         self.assertIn('conan_cppdefines = {"MYDEFINE2", "MYDEFINE1"}', content)
         self.assertIn('conan_cppdefines_MyPkg1 = {"MYDEFINE1"}', content)
         self.assertIn('conan_cppdefines_MyPkg2 = {"MYDEFINE2"}', content)
 
-        inc1 = os.path.join(tmp_folder1, 'include1').replace('\\', '/')
-        inc2 = os.path.join(tmp_folder2, 'include2').replace('\\', '/')
+        inc1 = os.path.join(self.tmp_folder1, 'include1').replace('\\', '/')
+        inc2 = os.path.join(self.tmp_folder2, 'include2').replace('\\', '/')
         self.assertIn('conan_includedirs = {"%s",\n"%s"}' % (inc1, inc2), content)
         self.assertIn('conan_includedirs_MyPkg1 = {"%s"}' % inc1, content)
         self.assertIn('conan_includedirs_MyPkg2 = {"%s"}' % inc2, content)
 
-        lib1 = os.path.join(tmp_folder1, 'lib1').replace('\\', '/')
-        lib2 = os.path.join(tmp_folder2, 'lib2').replace('\\', '/')
+        lib1 = os.path.join(self.tmp_folder1, 'lib1').replace('\\', '/')
+        lib2 = os.path.join(self.tmp_folder2, 'lib2').replace('\\', '/')
         self.assertIn('conan_libdirs = {"%s",\n"%s"}' % (lib1, lib2), content)
         self.assertIn('conan_libdirs_MyPkg1 = {"%s"}' % lib1, content)
         self.assertIn('conan_libdirs_MyPkg2 = {"%s"}' % lib2, content)
 
-        bin1 = os.path.join(tmp_folder1, 'bin1').replace('\\', '/')
-        bin2 = os.path.join(tmp_folder2, 'bin2').replace('\\', '/')
+        bin1 = os.path.join(self.tmp_folder1, 'bin1').replace('\\', '/')
+        bin2 = os.path.join(self.tmp_folder2, 'bin2').replace('\\', '/')
         self.assertIn('conan_bindirs = {"%s",\n"%s"}' % (bin1, bin2), content)
         self.assertIn('conan_bindirs_MyPkg1 = {"%s"}' % bin1, content)
         self.assertIn('conan_bindirs_MyPkg2 = {"%s"}' % bin2, content)
@@ -89,10 +92,28 @@ class PremakeGeneratorTest(unittest.TestCase):
         self.assertIn('conan_cppflags_MyPkg1 = {"-fPIE"}', content)
         self.assertIn('conan_cppflags_MyPkg2 = {"-march=native"}', content)
 
-        self.assertIn('conan_sharedlinkflags = {"-framework AudioFoundation", "-framework Cocoa"}', content)
+        self.assertIn('conan_sharedlinkflags = {"-framework AudioFoundation", "-framework Cocoa"}',
+                      content)
         self.assertIn('conan_sharedlinkflags_MyPkg1 = {"-framework Cocoa"}', content)
         self.assertIn('conan_sharedlinkflags_MyPkg2 = {"-framework AudioFoundation"}', content)
 
-        self.assertIn('conan_exelinkflags = {"-framework VideoToolbox", "-framework QuartzCore"}', content)
+        self.assertIn('conan_exelinkflags = {"-framework VideoToolbox", "-framework QuartzCore"}',
+                      content)
         self.assertIn('conan_exelinkflags_MyPkg1 = {"-framework QuartzCore"}', content)
         self.assertIn('conan_exelinkflags_MyPkg2 = {"-framework VideoToolbox"}', content)
+
+    def test_conan_basic_setup_content(self):
+        generator = PremakeGenerator(self.conanfile)
+        content = generator.content
+
+        basic_setup_expected = textwrap.dedent("""
+                function conan_basic_setup()
+                    configurations{conan_build_type}
+                    architecture(conan_arch)
+                    includedirs{conan_includedirs}
+                    libdirs{conan_libdirs}
+                    links{conan_libs}
+                    defines{conan_cppdefines}
+                    bindirs{conan_bindirs}
+                end""")
+        self.assertIn(basic_setup_expected, content)
