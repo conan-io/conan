5d95755746898ab4cc5021d3a0c865c35edd7fc2
Javier G. Sogo <jgsogo@gmail.com>
2018-12-28 13:20:08 +0100

    add 'remove_credentials' argument to SCMBase.get_remote_url

---
+++ conans/test/unittests/client/tools/scm/test_scm_base.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+
+import unittest
+
+from conans.client.tools.scm import SCMBase
+
+
+class RemoveCredentialsTest(unittest.TestCase):
+
+    def test_http(self):
+        expected_url = 'https://myrepo.com/path/to/repo.git'
+        test_urls = ['https://myrepo.com/path/to/repo.git',
+                     'https://username:password@myrepo.com/path/to/repo.git',
+                     'https://username@myrepo.com/path/to/repo.git',
+                     'https://gitlab-ci-token:1324@myrepo.com/path/to/repo.git',
+                     ]
+
+        for it in test_urls:
+            self.assertEqual(expected_url, SCMBase._remove_credentials_url(it))
+
+    def test_http_with_port_number(self):
+        self.assertEqual('https://myrepo.com:8000/path/to/repo.git',
+                         SCMBase._remove_credentials_url(
+                             'https://username@myrepo.com:8000/path/to/repo.git'))
+
+    def test_ssh(self):
+        # Here, for ssh, we don't want to remove the user ('git' in this example)
+        self.assertEqual('git@github.com:conan-io/conan.git',
+                         SCMBase._remove_credentials_url(
+                             'git@github.com:conan-io/conan.git'))
+
+    def test_local(self):
+        self.assertEqual('file:///srv/git/project.git',
+                         SCMBase._remove_credentials_url('file:///srv/git/project.git'))
+
+    def test_svn_ssh(self):
+        self.assertEqual('svn+ssh://10.106.191.164/home/svn/shproject',
+                         SCMBase._remove_credentials_url(
+                             'svn+ssh://username:password@10.106.191.164/home/svn/shproject'))
