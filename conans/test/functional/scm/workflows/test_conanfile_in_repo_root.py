3e4cfc589608f31ae294067d6284a74b755788bb
Javier G. Sogo <jgsogo@gmail.com>
2019-01-04 19:46:40 +0100

    add tests with conanfile in subfolder

--- conans/test/functional/scm/workflows/test_conanfile_in_repo_root.py
+++ conans/test/functional/scm/workflows/test_conanfile_in_repo_root.py
@@ -4,81 +4,28 @@ import os
 import textwrap
 import unittest
 
-from conans.client.tools import environment_append
+from conans.test.functional.scm.workflows.common import TestWorkflow
 from conans.test.utils.tools import SVNLocalRepoTestCase
 from conans.test.utils.tools import TestClient, create_local_git_repo
 
 
-class TestWorkflows:
-    path_to_conanfile = "."  # TODO: Test with 'conan'
+class ConanfileInRepoRoot(TestWorkflow):
+    path_to_conanfile = "."
     path_from_conanfile_to_root = "."
 
-    conanfile_base = textwrap.dedent("""\
-        import os
-        from conans import ConanFile, tools
 
-        {extra_header}
-        
-        class Pkg(ConanFile):
-            scm = {{"type": "{type}",
-                   "url": {url},
-                   "revision": "auto"}}
-
-            def source(self):
-                self.output.info(self.source_folder)
-                content = tools.load(os.path.join(self.source_folder, "file.txt"))
-                self.output.info(">>>> I'm {{}}/{{}}@{{}}/{{}}".format(self.name, self.version, 
-                                                                       self.user, self.channel))
-                self.output.info(">>>> content: {{}} ".format(content)) 
-        """)
-
-    def get_files(self, subfolder, conanfile, lib_ref):
-        return {os.path.join(subfolder, self.path_to_conanfile, 'conanfile.py'): conanfile,
-                os.path.join(subfolder, 'file.txt'): lib_ref}
-
-    def run(self, *args, **kwargs):
-        # path_to_conanfile and path_from_conanfile_to_root must be opposite ones.
-        self.assertEqual(os.path.normpath(os.path.join(self.path_to_conanfile,
-                                                       self.path_from_conanfile_to_root)),
-                         '.')
-
-        with environment_append({'CONAN_USERNAME': "user", "CONAN_CHANNEL": "channel"}):
-            super(TestWorkflows, self).run(*args, **kwargs)
-
-    def _run_local_test(self, t, working_dir, path_to_conanfile):
-        old_wd = t.current_folder
-        try:
-            t.current_folder = working_dir
-            t.run("install {} -if tmp".format(path_to_conanfile))
-            t.run("source {} -if tmp -sf src".format(path_to_conanfile))
-            self.assertIn(">>>> I'm None/None@user/channel".format(self.lib1_ref), t.out)
-            self.assertIn(">>>> content: {}".format(self.lib1_ref), t.out)
-        finally:
-            t.current_folder = old_wd
-
-    def _run_remote_test(self, t, working_dir, path_to_conanfile):
-        old_wd = t.current_folder
-        try:
-            t.current_folder = working_dir
-            t.run("create {} {}".format(path_to_conanfile, self.lib1_ref))
-            self.assertIn(">>>> I'm {}".format(self.lib1_ref), t.out)
-            self.assertIn(">>>> content: {}".format(self.lib1_ref), t.out)
-        finally:
-            t.current_folder = old_wd
-
-
-class SVNWorkflowsTest(TestWorkflows, SVNLocalRepoTestCase):
+class SVNConanfileInRepoRootTest(ConanfileInRepoRoot, SVNLocalRepoTestCase):
 
     extra_header = textwrap.dedent("""\
         def get_remote_url():
             here = os.path.dirname(__file__)
             svn = tools.SVN(os.path.join(here, "%s"))
             return svn.get_remote_url()
-        """ % TestWorkflows.path_from_conanfile_to_root)
+        """ % ConanfileInRepoRoot.path_from_conanfile_to_root)
 
-    conanfile = TestWorkflows.conanfile_base.format(extra_header=extra_header,
-                                                    type="svn",
-                                                    url="get_remote_url()")
+    conanfile = ConanfileInRepoRoot.conanfile_base.format(extra_header=extra_header,
+                                                          type="svn",
+                                                          url="get_remote_url()")
 
     def setUp(self):
         self.lib1_ref = "lib1/version@user/channel"
@@ -118,11 +65,11 @@ class SVNWorkflowsTest(TestWorkflows, SVNLocalRepoTestCase):
         self._run_remote_test(t, os.path.join(t.current_folder, "lib1"), self.path_to_conanfile)
 
 
-class GitWorkflowsTest(TestWorkflows, unittest.TestCase):
+class GitConanfileInRepoRootTest(ConanfileInRepoRoot, unittest.TestCase):
 
-    conanfile = TestWorkflows.conanfile_base.format(extra_header="",
-                                                    type="git",
-                                                    url="\"auto\"")
+    conanfile = ConanfileInRepoRoot.conanfile_base.format(extra_header="",
+                                                          type="git",
+                                                          url="\"auto\"")
 
     def setUp(self):
         self.lib1_ref = "lib1/version@user/channel"
