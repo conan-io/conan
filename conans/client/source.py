100eb2c55c8274f78da782516bb340c2b2809b87
Javier G. Sogo <jgsogo@gmail.com>
2019-01-04 20:00:22 +0100

    fix local source method

--- conans/client/source.py
+++ conans/client/source.py
@@ -194,11 +194,23 @@ def _run_scm(conanfile, src_folder, local_sources_path, output, cache):
     dest_dir = os.path.normpath(os.path.join(src_folder, scm_data.subfolder))
     if cache:
         # When in cache, capturing the sources from user space is done only if exists
-        captured = local_sources_path and os.path.exists(local_sources_path)
+        if not local_sources_path or not os.path.exists(local_sources_path):
+            local_sources_path = None
     else:
         # In user space, if revision="auto", then copy
         captured = scm_data.capture_origin or scm_data.capture_revision
-    local_sources_path = local_sources_path if captured else None
+
+        if captured:
+            # TODO: Factor out this functionality
+            conanfile_dir = local_sources_path
+            scm = SCM(scm_data, conanfile_dir, output)
+            scm_url = scm_data.url if scm_data.url != "auto" else scm.get_qualified_remote_url(remove_credentials=True)
+
+            src_path = scm.get_repo_root()
+            url_root = SCM(scm_data, src_path, output).get_remote_url(remove_credentials=True)
+            local_sources_path = os.path.join(src_path, os.path.relpath(scm_url, url_root))
+        else:
+            local_sources_path = None
 
     if local_sources_path:
         excluded = SCM(scm_data, local_sources_path, output).excluded_files
@@ -208,6 +220,7 @@ def _run_scm(conanfile, src_folder, local_sources_path, output, cache):
         output.info("Getting sources from url: '%s'" % scm_data.url)
         scm = SCM(scm_data, dest_dir, output)
         scm.checkout()
+
     if cache:
         # This is a bit weird. Why after a SCM should we remove files. Maybe check conan 2.0
         _clean_source_folder(dest_dir)
