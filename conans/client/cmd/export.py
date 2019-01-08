7eafda4bd357fedccc9c01a0722d84560ef16124
Javier G. Sogo <jgsogo@gmail.com>
2019-01-08 18:47:55 +0100

    clean URL (remove peg-revision from SVN), otherwise relpath between urls won't be computed properly

--- conans/client/cmd/export.py
+++ conans/client/cmd/export.py
@@ -99,7 +99,8 @@ def _capture_export_scm_data(conanfile, conanfile_dir, destination_folder, outpu
         # Generate the scm_folder.txt file pointing to the src_path
         src_path = scm.get_repo_root()
         url_root = SCM(scm_data, src_path, output).get_remote_url(remove_credentials=True)
-        src_path = os.path.join(src_path, os.path.relpath(scm_data.url, url_root))
+        scm_url = SCM.clean_url(scm_data.url)  # Remove peg_revision (SVN), ....
+        src_path = os.path.join(src_path, os.path.relpath(scm_url, url_root))
         save(scm_src_file, os.path.normpath(src_path).replace("\\", "/"))
 
     return scm_data, captured_revision
