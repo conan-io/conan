8487db9407c4e01954bb24fdc2e1fb078fd84e05
Javier G. Sogo <jgsogo@gmail.com>
2019-01-08 19:03:25 +0100

    remove dupe test

--- conans/test/functional/scm/workflows/test_conanfile_in_repo_root.py
+++ conans/test/functional/scm/workflows/test_conanfile_in_repo_root.py
@@ -19,8 +19,20 @@ class ConanfileInRepoRoot(TestWorkflow):
 
 
 @attr("svn")
-class SVNAutoConanfileInRepoRootTest(ConanfileInRepoRoot, SVNLocalRepoTestCase):
-    """ Test SCM url='auto' with SVN, it can only work if conanfile is in the root of the repo """
+class SVNConanfileInRepoRootTest(ConanfileInRepoRoot, SVNLocalRepoTestCase):
+    """ Test SCM url='auto' with SVN, it can only work if conanfile is in the root of the repo
+
+        In this case, it is exactly the same to have the url="auto" or to implement a custom
+        get_remote_url function with the following behavior because the SVN class will be
+        created in the conanfile.py directory by default:
+
+        def get_remote_url():
+            here = os.path.dirname(__file__)
+            svn = tools.SVN(os.path.join(here, "."))
+            return svn.get_remote_url()
+
+    """
+
     conanfile = ConanfileInRepoRoot.conanfile_base.format(extra_header="",
                                                           type="svn",
                                                           url="\"auto\"")
@@ -64,61 +76,6 @@ class SVNAutoConanfileInRepoRootTest(ConanfileInRepoRoot, SVNLocalRepoTestCase):
         t.runner("svn co {} .".format(self.url), cwd=t.current_folder)
         self._run_remote_test(t, os.path.join(t.current_folder, "lib1"), self.path_to_conanfile)
         self.assertIn("Repo origin deduced by 'auto':", t.out)
-
-
-@attr("svn")
-class SVNConanfileInRepoRootTest(ConanfileInRepoRoot, SVNLocalRepoTestCase):
-
-    extra_header = textwrap.dedent("""\
-        def get_remote_url():
-            here = os.path.dirname(__file__)
-            svn = tools.SVN(os.path.join(here, "%s"))
-            return svn.get_remote_url()
-        """ % ConanfileInRepoRoot.path_from_conanfile_to_root)
-
-    conanfile = ConanfileInRepoRoot.conanfile_base.format(extra_header=extra_header,
-                                                          type="svn",
-                                                          url="get_remote_url()")
-
-    def setUp(self):
-        self.lib1_ref = "lib1/version@user/channel"
-        files = self.get_files(subfolder='lib1', conanfile=self.conanfile, lib_ref=self.lib1_ref)
-        self.url, _ = self.create_project(files=files)
-
-    # Local workflow
-    def test_local_root_folder(self):
-        t = TestClient(path_with_spaces=False)
-        t.runner("svn co {}/lib1 .".format(self.url), cwd=t.current_folder)
-        self._run_local_test(t, t.current_folder, self.path_to_conanfile)
-
-    def test_local_monorepo(self):
-        t = TestClient(path_with_spaces=False)
-        t.runner("svn co {} .".format(self.url), cwd=t.current_folder)
-        self._run_local_test(t, t.current_folder, os.path.join("lib1", self.path_to_conanfile))
-
-    def test_local_monorepo_chdir(self):
-        t = TestClient(path_with_spaces=False)
-        t.runner("svn co {} .".format(self.url), cwd=t.current_folder)
-        self._run_local_test(t, os.path.join(t.current_folder, "lib1"), self.path_to_conanfile)
-
-    # Cache workflow
-    def test_remote_root_folder(self):
-        t = TestClient(path_with_spaces=False)
-        t.runner("svn co {}/lib1 .".format(self.url), cwd=t.current_folder)
-        self._run_remote_test(t, t.current_folder, self.path_to_conanfile)
-        self.assertNotIn("Repo origin deduced by 'auto':", t.out)
-
-    def test_remote_monorepo(self):
-        t = TestClient(path_with_spaces=False)
-        t.runner("svn co {} .".format(self.url), cwd=t.current_folder)
-        self._run_remote_test(t, t.current_folder, os.path.join("lib1", self.path_to_conanfile))
-        self.assertNotIn("Repo origin deduced by 'auto':", t.out)
-
-    def test_remote_monorepo_chdir(self):
-        t = TestClient(path_with_spaces=False)
-        t.runner("svn co {} .".format(self.url), cwd=t.current_folder)
-        self._run_remote_test(t, os.path.join(t.current_folder, "lib1"), self.path_to_conanfile)
-        self.assertNotIn("Repo origin deduced by 'auto':", t.out)
 
 
 class GitConanfileInRepoRootTest(ConanfileInRepoRoot, unittest.TestCase):
