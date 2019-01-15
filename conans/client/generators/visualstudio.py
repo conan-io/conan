f1973de4ac0073a374b2200d976f1156ec0a4415
SSE4 <tomskside@gmail.com>
2019-01-15 08:41:23 +0700

    - support N configs -> 1 package scheme for Visual Studio generator
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conans/client/generators/visualstudio.py
+++ conans/client/generators/visualstudio.py
@@ -13,15 +13,19 @@ class VisualStudioGenerator(Generator):
   <PropertyGroup Label="UserMacros" />
   <PropertyGroup Label="Conan-RootDirs">{item_properties}
   </PropertyGroup>
-  <PropertyGroup Label="ConanVariables">
-    <ConanBinaryDirectories>{bin_dirs}</ConanBinaryDirectories>
-    <ConanResourceDirectories>{res_dirs}</ConanResourceDirectories>
+  {properties}
+  <ItemGroup />
+</Project>'''
+
+    properties_template = '''  <PropertyGroup Label="ConanVariables"{condition}>
+    <ConanBinaryDirectories>{bin_dirs};%(ConanBinaryDirectories)</ConanBinaryDirectories>
+    <ConanResourceDirectories>{res_dirs};%(ConanResourceDirectories)$(</ConanResourceDirectories>
   </PropertyGroup>
-  <PropertyGroup>
+  <PropertyGroup{condition}>
     <LocalDebuggerEnvironment>PATH=%PATH%;{bin_dirs}</LocalDebuggerEnvironment>
     <DebuggerFlavor>WindowsLocalDebugger</DebuggerFlavor>
   </PropertyGroup>
-  <ItemDefinitionGroup>
+  <ItemDefinitionGroup{condition}>
     <ClCompile>
       <AdditionalIncludeDirectories>{include_dirs}%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <PreprocessorDefinitions>{definitions}%(PreprocessorDefinitions)</PreprocessorDefinitions>
@@ -40,16 +44,14 @@ class VisualStudioGenerator(Generator):
       <PreprocessorDefinitions>{definitions}%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalOptions>{compiler_flags} %(AdditionalOptions)</AdditionalOptions>
     </ResourceCompile>
-  </ItemDefinitionGroup>
-  <ItemGroup />
-</Project>'''
+  </ItemDefinitionGroup>'''
 
     item_template = '''
     <Conan-{name}-Root>{root_dir}</Conan-{name}-Root>'''
 
     def _format_items(self):
         sections = []
-        for dep_name, cpp_info in self.deps_build_info.dependencies:
+        for dep_name, cpp_info in self._deps_build_info.dependencies:
             fields = {
                 'root_dir': cpp_info.rootpath.replace("\\", "/"),
                 'name': dep_name.replace(".", "-")
@@ -62,23 +64,39 @@ class VisualStudioGenerator(Generator):
     def filename(self):
         return BUILD_INFO_VISUAL_STUDIO
 
+    def _format_properties(self, build_info, condition):
+
+        fields = {
+            'condition': condition,
+            'bin_dirs': "".join("%s;" % p for p in build_info.bin_paths).replace("\\", "/"),
+            'res_dirs': "".join("%s;" % p for p in build_info.res_paths).replace("\\", "/"),
+            'include_dirs': "".join("%s;" % p for p in build_info.include_paths).replace("\\", "/"),
+            'lib_dirs': "".join("%s;" % p for p in build_info.lib_paths).replace("\\", "/"),
+            'libs': "".join(['%s.lib;' % lib if not lib.endswith(".lib")
+                             else '%s;' % lib for lib in build_info.libs]),
+            'definitions': "".join("%s;" % d for d in build_info.defines),
+            'compiler_flags': " ".join(build_info.cppflags + build_info.cflags),
+            'linker_flags': " ".join(build_info.sharedlinkflags),
+            'exe_flags': " ".join(build_info.exelinkflags)
+        }
+        formatted_template = self.properties_template.format(**fields)
+        return formatted_template
+
     @property
     def content(self):
         per_item_props = self._format_items()
+
+        properties = [self._format_properties(self._deps_build_info, condition='')]
+        for config, cpp_info in self._deps_build_info.configs.items():
+            condition = " Condition=\"'$(Configuration)' == '%s'\"" % config
+            properties.append(self._format_properties(cpp_info, condition=condition))
+
         fields = {
             'item_properties': per_item_props,
-            'bin_dirs': "".join("%s;" % p for p in self._deps_build_info.bin_paths).replace("\\", "/"),
-            'res_dirs': "".join("%s;" % p for p in self._deps_build_info.res_paths).replace("\\", "/"),
-            'include_dirs': "".join("%s;" % p for p in self._deps_build_info.include_paths).replace("\\", "/"),
-            'lib_dirs': "".join("%s;" % p for p in self._deps_build_info.lib_paths).replace("\\", "/"),
-            'libs': "".join(['%s.lib;' % lib if not lib.endswith(".lib")
-                             else '%s;' % lib for lib in self._deps_build_info.libs]),
-            'definitions': "".join("%s;" % d for d in self._deps_build_info.defines),
-            'compiler_flags': " ".join(self._deps_build_info.cppflags + self._deps_build_info.cflags),
-            'linker_flags': " ".join(self._deps_build_info.sharedlinkflags),
-            'exe_flags': " ".join(self._deps_build_info.exelinkflags)
+            'properties': '\n'.join(properties)
         }
         formatted_template = self.template.format(**fields)
+
         userprofile = os.getenv("USERPROFILE")
         if userprofile:
             userprofile = userprofile.replace("\\", "/")
