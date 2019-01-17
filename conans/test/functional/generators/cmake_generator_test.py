bd5052a9cdbfa7e7b062a702a664d1287c8aa0e7
danimtb <danimanzaneque@gmail.com>
2019-01-17 19:08:54 +0100

    simplified helper

--- conans/test/functional/generators/cmake_generator_test.py
+++ conans/test/functional/generators/cmake_generator_test.py
@@ -46,30 +46,26 @@ compiler.libcxx=libstdc++
 
 class CMakeGeneratorTest(unittest.TestCase):
 
-    def _generator_test_helper(self, os_build, compiler, compiler_version, generator):
-        try:
-            client = TestClient()
-            client.save({"conanfile.py": CONAN_RECIPE,
-                         "CMakeLists.txt": CMAKE_RECIPE,
-                         "dummy.cpp": CPP_CONTENT,
-                         "my_profile": PROFILE.format(os_build=os_build, compiler=compiler,
-                                                      compiler_version=compiler_version)})
-            client.run("install . -p my_profile")
-            client.run("build .")
-        finally:
-            self.assertNotIn("TypeError: argument of type 'NoneType' is not iterable", client.out)
-            self.assertNotIn("ERROR:", client.out)
-            self.assertIn("Check for working CXX compiler", client.out)
-            self.assertIn('cmake -G "{}"'.format(generator), client.out)
+    def _check_build_generator(self, os_build, compiler, compiler_version, generator):
+        client = TestClient()
+        client.save({"conanfile.py": CONAN_RECIPE,
+                     "CMakeLists.txt": CMAKE_RECIPE,
+                     "dummy.cpp": CPP_CONTENT,
+                     "my_profile": PROFILE.format(os_build=os_build, compiler=compiler,
+                                                  compiler_version=compiler_version)})
+        client.run("install . -p my_profile")
+        client.run("build .")
+        self.assertIn("Check for working CXX compiler", client.out)
+        self.assertIn('cmake -G "{}"'.format(generator), client.out)
 
     @unittest.skipUnless(tools.os_info.is_linux, "Compilation with real gcc needed")
     def test_cmake_default_generator_linux(self):
-        self._generator_test_helper("Linux", "gcc", "5", "Unix Makefiles")
+        self._check_build_generator("Linux", "gcc", "5", "Unix Makefiles")
 
     @unittest.skipUnless(tools.os_info.is_windows, "MinGW is only supported on Windows")
     def test_cmake_default_generator_windows(self):
-        self._generator_test_helper("Windows", "gcc", "7", "MinGW Makefiles")
+        self._check_build_generator("Windows", "gcc", "7", "MinGW Makefiles")
 
     @unittest.skipUnless(tools.os_info.is_macos, "Compilation with real clang is needed")
     def test_cmake_default_generator_osx(self):
-        self._generator_test_helper("Macos", "apple-clang", "9.0", "Unix Makefiles")
+        self._check_build_generator("Macos", "apple-clang", "9.0", "Unix Makefiles")
