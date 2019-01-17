375e93853e59b81aceeb4fa43536a0e419e24caa
danimtb <danimanzaneque@gmail.com>
2019-01-17 18:34:19 +0100

    use compiler version

--- conans/test/functional/generators/cmake_generator_test.py
+++ conans/test/functional/generators/cmake_generator_test.py
@@ -39,20 +39,21 @@ PROFILE = """
 [settings]
 os_build={os_build}
 compiler={compiler}
-compiler.version=5
+compiler.version={compiler_version}
 compiler.libcxx=libstdc++
 """
 
 
 class CMakeGeneratorTest(unittest.TestCase):
 
-    def _generator_test_helper(self, os_build, compiler, generator):
+    def _generator_test_helper(self, os_build, compiler, compiler_version, generator):
         try:
             client = TestClient()
             client.save({"conanfile.py": CONAN_RECIPE,
                          "CMakeLists.txt": CMAKE_RECIPE,
                          "dummy.cpp": CPP_CONTENT,
-                         "my_profile": PROFILE.format(os_build=os_build, compiler=compiler)})
+                         "my_profile": PROFILE.format(os_build=os_build, compiler=compiler,
+                                                      compiler_version=compiler_version)})
             client.run("install . -p my_profile")
             client.run("build .")
         finally:
@@ -61,12 +62,14 @@ class CMakeGeneratorTest(unittest.TestCase):
             self.assertIn("Check for working CXX compiler", client.out)
             self.assertIn('cmake -G "{}"'.format(generator), client.out)
 
+    @unittest.skipUnless(tools.os_info.is_windows, "Compilation with real gcc needed")
     def test_cmake_default_generator_linux(self):
-        self._generator_test_helper("Linux", "gcc", "Unix Makefiles")
+        self._generator_test_helper("Linux", "gcc", "5", "Unix Makefiles")
 
     @unittest.skipUnless(tools.os_info.is_windows, "MinGW is only supported on Windows")
     def test_cmake_default_generator_windows(self):
-        self._generator_test_helper("Windows", "gcc", "MinGW Makefiles")
+        self._generator_test_helper("Windows", "gcc", "7", "MinGW Makefiles")
 
+    @unittest.skipUnless(tools.os_info.is_macos, "Compilation with real clang is needed")
     def test_cmake_default_generator_osx(self):
-        self._generator_test_helper("Macos", "gcc", "Unix Makefiles")
+        self._generator_test_helper("Macos", "apple-clang", "9.1" "Unix Makefiles")
