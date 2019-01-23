b7d0d5147ee22f62c24b8edaa51aaaa1e5fa0f45
SSE4 <tomskside@gmail.com>
2019-01-23 16:48:06 +0700

    - add v142 to settings.yml
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conans/client/conf/__init__.py
+++ conans/client/conf/__init__.py
@@ -64,7 +64,7 @@ compiler:
         toolset: [None, v90, v100, v110, v110_xp, v120, v120_xp,
                   v140, v140_xp, v140_clang_c2, LLVM-vs2012, LLVM-vs2012_xp,
                   LLVM-vs2013, LLVM-vs2013_xp, LLVM-vs2014, LLVM-vs2014_xp,
-                  LLVM-vs2017, LLVM-vs2017_xp, v141, v141_xp, v141_clang_c2]
+                  LLVM-vs2017, LLVM-vs2017_xp, v141, v141_xp, v141_clang_c2, v142]
     clang:
         version: ["3.3", "3.4", "3.5", "3.6", "3.7", "3.8", "3.9", "4.0",
                   "5.0", "6.0", "7.0",
