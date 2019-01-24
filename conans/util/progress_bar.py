9a694679cf3ad2755c6d8e2ad437417296c51397
memsharded <james@conan.io>
2019-01-24 10:30:36 +0100

    iteration over IOError for stream output

--- conans/util/progress_bar.py
+++ conans/util/progress_bar.py
@@ -1,49 +1,10 @@
-
 import os
 from contextlib import contextmanager
 
 from tqdm import tqdm
-from conans.client.rest.uploader_downloader import print_progress,\
-    progress_units, human_readable_progress
 
 TIMEOUT_BEAT_SECONDS = 30
 TIMEOUT_BEAT_CHARACTER = '.'
-
-
-class _FileReaderWithConanProgressBar(object):
-
-    def __init__(self, fileobj, output, desc=None):
-        self._output = output
-        self._fileobj = fileobj
-        self.seek(0, os.SEEK_END)
-        self.totalsize = self.tell()
-        self.seek(0)
-        self._last = 0
-
-    def seekable(self):
-        return self._fileobj.seekable()
-
-    def seek(self, *args, **kwargs):
-        return self._fileobj.seek(*args, **kwargs)
-
-    def tell(self):
-        return self._fileobj.tell()
-
-    def read(self, size):
-        ret = self._fileobj.read(size)
-        diff = self.tell()
-        if diff - self._last > self.totalsize/200.0:
-            self._last = diff
-            if self._output.is_terminal:
-                units = progress_units(diff, self.totalsize)
-                progress = human_readable_progress(diff, self.totalsize)
-                print_progress(self._output, units, progress=progress)
-        return ret
-
-    def pb_close(self):
-        progress = human_readable_progress(self.totalsize, self.totalsize)
-        print_progress(self._output, progress_units(100, 100), progress)
-        self._output.writeln("")
 
 
 class _FileReaderWithProgressBar(object):
@@ -56,17 +17,15 @@ class _FileReaderWithProgressBar(object):
 
     def __init__(self, fileobj, output, desc=None):
         pb_kwargs = self.tqdm_defaults.copy()
-        self._ori_output = output
 
         # If there is no terminal, just print a beat every TIMEOUT_BEAT seconds.
         if not output.is_terminal:
             output = _NoTerminalOutput(output)
             pb_kwargs['mininterval'] = TIMEOUT_BEAT_SECONDS
 
-        self._output = output
         self._fileobj = fileobj
         self.seek(0, os.SEEK_END)
-        self._pb = tqdm(total=self.tell(), desc=desc, file=output, **pb_kwargs)
+        self._tqdm_bar = tqdm(total=self.tell(), desc=desc, file=output, **pb_kwargs)
         self.seek(0)
 
     def seekable(self):
@@ -81,15 +40,11 @@ class _FileReaderWithProgressBar(object):
     def read(self, size):
         prev = self.tell()
         ret = self._fileobj.read(size)
-        self._pb.update(self.tell() - prev)
+        self._tqdm_bar.update(self.tell() - prev)
         return ret
 
     def pb_close(self):
-        self._pb.close()
-
-    def pb_write(self, message):
-        """ Allow to write messages to output without interfering with the progress bar """
-        tqdm.write(message, file=self._ori_output)
+        self._tqdm_bar.close()
 
 
 class _NoTerminalOutput(object):
@@ -106,13 +61,9 @@ class _NoTerminalOutput(object):
 
 @contextmanager
 def open_binary(path, output, **kwargs):
-    output.writeln("Extracting %s" % os.path.basename(path))
     with open(path, mode='rb') as f:
-        if output.is_terminal:
-            file_wrapped = _FileReaderWithConanProgressBar(f, output=output, **kwargs)
-            yield file_wrapped
-            file_wrapped.pb_close()
-            if not output.is_terminal:
-                output.write("\n")
-        else:
-            yield f
+        file_wrapped = _FileReaderWithProgressBar(f, output=output, **kwargs)
+        yield file_wrapped
+        file_wrapped.pb_close()
+        if not output.is_terminal:
+            output.write("\n")
