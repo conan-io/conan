b14c49edee1d1ee4f1c5c8f5e3c675582d4752d4
Luis Martinez de Bartolome Izquierdo <lasote@gmail.com>
2019-02-04 11:13:59 +0100

    Feature/revisions list endpoints (#4293)
    * WIP
    
    * layout of dev
    
    * wip
    
    * Removed versions
    
    * working but not tested
    
    * local
    
    * Local, package failing because of none timestamp in PREV
    
    * WIP
    
    * search tests ok
    
    * Fixing tests and small refactor
    
    * renamed module
    
    * review
    
    * ISO in client, reviewed
    
    * Fixing tests
    
    * Fixing tests
    
    * New tests about metadata being updated
    
    * Review
    
    * Time cleared on export/package
    
    * Fix revisions list
    
    * fixed test
    
    * Unused import
    
    * Review
    
    * Fixed test message assert
    
    * fixed import
    
    * Fixed test

--- conans/server/rest/api_v2.py
+++ conans/server/rest/api_v2.py
@@ -5,6 +5,7 @@ from conans.server.rest.controllers.ping_controller import PingController
 from conans.server.rest.controllers.users_controller import UsersController
 from conans.server.rest.controllers.v2.conan_controller import ConanControllerV2
 from conans.server.rest.controllers.v2.delete_controller import DeleteControllerV2
+from conans.server.rest.controllers.v2.revisions_controller import RevisionsController
 from conans.server.rest.controllers.v2.search_controller import SearchControllerV2
 
 
@@ -22,9 +23,11 @@ class ApiV2(ApiV1):
         # Capabilities in a ping
         PingController("").attach_to(self)
 
-        SearchControllerV2("/conans").attach_to(self)
-        DeleteControllerV2("/conans").attach_to(self)
-        ConanControllerV2("/conans").attach_to(self)
+        conan_endpoint = "/conans"
+        SearchControllerV2(conan_endpoint).attach_to(self)
+        DeleteControllerV2(conan_endpoint).attach_to(self)
+        ConanControllerV2(conan_endpoint).attach_to(self)
+        RevisionsController(conan_endpoint).attach_to(self)
 
         # Install users controller
         UsersController("/users").attach_to(self)
