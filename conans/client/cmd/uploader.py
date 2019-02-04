1379318cb7d1940a93ab8625c7d058aa307f6140
Luis Martinez de Bartolome <lasote@gmail.com>
2019-02-04 09:38:34 +0100

    Upload package id not reference

--- conans/client/cmd/uploader.py
+++ conans/client/cmd/uploader.py
@@ -119,11 +119,10 @@ class CmdUpload(object):
                                      "no packages can be uploaded")
             total = len(packages_ids)
             for index, package_id in enumerate(packages_ids):
-                pref = PackageReference(ref, package_id)
                 p_remote = recipe_remote
-                self._upload_package(pref, metadata, index + 1, total, retry, retry_wait,
-                                     integrity_check, policy, p_remote)
-                upload_recorder.add_package(pref, p_remote.name, p_remote.url)
+                new_pref = self._upload_package(ref, package_id, metadata, index + 1, total, retry, retry_wait,
+                                                integrity_check, policy, p_remote)
+                upload_recorder.add_package(new_pref, p_remote.name, p_remote.url)
 
         # FIXME: I think it makes no sense to specify a remote to "post_upload"
         # FIXME: because the recipe can have one and the package a different one
@@ -146,18 +145,18 @@ class CmdUpload(object):
 
         return ref
 
-    def _upload_package(self, pref, metadata, index=1, total=1, retry=None, retry_wait=None,
+    def _upload_package(self, ref, package_id, metadata, index=1, total=1, retry=None, retry_wait=None,
                         integrity_check=False, policy=None, p_remote=None):
         """Uploads the package identified by package_id"""
 
-        msg = ("Uploading package %d/%d: %s to '%s'" % (index, total, str(pref.id), p_remote.name))
+        msg = ("Uploading package %d/%d: %s to '%s'" % (index, total, str(package_id), p_remote.name))
         t1 = time.time()
         self._user_io.out.info(msg)
 
         # Read the revisions and build a correct package reference for the server
-        package_revision = metadata.packages[pref.id].revision
+        package_revision = metadata.packages[package_id].revision
         # Copy to not modify the original with the revisions
-        new_pref = pref.copy_with_revs(pref.ref.revision, package_revision)
+        new_pref = PackageReference(ref, package_id, package_revision)
 
         assert (new_pref.revision is not None)
         assert (new_pref.ref.revision is not None)
