1ddad76424698ff1dda0f3900e6c7cb608572b43
danimtb <danimanzaneque@gmail.com>
2019-02-05 16:04:48 +0100

    split test

--- conans/test/functional/tools/warning_test.py
+++ conans/test/functional/tools/warning_test.py
@@ -1,6 +1,9 @@
+from nose.plugins.attrib import attr
 import os
+import platform
 import shutil
 import tarfile
+import textwrap
 import unittest
 import zipfile
 
@@ -9,7 +12,114 @@ from conans.test.utils.test_files import temp_folder
 from conans.test.utils.tools import TestClient, StoppableThreadBottle
 
 
-conanfile = """
+@unittest.skipUnless(platform.system() == "Windows", "Only Windows tools")
+class WindowsWarningTest(unittest.TestCase):
+    """
+    Check there are no warnings related to global output and global requester in tools
+    """
+
+    def setUp(self):
+        conanfile = textwrap.dedent("""
+        import os
+        import shutil
+        
+        from conans import ConanFile, tools
+        
+        class TestConan(ConanFile):
+            settings = "os", "build_type", "arch", "compiler"
+        
+            def build(self):
+                tools.vcvars(self.settings)
+                tools.vcvars_dict(self.settings)
+                tools.vcvars(self.settings)
+                tools.build_sln_command(self.settings, "project.sln")
+                tools.msvc_build_command(self.settings, "project.sln")
+                tools.vswhere()
+                tools.vs_comntools("14")
+                tools.vs_installation_path("14")
+                tools.latest_vs_version_installed()
+                tools.run_in_windows_bash(self, "pwd")
+        """)
+        self.client = TestClient()
+        self.client.save({"conanfile.py": conanfile})
+
+    def warning_test(self):
+        self.client.run("install .")
+        self.client.run("build .")
+        self.assertNotIn("Provide the output argument explicitly to function", self.client.out)
+
+    def tearDown(self):
+        shutil.rmtree(self.client.current_folder)
+        self.client.localdb.connection.close()  # FIXME
+        shutil.rmtree(self.client.cache.conan_folder)
+
+
+@attr("slow")
+class DownloadWarningTest(unittest.TestCase):
+    """
+    Check there are no warnings related to global output and global requester in tools
+    """
+
+    def setUp(self):
+        from bottle import static_file
+        self.http_server = StoppableThreadBottle()
+        self.http_server.current_folder = temp_folder()
+
+        with chdir(self.http_server.current_folder):
+            zipfile.ZipFile('file.zip', 'w', zipfile.ZIP_DEFLATED).close()
+            zip_file = os.path.abspath("file.zip")
+
+        @self.http_server.server.get("/file.zip")
+        def get_zip():
+            return static_file(os.path.basename(zip_file), os.path.dirname(zip_file))
+
+        with chdir(self.http_server.current_folder):
+            tarfile.open("file.tar.gz", "w:gz").close()
+            targz_file = os.path.abspath("file.tar.gz")
+
+        @self.http_server.server.get("/file.tar.gz")
+        def get_targz():
+            return static_file(os.path.basename(targz_file), os.path.dirname(targz_file))
+
+        self.http_server.run_server()
+
+        self.client = TestClient()
+        conanfile = textwrap.dedent("""
+        import os
+        import shutil
+
+        from conans import ConanFile, tools
+
+        class TestConan(ConanFile):
+            settings = "os", "build_type", "arch", "compiler"
+
+            def build(self):
+                tools.download("http://localhost:{port}/file.zip", "file.zip")
+                tools.unzip("file.zip")
+                tools.get("http://localhost:{port}/file.tar.gz")  # Uses tools.untargz()
+        """)
+        self.client.save({"conanfile.py": conanfile.format(port=self.http_server.port)})
+
+    def warning_test(self):
+        self.client.run("install .")
+        self.client.run("build .")
+        self.assertNotIn("Provide the output argument explicitly to function", self.client.out)
+
+    def tearDown(self):
+        self.http_server.stop()
+        shutil.rmtree(self.http_server.current_folder)
+        shutil.rmtree(self.client.current_folder)
+        self.client.localdb.connection.close()  # FIXME
+        shutil.rmtree(self.client.cache.conan_folder)
+
+
+class ToolsWarningTest(unittest.TestCase):
+    """
+    Check there are no warnings related to global output and global requester in tools
+    """
+
+    def setUp(self):
+        conanfile = textwrap.dedent("""
 import os
 import shutil
 
@@ -20,21 +130,6 @@ class TestConan(ConanFile):
 
     def build(self):
         tools.cpu_count()
-        if self.settings.os:
-            tools.vcvars(self.settings)
-            tools.vcvars_dict(self.settings)
-            tools.vcvars(self.settings)
-            tools.build_sln_command(self.settings, "project.sln")
-            tools.msvc_build_command(self.settings, "project.sln")
-            tools.vswhere()
-            tools.vs_comntools("14")
-            tools.vs_installation_path("14")
-            tools.latest_vs_version_installed()
-            tools.run_in_windows_bash(self, "pwd")
-        
-        tools.download("http://localhost:{port}/file.zip", "file.zip")
-        tools.unzip("file.zip")
-        tools.get("http://localhost:{port}/file.tar.gz")  # Uses tools.untargz()
         self.output.info("CONAN_COMPRESSION_LEVEL %s" % tools.get_env("CONAN_COMPRESSION_LEVEL"))
         with open("manual.html", "w") as fmanual:
             fmanual.write("this is some content C:/some/PATH/File.txt")
@@ -52,7 +147,7 @@ class TestConan(ConanFile):
         tools.check_with_algorithm_sum("sha1", manual_file, sha1)
         tools.check_sha256(manual_file, sha256)
 
-        with tools.environment_append({{"PATH": None}}):
+        with tools.environment_append({"PATH": None}):
             self.output.info("PATH: %s" % tools.get_env("PATH"))
             
         self.output.info("human size: %s" % tools.human_size(1024))
@@ -89,61 +184,16 @@ class TestConan(ConanFile):
         tools.detected_architecture()
         tools.args_to_string(["one", "two"])
         tools.get_cross_building_settings(self.settings)
-
-        # Not tested:
-        #  tools.ftp_donwload()
-        #  tools.patch()
-        #  tools.PKgConfig()
-        #  tools.Git()
-        #  tools.SVN()
-        #  tools.XCRun()
-"""
-
-
-class ToolsWarningTest(unittest.TestCase):
-    """
-    Check there are no warnings related to global output and global requester in tools
-    """
-
-    def setUp(self):
-        from bottle import static_file
-        self.http_server = StoppableThreadBottle()
-        self.http_server.current_folder = temp_folder()
-
-        with chdir(self.http_server.current_folder):
-            zipfile.ZipFile('file.zip', 'w', zipfile.ZIP_DEFLATED).close()
-            zip_file = os.path.abspath("file.zip")
-
-        @self.http_server.server.get("/file.zip")
-        def get_zip():
-            return static_file(os.path.basename(zip_file), os.path.dirname(zip_file))
-
-        with chdir(self.http_server.current_folder):
-            tarfile.open("file.tar.gz", "w:gz").close()
-            targz_file = os.path.abspath("file.tar.gz")
-
-        @self.http_server.server.get("/file.tar.gz")
-        def get_targz():
-            return static_file(os.path.basename(targz_file), os.path.dirname(targz_file))
-
-        self.http_server.run_server()
-
+        """)
         self.client = TestClient()
-        self.client.save({"conanfile.py": conanfile.format(port=self.http_server.port)})
+        self.client.save({"conanfile.py": conanfile})
 
     def warning_test(self):
         self.client.run("install .")
         self.client.run("build .")
         self.assertNotIn("Provide the output argument explicitly to function", self.client.out)
-        self.assertIn("CONAN_COMPRESSION_LEVEL 9", self.client.out)
-        self.assertIn("this is something C:/file.txt", self.client.out)
-        self.assertIn("PATH: None", self.client.out)
-        self.assertIn("human size: 1.0KB", self.client.out)
-        self.assertIn("cross-building: False", self.client.out)
 
     def tearDown(self):
-        self.http_server.stop()
-        shutil.rmtree(self.http_server.current_folder)
         shutil.rmtree(self.client.current_folder)
         self.client.localdb.connection.close()  # FIXME
         shutil.rmtree(self.client.cache.conan_folder)
