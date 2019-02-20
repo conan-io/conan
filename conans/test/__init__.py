0c878d42ac30f61c79932362c78b42899b680782
Javier G. Sogo <jgsogo@gmail.com>
2019-02-20 11:40:31 +0100

    limit the scope of errors to 'conans' module

--- conans/test/__init__.py
+++ conans/test/__init__.py
@@ -1,7 +1,8 @@
 import os
+import warnings
 
 CONAN_TEST_FOLDER = os.getenv('CONAN_TEST_FOLDER', None)
 os.environ["CONAN_RECIPE_LINTER"] = "False"
 
-import warnings
-warnings.filterwarnings("error")
+# Enable warnings as errors only for `conans` module
+warnings.filterwarnings("error", module="conans.*")
