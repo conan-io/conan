cf2f294e4847a4ea8746ab2ebdf189ce22645e22
memsharded <james@conan.io>
2019-03-04 12:24:16 +0100

    fixed migration

--- conans/client/migrations.py
+++ conans/client/migrations.py
@@ -165,7 +165,8 @@ def _migrate_create_metadata(cache, out):
             base_folder = os.path.normpath(os.path.join(cache.store, ref.dir_repr()))
             # Force using a package cache layout for everything, we want to alter the cache,
             # not the editables
-            layout = PackageCacheLayout(base_folder=base_folder, ref=ref, short_paths=False)
+            layout = PackageCacheLayout(base_folder=base_folder, ref=ref, short_paths=False,
+                                        no_lock=False)
             folder = layout.export()
             try:
                 manifest = FileTreeManifest.load(folder)
