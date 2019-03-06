3216966dcaa745d4356c4ef64f6f3d0ab1b2330b
Daniel <danimanzaneque@gmail.com>
2019-03-06 15:13:07 +0100

    Added with_login parameter to tools.run_in_windows_bash() (#4673)
    * Added test to check with_login parameter
    
    * Added with_login parameter to tools.run_in_windows_bash()

--- conans/model/conan_file.py
+++ conans/model/conan_file.py
@@ -247,13 +247,13 @@ class ConanFile(object):
         """
 
     def run(self, command, output=True, cwd=None, win_bash=False, subsystem=None, msys_mingw=True,
-            ignore_errors=False, run_environment=False):
+            ignore_errors=False, run_environment=False, with_login=True):
         def _run():
             if not win_bash:
                 return self._conan_runner(command, output, os.path.abspath(RUN_LOG_NAME), cwd)
             # FIXME: run in windows bash is not using output
             return tools.run_in_windows_bash(self, bashcmd=command, cwd=cwd, subsystem=subsystem,
-                                             msys_mingw=msys_mingw)
+                                             msys_mingw=msys_mingw, with_login=with_login)
         if run_environment:
             with tools.run_environment(self):
                 if OSInfo().is_macos:
