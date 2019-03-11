6ce51593920710c8e4a4a999dca4a0bbcefa7fee
Luis Martinez de Bartolome <lasote@gmail.com>
2019-03-11 16:56:11 +0100

    Exclude tests from distribution

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(),
+    packages=find_packages(exclude=("conans.test*",)),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
