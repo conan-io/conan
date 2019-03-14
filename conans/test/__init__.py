89cb404375e0b471de0edbf9c0a54622b2caf96c
Javier G. Sogo <jgsogo@gmail.com>
2019-03-14 15:19:19 +0100

    one expression includes de other one

--- conans/test/__init__.py
+++ conans/test/__init__.py
@@ -4,6 +4,5 @@ import warnings
 CONAN_TEST_FOLDER = os.getenv('CONAN_TEST_FOLDER', None)
 os.environ["CONAN_RECIPE_LINTER"] = "False"
 
-# Enable warnings as errors only for `conans` module
-warnings.filterwarnings("error", module="conans.*")
+# Enable warnings as errors only for `conan[s]` module
 warnings.filterwarnings("error", module="conan.*")
