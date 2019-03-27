b66f662520a450c9739cb9ebbbb31eef3bd115db
memsharded <james@conan.io>
2019-03-27 23:13:19 +0100

    fix export-pkg with build-requires

--- conans/client/cmd/export_pkg.py
+++ conans/client/cmd/export_pkg.py
@@ -20,7 +20,8 @@ def export_pkg(cache, graph_manager, hook_manager, recorder, output,
     # to be downloaded from remotes
     deps_graph, _ = graph_manager.load_graph(ref, None, graph_info=graph_info, build_mode=[ref.name],
                                              check_updates=False, update=False,
-                                             remote_name=None, recorder=recorder)
+                                             remote_name=None, recorder=recorder,
+                                             apply_build_requires=False)
     # this is a bit tricky, but works. The root (virtual), has only 1 neighbor,
     # which is the exported pkg
     nodes = deps_graph.root.neighbors()
