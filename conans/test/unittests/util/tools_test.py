c651012d646a2ddb8034407c147b6506296cf146
Javier G. Sogo <jgsogo@gmail.com>
2019-04-02 11:13:11 +0200

    mock the new check_output func

--- conans/test/unittests/util/tools_test.py
+++ conans/test/unittests/util/tools_test.py
@@ -1042,7 +1042,7 @@ ProgramFiles(x86)=C:\Program Files (x86)
             return output_with_newline_and_spaces
 
         with mock.patch('conans.client.tools.win.vcvars_command', new=vcvars_command_mock):
-            with mock.patch('subprocess.check_output', new=subprocess_check_output_mock):
+            with mock.patch('conans.client.tools.oss.check_output', new=subprocess_check_output_mock):
                 vcvars = tools.vcvars_dict(None, only_diff=False, output=self.output)
                 self.assertEqual(vcvars["PROCESSOR_ARCHITECTURE"], "AMD64")
                 self.assertEqual(vcvars["PROCESSOR_IDENTIFIER"], "Intel64 Family 6 Model 158 Stepping 9, GenuineIntel")
