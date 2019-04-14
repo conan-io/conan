5a763187ccfc0950ff69fa000c3b235e4f67dc4d
danimtb <danimanzaneque@gmail.com>
2019-04-14 20:04:05 +0200

    Deploy generator with manifest file and test

--- conans/client/generators/deploy.py
+++ conans/client/generators/deploy.py
@@ -1,20 +1,26 @@
+import calendar
 import os
 import shutil
+import time
 from collections import OrderedDict
 
+from conans.client.file_copier import FileCopier
 from conans.model import Generator
 from conans import ConanFile
+from conans.model.manifest import FileTreeManifest
 from conans.paths import BUILD_INFO_DEPLOY
+from conans.util.files import mkdir, md5sum
 
 
 class DeployGenerator(Generator):
-    self.directory_dict = OrderedDict({"include_paths": "include",
-                                       "src_paths": "src",
-                                       "lib_paths": "lib",
-                                       "res_paths": "res",
-                                       "build_paths": "build"
-                                       })
+    directory_dict = OrderedDict({"include_paths": "include",
+                                  "src_paths": "src",
+                                  "lib_paths": "lib",
+                                  "res_paths": "res",
+                                  "build_paths": "build"
+                                  })
 
+    @staticmethod
     def files(path):
         for file in os.listdir(path):
             if os.path.isfile(os.path.join(path, file)):
@@ -23,7 +29,16 @@ class DeployGenerator(Generator):
     def to_directory_dir_name(self, directory_path_name):
         return self.directory_dict.get(directory_path_name)
 
-    @pproperty
+    def deploy_manifest_content(self, copied_files):
+        date = calendar.timegm(time.gmtime())
+        file_dict = {}
+        for f in copied_files:
+            abs_path = os.path.join(self.output_path, f)
+            file_dict[f] = md5sum(abs_path)
+        manifest = FileTreeManifest(date, file_dict)
+        return repr(manifest)
+
+    @property
     def directory_path_names(self):
         return self.directory_dict.keys()
 
@@ -33,18 +48,30 @@ class DeployGenerator(Generator):
 
     @property
     def content(self):
+        copied_files = []
+
         for dep_name, dep_cpp_info in self.deps_build_info.dependencies:
-            for key in self.directory_path_names:
-                if getattr(dep_cpp_info, key):
-                    for path in getattr(dep_cpp_info, key):
-                        if path == dep_cpp_info.sysroot:  # Copy only first level files
-                            for file in self.files(path):
-                                src = os.path.normpath(os.path.join(path, file))
-                                dst = os.path.join(self.output_path, dep_name, file)
-                                shutil.copyfile(src, dst)
+            for directory_path in self.directory_path_names:
+                if not getattr(dep_cpp_info, directory_path):
+                    continue
+                for path in getattr(dep_cpp_info, directory_path):
+                    if os.path.normpath(path) == os.path.normpath(dep_cpp_info.rootpath):
+                        # Copy only first level files of default value build_paths
+                        for file in self.files(path):
+                            if file in ["conaninfo.txt", "conanmanifest.txt"]:
+                                continue
+                            src = os.path.normpath(os.path.join(path, file))
+                            dst = os.path.join(self.output_path, dep_name,
+                                               self.to_directory_dir_name(directory_path), file)
+                            mkdir(os.path.dirname(dst))
+                            shutil.copyfile(src, dst)
+                            copied_files.append(dst)
+                    else:
                         if os.listdir(path):
                             src = os.path.normpath(path)
-                            dir_name = os.path.basename(path)
-                            dst = os.path.join(self.output_path, dep_name, dir_name)
-                            shutil.copytree(src, dst)
-        return "Files deployed!"
+                            dst = os.path.join(self.output_path, dep_name,
+                                               self.to_directory_dir_name(directory_path))
+                            copier = FileCopier([src], dst)
+                            copier("*")
+                            copied_files.extend([os.path.join(dst, f) for f in copier._copied])
+        return self.deploy_manifest_content(copied_files)
---
+++ conans/test/functional/generators/deploy_test.py
@@ -0,0 +1,155 @@
+import os
+import platform
+import textwrap
+import unittest
+
+from nose.plugins.attrib import attr
+
+from conans import load
+from conans.client.tools import chdir, replace_in_file
+from conans.test.utils.test_files import temp_folder
+from conans.test.utils.tools import TestClient
+
+
+class DeployGeneratorTest(unittest.TestCase):
+    """
+    Deploy generator set of tests with only one package
+    """
+
+    def setUp(self):
+        conanfile = textwrap.dedent("""
+        from conans import ConanFile
+
+        class Test(ConanFile):
+            exports_sources = "*"
+
+            def package(self):
+                self.copy("*.h", dst="include")
+                self.copy("*.lib", dst="my_libs")
+                self.copy("*.config")
+                
+            def package_info(self):
+                self.cpp_info.libdirs = ["my_libs"]
+        """)
+
+        self.client = TestClient()
+        self.client.save({"conanfile.py": conanfile,
+                          "header.h": "",
+                          "file.lib": "",
+                          "file.config": ""})
+        self.client.run("create . name/version@user/channel")
+        self.client.current_folder = temp_folder()
+        self.client.run("install name/version@user/channel -g deploy")
+
+    def deploy_folder_path_test(self):
+        base_path = os.path.join(self.client.current_folder, "name")
+        expected_include_path = os.path.join(base_path, "include")
+        expected_lib_path = os.path.join(base_path, "lib")
+        expected_build_path = os.path.join(base_path, "build")
+        self.assertTrue(os.path.exists(expected_include_path))
+        self.assertTrue(os.path.exists(expected_lib_path))
+        self.assertTrue(os.path.exists(expected_build_path))
+
+    def deploy_manifest_path_test(self):
+        expected_manifest_path = os.path.join(self.client.current_folder, "deploy_manifest.txt")
+        self.assertTrue(os.path.exists(expected_manifest_path))
+
+    def deploy_manifest_content_test(self):
+        base_path = os.path.join(self.client.current_folder, "name")
+        header_path = os.path.join(base_path, "include", "header.h")
+        lib_path = os.path.join(base_path, "lib", "file.lib")
+        config_path = os.path.join(base_path, "build", "file.config")
+        manifest_path = os.path.join(self.client.current_folder, "deploy_manifest.txt")
+        content = load(manifest_path)
+        self.assertIn(header_path, content)
+        self.assertIn(lib_path, content)
+        self.assertIn(config_path, content)
+
+    def no_conan_metadata_files_test(self):
+        metadata_files = ["conaninfo.txt", "conanmanifest.txt"]
+        # Assert not in directory tree
+        for root, _, _ in os.walk(self.client.current_folder):
+            for name in metadata_files:
+                self.assertNotIn(name, os.listdir(root))
+        # Assert not in manifest
+        manifest_path = os.path.join(self.client.current_folder, "deploy_manifest.txt")
+        content = load(manifest_path)
+        for name in metadata_files:
+            self.assertNotIn(name, content)
+
+
+class DeployGeneratorTest(unittest.TestCase):
+    """
+    Deploy generator set of tests with more than one package in the graph
+    """
+
+    def setUp(self):
+        conanfile1 = textwrap.dedent("""
+        from conans import ConanFile
+
+        class Test(ConanFile):
+            exports_sources = "*"
+
+            def package(self):
+                self.copy("*.h", dst="include")
+                self.copy("*.lib", dst="my_libs")
+                self.copy("*.config")
+
+            def package_info(self):
+                self.cpp_info.libdirs = ["my_libs"]
+        """)
+        self.client = TestClient()
+        self.client.save({"conanfile.py": conanfile1,
+                          "header1.h": "",
+                          "file1.lib": "",
+                          "file1.config": ""})
+        self.client.run("create . name1/version@user/channel")
+
+        conanfile2 = textwrap.dedent("""
+        from conans import ConanFile
+
+        class Test(ConanFile):
+            exports_sources = "*"
+            requires = "name1/version@user/channel"
+
+            def package(self):
+                self.copy("*.h", dst="include_files")
+                self.copy("*.lib", dst="my_other_libs")
+                self.copy("*.config", dst="build")
+
+            def package_info(self):
+                self.cpp_info.includedirs = ["include_files"]
+                self.cpp_info.libdirs = ["my_other_libs"]
+                self.cpp_info.builddirs = ["build"]
+        """)
+        self.client.save({"conanfile.py": conanfile2,
+                          "header2.h": "",
+                          "file2.lib": "",
+                          "file2.config": ""}, clean_first=True)
+        self.client.run("create . name2/version@user/channel")
+        self.client.current_folder = temp_folder()
+        self.client.run("install name2/version@user/channel -g deploy")
+
+    def get_expected_paths(self):
+        base1_path = os.path.join(self.client.current_folder, "name1")
+        header1_path = os.path.join(base1_path, "include", "header1.h")
+        lib1_path = os.path.join(base1_path, "lib", "file1.lib")
+        config1_path = os.path.join(base1_path, "build", "file1.config")
+
+        base2_path = os.path.join(self.client.current_folder, "name2")
+        header2_path = os.path.join(base2_path, "include", "header2.h")
+        lib2_path = os.path.join(base2_path, "lib", "file2.lib")
+        config2_path = os.path.join(base2_path, "build", "file2.config")
+
+        return [header1_path, lib1_path, config1_path, header2_path, lib2_path, config2_path]
+
+    def deploy_manifest_content_test(self):
+        manifest_path = os.path.join(self.client.current_folder, "deploy_manifest.txt")
+        content = load(manifest_path)
+        print(content)
+        for path in self.get_expected_paths():
+            self.assertIn(path, content)
+
+    def file_paths_test(self):
+        for path in self.get_expected_paths():
+            self.assertTrue(os.path.exists(path))
