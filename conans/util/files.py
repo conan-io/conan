fb20b3431a2de97eb09413af4e14d03d8761fc56
Luis Martinez de Bartolome <lasote@gmail.com>
2019-04-22 11:38:15 +0200

    Try except better

--- conans/util/files.py
+++ conans/util/files.py
@@ -73,9 +73,10 @@ def touch(fname, times=None):
 def touch_folder(folder):
     for dirname, _, filenames in walk(folder):
         for fname in filenames:
-            path = os.path.join(dirname, fname)
-            if not os.path.islink(path):
-                os.utime(path, None)
+            try:
+                os.utime(os.path.join(dirname, fname), None)
+            except Exception:
+                pass
 
 
 def normalize(text):
