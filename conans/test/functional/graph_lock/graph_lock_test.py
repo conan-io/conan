f176d3c72440395f66b12e2fb934e83820d69e69
memsharded <james@conan.io>
2019-04-24 13:57:37 +0200

    tests

--- conans/test/functional/graph_lock/graph_lock_test.py
+++ conans/test/functional/graph_lock/graph_lock_test.py
@@ -1,23 +1,24 @@
 import os
+import textwrap
 import unittest
-
 
 from conans.test.utils.tools import TestClient
 from conans.util.files import load
 from conans.test.utils.conanfile import TestConanFile
-import textwrap
 
 
-class CIGraphLockTest(unittest.TestCase):
+class GraphLockErrorsTest(unittest.TestCase):
     def missing_lock_error_test(self):
         client = TestClient()
         client.save({"conanfile.py": str(TestConanFile("PkgA", "0.1"))})
         client.run("install . --lock", assert_error=True)
         self.assertIn("ERROR: Failed to load graphinfo file in install-folder", client.out)
 
-    def export_lock_test(self):
-        # locking a version range at export
+
+class GraphLockVersionRangeTest(unittest.TestCase):
+    def setUp(self):
         client = TestClient()
+        self.client = client
         client.save({"conanfile.py": str(TestConanFile("PkgA", "0.1"))})
         client.run("create . PkgA/0.1@user/channel")
 
@@ -33,35 +34,15 @@ class CIGraphLockTest(unittest.TestCase):
                       lock_file)
         # The consumer node is None => null in json
         self.assertNotIn("PkgB", lock_file)
-        client.run("export . user/testing --lock")
-        lock_file = load(os.path.join(client.current_folder, "graph_info.json"))
-        self.assertIn("PkgB/0.1@user/testing#180919b324d7823f2683af9381d11431:Unkonwn Package ID",
-                      lock_file)
-
-    def version_ranges_lock_test(self):
-        # locking a version range
-        client = TestClient()
-        client.save({"conanfile.py": str(TestConanFile("PkgA", "0.1"))})
-        client.run("create . PkgA/0.1@user/channel")
-
-        # Use a consumer with a version range
-        consumer = str(TestConanFile("PkgB", "0.1", requires=["PkgA/[>=0.1]@user/channel"]))
-        client.save({"conanfile.py": consumer})
-        client.run("install .")
-        self.assertIn("PkgA/0.1@user/channel:5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9",
-                      client.out)
-
-        lock_file = load(os.path.join(client.current_folder, "graph_info.json"))
-        self.assertIn("PkgA/0.1@user/channel#b55538d56afb03f068a054f11310ce5a:"
-                      "5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9#6d56ca1040e37a13b75bc286f3e1a5ad",
-                      lock_file)
 
         # If we create a new PkgA version
         client.save({"conanfile.py": str(TestConanFile("PkgA", "0.2"))})
         client.run("create . PkgA/0.2@user/channel")
-
-        # Normal install will use it (use install-folder to not change graph-info)
         client.save({"conanfile.py": consumer})
+
+    def install_info_lock_test(self):
+        # Normal install will use it (use install-folder to not change graph-info)
+        client = self.client
         client.run("install . -if=tmp")  # Output graph_info to temporary
         self.assertIn("PkgA/0.2@user/channel", client.out)
         self.assertNotIn("PkgA/0.1@user/channel", client.out)
@@ -80,16 +61,28 @@ class CIGraphLockTest(unittest.TestCase):
         self.assertIn("PkgA/0.1@user/channel", client.out)
         self.assertNotIn("PkgA/0.2/user/channel", client.out)
 
+    def export_lock_test(self):
+        # locking a version range at export
+        self.client.run("export . user/testing --lock")
+        lock_file = load(os.path.join(self.client.current_folder, "graph_info.json"))
+        self.assertIn("PkgB/0.1@user/testing#180919b324d7823f2683af9381d11431:Unkonwn Package ID",
+                      lock_file)
+
+    def create_lock_test(self):
         # Create is also possible
-        # Updating the root in the graph-info file
+        client = self.client
         client.run("create . PkgB/0.1@user/channel --lock")
+        print client.out
         self.assertIn("PkgA/0.1@user/channel", client.out)
         self.assertNotIn("PkgA/0.2/user/channel", client.out)
 
         lock_file = load(os.path.join(client.current_folder, "graph_info.json"))
-        self.assertIn("PkgB/0.1@user/channel#180919b324d7823f2683af9381d11431:Unkonwn Package ID",
+        self.assertIn("PkgB/0.1@user/channel#180919b324d7823f2683af9381d11431:"
+                      "5bf1ba84b5ec8663764a406f08a7f9ae5d3d5fb5",
                       lock_file)
 
+
+class GraphLockBuildRequireVersionRangeTest(unittest.TestCase):
     def build_require_lock_export_test(self):
         # locking a version range
         client = TestClient()
