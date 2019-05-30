1a75dba5ff3547d475bdfdbde3929df7841c6cf1
danimtb <danimanzaneque@gmail.com>
2019-05-30 13:50:04 +0200

    Fixed imports

--- conans/client/tools/__init__.py
+++ conans/client/tools/__init__.py
@@ -1,9 +1,8 @@
-
-# noinspection PyUnresolvedReferences
 # noinspection PyUnresolvedReferences
 from .android import *
 # noinspection PyUnresolvedReferences
 from .apple import *
+# noinspection PyUnresolvedReferences
 from .env import *
 # noinspection PyUnresolvedReferences
 from .files import *
@@ -19,4 +18,3 @@ from .scm import *
 from .system_pm import *
 # noinspection PyUnresolvedReferences
 from .win import *
-
