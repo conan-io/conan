conan-io/conan

commit 083e389e3e82a99eeb29cc5f948d657ec4fa7484

Copy symlinks (folders) in 'merge_directories' func (#5237)
* POC of managing better the symlinked folders

* indent the conanfile

* use package_layout

* check that the symlinks are pointing to the folder they have to

* order imports

* as the target exists, no need to declare type. Also, it didn't work for py2

conans/test/integration/symlinks_test.py
@@ -1,9 +1,10 @@
 import os
 import platform
+import textwrap
 import unittest
 
 from conans.model.ref import ConanFileReference, PackageReference
-from conans.test.utils.tools import NO_SETTINGS_PACKAGE_ID, TestClient, TestServer
+from conans.test.utils.tools import NO_SETTINGS_PACKAGE_ID, TestClient, TestServer, TurboTestClient
 from conans.util.files import load, mkdir, save
 
 conanfile = """
@@ -233,3 +234,49 @@ class ConanSymlink(ConanFile):
         client.run("create . danimtb/testing")
         self.assertTrue(os.path.exists(package_file))
         self.assertTrue(os.path.exists(package_other_dir))
+
+    def test_create_keep_folder_symlink(self):
+        conanfile = textwrap.dedent("""
+            import os
+            from conans import ConanFile
+            class ConanSymlink(ConanFile):
+                name = "ConanSymlink"
+                version = "3.0.0"
+                exports_sources = ["*"]
+                def build(self):
+                    debug_path = os.path.join(self.build_folder, "debug")
+                    assert os.path.exists(debug_path), "Symlinked folder not created!"
+            """)
+
+        client = TurboTestClient()
+        client.save({"conanfile.py": conanfile})
+        client.save({"release/file.cpp": conanfile})
+        real_dir_path = os.path.join(client.current_folder, "release")
+        symlink_path = os.path.join(client.current_folder, "debug")
+        os.symlink(real_dir_path, symlink_path)
+
+        # Verify that the symlink is created correctly
+        self.assertEqual(os.path.realpath(symlink_path), real_dir_path)
+
+        ref = ConanFileReference.loads("ConanSymlink/3.0.0@user/channel")
+        package_layout = client.cache.package_layout(ref)
+        # Export the recipe and check that the symlink is still there
+        client.export(ref, conanfile=conanfile)
+        sf = package_layout.export_sources()
+        sf_symlink = os.path.join(sf, "debug")
+        self.assertTrue(os.path.islink(sf_symlink))
+        self.assertEqual(os.path.realpath(sf_symlink), os.path.join(sf, "release"))
+
+        # Now do the create
+        pref = client.create(ref, conanfile=conanfile)
+        # Assert that the symlink is preserved when copy to source from exports_sources
+        sf = package_layout.source()
+        sf_symlink = os.path.join(sf, "debug")
+        self.assertTrue(os.path.islink(sf_symlink))
+        self.assertEqual(os.path.realpath(sf_symlink), os.path.join(sf, "release"))
+
+        # Assert that the symlink is preserved when copy to build folder
+        bf = client.cache.package_layout(pref.ref).build(pref)
+        bf_symlink = os.path.join(bf, "debug")
+        self.assertTrue(os.path.islink(bf_symlink))
+        self.assertEqual(os.path.realpath(bf_symlink), os.path.join(bf, "release"))

conans/client/file_copier.py
@@ -38,7 +38,7 @@ class FileCopier(object):
         will not be used in the relative names while copying
         param source_folders: list of folders to copy things from, typically the
                                   store build folder
-        param root_destination_folder: The base folder to copy things to, typicall the
+        param root_destination_folder: The base folder to copy things to, typically the
                                        store package folder
         """
         assert isinstance(source_folders, list), "source folders must be a list"
@@ -91,11 +91,12 @@ class FileCopier(object):
         files_to_copy, link_folders = self._filter_files(src, pattern, symlinks, excludes,
                                                          ignore_case, excluded_folders)
         copied_files = self._copy_files(files_to_copy, src, dst, keep_path, symlinks)
-        self._link_folders(src, dst, link_folders)
+        self.link_folders(src, dst, link_folders)
         self._copied.extend(files_to_copy)
         return copied_files
 
-    def _filter_files(self, src, pattern, links, excludes, ignore_case, excluded_folders):
+    @staticmethod
+    def _filter_files(src, pattern, links, excludes, ignore_case, excluded_folders):
 
         """ return a list of the files matching the patterns
         The list will be relative path names wrt to the root src folder
@@ -155,7 +156,7 @@ class FileCopier(object):
         return files_to_copy, linked_folders
 
     @staticmethod
-    def _link_folders(src, dst, linked_folders):
+    def link_folders(src, dst, linked_folders):
         created_links = []
         for linked_folder in linked_folders:
             src_link = os.path.join(src, linked_folder)

conans/client/source.py
@@ -5,8 +5,9 @@ import six
 
 from conans.client import tools
 from conans.client.cmd.export import export_recipe, export_source
-from conans.errors import (ConanException, ConanExceptionInUserConanfileMethod,
-                           conanfile_exception_formatter)
+from conans.client.file_copier import FileCopier
+from conans.errors import ConanException, ConanExceptionInUserConanfileMethod, \
+    conanfile_exception_formatter
 from conans.model.conan_file import get_env_context_manager
 from conans.model.scm import SCM, get_scm_data
 from conans.paths import CONANFILE, CONAN_MANIFEST, EXPORT_SOURCES_TGZ_NAME, EXPORT_TGZ_NAME
@@ -53,9 +54,16 @@ def merge_directories(src, dst, excluded=None, symlinks=True):
             return True
         return False
 
+    linked_folders = []
     for src_dir, dirs, files in walk(src, followlinks=True):
+
         if is_excluded(src_dir):
             dirs[:] = []
+            continue
+
+        if os.path.islink(src_dir):
+            rel_link = os.path.relpath(src_dir, src)
+            linked_folders.append(rel_link)
             continue
 
         # Overwriting the dirs will prevents walk to get into them
@@ -72,6 +80,8 @@ def merge_directories(src, dst, excluded=None, symlinks=True):
                 os.symlink(linkto, dst_file)
             else:
                 shutil.copy2(src_file, dst_file)
+
+    FileCopier.link_folders(src, dst, linked_folders)
 
 
 def config_source_local(src_folder, conanfile, conanfile_path, hook_manager):

commit 258b24ffe88b562e34f590bc44104bf030e8097b

add single file (possibly regression in #5237

conans/test/integration/symlinks_test.py
@@ -295,9 +295,10 @@ class SymlinkExportSources(unittest.TestCase):
     def test_create_source(self):
         # Reproduces issue: https://github.com/conan-io/conan/issues/5329
         t = TestClient()
-        rel_path_content = os.path.join('src', 'framework', 'Versions', 'v1', 'headers', 'content')
+        relpath_v1 = os.path.join('src', 'framework', 'Versions', 'v1')
         t.save({'conanfile.py': self.conanfile,
-                rel_path_content: "whatever"})
+                os.path.join(relpath_v1, 'headers', 'content'): "whatever",
+                os.path.join(relpath_v1, 'file'): "content"})
 
         # Add two levels of symlinks
         os.symlink('v1',
@@ -306,10 +307,15 @@ class SymlinkExportSources(unittest.TestCase):
         os.symlink('Versions/Current/headers',
                    os.path.join(t.current_folder, 'src', 'framework', 'headers'),
                    target_is_directory=True)
+        os.symlink('Versions/Current/file',
+                   os.path.join(t.current_folder, 'src', 'framework', 'file'))
 
         # Check that things are in place
         content = os.path.join(t.current_folder, 'src', 'framework', 'headers', 'content')
         self.assertTrue(os.path.exists(content))
-        self.assertEqual(os.path.realpath(content), os.path.join(t.current_folder, rel_path_content))
+        self.assertEqual(os.path.realpath(content),
+                         os.path.join(t.current_folder, relpath_v1, 'headers', 'content'))
 
         t.run("create . user/channel")
+
+        # TODO: Check copied files