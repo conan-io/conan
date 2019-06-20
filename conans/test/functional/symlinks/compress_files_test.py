d507b3d7bbf875aa4c95c5e57916f188e77dd2e8
Javier G. Sogo <jgsogo@gmail.com>
2019-06-20 18:58:08 +0200

    tests for compress files

---
+++ conans/test/functional/symlinks/compress_files_test.py
@@ -0,0 +1,60 @@
+# coding=utf-8
+
+import os
+import platform
+import unittest
+
+import six
+
+from conans.client.cmd.uploader import _compress_files
+from conans.client.tools import environment_append
+from conans.client.tools.files import chdir
+from conans.model.manifest import gather_files
+from conans.test.utils.test_files import temp_folder
+from conans.test.utils.tools import TestBufferConanOutput
+from conans.util.files import save, save_files
+
+
+@unittest.skipUnless(platform.system() != "Windows", "Symlinks not handled for Windows")
+class CompressFilesTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.tmp_folder = temp_folder()
+        with chdir(self.tmp_folder):
+            # Create some files
+            save("linked_file", "")
+            os.makedirs("linked_folder")
+            ori_files_dir = os.path.join(self.tmp_folder, "ori")
+            save_files(ori_files_dir, {"file1": "",
+                                       "folder/file2": ""})
+            # And symlinks pointing outside
+            self.out_folder = os.path.join(ori_files_dir, "out_folder")
+            os.symlink(os.path.join(self.tmp_folder, "linked_folder"), self.out_folder)
+            self.out_file = os.path.join(ori_files_dir, "out_file")
+            os.symlink(os.path.join(self.tmp_folder, "linked_file"), self.out_file)
+
+        with environment_append({"CONAN_SKIP_BROKEN_SYMLINKS_CHECK": "True"}):
+            self.gathered_files = gather_files(os.path.join(self.tmp_folder, "ori"), output=None)
+
+    def test_warn_outside(self):
+        output = TestBufferConanOutput()
+        _compress_files(self.gathered_files, "compress.tar.gz", self.tmp_folder, output=output)
+        # Warn about the folder
+        self.assertIn("WARN: Symbolic link '{}' points to".format(self.out_folder), output)
+        # Want about the file
+        self.assertIn("WARN: Symbolic link '{}' points to".format(self.out_file), output)
+
+    def test_broken_file(self):
+        os.unlink(os.path.join(self.tmp_folder, 'linked_file'))
+
+        output = TestBufferConanOutput()
+        with six.assertRaisesRegex(self, Exception, "No such file or directory:"
+                                                    " '{}'".format(self.out_file)):
+            _compress_files(self.gathered_files, "compress.tar.gz", self.tmp_folder, output=output)
+
+    def test_broken_directory(self):
+        os.rmdir(os.path.join(self.tmp_folder, 'linked_folder'))
+
+        output = TestBufferConanOutput()
+        _compress_files(self.gathered_files, "compress.tar.gz", self.tmp_folder, output=output)
+        # TODO: Should raise exception?
