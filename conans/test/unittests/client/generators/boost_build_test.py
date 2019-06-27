d2a169a41b1d36e08124f91a673aa203cf122cca
danimtb <danimanzaneque@gmail.com>
2019-06-27 18:00:50 +0200

    fix boost build test

--- conans/test/unittests/client/generators/boost_build_test.py
+++ conans/test/unittests/client/generators/boost_build_test.py
@@ -18,46 +18,53 @@ class BoostJamGeneratorTest(unittest.TestCase):
 
         ref = ConanFileReference.loads("MyPkg/0.1@lasote/stables")
         cpp_info = CppInfo("dummy_root_folder1")
+        cpp_info._filter_empty = False  # For testing purposes
         cpp_info.defines = ["MYDEFINE1"]
         cpp_info.cflags.append("-Flag1=23")
-        cpp_info.version = "1.3"
-        cpp_info.description = "My cool description"
+        cpp_info._version = "1.3"
+        cpp_info._description = "My cool description"
         cpp_info.libs = ["MyLib1"]
-
         conanfile.deps_cpp_info.update(cpp_info, ref.name)
+
         ref = ConanFileReference.loads("MyPkg2/0.1@lasote/stables")
         cpp_info = CppInfo("dummy_root_folder2")
+        cpp_info._filter_empty = False  # For testing purposes
         cpp_info.libs = ["MyLib2"]
         cpp_info.defines = ["MYDEFINE2"]
-        cpp_info.version = "2.3"
+        cpp_info._version = "2.3"
         cpp_info.exelinkflags = ["-exelinkflag"]
         cpp_info.sharedlinkflags = ["-sharedlinkflag"]
         cpp_info.cxxflags = ["-cxxflag"]
-        cpp_info.public_deps = ["MyPkg"]
-        cpp_info.lib_paths.extend(["Path\\with\\slashes", "regular/path/to/dir"])
-        cpp_info.include_paths.extend(["other\\Path\\with\\slashes", "other/regular/path/to/dir"])
+        cpp_info._public_deps = ["MyPkg"]
+        cpp_info.libdirs.extend(["Path\\with\\slashes", "regular/path/to/dir"])
+        cpp_info.includedirs.extend(["other\\Path\\with\\slashes", "other/regular/path/to/dir"])
         conanfile.deps_cpp_info.update(cpp_info, ref.name)
+
         generator = BoostBuildGenerator(conanfile)
 
-        self.assertEqual(generator.content, """lib MyLib1 :
+        self.assertEqual("""lib MyLib1 :
 	: # requirements
 	<name>MyLib1
+	<search>dummy_root_folder1/lib
 	: # default-build
 	: # usage-requirements
 	<define>MYDEFINE1
+	<include>dummy_root_folder1/include
 	<cflags>-Flag1=23
 	;
 
 lib MyLib2 :
 	: # requirements
 	<name>MyLib2
-	<search>Path/with/slashes
-	<search>regular/path/to/dir
+	<search>dummy_root_folder2/lib
+	<search>dummy_root_folder2/Path/with/slashes
+	<search>dummy_root_folder2/regular/path/to/dir
 	: # default-build
 	: # usage-requirements
 	<define>MYDEFINE2
-	<include>other/Path/with/slashes
-	<include>other/regular/path/to/dir
+	<include>dummy_root_folder2/include
+	<include>dummy_root_folder2/other/Path/with/slashes
+	<include>dummy_root_folder2/other/regular/path/to/dir
 	<cxxflags>-cxxflag
 	<ldflags>-sharedlinkflag
 	;
@@ -66,4 +73,4 @@ alias conan-deps :
 	MyLib1
 	MyLib2
 ;
-""")
+""", generator.content)
