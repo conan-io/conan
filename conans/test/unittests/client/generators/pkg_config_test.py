06f60b22f1976b0bbe497fb1be54e3d9967bf253
danimtb <danimanzaneque@gmail.com>
2019-06-27 19:49:39 +0200

    fix pkg_config test

--- conans/test/unittests/client/generators/pkg_config_test.py
+++ conans/test/unittests/client/generators/pkg_config_test.py
@@ -18,18 +18,18 @@ class PkgGeneratorTest(unittest.TestCase):
         cpp_info = CppInfo("dummy_root_folder1")
         cpp_info.defines = ["MYDEFINE1"]
         cpp_info.cflags.append("-Flag1=23")
-        cpp_info._version = "1.3"
-        cpp_info._description = "My cool description"
+        cpp_info.version = "1.3"
+        cpp_info.description = "My cool description"
 
         conanfile.deps_cpp_info.update(cpp_info, ref.name)
         ref = ConanFileReference.loads("MyPkg2/0.1@lasote/stables")
         cpp_info = CppInfo("dummy_root_folder2")
         cpp_info.defines = ["MYDEFINE2"]
-        cpp_info._version = "2.3"
+        cpp_info.version = "2.3"
         cpp_info.exelinkflags = ["-exelinkflag"]
         cpp_info.sharedlinkflags = ["-sharedlinkflag"]
         cpp_info.cxxflags = ["-cxxflag"]
-        cpp_info._public_deps = ["MyPkg"]
+        cpp_info.public_deps = ["MyPkg"]
         conanfile.deps_cpp_info.update(cpp_info, ref.name)
         generator = PkgConfigGenerator(conanfile)
         files = generator.content
