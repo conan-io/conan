fe4bcaf2133fe057d0696867c05c464e2b84014e
danimtb <danimanzaneque@gmail.com>
2019-06-28 10:48:09 +0200

    fix scons partially

--- conans/test/unittests/client/generators/scons_test.py
+++ conans/test/unittests/client/generators/scons_test.py
@@ -16,18 +16,20 @@ class SConsGeneratorTest(unittest.TestCase):
         conanfile.initialize(Settings({}), EnvValues())
         ref = ConanFileReference.loads("MyPkg/0.1@lasote/stables")
         cpp_info = CppInfo("")
+        cpp_info._filter_empty = False  # For testing purposes
         cpp_info.defines = ["MYDEFINE1"]
         cpp_info.version = "0.1"
         conanfile.deps_cpp_info.update(cpp_info, ref.name)
         ref = ConanFileReference.loads("MyPkg2/3.2.3@lasote/stables")
         cpp_info = CppInfo("")
+        cpp_info._filter_empty = False  # For testing purposes
         cpp_info.defines = ["MYDEFINE2"]
         cpp_info.version = "3.2.3"
         conanfile.deps_cpp_info.update(cpp_info, ref.name)
         generator = SConsGenerator(conanfile)
         content = generator.content
         scons_lines = content.splitlines()
-        self.assertIn("        \"CPPDEFINES\"  : [\'MYDEFINE2\', \'MYDEFINE1\'],", scons_lines)
+        self.assertIn("        \"CPPDEFINES\"  : [\'MYDEFINE1\', \'MYDEFINE2\'],", scons_lines)
         self.assertIn("        \"CPPDEFINES\"  : [\'MYDEFINE1\'],", scons_lines)
         self.assertIn("        \"CPPDEFINES\"  : [\'MYDEFINE2\'],", scons_lines)
         self.assertIn('    "conan_version" : "None",', scons_lines)
