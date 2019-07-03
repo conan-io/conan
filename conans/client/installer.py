d29616a69ac891b0184860a68f712af5a49cc6c2
Uilian Ries <uilianries@gmail.com>
2019-07-03 17:18:07 -0300

    #3835 set_dirt_context_manager
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/client/installer.py
+++ conans/client/installer.py
@@ -24,7 +24,8 @@ from conans.model.ref import PackageReference
 from conans.model.user_info import UserInfo
 from conans.paths import BUILD_INFO, CONANINFO, RUN_LOG_NAME
 from conans.util.env_reader import get_env
-from conans.util.files import (clean_dirty, is_dirty, make_read_only, mkdir, rmdir, save, set_dirty)
+from conans.util.files import (clean_dirty, is_dirty, make_read_only, mkdir, rmdir, save, set_dirty,
+                               set_dirty_context_manager)
 from conans.util.log import logger
 from conans.util.tracer import log_package_built, log_package_got_from_local_cache
 
@@ -386,9 +387,8 @@ class BinaryInstaller(object):
                 processed_package_references.add(pref)
                 if node.binary == BINARY_BUILD:
                     assert node.prev is None, "PREV for %s to be built should be None" % str(pref)
-                    set_dirty(package_folder)
-                    pref = self._build_package(node, output, keep_build, remotes)
-                    clean_dirty(package_folder)
+                    with set_dirty_context_manager(package_folder):
+                        pref = self._build_package(node, output, keep_build, remotes)
                     assert node.prev, "Node PREV shouldn't be empty"
                     assert node.pref.revision, "Node PREF revision shouldn't be empty"
                     assert node.prev is not None, "PREV for %s to be built is None" % str(pref)
@@ -397,15 +397,15 @@ class BinaryInstaller(object):
                     assert node.prev, "PREV for %s is None" % str(pref)
                     # not really concurrently, but a different node with same pref
                     if not self._node_concurrently_installed(node, package_folder):
-                        set_dirty(package_folder)
-                        assert pref.revision is not None, "Installer should receive #PREV always"
-                        self._remote_manager.get_package(pref, package_folder,
-                                                         node.binary_remote, output,
-                                                         self._recorder)
-                        output.info("Downloaded package revision %s" % pref.revision)
-                        with self._cache.package_layout(pref.ref).update_metadata() as metadata:
-                            metadata.packages[pref.id].remote = node.binary_remote.name
-                        clean_dirty(package_folder)
+                        with set_dirty_context_manager(package_folder):
+                            assert pref.revision is not None, \
+                                "Installer should receive #PREV always"
+                            self._remote_manager.get_package(pref, package_folder,
+                                                             node.binary_remote, output,
+                                                             self._recorder)
+                            output.info("Downloaded package revision %s" % pref.revision)
+                            with self._cache.package_layout(pref.ref).update_metadata() as metadata:
+                                metadata.packages[pref.id].remote = node.binary_remote.name
                     else:
                         output.success('Download skipped. Probable concurrent download')
                         log_package_got_from_local_cache(pref)
