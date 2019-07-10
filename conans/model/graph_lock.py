a2a77afba0cc2893d9c9e0553fc54ae353e9f192
Luis Martinez de Bartolome Izquierdo <lasote@gmail.com>
2019-07-10 17:56:32 +0200

    Feature/fix merge graph (#5467)
    * WIP
    
    * Fixed red compatible
    
    * Compatible of RREV too
    
    * compatible updates of the graph
    
    * Fix comparisson of ref

--- conans/model/graph_lock.py
+++ conans/model/graph_lock.py
@@ -153,7 +153,8 @@ class GraphLock(object):
             if node.modified:
                 old_node = self._nodes[id_]
                 if old_node.modified:
-                    raise ConanException("Lockfile had already modified %s" % str(node.pref))
+                    if not old_node.pref.is_compatible_with(node.pref):
+                        raise ConanException("Lockfile had already modified %s" % str(node.pref))
                 self._nodes[id_] = node
 
     def clean_modified(self):
@@ -205,11 +206,14 @@ class GraphLock(object):
                 if node.recipe == RECIPE_CONSUMER:
                     continue  # If the consumer node is not found, could be a test_package
                 raise
-            if lock_node.pref and lock_node.pref.full_repr() != node.pref.full_repr():
-                if node.binary == BINARY_BUILD or node.id in affected:
+            if lock_node.pref:
+                # If the update is compatible (resolved complete PREV) or if the node has
+                # been build, then update the graph
+                if lock_node.pref.is_compatible_with(node.pref) or \
+                        node.binary == BINARY_BUILD or node.id in affected:
                     lock_node.pref = node.pref
                 else:
-                    raise ConanException("Mistmatch between lock and graph:\nLock:  %s\nGraph: %s"
+                    raise ConanException("Mismatch between lock and graph:\nLock:  %s\nGraph: %s"
                                          % (lock_node.pref.full_repr(), node.pref.full_repr()))
 
     def lock_node(self, node, requires):
