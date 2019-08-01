2edb2d9ac63ad8aa1cbef17a82d14d50d514dbb6
Birger Brekke <birger@imerso.com>
2019-08-01 10:16:14 +0200

    Use pre aggregated PYTHONPATH value for deps
    PYTHONPATH from conanfile.env is also added.

--- conans/client/generators/virtualenv_python.py
+++ conans/client/generators/virtualenv_python.py
@@ -1,16 +1,13 @@
 from conans.client.generators.virtualrunenv import VirtualRunEnvGenerator
 
 
-def _python_paths(deps_env_info):
-    result = set()
-    for dep in deps_env_info.deps:
-        result.update(deps_env_info[dep].vars.get("PYTHONPATH", set()))
-
-    return list(result)
-
-
 class VirtualEnvPythonGenerator(VirtualRunEnvGenerator):
     def __init__(self, conanfile):
         super(VirtualEnvPythonGenerator, self).__init__(conanfile)
         self.venv_name = "conanenvpython"
-        self.env["PYTHONPATH"] = _python_paths(self.conanfile.deps_env_info)
+        self.env["PYTHONPATH"] = list(
+            set(
+                conanfile.env.get("PYTHONPATH", [])
+                + self.deps_env_info.vars.get("PYTHONPATH", [])
+            )
+        )
