550e898fde5e10a51aaee61ac716c76873d8b666
czoido <mrgalleta@gmail.com>
2019-08-13 17:06:28 +0200

    list remotes with disabled option

--- conans/client/conan_command_output.py
+++ conans/client/conan_command_output.py
@@ -30,9 +30,21 @@ class CommandOutputer(object):
     def remote_list(self, remotes, raw):
         for r in remotes:
             if raw:
-                self._output.info("%s %s %s" % (r.name, r.url, r.verify_ssl))
+                if r.disabled:
+                    self._output.info(
+                        "%s %s %s %s" %
+                        (r.name, r.url, r.verify_ssl, r.disabled))
+                else:
+                    self._output.info("%s %s %s" %
+                                      (r.name, r.url, r.verify_ssl))
             else:
-                self._output.info("%s: %s [Verify SSL: %s]" % (r.name, r.url, r.verify_ssl))
+                if r.disabled:
+                    self._output.info(
+                        "%s: %s [Verify SSL: %s, Disabled: %s]" %
+                        (r.name, r.url, r.verify_ssl, r.disabled))
+                else:
+                    self._output.info("%s: %s [Verify SSL: %s]" %
+                                      (r.name, r.url, r.verify_ssl))
 
     def remote_ref_list(self, refs):
         for reference, remote_name in refs.items():
