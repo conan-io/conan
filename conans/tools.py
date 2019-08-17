3367b2a8cef3a7e7e43165375efcd4a492cbb57b
Changjiang Yang <yangcha@gmail.com>
2019-08-17 10:10:52 -0400

    rename a file or folder with retrying.

--- conans/tools.py
+++ conans/tools.py
@@ -94,6 +94,7 @@ collect_libs = tools_files.collect_libs
 which = tools_files.which
 unix2dos = tools_files.unix2dos
 dos2unix = tools_files.dos2unix
+rename = tools_files.rename
 
 
 def unzip(*args, **kwargs):
