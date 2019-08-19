7a8d4e2cdd760a2a16857bb75dada163c1a03716
czoido <mrgalleta@gmail.com>
2019-08-19 17:35:33 +0200

    test skip disabled remotes for search

--- conans/test/functional/command/search_test.py
+++ conans/test/functional/command/search_test.py
@@ -252,6 +252,15 @@ class SearchTest(unittest.TestCase):
         self.client.run("search * -r search_able", assert_error=True)
         self.assertIn("ERROR: Remote 'search_able' is disabled", self.client.out)
 
+    def search_skip_disabled_remote_test(self):
+        os.rmdir(self.servers["local"].server_store.store)
+        self._copy_to_server(self.client.cache, self.servers["local"].server_store)
+        os.rmdir(self.servers["search_able"].server_store.store)
+        self._copy_to_server(self.client.cache, self.servers["search_able"].server_store)
+        self.client.run("remote disable local")
+        self.client.run("search Hello* -r all")
+        self.assertNotIn("Remote 'local':", self.client.out)
+
     def recipe_search_all_test(self):
         os.rmdir(self.servers["local"].server_store.store)
         self._copy_to_server(self.client.cache, self.servers["local"].server_store)
