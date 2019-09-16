ecac1025a0bb7d00c5cd273a2cf1a6f3af857b26
danimtb <danimanzaneque@gmail.com>
2019-09-16 16:47:42 +0200

    Applied cpp_info.name to pkg_config generator

--- conans/test/unittests/client/generators/pkg_config_test.py
+++ conans/test/unittests/client/generators/pkg_config_test.py
@@ -16,6 +16,7 @@ class PkgGeneratorTest(unittest.TestCase):
         conanfile.initialize(Settings({}), EnvValues())
         ref = ConanFileReference.loads("MyPkg/0.1@lasote/stables")
         cpp_info = CppInfo("dummy_root_folder1")
+        cpp_info.name = ref.name
         cpp_info.defines = ["MYDEFINE1"]
         cpp_info.cflags.append("-Flag1=23")
         cpp_info.version = "1.3"
@@ -24,6 +25,7 @@ class PkgGeneratorTest(unittest.TestCase):
         conanfile.deps_cpp_info.update(cpp_info, ref.name)
         ref = ConanFileReference.loads("MyPkg2/0.1@lasote/stables")
         cpp_info = CppInfo("dummy_root_folder2")
+        cpp_info.name = ref.name
         cpp_info.defines = ["MYDEFINE2"]
         cpp_info.version = "2.3"
         cpp_info.exelinkflags = ["-exelinkflag"]
