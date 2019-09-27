17a7c444f66330648e5dcae562135ac31d353e51
Javier G. Sogo <javierg@jfrog.com>
2019-09-27 12:59:57 +0200

    jenkins already has a PROMPT

--- conans/test/functional/generators/virtualenv_test.py
+++ conans/test/functional/generators/virtualenv_test.py
@@ -188,8 +188,6 @@ class VirtualEnvIntegrationTestCase(unittest.TestCase):
         if platform.system() == "Darwin":
             env_after.pop(six.u("PS1"), None)  # TODO: FIXME: Needed for the test to pass
             env_after.pop("PS1", None)  # TODO: FIXME: Needed for the test to pass
-        elif self.commands.id == "cmd":
-            env_before.pop(six.u('PROMPT'), None)  # TODO: FIXME: Needed for the test to pass
         self.assertDictEqual(env_before, env_after)  # Environment restored incorrectly
 
         return stdout, _load_env_file(os.path.join(self.test_folder, self.env_activated))
