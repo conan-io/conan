af4b82d34ca5482d0ac0e29e974fcc87b955049f
Javier G. Sogo <javierg@jfrog.com>
2019-10-16 16:25:42 +0200

    better testing

--- conans/test/functional/toolchain/test_find_package_multi.py
+++ conans/test/functional/toolchain/test_find_package_multi.py
@@ -1,13 +1,15 @@
 # coding=utf-8
 
-import textwrap
 import platform
+import textwrap
 import unittest
 
+from parameterized.parameterized import parameterized
+
 from conans.client.toolchain.cmake import CMakeToolchain
+from conans.client.tools import environment_append
 from conans.model.ref import ConanFileReference
 from conans.test.utils.tools import TurboTestClient
-from conans.client.tools import environment_append
 
 
 class FindPackageMultiTestCase(unittest.TestCase):
@@ -51,16 +53,13 @@ class FindPackageMultiTestCase(unittest.TestCase):
         
         # Pass information to the C++ source so we can print and debug it
         target_compile_definitions(app PRIVATE CMAKE_CXX_COMPILER="${CMAKE_CXX_COMPILER}")
-        get_property(_GENERATOR_IS_MULTI_CONFIG GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG )
-        if(NOT _GENERATOR_IS_MULTI_CONFIG)
+        get_property(_GENERATOR_IS_MULTI_CONFIG GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+        target_compile_definitions(app PRIVATE GENERATOR_IS_MULTI_CONFIG="${_GENERATOR_IS_MULTI_CONFIG}")
+        if(NOT ${_GENERATOR_IS_MULTI_CONFIG})
             target_compile_definitions(app PRIVATE CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}")
-        else()
-            target_compile_definitions(app PRIVATE "$<$<CONFIG:RELEASE>:NDEBUG>")
         endif()
+        target_compile_definitions(app PRIVATE "$<$<CONFIG:RELEASE>:NDEBUG>")
         target_compile_definitions(app PRIVATE CMAKE_GENERATOR="${CMAKE_GENERATOR}")
-        target_compile_definitions(app PRIVATE CMAKE_GENERATOR_INSTANCE="${CMAKE_GENERATOR_INSTANCE}")
-        target_compile_definitions(app PRIVATE CMAKE_GENERATOR_PLATFORM="${CMAKE_GENERATOR_PLATFORM}")
-        target_compile_definitions(app PRIVATE CMAKE_GENERATOR_TOOLSET="${CMAKE_GENERATOR_TOOLSET}")
     """)
 
     app_cpp = textwrap.dedent("""
@@ -80,54 +79,59 @@ class FindPackageMultiTestCase(unittest.TestCase):
                 std::cout << "CMAKE_BUILD_TYPE: " << CMAKE_BUILD_TYPE << "\\n";
             #endif
             std::cout << "CMAKE_GENERATOR: " << CMAKE_GENERATOR << "\\n";
-            std::cout << "CMAKE_GENERATOR_INSTANCE: " << CMAKE_GENERATOR_INSTANCE << "\\n";
-            std::cout << "CMAKE_GENERATOR_PLATFORM: " << CMAKE_GENERATOR_PLATFORM << "\\n";
-            std::cout << "CMAKE_GENERATOR_TOOLSET: " << CMAKE_GENERATOR_TOOLSET << "\\n";
+            std::cout << "GENERATOR_IS_MULTI_CONFIG: " << GENERATOR_IS_MULTI_CONFIG << "\\n";
             return 0;
         }
     """)
 
+    @classmethod
+    def setUpClass(cls):
+        cls.t = TurboTestClient(path_with_spaces=False)
+        # Create the 'requirement' require
+        cls.t.run("new requirement/version -s")
+        cls.t.run("create . requirement/version@ -s build_type=Release")
+        cls.t.run("create . requirement/version@ -s build_type=Debug")
+
     def setUp(self):
-        # TODO: Move to setupClass (at least creating of the requirement)
-        self.t = TurboTestClient(path_with_spaces=False)
-
-        # Create the 'Requirement' require
-        self.t.run("new requirement/version -s")
-        self.t.run("create . requirement/version@ -s build_type=Release")
-        self.t.run("create . requirement/version@ -s build_type=Debug")
-
         # Prepare the actual consumer package
         self.app_ref = ConanFileReference.loads("app/version@user/channel")
         self.t.save({"conanfile.py": self.conanfile,
                      "CMakeLists.txt": self.cmakelist,
                      "src/app.cpp": self.app_cpp}, clean_first=True)
 
-    def _check_cmake_configure_output(self, output):
-        # Same output for all the modes
-        self.assertIn("Using Conan toolchain through {}.".format(CMakeToolchain.filename),
-                      self.t.out)
+    @parameterized.expand([("Debug",), ("Release",)])
+    def test_cache_create(self, build_type):
+        # TODO: Remove. It is here just to check that the package builds in the cache
+        # Compile the app in the cache
+        pref = self.t.create(ref=self.app_ref, conanfile=self.conanfile,
+                             args=" -s build_type={}".format(build_type))
+        self.assertIn("Using Conan toolchain", self.t.out)
 
-    def _run_app_cache(self, pref):
+        # Run the app and check it has been properly compiled
         package_layout = self.t.cache.package_layout(pref.ref)
         self.t.run_command("./app", cwd=package_layout.package(pref))
+        self.assertIn("Hello World {}!".format(build_type), self.t.out)
+        self.assertIn("App: {}".format(build_type), self.t.out)
+        # self.assertIn("CMAKE_GENERATOR: ", self.t.out)
+        self.assertIn("GENERATOR_IS_MULTI_CONFIG: 0", self.t.out)
+        self.assertIn("CMAKE_BUILD_TYPE: {}".format(build_type), self.t.out)
 
-    def test_cache_create(self):
-        # TODO: Remove. It is here just to check that the package builds in the cache
-        pref = self.t.create(ref=self.app_ref, conanfile=self.conanfile)
-        self._check_cmake_configure_output(self.t.out)
-        self._run_app_cache(pref)
-        print(self.t.out)
-
-    def test_local_conan(self):
+    @parameterized.expand([("Debug",), ("Release",)])
+    def test_local_conan(self, build_type):
         # TODO: Remove. Here just to check another way of building
         # Conan local workflow
         with self.t.chdir("build"):
-            self.t.run("install ..")
+            self.t.run("install .. -s build_type={}".format(build_type))
             self.t.run("build ..")
-            self._check_cmake_configure_output(self.t.out)
+            self.assertIn("Using Conan toolchain", self.t.out)
 
+        # Run the app and check it has been properly compiled
         self.t.run_command("./build/app")
-        print(self.t.out)
+        self.assertIn("Hello World {}!".format(build_type), self.t.out)
+        self.assertIn("App: {}".format(build_type), self.t.out)
+        # self.assertIn("CMAKE_GENERATOR: ", self.t.out)
+        self.assertIn("GENERATOR_IS_MULTI_CONFIG: 0", self.t.out)
+        self.assertIn("CMAKE_BUILD_TYPE: {}".format(build_type), self.t.out)
 
     @unittest.skipUnless(platform.system() in ["Windows", "Darwin"], "Require multiconfig generator")
     def test_multiconfig_generator(self):
@@ -135,23 +139,27 @@ class FindPackageMultiTestCase(unittest.TestCase):
             self.t.run("install .. -s build_type=Debug")
             self.t.run("install .. -s build_type=Release")
 
+            # Configure once
             mgenerator = "Xcode" if platform.system() == "Darwin" else "Visual Studio 15 Win64"
             with environment_append({"CMAKE_GENERATOR": mgenerator}):
                 cmake_configure = 'cmake .. -DCMAKE_TOOLCHAIN_FILE={}'.format(CMakeToolchain.filename)
                 self.t.run_command(cmake_configure)
-            self._check_cmake_configure_output(self.t.out)
+                self.assertIn("Using Conan toolchain", self.t.out)
 
             # Test debug
             self.t.run_command("cmake --build . --config Debug")
             self.t.run_command("./Debug/app")
             self.assertIn("Hello World Debug!", self.t.out)
             self.assertIn("App: Debug", self.t.out)
-            self.assertIn("CMAKE_GENERATOR: Xcode", self.t.out)
+            self.assertIn("CMAKE_GENERATOR: {}".format(mgenerator), self.t.out)
+            self.assertIn("GENERATOR_IS_MULTI_CONFIG: 1", self.t.out)
+            self.assertNotIn("CMAKE_BUILD_TYPE", self.t.out)
 
             # Test release
             self.t.run_command("cmake --build . --config Release")
             self.t.run_command("./Release/app")
             self.assertIn("Hello World Release!", self.t.out)
             self.assertIn("App: Release", self.t.out)
-            self.assertIn("CMAKE_GENERATOR: Xcode", self.t.out)
-
+            self.assertIn("CMAKE_GENERATOR: {}".format(mgenerator), self.t.out)
+            self.assertIn("GENERATOR_IS_MULTI_CONFIG: 1", self.t.out)
+            self.assertNotIn("CMAKE_BUILD_TYPE", self.t.out)
