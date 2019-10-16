eefdf697ead4fbc1f2435d56f273280f2c769dda
czoido <mrgalleta@gmail.com>
2019-10-16 14:26:44 +0200

    send messages to log

--- conans/client/rest/rest_client_v1.py
+++ conans/client/rest/rest_client_v1.py
@@ -130,7 +130,9 @@ class RestV1Methods(RestCommonMethods):
         url = self.router.package_upload_urls(pref)
         file_sizes = {filename: os.stat(abs_path).st_size for filename,
                       abs_path in files_to_upload.items()}
+        logger.debug("Requesting upload urls...")
         urls = self._get_file_to_url_dict(url, data=file_sizes)
+        logger.debug("Requesting upload urls...Done!")
         self._upload_files(urls, files_to_upload, self._output, retry, retry_wait)
 
     def _upload_files(self, file_urls, files, output, retry, retry_wait):
