594cd872a8bdf519077f024ba25d51ebdd4713e7
Javier G. Sogo <javierg@jfrog.com>
2019-10-16 11:46:09 +0200

    working on the toolchain (early draft)

--- conans/client/toolchain/cmake.py
+++ conans/client/toolchain/cmake.py
@@ -4,6 +4,9 @@ import os
 import textwrap
 from jinja2 import Template
 
+from conans.client.build.cmake_flags import get_generator, get_generator_platform, CMakeDefinitionsBuilder, get_toolset
+
+
 # https://stackoverflow.com/questions/30503631/cmake-in-which-order-are-files-parsed-cache-toolchain-etc
 # https://cmake.org/cmake/help/v3.6/manual/cmake-toolchains.7.html
 # https://github.com/microsoft/vcpkg/tree/master/scripts/buildsystems
@@ -11,12 +14,62 @@ from jinja2 import Template
 
 class CMakeToolchain:
     filename = "conan.cmake"
-    definitions = {}
+    conan_adjustements = os.path.join(os.path.dirname(__file__), "conan_adjustments.cmake")
 
     _template = textwrap.dedent("""
         # Conan generated toolchain file
-        message(INFO "Using Conan toolchain through ${CMAKE_TOOLCHAIN_FILE}.")
+        message("**************** TOOLCHAIN *************")
+        message("Using Conan toolchain through ${CMAKE_TOOLCHAIN_FILE}.")
 
+        # Configure
+        # -- CMake::command_line
+        {% if generator_platform %}set(CMAKE_GENERATOR_PLATFORM "{{ generator_platform }}"){% endif %}
+        {% if toolset %}set(CMAKE_GENERATOR_TOOLSET "{{ toolset }}"){% endif%}
+        
+        # --  - CMake.flags --> CMakeDefinitionsBuilder::get_definitions
+        {%- for it, value in definitions.items() %}
+        set({{ it }} "{{ value }}")
+        {%- endfor %}
+        
+        # -- args  # TODO: The user cannot set specific arguments for the cmake.configure
+        # -- defs  # TODO: The user cannot set specific definitions for the cmake.configure
+        
+        # Set some environment variables
+        {%- for it, value in environment.items() %}
+        set(ENV{{ '{' }}{{ it }}{{ '}' }} "{{ value }}")
+        {%- endfor %}
+        
+        
+        get_property( _CMAKE_IN_TRY_COMPILE GLOBAL PROPERTY IN_TRY_COMPILE )
+        if(NOT _CMAKE_IN_TRY_COMPILE)
+            message(">>>> NOT TRY COMPILE")
+            include("{{conan_adjustements_cmake}}")
+            
+            # We are going to adjust automagically many things as requested by Conan
+            #   these are the things done by 'conan_basic_setup()'
+            
+            # conan_global_flags()  We skip this step as it was configuring libraries! (also calling below 'conan_set_flags' functions
+            conan_set_flags("")
+            conan_set_flags("_RELEASE")
+            conan_set_flags("_DEBUG")
+            # TODO: Do we want to set flags here if we are going to use targets? Maybe these flags are defined inside the 'toolchain' method of the recipe
+            
+            conan_set_rpath()
+            conan_set_std()
+            conan_set_fpic()
+            
+            conan_check_compiler()
+            conan_set_libcxx()
+            conan_set_vs_runtime()
+            conan_set_find_paths()
+            conan_set_find_library_paths()
+            
+        else()
+            message(">>>> TRY COMPILE")
+        endif()
+
+                
+        {#
         # Host machine        
         set(CMAKE_SYSTEM_NAME {{host.os}})
         set(CMAKE_SYSTEM_VERSION {{host.os_version}})
@@ -29,30 +82,79 @@ class CMakeToolchain:
         
         set(CMAKE_C_COMPILER {{c_compiler}})
         set(CMAKE_CXX_COMPILER {{cxx_compiler}})
+        #}
     """)
 
-    def __init__(self, conanfile):
+    def __init__(self, conanfile,
+                 generator=None,
+                 cmake_system_name=True,
+                 parallel=True,
+                 build_type=None,
+                 toolset=None,
+                 make_program=None,
+                 set_cmake_flags=False,
+                 msbuild_verbosity="minimal",
+                 # cmake_program=None,  # TODO: cmake program should be considered in the environment
+                 generator_platform=None
+                 ):
         self._conanfile = conanfile
+        del conanfile
+
+        generator = generator or get_generator(self._conanfile.settings)
+        self._context = {
+            "conan_adjustements_cmake": self.conan_adjustements,
+            "build_type": build_type or self._conanfile.settings.get_safe("build_type"),
+            #"generator": generator,
+            "generator_platform": generator_platform or
+                                  get_generator_platform(self._conanfile.settings, generator),
+            # "parallel": parallel,  # TODO: This is for the --build
+            # '-Wno-dev'  # TODO: Can I add this to a CMake variable?
+            "toolset": toolset or get_toolset(self._conanfile.settings)
+        }
+
+        builder = CMakeDefinitionsBuilder(self._conanfile,
+                                          cmake_system_name=cmake_system_name,
+                                          make_program=make_program, parallel=parallel,
+                                          generator=generator,
+                                          set_cmake_flags=set_cmake_flags,
+                                          forced_build_type=build_type,
+                                          output=self._conanfile.output)
+        self.definitions = builder.get_definitions()
+
+        # Some variables can go to the environment
+        # TODO: Do we need this or can we move it to environment stuff
+        self.environment = {}
+        set_env = "pkg_config" in self._conanfile.generators and "PKG_CONFIG_PATH" not in os.environ
+        if set_env:
+            self.environment.update({
+                "PKG_CONFIG_PATH": self._conanfile.install_folder
+            })
+
+        # self._msbuild_verbosity = os.getenv("CONAN_MSBUILD_VERBOSITY") or msbuild_verbosity  # TODO: This is for the --build
 
     def dump(self, install_folder):
+        # The user can modify these dictionaries, add them to the context in the very last moment
+        self._context.update({
+            "definitions": self.definitions,
+            "environment": self.environment
+        })
+
         with open(os.path.join(install_folder, self.filename), "w") as f:
             # TODO: I need the profile_host and profile_build here!
             # TODO: What if the compiler is a build require?
             # TODO: Add all the stuff related to settings (ALL settings or just _MY_ settings?)
             # TODO: I would want to have here the path to the compiler too
-            compiler = "clang" if self._conanfile.settings.compiler == "apple-clang" else self._conanfile.settings.compiler
-            context = {"os": self._conanfile.settings.os,
-                       "arch": self._conanfile.settings.arch,
-                       "c_compiler": compiler,
-                       "cxx_compiler": compiler+'++'}
+            # compiler = "clang" if self._conanfile.settings.compiler == "apple-clang" else self._conanfile.settings.compiler
+            # self._context.update({"os": self._conanfile.settings.os,
+            #            "arch": self._conanfile.settings.arch,
+            #            "c_compiler": compiler,
+            #            "cxx_compiler": compiler+'++'})
 
             t = Template(self._template)
-            content = t.render(**context)
+            content = t.render(**self._context)
 
             f.write(content)
-            f.write("\n\n# ")
-            f.write('set(CMAKE_MODULE_PATH "{}")\n'.format(install_folder))  # CMAKE_MODULE_PATH can depend on the configuration (different FindXXX.cmake files provided),
-                                                                             #  it should be ok to define it into the toolchain file
+            print(content)
 
             # TODO: Remove this, intended only for testing
             f.write('set(ENV{TOOLCHAIN_ENV} "toolchain_environment")\n')
---
+++ conans/client/toolchain/conan_adjustments.cmake
@@ -0,0 +1,236 @@
+# This file contain functions to adjust CMake variables. It is the '_cmake_common_macros'
+
+function(conan_message MESSAGE_OUTPUT)
+    if(NOT CONAN_CMAKE_SILENT_OUTPUT)
+        message(${ARGV${0}})
+    endif()
+endfunction()
+
+
+macro(conan_output_dirs_setup)
+    conan_message(STATUS "Conan: Adjusting output directories")
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/bin)
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib)
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY})
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELWITHDEBINFO ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY})
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_MINSIZEREL ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY})
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY})
+
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib)
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELWITHDEBINFO ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_MINSIZEREL ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
+endmacro()
+
+
+
+macro(conan_set_flags build_type)
+    conan_message(STATUS "Conan: Adding flags for build_type '${build_type}'")
+    set(CMAKE_CXX_FLAGS${build_type} "${CMAKE_CXX_FLAGS${build_type}} ${CONAN_CXX_FLAGS${build_type}}")
+    set(CMAKE_C_FLAGS${build_type} "${CMAKE_C_FLAGS${build_type}} ${CONAN_C_FLAGS${build_type}}")
+    set(CMAKE_SHARED_LINKER_FLAGS${build_type} "${CMAKE_SHARED_LINKER_FLAGS${build_type}} ${CONAN_SHARED_LINKER_FLAGS${build_type}}")
+    set(CMAKE_EXE_LINKER_FLAGS${build_type} "${CMAKE_EXE_LINKER_FLAGS${build_type}} ${CONAN_EXE_LINKER_FLAGS${build_type}}")
+endmacro()
+
+
+macro(conan_set_rpath)
+    conan_message(STATUS "Conan: Adjusting default RPATHs Conan policies")
+    if(APPLE)
+        # https://cmake.org/Wiki/CMake_RPATH_handling
+        # CONAN GUIDE: All generated libraries should have the id and dependencies to other
+        # dylibs without path, just the name, EX:
+        # libMyLib1.dylib:
+        #     libMyLib1.dylib (compatibility version 0.0.0, current version 0.0.0)
+        #     libMyLib0.dylib (compatibility version 0.0.0, current version 0.0.0)
+        #     /usr/lib/libc++.1.dylib (compatibility version 1.0.0, current version 120.0.0)
+        #     /usr/lib/libSystem.B.dylib (compatibility version 1.0.0, current version 1197.1.1)
+        set(CMAKE_SKIP_RPATH 1)  # AVOID RPATH FOR *.dylib, ALL LIBS BETWEEN THEM AND THE EXE
+                                 # SHOULD BE ON THE LINKER RESOLVER PATH (./ IS ONE OF THEM)
+        # Policy CMP0068
+        # We want the old behavior, in CMake >= 3.9 CMAKE_SKIP_RPATH won't affect the install_name in OSX
+        set(CMAKE_INSTALL_NAME_DIR "")
+    endif()
+endmacro()
+
+
+macro(conan_set_std)
+    conan_message(STATUS "Conan: Adjusting language standard")
+    # Do not warn "Manually-specified variables were not used by the project"
+    set(ignorevar "${CONAN_STD_CXX_FLAG}${CONAN_CMAKE_CXX_STANDARD}${CONAN_CMAKE_CXX_EXTENSIONS}")
+    if (CMAKE_VERSION VERSION_LESS "3.1" OR
+        (CMAKE_VERSION VERSION_LESS "3.12" AND ("${CONAN_CMAKE_CXX_STANDARD}" STREQUAL "20" OR "${CONAN_CMAKE_CXX_STANDARD}" STREQUAL "gnu20")))
+    if(CONAN_STD_CXX_FLAG)
+        conan_message(STATUS "Conan setting CXX_FLAGS flags: ${CONAN_STD_CXX_FLAG}")
+        set(CMAKE_CXX_FLAGS "${CONAN_STD_CXX_FLAG} ${CMAKE_CXX_FLAGS}")
+    endif()
+    else()
+        if(CONAN_CMAKE_CXX_STANDARD)
+            conan_message(STATUS "Conan setting CPP STANDARD: ${CONAN_CMAKE_CXX_STANDARD} WITH EXTENSIONS ${CONAN_CMAKE_CXX_EXTENSIONS}")
+            set(CMAKE_CXX_STANDARD ${CONAN_CMAKE_CXX_STANDARD})
+            set(CMAKE_CXX_EXTENSIONS ${CONAN_CMAKE_CXX_EXTENSIONS})
+        endif()
+    endif()
+endmacro()
+
+
+macro(conan_set_fpic)
+    if(DEFINED CONAN_CMAKE_POSITION_INDEPENDENT_CODE)
+        conan_message(STATUS "Conan: Adjusting fPIC flag (${CONAN_CMAKE_POSITION_INDEPENDENT_CODE})")
+        set(CMAKE_POSITION_INDEPENDENT_CODE ${CONAN_CMAKE_POSITION_INDEPENDENT_CODE})
+    endif()
+endmacro()
+
+
+function(conan_check_compiler)
+    if(CONAN_DISABLE_CHECK_COMPILER)
+        conan_message(STATUS "WARN: Disabled conan compiler checks")
+        return()
+    endif()
+    if(NOT DEFINED CMAKE_CXX_COMPILER_ID)
+        if(DEFINED CMAKE_C_COMPILER_ID)
+            conan_message(STATUS "This project seems to be plain C, using '${CMAKE_C_COMPILER_ID}' compiler")
+            set(CMAKE_CXX_COMPILER_ID ${CMAKE_C_COMPILER_ID})
+            set(CMAKE_CXX_COMPILER_VERSION ${CMAKE_C_COMPILER_VERSION})
+        else()
+            message(FATAL_ERROR "This project seems to be plain C, but no compiler defined")
+        endif()
+    endif()
+    if(NOT CMAKE_CXX_COMPILER_ID AND NOT CMAKE_C_COMPILER_ID)
+        # This use case happens when compiler is not identified by CMake, but the compilers are there and work
+        conan_message(STATUS "*** WARN: CMake was not able to identify a C or C++ compiler ***")
+        conan_message(STATUS "*** WARN: Disabling compiler checks. Please make sure your settings match your environment ***")
+        return()
+    endif()
+    if(NOT DEFINED CONAN_COMPILER)
+        conan_get_compiler(CONAN_COMPILER CONAN_COMPILER_VERSION)
+        if(NOT DEFINED CONAN_COMPILER)
+            conan_message(STATUS "WARN: CONAN_COMPILER variable not set, please make sure yourself that "
+                          "your compiler and version matches your declared settings")
+            return()
+        endif()
+    endif()
+
+    if(NOT CMAKE_HOST_SYSTEM_NAME STREQUAL ${CMAKE_SYSTEM_NAME})
+        set(CROSS_BUILDING 1)
+    endif()
+
+    # If using VS, verify toolset
+    if (CONAN_COMPILER STREQUAL "Visual Studio")
+        if (CONAN_SETTINGS_COMPILER_TOOLSET MATCHES "LLVM" OR
+            CONAN_SETTINGS_COMPILER_TOOLSET MATCHES "clang")
+            set(EXPECTED_CMAKE_CXX_COMPILER_ID "Clang")
+        elseif (CONAN_SETTINGS_COMPILER_TOOLSET MATCHES "Intel")
+            set(EXPECTED_CMAKE_CXX_COMPILER_ID "Intel")
+        else()
+            set(EXPECTED_CMAKE_CXX_COMPILER_ID "MSVC")
+        endif()
+
+        if (NOT CMAKE_CXX_COMPILER_ID MATCHES ${EXPECTED_CMAKE_CXX_COMPILER_ID})
+            message(FATAL_ERROR "Incorrect '${CONAN_COMPILER}'. Toolset specifies compiler as '${EXPECTED_CMAKE_CXX_COMPILER_ID}' "
+                                "but CMake detected '${CMAKE_CXX_COMPILER_ID}'")
+        endif()
+
+    # Avoid checks when cross compiling, apple-clang crashes because its APPLE but not apple-clang
+    # Actually CMake is detecting "clang" when you are using apple-clang, only if CMP0025 is set to NEW will detect apple-clang
+    elseif((CONAN_COMPILER STREQUAL "gcc" AND NOT CMAKE_CXX_COMPILER_ID MATCHES "GNU") OR
+        (CONAN_COMPILER STREQUAL "apple-clang" AND NOT CROSS_BUILDING AND (NOT APPLE OR NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang")) OR
+        (CONAN_COMPILER STREQUAL "clang" AND NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang") OR
+        (CONAN_COMPILER STREQUAL "sun-cc" AND NOT CMAKE_CXX_COMPILER_ID MATCHES "SunPro") )
+        message(FATAL_ERROR "Incorrect '${CONAN_COMPILER}', is not the one detected by CMake: '${CMAKE_CXX_COMPILER_ID}'")
+    endif()
+
+
+    if(NOT DEFINED CONAN_COMPILER_VERSION)
+        conan_message(STATUS "WARN: CONAN_COMPILER_VERSION variable not set, please make sure yourself "
+                             "that your compiler version matches your declared settings")
+        return()
+    endif()
+    check_compiler_version()
+endfunction()
+
+
+macro(conan_set_libcxx)
+    if(DEFINED CONAN_LIBCXX)
+        conan_message(STATUS "Conan: C++ stdlib: ${CONAN_LIBCXX}")
+        if(CONAN_COMPILER STREQUAL "clang" OR CONAN_COMPILER STREQUAL "apple-clang")
+            if(CONAN_LIBCXX STREQUAL "libstdc++" OR CONAN_LIBCXX STREQUAL "libstdc++11" )
+                set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libstdc++")
+            elseif(CONAN_LIBCXX STREQUAL "libc++")
+                set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+            endif()
+        endif()
+        if(CONAN_COMPILER STREQUAL "sun-cc")
+            if(CONAN_LIBCXX STREQUAL "libCstd")
+                set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -library=Cstd")
+            elseif(CONAN_LIBCXX STREQUAL "libstdcxx")
+                set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -library=stdcxx4")
+            elseif(CONAN_LIBCXX STREQUAL "libstlport")
+                set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -library=stlport4")
+            elseif(CONAN_LIBCXX STREQUAL "libstdc++")
+                set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -library=stdcpp")
+            endif()
+        endif()
+        if(CONAN_LIBCXX STREQUAL "libstdc++11")
+            add_definitions(-D_GLIBCXX_USE_CXX11_ABI=1)
+        elseif(CONAN_LIBCXX STREQUAL "libstdc++")
+            add_definitions(-D_GLIBCXX_USE_CXX11_ABI=0)
+        endif()
+    endif()
+endmacro()
+
+
+macro(conan_set_vs_runtime)
+    if(CONAN_LINK_RUNTIME)
+        foreach(flag CMAKE_C_FLAGS_RELEASE CMAKE_CXX_FLAGS_RELEASE
+                     CMAKE_C_FLAGS_RELWITHDEBINFO CMAKE_CXX_FLAGS_RELWITHDEBINFO
+                     CMAKE_C_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_MINSIZEREL)
+            if(DEFINED ${flag})
+                string(REPLACE "/MD" ${CONAN_LINK_RUNTIME} ${flag} "${${flag}}")
+            endif()
+        endforeach()
+        foreach(flag CMAKE_C_FLAGS_DEBUG CMAKE_CXX_FLAGS_DEBUG)
+            if(DEFINED ${flag})
+                string(REPLACE "/MDd" ${CONAN_LINK_RUNTIME} ${flag} "${${flag}}")
+            endif()
+        endforeach()
+    endif()
+endmacro()
+
+
+macro(conan_set_find_paths)
+    # CMAKE_MODULE_PATH does not have Debug/Release config, but there are variables
+    # CONAN_CMAKE_MODULE_PATH_DEBUG to be used by the consumer
+    # CMake can find findXXX.cmake files in the root of packages
+    set(CMAKE_MODULE_PATH ${CONAN_CMAKE_MODULE_PATH} ${CMAKE_MODULE_PATH})
+
+    # Make find_package() to work
+    set(CMAKE_PREFIX_PATH ${CONAN_CMAKE_MODULE_PATH} ${CMAKE_PREFIX_PATH})
+
+    # Set the find root path (cross build)
+    set(CMAKE_FIND_ROOT_PATH ${CONAN_CMAKE_FIND_ROOT_PATH} ${CMAKE_FIND_ROOT_PATH})
+    if(CONAN_CMAKE_FIND_ROOT_PATH_MODE_PROGRAM)
+        set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ${CONAN_CMAKE_FIND_ROOT_PATH_MODE_PROGRAM})
+    endif()
+    if(CONAN_CMAKE_FIND_ROOT_PATH_MODE_LIBRARY)
+        set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ${CONAN_CMAKE_FIND_ROOT_PATH_MODE_LIBRARY})
+    endif()
+    if(CONAN_CMAKE_FIND_ROOT_PATH_MODE_INCLUDE)
+        set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ${CONAN_CMAKE_FIND_ROOT_PATH_MODE_INCLUDE})
+    endif()
+endmacro()
+
+
+macro(conan_set_find_library_paths)
+    # CMAKE_INCLUDE_PATH, CMAKE_LIBRARY_PATH does not have Debug/Release config, but there are variables
+    # CONAN_INCLUDE_DIRS_DEBUG/RELEASE CONAN_LIB_DIRS_DEBUG/RELEASE to be used by the consumer
+    # For find_library
+    set(CMAKE_INCLUDE_PATH ${CONAN_INCLUDE_DIRS} ${CMAKE_INCLUDE_PATH})
+    set(CMAKE_LIBRARY_PATH ${CONAN_LIB_DIRS} ${CMAKE_LIBRARY_PATH})
+endmacro()
+
