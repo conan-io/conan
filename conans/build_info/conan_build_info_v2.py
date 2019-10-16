2a54a632da5612203d475c07d97337f220d48a27
czoido <mrgalleta@gmail.com>
2019-10-16 17:28:05 +0200

    wip

--- conans/build_info/conan_build_info_v2.py
+++ conans/build_info/conan_build_info_v2.py
@@ -1,10 +1,33 @@
-def build_info_start(build_info_name, build_info_number):
-    print(build_info_name, build_info_number)
-    pass
+import os
+import sys
+
+from conans.client.cache.cache import ClientCache
+from conans.client.output import ConanOutput
+from conans.errors import ConanException
+from conans.paths import get_conan_user_home
+from conans.util.files import save
+
+output = ConanOutput(sys.stdout, sys.stderr, True)
+
+
+def build_info_start(build_name, build_number):
+    paths = ClientCache(os.path.join(get_conan_user_home(), ".conan"), output)
+    content = "artifact_property_build.name={}\n" \
+              "artifact_property_build.number={}\n".format(build_name, build_number)
+    try:
+        artifact_properties_file = paths.put_headers_path
+        save(artifact_properties_file, content)
+    except Exception:
+        raise ConanException("Can't write properties file in %s" % artifact_properties_file)
 
 
 def build_info_stop():
-    pass
+    paths = ClientCache(os.path.join(get_conan_user_home(), ".conan"), output)
+    try:
+        artifact_properties_file = paths.put_headers_path
+        save(artifact_properties_file, "")
+    except Exception:
+        raise ConanException("Can't write properties file in %s" % artifact_properties_file)
 
 
 def build_info_create(build_info_file, lockfile):
