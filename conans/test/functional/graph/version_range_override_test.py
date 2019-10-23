252f9b562d043bcb4249f4802f88750b2d1da5d7
javierg <javierg@jfrog.com>
2019-10-23 18:09:41 +0200

    fix tests

--- conans/test/functional/graph/version_range_override_test.py
+++ conans/test/functional/graph/version_range_override_test.py
@@ -28,7 +28,7 @@ class VersionRangeOverrideTestCase(unittest.TestCase):
                                                    .with_require_plain("libC/1.0@user/channel")})
         self.t.run("info . --only requires")
         self.assertIn("libB/3.0@user/channel", self.t.out)
-        self.assertIn("WARN: libC/1.0@user/channel requirement libB/[<=2.0]@user/channel overridden"
+        self.assertIn("WARN: libC/1.0@user/channel: requirement libB/[<=2.0]@user/channel overridden"
                       " by your conanfile to libB/3.0@user/channel", self.t.out)
 
     def test_override_using_version_range(self):
@@ -37,7 +37,7 @@ class VersionRangeOverrideTestCase(unittest.TestCase):
                                                    .with_require_plain("libC/1.0@user/channel")})
         self.t.run("info . --only requires")
         self.assertIn("libB/1.0@user/channel", self.t.out)
-        self.assertIn("WARN: libC/1.0@user/channel requirement libB/[<=2.0]@user/channel overridden"
+        self.assertIn("WARN: libC/1.0@user/channel: requirement libB/[<=2.0]@user/channel overridden"
                       " by your conanfile to libB/1.0@user/channel", self.t.out)
         self.assertIn("Version range '<2.x' required by 'conanfile.py' resolved to"
                       " 'libB/1.0@user/channel' in local cache", self.t.out)
@@ -49,7 +49,7 @@ class VersionRangeOverrideTestCase(unittest.TestCase):
         self.t.save({"conanfile.py": GenConanfile().with_require_plain("libB/[>2.x]@user/channel")
                                                    .with_require_plain("libC/1.0@user/channel")})
         self.t.run("info . --only requires", assert_error=True)
-        self.assertIn("WARN: libC/1.0@user/channel requirement libB/[<=2.0]@user/channel overridden"
+        self.assertIn("WARN: libC/1.0@user/channel: requirement libB/[<=2.0]@user/channel overridden"
                       " by your conanfile to libB/3.0@user/channel", self.t.out)
         self.assertIn("ERROR: Version range '<=2.0' required by 'libC/1.0@user/channel' not valid"
                       " for downstream requirement 'libB/3.0@user/channel'", self.t.out)
