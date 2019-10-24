b39d6f335cf73654b21072d1c25c7ef29962b06a
czoido <mrgalleta@gmail.com>
2019-10-24 16:02:58 +0200

    wip

--- conans/test/integration/test_build_info_creation.py
+++ conans/test/integration/test_build_info_creation.py
@@ -3,6 +3,7 @@ import os
 import sys
 import textwrap
 import unittest
+from operator import itemgetter
 
 from mock import patch, PropertyMock
 
@@ -51,13 +52,6 @@ class MyBuildInfoCreation(unittest.TestCase):
                     self.copy("myfile.txt", folder=True)
                 def package(self):
                     self.copy("*myfile.txt")
-                def package_info(self):
-                    self.output.info("SELF FILE: %s"
-                        % load(os.path.join(self.package_folder, "myfile.txt")))
-                    for d in os.listdir(self.package_folder):
-                        p = os.path.join(self.package_folder, d, "myfile.txt")
-                        if os.path.isfile(p):
-                            self.output.info("DEP FILE %s: %s" % (d, load(p)))
                 """)
         client = TestClient(default_server_user=True)
         mock_cache.return_value = client.cache
@@ -80,7 +74,7 @@ class MyBuildInfoCreation(unittest.TestCase):
         client.save({"conanfile.py": conanfile.format(
             requires='requires = "PkgA/0.1@user/channel"'),
             "myfile.txt": "HelloB"})
-        client.run("create . PkgB/0.2@user/testing --lockfile")
+        client.run("create . PkgB/0.2@user/channel --lockfile")
 
         client.run("upload * --all --confirm")
         sys.argv = ['conan_build_info', 'start', 'MyBuildName', '42']
@@ -94,122 +88,34 @@ class MyBuildInfoCreation(unittest.TestCase):
             buildinfo = json.load(f)
             self.assertEqual(buildinfo["name"], "MyBuildName")
             self.assertEqual(buildinfo["number"], "42")
-            self.assertEqual(buildinfo["modules"][0]["id"], "PkgB/0.2@user/testing")
+            self.assertEqual(buildinfo["modules"][0]["id"], "PkgB/0.2@user/channel")
             self.assertEqual(buildinfo["modules"][1]["id"],
-                             "PkgB/0.2@user/testing:5bf1ba84b5ec8663764a406f08a7f9ae5d3d5fb5")
+                             "PkgB/0.2@user/channel:5bf1ba84b5ec8663764a406f08a7f9ae5d3d5fb5")
             self.assertEqual(buildinfo["modules"][0]["artifacts"][0]["name"], "conan_sources.tgz")
             self.assertEqual(buildinfo["modules"][1]["artifacts"][0]["name"], "conan_package.tgz")
 
-    def test_build_info_update(self):
-        conanfile = textwrap.dedent("""
-            from conans import ConanFile, load
-            import os
-            class Pkg(ConanFile):
-                {requires}
-                exports_sources = "myfile.txt"
-                keep_imports = True
-                def imports(self):
-                    self.copy("myfile.txt", folder=True)
-                def package(self):
-                    self.copy("*myfile.txt")
-                def package_info(self):
-                    self.output.info("SELF FILE: %s"
-                        % load(os.path.join(self.package_folder, "myfile.txt")))
-                    for d in os.listdir(self.package_folder):
-                        p = os.path.join(self.package_folder, d, "myfile.txt")
-                        if os.path.isfile(p):
-                            self.output.info("DEP FILE %s: %s" % (d, load(p)))
-                """)
-        client = TestClient(default_server_user=True)
-        client.save({"conanfile.py": conanfile.format(requires=""),
-                     "myfile.txt": "HelloA"})
-        client.run("create . PkgA/0.1@user/channel")
-        client.save({"conanfile.py": conanfile.format(
-            requires='requires = "PkgA/0.1@user/channel"'),
-            "myfile.txt": "HelloB"})
-        client.run("create . PkgB/0.1@user/channel")
-        client.save({"conanfile.py": conanfile.format(
-            requires='requires = "PkgB/0.1@user/channel"'),
-            "myfile.txt": "HelloC"})
-        client.run("create . PkgC/0.1@user/channel")
-        client.save({"conanfile.py": conanfile.format(
-            requires='requires = "PkgC/0.1@user/channel"'),
-            "myfile.txt": "HelloD"})
-        client.run("create . PkgD/0.1@user/channel")
-        client.run("graph lock PkgD/0.1@user/channel")
-        client.save({"conanfile.py": conanfile.format(
-            requires='requires = "PkgA/0.1@user/channel"'),
-            "myfile.txt": "HelloB"})
-        client.run("create . PkgB/0.2@user/testing --lockfile")
-        registry = client.cache.registry
-        headers_path = client.cache.put_headers_path
-        client.run("upload * --all --confirm")
-
-        @patch('conans.client.cache.cache.ClientCache.put_headers_path', new_callable=PropertyMock)
-        @patch('conans.client.cache.cache.ClientCache.registry', new_callable=PropertyMock)
-        @patch('conans.client.cache.cache.ClientCache.package_layout',
-               new=client.cache.package_layout)
-        @patch('conans.client.cache.cache.ClientCache.read_put_headers',
-               new=client.cache.read_put_headers)
-        def create_bi_a(registry_mock, mock_put_headers_path):
-            registry_mock.return_value = registry
-            mock_put_headers_path.return_value = headers_path
-            sys.argv = ['conan_build_info', 'start', 'MyBuildName', '42']
-            run()
-            sys.argv = ['conan_build_info', 'create',
-                        os.path.join(client.current_folder, 'buildinfo_a.json'), '--lockfile',
-                        os.path.join(client.current_folder, LOCKFILE),
-                        '--user', 'user', '--password', 'password']
-            run()
-
-        create_bi_a()
-
+        # now test update build_info
         client.save({"conanfile.py": conanfile.format(
             requires='requires = "PkgB/0.2@user/channel"'),
             "myfile.txt": "HelloC"})
-        client.run("create . PkgC/0.2@user/testing --lockfile")
+        client.run("create . PkgC/0.2@user/channel --lockfile")
         client.run("upload * --all --confirm")
+        sys.argv = ['conan_build_info', 'create',
+                    os.path.join(client.current_folder, 'buildinfo_b.json'), '--lockfile',
+                    os.path.join(client.current_folder, LOCKFILE),
+                    '--user', 'user', '--password', 'password']
+        run()
 
-        @patch('conans.client.cache.cache.ClientCache.put_headers_path', new_callable=PropertyMock)
-        @patch('conans.client.cache.cache.ClientCache.registry', new_callable=PropertyMock)
-        @patch('conans.client.cache.cache.ClientCache.package_layout',
-               new=client.cache.package_layout)
-        @patch('conans.client.cache.cache.ClientCache.read_put_headers',
-               new=client.cache.read_put_headers)
-        def create_bi_b(registry_mock, mock_put_headers_path):
-            registry_mock.return_value = registry
-            mock_put_headers_path.return_value = headers_path
-            sys.argv = ['conan_build_info', 'start', 'MyBuildName', '43']
-            run()
-            sys.argv = ['conan_build_info', 'create',
-                        os.path.join(client.current_folder, 'buildinfo_b.json'), '--lockfile',
-                        os.path.join(client.current_folder, LOCKFILE),
-                        '--user', 'user', '--password', 'password']
-            run()
-
-        create_bi_b()
-
-        with open(os.path.join(client.current_folder, 'buildinfo_a.json')) as f:
-            buildinfo = json.load(f)
-            print(buildinfo)
-
-        with open(os.path.join(client.current_folder, 'buildinfo_b.json')) as f:
-            buildinfo = json.load(f)
-            print(buildinfo)
-
-        @patch('conans.client.cache.cache.ClientCache.put_headers_path', new_callable=PropertyMock)
-        @patch('conans.client.cache.cache.ClientCache.registry', new_callable=PropertyMock)
-        @patch('conans.client.cache.cache.ClientCache.package_layout',
-               new=client.cache.package_layout)
-        @patch('conans.client.cache.cache.ClientCache.read_put_headers',
-               new=client.cache.read_put_headers)
-        def update(registry_mock, mock_put_headers_path):
-            registry_mock.return_value = registry
-            mock_put_headers_path.return_value = headers_path
-            sys.argv = ['conan_build_info', 'update', 'buildinfo_a.json', 'buildinfo_b.json',
-                        '--output-file', 'mergedbuildinfo.json']
-            run()
+        sys.argv = ['conan_build_info', 'update',
+                    os.path.join(client.current_folder, 'buildinfo.json'),
+                    os.path.join(client.current_folder, 'buildinfo_b.json'),
+                    '--output-file', os.path.join(client.current_folder, 'mergedbuildinfo.json')]
+        run()
 
         with open(os.path.join(client.current_folder, 'mergedbuildinfo.json')) as f:
             buildinfo = json.load(f)
-            print(buildinfo)
+            self.assertEqual(buildinfo["name"], "MyBuildName")
+            self.assertEqual(buildinfo["number"], "42")
+            ids_list = list(map(itemgetter("id"), buildinfo["modules"]))
+            self.assertTrue("PkgC/0.2@user/channel" in ids_list)
+            self.assertTrue("PkgB/0.2@user/channel" in ids_list)
