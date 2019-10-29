a7d6a4493208aede4a53c50303469b1e82cd7b60
Javier G. Sogo <jgsogo@gmail.com>
2019-10-29 15:21:07 +0100

    remove maxDiff

--- conans/test/functional/generators/virtualbuildenv_test.py
+++ conans/test/functional/generators/virtualbuildenv_test.py
@@ -9,7 +9,6 @@ from conans.test.utils.tools import TestClient
 
 
 class VirtualBuildEnvTest(unittest.TestCase):
-    maxDiff = None
 
     @unittest.skipUnless(platform.system() == "Windows", "needs Windows")
     def test_delimiter_error(self):
