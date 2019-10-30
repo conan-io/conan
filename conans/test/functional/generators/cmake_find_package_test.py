666a4a8406276b3758e356da12c4b8c6d17f1106
danimtb <danimanzaneque@gmail.com>
2019-10-30 19:42:19 +0100

    sorted

--- conans/test/functional/generators/cmake_find_package_test.py
+++ conans/test/functional/generators/cmake_find_package_test.py
@@ -347,7 +347,8 @@ message("Target libs: ${tmp}")
         pref = PackageReference(ref, NO_SETTINGS_PACKAGE_ID, None)
         package_path = client.cache.package_layout(ref).package(pref)
         modules_path = os.path.join(package_path, "share", "cmake")
-        self.assertListEqual(os.listdir(modules_path), ["FindFindModule.cmake", "my-module.cmake"])
+        self.assertListEqual(sorted(os.listdir(modules_path)),
+                             sorted(["FindFindModule.cmake", "my-module.cmake"]))
         consumer = textwrap.dedent("""
             from conans import ConanFile, CMake
 
