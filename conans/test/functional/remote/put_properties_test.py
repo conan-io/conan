f51a8c9d04b89f18e42ec5a5cd45b06cf2bb0704
javierg <javierg@jfrog.com>
2019-11-04 19:14:35 +0100

    add tests with many different values

--- conans/test/functional/remote/put_properties_test.py
+++ conans/test/functional/remote/put_properties_test.py
@@ -1,9 +1,10 @@
 import os
 import unittest
-
+import re
 from conans.test.utils.cpp_test_files import cpp_hello_conan_files
 from conans.test.utils.tools import TestClient, TestRequester, TestServer
 from conans.util.files import save
+from six.moves.urllib.parse import quote_plus, unquote, urlparse
 
 
 class PutPropertiesTest(unittest.TestCase):
@@ -22,17 +23,34 @@ class PutPropertiesTest(unittest.TestCase):
         self.assertTrue(os.path.exists(props_file))
 
     def put_properties_test(self):
-        wanted_vars = {"MyHeader1": "MyHeaderValue1;MyHeaderValue2", "Other": "Value"}
+        wanted_vars = {"key0": "value",
+                       "key1": "with space",
+                       #"key2": "with/slash",  # FIXME: Not supported in conan-server
+                       "key3": "with.dot",
+                       "key4": "with;semicolon",
+                       "key5": "with~virgul",
+                       "key6": "with#hash"
+                       }
 
-        class RequesterCheckHeaders(TestRequester):
-            def put(self, url, **kwargs):
+        class RequesterCheckArtifactProperties(TestRequester):
+            def put(self_requester, url, **kwargs):
+                # Check headers
                 for name, value in wanted_vars.items():
                     value1 = kwargs["headers"][name]
-                    if value1 != value:
-                        raise Exception()
-                return super(RequesterCheckHeaders, self).put(url, **kwargs)
+                    self.assertEqual(value1, value)
 
-        self.client = TestClient(requester_class=RequesterCheckHeaders, servers=self.servers,
+                # Check matrix params
+                mp = re.match(r"^[^;\s]+;(?P<matrix_params>[^/]+)/.*", url).group("matrix_params")
+                values = [it.split("=") for it in mp.split(';')]
+                values = {key: unquote(value) for (key, value) in values}
+                for name, value in wanted_vars.items():
+                    value1 = values[name]
+                    self.assertEqual(value1, value)
+
+                return super(RequesterCheckArtifactProperties, self_requester).put(url, **kwargs)
+
+        self.client = TestClient(requester_class=RequesterCheckArtifactProperties,
+                                 servers=self.servers,
                                  users={"default": [("lasote", "mypass")]})
         _create_property_files(self.client, wanted_vars)
 
