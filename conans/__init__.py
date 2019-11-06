44a9332ff83dce92abe9228b446fa47e3a27863d
Luis Martinez de Bartolome <lasote@gmail.com>
2019-11-06 12:42:20 +0100

    six fix

--- conans/__init__.py
+++ conans/__init__.py
@@ -18,5 +18,5 @@ CHECKSUM_DEPLOY = "checksum_deploy"  # Only when v2
 REVISIONS = "revisions"  # Only when enabled in config, not by default look at server_launcher.py
 SERVER_CAPABILITIES = [COMPLEX_SEARCH_CAPABILITY, ]  # Still without v2 because it is changing
 
-__version__ = '1.9.2'
+__version__ = '1.9.3'
 
