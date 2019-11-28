1312d29db5da6062e44a3c5d2fe8cc47f8c3c717
javierg <javierg@jfrog.com>
2019-11-28 13:56:49 +0100

    no need to check here

--- conans/util/tracer.py
+++ conans/util/tracer.py
@@ -69,8 +69,6 @@ def _append_action(action_name, props):
 # ############## LOG METHODS ######################
 
 def _file_document(name, path):
-    if os.path.islink(path):
-        return {"name": name, "path": path, "md5": 0, "sha1": 0}
     return {"name": name, "path": path, "md5": md5sum(path), "sha1": sha1sum(path)}
 
 
