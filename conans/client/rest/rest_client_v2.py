81102434601f93b8699fe9f9a482c369ca026c1a
Javier G. Sogo <jgsogo@gmail.com>
2019-12-02 17:53:50 +0100

    Publish 'artifacts.properties' using matrix params (#6014)
    * draft to test
    
    * add missing argument to functions
    
    * rename 'put_headers' to 'artifact_properties'
    
    * store matrix params as the preformatted string in the router
    
    * fallback to empty string
    
    * something going wrong: too many cchanges
    
    * at least these are not needed
    
    * typo
    
    * it was only needed to remove the matrix_params for the bottle server
    
    * soooo
    
    * remove class name
    
    * fix minor bug
    
    * now with revisions too
    
    * make docs with triple double quotes
    
    * we need to modify the conan_server to accept matrix_params too
    
    * fix tests
    
    * add tests with artifacts.properties
    
    * quote values of matrix params
    
    * we can stack routes
    
    * add tests with many different values
    
    * remove changes not needed
    
    * use server capability to choose the URL to build
    
    * do not send headers if matrix_params capability
    
    * do not add 'matrix_params' functionality to conan-server
    
    * rename variable, 'file' is reserved
    
    * remove change uneeded
    
    * conan server does not support matrix params at all
    
    * use the proper if/else

--- conans/client/rest/rest_client_v2.py
+++ conans/client/rest/rest_client_v2.py
@@ -19,15 +19,16 @@ from conans.util.log import logger
 class RestV2Methods(RestCommonMethods):
 
     def __init__(self, remote_url, token, custom_headers, output, requester, verify_ssl,
-                 artifacts_properties=None, checksum_deploy=False):
+                 artifacts_properties=None, checksum_deploy=False, matrix_params=False):
 
         super(RestV2Methods, self).__init__(remote_url, token, custom_headers, output, requester,
-                                            verify_ssl, artifacts_properties)
+                                            verify_ssl, artifacts_properties, matrix_params)
         self._checksum_deploy = checksum_deploy
 
     @property
     def router(self):
-        return ClientV2Router(self.remote_url.rstrip("/"))
+        return ClientV2Router(self.remote_url.rstrip("/"), self._artifacts_properties,
+                              self._matrix_params)
 
     def _get_file_list_json(self, url):
         data = self.get_json(url)
@@ -161,11 +162,11 @@ class RestV2Methods(RestCommonMethods):
 
     def _upload_recipe(self, ref, files_to_upload, retry, retry_wait):
         # Direct upload the recipe
-        urls = {fn: self.router.recipe_file(ref, fn) for fn in files_to_upload}
+        urls = {fn: self.router.recipe_file(ref, fn, add_matrix_params=True) for fn in files_to_upload}
         self._upload_files(files_to_upload, urls, retry, retry_wait)
 
     def _upload_package(self, pref, files_to_upload, retry, retry_wait):
-        urls = {fn: self.router.package_file(pref, fn)
+        urls = {fn: self.router.package_file(pref, fn, add_matrix_params=True)
                 for fn in files_to_upload}
         self._upload_files(files_to_upload, urls, retry, retry_wait)
 
@@ -180,10 +181,11 @@ class RestV2Methods(RestCommonMethods):
                 self._output.rewrite_line("Uploading %s" % filename)
             resource_url = urls[filename]
             try:
+                headers = self._artifacts_properties if not self._matrix_params else {}
                 uploader.upload(resource_url, files[filename], auth=self.auth,
                                 dedup=self._checksum_deploy, retry=retry,
                                 retry_wait=retry_wait,
-                                headers=self._artifacts_properties)
+                                headers=headers)
             except (AuthenticationException, ForbiddenException):
                 raise
             except Exception as exc:
