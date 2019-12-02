6b9a2dd75013cb908daa7938de0bd500951e57e8
SSE4 <tomskside@gmail.com>
2019-12-02 22:11:55 +0700

    - generate ConfigVersion.cmake file (#6063)
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conans/client/generators/cmake_find_package_multi.py
+++ conans/client/generators/cmake_find_package_multi.py
@@ -61,6 +61,33 @@ set_property(TARGET {name}::{name}
                  $<$<CONFIG:Debug>:${{{name}_COMPILE_OPTIONS_DEBUG_LIST}}>) 
     """
 
+# https://gitlab.kitware.com/cmake/cmake/blob/master/Modules/BasicConfigVersion-SameMajorVersion.cmake.in
+    version_template = """
+set(PACKAGE_VERSION "{version}")
+
+if(PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION)
+  set(PACKAGE_VERSION_COMPATIBLE FALSE)
+else()
+
+  if("{version}" MATCHES "^([0-9]+)\\\\.")
+    set(CVF_VERSION_MAJOR "${{CMAKE_MATCH_1}}")
+  else()
+    set(CVF_VERSION_MAJOR "{version}")
+  endif()
+
+  if(PACKAGE_FIND_VERSION_MAJOR STREQUAL CVF_VERSION_MAJOR)
+    set(PACKAGE_VERSION_COMPATIBLE TRUE)
+  else()
+    set(PACKAGE_VERSION_COMPATIBLE FALSE)
+  endif()
+
+  if(PACKAGE_FIND_VERSION STREQUAL PACKAGE_VERSION)
+      set(PACKAGE_VERSION_EXACT TRUE)
+  endif()
+
+endif()
+"""
+
     @property
     def filename(self):
         pass
@@ -79,6 +106,8 @@ set_property(TARGET {name}::{name}
             find_lib = target_template.format(name=depname, deps=deps,
                                               build_type_suffix=build_type_suffix)
             ret["{}Target-{}.cmake".format(depname, build_type.lower())] = find_lib
+            ret["{}ConfigVersion.cmake".format(depname)] = self.version_template.\
+                format(version=cpp_info.version)
         return ret
 
     def _build_type_suffix(self, build_type):
