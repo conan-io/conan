438f2ff209e263f383a0213477a6c8f52e72d080
memsharded <james@conan.io>
2019-12-05 14:01:58 +0100

    range_resolver.clear_output()

--- conans/client/conan_api.py
+++ conans/client/conan_api.py
@@ -343,7 +343,7 @@ class ConanAPIV1(object):
                                  not not_export, graph_lock=graph_info.graph_lock,
                                  ignore_dirty=ignore_dirty)
 
-            self.app.range_resolver.output  # invalidate version range output
+            self.app.range_resolver.clear_output()  # invalidate version range output
 
             # The new_ref contains the revision
             # To not break existing things, that they used this ref without revision
