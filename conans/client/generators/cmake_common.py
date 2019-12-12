63615ed95b174de401c969b26574748f6c71e1b8
javierg <javierg@jfrog.com>
2019-12-12 20:07:43 +0100

    moving from the toolchain to commons

--- conans/client/generators/cmake_common.py
+++ conans/client/generators/cmake_common.py
@@ -289,14 +289,15 @@ class CMakeCommonMacros:
 
     conan_set_std = textwrap.dedent("""
         macro(conan_set_std)
+            conan_message(STATUS "Conan: Adjusting language standard")
             # Do not warn "Manually-specified variables were not used by the project"
             set(ignorevar "${CONAN_STD_CXX_FLAG}${CONAN_CMAKE_CXX_STANDARD}${CONAN_CMAKE_CXX_EXTENSIONS}")
             if (CMAKE_VERSION VERSION_LESS "3.1" OR
                 (CMAKE_VERSION VERSION_LESS "3.12" AND ("${CONAN_CMAKE_CXX_STANDARD}" STREQUAL "20" OR "${CONAN_CMAKE_CXX_STANDARD}" STREQUAL "gnu20")))
-            if(CONAN_STD_CXX_FLAG)
-                conan_message(STATUS "Conan setting CXX_FLAGS flags: ${CONAN_STD_CXX_FLAG}")
-                set(CMAKE_CXX_FLAGS "${CONAN_STD_CXX_FLAG} ${CMAKE_CXX_FLAGS}")
-            endif()
+                if(CONAN_STD_CXX_FLAG)
+                    conan_message(STATUS "Conan setting CXX_FLAGS flags: ${CONAN_STD_CXX_FLAG}")
+                    set(CONAN_CXX_FLAGS "${CONAN_STD_CXX_FLAG} ${CONAN_CXX_FLAGS}")
+                endif()
             else()
                 if(CONAN_CMAKE_CXX_STANDARD)
                     conan_message(STATUS "Conan setting CPP STANDARD: ${CONAN_CMAKE_CXX_STANDARD} WITH EXTENSIONS ${CONAN_CMAKE_CXX_EXTENSIONS}")
@@ -309,6 +310,7 @@ class CMakeCommonMacros:
 
     conan_set_rpath = textwrap.dedent("""
         macro(conan_set_rpath)
+            conan_message(STATUS "Conan: Adjusting default RPATHs Conan policies")
             if(APPLE)
                 # https://cmake.org/Wiki/CMake_RPATH_handling
                 # CONAN GUIDE: All generated libraries should have the id and dependencies to other
@@ -318,7 +320,7 @@ class CMakeCommonMacros:
                 #     libMyLib0.dylib (compatibility version 0.0.0, current version 0.0.0)
                 #     /usr/lib/libc++.1.dylib (compatibility version 1.0.0, current version 120.0.0)
                 #     /usr/lib/libSystem.B.dylib (compatibility version 1.0.0, current version 1197.1.1)
-                set(CMAKE_SKIP_RPATH 1)  # AVOID RPATH FOR *.dylib, ALL LIBS BETWEEN THEM AND THE EXE
+                set(CMAKE_SKIP_RPATH 1 CACHE BOOL "rpaths")  # AVOID RPATH FOR *.dylib, ALL LIBS BETWEEN THEM AND THE EXE
                                          # SHOULD BE ON THE LINKER RESOLVER PATH (./ IS ONE OF THEM)
                 # Policy CMP0068
                 # We want the old behavior, in CMake >= 3.9 CMAKE_SKIP_RPATH won't affect the install_name in OSX
@@ -779,12 +781,10 @@ macro(conan_basic_setup)
 
     if(NOT ARGUMENTS_SKIP_RPATH AND NOT ARGUMENTS_KEEP_RPATHS)
         # Parameter has renamed, but we keep the compatibility with old SKIP_RPATH
-        conan_message(STATUS "Conan: Adjusting default RPATHs Conan policies")
         conan_set_rpath()
     endif()
 
     if(NOT ARGUMENTS_SKIP_STD)
-        conan_message(STATUS "Conan: Adjusting language standard")
         conan_set_std()
     endif()
 
