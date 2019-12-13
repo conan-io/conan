7ccde620df09cf8b68e06751e20e4a4c46b4b839
javierg <javierg@jfrog.com>
2019-12-13 13:48:29 +0100

    test all multi generators

--- conans/test/functional/generators/multi_generators_test.py
+++ conans/test/functional/generators/multi_generators_test.py
@@ -1,39 +1,24 @@
-
-import os
-import platform
-import unittest
 import textwrap
-from nose.plugins.attrib import attr
+import unittest
+
 from parameterized import parameterized
 
-from conans import MSBuild, tools
-from conans.client.runner import ConanRunner
-from conans.test.utils.conanfile import MockConanfile, MockSettings
-from conans.test.utils.tools import TestClient, TestBufferConanOutput
-from conans.test.utils.visual_project_files import get_vs_project_files
+from conans.test.utils.tools import TestClient
 
 
 class MultiGeneratorsTestCase(unittest.TestCase):
 
-    @parameterized.expand([("cmake_find_package_multi",), ("visual_studio_multi", ), ("cmake_multi", )])
+    @parameterized.expand([("cmake_find_package_multi",),
+                           ("visual_studio_multi", ),
+                           ("cmake_multi", )])
     def test_no_build_type_test(self, generator):
         client = TestClient()
-        client.run("new req/version")
-        client.run("create .")
-
-        cmakelists = textwrap.dedent("""
-            cmake_minimum_required(VERSION 3.1)
-            project(consumer)
-            find_package(req)
-            message(STATUS "hello found: ${{hello_FOUND}}")
-        """)
 
         conanfile = textwrap.dedent("""
             from conans import ConanFile, CMake
 
             class Conan(ConanFile):
                 settings = "os", "arch", "compiler"
-                requires = "req/version"
                 generators = "{generator}"
 
                 def build(self):
@@ -41,6 +26,7 @@ class MultiGeneratorsTestCase(unittest.TestCase):
                     cmake.configure()
         """.format(generator=generator))
 
-        client.save({"conanfile.py": conanfile, "CMakeLists.txt": cmakelists})
-        client.run("install .", assert_error=True)
+        client.save({"conanfile.py": conanfile})
+        client.run('install . -s compiler="Visual Studio"'
+                   ' -s compiler.version=15 -s compiler.toolset=v100', assert_error=True)
         self.assertIn("ERROR: 'settings.build_type' doesn't exist", client.out)
