d6ee8bccdf5351c49b89016fe90f79e29655edda
jgsogo <jgsogo@gmail.com>
2019-12-31 10:24:08 +0100

    writing tests for link ordering #6280
    test for link order
    
    check link order for all generators
    
    add tests
    
    share in class

---
+++ conans/test/functional/generators/link_order_test.py
@@ -0,0 +1,207 @@
+import os
+import platform
+import textwrap
+import unittest
+
+from jinja2 import Template
+
+from conans.model.ref import ConanFileReference
+from conans.test.utils.tools import TestClient
+
+
+class LinkOrderTest(unittest.TestCase):
+    """ Check that the link order of libraries is preserved when using CMake generators
+        https://github.com/conan-io/conan/issues/6280
+    """
+
+    conanfile = Template(textwrap.dedent("""
+        from conans import ConanFile
+        
+        class Recipe(ConanFile):
+            name = "{{ref.name}}"
+            version = "{{ref.version}}"
+            
+            {% if requires %}
+            requires = {% for req in requires %}"{{ req }}"{% if not loop.last %}, {% endif %}{% endfor %}
+            {% endif %}
+            generators = "{{ generator }}"
+            
+            def build(self):
+                with open("lib" + self.name + ".a", "w+") as f:
+                    f.write("fake library content")
+            
+            def package(self):
+                self.copy("*.a", dst="lib")
+                self.copy("*.lib", dst="lib")
+
+            def package_info(self):
+                self.cpp_info.libs = ["{{ ref.name }}"]
+                {% if system_deps %}self.cpp_info.system_deps = [{% for it in system_deps %}"{{ it }}"{% if not loop.last %}, {% endif %}{% endfor %}]{% endif %}
+                {% if extra_libs %}
+                {% for it in extra_libs %}
+                self.cpp_info.libs.append("{{ it }}")
+                {% endfor %}
+                {% endif %}
+    """))
+
+    conanfile_txt = Template(textwrap.dedent("""
+        [requires]
+        {% for it in requires %}
+        {{ it }}
+        {% endfor %}
+        
+        [generators]
+        {{ generator }}
+    """))
+
+    cmakelists = Template(textwrap.dedent("""
+        include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+        conan_basic_setup()
+
+        find_package(Fontconfig REQUIRED)
+
+        add_executable(example example.c)
+        target_link_libraries(example ${CONAN_LIBS})
+        target_link_libraries(example Fontconfig::Fontconfig)
+        set_target_properties(example PROPERTIES C_STANDARD 99)
+    """))
+
+    @classmethod
+    def setUpClass(cls):
+        libA_ref = ConanFileReference.loads("libA/version")
+        libB_ref = ConanFileReference.loads("libB/version")
+        libC_ref = ConanFileReference.loads("libC/version")
+        libD_ref = ConanFileReference.loads("libD/version")
+
+        t = TestClient(path_with_spaces=False)
+        cls._cache_folder = t.cache_folder
+        t.save({
+            'libA/conanfile.py': cls.conanfile.render(ref=libA_ref, generator="cmake_find_package"),
+            'libB/conanfile.py': cls.conanfile.render(ref=libB_ref, generator="cmake_find_package", requires=[libA_ref]),
+            'libC/conanfile.py': cls.conanfile.render(ref=libC_ref, generator="cmake_find_package", requires=[libA_ref]),
+            'libD/conanfile.py': cls.conanfile.render(ref=libD_ref, generator="cmake_find_package", requires=[libB_ref, libC_ref]),
+            'conanfile.txt': cls.conanfile_txt.render(requires=[libD_ref,], generator="cmake_find_package"),
+            'CMakeLists.txt': cls.cmakelists.render(),
+        })
+
+        # Create all of them
+        t.run("create libA")
+        t.run("create libB")
+        t.run("create libC")
+        t.run("create libD")
+
+    @unittest.skipIf(platform.system() != "Darwin", "Xcode is needed")
+    def test_xcode_find_package_multi(self):
+        t = TestClient(cache_folder=self._cache_folder)
+        t.save({
+            'conanfile.txt': textwrap.dedent("""
+                [requires]
+                libD/version
+                [generators]
+                cmake_find_package_multi
+                """),
+            'CMakeLists.txt': textwrap.dedent("""
+                cmake_minimum_required(VERSION 2.8.12)
+                project(executable CXX)
+
+                find_package(libD)
+                add_executable(example main.cpp)
+                target_link_libraries(example libD::libD)
+                """),
+            'main.cpp': textwrap.dedent("""
+                int main() {return 0;}
+                """)
+        })
+
+        t.run("install . -s build_type=Release")
+        t.run_command("cmake . -G Xcode -DCMAKE_PREFIX_PATH=. -DCMAKE_VERBOSE_MAKEFILE:BOOL=True")
+        t.run_command("cmake --build .")
+
+        # Get the actual link order from the CMake call
+        libs = []
+        for line in t.load(os.path.join('executable.xcodeproj', 'project.pbxproj')).splitlines():
+            if 'OTHER_LDFLAGS = " -Wl,-search_paths_first -Wl,-headerpad_max_install_names' in line.strip():
+                _, links = line.split('OTHER_LDFLAGS = " -Wl,-search_paths_first -Wl,-headerpad_max_install_names')
+                if links == '  ";':
+                    continue
+                for it_lib in links.strip().split():
+                    _, libname = it_lib.rsplit('/', 1)
+                    libs.append(libname.strip('";'))
+
+        self.assertListEqual(['liblibD.a', 'liblibB.a', 'liblibC.a', 'liblibA.a'], libs)
+
+    def test_cmake_find_package(self):
+        t = TestClient(cache_folder=self._cache_folder)
+        t.save({
+            'conanfile.txt': textwrap.dedent("""
+                [requires]
+                libD/version
+                [generators]
+                cmake_find_package
+                """),
+            'CMakeLists.txt': textwrap.dedent("""
+                cmake_minimum_required(VERSION 2.8.12)
+                project(executable CXX)
+
+                find_package(libD)
+                add_executable(example main.cpp)
+                target_link_libraries(example libD::libD)
+                """),
+            'main.cpp': textwrap.dedent("""
+                int main() {return 0;}
+                """)
+        })
+
+        t.run("install .")
+        t.run_command("cmake . -DCMAKE_MODULE_PATH=. -DCMAKE_VERBOSE_MAKEFILE:BOOL=True")
+        t.run_command("cmake --build .")
+
+        # Get the actual link order from the CMake call
+        libs = []
+        for it in str(t.out).splitlines():
+            if 'main.cpp.o  -o example' in it:
+                _, links = it.split("main.cpp.o  -o example")
+                for it_lib in links.split():
+                    _, libname = it_lib.rsplit('/', 1)
+                    libs.append(libname)
+
+        self.assertListEqual(['liblibD.a', 'liblibB.a', 'liblibC.a', 'liblibA.a'], libs)
+
+    def test_cmake(self):
+        t = TestClient(cache_folder=self._cache_folder)
+        t.save({
+            'conanfile.txt': textwrap.dedent("""
+                [requires]
+                libD/version
+                [generators]
+                cmake
+                """),
+            'CMakeLists.txt': textwrap.dedent("""
+                cmake_minimum_required(VERSION 2.8.12)
+                project(executable CXX)
+
+                include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+                conan_basic_setup(TARGETS)
+                
+                add_executable(example main.cpp)
+                target_link_libraries(example CONAN_PKG::libD)
+                """),
+            'main.cpp': textwrap.dedent("""
+                int main() {return 0;}
+                """)
+        })
+
+        t.run("install .")
+        t.run_command("cmake . -DCMAKE_MODULE_PATH=. -DCMAKE_VERBOSE_MAKEFILE:BOOL=True")
+        t.run_command("cmake --build .")
+
+        # Get the actual link order from the CMake call
+        libs = []
+        for it in str(t.out).splitlines():
+            if 'main.cpp.o  -o bin/example' in it:
+                _, links = it.split("main.cpp.o  -o bin/example")
+                for it_lib in links.split():
+                    _, libname = it_lib.rsplit('/', 1)
+                    libs.append(libname)
+
+        self.assertListEqual(['liblibD.a', 'liblibB.a', 'liblibC.a', 'liblibA.a'], libs)
