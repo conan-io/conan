cc19f9868fc87d0610206a5ab6f1e2fac5ab8e8b
jgsogo <jgsogo@gmail.com>
2020-01-03 17:11:41 +0100

    add a title per section

--- conans/client/generators/cmake_common.py
+++ conans/client/generators/cmake_common.py
@@ -1,6 +1,9 @@
 import textwrap
 
 _cmake_single_dep_vars = """
+#################
+###  {dep}
+#################
 set(CONAN_{dep}_ROOT{build_type} {deps.rootpath})
 set(CONAN_INCLUDE_DIRS_{dep}{build_type} {deps.include_paths})
 set(CONAN_LIB_DIRS_{dep}{build_type} {deps.lib_paths})
