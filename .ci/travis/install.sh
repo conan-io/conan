ac8b190a3a7ebd2b1c49bcfb98d79d39e1aed328
Rui Chen <chenrui333@gmail.com>
2020-01-21 23:52:38 -0500

    Update PYVER in install.sh

--- .ci/travis/install.sh
+++ .ci/travis/install.sh
@@ -20,24 +20,24 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 
     case "${PYVER}" in
         py27)
-            pyenv install 2.7.10
-            pyenv virtualenv 2.7.10 conan
-            ;;
-        py33)
-            pyenv install 3.3.6
-            pyenv virtualenv 3.3.6 conan
-            ;;
-        py34)
-            pyenv install 3.4.3
-            pyenv virtualenv 3.4.3 conan
+            pyenv install 2.7.17
+            pyenv virtualenv 2.7.17 conan
             ;;
         py35)
-            pyenv install 3.5.0
-            pyenv virtualenv 3.5.0 conan
+            pyenv install 3.5.9
+            pyenv virtualenv 3.5.9 conan
             ;;
         py36)
-            pyenv install 3.6.0
-            pyenv virtualenv 3.6.0 conan
+            pyenv install 3.6.10
+            pyenv virtualenv 3.6.10 conan
+            ;;
+        py37)
+            pyenv install 3.7.6
+            pyenv virtualenv 3.7.6 conan
+            ;;
+        py38)
+            pyenv install 3.8.1
+            pyenv virtualenv 3.8.1 conan
             ;;
 
     esac
