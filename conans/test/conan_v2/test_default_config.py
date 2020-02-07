8149614c1ff0305b314e43bb56d09f3f340c10a7
jgsogo <jgsogo@gmail.com>
2020-02-07 20:35:50 +0100

    need to know a value

--- conans/test/conan_v2/test_default_config.py
+++ conans/test/conan_v2/test_default_config.py
@@ -6,8 +6,10 @@ from conans.test.utils.conan_v2_tests import ConanV2ModeTestCase
 
 class DefaultConfigTestCase(ConanV2ModeTestCase):
     def test_revisions_enabled(self):
-        t = self.get_client()
         import os
+        print("CONAN_V2_MODE_ENVVAR: {}".format(os.environ.get('CONAN_V2_MODE', '-----')))
+        print("TESTING_REVISIONS_ENABLED: {}".format(os.environ.get('TESTING_REVISIONS_ENABLED', '-----')))
+        t = self.get_client()
         c = t.load(os.path.join(t.cache_folder, 'conan.conf'))
         print(c)
         t.run('config get general.revisions_enabled')
