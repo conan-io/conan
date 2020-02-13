f7b24cb2c27b512e1b5a66bccccef490c55b0058
jgsogo <jgsogo@gmail.com>
2020-02-13 18:08:02 +0100

    protect self.cpp_info in package_id()

--- conans/test/conan_v2/conanfile/test_package_id_method.py
+++ conans/test/conan_v2/conanfile/test_package_id_method.py
@@ -3,11 +3,10 @@ import unittest
 
 from conans.test.utils.conan_v2_tests import ConanV2ModeTestCase
 from conans.test.utils.tools import TestClient
-from conans.test.utils.tools import NO_SETTINGS_PACKAGE_ID
 
 
 class ConanfileSourceTestCase(ConanV2ModeTestCase):
-    """ Conan v2: 'self.info' is not available in 'package()' """
+    """ Conan v2: 'self.cpp_info' is not available in 'package_id()' """
 
     def test_proper_usage(self):
         t = self.get_client()
@@ -18,35 +17,38 @@ class ConanfileSourceTestCase(ConanV2ModeTestCase):
             class Recipe(ConanFile):
                 def build(self):
                     pass
-                    
+
                 def package(self):
                     tools.save(os.path.join(self.package_folder, 'file'), "AAA")  # Avoid package() WARN
-                    
-                def package_id(self):
-                    self.info.header_only()
+
+                def package_info(self):
+                    self.cpp_info.libs = ["libA"]
         """)
         t.save({'conanfile.py': conanfile})
         t.run('create . name/version@')
-        self.assertIn("name/version: Package '{}' created".format(NO_SETTINGS_PACKAGE_ID), t.out)
-        self.assertNotIn("WARN", t.out)
 
-    def test_info_not_in_package(self):
-        # self.info is not available in 'package'
+        t.save({'conanfile.txt': "[requires]\nname/version"})
+        t.run('install conanfile.txt')
+        content = t.load('conanbuildinfo.txt')
+        self.assertIn("[libs]\nlibA", content)
+
+    def test_cppinfo_not_in_package_id(self):
+        # self.cpp_info is not available in 'package_id'
         t = self.get_client()
         conanfile = textwrap.dedent("""
             from conans import ConanFile
 
             class Recipe(ConanFile):
-            
-                def package(self):
-                    self.info.header_only()
+
+                def package_id(self):
+                    self.cpp_info.libs = ["A"]
         """)
         t.save({'conanfile.py': conanfile})
         t.run('create . name/version@ -s os=Linux', assert_error=True)
-        self.assertIn("Conan v2 incompatible: 'self.info' access in package() method is deprecated", t.out)
+        self.assertIn("Conan v2 incompatible: 'self.cpp_info' access in package_id() method is deprecated", t.out)
 
 
-class ConanfileSourceV1TestCase(unittest.TestCase):
+class ConanfilePackageIdV1TestCase(unittest.TestCase):
     """ Conan v1 will show a warning """
 
     def test_v1_warning(self):
@@ -55,10 +57,11 @@ class ConanfileSourceV1TestCase(unittest.TestCase):
             from conans import ConanFile
 
             class Recipe(ConanFile):
-            
-                def package(self):
-                    self.info.header_only()  # No sense, it will warn the user
+
+                def package_id(self):
+                    self.cpp_info.libs = ["A"]  # No sense, it will warn the user
         """)
         t.save({'conanfile.py': conanfile})
-        t.run('create . name/version@')
-        self.assertIn("name/version: WARN: 'self.info' access in package() method is deprecated", t.out)
+        t.run('create . name/version@', assert_error=True)  # It is already raising
+        self.assertIn("AttributeError: 'NoneType' object has no attribute 'libs'", t.out)
+        # self.assertIn("name/version: WARN: 'self.info' access in package() method is deprecated", t.out)
