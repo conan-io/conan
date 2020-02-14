ca356a81b2beecef496e58adf3fa80fd7f0cdd6e
James <james@conan.io>
2020-02-14 09:44:01 +0100

    Manage the dirty state in "conan export-pkg" command (#6517)
    * solved conflicts
    
    * fix test

--- conans/client/cmd/export_pkg.py
+++ conans/client/cmd/export_pkg.py
@@ -5,7 +5,7 @@ from conans.client.graph.graph_manager import load_deps_info
 from conans.errors import ConanException
 from conans.model.conan_file import get_env_context_manager
 from conans.model.ref import PackageReference
-from conans.util.files import rmdir
+from conans.util.files import rmdir, set_dirty_context_manager
 
 
 def export_pkg(app, recorder, full_ref, source_folder, build_folder, package_folder, install_folder,
@@ -45,14 +45,17 @@ def export_pkg(app, recorder, full_ref, source_folder, build_folder, package_fol
     recipe_hash = layout.recipe_manifest().summary_hash
     conanfile.info.recipe_hash = recipe_hash
     conanfile.develop = True
-    if package_folder:
-        prev = packager.export_pkg(conanfile, package_id, package_folder, dest_package_folder,
-                                   hook_manager, conan_file_path, ref)
-    else:
-        with get_env_context_manager(conanfile):
-            prev = packager.run_package_method(conanfile, package_id, source_folder, build_folder,
-                                               dest_package_folder, install_folder, hook_manager,
-                                               conan_file_path, ref, local=True)
+
+    with set_dirty_context_manager(dest_package_folder):
+        if package_folder:
+            prev = packager.export_pkg(conanfile, package_id, package_folder, dest_package_folder,
+                                       hook_manager, conan_file_path, ref)
+        else:
+            # FIXME: Remove this get_env_context_manager() when merging to master
+            with get_env_context_manager(conanfile):
+                prev = packager.run_package_method(conanfile, package_id, source_folder, build_folder,
+                                                   dest_package_folder, install_folder, hook_manager,
+                                                   conan_file_path, ref, local=True)
 
     packager.update_package_metadata(prev, layout, package_id, full_ref.revision)
     pref = PackageReference(pref.ref, pref.id, prev)
