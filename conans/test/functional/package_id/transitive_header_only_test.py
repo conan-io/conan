c589e2cbc11e26f4a213733d00a7f63a00647232
memsharded <james@conan.io>
2020-02-20 12:42:23 +0100

    renamed and documented test

--- conans/test/functional/package_id/transitive_header_only_test.py
+++ conans/test/functional/package_id/transitive_header_only_test.py
@@ -29,8 +29,12 @@ class TransitiveIdsTest(unittest.TestCase):
         self.assertIn("libc/1.0:4599994f77257f88443590022af4754d5520b753 - Missing", client.out)
         self.assertIn("libd/1.0:39906c34335d9ad465711e847688c4a27894af0f - Build", client.out)
 
-    def transitive_minor_mode_test(self):
+    def transitive_major_mode_test(self):
         # https://github.com/conan-io/conan/issues/6450
+        # Test LibE->LibD->LibC->LibB->LibA
+        # LibC declares that it only depends on major version changes of its upstream
+        # So LibC package ID doesn't change, even if LibA changes
+        # But LibD package ID changes, even if its direct dependency LibC doesn't
         client = TestClient()
         client.run("config set general.default_package_id_mode=full_version_mode")
         # LibA
