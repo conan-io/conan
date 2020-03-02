d981d47020b8c0257697c7e428191187d6e4157e
Javier G. Sogo <jgsogo@gmail.com>
2020-03-02 16:25:05 +0100

    br_host with all its closure, propagate to env_info the path to libs

--- conans/client/installer.py
+++ conans/client/installer.py
@@ -474,8 +474,12 @@ class BinaryInstaller(object):
         # List sort is stable, will keep the original order of the closure, but prioritize levels
         conan_file = node.conanfile
         transitive = [it for it in node.transitive_closure.values()]
-        br_host = [it.dst for it in node.dependencies
-                   if it.require.build_require_context == CONTEXT_HOST]
+
+        br_host = []
+        for it in node.dependencies:
+            if it.require.build_require_context == CONTEXT_HOST:
+                br_host.extend(it.dst.transitive_closure.values())
+
         for n in node_order:
             if n not in transitive:
                 conan_file.output.info("Applying build-requirement: %s" % str(n.ref))
@@ -488,7 +492,14 @@ class BinaryInstaller(object):
                 if n in transitive or n in br_host:
                     conan_file.deps_cpp_info.update(n.conanfile.cpp_info, n.ref.name)
                 else:
-                    conan_file.deps_env_info.update(n.conanfile.env_info, n.ref.name)
+                    env_info = EnvInfo()
+                    env_info._values_ = n.conanfile.env_info._values_.copy()
+                    # Add cpp_info.bin_paths/lib_paths to env_info (it is needed for runtime)
+                    env_info.DYLD_LIBRARY_PATH.extend(n.conanfile.cpp_info.lib_paths)
+                    env_info.DYLD_LIBRARY_PATH.extend(n.conanfile.cpp_info.framework_paths)
+                    env_info.LD_LIBRARY_PATH.extend(n.conanfile.cpp_info.lib_paths)
+                    env_info.PATH.extend(n.conanfile.cpp_info.bin_paths)
+                    conan_file.deps_env_info.update(env_info, n.ref.name)
 
         # Update the info but filtering the package values that not apply to the subtree
         # of this current node and its dependencies.
