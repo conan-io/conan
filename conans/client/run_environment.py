32d4587e323830ddfa319155dbb22eb47bacd632
Javier G. Sogo <jgsogo@gmail.com>
2020-03-03 13:09:38 +0100

    changes not needed now

--- conans/client/run_environment.py
+++ conans/client/run_environment.py
@@ -15,7 +15,9 @@ class RunEnvironment(object):
 
     @property
     def vars(self):
-        lib_paths = bin_paths = framework_paths = []
+        lib_paths = []
+        bin_paths = []
+        framework_paths = []
         for dep in self.conanfile.deps_cpp_info.deps:
             lib_paths.extend(self.conanfile.deps_cpp_info[dep].lib_paths)
             bin_paths.extend(self.conanfile.deps_cpp_info[dep].bin_paths)
@@ -26,13 +28,5 @@ class RunEnvironment(object):
                "PATH": bin_paths}
         if framework_paths:
             ret["DYLD_FRAMEWORK_PATH"] = framework_paths
+
         return ret
-
-
-class RunBuildEnvironment(object):
-    def __init__(self, conanfile):
-        self.conanfile = conanfile
-
-    @property
-    def vars(self):
-        return self.conanfile.deps_env_info.vars
