3f33ece3b889d76b7c4de3144722a48d0f815505
danimtb <danimanzaneque@gmail.com>
2020-03-11 14:30:30 +0100

    Simplify DefaultOrderedDict

--- conans/model/build_info.py
+++ conans/model/build_info.py
@@ -1,6 +1,5 @@
 import os
-from collections import defaultdict, OrderedDict, Callable
-from copy import deepcopy
+from collections import OrderedDict
 
 import deprecation
 
@@ -13,48 +12,19 @@ DEFAULT_BUILD = ""
 DEFAULT_FRAMEWORK = "Frameworks"
 
 
-class DefaultOrderedDict(OrderedDict):
-    # Source: http://stackoverflow.com/a/6190500/562769
-    def __init__(self, default_factory=None, *a, **kw):
-        if (default_factory is not None and
-           not isinstance(default_factory, Callable)):
-            raise TypeError('first argument must be callable')
-        OrderedDict.__init__(self, *a, **kw)
-        self.default_factory = default_factory
+class DefaultOrderedDict(object):
+
+    def __init__(self, factory):
+        self.factory = factory
+        self.ordered_dict = OrderedDict()
 
     def __getitem__(self, key):
-        try:
-            return OrderedDict.__getitem__(self, key)
-        except KeyError:
-            return self.__missing__(key)
+        if key not in self.ordered_dict.keys():
+            self.ordered_dict[key] = self.factory()
+        return self.ordered_dict[key]
 
-    def __missing__(self, key):
-        if self.default_factory is None:
-            raise KeyError(key)
-        self[key] = value = self.default_factory()
-        return value
-
-    def __reduce__(self):
-        if self.default_factory is None:
-            args = tuple()
-        else:
-            args = self.default_factory,
-        return type(self), args, None, None, self.items()
-
-    def copy(self):
-        return self.__copy__()
-
-    def __copy__(self):
-        return type(self)(self.default_factory, self)
-
-    def __deepcopy__(self, memo):
-        import copy
-        return type(self)(self.default_factory,
-                          copy.deepcopy(self.items()))
-
-    def __repr__(self):
-        return 'DefaultOrderedDict(%s, %s)' % (self.default_factory,
-                                               OrderedDict.__repr__(self))
+    def __getattr__(self, item):
+        return getattr(self.ordered_dict, item)
 
 
 class _CppInfo(object):
@@ -311,7 +281,6 @@ class DepCppInfo(object):
         return [s for s in seq1 if s not in seq2] + seq2
 
     def _aggregated_values(self, item):
-
         if getattr(self, "_%s" % item) is None:
             values = getattr(self._cpp_info, item)
             for _, component in self._cpp_info.components.items():
