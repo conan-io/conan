bdb256ee1b456f3467d2b3f824de890f6c004bbd
jgsogo <jgsogo@gmail.com>
2020-03-19 10:22:21 +0100

    decode for py2

--- conans/client/runner.py
+++ conans/client/runner.py
@@ -96,7 +96,7 @@ class ConanRunner(object):
                 if log_handler:
                     # Write decoded in PY2 causes some ASCII encoding problems
                     # tried to open the log_handler binary but same result.
-                    log_handler.write(line if six.PY2 else decoded_line)
+                    log_handler.write(line.decode('utf-8') if six.PY2 else decoded_line)
 
         get_stream_lines(proc.stdout)
         # get_stream_lines(proc.stderr)
