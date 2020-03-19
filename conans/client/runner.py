432e5706b650c07efe3b805b9e046d786b3b3130
jgsogo <jgsogo@gmail.com>
2020-03-19 09:35:39 +0100

    use io.open(..., encoding=utf-8) to open log file

--- conans/client/runner.py
+++ conans/client/runner.py
@@ -1,7 +1,6 @@
 import io
 import os
 import sys
-
 from subprocess import PIPE, Popen, STDOUT
 
 import six
@@ -63,7 +62,7 @@ class ConanRunner(object):
             elif log_filepath:
                 if stream_output:
                     stream_output.write("Logging command output to file '%s'\n" % log_filepath)
-                with open(log_filepath, "a+") as log_handler:
+                with io.open(log_filepath, "a+", encoding="utf-8") as log_handler:
                     if self._print_commands_to_output:
                         log_handler.write(call_message)
                     return self._pipe_os_call(command, stream_output, log_handler, cwd)
