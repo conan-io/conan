7862db63487bae545cf05cc977c6da43a1911600
Uilian Ries <uilianries@gmail.com>
2020-03-24 10:21:08 -0300

    #6349 Add support for mirror when downloading files (#6679)
    * #6349 Tools download supports mirror
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Fix exceptions for connection
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Add mirrors for ftp_download
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * Revert "#6349 Add mirrors for ftp_download"
    
    This reverts commit f554ed8d92a0b0b79f8121d5b2967a23cc0dc5b7.
    
    * #6349 Add mirrors for tools.get
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Avoid key error for checksums
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Update error message for testing
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Update download tests for mirrors
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Use same checksum for all mirrors
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Remove slow tag for mirrors test
    
    Co-Authored-By: Javier G. Sogo <jgsogo@gmail.com>
    
    * #6349 Update error message when all fail
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Fix code indent
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Update assert message for cache test
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Fix download test
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Consider tuple as possible list for urls
    
    Co-Authored-By: Javier G. Sogo <jgsogo@gmail.com>
    
    * #6349 catch ConanException only
    
    Co-Authored-By: Javier G. Sogo <jgsogo@gmail.com>
    
    * #6349 Improve error messages
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 remove unused imports
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Fix exception error for url test
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #6349 Update mirror error to warning level
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>

--- conans/client/tools/net.py
+++ conans/client/tools/net.py
@@ -12,10 +12,13 @@ def get(url, md5='', sha1='', sha256='', destination=".", filename="", keep_perm
         overwrite=False, auth=None, headers=None):
     """ high level downloader + unzipper + (optional hash checker) + delete temporary zip
     """
-    if not filename and ("?" in url or "=" in url):
-        raise ConanException("Cannot deduce file name from url. Use 'filename' parameter.")
 
-    filename = filename or os.path.basename(url)
+    url = [url] if not isinstance(url, (list, tuple)) else url
+    if not filename and ("?" in url[0] or "=" in url[0]):
+        raise ConanException("Cannot deduce file name from the url: '{}'. Use 'filename' "
+                             "parameter.".format(url[0]))
+    filename = filename or os.path.basename(url[0])
+
     download(url, filename, out=output, requester=requester, verify=verify, retry=retry,
              retry_wait=retry_wait, overwrite=overwrite, auth=auth, headers=headers,
              md5=md5, sha1=sha1, sha256=sha256)
@@ -49,7 +52,30 @@ def ftp_download(ip, filename, login='', password=''):
 
 def download(url, filename, verify=True, out=None, retry=None, retry_wait=None, overwrite=False,
              auth=None, headers=None, requester=None, md5='', sha1='', sha256=''):
+    """Retrieves a file from a given URL into a file with a given filename.
+       It uses certificates from a list of known verifiers for https downloads,
+       but this can be optionally disabled.
 
+    :param url: URL to download. It can be a list, which only the first one will be downloaded, and
+                the follow URLs will be used as mirror in case of download error.
+    :param filename: Name of the file to be created in the local storage
+    :param verify: When False, disables https certificate validation
+    :param out: An object with a write() method can be passed to get the output. stdout will use if
+                not specified
+    :param retry: Number of retries in case of failure. Default is overriden by general.retry in the
+                  conan.conf file or an env variable CONAN_RETRY
+    :param retry_wait: Seconds to wait between download attempts. Default is overriden by
+                       general.retry_wait in the conan.conf file or an env variable CONAN_RETRY_WAIT
+    :param overwrite: When True, Conan will overwrite the destination file if exists. Otherwise it
+                      will raise an exception
+    :param auth: A tuple of user and password to use HTTPBasic authentication
+    :param headers: A dictionary with additional headers
+    :param requester: HTTP requests instance
+    :param md5: MD5 hash code to check the downloaded file
+    :param sha1: SHA-1 hash code to check the downloaded file
+    :param sha256: SHA-256 hash code to check the downloaded file
+    :return: None
+    """
     out = default_output(out, 'conans.client.tools.net.download')
     requester = default_requester(requester, 'conans.client.tools.net.download')
     from conans.tools import _global_config as config
@@ -60,21 +86,38 @@ def download(url, filename, verify=True, out=None, retry=None, retry_wait=None, 
     retry_wait = retry_wait if retry_wait is not None else config.retry_wait
     retry_wait = retry_wait if retry_wait is not None else 5
 
-    downloader = FileDownloader(requester=requester, output=out, verify=verify, config=config)
+    url = [url] if not isinstance(url, (list, tuple)) else url
     checksum = sha256 or sha1 or md5
-    # The download cache is only used if a checksum is provided, otherwise, a normal download
+
+    downloader = FileDownloader(requester=requester, output=out, verify=verify, config=config)
     if config and config.download_cache and checksum:
         downloader = CachedFileDownloader(config.download_cache, downloader, user_download=True)
-        downloader.download(url, filename, retry=retry, retry_wait=retry_wait, overwrite=overwrite,
-                            auth=auth, headers=headers, md5=md5, sha1=sha1, sha256=sha256)
-    else:
-        downloader.download(url, filename, retry=retry, retry_wait=retry_wait, overwrite=overwrite,
-                            auth=auth, headers=headers)
-        if md5:
-            check_md5(filename, md5)
-        if sha1:
-            check_sha1(filename, sha1)
-        if sha256:
-            check_sha256(filename, sha256)
 
-    out.writeln("")
+    def _download_file(downloader, url):
+        # The download cache is only used if a checksum is provided, otherwise, a normal download
+        if isinstance(downloader, CachedFileDownloader):
+            downloader.download(url, filename, retry=retry, retry_wait=retry_wait,
+                                overwrite=overwrite, auth=auth, headers=headers, md5=md5,
+                                sha1=sha1, sha256=sha256)
+        else:
+            downloader.download(url, filename, retry=retry, retry_wait=retry_wait,
+                                overwrite=overwrite, auth=auth, headers=headers)
+            if md5:
+                check_md5(filename, md5)
+            if sha1:
+                check_sha1(filename, sha1)
+            if sha256:
+                check_sha256(filename, sha256)
+        out.writeln("")
+
+    for index, url_it in enumerate(url):
+        try:
+            _download_file(downloader, url_it)
+            break
+        except ConanException as error:
+            message = "Could not download from the URL {}: {}.".format(url_it, str(error))
+            if len(url) == 1:
+                raise ConanException(message)
+            out.warn(message + " Trying another mirror.")
+            if (index + 1) == len(url):
+                raise ConanException("All downloads from ({}) URLs have failed.".format(len(url)))
