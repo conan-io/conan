23feeabf07e7edbc7d8adaf6d16a1376e77732ea
jgsogo <jgsogo@gmail.com>
2020-04-02 17:37:15 +0200

    v2 will be string, v1 keeps being a Version

--- conans/test/functional/conanfile/version_type_test.py
+++ conans/test/functional/conanfile/version_type_test.py
@@ -15,13 +15,8 @@ class VersionTypeTestCase(unittest.TestCase):
         from conans.model.version import Version
 
         class Recipe(ConanFile):
-            {% if not use_set_name and name %}name = {{ name }}{% endif %}
+            name = "name"
             {% if not use_set_version and version %}version = {{ version }}{% endif %}
-
-            {% if use_set_name %}
-            def set_name(self):
-                self.name = {{ name }}
-            {% endif %}
 
             {% if use_set_version %}
             def set_version(self):
@@ -29,13 +24,6 @@ class VersionTypeTestCase(unittest.TestCase):
             {% endif %}
 
             def _checks(self, function_name):
-                name_is_str = isinstance(self.name, six.string_types)
-                if name_is_str:
-                    self.output.info("> {}: name ok".format(function_name))
-                else:
-                    name_type = type(self.name)
-                    raise ConanException("> {}: name is type '{}'".format(function_name, name_type))
-
                 version_is_str = isinstance(self.version, six.string_types)
                 if version_is_str:
                     self.output.info("> {}: version ok".format(function_name))
@@ -101,12 +89,10 @@ class VersionTypeTestCase(unittest.TestCase):
         for func in ("source", "build", "package", "package_info", "configure", "config_options",
                      "requirements", "build_requirements", "system_requirements", "imports",
                      "package_id", "build_id", ):  # TODO: `init()` not here
-            self.assertIn("> {}: name ok".format(func), t.out)
             self.assertIn("> {}: version ok".format(func), t.out)
 
         ref_install = ref_install or ref_create
         t.run("install {} -g deploy".format(ref_install))
-        self.assertIn("> deploy: name ok", t.out)
         self.assertIn("> deploy: version ok", t.out)
 
     def test_cli(self):
@@ -114,25 +100,21 @@ class VersionTypeTestCase(unittest.TestCase):
         self._run_testing(conanfile, ref_create="name/42@")
 
     def test_recipe_attribute(self):
-        conanfile = self.conanfile_tpl.render(name='"name"', version='"42"')
+        conanfile = self.conanfile_tpl.render(version='"42"')
         self._run_testing(conanfile, ref_create="name/42@")
 
-        conanfile = self.conanfile_tpl.render(name='"name"', version='42')
+        conanfile = self.conanfile_tpl.render(version='42')
         self._run_testing(conanfile, ref_create=None, ref_install="name/42@")
 
-        conanfile = self.conanfile_tpl.render(name='"name"', version='42.23')
+        conanfile = self.conanfile_tpl.render(version='42.23')
         self._run_testing(conanfile, ref_create=None, ref_install="name/42.23@")
 
     def test_set_functions(self):
-        conanfile = self.conanfile_tpl.render(name='"name"', version='"42"',
-                                              use_set_name=True, use_set_version=True)
+        conanfile = self.conanfile_tpl.render(version='"42"', use_set_version=True)
         self._run_testing(conanfile, ref_create="name/42@")
 
-        conanfile = self.conanfile_tpl.render(name='"name"', version='42',
-                                              use_set_name=True, use_set_version=True)
+        conanfile = self.conanfile_tpl.render(version='42', use_set_version=True)
         self._run_testing(conanfile, ref_create=None, ref_install="name/42@")
 
-        conanfile = self.conanfile_tpl.render(name='"name"', version='42.23',
-                                              use_set_name=True, use_set_version=True)
+        conanfile = self.conanfile_tpl.render(version='42.23', use_set_version=True)
         self._run_testing(conanfile, ref_create=None, ref_install="name/42.23@")
-
