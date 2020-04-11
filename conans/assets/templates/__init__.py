b9d991153cd127ad37a1e381f4eccff2414fd84b
jgsogo <jgsogo@gmail.com>
2020-04-11 16:29:01 +0200

    templates for the CLI output go to 'output' folder

--- conans/assets/templates/__init__.py
+++ conans/assets/templates/__init__.py
@@ -3,7 +3,7 @@ from jinja2 import DictLoader
 
 import conans.assets.templates.search_table_html
 
-SEARCH_TABLE_HTML = 'search_table.html'
+SEARCH_TABLE_HTML = 'output/search_table.html'
 
 
 dict_loader = DictLoader({
