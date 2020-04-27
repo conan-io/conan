32ad28e8d5b7a733a8b95d24b724023d60f8e1af
Uilian Ries <uilianries@gmail.com>
2020-04-27 12:02:59 -0300

    #6811 Rename sched file to avoid conflicts
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/client/cache/cache.py
+++ conans/client/cache/cache.py
@@ -27,7 +27,7 @@ LOCALDB = ".conan.db"
 REMOTES = "remotes.json"
 PROFILES_FOLDER = "profiles"
 HOOKS_FOLDER = "hooks"
-SCHED_FILE = '.sched'
+SCHED_FILE = '.conan_config_install_sched'
 
 
 def is_case_insensitive_os():
