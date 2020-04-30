f0d9a141e25c7bd7fcf54e84e5988f458fb9569c
Javier G. Sogo <jgsogo@gmail.com>
2020-04-30 13:18:23 +0200

    run tools.unix_path only in Windows

--- conans/client/tools/win.py
+++ conans/client/tools/win.py
@@ -577,6 +577,9 @@ def unix_path(path, path_flavor=None):
     if not path:
         return None
 
+    if not OSInfo().is_windows:
+        return path
+
     if os.path.exists(path):
         path = get_cased_path(path)  # if the path doesn't exist (and abs) we cannot guess the casing
 
