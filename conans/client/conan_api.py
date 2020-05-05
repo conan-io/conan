dc075efc3fb6d8e02a15f92501c3750527117ea7
Marco Studerus <marco.studerus2@leica-geosystems.com>
2020-05-05 17:15:44 +0200

    add `conan config init` command * remove return

--- conans/client/conan_api.py
+++ conans/client/conan_api.py
@@ -615,7 +615,6 @@ class ConanAPIV1(object):
     def config_init(self):
         self.app.cache.registry.initialize_remotes()
         self.app.cache.settings
-        return
 
     def _info_args(self, reference_or_path, install_folder, profile_host, profile_build, lockfile=None):
         cwd = get_cwd()
