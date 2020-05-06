5464eddfac7449ed20ec82809ea29a7d5c984310
Andrey Upadyshev <andreyu@amazon.com>
2020-05-06 23:17:50 +0200

    cmake_paths generator appends Conan paths to CMAKE_MODULE_PATH and CMAKE_PREFIX_PATH rather than overrides those variables
    It is not unrare when CMAKE_MODULE_PATH and/or CMAKE_PREFIX_PATH are passed
    to CMake via command line or set by CMake initial cache script thus it's
    less intrusive if cmake_paths generator appends Conan paths to aforementioned
    variables rather than overrides them.

--- conans/client/generators/cmake_paths.py
+++ conans/client/generators/cmake_paths.py
@@ -25,9 +25,9 @@ class CMakePathsGenerator(Generator):
         #    if the user used the "cmake_find_package" will find the auto-generated
         # 4. The CMake installation dir/Modules ones.
         deps = DepsCppCmake(self.deps_build_info)
-        lines.append("set(CMAKE_MODULE_PATH {deps.build_paths} ${{CMAKE_MODULE_PATH}} "
+        lines.append("list(APPEND CMAKE_MODULE_PATH {deps.build_paths} ${{CMAKE_MODULE_PATH}} "
                      "${{CMAKE_CURRENT_LIST_DIR}})".format(deps=deps))
-        lines.append("set(CMAKE_PREFIX_PATH {deps.build_paths} ${{CMAKE_PREFIX_PATH}} "
+        lines.append("list(APPEND CMAKE_PREFIX_PATH {deps.build_paths} ${{CMAKE_PREFIX_PATH}} "
                      "${{CMAKE_CURRENT_LIST_DIR}})".format(deps=deps))
 
         return "\n".join(lines)
