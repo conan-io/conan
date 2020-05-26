48598f626327d1a1990cb900a3e2841bdf3513c0
memsharded <james@conan.io>
2020-05-26 11:03:37 +0200

    started cli 2.0

---
+++ conans/cli/command.py
@@ -0,0 +1,254 @@
+import argparse
+import inspect
+import signal
+import sys
+import textwrap
+from difflib import get_close_matches
+
+
+from conans import __version__ as client_version
+from conans.client.conan_api import Conan
+from conans.client.output import Color
+from conans.errors import ConanException, ConanInvalidConfiguration,  ConanMigrationError
+from conans.util.files import exception_message_safe
+from conans.util.log import logger
+
+
+# Exit codes for conan command:
+SUCCESS = 0                         # 0: Success (done)
+ERROR_GENERAL = 1                   # 1: General ConanException error (done)
+ERROR_MIGRATION = 2                 # 2: Migration error
+USER_CTRL_C = 3                     # 3: Ctrl+C
+USER_CTRL_BREAK = 4                 # 4: Ctrl+Break
+ERROR_SIGTERM = 5                   # 5: SIGTERM
+ERROR_INVALID_CONFIGURATION = 6     # 6: Invalid configuration (done)
+
+
+class SmartFormatter(argparse.HelpFormatter):
+
+    def _fill_text(self, text, width, indent):
+        text = textwrap.dedent(text)
+        return ''.join(indent + line for line in text.splitlines(True))
+
+
+class Command(object):
+    """A single command of the conan application, with all the first level commands. Manages the
+    parsing of parameters and delegates functionality in collaborators. It can also show the
+    help of the tool.
+    """
+    def __init__(self, conan_api):
+        assert isinstance(conan_api, Conan)
+        self._conan = conan_api
+        self._out = conan_api.out
+
+    def help(self, *args):
+        """
+        Shows help for a specific command.
+        """
+        parser = argparse.ArgumentParser(description=self.help.__doc__,
+                                         prog="conan help",
+                                         formatter_class=SmartFormatter)
+        parser.add_argument("command", help='command', nargs="?")
+        args = parser.parse_args(*args)
+        if not args.command:
+            self._show_help()
+            return
+        try:
+            commands = self._commands()
+            method = commands[args.command]
+            method(["--help"])
+        except KeyError:
+            raise ConanException("Unknown command '%s'" % args.command)
+
+    def search(self, *args):
+        """
+        Searches package recipes
+        """
+        parser = argparse.ArgumentParser(description=self.search.__doc__,
+                                         prog="conan search",
+                                         formatter_class=SmartFormatter)
+        parser.add_argument('pattern', help="pattern for matching package recipes")
+        parser.add_argument('-r', '--remote', action="append", nargs='?',
+                            help="Remote to search")
+        parser.add_argument('-c', '--cache', action="store_true", help="Search in the local cache")
+        args = parser.parse_args(*args)
+        info = self._conan.search_recipes(args.pattern, remote_name=args.remote)
+
+    def _show_help(self):
+        """
+        Prints a summary of all commands.
+        """
+        grps = [("Consumer commands", ("search", )),
+                ("Misc commands", ("help", ))]
+
+        def check_all_commands_listed():
+            """Keep updated the main directory, raise if don't"""
+            all_commands = self._commands()
+            all_in_grps = [command for _, command_list in grps for command in command_list]
+            if set(all_in_grps) != set(all_commands):
+                diff = set(all_commands) - set(all_in_grps)
+                raise Exception("Some command is missing in the main help: %s" % ",".join(diff))
+            return all_commands
+
+        commands = check_all_commands_listed()
+        max_len = max((len(c) for c in commands)) + 1
+        fmt = '  %-{}s'.format(max_len)
+
+        for group_name, comm_names in grps:
+            self._out.writeln(group_name, Color.BRIGHT_MAGENTA)
+            for name in comm_names:
+                # future-proof way to ensure tabular formatting
+                self._out.write(fmt % name, Color.GREEN)
+
+                # Help will be all the lines up to the first empty one
+                docstring_lines = commands[name].__doc__.split('\n')
+                start = False
+                data = []
+                for line in docstring_lines:
+                    line = line.strip()
+                    if not line:
+                        if start:
+                            break
+                        start = True
+                        continue
+                    data.append(line)
+
+                txt = textwrap.fill(' '.join(data), 80, subsequent_indent=" "*(max_len+2))
+                self._out.writeln(txt)
+
+        self._out.writeln("")
+        self._out.writeln('Conan commands. Type "conan <command> -h" for help', Color.BRIGHT_YELLOW)
+
+    def _commands(self):
+        """ Returns a list of available commands.
+        """
+        result = {}
+        for m in inspect.getmembers(self, predicate=inspect.ismethod):
+            method_name = m[0]
+            if not method_name.startswith('_'):
+                if "export_pkg" == method_name:
+                    method_name = "export-pkg"
+                method = m[1]
+                if method.__doc__ and not method.__doc__.startswith('HIDDEN'):
+                    result[method_name] = method
+        return result
+
+    def _print_similar(self, command):
+        """ Looks for similar commands and prints them if found.
+        """
+        matches = get_close_matches(
+            word=command, possibilities=self._commands().keys(), n=5, cutoff=0.75)
+
+        if len(matches) == 0:
+            return
+
+        if len(matches) > 1:
+            self._out.writeln("The most similar commands are")
+        else:
+            self._out.writeln("The most similar command is")
+
+        for match in matches:
+            self._out.writeln("    %s" % match)
+
+        self._out.writeln("")
+
+    def run(self, *args):
+        """HIDDEN: entry point for executing commands, dispatcher to class
+        methods
+        """
+        version = sys.version_info
+        if version.major == 2 or version.minor <= 4:
+            raise ConanException("Unsupported Python version")
+
+        ret_code = SUCCESS
+        try:
+            try:
+                command = args[0][0]
+            except IndexError:  # No parameters
+                self._show_help()
+                return False
+            try:
+                commands = self._commands()
+                method = commands[command]
+            except KeyError as exc:
+                if command in ["-v", "--version"]:
+                    self._out.success("Conan version %s" % client_version)
+                    return False
+
+                if command in ["-h", "--help"]:
+                    self._show_help()
+                    return False
+
+                self._out.writeln("'%s' is not a Conan command. See 'conan --help'." % command)
+                self._out.writeln("")
+                self._print_similar(command)
+                raise ConanException("Unknown command %s" % str(exc))
+
+            method(args[0][1:])
+        except KeyboardInterrupt as exc:
+            logger.error(exc)
+            ret_code = SUCCESS
+        except SystemExit as exc:
+            if exc.code != 0:
+                logger.error(exc)
+                self._out.error("Exiting with code: %d" % exc.code)
+            ret_code = exc.code
+        except ConanInvalidConfiguration as exc:
+            ret_code = ERROR_INVALID_CONFIGURATION
+            self._out.error(exc)
+        except ConanException as exc:
+            ret_code = ERROR_GENERAL
+            self._out.error(exc)
+        except Exception as exc:
+            import traceback
+            print(traceback.format_exc())
+            ret_code = ERROR_GENERAL
+            msg = exception_message_safe(exc)
+            self._out.error(msg)
+
+        return ret_code
+
+
+def main(args):
+    """ main entry point of the conan application, using a Command to
+    parse parameters
+
+    Exit codes for conan command:
+
+        0: Success (done)
+        1: General ConanException error (done)
+        2: Migration error
+        3: Ctrl+C
+        4: Ctrl+Break
+        5: SIGTERM
+        6: Invalid configuration (done)
+    """
+    try:
+        conan_api, _, _ = Conan.factory()
+    except ConanMigrationError:  # Error migrating
+        sys.exit(ERROR_MIGRATION)
+    except ConanException as e:
+        sys.stderr.write("Error in Conan initialization: {}".format(e))
+        sys.exit(ERROR_GENERAL)
+
+    def ctrl_c_handler(_, __):
+        print('You pressed Ctrl+C!')
+        sys.exit(USER_CTRL_C)
+
+    def sigterm_handler(_, __):
+        print('Received SIGTERM!')
+        sys.exit(ERROR_SIGTERM)
+
+    def ctrl_break_handler(_, __):
+        print('You pressed Ctrl+Break!')
+        sys.exit(USER_CTRL_BREAK)
+
+    signal.signal(signal.SIGINT, ctrl_c_handler)
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
+    if sys.platform == 'win32':
+        signal.signal(signal.SIGBREAK, ctrl_break_handler)
+
+    command = Command(conan_api)
+    error = command.run(args)
+    sys.exit(error)
--- conans/conan.py
+++ conans/conan.py
@@ -1,10 +1,15 @@
+import os
 import sys
 
 from conans.client.command import main
+from conans.cli.command import main
 
 
 def run():
-    main(sys.argv[1:])
+    if os.getenv("CONAN_V2_CLI"):
+        main(sys.argv[1:])
+    else:
+        main(sys.argv[1:])
 
 
 if __name__ == '__main__':
