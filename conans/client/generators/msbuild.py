379c16e769cdbf03205c7555c4c4a77e96f881f7
James <james@conan.io>
2020-05-26 23:35:34 +0200

    Update conans/client/generators/msbuild.py
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>

--- conans/client/generators/msbuild.py
+++ conans/client/generators/msbuild.py
@@ -22,7 +22,7 @@ class MSBuildGenerator(Generator):
         <?xml version="1.0" encoding="utf-8"?>
         <Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
           <ImportGroup Label="PropertySheets">
-          {transitive_imports}
+        {transitive_imports}
           </ImportGroup>
           <PropertyGroup Label="UserMacros" />
           <PropertyGroup>
