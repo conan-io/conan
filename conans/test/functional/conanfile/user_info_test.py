f3fb851ccf2a2f1a2ad7c9a075a8e43573b33d63
James <james@conan.io>
2020-06-02 17:22:39 +0200

    fix user_info getattr (#7131)

--- conans/test/functional/conanfile/user_info_test.py
+++ conans/test/functional/conanfile/user_info_test.py
@@ -1,4 +1,5 @@
 import os
+import textwrap
 import unittest
 
 from conans.paths import CONANFILE
@@ -8,25 +9,20 @@ from conans.test.utils.tools import TestClient
 class UserInfoTest(unittest.TestCase):
 
     def test_user_info_propagation(self):
-
         client = TestClient()
 
         def export_lib(name, requires, infolines):
-            base = '''
-import os
-from conans import ConanFile
+            base = textwrap.dedent("""
+                from conans import ConanFile
 
-class MyConanfile(ConanFile):
-    name = "%s"
-    version = "0.1"
-    requires = "%s"
+                class MyConanfile(ConanFile):
+                    name = "%s"
+                    version = "0.1"
+                    requires = "%s"
 
-    def build(self):
-        pass
-
-    def package_info(self):
-        %s
-    '''
+                    def package_info(self):
+                        %s
+                    """)
             client.save({CONANFILE: base % (name, requires, infolines)}, clean_first=True)
             client.run("export . lasote/stable")
 
@@ -36,24 +32,27 @@ class MyConanfile(ConanFile):
         export_lib("LIB_C", "LIB_B/0.1@lasote/stable", "self.user_info.VAR1=2")
         export_lib("LIB_D", "LIB_C/0.1@lasote/stable", "self.user_info.var1=2")
 
-        reuse = '''
-import os
-from conans import ConanFile
+        reuse = textwrap.dedent("""
+            from conans import ConanFile
 
-class MyConanfile(ConanFile):
-    name = "reuse"
-    version = "0.1"
-    requires = "LIB_D/0.1@lasote/stable"
+            class MyConanfile(ConanFile):
+                requires = "LIB_D/0.1@lasote/stable"
 
-    def build(self):
-        assert(self.deps_user_info["LIB_A"].VAR1=="2")
-        assert(self.deps_user_info["LIB_B"].VAR1=="2")
-        assert(self.deps_user_info["LIB_B"].VAR2=="3")
-        assert(self.deps_user_info["LIB_C"].VAR1=="2")
-        assert(self.deps_user_info["LIB_D"].var1=="2")
-    '''
+                def build(self):
+                    assert(self.deps_user_info["LIB_A"].VAR1=="2")
+                    assert(self.deps_user_info["LIB_B"].VAR1=="2")
+                    assert(self.deps_user_info["LIB_B"].VAR2=="3")
+                    assert(self.deps_user_info["LIB_C"].VAR1=="2")
+                    assert(self.deps_user_info["LIB_D"].var1=="2")
+                    # Idiomatic way to check for attribute existence
+                    # https://github.com/conan-io/conan/issues/7130
+                    assert(hasattr(self.deps_user_info["LIB_A"], "VAR1"))
+                    assert(not hasattr(self.deps_user_info["LIB_A"], "NONEXIST"))
+                    assert(getattr(self.deps_user_info["LIB_A"], "VAR1"))
+                    assert(not getattr(self.deps_user_info["LIB_A"], "NONEXIST", None))
+                """)
         client.save({CONANFILE: reuse}, clean_first=True)
-        client.run("export . lasote/stable")
+        client.run("export . reuse/0.1@lasote/stable")
         client.run('install reuse/0.1@lasote/stable --build -g txt')
 
         # Assert generator TXT
