cbc88e00db5d8caee756365131f810fbf4dad6f4
memsharded <james@conan.io>
2020-06-07 23:25:17 +0200

    develop broken due to settings migration

--- conans/client/migrations_settings.py
+++ conans/client/migrations_settings.py
@@ -1192,4 +1192,5 @@ build_type: [None, Debug, Release, RelWithDebInfo, MinSizeRel]
 cppstd: [None, 98, gnu98, 11, gnu11, 14, gnu14, 17, gnu17, 20, gnu20]  # Deprecated, use compiler.cppstd
 """
 
-settings_1_25_2=settings_1_25_1
+settings_1_25_2 = settings_1_25_1
+settings_1_26_0 = settings_1_25_2
