80713533fb6e291c3bae098040c8af64222c2841
jgsogo <jgsogo@gmail.com>
2020-06-15 18:41:13 +0200

    add test

--- conans/test/functional/build_helpers/cmake_test.py
+++ conans/test/functional/build_helpers/cmake_test.py
@@ -1,7 +1,6 @@
 import textwrap
 import unittest
 
-
 from conans.test.utils.tools import TestClient
 
 
@@ -17,3 +16,17 @@ class CMakeBuildHelper(unittest.TestCase):
         client.save({"conanfile.py": conanfile})
         client.run("create . pkg/0.1@user/testing")
         self.assertIn("CMAKE_VERSION: 3", client.out)
+
+    def test_inheriting_cmake_build_helper(self):
+        # https://github.com/conan-io/conan/issues/7196
+        base = textwrap.dedent("""
+            import conans
+            class CMake(conans.CMake):
+                pass
+            class BaseConanFile(conans.ConanFile):
+                pass
+            """)
+        client = TestClient()
+        client.save({"conanfile.py": base})
+        client.run("export . pkg/0.1@")
+        self.assertIn("pkg/0.1: Exported revision", client.out)
