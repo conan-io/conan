6a0bbe7a1e5b6ccf0e3c46729381c4cb91364a44
czoido <mrgalleta@gmail.com>
2020-06-18 08:32:36 +0200

    move to help formatter

--- conans/cli/commands/help.py
+++ conans/cli/commands/help.py
@@ -1,7 +1,6 @@
 import argparse
-import textwrap
 
-from conans.client.output import Color
+from conans.client.formatters.help_formatter import HelpFormatter
 from conans.errors import ConanException
 from conans.cli.command import SmartFormatter, conan_command
 
@@ -16,43 +15,10 @@ def help(conan_api, commands, groups, *args):
     parser.add_argument("command", help='command', nargs="?")
     args = parser.parse_args(*args)
     if not args.command:
-        _show_help(conan_api.out, commands, groups)
+        HelpFormatter.out("cli", conan_api.out, commands, groups)
         return
     try:
         method = commands[args.command].method
         method(conan_api, ["--help"])
     except KeyError:
         raise ConanException("Unknown command '%s'" % args.command)
-
-
-def _show_help(out, commands, groups):
-    """
-    Prints a summary of all commands.
-    """
-    max_len = max((len(c) for c in commands)) + 1
-    fmt = '  %-{}s'.format(max_len)
-
-    for group_name, comm_names in groups.items():
-        out.writeln(group_name, Color.BRIGHT_MAGENTA)
-        for name in comm_names:
-            # future-proof way to ensure tabular formatting
-            out.write(fmt % name, Color.GREEN)
-
-            # Help will be all the lines up to the first empty one
-            docstring_lines = commands[name].doc.split('\n')
-            start = False
-            data = []
-            for line in docstring_lines:
-                line = line.strip()
-                if not line:
-                    if start:
-                        break
-                    start = True
-                    continue
-                data.append(line)
-
-            txt = textwrap.fill(' '.join(data), 80, subsequent_indent=" " * (max_len + 2))
-            out.writeln(txt)
-
-    out.writeln("")
-    out.writeln('Conan commands. Type "conan <command> -h" for help', Color.BRIGHT_YELLOW)
---
+++ conans/client/formatters/help_formatter.py
@@ -0,0 +1,40 @@
+import textwrap
+
+from conans.client.formatters.base_formatter import BaseFormatter
+from conans.client.output import Color
+
+
+class HelpFormatter(BaseFormatter):
+
+    @classmethod
+    def cli(cls, out, commands, groups):
+        """
+        Prints a summary of all commands.
+        """
+        max_len = max((len(c) for c in commands)) + 1
+        fmt = '  %-{}s'.format(max_len)
+
+        for group_name, comm_names in groups.items():
+            out.writeln(group_name, Color.BRIGHT_MAGENTA)
+            for name in comm_names:
+                # future-proof way to ensure tabular formatting
+                out.write(fmt % name, Color.GREEN)
+
+                # Help will be all the lines up to the first empty one
+                docstring_lines = commands[name].doc.split('\n')
+                start = False
+                data = []
+                for line in docstring_lines:
+                    line = line.strip()
+                    if not line:
+                        if start:
+                            break
+                        start = True
+                        continue
+                    data.append(line)
+
+                txt = textwrap.fill(' '.join(data), 80, subsequent_indent=" " * (max_len + 2))
+                out.writeln(txt)
+
+        out.writeln("")
+        out.writeln('Conan commands. Type "conan <command> -h" for help', Color.BRIGHT_YELLOW)
