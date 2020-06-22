d09e363e3656345d5e2c8951410c2cc2700d3b1d
Luis Martinez de Bartolome <lasote@gmail.com>
2020-06-22 17:30:49 +0200

    nowrap

--- conans/assets/templates/search_table_html.py
+++ conans/assets/templates/search_table_html.py
@@ -6,6 +6,11 @@ content = """
         <title>Conan | {{ search.reference }}</title>
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap.min.css"/>
+        <style>
+            tr td {
+                white-space:nowrap;
+            }
+        </style>
     </head>
     <body>
         <div class="container-fluid">
