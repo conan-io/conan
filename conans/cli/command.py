11bffc7dd8d4e076571ac7452d65efc4574b1aff
czoido <mrgalleta@gmail.com>
2020-06-25 08:28:39 +0200

    allow commands with empty desc

--- conans/cli/command.py
+++ conans/cli/command.py
@@ -78,7 +78,7 @@ class ConanCommand(object):
         self._group = group or "Misc commands"
         self._name = method.__name__.replace("_", "-")
         self._method = method
-        self._doc = method.__doc__
+        self._doc = method.__doc__ or "Empty description"
         self._parser = argparse.ArgumentParser(description=self._doc,
                                                prog="conan {}".format(self._name),
                                                formatter_class=SmartFormatter)
