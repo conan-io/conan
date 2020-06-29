0da5ac3e5516a058f33abbb92f3db9baacf172ef
jgsogo <jgsogo@gmail.com>
2020-06-29 16:49:46 +0200

    refactor  tests

--- conans/test/functional/cross_building/test_package_test.py
+++ conans/test/functional/cross_building/test_package_test.py
@@ -2,6 +2,7 @@ import textwrap
 import unittest
 
 from jinja2 import Template
+from parameterized import parameterized
 
 from conans.client.tools import save
 from conans.test.utils.tools import TestClient
@@ -40,60 +41,38 @@ class TestPackageTestCase(unittest.TestCase):
             Build:
     """)
 
-    def setUp(self):
-        self.t = TestClient()
-        save(self.t.cache.settings_path, self.settings_yml)
-        self.t.save({'br.py': self.conanfile_br,
-                     'conanfile.py': self.conanfile,
-                     'test_package/conanfile.py': self.conanfile_test,
-                     'profile_host': '[settings]\nos=Host',
-                     'profile_build': '[settings]\nos=Build', })
-        self.t.run("export br.py br1/version@")
-        self.t.run("export br.py br2/version@")
+    @parameterized.expand([("create conanfile.py name/version@",),
+                           ("test test_package/conanfile.py name/version@",)])
+    def test_command(self, command):
+        t = TestClient()
+        save(t.cache.settings_path, self.settings_yml)
+        t.save({'br.py': self.conanfile_br,
+                'conanfile.py': self.conanfile,
+                'test_package/conanfile.py': self.conanfile_test,
+                'profile_host': '[settings]\nos=Host',
+                'profile_build': '[settings]\nos=Build', })
+        t.run("export br.py br1/version@")
+        t.run("export br.py br2/version@")
+        t.run("export conanfile.py name/version@")
 
-    def test_command_create(self):
-        self.t.run("create . name/version@ --build=missing"
-                   " --profile:host=profile_host --profile:build=profile_build")
-
-        # Build requires are built in the 'build' context:
-        self.assertIn("br1/version: >> settings.os: Build", self.t.out)
-        self.assertIn("br1/version: >> settings_build.os: Build", self.t.out)
-        self.assertIn("br1/version: >> tools.get_env('INFO'): None", self.t.out)
-
-        self.assertIn("br2/version: >> settings.os: Build", self.t.out)
-        self.assertIn("br2/version: >> settings_build.os: Build", self.t.out)
-        self.assertIn("br2/version: >> tools.get_env('INFO'): None", self.t.out)
-
-        # Package 'name' is built for the 'host' context (br1 as build_requirement)
-        self.assertIn("name/version: >> settings.os: Host", self.t.out)
-        self.assertIn("name/version: >> settings_build.os: Build", self.t.out)
-        self.assertIn("name/version: >> tools.get_env('INFO'): br1-Build", self.t.out)
-
-        # Test_package is executed with the same profiles as the package itself
-        self.assertIn("name/version (test package): >> settings.os: Host", self.t.out)
-        self.assertIn("name/version (test package): >> settings_build.os: Build", self.t.out)
-        self.assertIn("name/version (test package): >> tools.get_env('INFO'): br2-Build", self.t.out)
-
-    def test_command_test(self):
-        self.t.run("export . name/version@")
-        self.t.run("test test_package/conanfile.py name/version@ --build=missing"
-                   " --profile:host=profile_host --profile:build=profile_build")
+        # Execute the actual command we are testing
+        t.run(command + " --build=missing --profile:host=profile_host --profile:build=profile_build")
 
         # Build requires are built in the 'build' context:
-        self.assertIn("br1/version: >> settings.os: Build", self.t.out)
-        self.assertIn("br1/version: >> settings_build.os: Build", self.t.out)
-        self.assertIn("br1/version: >> tools.get_env('INFO'): None", self.t.out)
+        self.assertIn("br1/version: >> settings.os: Build", t.out)
+        self.assertIn("br1/version: >> settings_build.os: Build", t.out)
+        self.assertIn("br1/version: >> tools.get_env('INFO'): None", t.out)
 
-        self.assertIn("br2/version: >> settings.os: Build", self.t.out)
-        self.assertIn("br2/version: >> settings_build.os: Build", self.t.out)
-        self.assertIn("br2/version: >> tools.get_env('INFO'): None", self.t.out)
+        self.assertIn("br2/version: >> settings.os: Build", t.out)
+        self.assertIn("br2/version: >> settings_build.os: Build", t.out)
+        self.assertIn("br2/version: >> tools.get_env('INFO'): None", t.out)
 
         # Package 'name' is built for the 'host' context (br1 as build_requirement)
-        self.assertIn("name/version: >> settings.os: Host", self.t.out)
-        self.assertIn("name/version: >> settings_build.os: Build", self.t.out)
-        self.assertIn("name/version: >> tools.get_env('INFO'): br1-Build", self.t.out)
+        self.assertIn("name/version: >> settings.os: Host", t.out)
+        self.assertIn("name/version: >> settings_build.os: Build", t.out)
+        self.assertIn("name/version: >> tools.get_env('INFO'): br1-Build", t.out)
 
         # Test_package is executed with the same profiles as the package itself
-        self.assertIn("name/version (test package): >> settings.os: Host", self.t.out)
-        self.assertIn("name/version (test package): >> settings_build.os: Build", self.t.out)
-        self.assertIn("name/version (test package): >> tools.get_env('INFO'): br2-Build", self.t.out)
+        self.assertIn("name/version (test package): >> settings.os: Host", t.out)
+        self.assertIn("name/version (test package): >> settings_build.os: Build", t.out)
+        self.assertIn("name/version (test package): >> tools.get_env('INFO'): br2-Build", t.out)
