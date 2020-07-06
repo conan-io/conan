1c90c62eb0e98f6cae241145f639145cb0bea31e
James <james@conan.io>
2020-07-06 16:54:20 +0200

    adding system libs to scons generator (#7302)
    * adding system libs to scons generator
    
    * adding frameworks
    
    * Update conans/test/unittests/client/generators/scons_test.py
    
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>
    
    * Update conans/test/unittests/client/generators/scons_test.py
    
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>
    
    * Update conans/test/unittests/client/generators/scons_test.py
    
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>
    
    * using jinja2
    
    * removed unused libs
    
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>

--- conans/client/generators/scons.py
+++ conans/client/generators/scons.py
@@ -1,3 +1,7 @@
+import textwrap
+
+from jinja2 import Template
+
 from conans.model import Generator
 
 
@@ -8,36 +12,43 @@ class SConsGenerator(Generator):
 
     @property
     def content(self):
-        template = ('    "{dep}" : {{\n'
-                    '        "CPPPATH"     : {info.include_paths},\n'
-                    '        "LIBPATH"     : {info.lib_paths},\n'
-                    '        "BINPATH"     : {info.bin_paths},\n'
-                    '        "LIBS"        : {info.libs},\n'
-                    '        "CPPDEFINES"  : {info.defines},\n'
-                    '        "CXXFLAGS"    : {info.cxxflags},\n'
-                    '        "CCFLAGS"     : {info.cflags},\n'
-                    '        "SHLINKFLAGS" : {info.sharedlinkflags},\n'
-                    '        "LINKFLAGS"   : {info.exelinkflags},\n'
-                    '    }},\n'
-                    '    "{dep}_version" : "{info.version}",\n')
+        template = textwrap.dedent("""\
+            "{{dep}}" : {
+                "CPPPATH"     : {{info.include_paths}},
+                "LIBPATH"     : {{info.lib_paths}},
+                "BINPATH"     : {{info.bin_paths}},
+                {% set libs = info.libs + info.system_libs -%}
+                "LIBS"        : [{% if libs %}'{{ libs|join("', '") }}'{% endif %}],
+                "FRAMEWORKS"  : {{info.frameworks}},
+                "FRAMEWORKPATH"  : {{info.framework_paths}},
+                "CPPDEFINES"  : {{info.defines}},
+                "CXXFLAGS"    : {{info.cxxflags}},
+                "CCFLAGS"     : {{info.cflags}},
+                "SHLINKFLAGS" : {{info.sharedlinkflags}},
+                "LINKFLAGS"   : {{info.exelinkflags}},
+            },
+            "{{dep}}_version" : "{{info.version}}",
+            """)
+        # Nice nested indent
+        template = "\n".join("    " + line for line in template.splitlines())
+        template = Template(template)
 
-        sections = []
-        sections.append("conan = {\n")
+        sections = ["conan = {\n"]
 
-        all_flags = template.format(dep="conan", info=self.deps_build_info)
+        all_flags = template.render(dep="conan", info=self.deps_build_info)
         sections.append(all_flags)
 
         for config, cpp_info in self.deps_build_info.configs.items():
-            all_flags = template.format(dep="conan:" + config, info=cpp_info)
+            all_flags = template.render(dep="conan:" + config, info=cpp_info)
             sections.append(all_flags)
 
         for dep_name, info in self.deps_build_info.dependencies:
             dep_name = dep_name.replace("-", "_")
-            dep_flags = template.format(dep=dep_name, info=info)
+            dep_flags = template.render(dep=dep_name, info=info)
             sections.append(dep_flags)
 
             for config, cpp_info in info.configs.items():
-                all_flags = template.format(dep=dep_name + ":" + config, info=cpp_info)
+                all_flags = template.render(dep=dep_name + ":" + config, info=cpp_info)
                 sections.append(all_flags)
 
         sections.append("}\n")
