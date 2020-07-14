2b645ebe4e5e379b166fdc61fe4c38384081ef08
memsharded <james@conan.io>
2020-07-15 01:53:54 +0200

    fix test

--- conans/test/functional/conanfile/min_version_test.py
+++ conans/test/functional/conanfile/min_version_test.py
@@ -19,11 +19,11 @@ class MinVersionTest(unittest.TestCase):
             """)
         client.save({"conanfile.py": conanfile})
         client.run("export . pkg/1.0@", assert_error=True)
-        self.assertIn("minimum version: 100.0.0 > %s" % __version__, client.out)
+        self.assertIn("minimum required Conan version: 100.0.0 > %s" % __version__, client.out)
         client.run("inspect . ", assert_error=True)
-        self.assertIn("minimum version: 100.0.0 > %s" % __version__, client.out)
+        self.assertIn("minimum required Conan version: 100.0.0 > %s" % __version__, client.out)
         with mock.patch("conans.client.loader.current_version", Version("101.0")):
             client.run("export . pkg/1.0@")
 
         client.run("install pkg/1.0@", assert_error=True)
-        self.assertIn("minimum version: 100.0.0 > %s" % __version__, client.out)
+        self.assertIn("minimum required Conan version: 100.0.0 > %s" % __version__, client.out)
