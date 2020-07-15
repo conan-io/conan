e558c21e4e3bb46e17ff8fa45c09ee52a3bdaa32
Javier G. Sogo <jgsogo@gmail.com>
2020-07-15 13:57:32 +0200

    Do not fail with 'conan remove -r remote -p' if there are no packages in the remote (#7338)
    * remove only if there are packages to remove
    
    * working on v1 too
    
    * no need to scape an r-string
    
    * v1 -> no revs
    
    * if not conaninfo, search doesn't work
    
    * propagate 404 only if there are packages for the reference
    
    * Update conans/client/rest/rest_client_v1.py
    
    Co-authored-by: James <james@conan.io>
    
    * when it is not none
    
    * package_ids is always a list. Capture unrelated errors
    
    Co-authored-by: James <james@conan.io>

--- conans/client/rest/rest_client_v1.py
+++ conans/client/rest/rest_client_v1.py
@@ -1,6 +1,7 @@
 import os
 import time
 import traceback
+from collections import namedtuple
 
 from six.moves.urllib.parse import parse_qs, urljoin, urlparse, urlsplit
 
@@ -304,12 +305,21 @@ class RestV1Methods(RestCommonMethods):
         return self._post_json(url, payload)
 
     @handle_return_deserializer()
-    def remove_packages(self, ref, package_ids=None):
+    def remove_packages(self, ref, package_ids):
         """ Remove any packages specified by package_ids"""
         self.check_credentials()
         payload = {"package_ids": package_ids}
         url = self.router.remove_packages(ref)
-        return self._post_json(url, payload)
+        ret = self._post_json(url, payload)
+        if not package_ids and ret.status_code == 404:
+            # Double check if it is a 404 because there are no packages
+            try:
+                if not self.search_packages(ref, query=None):
+                    return namedtuple("_", ['status_code', 'content'])(200, b'')
+            except Exception as e:
+                logger.warning("Unexpected error searching {} packages"
+                               " in remote {}: {}".format(ref, self.remote_url, e))
+        return ret
 
     @handle_return_deserializer()
     def remove_conanfile(self, ref):
