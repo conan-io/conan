c66c5675ac70ad0535d0934500f5c13514579a05
memsharded <james@conan.io>
2020-07-28 11:15:23 +0200

    working

--- conans/client/toolchain/base.py
+++ conans/client/toolchain/base.py
@@ -5,19 +5,19 @@ from conans.errors import conanfile_exception_formatter, ConanException
 
 def write_toolchain(conanfile, path, output):
     if hasattr(conanfile, "toolchain"):
+        output.highlight("Generating toolchain files")
         if callable(conanfile.toolchain):
             # This is the toolchain
             with chdir(path):
                 with conanfile_exception_formatter(str(conanfile), "toolchain"):
-                    tc = conanfile.toolchain()
+                    conanfile.toolchain()
         else:
             try:
                 toolchain = {"cmake": CMakeToolchain}[conanfile.toolchain]
             except KeyError:
                 raise ConanException("Unknown toolchain '%s'" % conanfile.toolchain)
             tc = toolchain(conanfile)
-        output.highlight("Generating toolchain files")
-        if tc is not None:  # Allow the toolchain method to not return
-            tc.create_toolchain_files(path)
+            with chdir(path):
+                tc.write_toolchain_files()
 
         # TODO: Lets discuss what to do with the environment
