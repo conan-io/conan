bcf74b8cd849131b4a34f388f0bb39c8626d832a
Kai Dohmen <psykai1993@googlemail.com>
2020-09-21 08:51:20 +0200

    Add Depends item for every public_dep of the conanfiles deps

--- conans/client/generators/qbs.py
+++ conans/client/generators/qbs.py
@@ -26,6 +26,8 @@ class DepsCppQbs(object):
 
 
 class QbsGenerator(Generator):
+    name = "qbs"
+
     @property
     def filename(self):
         return BUILD_INFO_QBS
@@ -48,17 +50,24 @@ class QbsGenerator(Generator):
                     '            cpp.cxxFlags: [{deps.cxxflags}]\n'
                     '            cpp.cFlags: [{deps.cflags}]\n'
                     '            cpp.linkerFlags: [{deps.linkerFlags}]\n'
+                    '{depends_items}'
                     '        }}\n'
                     '    }}\n')
 
+        depends_template = ('            Depends {{ name: "{dep}" }}\n')
+
         sections = []
-        all_flags = template.format(dep="ConanBasicSetup", deps=deps)
+        all_flags = template.format(dep="ConanBasicSetup", deps=deps, depends_items="")
         sections.append(all_flags)
         template_deps = template + '    // {dep} root path: {deps.rootpath}\n'
 
         for dep_name, dep_cpp_info in self.deps_build_info.dependencies:
             deps = DepsCppQbs(dep_cpp_info)
-            dep_flags = template_deps.format(dep=dep_name, deps=deps)
+            depends_items = ""
+            for public_dep in dep_cpp_info.public_deps:
+                name = self.deps_build_info[public_dep].get_name(QbsGenerator.name)
+                depends_items += depends_template.format(dep=name)
+            dep_flags = template_deps.format(dep=dep_name, deps=deps, depends_items=depends_items)
             sections.append(dep_flags)
 
         output = 'import qbs 1.0\n\nProject {\n'
