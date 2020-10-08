c9077e7941d1c2f56d1a5a43c292055d8e487aa0
jgsogo <jgsogo@gmail.com>
2020-10-08 20:19:19 +0200

    use the power of jinja2

--- conans/client/toolchain/cmake/base.py
+++ conans/client/toolchain/cmake/base.py
@@ -1,7 +1,9 @@
 import os
+import textwrap
 from collections import OrderedDict, defaultdict
 
-from jinja2 import Template
+from jinja2 import DictLoader
+from jinja2 import Environment
 
 from conans.util.files import save
 
@@ -33,8 +35,94 @@ class CMakeToolchainBase(object):
     filename = "conan_toolchain.cmake"
     project_include_filename = "conan_project_include.cmake"
 
-    _template_project_include = None
-    _template_toolchain = None
+    _toolchain_macros_tpl = textwrap.dedent("""
+        {% macro iterate_configs(var_config, action) -%}
+            {% for it, values in var_config.items() -%}
+                {%- set genexpr = namespace(str='') %}
+                {%- for conf, value in values -%}
+                    {%- set genexpr.str = genexpr.str +
+                                          '$<IF:$<CONFIG:' + conf + '>,"' + value|string + '",' %}
+                    {%- if loop.last %}{% set genexpr.str = genexpr.str + '""' -%}{%- endif -%}
+                {%- endfor -%}
+                {% for i in range(values|count) %}{%- set genexpr.str = genexpr.str + '>' %}{%- endfor -%}
+                {% if action=='set' %}
+                set({{ it }} {{ genexpr.str }})
+                {% elif action=='add_definitions' %}
+                add_definitions(-D{{ it }}={{ genexpr.str }})
+                {% endif %}
+            {%- endfor %}
+        {% endmacro %}
+        """)
+
+    _base_toolchain_tpl = textwrap.dedent("""
+        {% import 'toolchain_macros' as toolchain_macros %}
+
+        # Conan automatically generated toolchain file
+        # DO NOT EDIT MANUALLY, it will be overwritten
+
+        # Avoid including toolchain file several times (bad if appending to variables like
+        #   CMAKE_CXX_FLAGS. See https://github.com/android/ndk/issues/323
+        if(CONAN_TOOLCHAIN_INCLUDED)
+          return()
+        endif()
+        set(CONAN_TOOLCHAIN_INCLUDED TRUE)
+
+        {% block before_try_compile %}
+        {% endblock %}
+
+        get_property( _CMAKE_IN_TRY_COMPILE GLOBAL PROPERTY IN_TRY_COMPILE )
+        if(_CMAKE_IN_TRY_COMPILE)
+            message(STATUS "Running toolchain IN_TRY_COMPILE")
+            return()
+        endif()
+
+        message("Using Conan toolchain through ${CMAKE_TOOLCHAIN_FILE}.")
+
+        {% if conan_project_include_cmake %}
+        if(CMAKE_VERSION VERSION_LESS "3.15")
+            message(WARNING
+                " CMake version less than 3.15 doesn't support CMAKE_PROJECT_INCLUDE variable\\n"
+                " used by Conan toolchain to work. In order to get the same behavior you will\\n"
+                " need to manually include the generated file after your 'project()' call in the\\n"
+                " main CMakeLists.txt file:\\n"
+                " \\n"
+                "     project(YourProject C CXX)\\n"
+                "     include(\\"\\${CMAKE_BINARY_DIR}/conan_project_include.cmake\\")\\n"
+                " \\n"
+                " This file contains some definitions and extra adjustments that depend on\\n"
+                " the build_type and it cannot be done in the toolchain.")
+        else()
+            # Will be executed after the 'project()' command
+            set(CMAKE_PROJECT_INCLUDE "{{ conan_project_include_cmake }}")
+        endif()
+        {% endif %}
+
+        {% block main %}
+        {% endblock %}
+
+        # Install prefix
+        {% if install_prefix -%}
+        set(CMAKE_INSTALL_PREFIX {{install_prefix}} CACHE STRING "" FORCE)
+        {%- endif %}
+
+        # Variables
+        {% for it, value in variables.items() -%}
+        set({{ it }} "{{ value }}")
+        {%- endfor %}
+        # Variables  per configuration
+        {{ toolchain_macros.iterate_configs(variables_config, action='set') }}
+
+        # Preprocessor definitions
+        {% for it, value in preprocessor_definitions.items() -%}
+        # add_compile_definitions only works in cmake >= 3.12
+        add_definitions(-D{{ it }}="{{ value }}")
+        {%- endfor %}
+        # Preprocessor definitions per configuration
+        {{ toolchain_macros.iterate_configs(preprocessor_definitions_config, action='add_definitions') }}
+
+        {% block footer %}
+        {% endblock %}
+        """)
 
     def __init__(self, conanfile, *args, **kwargs):
         self._conanfile = conanfile
@@ -46,6 +134,12 @@ class CMakeToolchainBase(object):
         except AttributeError:
             # FIXME: In the local flow, we don't know the package_folder
             self.install_prefix = None
+
+    def _get_templates(self):
+        return {
+            'toolchain_macros': self._toolchain_macros_tpl,
+            'base_toolchain': self._base_toolchain_tpl
+        }
 
     def _get_template_context_data(self):
         """ Returns two dictionaries, the context for the '_template_toolchain' and
@@ -61,15 +155,21 @@ class CMakeToolchainBase(object):
         return ctxt_toolchain, {}
 
     def write_toolchain_files(self):
+        # Prepare templates to be loaded
+        dict_loader = DictLoader(self._get_templates())
+        env = Environment(loader=dict_loader)
+
         ctxt_toolchain, ctxt_project_include = self._get_template_context_data()
+        if ctxt_project_include:
+            # Make it absolute, wrt to current folder, set by the caller
+            conan_project_include_cmake = os.path.abspath(self.project_include_filename)
+            conan_project_include_cmake = conan_project_include_cmake.replace("\\", "/")
+            t = env.get_template(self.project_include_filename)
+            content = t.render(**ctxt_project_include)
+            save(conan_project_include_cmake, content)
 
-        # Make it absolute, wrt to current folder, set by the caller
-        conan_project_include_cmake = os.path.abspath(self.project_include_filename)
-        conan_project_include_cmake = conan_project_include_cmake.replace("\\", "/")
-        t = Template(self._template_project_include)
-        content = t.render(**ctxt_project_include)
-        save(conan_project_include_cmake, content)
+            ctxt_toolchain.update({'conan_project_include_cmake': conan_project_include_cmake})
 
-        t = Template(self._template_toolchain)
-        content = t.render(conan_project_include_cmake=conan_project_include_cmake, **ctxt_toolchain)
+        t = env.get_template(self.filename)
+        content = t.render(**ctxt_toolchain)
         save(self.filename, content)
