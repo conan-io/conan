00aecf792e2f8539e81634988088f5d11e099b5a
jgsogo <jgsogo@gmail.com>
2020-10-08 11:18:05 +0200

    skip x86-x64 as cross-compile for Windows and Linux

--- conans/client/toolchain/cmake/__init__.py
+++ conans/client/toolchain/cmake/__init__.py
@@ -1,16 +1,17 @@
 from conans.client.tools import cross_building
-from .native import CMakeNativeToolchain
+from conans.errors import ConanException
 from .android import CMakeAndroidToolchain
+from .native import CMakeNativeToolchain
 
 
 def CMakeToolchain(conanfile, *args, **kwargs):
-    if not cross_building(conanfile):
+    skip_x64_x86 = conanfile.settings.os in ['Windows', 'Linux']
+    if not cross_building(conanfile, skip_x64_x86=skip_x64_x86):
         return CMakeNativeToolchain(conanfile, *args, **kwargs)
     else:
-        # Exceptions to cross-building scenarios
-        if conanfile.settings.os == 'Windows' and conanfile.settings_build.os == 'Windows':
-            return CMakeNativeToolchain(conanfile, *args, **kwargs)
-
-        # Actual cross-building
         if conanfile.settings.os == 'Android':
             return CMakeAndroidToolchain(conanfile, *args, **kwargs)
+        else:
+            # TODO: We need to provide a way to inject a toolchain from the outside
+            raise ConanException("No toolchain available to cross-compile"
+                                 " to 'os={}'".format(conanfile.settings.os))
