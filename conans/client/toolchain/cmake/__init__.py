fc16e3fbcda962eb412bc540c131ea5e49d264da
jgsogo <jgsogo@gmail.com>
2020-10-08 11:58:49 +0200

    not all settings has os

--- conans/client/toolchain/cmake/__init__.py
+++ conans/client/toolchain/cmake/__init__.py
@@ -5,13 +5,13 @@ from .native import CMakeNativeToolchain
 
 
 def CMakeToolchain(conanfile, *args, **kwargs):
-    skip_x64_x86 = conanfile.settings.os in ['Windows', 'Linux']
+    skip_x64_x86 = conanfile.settings.get_safe('os') in ['Windows', 'Linux']
     if not cross_building(conanfile, skip_x64_x86=skip_x64_x86):
         return CMakeNativeToolchain(conanfile, *args, **kwargs)
     else:
-        if conanfile.settings.os == 'Android':
+        os_ = conanfile.settings.get_safe('os')
+        if os_ == 'Android':
             return CMakeAndroidToolchain(conanfile, *args, **kwargs)
         else:
             # TODO: We need to provide a way to inject a toolchain from the outside
-            raise ConanException("No toolchain available to cross-compile"
-                                 " to 'os={}'".format(conanfile.settings.os))
+            raise ConanException("No toolchain available to cross-compile to 'os={}'".format(os_))
