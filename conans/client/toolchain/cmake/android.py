30cc0f378a600415981dd629f5358344d3a46309
jgsogo <jgsogo@gmail.com>
2020-10-20 12:16:25 +0200

    android using templates branch

--- conans/client/toolchain/cmake/android.py
+++ conans/client/toolchain/cmake/android.py
@@ -8,33 +8,28 @@ class CMakeAndroidToolchain(CMakeToolchainBase):
         {% extends 'base_toolchain' %}
 
         {% block main %}
-        {{ super() }}
+            {% if shared_libs -%}
+            message(STATUS "Conan toolchain: Setting BUILD_SHARED_LIBS= {{ shared_libs }}")
+            set(BUILD_SHARED_LIBS {{ shared_libs }})
+            {%- endif %}
 
-        # shared libs
-        {% if shared_libs -%}
-        message(STATUS "Conan toolchain: Setting BUILD_SHARED_LIBS= {{ shared_libs }}")
-        set(BUILD_SHARED_LIBS {{ shared_libs }})
-        {%- endif %}
+            {% if parallel -%}
+            set(CONAN_CXX_FLAGS "${CONAN_CXX_FLAGS} {{ parallel }}")
+            set(CONAN_C_FLAGS "${CONAN_C_FLAGS} {{ parallel }}")
+            {%- endif %}
 
-        # Parallel builds
-        {% if parallel -%}
-        set(CONAN_CXX_FLAGS "${CONAN_CXX_FLAGS} {{ parallel }}")
-        set(CONAN_C_FLAGS "${CONAN_C_FLAGS} {{ parallel }}")
-        {%- endif %}
-
-        # C++ Standard
-        {% if cppstd -%}
-        message(STATUS "Conan C++ Standard {{ cppstd }} with extensions {{ cppstd_extensions }}}")
-        set(CMAKE_CXX_STANDARD {{ cppstd }})
-        set(CMAKE_CXX_EXTENSIONS {{ cppstd_extensions }})
-        {%- endif %}
+            {% if cppstd -%}
+            message(STATUS "Conan C++ Standard {{ cppstd }} with extensions {{ cppstd_extensions }}}")
+            set(CMAKE_CXX_STANDARD {{ cppstd }})
+            set(CMAKE_CXX_EXTENSIONS {{ cppstd_extensions }})
+            {%- endif %}
         {% endblock %}
 
         {% block footer %}
-        set(CMAKE_CXX_FLAGS_INIT "${CONAN_CXX_FLAGS}" CACHE STRING "" FORCE)
-        set(CMAKE_C_FLAGS_INIT "${CONAN_C_FLAGS}" CACHE STRING "" FORCE)
-        set(CMAKE_SHARED_LINKER_FLAGS_INIT "${CONAN_SHARED_LINKER_FLAGS}" CACHE STRING "" FORCE)
-        set(CMAKE_EXE_LINKER_FLAGS_INIT "${CONAN_EXE_LINKER_FLAGS}" CACHE STRING "" FORCE)
+            set(CMAKE_CXX_FLAGS_INIT "${CONAN_CXX_FLAGS}" CACHE STRING "" FORCE)
+            set(CMAKE_C_FLAGS_INIT "${CONAN_C_FLAGS}" CACHE STRING "" FORCE)
+            set(CMAKE_SHARED_LINKER_FLAGS_INIT "${CONAN_SHARED_LINKER_FLAGS}" CACHE STRING "" FORCE)
+            set(CMAKE_EXE_LINKER_FLAGS_INIT "${CONAN_EXE_LINKER_FLAGS}" CACHE STRING "" FORCE)
         {% endblock %}
     """)
 
@@ -44,14 +39,6 @@ class CMakeAndroidToolchain(CMakeToolchainBase):
 
     def __init__(self, conanfile, build_type=None, **kwargs):
         super(CMakeAndroidToolchain, self).__init__(conanfile, build_type=build_type, **kwargs)
-        # TODO: Is this abuse of 'variables' attribute?
-        self.variables['CMAKE_SYSTEM_NAME'] = 'Android'
-        self.variables['CMAKE_SYSTEM_VERSION'] = self._conanfile.settings.os.api_level
-        self.variables['CMAKE_ANDROID_ARCH_ABI'] = self._get_android_abi()
-        self.variables[
-            'CMAKE_ANDROID_NDK'] = '/Users/jgsogo/Library/Android/sdk/ndk/21.0.6113669'  # TODO: ???
-        self.variables['CMAKE_ANDROID_STL_TYPE'] = self._get_android_stl()
-
         self.build_type = build_type or self._conanfile.settings.get_safe("build_type")
 
     def _get_templates(self):
@@ -70,3 +57,15 @@ class CMakeAndroidToolchain(CMakeToolchainBase):
     def _get_android_stl(self):
         libcxx_str = str(self._conanfile.settings.compiler.libcxx)
         return libcxx_str  # TODO: only 'c++_shared' y 'c++_static' supported?
+
+    def _get_template_context_data(self):
+        ctxt_toolchain, _ = super(CMakeAndroidToolchain, self)._get_template_context_data()
+        ctxt_toolchain.update({
+            'CMAKE_SYSTEM_NAME': 'Android',
+            'CMAKE_SYSTEM_VERSION': self._conanfile.settings.os.api_level,
+            'CMAKE_ANDROID_ARCH_ABI': self._get_android_abi(),
+            'CMAKE_ANDROID_STL_TYPE': self._get_android_stl()
+            'CMAKE_ANDROID_NDK': '/Users/jgsogo/Library/Android/sdk/ndk/21.0.6113669',  # TODO: ???
+
+        })
+        return ctxt_toolchain, {}
