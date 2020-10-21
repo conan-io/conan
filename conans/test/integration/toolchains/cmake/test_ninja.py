87de661d9294bd95db1eed91262cccb6349615c1
Uilian Ries <uilianries@gmail.com>
2020-10-21 17:32:15 -0300

    #7814 Add new integration test for cmake-ninja
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/test/integration/toolchains/cmake/test_ninja.py
+++ conans/test/integration/toolchains/cmake/test_ninja.py
@@ -1,6 +1,7 @@
 import shutil
 import textwrap
 import unittest
+import os
 
 from conans.test.utils.tools import TestClient
 from conans.test.utils.test_files import temp_folder
@@ -77,7 +78,7 @@ class CMakeNinjaTestCase(unittest.TestCase):
             raise unittest.SkipTest("Ninja expected in PATH")
 
     def setUp(self):
-        folder = temp_folder()
+        folder = temp_folder(False)
         cpp_project = CppProject()
         self.client = TestClient(current_folder=folder)
         cpp_project.create_project(self.client)
@@ -86,7 +87,7 @@ class CMakeNinjaTestCase(unittest.TestCase):
         })
 
     def test_regular_build(self):
-        """ Ninja build must proceed using default profile
+        """ Ninja build must proceed using default profile and conan create
         """
         with environment_append({"CONAN_CMAKE_GENERATOR": "Ninja"}):
             self.client.run("create . foobar/0.1.0@")
@@ -100,3 +101,24 @@ class CMakeNinjaTestCase(unittest.TestCase):
         self.client.run("create . foobar/0.1.0@")
         self.assertIn('CMake command: cmake -G "Ninja" '
                       '-DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"', self.client.out)
+
+    def test_devflow_build(self):
+        """ Ninja build must proceed using default profile and conan development flow
+        """
+        conanfile = CMakeNinjaTestCase.conanfile.replace("(self)", "(self, generator='Ninja')")
+        self.client.save({
+            "conanfile.py": conanfile,
+        })
+
+        build_folder = os.path.join(self.client.current_folder, "build")
+        package_folder = os.path.join(self.client.current_folder, "pkg")
+        with environment_append({"CONAN_PRINT_RUN_COMMANDS": "1"}):
+            self.client.run("export . foobar/0.1.0@")
+            self.client.run("install . --install-folder={}".format(build_folder))
+            self.client.run("build . --build-folder={}".format(build_folder))
+            self.assertIn('CMake command: cmake -G "Ninja" '
+                          '-DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"', self.client.out)
+            # FIXME: conan package tries to install on /usr/local. CMAKE_PREFIX_PATH is empty
+            self.client.run("package . --build-folder={} --package-folder={}"
+                            .format(build_folder, package_folder), assert_error=True)
+            self.assertIn('Permission denied.', self.client.out)
