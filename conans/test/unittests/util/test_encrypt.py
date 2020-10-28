6ce8119b9f0cefdd3042d3efd8717a46aa788e1d
jgsogo <jgsogo@gmail.com>
2020-10-28 19:08:02 +0100

    check we won't use an empty key

--- conans/test/unittests/util/test_encrypt.py
+++ conans/test/unittests/util/test_encrypt.py
@@ -30,3 +30,11 @@ class EncryptTestCase(unittest.TestCase):
         data = encrypt.encode(message, key)
         self.assertNotEqual(message, data)
         self.assertEquals(message, encrypt.decode(data, key))
+
+    def test_key_empty(self):
+        # Empty keys, or keys with only non-ascii chars are not allowed
+        with self.assertRaises(AssertionError):
+            encrypt.encode('message', '')
+
+        with self.assertRaises(AssertionError):
+            encrypt.encode('message', b'\xc3\xb1\xe2\x82\xac'.decode('utf-8'))
