5c13ffe54075fc194f9876e640c9b29c2539505b
jgsogo <jgsogo@gmail.com>
2020-10-28 13:26:45 +0100

    alternate impl for py2

--- conans/util/encrypt.py
+++ conans/util/encrypt.py
@@ -1,11 +1,16 @@
 from base64 import urlsafe_b64encode, urlsafe_b64decode
 
+import six
+
 
 # WARNING: These are useful functions to obfuscate some data, but they are NO WAY OK FOR SECURITY!
 
 def encode(data, key):
-    assert isinstance(data, str), "Expected 'str', got '{}'".format(type(data))
-    return urlsafe_b64encode(bytes(key + data, 'utf-8'))
+    assert isinstance(data, six.string_types), "Expected string type, got '{}'".format(type(data))
+    if six.PY3:
+        return urlsafe_b64encode(bytes(key + data, 'utf-8'))
+    else:
+        return urlsafe_b64encode(str(key + data).encode('utf-8'))
 
 
 def decode(enc, key):
