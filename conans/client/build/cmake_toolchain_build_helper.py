06312b7fa706a2b4cb8e3896db457eb4ffb185f8
Javier G. Sogo <jgsogo@gmail.com>
2020-10-28 16:27:06 +0100

    Move 'CMAKE_INSTALL_PREFIX' to the command-line (#7946)
    * move 'CMAKE_INSTALL_PREFIX' to the command line
    
    * add replacement to path
    
    * Update conans/client/build/cmake_toolchain_build_helper.py
    
    Co-authored-by: James <james@conan.io>

--- conans/client/build/cmake_toolchain_build_helper.py
+++ conans/client/build/cmake_toolchain_build_helper.py
@@ -75,7 +75,10 @@ class CMakeToolchainBuildHelper(object):
             build_folder = os.path.join(self._conanfile.build_folder, self._build_folder)
 
         mkdir(build_folder)
-        arg_list = '-DCMAKE_TOOLCHAIN_FILE="%s" "%s"' % (CMakeToolchainBase.filename, source)
+        arg_list = '-DCMAKE_TOOLCHAIN_FILE="{}" -DCMAKE_INSTALL_PREFIX="{}" "{}"'.format(
+            CMakeToolchainBase.filename,
+            self._conanfile.package_folder.replace("\\", "/"),
+            source)
 
         generator = '-G "{}" '.format(self._generator) if self._generator else ""
         command = "%s %s%s" % (self._cmake_program, generator, arg_list)
