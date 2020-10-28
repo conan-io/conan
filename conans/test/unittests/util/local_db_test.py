00f1d248f329e49d7f696786fe9f548cdd711a53
jgsogo <jgsogo@gmail.com>
2020-10-28 13:12:23 +0100

    unittests using database

--- conans/test/unittests/util/local_db_test.py
+++ conans/test/unittests/util/local_db_test.py
@@ -32,30 +32,17 @@ class LocalStoreTest(unittest.TestCase):
         encryption_key = str(uuid.uuid4())
         localdb = LocalDB.create(db_file, encryption_key=encryption_key)
 
-        # Test write and read login
-        user, token, access_token = localdb.get_login("myurl1")
-        self.assertIsNone(user)
-        self.assertIsNone(token)
-        self.assertIsNone(access_token)
-
         localdb.store("pepe", "token", "access_token", "myurl1")
         user, token, access_token = localdb.get_login("myurl1")
         self.assertEqual("pepe", user)
         self.assertEqual("token", token)
         self.assertEqual("access_token", access_token)
-        self.assertEqual("pepe", localdb.get_username("myurl1"))
 
     def test_token_encryption_unicode(self):
         tmp_dir = temp_folder()
         db_file = os.path.join(tmp_dir, "dbfile")
         encryption_key = str(uuid.uuid4())
         localdb = LocalDB.create(db_file, encryption_key=encryption_key)
-
-        # Test write and read login
-        user, token, access_token = localdb.get_login("myurl1")
-        self.assertIsNone(user)
-        self.assertIsNone(token)
-        self.assertIsNone(access_token)
 
         token_input = b'espa\xc3\xb1a\xe2\x82\xac$'.decode('utf-8')  # Only ASCII files in codebase
         localdb.store("pepe", token_input, token_input, "myurl1")
@@ -64,3 +51,10 @@ class LocalStoreTest(unittest.TestCase):
         self.assertEqual(token_input, token)
         self.assertEqual(token_input, access_token)
         self.assertEqual("pepe", localdb.get_username("myurl1"))
+
+        # Without the encryption key we get obfuscated values
+        other_db = LocalDB.create(db_file)
+        user, token, access_token = other_db.get_login("myurl1")
+        self.assertEqual("pepe", user)
+        self.assertNotEqual(token_input, token)
+        self.assertNotEqual(token_input, access_token)
