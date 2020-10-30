706e9b96d8b7d9e78a8d723f00c822f6dc584c19
James <james@conan.io>
2020-10-30 01:05:43 +0100

    Feature/toolchain msbuild cmd (#7941)
    * working in msbuild cmd
    
    * trying to manage the toolset
    
    * working on it
    
    * test passing
    
    * review
    
    * fix broken tests
    
    * Update conans/client/build/msbuild.py
    
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>
    
    * Update conans/test/functional/toolchain/test_msbuild.py
    
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>
    
    * fix test
    
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>

--- conans/client/toolchain/msbuild.py
+++ conans/client/toolchain/msbuild.py
@@ -2,8 +2,46 @@ import os
 import textwrap
 from xml.dom import minidom
 
+from conans.client.toolchain.visual import vcvars_arch, vcvars_command
+from conans.client.tools import msvs_toolset
 from conans.errors import ConanException
 from conans.util.files import save, load
+
+
+class MSBuildCmd(object):
+    def __init__(self, conanfile):
+        self._conanfile = conanfile
+        self.version = conanfile.settings.get_safe("compiler.version")
+        self.vcvars_arch = vcvars_arch(conanfile)
+        self.build_type = conanfile.settings.get_safe("build_type")
+        msvc_arch = {'x86': 'x86',
+                     'x86_64': 'x64',
+                     'armv7': 'ARM',
+                     'armv8': 'ARM64'}
+        # if platforms:
+        #    msvc_arch.update(platforms)
+        arch = conanfile.settings.get_safe("arch")
+        msvc_arch = msvc_arch.get(str(arch))
+        if conanfile.settings.get_safe("os") == "WindowsCE":
+            msvc_arch = conanfile.settings.get_safe("os.platform")
+        self.platform = msvc_arch
+
+    def command(self, sln):
+        vcvars = vcvars_command(self.version, architecture=self.vcvars_arch,
+                                platform_type=None, winsdk_version=None,
+                                vcvars_ver=None)
+        cmd = ('%s && msbuild "%s" /p:Configuration=%s /p:Platform=%s '
+               % (vcvars, sln, self.build_type, self.platform))
+        return cmd
+
+    def build(self, sln):
+        cmd = self.command(sln)
+        self._conanfile.run(cmd)
+
+    @staticmethod
+    def get_version(settings):
+        return NotImplementedError("get_version() method is not supported in MSBuild "
+                                   "toolchain helper")
 
 
 class MSBuildToolchain(object):
@@ -36,6 +74,7 @@ class MSBuildToolchain(object):
 
         runtime = self._conanfile.settings.get_safe("compiler.runtime")
         cppstd = self._conanfile.settings.get_safe("compiler.cppstd")
+        toolset = msvs_toolset(self._conanfile.settings)
         runtime_library = {"MT": "MultiThreaded",
                            "MTd": "MultiThreadedDebug",
                            "MD": "MultiThreadedDLL",
@@ -53,13 +92,17 @@ class MSBuildToolchain(object):
                   <LanguageStandard>{}</LanguageStandard>
                 </ClCompile>
               </ItemDefinitionGroup>
+              <PropertyGroup Label="Configuration">
+                <PlatformToolset>{}</PlatformToolset>
+              </PropertyGroup>
             </Project>
             """)
         preprocessor_definitions = ";".join([format_macro(k, v)
                                              for k, v in self.preprocessor_definitions.items()])
         # It is useless to set PlatformToolset in the config file, because the conditional checks it
         cppstd = "stdcpp%s" % cppstd if cppstd else ""
-        config_props = content.format(preprocessor_definitions, runtime_library, cppstd)
+        toolset = toolset or ""
+        config_props = content.format(preprocessor_definitions, runtime_library, cppstd, toolset)
         config_filepath = os.path.abspath(config_filename)
         self._conanfile.output.info("MSBuildToolchain created %s" % config_filename)
         save(config_filepath, config_props)
