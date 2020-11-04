f55d54472cb290f065d6eae4bf370054f8e062cb
danimtb <danimanzaneque@gmail.com>
2020-11-04 17:25:37 +0100

    Add test options settings

--- conans/test/functional/profiles/profiles_test.py
+++ conans/test/functional/profiles/profiles_test.py
@@ -16,23 +16,36 @@ class ProfileInterpolator(BasicInterpolation):
         missing_keys = defaultdict(str)
         rawval % missing_keys
         all_keys = dict(missing_keys)
-        all_keys.update(parser.defaults())
+        if section in parser.section_defaults:
+            all_keys.update(parser.section_defaults[section])
         val = rawval % all_keys
         return val
 
 
 class ProfileConfigParser(ConfigParser):
 
-    def __init__(self, *args, **kargs):
-        super(ConfigParser, self).__init__(interpolation=ProfileInterpolator(), *args, **kargs)
+    def __init__(self, section_defaults=None, *args, **kargs):
+        super(ConfigParser, self).__init__(interpolation=ProfileInterpolator(), delimiters=("=",),
+                                           *args, **kargs)
+        self.section_defaults = section_defaults or {}
 
     def optionxform(self, optionstr):
         return optionstr
 
+    def items(self, *args, **kargs):  # section=_UNSET, raw=False, vars=None
+        items = dict(super(ConfigParser, self).items(*args, **kargs))
+        result = {}
+        section = args[0] if args else None
+        if section:
+            if section in self.section_defaults:
+                result = self.section_defaults[section]
+        result.update(items)
+        return result
+
 
 class ProfileTest(unittest.TestCase):
 
-    def test_config_parser(self):
+    def test_env(self):
         parser1 = ProfileConfigParser()
         profile1 = textwrap.dedent("""
             [env]
@@ -44,7 +57,7 @@ class ProfileTest(unittest.TestCase):
         p1_vars = dict(parser1.items("env"))
         self.assertDictEqual({"bar": "bar1", "CXXFLAGS": "-fPIC", "zet": "zet"}, p1_vars)
 
-        parser2 = ProfileConfigParser(defaults=p1_vars)
+        parser2 = ProfileConfigParser(section_defaults=dict(parser1.items()))
         profile2 = textwrap.dedent("""
             [env]
             foo=foo1
@@ -56,3 +69,37 @@ class ProfileTest(unittest.TestCase):
         self.assertDictEqual({"foo": "foo1", "CXXFLAGS": "-fPIC -fother", "bar": "bar2",
                               "zet": "zet", "OTHERFLAGS": " -fother"},
                              dict(parser2.items("env")))
+
+    def test_settings_options(self):
+        parser1 = ProfileConfigParser()
+        profile1 = textwrap.dedent("""
+            [settings]
+            os=Windows
+            arch=x86
+            [options]
+            poco:with_zlib=True
+            poco:with_openssl=True
+        """)
+        parser1.read_string(profile1)
+        p1_settings = dict(parser1.items("settings"))
+        self.assertDictEqual({"os": "Windows", "arch": "x86"}, p1_settings)
+        p1_options = dict(parser1.items("options"))
+        self.assertDictEqual({"poco:with_zlib": "True", "poco:with_openssl": "True"}, p1_options)
+
+        parser2 = ProfileConfigParser(section_defaults=dict(parser1.items()))
+        profile2 = textwrap.dedent("""
+            [settings]
+            arch=x86_64
+            build_type=Release
+            [options]
+            poco:with_openssl=False
+            poco:with_other=False
+        """)
+        parser2.read_string(profile2)
+        p2_settings = dict(parser2.items("settings"))
+        self.assertDictEqual({"os": "Windows", "arch": "x86_64", "build_type": "Release"},
+                             p2_settings)
+        p2_options = dict(parser2.items("options"))
+        self.assertDictEqual({"poco:with_zlib": "True", "poco:with_openssl": "False",
+                              "poco:with_other": "False"},
+                             p2_options)
