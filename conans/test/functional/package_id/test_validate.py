fe1b1127ff579afd873679bf07a1040fa7393735
James <james@conan.io>
2020-11-20 14:17:29 +0100

    Update conans/test/functional/package_id/test_validate.py
    Co-authored-by: Javier G. Sogo <jgsogo@gmail.com>

--- conans/test/functional/package_id/test_validate.py
+++ conans/test/functional/package_id/test_validate.py
@@ -38,7 +38,7 @@ class TestValidate(unittest.TestCase):
         client.run("info pkg/0.1@ -s os=Windows --json=myjson")
         myjson = json.loads(client.load("myjson"))
         self.assertEqual(myjson[0]["binary"], BINARY_INVALID)
-        self.assertEqual(myjson[0]["id"], PACKAGE_ID_INVALID)
+        self.assertEqual(myjson[0]["id"], 'Invalid')
 
     def test_validate_compatible(self):
         client = TestClient()
