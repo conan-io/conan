c2cfb5be13d604fb8675a0ad6aada7eb32377bb3
Kai Dohmen <psykai1993@googlemail.com>
2020-11-21 16:33:44 +0100

    Finished first implementation of Qbs build helper

--- conans/client/build/qbs.py
+++ conans/client/build/qbs.py
@@ -1,5 +1,4 @@
 import os
-import platform
 
 from conans import tools
 from conans.errors import ConanException
@@ -11,139 +10,9 @@ class QbsException(ConanException):
         return "Qbs build helper: {}".format(msg)
 
 
-def _qbs_config(conanfile, profile_name, values):
-    key_prefix = "profiles.%s" % profile_name
-    for key, value in values.items():
-        config_key = "%s.%s" % (key_prefix, key)
-        if type(value) is bool:
-            if value:
-                value = 'true'
-            else:
-                value = 'false'
-        cmd = 'qbs-config --settings-dir %s %s "%s"' % (
-              _settings_dir(conanfile), config_key, value)
-        conanfile.run(cmd)
-
-
-def _env_var_to_list(var):
-    elements = []
-    flag_groups = var.split('"')
-
-    latest_flag = ''
-    for i in range(len(flag_groups)):
-        if not flag_groups[i]:
-            continue
-
-        def is_quoted_flag():
-            return i % 2
-
-        if is_quoted_flag():
-            if latest_flag:
-                elements.append(latest_flag + flag_groups[i])
-            else:
-                elements.append(flag_groups[i])
-        else:
-            flags = flag_groups[i].split()
-            latest_flag = flags.pop()
-            for s in flags:
-                elements.append(s)
-            if not latest_flag.endswith('='):
-                elements.append(latest_flag)
-                latest_flag = ''
-
-    return elements
-
-
-def _check_for_compiler(conanfile):
-    compiler = conanfile.settings.get_safe("compiler")
-    if not compiler:
-        raise QbsException("need compiler to be set in settings")
-
-    if compiler not in ["Visual Studio", "gcc", "clang"]:
-        raise QbsException("compiler not supported")
-
-
-def _compiler_name(conanfile):
-    # needs more work since currently only windows and linux is supported
-    compiler = conanfile.settings.compiler
-    if platform.system() == "Windows":
-        if compiler == "gcc":
-            return "mingw"
-        if compiler == "Visual Studio":
-            if tools.msvs_toolset(conanfile) == "ClangCl":
-                return "clang-cl"
-            return "cl"
-        if compiler == "clang":
-            return "clang-cl"
-        raise QbsException("unknown windows compiler")
-
-    if platform.system() == "Linux":
-        return compiler
-
-    raise QbsException("unknown compiler")
-
-
-def _generate_profile_name(conanfile):
-    compiler = "_%s" % (conanfile.settings.get_safe("compiler"))
-    compiler_version = conanfile.settings.compiler.get_safe(
-        "version")
-    if compiler_version:
-        compiler_version = '_' + compiler_version
-    the_os = conanfile.settings.get_safe("os")
-    if the_os:
-        the_os = '_' + the_os
-
-    return "_conan%s%s%s" % (the_os, compiler, compiler_version)
-
-
-def _settings_dir(conanfile):
-    return conanfile.build_folder
-
-
-def _setup_toolchain(conanfile, profile_name):
-    if tools.get_env("CC"):
-        compiler = tools.get_env("CC")
-    else:
-        compiler = _compiler_name(conanfile)
-
-    env_context = tools.no_op()
-    if platform.system() == "Windows":
-        if compiler in ["cl", "clang-cl"]:
-            env_context = tools.vcvars()
-
-    with env_context:
-        cmd = "qbs-setup-toolchains --settings-dir %s %s %s" % (
-              _settings_dir(conanfile), compiler, profile_name)
-        conanfile.run(cmd)
-
-    flags_from_env = {}
-    if tools.get_env("ASFLAGS"):
-        flags_from_env["cpp.assemblerFlags"] = '%s' % (
-            _env_var_to_list(tools.get_env("ASFLAGS")))
-    if tools.get_env("CFLAGS"):
-        flags_from_env["cpp.cFlags"] = '%s' % (
-            _env_var_to_list(tools.get_env("CFLAGS")))
-    if tools.get_env("CPPFLAGS"):
-        flags_from_env["cpp.cppFlags"] = '%s' % (
-            _env_var_to_list(tools.get_env("CPPFLAGS")))
-    if tools.get_env("CXXFLAGS"):
-        flags_from_env["cpp.cxxFlags"] = '%s' % (
-            _env_var_to_list(tools.get_env("CXXFLAGS")))
-    if tools.get_env("LDFLAGS"):
-        ld_flags = []
-        for item in _env_var_to_list(tools.get_env("LDFLAGS")):
-            flags = item[len("-Wl,"):]
-            ld_flags.extend(flags.split(","))
-        flags_from_env["cpp.linkerFlags"] = '%s' % (ld_flags)
-
-    if flags_from_env:
-        _qbs_config(conanfile, profile_name, flags_from_env)
-
-
 def _configuration_dict_to_commandlist(name, dict):
     command_list = ["config:%s" % name]
-    for key in dict:
-        value = dict[key]
+    for key, value in dict.items():
         if type(value) is bool:
             if value:
                 b = "true"
@@ -157,14 +26,10 @@ def _configuration_dict_to_commandlist(name, dict):
 
 class Qbs(object):
     def __init__(self, conanfile, project_file=None):
-        _check_for_compiler(conanfile)
         self._conanfile = conanfile
         self._set_project_file(project_file)
-        self._base_profile_name = _generate_profile_name(conanfile)
-        _setup_toolchain(conanfile, self._base_profile_name)
         self.jobs = tools.cpu_count()
         self._configuration = dict()
-        self._profile_name = ""
 
     def _set_project_file(self, project_file):
         if not project_file:
@@ -175,33 +40,37 @@ class Qbs(object):
         if not os.path.exists(self._project_file):
             raise QbsException("could not find project file")
 
-    def setup_profile(self, name, values):
-        temp = {"baseProfile": self._base_profile_name}
-        temp.update(values)
-        _qbs_config(self._conanfile, name, temp)
-        self._profile_name = name
-
     def add_configuration(self, name, values):
         self._configuration[name] = values
 
-    def build(self, all_products=False, products=[]):
-        if not self._profile_name:
-            self._profile_name = self._base_profile_name
-
+    def build(self, products=[]):
         args = [
             "--no-install",
-            "--settings-dir", _settings_dir(self._conanfile),
             "--build-directory", self._conanfile.build_folder,
             "--file", self._project_file,
         ]
 
-        if all_products:
-            args.append("--all-products")
         if products:
-            args.extend(["-p", ",".join(products)])
+            args.extend(["--products", ",".join(products)])
 
         args.extend(["--jobs", "%s" % self.jobs])
-        args.append("profile:%s" % self._profile_name)
+
+        for name in self._configuration:
+            config = self._configuration[name]
+            args.extend(_configuration_dict_to_commandlist(name, config))
+
+        cmd = "qbs build %s" % (" ".join(args))
+        self._conanfile.run(cmd)
+
+    def build_all(self):
+        args = [
+            "--no-install",
+            "--build-directory", self._conanfile.build_folder,
+            "--file", self._project_file,
+            "--all-products"
+        ]
+
+        args.extend(["--jobs", "%s" % self.jobs])
 
         for name in self._configuration:
             config = self._configuration[name]
--- conans/test/unittests/client/build/qbs_test.py
+++ conans/test/unittests/client/build/qbs_test.py
@@ -2,168 +2,25 @@ import unittest
 import conans.client.build.qbs as qbs
 
 from conans.client import tools
-from conans.test.utils.mocks import ConanFileMock, MockSettings
+from conans.test.utils.mocks import MockConanfile, MockSettings
 
 
-class ConanFileMockWithMultipleCommands(ConanFileMock):
-    def __init__(self):
-        self.command = []
+class RunnerMock(object):
+    def __init__(self, return_ok=True, output=None):
+        self.command_called = None
+        self.return_ok = return_ok
+        self.output = output
 
-    def run(self, command):
-        self.command.append(command)
+    def __call__(self, command, output, win_bash=False, subsystem=None):
+        self.command_called = command
+        self.win_bash = win_bash
+        self.subsystem = subsystem
+        if self.output and output and hasattr(output, "write"):
+            output.write(self.output)
+        return 0 if self.return_ok else 1
 
 
 class QbsTest(unittest.TestCase):
-    def test_call_qbs_config_with_single_value(self):
-        conanfile = ConanFileMock()
-        profile_name = "my_fancy_profile"
-        key = "test_key"
-        value = "test_value"
-        expected_key = "profiles.%s.%s" % (profile_name, key)
-        expected_value = value
-        qbs._qbs_config(conanfile, profile_name, value)
-        self.assertEqual(conanfile.command,
-                         "qbs-config --settings-dir %s %s %s" % (
-                            conanfile.build_folder, expected_key,
-                            expected_value))
-
-    def test_split_env_var_into_list(self):
-        expected_list = ['-p1', '-p2', '-p3_with_value=13',
-                         '-p_with_space1="hello world"',
-                         '"-p_with_space2=Hello World"']
-        env_var = " ".join(expected_list)
-        self.assertEqual(qbs._env_var_to_list(env_var), expected_list)
-
-    def test_compiler_not_in_settings(self):
-        conanfile = ConanFileMock()
-        conanfile.settings = MockSettings()
-        with self.assertRaises(qbs.QbsException):
-            qbs._check_for_compiler(conanfile)
-
-    def test_compiler_in_settings_not_supported(self):
-        conanfile = ConanFileMock()
-        conanfile.settings = MockSettings({"compiler":
-                                           "not realy a compiler name"})
-        with self.assertRaises(qbs.QbsException):
-            qbs._check_for_compiler(conanfile)
-
-    def test_valid_compiler(self):
-        supported_compilers = ["Visual Studio", "gcc", "clang"]
-        for compiler in supported_compilers:
-            conanfile = ConanFileMock()
-            conanfile.settings = MockSettings({"compiler": compiler})
-            qbs._check_for_compiler(conanfile)
-
-    @staticmethod
-    def _settings_to_test_against(self):
-        return [
-            {"os": "Windows", "compiler": "gcc", "qbs_compiler": "mingw"},
-            {"os": "Windows", "compiler": "clang",
-             "qbs_compiler": "clang-cl"},
-            {"os": "Windows", "compiler": "Visual Studio",
-             "qbs_compiler": "cl"},
-            {"os": "Windows", "compiler": "Visual Studio",
-             "toolset": "ClangCl", "qbs_compiler": "clang-cl"},
-            {"os": "Linux", "compiler": "gcc", "qbs_compiler": "gcc"},
-            {"os": "Linux", "compiler": "clang", "qbs_compiler": "clang"}
-        ]
-
-    def test_convert_compiler_name_to_qbs_compiler_name(self):
-        for settings in self._settings_to_test_against():
-            def expected():
-                return settings["qbs_compiler"]
-            conanfile = ConanFileMock()
-            conanfile.settings = settings
-            self.assertEqual(qbs._compiler_name(conanfile), expected())
-
-    def test_settings_dir_location(self):
-        conanfile = ConanFileMock()
-        self.assertEqual(qbs._settings_dir(conanfile), conanfile.build_folder)
-
-    def test_setup_toolchain_without_any_env_values(self):
-        stub_profile_name = "foobar"
-        for settings in self._settings_to_test_against():
-            conanfile = ConanFileMockWithMultipleCommands()
-            conanfile.settings = settings
-            qbs._setup_toolchain(conanfile, stub_profile_name)
-            self.assertEqual(len(conanfile.command), 1)
-            self.assertEqual(
-                conanfile.command[0],
-                "qbs-setup-toolchains --settings-dir %s %s %s" % (
-                    conanfile.build_folder, settings["qbs_compiler"],
-                    stub_profile_name))
-
-    def test_setup_toolchain_with_compiler_from_env(self):
-        compiler = "compiler_from_env"
-        stub_profile_name = "foobar"
-        for settings in self._settings_to_test_against():
-            conanfile = ConanFileMockWithMultipleCommands()
-            conanfile.settings = settings
-            with tools.environment_append({"CC": compiler}):
-                qbs._setup_toolchain(conanfile, stub_profile_name)
-            self.assertEqual(len(conanfile.command), 1)
-            self.assertEqual(
-                conanfile.command[0],
-                "qbs-setup-toolchains --settings-dir %s %s %s" % (
-                    conanfile.build_folder, compiler,
-                    stub_profile_name))
-
-    @staticmethod
-    def _generate_flags(flag, qbs_key):
-        return {"env": ('-{0}1 -{0}2 -{0}3_with_value=13 '
-                        '-{0}_with_space1="hello world" '
-                        '"-{0}_with_space2=Hello World"').format(flag),
-                "qbs_value": ('["-{0}1", "-{0}2", "-{0}3_with_value=13", '
-                              '"-{0}_with_space1=hello world", '
-                              '"-{0}_with_space2=Hello World"').format(flag),
-                "qbs_key": qbs_key}
-
-    def test_setup_toolchain_with_flags_from_env(self):
-        stub_profile_name = "foobar"
-        conanfile = ConanFileMockWithMultipleCommands()
-        compiler = "gcc"
-        conanfile.settings = {"os": "Linux", "compiler": compiler}
-
-        asm = self._generate_flags("asm", "assemblerFlags")
-        c = self._generate_flags("c", "cFlags")
-        cpp = self._generate_flags("cpp", "cppFlags")
-        cxx = self._generate_flags("cxx", "cxxFlags")
-        wl = self._generate_flags("Wl,", "linkerFlags")
-        ld = self._generate_flags("ld", "linkerFlags")
-        env = {
-            "ASFLAGS": asm["env"],
-            "CFLAGS": c["env"],
-            "CPPFLAGS": cpp["env"],
-            "CXXFLAGS": cxx["env"],
-            "LDFLAGS": wl["env"] + "-Wl," + ld["env"].replace(" -", ",-")
-        }
-        with tools.environment_append(env):
-            qbs._setup_toolchain(conanfile, stub_profile_name)
-        self.assertEqual(len(conanfile.command), 1 + len(env))
-        self.assertEqual(
-            conanfile.command[0],
-            "qbs-setup-toolchains --settings-dir %s %s %s" % (
-                conanfile.build_folder, compiler,
-                stub_profile_name))
-
-        qbs_config = [
-            {"key": asm["qbs_key"], "value": asm["qbs_value"]},
-            {"key": c["qbs_key"], "value": c["qbs_value"]},
-            {"key": cpp["qbs_key"], "value": cpp["qbs_value"]},
-            {"key": cxx["qbs_key"], "value": cxx["qbs_value"]},
-            {"key": wl["qbs_key"],
-             "value": (wl["qbs_value"] +
-                       ld["qbs_values"]).replace("][", ", ", 1)},
-        ]
-        self.assertEqual(len(conanfile.command)-1, len(qbs_config))
-        for i in range(len(qbs_config)):
-            item = qbs_config[i]
-            key = "profiles.%s.%s" % (stub_profile_name, item["key"])
-            self.assertEqual(
-                conanfile.command[i+1],
-                "qbs-config --settings-dir %s %s. %s" % (
-                    conanfile.build_folder, key, item["value"]))
-
     def test_generating_config_command_line(self):
         name = "default"
         dict = {
@@ -172,91 +29,127 @@ class QbsTest(unittest.TestCase):
             "products.App.myIntProperty": 13,
             "products.App.myBoolProperty": True
         }
-        self.assetEqual(
+        expected_config_line = [
+            'config:%s' % name,
+        ]
+        for key, value in dict.items():
+            if type(value) is bool:
+                if value:
+                    value = 'true'
+                else:
+                    value = 'false'
+            expected_config_line.append('%s:%s' % (key, value))
+        print(qbs._configuration_dict_to_commandlist(name, dict))
+        print(expected_config_line)
+        self.assertEqual(
             qbs._configuration_dict_to_commandlist(name, dict),
-            'config:%s %s:["%s"] %s:"%s" %s:%s %s:%s' % (
-                name,
-                "modules.cpp.cxxFlags",
-                '", "'.join(dict["modules.cpp.cxxFlags"]),
-                "modules.cpp.ldFlags",
-                dict["modules.cpp.ldFlags"],
-                "products.App.myIntProperty",
-                dict["products.App.myIntProperty"],
-                "products.App.myBoolProperty", "true"))
+            expected_config_line)
 
     def test_construct_build_helper_without_project_file(self):
-        conanfile = ConanFileMock()
-        conanfile.settings = {"os": "Linux", "compiler": "gcc"}
+        conanfile = MockConanfile(
+            MockSettings({"os": "Linux", "compiler": "gcc"}))
+        conanfile.source_folder = '.'
         build_helper = qbs.Qbs(conanfile)
         self.assertEqual(build_helper.jobs, tools.cpu_count())
         self.assertEqual(build_helper._project_file, conanfile.source_folder)
 
     def test_construct_build_helper_with_project_file(self):
-        conanfile = ConanFileMock()
-        conanfile.settings = {"os": "Linux", "compiler": "gcc"}
-        build_helper = qbs.Qbs(conanfile, project_file=conanfile.source_folder)
-        self.assertEqual(build_helper._project_file, conanfile.source_folder)
+        conanfile = MockConanfile(
+            MockSettings({"os": "Linux", "compiler": "gcc"}))
+        # just asume that the test is called from repo root
+        profile_file_path = "conans/client"
+        build_helper = qbs.Qbs(conanfile, project_file=profile_file_path)
+        self.assertEqual(build_helper._project_file, profile_file_path)
 
     def test_construct_build_helper_with_wrong_project_file_path(self):
-        conanfile = ConanFileMock()
-        conanfile.settings = {"os": "Linux", "compiler": "gcc"}
+        conanfile = MockConanfile(
+            MockSettings({"os": "Linux", "compiler": "gcc"}))
         with self.assertRaises(qbs.QbsException):
             qbs.Qbs(conanfile, project_file="random/file/path")
 
-    def test_setup_profile(self):
-        conanfile = ConanFileMockWithMultipleCommands()
-        conanfile.settings = {"os": "Linux", "compiler": "gcc"}
-        build_helper = qbs.Qbs(conanfile)
-
-        conanfile.command = []
-        stub_profile_name = "stub_profile"
-        base_profile_name = build_helper._base_profile_name
-        key = "testKey"
-        value = "testValue"
-        expected_key = "profiles.%s.%s" % (stub_profile_name, key)
-        build_helper.setup_profile(stub_profile_name, {key, value})
-        self.assertEqual(len(conanfile.command), 2)
-        self.assertEqual(
-            conanfile.command[0],
-            "qbs-config --settings-dir . %s profiles.%s.baseProfile %s" % (
-                conanfile.build_folder, stub_profile_name, base_profile_name))
-        self.assertEqual(
-            conanfile.command[1],
-            "qbs-config --settings-dir . %s %s %s" % (
-                conanfile.build_folder, expected_key, value))
-
     def test_add_configuration(self):
-        conanfile = ConanFileMock()
-        conanfile.settings = {"os": "Linux", "compiler": "gcc"}
+        conanfile = MockConanfile(
+            MockSettings({"os": "Linux", "compiler": "gcc"}))
+        conanfile.source_folder = '.'
         build_helper = qbs.Qbs(conanfile)
-        configurations = [
-            {"name": "debug",
-             "values": {"modules.cpp.cxxFlags": ["-frtti", "-fexceptions"]}},
-            {"name": "release",
-             "values": {"modules.cpp.cxxFlags": ["-fno-exceptions",
-                                                 "-fno-rtti"]}}
-        ]
-        for config in configurations:
-            build_helper.add_configuration(config["name"], config["values"])
+        configurations = {
+            "debug":  {"products.MyLib.specialFlags": ["-frtti",
+                                                       "-fexceptions"]},
+            "release": {"products.MyLib.specialFlags": ["-fno-exceptions",
+                                                        "-fno-rtti"]}
+        }
+        for name, config in configurations.items():
+            build_helper.add_configuration(name, config)
         self.assertEqual(build_helper._configuration, configurations)
 
     def test_build(self):
-        conanfile = ConanFileMock()
-        conanfile.settings = {"os": "Linux", "compiler": "gcc"}
+        conanfile = MockConanfile(
+            MockSettings({"os": "Linux", "compiler": "gcc"}),
+            runner=RunnerMock())
+        conanfile.source_folder = '.'
+        conanfile.build_folder = '.'
         build_helper = qbs.Qbs(conanfile)
 
+        build_helper.build()
         self.assertEqual(
-            conanfile.command,
-            ("qbs build --no-install --settings-dir %s --build-directory %s "
-             "--file %s --jobs %s profile:%s") % (
-                conanfile.build_folder,
+            conanfile.runner.command_called,
+            ("qbs build --no-install --build-directory %s "
+             "--file %s --jobs %s") % (
                 conanfile.build_folder, build_helper._project_file,
-                build_helper.jobs, build_helper._base_profile_name))
+                build_helper.jobs))
+
+        build_helper.build(products=["app1", "app2", "lib"])
+        self.assertEqual(
+            conanfile.runner.command_called,
+            ("qbs build --no-install --build-directory %s "
+             "--file %s --products app1,app2,lib --jobs %s") % (
+                conanfile.build_folder, build_helper._project_file,
+                build_helper.jobs))
+
+    def test_build_all(self):
+        conanfile = MockConanfile(
+            MockSettings({"os": "Linux", "compiler": "gcc"}),
+            runner=RunnerMock())
+        conanfile.source_folder = '.'
+        conanfile.build_folder = '.'
+        build_helper = qbs.Qbs(conanfile)
+
+        build_helper.build_all()
+        self.assertEqual(
+            conanfile.runner.command_called,
+            ("qbs build --no-install --build-directory %s "
+             "--file %s --all-products --jobs %s") % (
+                conanfile.build_folder, build_helper._project_file,
+                build_helper.jobs))
 
     def test_build_with_custom_configuration(self):
-        conanfile = ConanFileMock()
-        conanfile.settings = {"os": "Linux", "compiler": "gcc"}
+        conanfile = MockConanfile(
+            MockSettings({"os": "Linux", "compiler": "gcc"}),
+            runner=RunnerMock())
+        conanfile.source_folder = '.'
+        conanfile.build_folder = '.'
         build_helper = qbs.Qbs(conanfile)
         config_name = "debug"
-        config_values = {"product.App.customProperty": []}
+        config_values = {
+            'product.App.boolProperty': True,
+            'product.App.intProperty': 1337,
+            'product.App.stringProperty': 'Hello World',
+            'product.App.stringListProperty': ['Hello', 'World']
+        }
         build_helper.add_configuration(config_name, config_values)
+        build_helper.build()
+        self.assertEqual(
+            conanfile.runner.command_called,
+            ("qbs build --no-install --build-directory %s "
+             "--file %s --jobs %s "
+             "config:%s %s:%s %s:%s %s:%s %s:%s") % (
+                conanfile.build_folder, build_helper._project_file,
+                build_helper.jobs, config_name,
+                'product.App.boolProperty',
+                'true',
+                'product.App.intProperty',
+                config_values['product.App.intProperty'],
+                'product.App.stringProperty',
+                config_values['product.App.stringProperty'],
+                'product.App.stringListProperty',
+                config_values['product.App.stringListProperty']))
