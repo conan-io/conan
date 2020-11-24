76f0dab56253898feaaf58ca95143b9fefdfc87d
memsharded <james@conan.io>
2020-11-24 14:34:19 +0100

    review

--- conans/client/generators/__init__.py
+++ conans/client/generators/__init__.py
@@ -69,14 +69,12 @@ class GeneratorManager(object):
                             "make": MakeGenerator,
                             "deploy": DeployGenerator,
                             "markdown": MarkdownGenerator}
+        self._new_generators = ["CMakeToolchain", "MakeToolchain", "MSBuildToolchain",
+                                "MesonToolchain"]
 
     def add(self, name, generator_class, custom=False):
         if name not in self._generators or custom:
             self._generators[name] = generator_class
-
-    @property
-    def available(self):
-        return list(self._generators.keys())
 
     def __contains__(self, name):
         return name in self._generators
@@ -84,8 +82,9 @@ class GeneratorManager(object):
     def __getitem__(self, key):
         return self._generators[key]
 
-    @staticmethod
-    def _new_generator(generator_name):
+    def _new_generator(self, generator_name):
+        if generator_name not in self._new_generators:
+            return
         if generator_name == "CMakeToolchain":
             from conan.tools.cmake import CMakeToolchain
             return CMakeToolchain
@@ -98,6 +97,9 @@ class GeneratorManager(object):
         elif generator_name == "MesonToolchain":
             from conan.tools.meson import MesonToolchain
             return MesonToolchain
+        else:
+            raise ConanException("Internal Conan error: Generator '{}' "
+                                 "not commplete".format(generator_name))
 
     def write_generators(self, conanfile, path, output):
         """ produces auxiliary files, required to build a project or a package.
@@ -118,8 +120,9 @@ class GeneratorManager(object):
             try:
                 generator_class = self._generators[generator_name]
             except KeyError:
+                available = list(self._generators.keys()) + self._new_generators
                 raise ConanException("Invalid generator '%s'. Available types: %s" %
-                                     (generator_name, ", ".join(self.available)))
+                                     (generator_name, ", ".join(available)))
             try:
                 generator = generator_class(conanfile)
             except TypeError:
@@ -157,7 +160,7 @@ def write_toolchain(conanfile, path, output):
                "******************************************************************\n"
                "The 'toolchain' attribute or method has been deprecated.\n"
                "It will be removed in next Conan release.\n"
-               "Use 'generators = ClassName' or 'generate()' method instead.\n"
+               "Use 'generators = \"ClassName\"' or 'generate()' method instead.\n"
                "********************************************************************\n"
                "********************************************************************\n")
         output.warn(msg)
@@ -180,7 +183,6 @@ def write_toolchain(conanfile, path, output):
         # TODO: Lets discuss what to do with the environment
 
     if hasattr(conanfile, "generate"):
-        assert callable(conanfile.generate), "generate should be a method, not an attribute"
         output.highlight("Calling generate()")
         with chdir(path):
             with conanfile_exception_formatter(str(conanfile), "generate"):
