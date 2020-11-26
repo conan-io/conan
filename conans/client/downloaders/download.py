5fdb40042b734952dc81bd13ef1fe62a83c814bb
jgsogo <jgsogo@gmail.com>
2020-11-26 18:48:24 +0100

    boolean to 'use_cache' or not

--- conans/client/downloaders/download.py
+++ conans/client/downloaders/download.py
@@ -2,14 +2,14 @@ from .cached_file_downloader import CachedFileDownloader
 from .file_downloader import FileDownloader
 
 
-def run_downloader(requester, output, verify, config, user_download=False, **kwargs):
+def run_downloader(requester, output, verify, config, user_download=False, use_cache=True, **kwargs):
     downloader = FileDownloader(requester=requester, output=output, verify=verify, config=config)
-    if config.download_cache:
+    if use_cache and config.download_cache:
         downloader = CachedFileDownloader(config.download_cache, downloader,
                                           user_download=user_download)
     else:
         # TODO: Make the 'download' signature compatible
-        kwargs.pop('md5')
-        kwargs.pop('sha1')
-        kwargs.pop('sha256')
+        kwargs.pop('md5', None)
+        kwargs.pop('sha1', None)
+        kwargs.pop('sha256', None)
     return downloader.download(**kwargs)
