3b5989b6a7d4e22f4ec4452677e80daf0fd089ee
Kai Dohmen <psykai1993@googlemail.com>
2020-11-28 14:24:06 +0100

    Added detection of buildVariant, architecture, optimization, sysroot, PIC, cxxLanguageVersion

--- conan/tools/qbs/generic.py
+++ conan/tools/qbs/generic.py
@@ -8,14 +8,79 @@ from conans import tools
 from conans.errors import ConanException
 from conans.util.files import save
 
-_profile_name = "conan"
-_profiles_prefix_in_config = "profiles.%s" % _profile_name
+_profile_name = 'conan'
+_profiles_prefix_in_config = 'profiles.%s' % _profile_name
+
+_architecture = {
+    'x86': 'x86',
+    'x86_64': 'x86_64',
+    'ppc32be': 'ppc',
+    'ppc32': 'ppc',
+    'ppc64le': 'ppc64',
+    'ppc64': 'ppc64',
+    'armv4': 'arm',
+    'armv4i': 'arm',
+    'armv5el': 'arm',
+    'armv5hf': 'arm',
+    'armv6': 'arm',
+    'armv7': 'arm',
+    'armv7hf': 'arm',
+    'armv7s': 'arm',
+    'armv7k': 'arm',
+    'armv8': 'arm64',
+    'armv8_32': 'arm64',
+    'armv8.3': 'arm64',
+    'sparc': 'sparc',
+    'sparcv9': 'sparc64',
+    'mips': 'mips',
+    'mips64': 'mips64',
+    'avr': 'avr',
+    's390': 's390x',
+    's390x': 's390x',
+    'asm.js': None,
+    'wasm': None,
+    'sh4le': 'sh'
+}
+_build_variant = {
+    'Debug': 'debug',
+    'Release': 'release',
+    'RelWithDebInfo': 'profiling',
+    'MinSizeRel': 'release'
+}
+_optimization = {
+    'MinSizeRel': 'small'
+}
+_cxx_language_version = {
+    '98': 'c++98',
+    'gnu98': 'c++98',
+    '11': 'c++11',
+    'gnu11': 'c++11',
+    '14': 'c++14',
+    'gnu14': 'c++14',
+    '17': 'c++17',
+    'gnu17': 'c++17',
+    '20': 'c++20',
+    'gnu20': 'c++20'
+}
 
 
 class QbsException(ConanException):
     def __str__(self):
         msg = super(QbsException, self).__str__()
-        return "Qbs generic toolchain: {}".format(msg)
+        return 'Qbs generic toolchain: {}'.format(msg)
+
+
+def _bool(b):
+    if b is None:
+        return None
+
+    if type(b)is not bool:
+        raise QbsException('Tried to convert non bool type value to bool')
+
+    if b:
+        return 'true'
+    else:
+        return 'false'
 
 
 def _env_var_to_list(var):
@@ -23,33 +88,30 @@ def _env_var_to_list(var):
 
 
 def _check_for_compiler(conanfile):
-    compiler = conanfile.settings.get_safe("compiler")
+    compiler = conanfile.settings.get_safe('compiler')
     if not compiler:
-        raise QbsException("need compiler to be set in settings")
+        raise QbsException('need compiler to be set in settings')
 
-    if compiler not in ["Visual Studio", "gcc", "clang"]:
-        raise QbsException("compiler not supported")
+    if compiler not in ['Visual Studio', 'gcc', 'clang']:
+        raise QbsException('compiler not supported')
 
 
-def _compiler_name(conanfile):
+def _default_compiler_name(conanfile):
     # needs more work since currently only windows and linux is supported
-    compiler = conanfile.settings.get_safe("compiler")
-    the_os = conanfile.settings.get_safe("os")
-    if the_os == "Windows":
-        if compiler == "gcc":
-            return "mingw"
-        if compiler == "Visual Studio":
-            if tools.msvs_toolset(conanfile) == "ClangCl":
-                return "clang-cl"
-            return "cl"
-        if compiler == "clang":
-            return "clang-cl"
-        raise QbsException("unknown windows compiler")
+    compiler = conanfile.settings.get_safe('compiler')
+    the_os = conanfile.settings.get_safe('os')
+    if the_os == 'Windows':
+        if compiler == 'gcc':
+            return 'mingw'
+        if compiler == 'Visual Studio':
+            if tools.msvs_toolset(conanfile) == 'ClangCl':
+                return 'clang-cl'
+            return 'cl'
+        if compiler == 'clang':
+            return 'clang-cl'
+        raise QbsException('unknown windows compiler')
 
-    if the_os == "Linux":
-        return compiler
-
-    raise QbsException("unknown compiler")
+    return compiler
 
 
 def _settings_dir(conanfile):
@@ -57,25 +119,25 @@ def _settings_dir(conanfile):
 
 
 def _setup_toolchains(conanfile):
-    if tools.get_env("CC"):
-        compiler = tools.get_env("CC")
+    if tools.get_env('CC'):
+        compiler = tools.get_env('CC')
     else:
-        compiler = _compiler_name(conanfile)
+        compiler = _default_compiler_name(conanfile)
 
     env_context = tools.no_op()
-    if platform.system() == "Windows":
-        if compiler in ["cl", "clang-cl"]:
+    if platform.system() == 'Windows':
+        if compiler in ['cl', 'clang-cl']:
             env_context = tools.vcvars()
 
     with env_context:
-        cmd = "qbs-setup-toolchains --settings-dir %s %s %s" % (
+        cmd = 'qbs-setup-toolchains --settings-dir %s %s %s' % (
               _settings_dir(conanfile), compiler, _profile_name)
         conanfile.run(cmd)
 
 
 def _read_qbs_toolchain_from_config(conanfile):
     s = StringIO()
-    conanfile.run("qbs-config --settings-dir %s --list" % (
+    conanfile.run('qbs-config --settings-dir %s --list' % (
                     _settings_dir(conanfile)), output=s)
     config = {}
     s.seek(0)
@@ -97,29 +159,29 @@ def _read_qbs_toolchain_from_config(conanfile):
 
 def _flags_from_env():
     flags_from_env = {}
-    if tools.get_env("ASFLAGS"):
-        flags_from_env["cpp.assemblerFlags"] = '%s' % (
-            _env_var_to_list(tools.get_env("ASFLAGS")))
-    if tools.get_env("CFLAGS"):
-        flags_from_env["cpp.cFlags"] = '%s' % (
-            _env_var_to_list(tools.get_env("CFLAGS")))
-    if tools.get_env("CPPFLAGS"):
-        flags_from_env["cpp.cppFlags"] = '%s' % (
-            _env_var_to_list(tools.get_env("CPPFLAGS")))
-    if tools.get_env("CXXFLAGS"):
-        flags_from_env["cpp.cxxFlags"] = '%s' % (
-            _env_var_to_list(tools.get_env("CXXFLAGS")))
-    if tools.get_env("LDFLAGS"):
+    if tools.get_env('ASFLAGS'):
+        flags_from_env['cpp.assemblerFlags'] = '%s' % (
+            _env_var_to_list(tools.get_env('ASFLAGS')))
+    if tools.get_env('CFLAGS'):
+        flags_from_env['cpp.cFlags'] = '%s' % (
+            _env_var_to_list(tools.get_env('CFLAGS')))
+    if tools.get_env('CPPFLAGS'):
+        flags_from_env['cpp.cppFlags'] = '%s' % (
+            _env_var_to_list(tools.get_env('CPPFLAGS')))
+    if tools.get_env('CXXFLAGS'):
+        flags_from_env['cpp.cxxFlags'] = '%s' % (
+            _env_var_to_list(tools.get_env('CXXFLAGS')))
+    if tools.get_env('LDFLAGS'):
         ld_flags = []
-        for item in _env_var_to_list(tools.get_env("LDFLAGS")):
+        for item in _env_var_to_list(tools.get_env('LDFLAGS')):
             if item not in ['-Wl', ',']:
                 ld_flags.append(item)
-        flags_from_env["cpp.linkerFlags"] = str(ld_flags)
+        flags_from_env['cpp.linkerFlags'] = str(ld_flags)
     return flags_from_env
 
 
 class QbsGenericToolchain(object):
-    filename = "conan_toolchain.qbs"
+    filename = 'conan_toolchain.qbs'
 
     _template_toolchain = textwrap.dedent('''\
         import qbs
@@ -127,9 +189,38 @@ class QbsGenericToolchain(object):
         Project {
             Profile {
                 name: "conan_toolchain_profile"
-                {%- for key, value in profile_values.items() %}
+
+                /* detected via qbs-setup-toolchains */
+                {%- for key, value in _profile_values_from_setup.items() %}
                 {{ key }}: {{ value }}
                 {%- endfor %}
+
+                /* deduced from environment */
+                {%- for key, value in _profile_values_from_env.items() %}
+                {{ key }}: {{ value }}
+                {%- endfor %}
+                {%- if sysroot %}
+                qbs.sysroot: "{{ sysroot }}"
+                {%- endif %}
+
+                /* conan settings */
+                {%- if build_variant %}
+                qbs.buildVariant: "{{ build_variant }}"
+                {%- endif %}
+                {%- if architecture %}
+                qbs.architecture: "{{ architecture }}"
+                {%- endif %}
+                {%- if optimization %}
+                qbs.optimization: "{{ optimization }}"
+                {%- endif %}
+                {%- if cxx_language_version %}
+                cpp.cxxLanguageVersion: "{{ cxx_language_version }}"
+                {%- endif %}
+
+                /* package options */
+                {%- if position_independent_code %}
+                cpp.positionIndependentCode: {{ position_independent_code }}
+                {%- endif %}
             }
         }
         ''')
@@ -138,16 +229,38 @@ class QbsGenericToolchain(object):
         _check_for_compiler(conanfile)
         self._conanfile = conanfile
         _setup_toolchains(conanfile)
-        self._profile_values = _read_qbs_toolchain_from_config(conanfile)
-        self._profile_values.update(_flags_from_env())
+        self._profile_values_from_setup = (
+            _read_qbs_toolchain_from_config(conanfile))
+        self._profile_values_from_env = _flags_from_env()
         tools.rmdir(_settings_dir(conanfile))
 
-    def write_toolchain_files(self):
+        self._architecture = _architecture.get(
+            conanfile.settings.get_safe('arch'))
+        self._build_variant = _build_variant.get(
+            conanfile.settings.get_safe('build_type'))
+        self._optimization = _optimization.get(
+            conanfile.settings.get_safe('build_type'))
+        self._cxx_language_version = _cxx_language_version.get(
+            str(conanfile.settings.get_safe('compiler.cppstd')))
+        self._sysroot = tools.get_env('SYSROOT')
+        self._position_independent_code = _bool(
+            conanfile.options.get_safe('fPIC'))
+
+    def generate(self):
         save(self.filename, self.content)
 
     @property
     def content(self):
-        context = {"profile_values": self._profile_values}
+        context = {
+            '_profile_values_from_setup': self._profile_values_from_setup,
+            '_profile_values_from_env': self._profile_values_from_env,
+            'build_variant': self._build_variant,
+            'architecture': self._architecture,
+            'optimization': self._optimization,
+            'sysroot': self._sysroot,
+            'position_independent_code': self._position_independent_code,
+            'cxx_language_version': self._cxx_language_version
+        }
         t = Template(self._template_toolchain)
         content = t.render(**context)
         return content
--- conans/test/unittests/client/toolchain/test_qbs_generic.py
+++ conans/test/unittests/client/toolchain/test_qbs_generic.py
@@ -3,8 +3,8 @@ import tempfile
 import textwrap
 import conans.client.toolchain.qbs.generic as qbs
 
-from conans.client import tools
-from conans.test.utils.mocks import MockConanfile, MockSettings
+from conans import tools
+from conans.test.utils.mocks import MockConanfile, MockSettings, MockOptions
 
 
 class RunnerMock(object):
@@ -24,7 +24,7 @@ class RunnerMock(object):
         if not self.expectations:
             return 1
         expectation = self.expectations.pop(0)
-        if expectation.output and output and hasattr(output, "write"):
+        if expectation.output and output and hasattr(output, 'write'):
             output.write(expectation.output)
         return 0 if expectation.return_ok else 1
 
@@ -37,12 +37,123 @@ class MockConanfileWithFolders(MockConanfile):
 
     def run(self, *args, **kwargs):
         if self.runner:
-            if "output" not in kwargs:
-                kwargs["output"] = None
+            if 'output' not in kwargs:
+                kwargs['output'] = None
             self.runner(*args, **kwargs)
 
 
 class QbsGenericTest(unittest.TestCase):
+    def test_convert_bool(self):
+        self.assertEqual(qbs._bool(True), 'true')
+        self.assertEqual(qbs._bool(False), 'false')
+        with self.assertRaises(qbs.QbsException):
+            qbs._bool("")
+
+    def test_convert_build_variant(self):
+        conanfile = MockConanfileWithFolders(MockSettings({
+            'os': 'Linux',
+            'compiler': 'gcc'}))
+
+        qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+        self.assertEqual(qbs_toolchain._build_variant, None)
+
+        for build_type, build_variant in qbs._build_variant.items():
+            conanfile = MockConanfileWithFolders(MockSettings({
+                'os': 'Linux',
+                'compiler': 'gcc',
+                'build_type': build_type}))
+
+            qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+            self.assertEqual(qbs_toolchain._build_variant, build_variant)
+
+    def test_convert_architecture(self):
+        conanfile = MockConanfileWithFolders(MockSettings({
+            'os': 'Linux',
+            'compiler': 'gcc'}))
+
+        qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+        self.assertEqual(qbs_toolchain._architecture, None)
+
+        for arch, architecture in qbs._architecture.items():
+            conanfile = MockConanfileWithFolders(MockSettings({
+                'os': 'Linux',
+                'compiler': 'gcc',
+                'arch': arch}))
+
+            qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+            self.assertEqual(qbs_toolchain._architecture, architecture)
+
+    def test_convert_optimization(self):
+        conanfile = MockConanfileWithFolders(MockSettings({
+            'os': 'Linux',
+            'compiler': 'gcc'}))
+
+        qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+        self.assertEqual(qbs_toolchain._optimization, None)
+
+        for build_type, optimization in qbs._optimization.items():
+            conanfile = MockConanfileWithFolders(MockSettings({
+                'os': 'Linux',
+                'compiler': 'gcc',
+                'build_type': build_type}))
+
+            qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+            self.assertEqual(qbs_toolchain._optimization, optimization)
+
+    def test_use_sysroot_from_env(self):
+        conanfile = MockConanfileWithFolders(MockSettings({
+            'os': 'Linux',
+            'compiler': 'gcc'}))
+
+        sysroot = '/path/to/sysroot/foo/bar'
+        with tools.environment_append({'SYSROOT': sysroot}):
+            qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+        self.assertEqual(qbs_toolchain._sysroot, sysroot)
+
+    def test_detect_fpic_from_options(self):
+        f_pic = {
+            True: 'true',
+            False: 'false',
+            None: None
+        }
+
+        for option, value in f_pic.items():
+            conanfile = MockConanfileWithFolders(MockSettings({
+                    'os': 'Linux',
+                    'compiler': 'gcc'
+                }),
+                MockOptions({
+                    'fPIC': option
+                }))
+
+            qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+            self.assertEqual(qbs_toolchain._position_independent_code, value)
+
+    def test_convert_cxx_language_version(self):
+        conanfile = MockConanfileWithFolders(MockSettings({
+            'os': 'Linux',
+            'compiler': 'gcc'}))
+
+        qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+        self.assertEqual(qbs_toolchain._cxx_language_version, None)
+        conanfile = MockConanfileWithFolders(MockSettings({
+            'os': 'Linux',
+            'compiler': 'gcc',
+            'compiler.cppstd': 17}))
+
+        qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+        self.assertEqual(qbs_toolchain._cxx_language_version, 'c++17')
+
+        for cppstd, cxx_language_version in qbs._cxx_language_version.items():
+            conanfile = MockConanfileWithFolders(MockSettings({
+                'os': 'Linux',
+                'compiler': 'gcc',
+                'compiler.cppstd': cppstd}))
+
+            qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+            self.assertEqual(qbs_toolchain._cxx_language_version,
+                             cxx_language_version)
+
     def test_split_env_var_into_list(self):
         list = ['-p1', '-p2', '-p3_with_value=13',
                 '-p_with_space1="hello world"',
@@ -50,7 +161,7 @@ class QbsGenericTest(unittest.TestCase):
         expected_list = ['-p1', '-p2', '-p3_with_value=13',
                          '-p_with_space1=hello world',
                          '-p_with_space2=Hello World']
-        env_var = " ".join(list)
+        env_var = ' '.join(list)
         self.assertEqual(qbs._env_var_to_list(env_var), expected_list)
 
     def test_compiler_not_in_settings(self):
@@ -60,36 +171,36 @@ class QbsGenericTest(unittest.TestCase):
 
     def test_compiler_in_settings_not_supported(self):
         conanfile = MockConanfile(
-            MockSettings({"compiler": "not realy a compiler name"}))
+            MockSettings({'compiler': 'not realy a compiler name'}))
         with self.assertRaises(qbs.QbsException):
             qbs._check_for_compiler(conanfile)
 
     def test_valid_compiler(self):
-        supported_compilers = ["Visual Studio", "gcc", "clang"]
+        supported_compilers = ['Visual Studio', 'gcc', 'clang']
         for compiler in supported_compilers:
-            conanfile = MockConanfile(MockSettings({"compiler": compiler}))
+            conanfile = MockConanfile(MockSettings({'compiler': compiler}))
             qbs._check_for_compiler(conanfile)
 
     @staticmethod
     def _settings_to_test_against():
         return [
-            {"os": "Windows", "compiler": "gcc", "qbs_compiler": "mingw"},
-            {"os": "Windows", "compiler": "clang",
-             "qbs_compiler": "clang-cl"},
-            {"os": "Windows", "compiler": "Visual Studio",
-             "qbs_compiler": "cl"},
-            {"os": "Windows", "compiler": "Visual Studio",
-             "compiler.toolset": "ClangCl", "qbs_compiler": "clang-cl"},
-            {"os": "Linux", "compiler": "gcc", "qbs_compiler": "gcc"},
-            {"os": "Linux", "compiler": "clang", "qbs_compiler": "clang"}
+            {'os': 'Windows', 'compiler': 'gcc', 'qbs_compiler': 'mingw'},
+            {'os': 'Windows', 'compiler': 'clang',
+             'qbs_compiler': 'clang-cl'},
+            {'os': 'Windows', 'compiler': 'Visual Studio',
+             'qbs_compiler': 'cl'},
+            {'os': 'Windows', 'compiler': 'Visual Studio',
+             'compiler.toolset': 'ClangCl', 'qbs_compiler': 'clang-cl'},
+            {'os': 'Linux', 'compiler': 'gcc', 'qbs_compiler': 'gcc'},
+            {'os': 'Linux', 'compiler': 'clang', 'qbs_compiler': 'clang'}
         ]
 
     def test_convert_compiler_name_to_qbs_compiler_name(self):
         for settings in self._settings_to_test_against():
             def expected():
-                return settings["qbs_compiler"]
+                return settings['qbs_compiler']
             conanfile = MockConanfile(MockSettings(settings))
-            self.assertEqual(qbs._compiler_name(conanfile), expected())
+            self.assertEqual(qbs._default_compiler_name(conanfile), expected())
 
     def test_settings_dir_location(self):
         conanfile = MockConanfileWithFolders(MockSettings({}))
@@ -105,58 +216,58 @@ class QbsGenericTest(unittest.TestCase):
             self.assertEqual(len(conanfile.runner.command_called), 1)
             self.assertEqual(
                 conanfile.runner.command_called[0],
-                "qbs-setup-toolchains --settings-dir %s %s %s" % (
-                    qbs._settings_dir(conanfile), settings["qbs_compiler"],
+                'qbs-setup-toolchains --settings-dir %s %s %s' % (
+                    qbs._settings_dir(conanfile), settings['qbs_compiler'],
                     qbs._profile_name))
 
     def test_setup_toolchain_with_compiler_from_env(self):
-        compiler = "compiler_from_env"
+        compiler = 'compiler_from_env'
         for settings in self._settings_to_test_against():
             conanfile = MockConanfileWithFolders(MockSettings(settings),
                                                  runner=RunnerMock())
-            with tools.environment_append({"CC": compiler}):
+            with tools.environment_append({'CC': compiler}):
                 qbs._setup_toolchains(conanfile)
             self.assertEqual(len(conanfile.runner.command_called), 1)
             self.assertEqual(
                 conanfile.runner.command_called[0],
-                "qbs-setup-toolchains --settings-dir %s %s %s" % (
+                'qbs-setup-toolchains --settings-dir %s %s %s' % (
                     qbs._settings_dir(conanfile), compiler,
                     qbs._profile_name))
 
     @staticmethod
     def _generate_flags(flag, qbs_key):
-        return {"env": ('-{0}1 -{0}2 -{0}3_with_value=13 '
+        return {'env': ('-{0}1 -{0}2 -{0}3_with_value=13 '
                         '-{0}_with_space="hello world"').format(flag),
-                "qbs_value": ("['-{0}1', '-{0}2', '-{0}3_with_value=13', "
+                'qbs_value': ("['-{0}1', '-{0}2', '-{0}3_with_value=13', "
                               "'-{0}_with_space=hello world']").format(flag),
-                "qbs_key": qbs_key}
+                'qbs_key': qbs_key}
 
     def test_flags_from_env(self):
-        asm = self._generate_flags("asm", "assemblerFlags")
-        c = self._generate_flags("c", "cFlags")
-        cpp = self._generate_flags("cpp", "cppFlags")
-        cxx = self._generate_flags("cxx", "cxxFlags")
-        wl = self._generate_flags("Wl,", "linkerFlags")
-        ld = self._generate_flags("ld", "linkerFlags")
+        asm = self._generate_flags('asm', 'assemblerFlags')
+        c = self._generate_flags('c', 'cFlags')
+        cpp = self._generate_flags('cpp', 'cppFlags')
+        cxx = self._generate_flags('cxx', 'cxxFlags')
+        wl = self._generate_flags('Wl,', 'linkerFlags')
+        ld = self._generate_flags('ld', 'linkerFlags')
         env = {
-            "ASFLAGS": asm["env"],
-            "CFLAGS": c["env"],
-            "CPPFLAGS": cpp["env"],
-            "CXXFLAGS": cxx["env"],
-            "LDFLAGS": "%s -Wl,%s" % (wl["env"], ld["env"].replace(" -", ",-"))
+            'ASFLAGS': asm['env'],
+            'CFLAGS': c['env'],
+            'CPPFLAGS': cpp['env'],
+            'CXXFLAGS': cxx['env'],
+            'LDFLAGS': '%s -Wl,%s' % (wl['env'], ld['env'].replace(' -', ',-'))
         }
         with tools.environment_append(env):
             flags_from_env = qbs._flags_from_env()
 
         expected_flags = {
-            'cpp.'+asm["qbs_key"]: asm["qbs_value"],
-            'cpp.'+c["qbs_key"]: c["qbs_value"],
-            'cpp.'+cpp["qbs_key"]: cpp["qbs_value"],
-            'cpp.'+cxx["qbs_key"]: cxx["qbs_value"],
-            'cpp.'+wl["qbs_key"]: ("%s%s" % (wl["qbs_value"],
-                                             ld["qbs_value"])).replace(
-                                                "][", ", ", 1).replace(
-                                                "-Wl,", ""),
+            'cpp.'+asm['qbs_key']: asm['qbs_value'],
+            'cpp.'+c['qbs_key']: c['qbs_value'],
+            'cpp.'+cpp['qbs_key']: cpp['qbs_value'],
+            'cpp.'+cxx['qbs_key']: cxx['qbs_value'],
+            'cpp.'+wl['qbs_key']: ('%s%s' % (wl['qbs_value'],
+                                             ld['qbs_value'])).replace(
+                                                '][', ', ', 1).replace(
+                                                '-Wl,', ''),
         }
         self.assertEqual(flags_from_env, expected_flags)
 
@@ -201,7 +312,7 @@ class QbsGenericTest(unittest.TestCase):
         config = qbs._read_qbs_toolchain_from_config(conanfile)
         self.assertEqual(len(conanfile.runner.command_called), 1)
         self.assertEqual(conanfile.runner.command_called[0],
-                         "qbs-config --settings-dir %s --list" % (
+                         'qbs-config --settings-dir %s --list' % (
                             qbs._settings_dir(conanfile)))
         self.assertEqual(config, expected_config)
 
@@ -212,6 +323,8 @@ class QbsGenericTest(unittest.TestCase):
             Project {
                 Profile {
                     name: "conan_toolchain_profile"
+
+                    /* detected via qbs-setup-toolchains */
                     cpp.cCompilerName: "gcc"
                     cpp.compilerName: "g++"
                     cpp.cxxCompilerName: "g++"
@@ -224,11 +337,32 @@ class QbsGenericTest(unittest.TestCase):
                     qbs.someBoolProp: true
                     qbs.someIntProp: 13
                     qbs.toolchain: ["gcc"]
+
+                    /* deduced from environment */
+                    qbs.sysroot: "/foo/bar/path"
+
+                    /* conan settings */
+                    qbs.buildVariant: "release"
+                    qbs.architecture: "x86_64"
+                    qbs.optimization: "small"
+                    cpp.cxxLanguageVersion: "c++17"
+
+                    /* package options */
+                    cpp.positionIndependentCode: true
                 }
             }''')
 
         conanfile = MockConanfileWithFolders(
-            MockSettings({"compiler": "gcc", "os": "Linux"}),
+            MockSettings({
+                'compiler': 'gcc',
+                'compiler.cppstd': 17,
+                'os': 'Linux',
+                'build_type': 'MinSizeRel',
+                'arch': 'x86_64'
+            }),
+            options=MockOptions({
+                'fPIC': True
+            }),
             runner=RunnerMock(
                 expectations=[
                     RunnerMock.Expectation(),
@@ -236,6 +370,7 @@ class QbsGenericTest(unittest.TestCase):
                         output=self._generate_qbs_config_output()),
                 ]))
 
-        qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
+        with tools.environment_append({'SYSROOT': '/foo/bar/path'}):
+            qbs_toolchain = qbs.QbsGenericToolchain(conanfile)
 
         self.assertEqual(qbs_toolchain.content, expected_content)
