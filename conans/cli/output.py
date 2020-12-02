eeaf31fc49e0e0cb0765349342ae83ee19f2b094
czoido <mrgalleta@gmail.com>
2020-12-02 13:00:25 +0100

    add file handler

--- conans/cli/output.py
+++ conans/cli/output.py
@@ -1,9 +1,11 @@
 import logging
+import os
 import sys
 
 import tqdm
 from colorama import Fore, Style
 
+from conans.errors import ConanException
 from conans.util.env_reader import get_env
 
 
@@ -70,26 +72,27 @@ class TqdmHandler(logging.StreamHandler):
 class ColorFormatter(logging.Formatter):
     level_color = {
         'WARNING': Color.YELLOW,
-        'INFO': Color.YELLOW,
         'DEBUG': Color.BLUE,
         'CRITICAL': Color.YELLOW,
         'ERROR': Color.RED
     }
 
-    def __init__(self, msg, use_color=True):
-        logging.Formatter.__init__(self, msg)
-        self.use_color = use_color
+    def __init__(self, msg, use_color=False, **kwargs):
+        logging.Formatter.__init__(self, msg, **kwargs)
+        self._use_color = use_color
 
     def format(self, record):
-        if self.use_color and record.levelname in self.level_color:
-            record.msg = "{}{}{}".format(self.level_color[record.levelname], record.msg, Style.RESET_ALL)
-        return logging.Formatter.format(self, record)
+        msg = super(ColorFormatter, self).format(record)
+        if self._use_color and record.levelname in self.level_color:
+            msg = "{}{}{}".format(self.level_color[record.levelname], msg, Style.RESET_ALL)
+        return msg
 
 
 class ConanOutput(object):
     def __init__(self, quiet=False):
         self._logger = logging.getLogger("conan_out_logger")
         self._stream_handler = None
+        self._file_handler = None
         self._quiet = quiet
         self._color = self._init_colors()
 
@@ -100,6 +103,22 @@ class ConanOutput(object):
             self._stream_handler = TqdmHandler(self._stream)
             self._stream_handler.setFormatter(ColorFormatter("%(message)s", use_color=self._color))
             self._logger.addHandler(self._stream_handler)
+
+            # TODO: Check for 2.0 if we want to use CONAN_TRACE_FILE or any other variable
+            trace_path = os.environ.get("CONAN_TRACE_FILE", None)
+            if trace_path:
+                try:
+                    self._file_handler = logging.FileHandler(trace_path)
+                except:
+                    raise ConanException("Bad CONAN_TRACE_FILE value. The specified "
+                                         "path has to be an absolute path to a file.")
+                self._file_handler.setLevel(logging.DEBUG)
+                file_formatter = ColorFormatter("{{'time':{asctime}, logger_name:'{name}', "
+                                                "level:'{levelname}', message:{message!r}}}",
+                                                style="{", use_color=False)
+                self._file_handler.setFormatter(file_formatter)
+                self._logger.addHandler(self._file_handler)
+
             self._logger.setLevel(logging.INFO)
             self._logger.propagate = False
 
@@ -125,7 +144,7 @@ class ConanOutput(object):
     def is_terminal(self):
         return hasattr(self._stream, "isatty") and self._stream.isatty()
 
-    def _write(self, msg, level, fg=None, bg=None):
+    def _write(self, msg, level):
         if self._scope:
             msg = "{}: {}".format(self.scope, msg)
         self._logger.log(level, msg)
