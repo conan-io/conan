af92ae1cac91aec6a4ba6541efe51c1a5a72d758
memsharded <james@conan.io>
2020-12-04 15:16:19 +0100

    minor renamings

--- conans/client/installer.py
+++ conans/client/installer.py
@@ -15,7 +15,7 @@ from conans.client.importer import remove_imports, run_imports
 from conans.client.packager import update_package_metadata
 from conans.client.recorder.action_recorder import INSTALL_ERROR_BUILDING, INSTALL_ERROR_MISSING, \
     INSTALL_ERROR_MISSING_BUILD_FOLDER
-from conans.client.source import complete_recipe_sources, config_source
+from conans.client.source import retrieve_exports_sources, config_source
 from conans.client.tools.env import no_op
 from conans.client.tools.env import pythonpath
 from conans.errors import (ConanException, ConanExceptionInUserConanfileMethod,
@@ -102,13 +102,14 @@ class _PackageBuilder(object):
         conanfile_path = package_layout.conanfile()
         source_folder = package_layout.source()
 
-        complete_recipe_sources(self._remote_manager, self._cache, conanfile, pref.ref, remotes)
+        retrieve_exports_sources(self._remote_manager, self._cache, conanfile, pref.ref, remotes)
         config_source(export_folder, export_source_folder, scm_sources_folder, source_folder,
                       conanfile, self._output, conanfile_path, pref.ref,
                       self._hook_manager, self._cache)
 
     @staticmethod
     def _copy_sources(conanfile, source_folder, build_folder):
+        # Copies the sources to the build-folder, unless no_copy_source is defined
         _remove_folder_raising(build_folder)
         if not getattr(conanfile, 'no_copy_source', False):
             conanfile.output.info('Copying sources to build folder')
@@ -529,8 +530,8 @@ class BinaryInstaller(object):
             for python_require in python_requires.values():
                 assert python_require.ref.revision is not None, \
                     "Installer should receive python_require.ref always"
-                complete_recipe_sources(self._remote_manager, self._cache,
-                                        python_require.conanfile, python_require.ref, remotes)
+                retrieve_exports_sources(self._remote_manager, self._cache,
+                                         python_require.conanfile, python_require.ref, remotes)
 
         builder = _PackageBuilder(self._cache, output, self._hook_manager, self._remote_manager,
                                   self._generator_manager)
