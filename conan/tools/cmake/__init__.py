1fdab33e191d72b1899bf90201659700d9ab97b2
memsharded <james@conan.io>
2020-12-11 00:48:34 +0100

    duplicate and isolate cmake utilities

--- conan/tools/cmake/__init__.py
+++ conan/tools/cmake/__init__.py
@@ -1,4 +1,4 @@
 # noinspection PyUnresolvedReferences
 
-from .toolchain import CMakeToolchain
-from .cmake import CMake
+from conan.tools.cmake.toolchain import CMakeToolchain
+from conan.tools.cmake.cmake import CMake
