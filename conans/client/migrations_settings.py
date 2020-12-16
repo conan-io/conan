ef36fd2e00cff3df904d83a46d69f02e8536bcf1
memsharded <james@conan.io>
2020-12-16 17:43:28 +0100

    fix migration test

--- conans/client/migrations_settings.py
+++ conans/client/migrations_settings.py
@@ -1883,6 +1883,7 @@ compiler:
                   "19.1", "19.10", "19.11", "19.12", "19.13", "19.14", "19.15", "19.16",
                   "19.2", "19.20", "19.21", "19.22", "19.23", "19.24", "19.25", "19.26", "19.27", "19.28"]
         runtime: [static, dynamic]
+        runtime_type: [Debug, Release]
         cppstd: [None, 14, 17, 20]
     Visual Studio: &visual_studio
         runtime: [MD, MT, MTd, MDd]
