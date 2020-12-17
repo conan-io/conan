1f77b542fd878bd6a4401200676bb50569447e3a
Javier G. Sogo <jgsogo@gmail.com>
2020-12-17 17:37:02 +0100

    [refact] Lock inside CachedFileDownloader class (#8221)

--- conans/client/downloaders/cached_file_downloader.py
+++ conans/client/downloaders/cached_file_downloader.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from contextlib import contextmanager
 from threading import Lock
 
 from six.moves.urllib_parse import urlsplit, urlunsplit
@@ -19,6 +20,19 @@ class CachedFileDownloader(object):
         self._file_downloader = file_downloader
         self._user_download = user_download
 
+    @contextmanager
+    def _lock(self, lock_id):
+        lock = os.path.join(self._cache_folder, "locks", lock_id)
+        with SimpleLock(lock):
+            # Once the process has access, make sure multithread is locked too
+            # as SimpleLock doesn't work multithread
+            thread_lock = self._thread_locks.setdefault(lock, Lock())
+            thread_lock.acquire()
+            try:
+                yield
+            finally:
+                thread_lock.release()
+
     def download(self, url, file_path=None, md5=None, sha1=None, sha256=None, **kwargs):
         """ compatible interface of FileDownloader + checksum
         """
@@ -26,36 +40,29 @@ class CachedFileDownloader(object):
         # If it is a user download, it must contain a checksum
         assert (not self._user_download) or (self._user_download and checksum)
         h = self._get_hash(url, checksum)
-        lock = os.path.join(self._cache_folder, "locks", h)
-        cached_path = os.path.join(self._cache_folder, h)
-        with SimpleLock(lock):
-            # Once the process has access, make sure multithread is locked too
-            # as SimpleLock doesn't work multithread
-            thread_lock = self._thread_locks.setdefault(lock, Lock())
-            thread_lock.acquire()
-            try:
-                if not os.path.exists(cached_path):
-                    self._file_downloader.download(url=url, file_path=cached_path, md5=md5,
-                                                   sha1=sha1, sha256=sha256, **kwargs)
-                else:
-                    # specific check for corrupted cached files, will raise, but do nothing more
-                    # user can report it or "rm -rf cache_folder/path/to/file"
-                    try:
-                        check_checksum(cached_path, md5, sha1, sha256)
-                    except ConanException as e:
-                        raise ConanException("%s\nCached downloaded file corrupted: %s"
-                                             % (str(e), cached_path))
 
-                if file_path is not None:
-                    file_path = os.path.abspath(file_path)
-                    mkdir(os.path.dirname(file_path))
-                    shutil.copy2(cached_path, file_path)
-                else:
-                    with open(cached_path, 'rb') as handle:
-                        tmp = handle.read()
-                    return tmp
-            finally:
-                thread_lock.release()
+        with self._lock(h):
+            cached_path = os.path.join(self._cache_folder, h)
+            if not os.path.exists(cached_path):
+                self._file_downloader.download(url=url, file_path=cached_path, md5=md5,
+                                               sha1=sha1, sha256=sha256, **kwargs)
+            else:
+                # specific check for corrupted cached files, will raise, but do nothing more
+                # user can report it or "rm -rf cache_folder/path/to/file"
+                try:
+                    check_checksum(cached_path, md5, sha1, sha256)
+                except ConanException as e:
+                    raise ConanException("%s\nCached downloaded file corrupted: %s"
+                                         % (str(e), cached_path))
+
+            if file_path is not None:
+                file_path = os.path.abspath(file_path)
+                mkdir(os.path.dirname(file_path))
+                shutil.copy2(cached_path, file_path)
+            else:
+                with open(cached_path, 'rb') as handle:
+                    tmp = handle.read()
+                return tmp
 
     def _get_hash(self, url, checksum=None):
         """ For Api V2, the cached downloads always have recipe and package REVISIONS in the URL,
