e61d197d87e5c7632c84b7eb13c90d2e90d2ce38
czoido <mrgalleta@gmail.com>
2020-12-23 20:33:35 +0100

    remove to not conflict

--- conans/test/functional/toolchains/test_basic.py
+++ conans/test/functional/toolchains/test_basic.py
@@ -144,7 +144,6 @@ class BasicTest(unittest.TestCase):
         self.assertIn("This 'MSBuild' build helper has been deprecated and moved.", client.out)
         self.assertIn("ERROR: conanfile.py: Error in build() method", client.out)
 
-    @pytest.mark.tool_compiler
     @pytest.mark.skipif(six.PY2, reason="The import to sibling fails in Python2")
     def test_old_gnu_tools_imports(self):
         conanfile = textwrap.dedent("""
@@ -161,7 +160,6 @@ class BasicTest(unittest.TestCase):
         self.assertIn("'from conans import MakeToolchain' has been deprecated and moved",
                       client.out)
 
-    @pytest.mark.tool_compiler
     @pytest.mark.skipif(six.PY2, reason="The import to sibling fails in Python2")
     def test_old_write_toolchain_files(self):
         conanfile = textwrap.dedent("""
