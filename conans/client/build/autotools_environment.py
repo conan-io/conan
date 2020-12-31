ff98fa6021c0ce259f0375fdfb4a3f2d2bb12b5b
Luis Martinez de Bartolome <lasote@gmail.com>
2020-12-31 13:45:04 +0100

    Build helpers using generators folder

--- conans/client/build/autotools_environment.py
+++ conans/client/build/autotools_environment.py
@@ -169,7 +169,7 @@ class AutoToolsBuildEnvironment(object):
         else:
             # If we are using pkg_config generator automate the pcs location, otherwise it could
             # read wrong files
-            pkg_env = {"PKG_CONFIG_PATH": [self._conanfile.install_folder]} \
+            pkg_env = {"PKG_CONFIG_PATH": [self._conanfile.layout.generators_folder]} \
                 if "pkg_config" in self._conanfile.generators else None
 
         configure_dir = self._adjust_path(configure_dir)
