1d050595f809c0b8b073f4402509f6e1dca4bb38
SSE4 <tomskside@gmail.com>
2021-01-14 02:28:13 +0700

    Bugfix: add test for AutoToolsBuildEnvironment on Apple platforms (#8118)
    * - add test for AutoToolsBuildEnvironment on Apple platforms
    
    Signed-off-by: SSE4 <tomskside@gmail.com>
    
    * - fix tests
    
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conans/client/build/autotools_environment.py
+++ conans/client/build/autotools_environment.py
@@ -359,6 +359,11 @@ class AutoToolsBuildEnvironment(object):
                                                                                 self._os_sdk,
                                                                                 self._os_subsystem,
                                                                                 self._arch))
+            if "-isysroot" not in concat and platform.system() == "Darwin":
+                tmp_compilation_flags.extend(["-isysroot",
+                                              tools.XCRun(self._conanfile.settings).sdk_path])
+            if "-arch" not in concat and self._arch:
+                tmp_compilation_flags.extend(["-arch", tools.to_apple_arch(self._arch)])
 
         cxx_flags = append(tmp_compilation_flags, self.cxx_flags, self.cppstd_flag)
         c_flags = tmp_compilation_flags
