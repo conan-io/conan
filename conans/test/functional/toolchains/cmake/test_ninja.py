4dbf60419c00a0b32883e6ed9640672a9fc1b2e4
Uilian Ries <uilianries@gmail.com>
2021-01-22 19:05:06 -0300

    Covert to pytest case
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/test/functional/toolchains/cmake/test_ninja.py
+++ conans/test/functional/toolchains/cmake/test_ninja.py
@@ -13,7 +13,7 @@ from conans.test.functional.toolchains.ios._utils import create_library
 
 
 @pytest.mark.tool_cmake
-class CMakeNinjaTestCase(unittest.TestCase):
+class TestCMakeNinja:
     # This test assumes that 'CMake' and 'Ninja' are available in the system
 
     conanfile = textwrap.dedent("""
@@ -50,11 +50,11 @@ class CMakeNinjaTestCase(unittest.TestCase):
         """)
 
     @classmethod
-    def setUpClass(cls):
+    def setup_class(cls):
         if not which("ninja"):
             raise unittest.SkipTest("Ninja expected in PATH")
 
-    def setUp(self):
+    def setup_method(self):
         self.client = TestClient(path_with_spaces=False)
         create_library(self.client)
         self.client.save({'conanfile.py': self.conanfile})
@@ -71,21 +71,18 @@ class CMakeNinjaTestCase(unittest.TestCase):
         self.client.run_command('cmake . -G"Ninja" -DCMAKE_TOOLCHAIN_FILE={}'
                                 .format(CMakeToolchainBase.filename))
         ninja_build_file = open(os.path.join(self.client.current_folder, 'build.ninja'), 'r').read()
-        if build_type == "Release":
-            self.assertIn("CONFIGURATION = Release", ninja_build_file)
-        else:
-            self.assertIn("CONFIGURATION = Debug", ninja_build_file)
+        assert "CONFIGURATION = {}".format(build_type) in ninja_build_file
 
         self.client.run_command('ninja')
         if shared:
-            self.assertIn("Linking CXX shared library libhello.so", self.client.out)
+            assert "Linking CXX shared library libhello.so" in self.client.out
             self.client.run_command("objdump -f libhello.so")
-            self.assertIn("architecture: i386:x86-64", self.client.out)
-            self.assertIn("DYNAMIC", self.client.out)
+            assert "architecture: i386:x86-64" in self.client.out
+            assert "DYNAMIC" in self.client.out
         else:
-            self.assertIn("Linking CXX static library libhello.a", self.client.out)
+            assert "Linking CXX static library libhello.a" in self.client.out
             self.client.run_command("objdump -f libhello.a")
-            self.assertIn("architecture: i386:x86-64", self.client.out)
+            assert "architecture: i386:x86-64"in  self.client.out
 
     @pytest.mark.skipif(platform.system() != "Windows", reason="Only windows")
     @pytest.mark.parametrize("build_type,shared", [("Release", False), ("Debug", True)])
@@ -113,11 +110,11 @@ class CMakeNinjaTestCase(unittest.TestCase):
     @pytest.mark.skipif(platform.system() != "Windows", reason="Only windows")
     @pytest.mark.parametrize("build_type,shared", [("Release", False), ("Debug", True)])
     @pytest.mark.tool_compiler
-    def test_locally_build_windows_debug(self):
+    def test_locally_build_windows_debug(self, build_type, shared):
         """ Ninja build must proceed using default profile and cmake build (Windows Debug)
         """
-        client = TestClient(path_with_spaces=False)
-        client.save({"conanfile.py": self.conanfile,
+        self.client = TestClient(path_with_spaces=False)
+        self.client.save({"conanfile.py": self.conanfile,
                      "main.cpp": self.main_cpp,
                      "CMakeLists.txt": self.cmake})
         win_host = textwrap.dedent("""
@@ -129,43 +126,41 @@ class CMakeNinjaTestCase(unittest.TestCase):
             compiler.runtime=MTd
             build_type=Debug
              """)
-        client.save({"win": win_host})
-        client.run("install . -pr=win")
+        self.client.save({"win": win_host})
+        self.client.run("install . -pr=win")
         # Ninja is single-configuration
         # It is necessary to set architecture=x86 here, otherwise final architecture is wrong
         vcvars = vcvars_command("15", architecture="x86")
-        client.run("install . -pr=win")
-        client.run_command('{} && cmake . -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake '
+        self.client.run("install . -pr=win")
+        self.client.run_command('{} && cmake . -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake '
                            .format(vcvars))
-        client.run_command("{} && cmake --build .".format(vcvars))
-        client.run_command("App")
-        self.assertIn("main: Debug!", client.out)
-        self.assertIn("main _M_IX86 defined", client.out)
-        self.assertIn("main _MSC_VER19", client.out)
-        self.assertIn("main _MSVC_LANG2014", client.out)
+        self.client.run_command("{} && cmake --build .".format(vcvars))
+        self.client.run_command("App")
+        assert "main: Debug!" in self.client.out
+        assert "main _M_IX86 defined" in self.client.out
+        assert "main _MSC_VER19" in self.client.out
+        assert "main _MSVC_LANG2014" in self.client.out
 
-        check_vs_runtime("App.exe", client, "15", build_type="Debug", static=True)
-
+        check_vs_runtime("App.exe", self.client, "15", build_type="Debug", static=True)
 
     @pytest.mark.skipif(platform.system() != "Darwin", reason="Requires apple-clang")
     @pytest.mark.parametrize("build_type,shared", [("Release", False), ("Debug", True)])
     @pytest.mark.tool_compiler
     def test_locally_build_macos(self, build_type, shared):
-        self.client.run('install . -s os=Macos -s arch=x86_64 -s build_type={} -o hello:shared={}'
-                        .format(build_type, shared))
-
+        self.client.run(
+            'install . -s os=Macos -s arch=x86_64 -s build_type={} -o hello:shared={}'
+            .format(build_type, shared))
         self.client.run_command('cmake . -G"Ninja" -DCMAKE_TOOLCHAIN_FILE={}'
                                 .format(CMakeToolchainBase.filename))
         ninja_build_file = open(os.path.join(self.client.current_folder, 'build.ninja'), 'r').read()
-        if build_type == "Release":
-            self.assertIn("CONFIGURATION = Release", ninja_build_file)
-        else:
-            self.assertIn("CONFIGURATION = Debug", ninja_build_file)
+        assert "CONFIGURATION = {}".format(build_type) in ninja_build_file
 
         self.client.run_command('ninja')
-
         if shared:
-            pass
+            assert "Linking CXX shared library libhello.dylib" in self.client.out
+            self.client.run_command("lipo -info libhello.dylib")
+            assert "Non-fat file: libhello.dylib is architecture: x86_64" in self.client.out
         else:
+            assert "Linking CXX static library libhello.a" in self.client.out
             self.client.run_command("lipo -info libhello.a")
-            self.assertIn("Non-fat file: libhello.a is architecture: arm64", self.client.out)
+            assert "Non-fat file: libhello.a is architecture: x86_64" in self.client.out
