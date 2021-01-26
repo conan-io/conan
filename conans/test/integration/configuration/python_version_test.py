f798604a5055ad0fbd2c6af14b127af785a7785f
czoido <mrgalleta@gmail.com>
2021-01-26 20:13:03 +0100

    remove test

--- conans/test/integration/configuration/python_version_test.py
+++ conans/test/integration/configuration/python_version_test.py
@@ -15,10 +15,6 @@ class PythonVersionTest(unittest.TestCase):
         self.client.run("--help")
         self.assertIn(expected_message, str(self.client.out))
 
-    def test_py2_warning_message(self):
-        self._validate_message("Python 2 is deprecated as of 01/01/2020 and Conan has stopped\n"
-                               "supporting it officially")
-
     @pytest.mark.skipif(sys.version_info.major != 3 or sys.version_info.minor != 4, reason="Requires Python 3.4")
     def test_py34_warning_message(self):
         self._validate_message("Python 3.4 support has been dropped. It is strongly "
