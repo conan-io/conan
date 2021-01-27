cac220ea033c3deecb3cab03a9585d8dc781bedc
memsharded <james@conan.io>
2021-01-27 13:46:25 +0100

    removing import

--- conans/client/conanfile/configure.py
+++ conans/client/conanfile/configure.py
@@ -1,4 +1,4 @@
-from conans.errors import (conanfile_exception_formatter, ConanInvalidConfiguration)
+from conans.errors import conanfile_exception_formatter
 from conans.model.conan_file import get_env_context_manager
 from conans.util.conan_v2_mode import conan_v2_error
 from conans.util.misc import make_tuple
