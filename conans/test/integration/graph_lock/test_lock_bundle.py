cc8e05e834016ec88e05f6715381f2261dcc48c7
memsharded <james@conan.io>
2021-02-02 13:56:44 +0100

    fix py2 tests

--- conans/test/integration/graph_lock/test_lock_bundle.py
+++ conans/test/integration/graph_lock/test_lock_bundle.py
@@ -23,12 +23,24 @@ def test_basic():
 
     client.run("lock create --ref=app1/0.1 -s os=Windows --lockfile=app1_base.lock "
                "--lockfile-out=app1_windows.lock")
+    assert "app1/0.1:3bcd6800847f779e0883ee91b411aad9ddd8e83c - Missing" in client.out
+    assert "pkga/0.1:3475bd55b91ae904ac96fde0f106a136ab951a5e - Missing" in client.out
+    assert "pkgb/0.1:cfd10f60aeaa00f5ca1f90b5fe97c3fe19e7ec23 - Missing" in client.out
     client.run("lock create --ref=app1/0.1 -s os=Linux  --lockfile=app1_base.lock "
                "--lockfile-out=app1_linux.lock")
+    assert "app1/0.1:60fbb0a22359b4888f7ecad69bcdfcd6e70e2784 - Missing" in client.out
+    assert "pkga/0.1:cb054d0b3e1ca595dc66bc2339d40f1f8f04ab31 - Missing" in client.out
+    assert "pkgb/0.1:cfd10f60aeaa00f5ca1f90b5fe97c3fe19e7ec23 - Missing" in client.out
     client.run("lock create --ref=app2/0.1 -s os=Windows  --lockfile=app2_base.lock "
                "--lockfile-out=app2_windows.lock")
+    assert "app2/0.1:0f886d82040d47739aa363db84eef5fe4c958c23 - Missing" in client.out
+    assert "pkga/0.1:3475bd55b91ae904ac96fde0f106a136ab951a5e - Missing" in client.out
+    assert "pkgb/0.2:cfd10f60aeaa00f5ca1f90b5fe97c3fe19e7ec23 - Missing" in client.out
     client.run("lock create --ref=app2/0.1 -s os=Linux  --lockfile=app2_base.lock "
                "--lockfile-out=app2_linux.lock")
+    assert "app2/0.1:156f38906bdcdceba1b26a206240cf199619fee1 - Missing" in client.out
+    assert "pkga/0.1:cb054d0b3e1ca595dc66bc2339d40f1f8f04ab31 - Missing" in client.out
+    assert "pkgb/0.2:cfd10f60aeaa00f5ca1f90b5fe97c3fe19e7ec23 - Missing" in client.out
 
     client.run("lock bundle create app1_windows.lock app1_linux.lock "
                "app2_windows.lock app2_linux.lock --bundle-out=lock1.bundle")
@@ -49,7 +61,8 @@ def test_basic():
             pkg_ids = bundle[ref]["package_id"]
             for pkg_id, lockfile_info in pkg_ids.items():
                 lockfiles = lockfile_info["lockfiles"]
-                lockfile = next(iter(lockfiles))
+                lockfile = next(iter(sorted(lockfiles)))
+
                 client.run("install {ref} --build={ref} --lockfile={lockfile} "
                            "--lockfile-out={lockfile}".format(ref=ref, lockfile=lockfile))
                 client.run("lock bundle update lock1.bundle")
@@ -59,27 +72,33 @@ def test_basic():
     nodes = app1_win.graph_lock.nodes
     assert nodes["1"].modified is True
     assert nodes["1"].ref.full_str() == "app1/0.1#584778f98ba1d0eb7c80a5ae1fe12fe2"
+    assert nodes["1"].package_id == "3bcd6800847f779e0883ee91b411aad9ddd8e83c"
     assert nodes["1"].prev == "c6658a5c66393cf4d210c35b5fbf34f8"
 
     assert nodes["2"].modified is True
     assert nodes["2"].ref.full_str() == "pkgb/0.1#cd8f22d6f264f65398d8c534046e8e20"
-    assert nodes["2"].prev == "615cacb4d607885967a627eab1d1069d"
+    assert nodes["2"].package_id == "cfd10f60aeaa00f5ca1f90b5fe97c3fe19e7ec23"
+    assert nodes["2"].prev == "d61b9f421cada3b4d8e39540b0aea3d0"
 
     assert nodes["3"].modified is True
     assert nodes["3"].ref.full_str() == "pkga/0.1#f096d7d54098b7ad7012f9435d9c33f3"
+    assert nodes["3"].package_id == "3475bd55b91ae904ac96fde0f106a136ab951a5e"
     assert nodes["3"].prev == "d0f0357277b3417d3984b5a9a85bbab6"
 
-    app2_win = GraphLockFile.load(os.path.join(client.current_folder, "app2_linux.lock"),
-                                  client.cache.config.revisions_enabled)
-    nodes = app2_win.graph_lock.nodes
+    app2_linux = GraphLockFile.load(os.path.join(client.current_folder, "app2_linux.lock"),
+                                    client.cache.config.revisions_enabled)
+    nodes = app2_linux.graph_lock.nodes
     assert nodes["1"].modified is True
     assert nodes["1"].ref.full_str() == "app2/0.1#3850895c1eac8223c43c71d525348019"
+    assert nodes["1"].package_id == "156f38906bdcdceba1b26a206240cf199619fee1"
     assert nodes["1"].prev == "f1ca88c668b7f573037d09cb04be0e6f"
 
     assert nodes["2"].modified is True
     assert nodes["2"].ref.full_str() == "pkgb/0.2#cd8f22d6f264f65398d8c534046e8e20"
-    assert nodes["2"].prev == "615cacb4d607885967a627eab1d1069d"
+    assert nodes["2"].package_id == "cfd10f60aeaa00f5ca1f90b5fe97c3fe19e7ec23"
+    assert nodes["2"].prev == "d61b9f421cada3b4d8e39540b0aea3d0"
 
     assert nodes["3"].modified is True
     assert nodes["3"].ref.full_str() == "pkga/0.1#f096d7d54098b7ad7012f9435d9c33f3"
+    assert nodes["3"].package_id == "cb054d0b3e1ca595dc66bc2339d40f1f8f04ab31"
     assert nodes["3"].prev == "9e99cfd92d0d7df79d687b01512ce844"
