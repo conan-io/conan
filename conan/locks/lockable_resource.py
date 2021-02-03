15fc970184f92a700578e2f4834f9ac26f3d4ef6
jgsogo <jgsogo@gmail.com>
2021-02-03 18:33:37 +0100

    class per file

---
+++ conan/locks/lockable_resource.py
@@ -0,0 +1,17 @@
+from conan.locks.locks_manager import LocksManager
+
+
+class LockableResource:
+    def __init__(self, manager: LocksManager, resource: str, blocking: bool, wait: bool):
+        self._manager = manager
+        self._resource = resource
+        self._bloking = blocking
+        self._wait = wait
+        self._lock_handler = None
+
+    def __enter__(self):
+        self._lock_handler = self._manager.try_acquire(self._resource, self._bloking, self._wait)
+
+    def __exit__(self, type, value, traceback):
+        assert self._lock_handler
+        self._manager.release(self._lock_handler)
