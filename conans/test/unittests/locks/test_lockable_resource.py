c274ad4ea77b6a66a38421c7893450964015a7e6
jgsogo <jgsogo@gmail.com>
2021-02-03 18:59:20 +0100

    Add some testing for basic behavior

--- conans/test/unittests/locks/test_lockable_resource.py
+++ conans/test/unittests/locks/test_lockable_resource.py
@@ -5,7 +5,7 @@ from locks.locks_manager import LocksManager
 
 class TestLockableResource:
 
-    def test_block(self):
+    def test_with_writers(self):
         manager = LocksManager.create('memory')
         resource = 'res'
 
@@ -23,3 +23,18 @@ class TestLockableResource:
                 with l1:
                     pass
             assert "Resource 'res' is already blocked" == str(excinfo.value)
+
+    def test_readers(self):
+        manager = LocksManager.create('memory')
+        resource = 'res'
+
+        l1 = manager.get_lockable_resource(resource, blocking=False, wait=False)
+        l2 = manager.get_lockable_resource(resource, blocking=False, wait=False)
+
+        with l1:
+            with l2:
+                pass
+
+        with l2:
+            with l1:
+                pass
