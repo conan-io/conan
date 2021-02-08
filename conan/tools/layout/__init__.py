68db2f8976da7c11662e9bf934b5a185ed321cf4
Luis Martinez de Bartolome <lasote@gmail.com>
2021-02-08 10:39:48 +0100

    Trying clion

---
+++ conan/tools/layout/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+
+def clion_layout(conanfile):
+    base = "cmake-build-{}".format(str(conanfile.settings.build_type).lower())
+    conanfile.layout.build.folder = base
+    conanfile.layout.generators.folder = os.path.join(base, "generators")
+    conanfile.layout.source.folder = "."
+    conanfile.layout.source.cpp_info.includedirs = [".", "include"]
