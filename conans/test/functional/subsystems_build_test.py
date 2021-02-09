45f8d97d0c58a95753750a08f8746cdd4dc74643
czoido <mrgalleta@gmail.com>
2021-02-09 14:08:44 +0100

    minor changes

--- conans/test/functional/subsystems_build_test.py
+++ conans/test/functional/subsystems_build_test.py
@@ -5,24 +5,22 @@ import pytest
 from conans.test.utils.tools import TestClient
 
 
-@pytest.fixture()
-def client():
-    return TestClient()
-
-
 @pytest.mark.skipif(platform.system() != "Windows", reason="Tests Windows Subsystems")
 class TestSubsystemsBuild:
 
     @pytest.mark.tool_msys2
-    def test_msys2_available(self, client):
+    def test_msys2_available(self):
+        client = TestClient()
         client.run_command('uname')
         assert "MSYS" in client.out
 
     @pytest.mark.tool_cygwin
-    def test_cygwin_available(self, client):
+    def test_cygwin_available(self):
+        client = TestClient()
         client.run_command('uname')
         assert "CYGWIN" in client.out
 
-    def test_tool_not_available(self, client):
+    def test_tool_not_available(self):
+        client = TestClient()
         client.run_command('uname', assert_error=True)
         assert "command not found" in client.out
