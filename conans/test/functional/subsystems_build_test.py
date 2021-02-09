cc4c62ae78c5455dd33eb3dde90c1429b9de66a5
czoido <mrgalleta@gmail.com>
2021-02-09 13:41:26 +0100

    remove test

--- conans/test/functional/subsystems_build_test.py
+++ conans/test/functional/subsystems_build_test.py
@@ -18,15 +18,11 @@ class TestSubsystemsBuild:
         client.run_command('uname')
         assert "MSYS" in client.out
 
-    def test_msys2_not_available(self, client):
-        client.run_command('uname', assert_error=True)
-        assert "command not found" in client.out
-
     @pytest.mark.tool_cygwin
     def test_cygwin_available(self, client):
         client.run_command('uname')
         assert "CYGWIN" in client.out
 
-    def test_cygwin_not_available(self, client):
+    def test_tool_not_available(self, client):
         client.run_command('uname', assert_error=True)
         assert "command not found" in client.out
