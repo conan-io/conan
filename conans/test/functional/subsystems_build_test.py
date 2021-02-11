edc269b2edcae72c0cc056e95ec2ffe9b1d8fa3b
memsharded <james@conan.io>
2021-02-12 00:13:53 +0100

    minor changes Windows subsystem tests

--- conans/test/functional/subsystems_build_test.py
+++ conans/test/functional/subsystems_build_test.py
@@ -9,38 +9,42 @@ from conans.test.utils.tools import TestClient
 
 
 @pytest.mark.skipif(platform.system() != "Windows", reason="Tests Windows Subsystems")
-class TestSubsystemsBuild:
-
-    @pytest.fixture
-    def client(self):
-        return TestClient()
+class TestSubsystems:
 
     @pytest.mark.tool_msys2
-    def test_msys2_available(self, client):
+    def test_msys2_available(self):
+        client = TestClient()
         client.run_command('uname')
         assert "MSYS" in client.out
 
     @pytest.mark.tool_cygwin
-    def test_cygwin_available(self, client):
+    def test_cygwin_available(self):
+        client = TestClient()
         client.run_command('uname')
         assert "CYGWIN" in client.out
 
     @pytest.mark.tool_msys2
     @pytest.mark.tool_mingw32
-    def test_mingw32_available(self, client):
+    def test_mingw32_available(self):
+        client = TestClient()
         client.run_command('uname')
         assert "MINGW32_NT" in client.out
 
     @pytest.mark.tool_msys2
     @pytest.mark.tool_mingw64
-    def test_mingw64_available(self, client):
+    def test_mingw64_available(self):
+        client = TestClient()
         client.run_command('uname')
         assert "MINGW64_NT" in client.out
 
-    def test_tool_not_available(self, client):
+    def test_tool_not_available(self):
+        client = TestClient()
         client.run_command('uname', assert_error=True)
         assert "'uname' is not recognized as an internal or external command" in client.out
 
+
+@pytest.mark.skipif(platform.system() != "Windows", reason="Tests Windows Subsystems")
+class TestSubsystemsBuild:
     makefile = textwrap.dedent("""
         .PHONY: all
         all: app
@@ -54,20 +58,18 @@ class TestSubsystemsBuild:
 
     def _build(self, client):
         main_cpp = gen_function_cpp(name="main")
-
         client.save({"Makefile": self.makefile,
-                          "main.cpp": main_cpp})
-
+                     "main.cpp": main_cpp})
         client.run_command("make")
-
         client.run_command("app")
 
     @pytest.mark.tool_msys2
-    def test_msys(self, client):
+    def test_msys(self):
         """
         native MSYS environment, binaries depend on MSYS runtime (msys-2.0.dll)
         posix-compatible, intended to be run only in MSYS environment (not in pure Windows)
         """
+        client = TestClient()
         # pacman -S gcc
         self._build(client)
 
@@ -79,10 +81,11 @@ class TestSubsystemsBuild:
 
     @pytest.mark.tool_msys2
     @pytest.mark.tool_mingw64
-    def test_mingw64(self, client):
+    def test_mingw64(self):
         """
         64-bit GCC, binaries for generic Windows (no dependency on MSYS runtime)
         """
+        client = TestClient()
         # pacman -S mingw-w64-x86_64-gcc
         self._build(client)
 
@@ -94,10 +97,11 @@ class TestSubsystemsBuild:
 
     @pytest.mark.tool_msys2
     @pytest.mark.tool_mingw32
-    def test_mingw32(self, client):
+    def test_mingw32(self):
         """
         32-bit GCC, binaries for generic Windows (no dependency on MSYS runtime)
         """
+        client = TestClient()
         # pacman -S mingw-w64-i686-gcc
         self._build(client)
 
@@ -108,14 +112,14 @@ class TestSubsystemsBuild:
         assert "__MSYS__" not in client.out
 
     @pytest.mark.tool_cygwin
-    def test_cygwin(self, client):
+    def test_cygwin(self):
         """
         Cygwin environment, binaries depend on Cygwin runtime (cygwin1.dll)
         posix-compatible, intended to be run only in Cygwin environment (not in pure Windows)
         """
+        client = TestClient()
         # install "gcc-c++" and "make" packages
         self._build(client)
-
         check_exe_run(client.out, "main", "gcc", None, "Debug", "x86_64", None)
 
         assert "__CYGWIN__" in client.out
