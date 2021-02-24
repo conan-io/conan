3d65438aff346d8f6f69f99e9bd8097576adf042
czoido <mrgalleta@gmail.com>
2021-02-24 18:56:32 +0100

    fix returns

--- conan/tools/files/files.py
+++ conan/tools/files/files.py
@@ -4,19 +4,19 @@ from conans.client.tools.net import ftp_download as util_ftp_download, download 
 
 
 def load(conanfile, path, binary=False, encoding="auto"):
-    util_load(path, binary=binary, encoding=encoding)
+    return util_load(path, binary=binary, encoding=encoding)
 
 
 def save(conanfile, path, content, append=False):
     # TODO: All this three functions: save, save_append and this one should be merged into one.
     if append:
-        save_append(path=path, content=content)
+        return save_append(path=path, content=content)
     else:
-        files_save(path=path, content=content, only_if_modified=False)
+        return files_save(path=path, content=content, only_if_modified=False)
 
 
 def mkdir(conanfile, path):
-    util_mkdir(path)
+    return util_mkdir(path)
 
 
 def ftp_download(conanfile, ip, filename, login='', password=''):
