9393a0d5df996c07ef2724823d4b307a3782d9fc
czoido <mrgalleta@gmail.com>
2021-02-24 18:37:42 +0100

    test download

--- conans/test/functional/tools/test_files.py
+++ conans/test/functional/tools/test_files.py
@@ -1,6 +1,11 @@
+import os
 import textwrap
 
-from conans.test.utils.tools import TestClient
+from bottle import static_file, request
+
+from conans.test.utils.test_files import temp_folder
+from conans.test.utils.tools import TestClient, StoppableThreadBottle
+from conans.util.files import save
 
 
 class TestConanToolFiles:
@@ -28,3 +33,33 @@ class TestConanToolFiles:
         client = TestClient()
         client.save({"conanfile.py": conanfile})
         client.run("install .")
+
+    def test_download(self):
+        http_server = StoppableThreadBottle()
+        file_path = os.path.join(temp_folder(), "myfile.txt")
+        save(file_path, "some content")
+
+        @http_server.server.get("/myfile.txt")
+        def get_file():
+            return static_file(os.path.basename(file_path), os.path.dirname(file_path))
+
+        http_server.run_server()
+
+        conanfile = textwrap.dedent("""
+            from conans import ConanFile
+            from conan.tools.files import download
+
+            class Pkg(ConanFile):
+                name = "mypkg"
+                version = "1.0"
+                def source(self):
+                    download(self,
+                             "http://localhost:{}/myfile.txt",
+                             "myfile.txt")
+            """.format(http_server.port))
+
+        client = TestClient()
+        client.save({"conanfile.py": conanfile})
+        client.run("source .")
+        local_path = os.path.join(client.current_folder, "myfile.txt")
+        assert os.path.exists(local_path)
