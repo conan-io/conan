140caf2e42b1b835a408080f0d8f188074419430
czoido <mrgalleta@gmail.com>
2021-02-24 19:49:51 +0100

    separate tools

--- conan/tools/files/files.py
+++ conan/tools/files/files.py
@@ -1,10 +1,58 @@
-from conans.util.files import load as util_load, save as files_save, save_append, mkdir as util_mkdir
-from conans.client.tools.net import ftp_download as util_ftp_download, download as util_download, \
-     get as util_get
+import errno
+import os
+
+from conans.errors import ConanException
+from conans.util.files import decode_text, to_file_bytes
+from conans.client.tools.files import unzip
+from conans.client.downloaders.download import run_downloader
 
 
 def load(conanfile, path, binary=False, encoding="auto"):
-    return util_load(path, binary=binary, encoding=encoding)
+    """ Loads a file content """
+    with open(path, 'rb') as handle:
+        tmp = handle.read()
+        return tmp if binary else decode_text(tmp, encoding)
+
+
+def save_append(path, content, encoding="utf-8"):
+    try:
+        os.makedirs(os.path.dirname(path))
+    except Exception:
+        pass
+
+    with open(path, "ab") as handle:
+        handle.write(to_file_bytes(content, encoding=encoding))
+
+
+def files_save(path, content, only_if_modified=False, encoding="utf-8"):
+    """
+    Saves a file with given content
+    Params:
+        path: path to write file to
+        content: contents to save in the file
+        only_if_modified: file won't be modified if the content hasn't changed
+        encoding: target file text encoding
+    """
+    dir_path = os.path.dirname(path)
+    if not os.path.isdir(dir_path):
+        try:
+            os.makedirs(dir_path)
+        except OSError as error:
+            if error.errno not in (errno.EEXIST, errno.ENOENT):
+                raise OSError("The folder {} does not exist and could not be created ({})."
+                              .format(dir_path, error.strerror))
+        except Exception:
+            raise
+
+    new_content = to_file_bytes(content, encoding)
+
+    if only_if_modified and os.path.exists(path):
+        old_content = load(path, binary=True, encoding=encoding)
+        if old_content == new_content:
+            return
+
+    with open(path, "wb") as handle:
+        handle.write(new_content)
 
 
 def save(conanfile, path, content, append=False):
@@ -16,16 +64,113 @@ def save(conanfile, path, content, append=False):
 
 
 def mkdir(conanfile, path):
-    return util_mkdir(path)
+    """Recursive mkdir, doesnt fail if already existing"""
+    if os.path.exists(path):
+        return
+    os.makedirs(path)
+
+
+def get(conanfile, url, md5='', sha1='', sha256='', destination=".", filename="", keep_permissions=False,
+        pattern=None, verify=True, retry=None, retry_wait=None,
+        overwrite=False, auth=None, headers=None, strip_root=False):
+    """ high level downloader + unzipper + (optional hash checker) + delete temporary zip
+    """
+    requester = conanfile._conan_requester
+    output = conanfile.output
+    if not filename:  # deduce filename from the URL
+        url_base = url[0] if isinstance(url, (list, tuple)) else url
+        if "?" in url_base or "=" in url_base:
+            raise ConanException("Cannot deduce file name from the url: '{}'. Use 'filename' "
+                                 "parameter.".format(url_base))
+        filename = os.path.basename(url_base)
+
+    download(url, filename, out=output, requester=requester, verify=verify,
+             retry=retry, retry_wait=retry_wait, overwrite=overwrite, auth=auth, headers=headers,
+             md5=md5, sha1=sha1, sha256=sha256)
+    unzip(filename, destination=destination, keep_permissions=keep_permissions, pattern=pattern,
+          output=output, strip_root=strip_root)
+    os.unlink(filename)
 
 
 def ftp_download(conanfile, ip, filename, login='', password=''):
-    return util_ftp_download(ip, filename, login=login, password=password)
+    import ftplib
+    try:
+        ftp = ftplib.FTP(ip)
+        ftp.login(login, password)
+        filepath, filename = os.path.split(filename)
+        if filepath:
+            ftp.cwd(filepath)
+        with open(filename, 'wb') as f:
+            ftp.retrbinary('RETR ' + filename, f.write)
+    except Exception as e:
+        try:
+            os.unlink(filename)
+        except OSError:
+            pass
+        raise ConanException("Error in FTP download from %s\n%s" % (ip, str(e)))
+    finally:
+        try:
+            ftp.quit()
+        except Exception:
+            pass
 
 
-def download(conanfile, *args, **kwargs):
-    return util_download(out=conanfile.output, requester=conanfile._conan_requester, *args, **kwargs)
+def download(conanfile, url, filename, verify=True, out=None, retry=None, retry_wait=None, overwrite=False,
+             auth=None, headers=None, requester=None, md5='', sha1='', sha256=''):
+    """Retrieves a file from a given URL into a file with a given filename.
+       It uses certificates from a list of known verifiers for https downloads,
+       but this can be optionally disabled.
 
+    :param url: URL to download. It can be a list, which only the first one will be downloaded, and
+                the follow URLs will be used as mirror in case of download error.
+    :param filename: Name of the file to be created in the local storage
+    :param verify: When False, disables https certificate validation
+    :param out: An object with a write() method can be passed to get the output. stdout will use if
+                not specified
+    :param retry: Number of retries in case of failure. Default is overriden by general.retry in the
+                  conan.conf file or an env variable CONAN_RETRY
+    :param retry_wait: Seconds to wait between download attempts. Default is overriden by
+                       general.retry_wait in the conan.conf file or an env variable CONAN_RETRY_WAIT
+    :param overwrite: When True, Conan will overwrite the destination file if exists. Otherwise it
+                      will raise an exception
+    :param auth: A tuple of user and password to use HTTPBasic authentication
+    :param headers: A dictionary with additional headers
+    :param requester: HTTP requests instance
+    :param md5: MD5 hash code to check the downloaded file
+    :param sha1: SHA-1 hash code to check the downloaded file
+    :param sha256: SHA-256 hash code to check the downloaded file
+    :return: None
+    """
+    out = conanfile.output
+    requester = conanfile._conan_requester
+    config = conanfile._conan_config
 
-def get(conanfile, *args, **kwargs):
-    return util_get(output=conanfile.output, requester=conanfile._conan_requester, *args, **kwargs)
+    # It might be possible that users provide their own requester
+    retry = retry if retry is not None else config.retry
+    retry = retry if retry is not None else 1
+    retry_wait = retry_wait if retry_wait is not None else config.retry_wait
+    retry_wait = retry_wait if retry_wait is not None else 5
+
+    checksum = sha256 or sha1 or md5
+
+    def _download_file(file_url):
+        # The download cache is only used if a checksum is provided, otherwise, a normal download
+        run_downloader(requester=requester, output=out, verify=verify, config=config,
+                       user_download=True, use_cache=bool(config and checksum), url=file_url,
+                       file_path=filename, retry=retry, retry_wait=retry_wait, overwrite=overwrite,
+                       auth=auth, headers=headers, md5=md5, sha1=sha1, sha256=sha256)
+        out.writeln("")
+
+    if not isinstance(url, (list, tuple)):
+        _download_file(url)
+    else:  # We were provided several URLs to try
+        for url_it in url:
+            try:
+                _download_file(url_it)
+                break
+            except Exception as error:
+                message = "Could not download from the URL {}: {}.".format(url_it, str(error))
+                out.warn(message + " Trying another mirror.")
+        else:
+            raise ConanException("All downloads from ({}) URLs have failed.".format(len(url)))
+
