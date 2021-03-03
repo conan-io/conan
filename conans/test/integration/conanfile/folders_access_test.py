7d10dbe018e30f6fee7bfc3d983b4ce0ca6e6152
czoido <mrgalleta@gmail.com>
2021-03-03 15:51:36 +0100

    fix test

--- conans/test/integration/conanfile/folders_access_test.py
+++ conans/test/integration/conanfile/folders_access_test.py
@@ -161,19 +161,17 @@ class TestFoldersAccess(unittest.TestCase):
                           "local_command": True}
         self.client.save({"conanfile.py": c1}, clean_first=True)
         self.client.run("build .", assert_error=True)
-        self.assertIn("ERROR: conanbuildinfo.txt file not found", self.client.out)
+        self.assertNotIn("ERROR: conanbuildinfo.txt file not found", self.client.out)
 
         c1 = conanfile % {"no_copy_source": False, "source_with_infos": False,
                           "local_command": True}
         self.client.save({"conanfile.py": c1}, clean_first=True)
-        self.client.run("install . --build missing")
-        self.client.run("build .")
+        self.client.run("build . --build missing")
 
         c1 = conanfile % {"no_copy_source": True, "source_with_infos": False,
                           "local_command": True}
         self.client.save({"conanfile.py": c1}, clean_first=True)
-        self.client.run("install . --build missing")
-        self.client.run("build .")
+        self.client.run("build . --build missing")
 
     def test_package_local_command(self):
         c1 = conanfile % {"no_copy_source": False, "source_with_infos": False,
