efb025e5b1b4ab656c1eea42722e5ccbfaff1ba8
danimtb <danimanzaneque@gmail.com>
2021-03-08 10:39:16 +0100

    Add diamond test

--- conans/test/integration/py_requires/python_requires_test.py
+++ conans/test/integration/py_requires/python_requires_test.py
@@ -895,3 +895,77 @@ def test_transitive_python_requires():
         """)
     client.save({"conanfile.py": conanfile})
     client.run("create . @user/channel")
+    assert "consumer/1.0@user/channel: Calling build()\nconsumer/1.0@user/channel: 123, 234" in \
+           client.out
+
+
+def test_transitive_diamond_python_requires():
+    client = TestClient()
+    conanfile = textwrap.dedent("""
+        from conans import ConanFile
+        myvar = 123
+        def myfunct():
+            return 234
+        class SharedFunction(ConanFile):
+            name = "shared-function"
+            version = "1.0"
+        """)
+    client.save({"conanfile.py": conanfile})
+    client.run("export . @user/channel")
+
+    conanfile = textwrap.dedent("""
+        from conans import ConanFile
+        myvar = 222
+        def myfunct():
+            return 2222
+        class SharedFunction2(ConanFile):
+            name = "shared-function2"
+            version = "1.0"
+        """)
+    client.save({"conanfile.py": conanfile})
+    client.run("export . @user/channel")
+
+    conanfile = textwrap.dedent("""
+        from conans import ConanFile
+        class BaseClass(ConanFile):
+            name = "base-class"
+            version = "1.0"
+            python_requires = "shared-function/1.0@user/channel", "shared-function2/1.0@user/channel"
+            def build(self):
+                pyreqs = self.python_requires
+                v = pyreqs["shared-function"].module.myvar  # v will be 123
+                f = pyreqs["shared-function2"].module.myfunct()  # f will be 2222
+                self.output.info("%s, %s" % (v, f))
+        """)
+    client.save({"conanfile.py": conanfile})
+    client.run("export . user/channel")
+
+    conanfile = textwrap.dedent("""
+        from conans import ConanFile
+        class BaseClass2(ConanFile):
+            name = "base-class2"
+            version = "1.0"
+            python_requires = "shared-function/1.0@user/channel", "shared-function2/1.0@user/channel"
+            def package(self):
+                pyreqs = self.python_requires
+                v = pyreqs["shared-function2"].module.myvar  # v will be 222
+                f = pyreqs["shared-function"].module.myfunct()  # f will be 234
+                self.output.info("%s, %s" % (v, f))
+        """)
+    client.save({"conanfile.py": conanfile})
+    client.run("export . user/channel")
+
+    conanfile = textwrap.dedent("""
+        from conans import ConanFile
+        class Consumer(ConanFile):
+            name = "consumer"
+            version = "1.0"
+            python_requires = "base-class/1.0@user/channel", "base-class2/1.0@user/channel"
+            python_requires_extend = "base-class.BaseClass", "base-class2.BaseClass2"
+        """)
+    client.save({"conanfile.py": conanfile})
+    client.run("create . @user/channel")
+    assert "consumer/1.0@user/channel: Calling build()\nconsumer/1.0@user/channel: 123, 2222" in \
+           client.out
+    assert "consumer/1.0@user/channel: Calling package()\nconsumer/1.0@user/channel: 222, 234" in \
+           client.out
