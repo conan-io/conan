1530a0f137f4c210be47ed184d82845548d4eea5
memsharded <james@conan.io>
2021-03-11 13:49:27 +0100

    review

--- conan/tools/env/environment.py
+++ conan/tools/env/environment.py
@@ -252,7 +252,9 @@ class ProfileEnvironment:
             else:
                 self._environments[pattern] = environment
 
-    def loads(self, text):
+    @staticmethod
+    def loads(text):
+        result = ProfileEnvironment()
         for line in text.splitlines():
             line = line.strip()
             if not line or line.startswith("#"):
@@ -278,11 +280,12 @@ class ProfileEnvironment:
                         method = method + "_path"
                     getattr(env, method)(name, value)
 
-                existing = self._environments.get(pattern)
+                existing = result._environments.get(pattern)
                 if existing is None:
-                    self._environments[pattern] = env
+                    result._environments[pattern] = env
                 else:
-                    self._environments[pattern] = existing.compose(env)
+                    result._environments[pattern] = existing.compose(env)
                 break
             else:
                 raise ConanException("Bad env defintion: {}".format(line))
+        return result
