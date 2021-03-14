e9163a75f5143c712dcc78cb95e098d01fbb180c
memsharded <james@conan.io>
2021-03-15 00:59:32 +0100

    fix test

--- conans/test/functional/toolchains/cmake/test_cmake.py
+++ conans/test/functional/toolchains/cmake/test_cmake.py
@@ -346,7 +346,10 @@ class LinuxTest(Base):
         pic_str = "" if shared else "ON"
         arch_str = "-m32" if arch == "x86" else "-m64"
         cxx11_abi_str = "1" if libcxx == "libstdc++11" else "0"
-        defines = '_GLIBCXX_USE_CXX11_ABI=%s;MYDEFINE="MYDEF_VALUE";MYDEFINEINT=42;' % cxx11_abi_str
+        defines = '_GLIBCXX_USE_CXX11_ABI=%s;MYDEFINE="MYDEF_VALUE";MYDEFINEINT=42;'\
+                  'MYDEFINEINT_CONFIG=$<IF:$<CONFIG:debug>,421,$<IF:$<CONFIG:release>,422,"">>;'\
+                  'MYDEFINE_CONFIG=$<IF:$<CONFIG:debug>,"MYDEF_DEBUG",'\
+                  '$<IF:$<CONFIG:release>,"MYDEF_RELEASE","">>' % cxx11_abi_str
         vals = {"CMAKE_CXX_STANDARD": "14",
                 "CMAKE_CXX_EXTENSIONS": extensions_str,
                 "CMAKE_BUILD_TYPE": build_type,
