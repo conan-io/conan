a56b88e06c7ffb7799827ae8f27bb7df8cf53769
czoido <mrgalleta@gmail.com>
2021-03-15 17:39:19 +0100

    do not generate conanbuildinfo.txt

--- conans/client/manager.py
+++ conans/client/manager.py
@@ -37,7 +37,6 @@ def deps_install(app, ref_or_path, install_folder, graph_info, remotes=None, bui
 
     if generators is not False:
         generators = set(generators) if generators else set()
-        generators.add("txt")  # Add txt generator by default
 
     if profile_build:
         out.info("Configuration (profile_host):")
