7655ffbedc04e98ee2dd8a5d90e14fde9799a9b1
czoido <mrgalleta@gmail.com>
2021-03-17 10:27:44 +0100

    refactor

--- conans/test/conftest.py
+++ conans/test/conftest.py
@@ -7,10 +7,10 @@ from conans.client.tools import vswhere, which
 from conans.errors import ConanException
 
 tool_locations = {
-    'msys2': {'Windows': {'default': os.getenv("CONAN_MSYS2_PATH", "C:/msys64/usr/bin")}},
-    'cygwin': {'Windows': {'default': os.getenv("CONAN_CYGWIN_PATH", "C:/cygwin64/bin")}},
-    'mingw32': {'Windows': {'default': os.getenv('CONAN_MINGW32_PATH', "C:/msys64/mingw32/bin")}},
-    'mingw64': {'Windows': {'default': os.getenv('CONAN_MINGW64_PATH', "C:/msys64/mingw64/bin")}},
+    'msys2': {'Windows': {'default': os.getenv('CONAN_MSYS2_PATH', 'C:/msys64/usr/bin')}},
+    'cygwin': {'Windows': {'default': os.getenv('CONAN_CYGWIN_PATH', 'C:/cygwin64/bin')}},
+    'mingw32': {'Windows': {'default': os.getenv('CONAN_MINGW32_PATH', 'C:/msys64/mingw32/bin')}},
+    'mingw64': {'Windows': {'default': os.getenv('CONAN_MINGW64_PATH', 'C:/msys64/mingw64/bin')}},
     'cmake': {
         'Windows': {
             'default': 'C:/Program Files/Cmake/bin',
@@ -75,32 +75,50 @@ if not which("conan"):
     tools_available.remove("conan")
 
 
+def _get_tool_location(locations, name, version, tool_platform):
+    path = None
+    try:
+        path = locations[name][tool_platform][version]
+    except KeyError as exc:
+        if version in str(exc):
+            raise ConanException(exc)
+    return path
+
+
+def _get_tool_environment(environments, name, tool_platform):
+    env = None
+    try:
+        env = environments[name][tool_platform]
+    except KeyError:
+        pass
+    return env
+
+
 @pytest.fixture(autouse=True)
 def add_tool(request):
-    add_tools = []
-    env_tools = dict()
+    tools_paths = []
+    tools_env_vars = dict()
     for mark in request.node.iter_markers():
         if mark.name.startswith("tool_"):
             version = mark.kwargs.get('version', 'default')
             tool_name = mark.name[5:]
-            if tool_name in tool_locations and tool_locations[tool_name].get(platform.system()):
-                tool_versions = tool_locations[tool_name].get(platform.system())
-                if version in tool_versions:
-                    add_tools.append(tool_versions[version])
-                else:
-                    pytest.fail("Required {} version: '{}' is not available".format(tool_name,
-                                                                                    version))
+            try:
+                tool_path = _get_tool_location(tool_locations, tool_name, version, platform.system())
+                if tool_path:
+                    tools_paths.append(tool_path)
+            except ConanException:
+                pytest.fail("Required {} version: '{}' is not available".format(tool_name, version))
 
-            if tool_name in tool_environments and tool_environments[tool_name].get(platform.system()):
-                tool_env = tool_environments[tool_name].get(platform.system())
-                env_tools.update(tool_env)
+            tool_env = _get_tool_environment(tool_environments, tool_name, platform.system())
+            if tool_env:
+                tools_env_vars.update(tool_env)
 
-    if add_tools:
-        add_tools.append(os.environ["PATH"])
-        temp_env = {'PATH': os.pathsep.join(add_tools)}
+    if tools_paths:
+        tools_paths.append(os.environ["PATH"])
+        temp_env = {'PATH': os.pathsep.join(tools_paths)}
         old_environ = dict(os.environ)
         os.environ.update(temp_env)
-        os.environ.update(env_tools)
+        os.environ.update(tools_env_vars)
         yield
         os.environ.clear()
         os.environ.update(old_environ)
