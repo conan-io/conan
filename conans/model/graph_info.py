786d5082b985ccb7e28f9e5d90773b7e8d9ec551
czoido <mrgalleta@gmail.com>
2021-03-23 13:22:01 +0100

    temp tuple for graphinfo

--- conans/model/graph_info.py
+++ conans/model/graph_info.py
@@ -1,43 +1,4 @@
-import json
-import os
-
-from conans.errors import ConanException
-from conans.model.options import OptionsValues
-from conans.model.ref import ConanFileReference
-from conans.util.files import load, save
+from collections import namedtuple
 
 
-class GraphInfo(object):
-
-    def __init__(self, profile_host=None, profile_build=None, options=None, root_ref=None):
-        # This field is a temporary hack, to store dependencies options for the local flow
-        self.options = options
-        self.root = root_ref
-        self.profile_host = profile_host
-        self.profile_build = profile_build
-        self.graph_lock = None
-
-    @staticmethod
-    def loads(text):
-        graph_json = json.loads(text)
-        try:
-            options = graph_json["options"]
-        except KeyError:
-            options = None
-        else:
-            options = OptionsValues(options)
-        root = graph_json.get("root", {"name": None, "version": None, "user": None, "channel": None})
-        root_ref = ConanFileReference(root["name"], root["version"], root["user"], root["channel"],
-                                      validate=False)
-
-        return GraphInfo(options=options, root_ref=root_ref)
-
-    def _dumps(self):
-        result = {}
-        if self.options is not None:
-            result["options"] = self.options.as_list()
-        result["root"] = {"name": self.root.name,
-                          "version": self.root.version,
-                          "user": self.root.user,
-                          "channel": self.root.channel}
-        return json.dumps(result, indent=True)
+GraphInfo = namedtuple("GraphInfo", "profile_host profile_build graph_lock root_ref")
