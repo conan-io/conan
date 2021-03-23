06e99eb44a1f9016e2331685267eaf7438198598
czoido <mrgalleta@gmail.com>
2021-03-23 11:30:04 +0100

    update help

--- conans/client/command.py
+++ conans/client/command.py
@@ -456,7 +456,7 @@ class Command(object):
                             help='Generators to use')
         parser.add_argument("-if", "--install-folder", action=OnceArgument,
                             help='Use this directory as the directory where to put the generator'
-                                 'files. e.g., conaninfo/conanbuildinfo.txt')
+                                 'files.')
 
         parser.add_argument("--no-imports", action='store_true', default=False,
                             help='Install specified packages but avoid running imports')
@@ -833,7 +833,7 @@ class Command(object):
 
         parser.add_argument("-if", "--install-folder", action=OnceArgument,
                             help='Use this directory as the directory where to put the generator'
-                                 'files. e.g., conaninfo/conanbuildinfo.txt')
+                                 'files.')
 
         parser.add_argument("--no-imports", action='store_true', default=False,
                             help='Install specified packages but avoid running imports')
