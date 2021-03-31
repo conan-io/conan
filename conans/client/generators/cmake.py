4ebe404bbd85c43c71a44626816dd419dc37c6b1
memsharded <james@conan.io>
2021-03-31 17:51:19 +0200

    filter flags starting with -

--- conans/client/generators/cmake.py
+++ conans/client/generators/cmake.py
@@ -34,6 +34,9 @@ class DepsCppCmake(object):
             """
             return '"%s"' % ";".join(p.replace('\\', '/').replace('$', '\\$') for p in values)
 
+        def format_link_flags(link_flags):
+            return [(f if f.startswith("-") else"-{}".format(f)) for f in link_flags]
+
         self.include_paths = join_paths(cpp_info.include_paths)
         self.include_path = join_paths_single_var(cpp_info.include_paths)
         self.lib_paths = join_paths(cpp_info.lib_paths)
@@ -58,9 +61,8 @@ class DepsCppCmake(object):
         # Issue: #1251
         self.cxxflags_list = join_flags(";", cpp_info.cxxflags)
         self.cflags_list = join_flags(";", cpp_info.cflags)
-        self.sharedlinkflags_list = join_flags(";", ["-{}".format(f)
-                                                     for f in cpp_info.sharedlinkflags])
-        self.exelinkflags_list = join_flags(";", ["-{}".format(f) for f in cpp_info.exelinkflags])
+        self.sharedlinkflags_list = join_flags(";", format_link_flags(cpp_info.sharedlinkflags))
+        self.exelinkflags_list = join_flags(";", format_link_flags(cpp_info.exelinkflags))
 
         self.rootpath = join_paths([cpp_info.rootpath])
         self.build_modules_paths = join_paths(cpp_info.build_modules_paths.get(generator_name, []))
