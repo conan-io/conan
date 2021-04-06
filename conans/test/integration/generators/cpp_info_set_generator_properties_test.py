491cc836376ac640659d9db5204fbe69c19e65db
czoido <mrgalleta@gmail.com>
2021-04-06 18:58:47 +0200

    add more tests

--- conans/test/integration/generators/cpp_info_set_generator_properties_test.py
+++ conans/test/integration/generators/cpp_info_set_generator_properties_test.py
@@ -44,11 +44,16 @@ def setup_client():
     build_module = textwrap.dedent("""
         message("I am a build module")
         """)
+
+    another_build_module = textwrap.dedent("""
+        message("I am another build module")
+        """)
+
     client.save({"consumer.py": GenConanfile("consumer", "1.0").with_requires("mypkg/1.0").
                 with_generator("custom_generator").with_generator("cmake_find_package").
                 with_generator("cmake_find_package_multi").with_generator("pkg_config").
                 with_setting("build_type"),
-                "mypkg_bm.cmake": build_module})
+                "mypkg_bm.cmake": build_module, "mypkg_anootherbm.cmake": another_build_module})
     return client
 
 
@@ -168,6 +173,67 @@ def test_same_results_without_components(setup_client):
                 self.cpp_info.names["cmake_find_package_multi"] = "mypkg-name"
                 self.cpp_info.names["custom_generator"] = "mypkg-name"
                 self.cpp_info.build_modules.append(os.path.join("lib", "mypkg_bm.cmake"))
+        """)
+    client.save({"mypkg.py": mypkg})
+    client.run("create mypkg.py")
+    client.run("install consumer.py -s build_type=Release")
+
+    old_approach_contents = get_files_contents(client.current_folder, files_to_compare)
+
+    assert new_approach_contents == old_approach_contents
+
+
+def test_same_results_specific_generators(setup_client):
+    client = setup_client
+    mypkg = textwrap.dedent("""
+        import os
+        from conans import ConanFile, CMake, tools
+        class MyPkg(ConanFile):
+            settings = "build_type"
+            name = "mypkg"
+            version = "1.0"
+            exports_sources = ["mypkg_bm.cmake", "mypkg_anootherbm.cmake"]
+            def package(self):
+                self.copy("mypkg_bm.cmake", dst="lib")
+                self.copy("mypkg_anootherbm.cmake", dst="lib")
+            def package_info(self):
+                self.cpp_info.set_property("cmake_file_name", "MyFileName", "cmake_find_package")
+                self.cpp_info.set_property("cmake_file_name", "MyFileNameMulti", "cmake_find_package_multi")
+                self.cpp_info.set_property("cmake_target_name", "mypkg-name", "cmake_find_package")
+                self.cpp_info.set_property("cmake_target_name", "mypkg-name-multi", "cmake_find_package_multi")
+                self.cpp_info.set_property("cmake_build_modules",[os.path.join("lib",
+                                                                 "mypkg_bm.cmake")], "cmake_find_package")
+                self.cpp_info.set_property("cmake_build_modules",[os.path.join("lib",
+                                                                 "mypkg_anootherbm.cmake")], "cmake_find_package_multi")
+        """)
+
+    client.save({"mypkg.py": mypkg})
+    client.run("export mypkg.py")
+
+    client.run("install consumer.py --build missing -s build_type=Release")
+
+    files_to_compare = ["FindMyFileName.cmake", "MyFileNameMultiConfig.cmake", "MyFileNameMultiTargets.cmake",
+                        "MyFileNameMultiTarget-release.cmake", "MyFileNameMultiConfigVersion.cmake"]
+    new_approach_contents = get_files_contents(client.current_folder, files_to_compare)
+
+    mypkg = textwrap.dedent("""
+        import os
+        from conans import ConanFile, CMake, tools
+        class MyPkg(ConanFile):
+            settings = "build_type"
+            name = "mypkg"
+            version = "1.0"
+            exports_sources = ["mypkg_bm.cmake", "mypkg_anootherbm.cmake"]
+            def package(self):
+                self.copy("mypkg_bm.cmake", dst="lib")
+                self.copy("mypkg_anootherbm.cmake", dst="lib")
+            def package_info(self):
+                self.cpp_info.filenames["cmake_find_package"] = "MyFileName"
+                self.cpp_info.filenames["cmake_find_package_multi"] = "MyFileNameMulti"
+                self.cpp_info.names["cmake_find_package"] = "mypkg-name"
+                self.cpp_info.names["cmake_find_package_multi"] = "mypkg-name-multi"
+                self.cpp_info.build_modules["cmake_find_package"].append(os.path.join("lib", "mypkg_bm.cmake"))
+                self.cpp_info.build_modules["cmake_find_package_multi"].append(os.path.join("lib", "mypkg_anootherbm.cmake"))
         """)
     client.save({"mypkg.py": mypkg})
     client.run("create mypkg.py")
