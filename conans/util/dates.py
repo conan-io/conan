5e846606c66cbcd08882ead59e3ee7c75f988d10
Uilian Ries <uilianries@gmail.com>
2021-04-07 14:42:45 -0300

    #8713 Improve config_install_interval
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/util/dates.py
+++ conans/util/dates.py
@@ -34,15 +34,11 @@ def timedelta_from_text(interval):
     match = re.search(r"(\d+)([smhdw])", interval)
     try:
         value, unit = match.group(1), match.group(2)
-        if unit == 's':
-            return datetime.timedelta(seconds=float(value))
-        elif unit == 'm':
-            return datetime.timedelta(minutes=float(value))
-        elif unit == 'h':
-            return datetime.timedelta(hours=float(value))
-        elif unit == 'd':
-            return datetime.timedelta(days=float(value))
-        elif unit == 'w':
-            return datetime.timedelta(weeks=float(value))
+        name = {'s': 'seconds',
+                'm': 'minutes',
+                'h': 'hours',
+                'd': 'days',
+                'w': 'weeks'}[unit]
+        return datetime.timedelta(**{name: float(value)})
     except Exception:
         raise ConanException("Incorrect time interval definition: %s" % interval)
