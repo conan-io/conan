19fd62b5d22b0ec2f63b38d0d10cc89723da63db
czoido <mrgalleta@gmail.com>
2021-04-14 15:53:57 +0200

    wip

--- conans/test/integration/cache/conan2_cache_test.py
+++ conans/test/integration/cache/conan2_cache_test.py
@@ -6,4 +6,4 @@ class TestCache:
     def test_conan_export(self):
         client = TestClient()
         client.save({"conanfile.py": GenConanfile()})
-        client.run("export .")
+        client.run("export . mypkg/1.0@user/channel")
