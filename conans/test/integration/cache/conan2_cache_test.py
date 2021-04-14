db9b59c501584be78686381e81a58c7feed65415
czoido <mrgalleta@gmail.com>
2021-04-14 11:58:15 +0200

    wip

---
+++ conans/test/integration/cache/conan2_cache_test.py
@@ -0,0 +1,9 @@
+from conans.test.assets.genconanfile import GenConanfile
+from conans.test.utils.tools import TestClient
+
+
+class TestCache:
+    def test_conan_export(self):
+        client = TestClient()
+        client.save({"conanfile.py": GenConanfile()})
+        client.run("conan export .")
