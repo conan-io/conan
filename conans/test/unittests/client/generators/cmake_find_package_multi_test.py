6d82d302ae3962001903c1997f1f3232a186bb00
memsharded <james@conan.io>
2021-04-15 21:17:27 +0200

    revert cmake linkflags handling

--- conans/test/unittests/client/generators/cmake_find_package_multi_test.py
+++ conans/test/unittests/client/generators/cmake_find_package_multi_test.py
@@ -42,8 +42,9 @@ def test_cmake_find_package_multi_links_flags():
     conanfile.settings.arch = "x86"
 
     cpp_info = CppInfo("mypkg", "dummy_root_folder1")
-    cpp_info.sharedlinkflags = ["/NODEFAULTLIB", "/OTHERFLAG"]
-    cpp_info.exelinkflags = ["/OPT:NOICF"]
+    # https://github.com/conan-io/conan/issues/8811 regression, fix with explicit - instead of /
+    cpp_info.sharedlinkflags = ["-NODEFAULTLIB", "-OTHERFLAG"]
+    cpp_info.exelinkflags = ["-OPT:NOICF"]
     conanfile.deps_cpp_info.add("mypkg", cpp_info)
 
     gen = CMakeFindPackageMultiGenerator(conanfile)
