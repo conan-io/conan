6aca17a6c462575ef4197518c19d4f7ea52c27b5
czoido <mrgalleta@gmail.com>
2021-04-23 13:09:38 +0200

    wip

--- conans/test/integration/cache/conan2_cache_test.py
+++ conans/test/integration/cache/conan2_cache_test.py
@@ -67,6 +67,7 @@ class TestCache:
             """)
 
         client = TestClient()
+        client.run_command("open '{}'".format(client.cache_folder))
         client.save({"conanfile.py": conanfile,
                      "CMakeLists.txt": cmakelists})
         client.run("create . mypkg/1.0@user/channel")
