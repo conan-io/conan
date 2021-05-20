533fd2b2c904a249cb7c2445bd0a590cd86f1afc
Luis Martinez de Bartolome Izquierdo <lasote@gmail.com>
2021-05-20 12:24:08 +0200

    CMakeDeps refactor (#8971)
    * Minimum changes
    
    * Fixed mock value install folder
    
    * litle review changes
    
    * Changed data structure
    
    * Clean code
    
    * Local methods
    
    * Introducing generators folder
    
    * Build helpers using generators folder
    
    * Build helpers with generators folder
    
    * in cache layout
    
    * Toolchain using layouts
    
    * Toolchains apple and windows
    
    * Fixed tests
    
    * Fix cmake path to toolchain
    
    * Windows slash
    
    * Windows tests
    
    * Move generators folder to following PR
    
    * Generators folder
    
    * More tests
    
    * Review
    
    * Add test
    
    * Fix packages in cache and refactor to clean directory passing as arguments
    
    * New test, different layout in cache
    
    * Fix windows \r\n
    
    * fix windows crlf
    
    * Fix win test again
    
    * Again, tests windows
    
    * Default not package layout in cache
    
    * Removed package layout
    
    * Improved access to layout
    
    * Review
    
    * Auto review
    
    * editables kind of working
    
    * Extracted defaults
    
    * editables
    
    * Trying clion
    
    * Moved tests
    
    * Auto pacakge
    
    * some tests
    
    * Filter empty default False
    
    * simplify
    
    * Fixed test
    
    * clion test
    
    * Self review
    
    * less change
    
    * remove comment
    
    * python 2 fix
    
    * Try to fix windows
    
    * More win
    
    * Partial review
    
    * Review
    
    * removed comments
    
    * Fix merge, remove unused tests, removed unnecessay rename
    
    * Fixing test
    
    * base path
    
    * Fix test package
    
    * One more test of test_package using generators folder
    
    * Removed comments
    
    * introducing cpp info, preliminar changes
    
    * integrated new cppinfo
    
    * Fix cmake toolchain tests
    
    * removed unnecesary test
    
    * get_name from cmake_find_package_multi too
    
    * Fix bug
    
    * Name from cmake_find_package_multi but not from CMakeDeps
    
    * refactored model, pending fix editables and tests
    
    * remove new generators output_path
    
    * Update conans/test/functional/layout/test_in_cache.py
    
    Co-authored-by: James <james@conan.io>
    
    * Editables example working, missing tests
    
    * Editables working, simplified code, missing more tests
    
    * Fixed test
    
    * imports and package working and tested
    
    * more tests, better merge logic
    
    * Windows test
    
    * Debug win test failing
    
    * win
    
    * Function instead of static
    
    * Empty lists in editable are explicit declared priority values
    
    * Simplified model, values always none
    
    * Fixed aggregration
    
    * Test package layout in cache
    
    * clear none
    
    * unnecessary code
    
    * Review
    
    * Remove unnecessary
    
    * vs_layout
    
    * Renamed infos to cpp
    
    * fix test
    
    * removed comment
    
    * Fixed build modules propagation to new model
    
    * Better CMakeDeps with constant files except -release
    
    * same code for components and no components
    
    * Refactor cmake deps
    
    * CMakeDeps tests running in OSX
    
    * Fixed test
    
    * Fix order in build modules
    
    * Multi config windows fixed
    
    * fixed test package folder var name
    
    * Apple frameworks test migrated to CMakeDeps
    
    * Adapted link order test
    
    * Ported konstantin test about m1 to toolchain and cmakedeps
    
    * Fix test, non-existent libs cause CMake FATAL_ERROR
    
    * CMake 3.19
    
    * Dead code and comments
    
    * review
    
    * Moved test
    
    * Comment
    
    * Fix test rename targets
    
    * Everything broken
    
    * Refactor CMakeDeps
    
    * Fixing tests
    
    * remove print
    
    * Fixing tests on Windows
    
    * Review
    
    * win test?
    
    * Win test?
    
    Co-authored-by: memsharded <james@conan.io>

---
+++ conan/tools/cmake/cmakedeps/cmakedeps.py
@@ -0,0 +1,54 @@
+import os
+
+from conan.tools.cmake.cmakedeps.templates.config import ConfigTemplate
+from conan.tools.cmake.cmakedeps.templates.config_version import ConfigVersionTemplate
+from conan.tools.cmake.cmakedeps.templates.macros import MacrosTemplate
+from conan.tools.cmake.cmakedeps.templates.target_configuration import TargetConfigurationTemplate
+from conan.tools.cmake.cmakedeps.templates.target_data import ConfigDataTemplate
+from conan.tools.cmake.cmakedeps.templates.targets import TargetsTemplate
+from conans.util.files import save
+
+
+class CMakeDeps(object):
+
+    def __init__(self, conanfile):
+        self._conanfile = conanfile
+        self.arch = self._conanfile.settings.get_safe("arch")
+        self.configuration = str(self._conanfile.settings.build_type)
+        self.configurations = [v for v in conanfile.settings.build_type.values_range if v != "None"]
+
+    def generate(self):
+        # Current directory is the generators_folder
+        generator_files = self.content
+        for generator_file, content in generator_files.items():
+            save(generator_file, content)
+
+    @property
+    def content(self):
+        macros = MacrosTemplate()
+        ret = {macros.filename: macros.render()}
+
+        host_requires = {r.ref.name: r for r in
+                         self._conanfile.dependencies.transitive_host_requires}
+        # Iterate all the transitive requires
+        for req in host_requires.values():
+
+            config_version = ConfigVersionTemplate(self, req)
+            ret[config_version.filename] = config_version.render()
+
+            data_target = ConfigDataTemplate(self, req)
+            ret[data_target.filename] = data_target.render()
+
+            target_configuration = TargetConfigurationTemplate(self, req)
+            ret[target_configuration.filename] = target_configuration.render()
+
+            targets = TargetsTemplate(self, req)
+            ret[targets.filename] = targets.render()
+
+            config = ConfigTemplate(self, req)
+            # Check if the XXConfig.cmake exists to keep the first generated configuration
+            # to only include the build_modules from the first conan install. The rest of the
+            # file is common for the different configurations.
+            if not os.path.exists(config.filename):
+                ret[config.filename] = config.render()
+        return ret
