d79ea10cfe0f893155bc4ffa33bbb557b0fac03b
memsharded <james@conan.io>
2021-06-01 16:49:13 +0200

    fix more tests

--- conans/test/integration/configuration/conf/test_conf_from_br.py
+++ conans/test/integration/configuration/conf/test_conf_from_br.py
@@ -27,17 +27,22 @@ def test_basic():
             def generate(self):
                 self.output.info("NDK: %s" % self.conf["tools.android:ndk_path"])
         """)
-    android_profile = textwrap.dedent("""
+    # CMakeToolchain needs compiler definition
+    linux_profile = textwrap.dedent("""
         [settings]
         os = Linux
         arch = x86_64
         compiler = gcc
         compiler.version = 4.9
         compiler.libcxx = libstdc++
+        """)
+    android_profile = textwrap.dedent("""
+        include(linux)
         [conf]
         tools.android:ndk_path=MY-SYSTEM-NDK!!!
         """)
     client.save({"conanfile.py": consumer,
+                 "linux": linux_profile,
                  "android": android_profile}, clean_first=True)
     client.run("install .")
     assert "conanfile.py: NDK: MY-NDK!!!" in client.out
