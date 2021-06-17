c8b5212460003b8a01405e59e5f16718458853cb
Uilian Ries <uilianries@gmail.com>
2021-06-17 14:11:17 -0300

    #7287 Validate folder path only
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/test/integration/command/config_test.py
+++ conans/test/integration/command/config_test.py
@@ -198,7 +198,7 @@ def test_config_user_home_short_path():
     client.save({"conanfile.py": GenConanfile().with_short_paths(True)})
     client.run("create . foobar/0.1.0@user/testing")
     source_folder = short_folder if platform.system() == "Windows" else client.cache_folder
-    assert "Configuring sources in {}".format(source_folder) in client.out
+    assert source_folder in client.out
     assert client.cache.config.short_paths_home == short_folder
 
 
@@ -209,7 +209,7 @@ def test_config_user_home_short_none():
     client.run('config set general.user_home_short=None')
     client.save({"conanfile.py": GenConanfile().with_short_paths(True)})
     client.run("create . foobar/0.1.0@user/testing")
-    assert "Configuring sources in {}".format(client.cache_folder) in client.out
+    assert client.cache_folder in client.out
     assert client.cache.config.short_paths_home == "None"
 
 
