d3363d4425e50efa1e7b0e48cd791679106c73d3
Uilian Ries <uilianries@gmail.com>
2021-06-21 15:48:05 -0300

    Revert "#7287 Linux doesn't use short paths"
    This reverts commit 23dbc0925d08412bb48e94d6458db70f005ba272.

--- conans/test/integration/command/config_test.py
+++ conans/test/integration/command/config_test.py
@@ -1,7 +1,6 @@
 import json
 import os
 import pytest
-import platform
 
 
 from conans.errors import ConanException
@@ -197,6 +196,7 @@ def test_config_user_home_short_path():
     client.run('config set general.user_home_short="{}"'.format(short_folder))
     client.save({"conanfile.py": GenConanfile().with_short_paths(True)})
     client.run("create . foobar/0.1.0@user/testing")
+    assert "Configuring sources in {}".format(short_folder) in client.out
     assert client.cache.config.short_paths_home == short_folder
 
 
