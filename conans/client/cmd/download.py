4050437d3fdb1dfd33f5dd90ba22fc0dadb80b0d
czoido <mrgalleta@gmail.com>
2021-07-01 10:00:14 +0200

    remove unused arg

--- conans/client/cmd/download.py
+++ conans/client/cmd/download.py
@@ -34,15 +34,14 @@ def download(app, ref, package_ids, remote, recipe, recorder, remotes):
                 output.warn("No remote binary packages found in remote")
 
         parallel = cache.config.parallel_download
-        _download_binaries(conanfile, ref, package_ids, cache, remote_manager,
+        _download_binaries(conanfile, ref, package_ids, remote_manager,
                            remote, output, recorder, parallel)
     hook_manager.execute("post_download", conanfile_path=conan_file_path, reference=ref,
                          remote=remote)
 
 
-def _download_binaries(conanfile, ref, package_ids, cache, remote_manager, remote, output,
+def _download_binaries(conanfile, ref, package_ids, remote_manager, remote, output,
                        recorder, parallel):
-    short_paths = conanfile.short_paths
 
     def _download(package_id):
         pref = PackageReference(ref, package_id)
