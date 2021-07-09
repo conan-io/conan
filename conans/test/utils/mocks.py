8cba45a3bef251d38b81e5aeb710921e15ecc9f7
Uilian Ries <uilianries@gmail.com>
2021-07-09 08:29:16 -0300

    Skip cmake.test and meson.test using [conf] profile (#9218)
    * #8454 Cmake conf skip test
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #8454 Meson conf skip test
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #8454 Rename to tools.build:skip_test
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #8454 Add conf to ConanMock
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #8454 Mock ConfDefinition
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/test/utils/mocks.py
+++ conans/test/utils/mocks.py
@@ -9,6 +9,7 @@ from six import StringIO
 from conans import ConanFile, Options
 from conans.client.output import ConanOutput
 from conans.client.userio import UserIO
+from conans.model.conf import ConfDefinition
 from conans.model.env_info import DepsEnvInfo, EnvInfo, EnvValues
 from conans.model.layout import Folders
 from conans.model.options import PackageOptions
@@ -181,6 +182,8 @@ class ConanFileMock(ConanFile):
         self._conan_channel = None
         self.environment_scripts = []
         self.win_bash = None
+        self.conf = ConfDefinition().get_conanfile_conf(None)
+
 
     def run(self, command, win_bash=False, subsystem=None, env=None):
         assert win_bash is False
