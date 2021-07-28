86261363327a9bc7e8ca31a8f3ab49882f2fd828
czoido <mrgalleta@gmail.com>
2021-07-28 14:23:52 +0200

    test subcommands

--- conans/test/integration/command_v2/custom_commands_test.py
+++ conans/test/integration/command_v2/custom_commands_test.py
@@ -5,7 +5,7 @@ from conans.test.utils.tools import TestClient
 
 
 class TestCustomCommands:
-    def test_new_custom_command(self):
+    def test_simple_custom_command(self):
         mycommand = textwrap.dedent("""
             import json
 
@@ -36,3 +36,41 @@ class TestCustomCommands:
         assert f"Conan cache folder is: {client.cache_folder}" in client.out
         client.run("mycommand -f json")
         assert f'{{"cache_folder": "{client.cache_folder}"}}' in client.out
+
+    def test_custom_command_with_subcommands(self):
+        complex_command = textwrap.dedent("""
+            import json
+
+            from conans.cli.output import cli_out_write
+            from conans.cli.command import conan_command, conan_subcommand
+
+            def output_cli(info):
+                cli_out_write(f"{info.get('argument1')}")
+
+            def output_json(info):
+                cli_out_write(json.dumps(info))
+
+            @conan_subcommand(formatters={"cli": output_cli, "json": output_json})
+            def complex_sub1(conan_api, parser, subparser, *args):
+                \"""
+                sub1 subcommand
+                \"""
+                subparser.add_argument("argument1", help="This is argument number 1")
+                args = parser.parse_args(*args)
+                info = {"argument1": args.argument1}
+                return info
+
+            @conan_command()
+            def complex(conan_api, parser, *args, **kwargs):
+                \"""
+                this is a command with subcommands
+                \"""
+            """)
+
+        client = TestClient()
+        command_file_path = os.path.join(client.cache_folder, 'commands', 'cmd_complex.py')
+        client.save({f"{command_file_path}": complex_command})
+        client.run("complex sub1 myargument")
+        assert "myargument" in client.out
+        client.run("complex sub1 myargument -f json")
+        assert f'{{"argument1": "myargument"}}' in client.out
