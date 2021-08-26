744ff487dc4a701262d11bacd40b84686eaa4254
czoido <mrgalleta@gmail.com>
2021-08-26 14:51:57 +0200

    fix test

--- conans/test/functional/revisions_test.py
+++ conans/test/functional/revisions_test.py
@@ -74,21 +74,15 @@ class InstallingPackagesWithRevisionsTest(unittest.TestCase):
 
         remote2_latest_prev = other_v2.get_latest_prev(self.ref)
 
-        # Install, it wont resolve the remote2 because it is in the registry, it will use the cache
-        # doing --update it will find a newer same revision in remote2 and update the date and server
-        # of the revision in the local cache
         self.c_v2.run("install {} --update".format(self.ref))
         self.assertIn("lib/1.0@conan/testing from 'remote2' - Cache (Updated date)", self.c_v2.out)
-        self.assertIn("lib/1.0@conan/testing:{} - Update".format(pref.id), self.c_v2.out)
-        self.assertIn("lib/1.0@conan/testing: Downloaded package revision {}".
-                      format(remote2_latest_prev.revision), self.c_v2.out)
-        assert self.c_v2.cache.get_remote(remote2_latest_prev) == "remote2"
-        assert self.c_v2.cache.get_remote(self.c_v2.cache.get_latest_rrev(self.ref)) == "remote2"
+        self.assertIn("lib/1.0@conan/testing:{} - Cache".format(pref.id), self.c_v2.out)
 
-        # This should do the same than before
+        # Now with the remote specified it will retrieve the binary fromm the remote2:
+        # https://github.com/conan-io/conan/pull/9355
         self.c_v2.run("install {} --update -r remote2".format(self.ref))
         self.assertIn("lib/1.0@conan/testing from 'remote2' - Cache", self.c_v2.out)
-        self.assertIn("lib/1.0@conan/testing:{} - Cache".format(pref.id), self.c_v2.out)
+        self.assertIn("lib/1.0@conan/testing:{} - Update".format(pref.id), self.c_v2.out)
 
     def test_diamond_revisions_conflict(self):
         """ If we have a diamond because of pinned conflicting revisions in the requirements,
