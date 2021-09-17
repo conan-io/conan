9213d2cccded419dc04abc126c072d7f9e925b86
czoido <mrgalleta@gmail.com>
2021-09-17 07:56:31 +0200

    add new line and literal comparison in test

--- conans/test/integration/test_migrations.py
+++ conans/test/integration/test_migrations.py
@@ -33,7 +33,7 @@ class TestMigrations(unittest.TestCase):
                         "Migrations var '{}' not found".format(var_name))
         migrations_settings_content = getattr(migrations_settings, var_name)
         # this is the same comparison made in client/migrations.py
-        self.assertListEqual(current_settings.splitlines(), migrations_settings_content.splitlines())
+        assert current_settings == migrations_settings_content
 
     def test_is_there_var_for_settings_previous_version(self):
         from conans import __version__ as current_version
