dddf80c4527361d5df939f7aeb502a00c1ce7dcb
memsharded <james@conan.io>
2021-09-22 22:32:20 +0200

    wip

--- conans/test/conftest.py
+++ conans/test/conftest.py
@@ -131,7 +131,7 @@ default_profiles = {
         os=Macos
         arch=x86_64
         compiler=apple-clang
-        compiler.version=12
+        compiler.version=12.0
         compiler.libcxx=libc++
         build_type=Release
         """)
