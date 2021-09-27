4739685df688d173dbe2bfeffad83dd2ef5bff7c
César García Tapia <cesargt@jfrog.com>
2021-09-27 16:40:03 +0200

    VirtualRunEnv: avoid generating LD_LIBRARY_PATH and DYLD_LIBRARY_PATH env variables on Windows

--- conan/tools/env/virtualrunenv.py
+++ conan/tools/env/virtualrunenv.py
@@ -10,11 +10,14 @@ def runenv_from_cpp_info(conanfile, cpp_info):
     if cpp_info.bin_paths:  # cpp_info.exes is not defined yet
         dyn_runenv.prepend_path("PATH", cpp_info.bin_paths)
     # If it is a build_require this will be the build-os, otherwise it will be the host-os
-    if cpp_info.lib_paths:
-        dyn_runenv.prepend_path("LD_LIBRARY_PATH", cpp_info.lib_paths)
-        dyn_runenv.prepend_path("DYLD_LIBRARY_PATH", cpp_info.lib_paths)
-    if cpp_info.framework_paths:
-        dyn_runenv.prepend_path("DYLD_FRAMEWORK_PATH", cpp_info.framework_paths)
+
+    os = str(conanfile.settings.get_safe("os"))
+    if not os.startswith("Windows"):
+        if cpp_info.lib_paths:
+            dyn_runenv.prepend_path("LD_LIBRARY_PATH", cpp_info.lib_paths)
+            dyn_runenv.prepend_path("DYLD_LIBRARY_PATH", cpp_info.lib_paths)
+        if cpp_info.framework_paths:
+            dyn_runenv.prepend_path("DYLD_FRAMEWORK_PATH", cpp_info.framework_paths)
     return dyn_runenv
 
 
