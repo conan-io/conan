f5005896394db7de1e13a2492e88954fb80426db
czoido <mrgalleta@gmail.com>
2021-09-28 18:59:18 +0200

    wip

--- conans/client/installer.py
+++ conans/client/installer.py
@@ -536,10 +536,9 @@ class BinaryInstaller(object):
                 self._hook_manager.execute("pre_package_info", conanfile=conanfile,
                                            reference=ref)
                 if hasattr(conanfile, "layout"):
-                    if not is_editable:
-                        package_cppinfo = conanfile.cpp.package.copy()
-                        package_cppinfo.set_relative_base_folder(conanfile.folders.package)
-                        conanfile.cpp_info = package_cppinfo
+                    package_cppinfo = conanfile.cpp.package.copy()
+                    package_cppinfo.set_relative_base_folder(conanfile.folders.package)
+                    conanfile.cpp_info = package_cppinfo
 
                 conanfile.cpp_info.clear_none()
 
