b3fb553da8bddab136414fb99920cf4a31c11524
czoido <mrgalleta@gmail.com>
2021-09-29 09:14:44 +0200

    add autotools and bazel

--- conans/test/functional/toolchains/gnu/autotools/test_basic.py
+++ conans/test/functional/toolchains/gnu/autotools/test_basic.py
@@ -5,7 +5,9 @@ import time
 
 import pytest
 
+from conan.tools import CONAN_TOOLCHAIN_ARGS_FILE
 from conan.tools.env.environment import environment_wrap_command
+from conan.tools.files import load_toolchain_args
 from conans.test.assets.autotools import gen_makefile_am, gen_configure_ac, gen_makefile
 from conans.test.assets.sources import gen_function_cpp
 from conans.test.functional.utils import check_exe_run
@@ -170,3 +172,35 @@ def test_autotoolsdeps_msys():
     assert "MINGW" not in out
     assert "main2 __MSYS__1" in out
     assert "main2 __CYGWIN__1" in out
+
+
+def test_namespace():
+    client = TestClient()
+    namespace = "somename"
+    conanfile = textwrap.dedent("""
+            from conans import ConanFile
+            from conan.tools.gnu import AutotoolsToolchain, Autotools
+
+            class Conan(ConanFile):
+                settings = "os", "arch", "compiler", "build_type"
+                def generate(self):
+                    autotools = AutotoolsToolchain(self, namespace='{0}')
+                    autotools.configure_args = ['a', 'b']
+                    autotools.make_args = ['c', 'd']
+                    autotools.generate()
+                def build(self):
+                    autotools = Autotools(self, namespace='{0}')
+                    self.output.info(autotools._configure_args)
+                    self.output.info(autotools._make_args)
+            """.format(namespace))
+
+    client.save({"conanfile.py": conanfile})
+    client.run("install .")
+    assert os.path.isfile(os.path.join(client.current_folder,
+                                       "{}_{}".format(namespace, CONAN_TOOLCHAIN_ARGS_FILE)))
+    content = load_toolchain_args(generators_folder=client.current_folder, namespace=namespace)
+    at_configure_args = content.get("configure_args")
+    at_make_args = content.get("make_args")
+    client.run("build .")
+    assert at_configure_args in client.out
+    assert at_make_args in client.out
