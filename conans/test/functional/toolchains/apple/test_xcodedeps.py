6228139eb0a103bca62ddffa0e8cfc440b88512b
czoido <mrgalleta@gmail.com>
2021-10-15 08:36:32 +0200

    wip

--- conans/test/functional/toolchains/apple/test_xcodedeps.py
+++ conans/test/functional/toolchains/apple/test_xcodedeps.py
@@ -10,8 +10,8 @@ def test_basic():
     client = TestClient()
     client.run("new hello/0.1 -m=cmake_lib")
     client.run("create .")
-    # client.run("new goodbye/0.1 -m=cmake_lib")
-    # client.run("create .")
-    client.save({"conanfile.txt": "[requires]\nhello/0.1"}, clean_first=True)  # \ngoodbye/0.1
+    client.run("new goodbye/0.1 -m=cmake_lib")
+    client.run("create .")
+    client.save({"conanfile.txt": "[requires]\nhello/0.1\ngoodbye/0.1\n"}, clean_first=True)
     client.run("install . -g XCodeDeps --build=missing -s build_type=Release")
     client.run("install . -g XCodeDeps --build=missing -s build_type=Debug")
