a6b610286b35ed1628d866a3963626f300918e1e
czoido <mrgalleta@gmail.com>
2021-10-18 10:30:34 +0200

    wip

--- conans/test/functional/toolchains/apple/test_xcodedeps.py
+++ conans/test/functional/toolchains/apple/test_xcodedeps.py
@@ -11,29 +11,43 @@ from conans.test.utils.tools import TestClient
 def test_xcodedeps_check_configurations():
     client = TestClient()
 
+    client.run("new hello/0.1 -m=cmake_lib")
+    client.run("export .")
+
     main = textwrap.dedent("""
     #include <iostream>
+    #include "hello.h"
     int main(int argc, char *argv[]) {
+        hello();
         #ifdef NDEBUG
-        std::cout << "Hello World Release!" << std::endl;
+        std::cout << "App Release!" << std::endl;
         #else
-        std::cout << "Hello World Debug!" << std::endl;
+        std::cout << "App Debug!" << std::endl;
         #endif
     }
     """)
 
     cmakelists = textwrap.dedent("""
     cmake_minimum_required (VERSION 3.1)
-    project (cmakehello)
-    add_executable (hello main.cpp)
+    project (cmakeapp)
+    add_executable (app app.cpp)
     """)
 
-    client.save({"main.cpp": main, "CMakeLists.txt": cmakelists})
+    client.save({"app.cpp": main, "CMakeLists.txt": cmakelists,
+                 "conanfile.txt": "[requires]\nhello/0.1\n"}, clean_first=True)
 
+    # we are using cmake here just to generate a Xcode project
     client.run_command("cmake . -G Xcode")
-    client.run_command("xcodebuild -project cmakehello.xcodeproj -configuration Debug")
-    client.run_command("./Debug/hello")
-    assert "Hello World Debug!" in client.out
-    client.run_command("xcodebuild -project cmakehello.xcodeproj -configuration Release")
-    client.run_command("./Release/hello")
-    assert "Hello World Release!" in client.out
+
+    client.run("install . -s build_type=Debug --build=missing -g XcodeDeps")
+    client.run("install . -s build_type=Release --build=missing -g XcodeDeps")
+
+    client.run_command("xcodebuild -project cmakeapp.xcodeproj -xcconfig conandeps.xcconfig -configuration Debug")
+    client.run_command("./Debug/app")
+    assert "App Debug!" in client.out
+    assert "hello/0.1: Hello World Debug!" in client.out
+
+    client.run_command("xcodebuild -project cmakeapp.xcodeproj -xcconfig conandeps.xcconfig -configuration Release")
+    client.run_command("./Release/app")
+    assert "App Release!" in client.out
+    assert "hello/0.1: Hello World Release!" in client.out
