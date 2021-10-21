4f70b581d6ae2058fb35799015ac9b60e9e4497a
memsharded <james@conan.io>
2021-10-21 13:29:19 +0200

    test that captures with FIXME current issue with per-package settings

--- conans/test/integration/settings/per_package_settings_test.py
+++ conans/test/integration/settings/per_package_settings_test.py
@@ -19,6 +19,29 @@ class PerPackageSettingTest(unittest.TestCase):
         client.run("create . consumer/0.1@user/testing -s os=Linux -s pkg*:os=Windows")
         self.assertIn("consumer/0.1@user/testing: Created package", client.out)
 
+    def test_per_package_setting_build_type(self):
+        # FIXME this is weird behavior if you use different patterns, only first matching one win:
+        # FIXME -s pkg*:os=Windows -s *model:build_type=Debug
+        # FIXME keeps the build_type=Release, because pkg* matches, assign os=Windows and stops
+        client = TestClient()
+        conanfile = textwrap.dedent("""
+            from conans import ConanFile
+            class Pkg(ConanFile):
+                settings = "os", "build_type"
+                def build(self):
+                    self.output.info("BUILDTYPE {}!!!!".format(self.settings.build_type))
+            """)
+        client.save({"conanfile.py": conanfile})
+        client.run("create . pkg-model/0.1@user/testing -s os=Windows -s *-model*:build_type=Debug "
+                   "-s build_type=Release")
+        assert "pkg-model/0.1@user/testing: BUILDTYPE Debug!!!!" in client.out
+
+        client.save({"conanfile.py": GenConanfile().with_require("pkg-model/0.1@user/testing")})
+        client.run('create . consumer/0.1@user/testing -s os=Linux -s *-model*:os=Windows '
+                   '-s "*-model*:build_type=Debug" -s build_type=Release --build')
+        assert "pkg-model/0.1@user/testing: BUILDTYPE Debug!!!!" in client.out
+        self.assertIn("consumer/0.1@user/testing: Created package", client.out)
+
     def test_per_package_setting_no_userchannel(self):
         client = TestClient()
         conanfile = textwrap.dedent("""
