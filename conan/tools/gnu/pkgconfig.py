6941bfbbac4fe2b5571dcc7b2425b224a44d37af
memsharded <james@conan.io>
2021-11-23 10:21:46 +0100

    modern tools.gnu.PkgConfig

---
+++ conan/tools/gnu/pkgconfig.py
@@ -0,0 +1,83 @@
+import subprocess
+
+from conan.tools.env import Environment
+from conans.errors import ConanException
+from conans.util.runners import check_output_runner
+
+
+class PkgConfig:
+
+    def __init__(self, conanfile, package, pkg_config_path=None):
+        """
+        :param package: library (package) name, such as libastral
+        """
+        self._conanfile = conanfile
+        self._package = package
+        self._info = dict()
+        self._pkg_config_path = pkg_config_path
+
+    def _parse_output(self, option):
+        executable = self._conanfile.conf["tools.gnu:pkg_config"] or "pkg-config"
+        command = [executable, '--' + option, self._package, '--print-errors']
+        try:
+            env = Environment()
+            if self._pkg_config_path:
+                env.prepend_path("PKG_CONFIG_PATH", self._pkg_config_path)
+            with env.vars(self._conanfile).apply():
+                return check_output_runner(command).strip()
+        except subprocess.CalledProcessError as e:
+            raise ConanException('pkg-config command %s failed with error: %s' % (command, e))
+
+    def _get_option(self, option):
+        if option not in self._info:
+            self._info[option] = self._parse_output(option)
+        return self._info[option]
+
+    @property
+    def includedirs(self):
+        return [include[2:] for include in self._get_option('cflags-only-I').split()]
+
+    @property
+    def cflags(self):
+        return [flag for flag in self._get_option('cflags-only-other').split()
+                if not flag.startswith("-D")]
+
+    @property
+    def defines(self):
+        return [flag for flag in self._get_option('cflags-only-other').split()
+                if flag.startswith("-D")]
+
+    @property
+    def libdirs(self):
+        return [lib[2:] for lib in self._get_option('libs-only-L').split()]
+
+    @property
+    def libs(self):
+        return [lib[2:] for lib in self._get_option('libs-only-l').split()]
+
+    @property
+    def linkflags(self):
+        return self._get_option('libs-only-other').split()
+
+    @property
+    def provides(self):
+        return self._get_option('print-provides')
+
+    @property
+    def version(self):
+        return self._get_option('modversion')
+
+    def cpp_info(self, cpp_info, is_system=True):
+        if not self.provides:
+            raise ConanException("PkgConfig error, '{}' files not available".format(self._package))
+        if is_system:
+            cpp_info.system_libs = self.libs
+        else:
+            cpp_info.libs = self.libs
+        cpp_info.libdirs = self.libdirs
+        cpp_info.sharedlinkflags = self.linkflags
+        cpp_info.exelinkflags = self.linkflags
+        cpp_info.defines = self.defines
+        cpp_info.includedirs = self.includedirs
+        cpp_info.cflags = self.cflags
+        cpp_info.cxxflags = self.cflags
---
+++ conans/test/functional/tools/test_pkg_config.py
@@ -0,0 +1,60 @@
+import os
+
+import pytest
+
+from conan.tools.gnu.pkgconfig import PkgConfig
+from conans.errors import ConanException
+from conans.model.new_build_info import NewCppInfo
+from conans.test.utils.mocks import ConanFileMock
+from conans.test.utils.test_files import temp_folder
+from conans.util.files import save
+
+libastral_pc = """
+PC FILE EXAMPLE:
+
+prefix=/usr/local
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib
+includedir=${prefix}/include
+
+Name: libastral
+Description: Interface library for Astral data flows
+Version: 6.6.6
+Libs: -L${libdir}/libastral -lastral -Wl,--whole-archive
+Cflags: -I${includedir}/libastral -D_USE_LIBASTRAL
+"""
+
+
+@pytest.mark.tool_pkg_config
+class TestPkgConfig:
+    def test_negative(self):
+        conanfile = ConanFileMock()
+        pkg_config = PkgConfig(conanfile, 'libsomething_that_does_not_exist_in_the_world')
+        with pytest.raises(ConanException):
+            pkg_config.libs()
+
+    def test_pc(self):
+        tmp_dir = temp_folder()
+        filename = os.path.join(tmp_dir, 'libastral.pc')
+        save(filename, libastral_pc)
+
+        conanfile = ConanFileMock()
+        pkg_config = PkgConfig(conanfile, "libastral", pkg_config_path=tmp_dir)
+
+        assert pkg_config.provides == "libastral = 6.6.6"
+        assert pkg_config.version == "6.6.6"
+        assert pkg_config.includedirs == ['/usr/local/include/libastral']
+        assert pkg_config.defines == ['-D_USE_LIBASTRAL']
+        assert pkg_config.libs == ['astral']
+        assert pkg_config.libdirs == ['/usr/local/lib/libastral']
+        assert pkg_config.linkflags == ['-Wl,--whole-archive']
+
+        cpp_info = NewCppInfo()
+        pkg_config.cpp_info(cpp_info)
+
+        assert cpp_info.includedirs == ['/usr/local/include/libastral']
+        assert cpp_info.defines == ['-D_USE_LIBASTRAL']
+        assert cpp_info.system_libs == ['astral']
+        assert cpp_info.libdirs == ['/usr/local/lib/libastral']
+        assert cpp_info.sharedlinkflags == ['-Wl,--whole-archive']
+
