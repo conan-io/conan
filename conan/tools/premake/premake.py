90bb91e5156eca1b20d66e303a458447d8f977fa
Enhex <enhex0@gmail.com>
2021-11-26 06:49:24 +0200

    add "msvc" compiler mapping to premake action

--- conan/tools/premake/premake.py
+++ conan/tools/premake/premake.py
@@ -15,5 +15,25 @@ class Premake(object):
                         '16': '2019'}
             premake_command = "premake5 vs%s" % _visuals.get(str(self.settings.compiler.version), "UnknownVersion %s" % str(self.settings.compiler.version))
             self.run(premake_command)
+        else if "msvc" in self.settings.compiler:
+            _visuals = {'14.0': '2005',
+                        '15.0': '2008',
+                        '16.0': '2010',
+                        '17.0': '2012',
+                        '18.0': '2013',
+                        '19.0': '2015',
+                        # add non-trailing 0 variation manually
+                        '19.1': '2017',
+                        '19.2': '2019'}
+            # generate VS2017 versions
+            for i in range(0,7):
+                ver = '19.1' + str(i)
+                _visuals[ver] = '2017'
+            # generate VS2019 versions
+            for i in range(0,10):
+                ver = '19.2' + str(i)
+                _visuals[ver] = '2019'
+            premake_command = "premake5 vs%s" % _visuals.get(str(self.settings.compiler.version), "UnknownVersion %s" % str(self.settings.compiler.version))
+            self.run(premake_command)
         else:
             self.run("premake5 gmake2")
