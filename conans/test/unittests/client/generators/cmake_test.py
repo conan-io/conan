bc5d12e82fcdfe42e6d065913fbe18998f399f83
memsharded <james@conan.io>
2021-12-10 14:07:07 +0100

    fix tests

--- conans/test/unittests/client/generators/cmake_test.py
+++ conans/test/unittests/client/generators/cmake_test.py
@@ -314,7 +314,7 @@ endmacro()""", macro)
         conanfile = ConanFile(Mock(), None)
         conanfile.initialize(settings_mock, EnvValues())
         install_folder = "/c/foo/testing"
-        setattr(conanfile, "install_folder", install_folder)
+        conanfile.folders.set_base_install(install_folder)
         conanfile.generators = ["cmake_find_package_multi"]
         definitions_builder = CMakeDefinitionsBuilder(conanfile)
         definitions = definitions_builder.get_definitions("3.13")
@@ -346,7 +346,7 @@ endmacro()""", macro)
             settings_mock = _MockSettings(build_type="Release")
             conanfile = ConanFile(Mock(), None)
             install_folder = "/c/foo/testing"
-            setattr(conanfile, "install_folder", install_folder)
+            conanfile.folders.set_base_install(install_folder)
             conanfile.initialize(settings_mock, EnvValues())
             assert CMake(conanfile)
 
