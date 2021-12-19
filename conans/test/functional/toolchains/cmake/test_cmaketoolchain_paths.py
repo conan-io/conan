f9978e8c0d478cc4e8735b2fe04ecf856f1f6c73
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-12-19 03:27:27 +0100

    tests of find_file()/find_path()/find_library()/find_program()

--- conans/test/functional/toolchains/cmake/test_cmaketoolchain_paths.py
+++ conans/test/functional/toolchains/cmake/test_cmaketoolchain_paths.py
@@ -108,6 +108,149 @@ def test_cmaketoolchain_path_include_cmake_modules(require_type):
 
 
 @pytest.mark.tool_cmake
+def test_cmaketoolchain_path_find_file_find_path():
+    """Test that headers in includedirs of requires can be found with
+    find_file() and find_path() in consumer CMakeLists
+    """
+    client = TestClient()
+
+    conanfile = textwrap.dedent("""
+        from conans import ConanFile
+        class TestConan(ConanFile):
+            settings = "os", "arch", "compiler", "build_type"
+            exports = "*"
+            def layout(self):
+                pass
+            def package(self):
+                self.copy(pattern="*.h", dst="include")
+    """)
+    client.save({"conanfile.py": conanfile, "hello.h": ""})
+    client.run("create . hello/0.1@")
+
+    consumer = textwrap.dedent("""
+        cmake_minimum_required(VERSION 3.15)
+        project(MyHello)
+        find_file(HELLOFILE hello.h)
+        if(HELLOFILE)
+            message("Found file hello.h")
+        endif()
+        find_path(HELLODIR hello.h)
+        if(HELLODIR)
+            message("Found path of hello.h")
+        endif()
+    """)
+    client.save({"CMakeLists.txt": consumer}, clean_first=True)
+    client.run("install hello/0.1@ -g CMakeToolchain")
+    with client.chdir("build"):
+        client.run_command("cmake .. -DCMAKE_TOOLCHAIN_FILE=../conan_toolchain.cmake")
+    assert "Found file hello.h" in client.out
+    assert "Found path of hello.h" in client.out
+
+
+@pytest.mark.tool_cmake
+def test_cmaketoolchain_path_find_library():
+    """Test that libraries in libdirs of requires can be found with
+    find_library() in consumer CMakeLists
+    """
+    client = TestClient()
+
+    conanfile = textwrap.dedent("""
+        from conans import ConanFile
+        class TestConan(ConanFile):
+            settings = "os", "arch", "compiler", "build_type"
+            exports = "*"
+            def layout(self):
+                pass
+            def package(self):
+                self.copy(pattern="*", dst="lib")
+    """)
+    client.save({"conanfile.py": conanfile, "libhello.a": "", "hello.lib": ""})
+    client.run("create . hello/0.1@")
+
+    consumer = textwrap.dedent("""
+        cmake_minimum_required(VERSION 3.15)
+        project(MyHello)
+        find_library(HELLOLIB hello)
+        if(HELLOLIB)
+            message("Found hello lib")
+        endif()
+    """)
+    client.save({"CMakeLists.txt": consumer}, clean_first=True)
+    client.run("install hello/0.1@ -g CMakeToolchain")
+    with client.chdir("build"):
+        client.run_command("cmake .. -DCMAKE_TOOLCHAIN_FILE=../conan_toolchain.cmake")
+    assert "Found hello lib" in client.out
+
+
+@pytest.mark.tool_cmake
+def test_cmaketoolchain_path_find_program():
+    """Test that executables in bindirs of build_requires can be found with
+    find_program() in consumer CMakeLists.
+    Moreover, they must be found before any other executable of requires.
+    """
+    client = TestClient()
+
+    conanfile = textwrap.dedent("""
+        import os
+        from conans import ConanFile
+        class TestConan(ConanFile):
+            settings = "os", "arch", "compiler", "build_type"
+            exports = "*"
+            def layout(self):
+                pass
+            def package(self):
+                self.copy(pattern="*", dst="bin")
+            def package_info(self):
+                self.cpp_info.bindirs = [os.path.join("bin", "require_host")]
+    """)
+    client.save({"conanfile.py": conanfile,
+                 "require_host/hello": "", "require_host/hello.exe": ""})
+    client.run("create . hello_host/0.1@")
+
+    conanfile = textwrap.dedent("""
+        import os
+        from conans import ConanFile
+        class TestConan(ConanFile):
+            settings = "os", "arch", "compiler", "build_type"
+            exports = "*"
+            def layout(self):
+                pass
+            def package(self):
+                self.copy(pattern="*", dst="bin")
+            def package_info(self):
+                self.cpp_info.bindirs = [os.path.join("bin", "require_build")]
+    """)
+    client.save({"conanfile.py": conanfile,
+                 "require_build/hello": "", "require_build/hello.exe": ""},
+                clean_first=True)
+    client.run("create . hello_build/0.1@")
+
+    conanfile = textwrap.dedent("""
+        from conans import ConanFile
+        class TestConan(ConanFile):
+            settings = "os", "arch", "compiler", "build_type"
+            requires = "hello_host/0.1"
+            build_requires = "hello_build/0.1"
+    """)
+    consumer = textwrap.dedent("""
+        cmake_minimum_required(VERSION 3.15)
+        project(MyHello)
+        find_program(HELLOPROG hello)
+        if(HELLOPROG)
+            message("Found hello prog: ${HELLOPROG}")
+        endif()
+    """)
+    client.save({"conanfile.py": conanfile, "CMakeLists.txt": consumer},
+                clean_first=True)
+    client.run("install . pkg/0.1@ -g CMakeToolchain")
+    with client.chdir("build"):
+        client.run_command("cmake .. -DCMAKE_TOOLCHAIN_FILE=../conan_toolchain.cmake")
+    assert "Found hello prog" in client.out
+    assert os.path.join("require_host", "hello") not in client.out
+    assert os.path.join("require_build", "hello") in client.out
+
+
+@pytest.mark.tool_cmake
 def test_cmaketoolchain_path_find_real_config():
     client = TestClient()
     conanfile = textwrap.dedent("""
