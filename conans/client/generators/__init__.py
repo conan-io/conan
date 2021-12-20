f9cbb2b015803a713304651952b62c01fa63e336
Luis <lasote@gmail.com>
2021-12-20 16:09:57 +0100

    purged ConfAggregator

--- conans/client/generators/__init__.py
+++ conans/client/generators/__init__.py
@@ -73,7 +73,7 @@ class GeneratorManager(object):
                                 "MesonToolchain", "MSBuildDeps", "QbsToolchain", "msbuild",
                                 "VirtualRunEnv", "VirtualBuildEnv", "AutotoolsDeps",
                                 "AutotoolsToolchain", "BazelDeps", "BazelToolchain", "PkgConfigDeps",
-                                "VCVars", "IntelCC", "XcodeDeps", "ConfAggregator"]
+                                "VCVars", "IntelCC", "XcodeDeps"]
 
     def add(self, name, generator_class, custom=False):
         if name not in self._generators or custom:
@@ -141,9 +141,6 @@ class GeneratorManager(object):
         elif generator_name == "XcodeDeps":
             from conan.tools.apple import XcodeDeps
             return XcodeDeps
-        elif generator_name == "ConfAggregator":
-            from conan.tools.conf import ConfAggregator
-            return ConfAggregator
         else:
             raise ConanException("Internal Conan error: Generator '{}' "
                                  "not commplete".format(generator_name))
