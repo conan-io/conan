af6afcfcfe0f9ebfbc359eac4e14ce1bf2ef1872
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-12-20 10:41:29 +0100

    add cross-build in real config test

--- conans/test/functional/toolchains/cmake/test_cmaketoolchain_paths.py
+++ conans/test/functional/toolchains/cmake/test_cmaketoolchain_paths.py
@@ -92,6 +92,103 @@ def test_cmaketoolchain_path_find_package(package, find_package, host):
 
 
 @pytest.mark.tool_cmake
+@pytest.mark.parametrize(
+    "host",
+    [
+        "native",
+        pytest.param(
+            "cross_build_iOS", marks=pytest.mark.skipif(
+                platform.system() != "Darwin", reason="OSX only",
+            ),
+        ),
+    ],
+)
+def test_cmaketoolchain_path_find_package_real_config(host):
+    client = TestClient()
+
+    cross_build = "cross_build" in host
+    if cross_build:
+        host_profile = "ios12.0-armv8"
+        profile_content = textwrap.dedent("""
+            include(default)
+            [settings]
+            os=iOS
+            os.version=12.0
+            arch=armv8
+        """)
+        client.save({host_profile: profile_content})
+
+    conanfile = textwrap.dedent("""
+        from conans import ConanFile
+        from conan.tools.cmake import CMake
+        import os
+        class TestConan(ConanFile):
+            settings = "os", "compiler", "build_type", "arch"
+            exports = "*"
+            generators = "CMakeToolchain"
+
+            def layout(self):
+                pass
+
+            def build(self):
+                cmake = CMake(self)
+                cmake.configure()
+
+            def package(self):
+                cmake = CMake(self)
+                cmake.install()
+
+            def package_info(self):
+                self.cpp_info.builddirs.append(os.path.join("hello", "cmake"))
+        """)
+    cmake = textwrap.dedent("""
+        cmake_minimum_required(VERSION 3.15)
+        project(MyHello NONE)
+
+        add_library(hello INTERFACE)
+        install(TARGETS hello EXPORT helloConfig)
+        export(TARGETS hello
+            NAMESPACE hello::
+            FILE "${CMAKE_CURRENT_BINARY_DIR}/helloConfig.cmake"
+        )
+        install(EXPORT helloConfig
+            DESTINATION "${CMAKE_INSTALL_PREFIX}/hello/cmake"
+            NAMESPACE hello::
+        )
+        """)
+    client.save({"conanfile.py": conanfile, "CMakeLists.txt": cmake})
+    client.run("create . hello/0.1@{}".format(
+        " -pr:b default -pr:h {}".format(host_profile) if cross_build else "",
+    ))
+
+    consumer = textwrap.dedent("""
+        project(MyHello NONE)
+        cmake_minimum_required(VERSION 3.15)
+
+        find_package(hello REQUIRED)
+        """)
+
+    client.save({"CMakeLists.txt": consumer}, clean_first=True)
+    if cross_build:
+        client.save({host_profile: profile_content})
+    client.run("install hello/0.1@ -g CMakeToolchain{}".format(
+        " -pr:b default -pr:h {}".format(host_profile) if cross_build else "",
+    ))
+    with client.chdir("build"):
+        client.run_command("cmake .. -DCMAKE_TOOLCHAIN_FILE=../conan_toolchain.cmake")
+    # If it didn't fail, it found the helloConfig.cmake
+    assert "Conan: Target declared" not in client.out
+
+    # If using the CMakeDeps generator, the in-package .cmake will be ignored
+    client.run("install hello/0.1@ -g CMakeToolchain -g CMakeDeps{}".format(
+        " -pr:b default -pr:h {}".format(host_profile) if cross_build else "",
+    ))
+    with client.chdir("build2"):  # A clean folder, not the previous one, CMake cache doesnt affect
+        client.run_command("cmake .. -DCMAKE_TOOLCHAIN_FILE=../conan_toolchain.cmake")
+    assert "Conan: Target declared 'hello::hello'" in client.out
+
+
+@pytest.mark.tool_cmake
 @pytest.mark.parametrize("require_type", ["requires", "build_requires"])
 def test_cmaketoolchain_path_include_cmake_modules(require_type):
     """Test that cmake module files in builddirs of requires and build_requires
@@ -348,70 +445,3 @@ def test_cmaketoolchain_path_find_program(host):
     assert "Found hello prog" in client.out
     assert "hello_host/0.1/" not in client.out
     assert "hello_build/0.1/" in client.out
-
-
-@pytest.mark.tool_cmake
-def test_cmaketoolchain_path_find_real_config():
-    client = TestClient()
-    conanfile = textwrap.dedent("""
-        from conans import ConanFile
-        from conan.tools.cmake import CMake
-        import os
-        class TestConan(ConanFile):
-            settings = "os", "compiler", "build_type", "arch"
-            exports = "*"
-            generators = "CMakeToolchain"
-
-            def layout(self):
-                pass
-
-            def build(self):
-                cmake = CMake(self)
-                cmake.configure()
-
-            def package(self):
-                cmake = CMake(self)
-                cmake.install()
-
-            def package_info(self):
-                self.cpp_info.builddirs.append(os.path.join("hello", "cmake"))
-        """)
-    cmake = textwrap.dedent("""
-        cmake_minimum_required(VERSION 3.15)
-        project(MyHello NONE)
-
-        add_library(hello INTERFACE)
-        install(TARGETS hello EXPORT helloConfig)
-        export(TARGETS hello
-            NAMESPACE hello::
-            FILE "${CMAKE_CURRENT_BINARY_DIR}/helloConfig.cmake"
-        )
-        install(EXPORT helloConfig
-            DESTINATION "${CMAKE_INSTALL_PREFIX}/hello/cmake"
-            NAMESPACE hello::
-        )
-        """)
-    client.save({"conanfile.py": conanfile,
-                 "CMakeLists.txt": cmake})
-    client.run("create . hello/0.1@")
-
-    consumer = textwrap.dedent("""
-        project(MyHello NONE)
-        cmake_minimum_required(VERSION 3.15)
-
-        find_package(hello REQUIRED)
-        """)
-
-    client.save({"CMakeLists.txt": consumer}, clean_first=True)
-    client.run("install hello/0.1@ -g CMakeToolchain")
-    with client.chdir("build"):
-        client.run_command("cmake .. -DCMAKE_TOOLCHAIN_FILE=../conan_toolchain.cmake")
-    # If it didn't fail, it found the helloConfig.cmake
-    assert "Conan: Target declared" not in client.out
-
-    # If using the CMakeDeps generator, the in-package .cmake will be ignored
-    # But it is still possible to include(owncmake)
-    client.run("install hello/0.1@ -g CMakeToolchain -g CMakeDeps")
-    with client.chdir("build2"):  # A clean folder, not the previous one, CMake cache doesnt affect
-        client.run_command("cmake .. -DCMAKE_TOOLCHAIN_FILE=../conan_toolchain.cmake")
-    assert "Conan: Target declared 'hello::hello'" in client.out
