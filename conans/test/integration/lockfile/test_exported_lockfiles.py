d52b9aecf3df2f6ee73a7b604975fba60a81ca12
memsharded <james@conan.io>
2022-01-03 12:56:04 +0100

    new test

--- conans/test/integration/lockfile/test_exported_lockfiles.py
+++ conans/test/integration/lockfile/test_exported_lockfiles.py
@@ -21,3 +21,35 @@ def test_exported_lockfile():
     client.run("install --reference=pkg/0.1")
     assert "dep/0.2" not in client.out
     assert "dep/0.1" in client.out
+
+
+def test_downstream_priority():
+    """ The downstream lockfile should always have priority. So far it works, because
+    a) when the consumer locks, it is already using the same set of locked dependencies
+    b) if the consumer forces other versions different to dependency locked ones, they will have
+       priority anyway, irrespective of the merge. The merge at the moment is still prioritizing
+       the latest, which might not be the downstream locked one
+    The possible way to achieve a failure in this approach at the moment is if we implement a
+    "ignore exported lockfiles" config, so downstream consumer can lock to a different version
+    that doesn't match the dependency one
+    """
+    client = TestClient()
+    client.save({"dep/conanfile.py": GenConanfile("dep", "0.1"),
+                 "dep2/conanfile.py": GenConanfile("dep", "0.2"),
+                 "pkg/conanfile.py": GenConanfile("pkg", "0.1").with_requires("dep/[*]")
+                                                               .with_exports("conan.lock"),
+                 "consumer/conanfile.py": GenConanfile().with_requirement("pkg/0.1")
+                                                        .with_requirement("dep/[<0.2]", force=True)})
+    client.run("create dep")
+    client.run("create dep2")
+    with client.chdir("pkg"):
+        client.run("lock create conanfile.py --lockfile-out=conan.lock")
+        client.run("create .")
+        assert "dep/0.2" in client.out
+        assert "dep/0.1" not in client.out
+
+    with client.chdir("consumer"):
+        client.run("lock create conanfile.py --lockfile-out=conan.lock")
+        client.run("install . --build=missing --lockfile=conan.lock")
+        assert "dep/0.2" not in client.out
+        assert "dep/0.1" in client.out
