f2e16a19461e603af4ae6a63315bb232299a7b2f
czoido <mrgalleta@gmail.com>
2022-01-19 18:31:51 +0100

    wip

--- conan/tools/system/package_manager.py
+++ conan/tools/system/package_manager.py
@@ -6,23 +6,35 @@ MODE_INSTALL = "install"
 
 
 class SystemPackageManagerTool(object):
+    tool_name = None
+    install_command = ""
+    update_command = ""
+    check_command = ""
+
     def __init__(self, conanfile):
         self._conanfile = conanfile
         self._active_tool = self._conanfile.conf["tools.system.package_manager:tool"]
         self._sudo = self._conanfile.conf["tools.system.package_manager:sudo"]
         self._sudo_askpass = self._conanfile.conf["tools.system.package_manager:sudo_askpass"]
         self._mode = self._conanfile.conf["tools.system.package_manager:mode"] or MODE_CHECK
-        self._arch_names = None
         self._arch = self._conanfile.settings.get_safe("arch")
+        self._arch_names = None
         self._arch_separator = ""
 
     def get_package_name(self, package):
         # TODO: should we only add the arch if cross-building?
         if self._arch in self._arch_names:
             return "{}{}{}".format(package, self._arch_separator,
-                                   self._arch_names.get(self._arch, ""))
+                                   self._arch_names.get(self._arch))
+        return package
 
-    def decorator(wrapped):
+    @property
+    def sudo_str(self):
+        sudo = "sudo " if self._sudo else ""
+        askpass = "-A " if self._sudo and self._sudo_askpass else ""
+        return "{}{}".format(sudo, askpass)
+
+    def method_decorator(wrapped):
         def wrapper(self, *args, **kwargs):
             method_name = wrapped.__name__
             if self._active_tool == self.__class__.tool_name:
@@ -40,16 +52,38 @@ class SystemPackageManagerTool(object):
 
         return wrapper
 
-    @property
-    def sudo_str(self):
-        sudo = "sudo " if self._sudo else ""
-        askpass = "-A " if self._sudo and self._sudo_askpass else ""
-        return "{}{}".format(sudo, askpass)
+    @method_decorator  # noqa
+    def install(self, packages):
+        packages_arch = [self.get_package_name(package) for package in packages]
+        command = self.install_command.format(sudo=self.sudo_str,
+                                              tool=self.tool_name,
+                                              packages=" ".join(packages_arch))
+        return self._conanfile.run(command)
+
+    @method_decorator  # noqa
+    def update(self):
+        command = self.update_command.format(sudo=self.sudo_str,
+                                              tool=self.tool_name)
+        return self._conanfile.run(command)
+
+    @method_decorator  # noqa
+    def check(self, packages):
+        not_installed = [pkg for pkg in packages if
+                         self.check_package(self.get_package_name(pkg)) != 0]
+        return not_installed
+
+    def check_package(self, package):
+        command = self.check_command.format(tool=self.tool_name,
+                                            package=package)
+        return self._conanfile.run(command, ignore_errors=True)
 
 
 class Apt(SystemPackageManagerTool):
     # TODO: apt? apt-get?
     tool_name = "apt-get"
+    install_command = "{sudo}{tool} install -y {recommends}{packages}"
+    update_command = "{sudo}{tool} update"
+    check_command = "dpkg-query -W -f='${{Status}}' {package} | grep -q \"ok installed\""
 
     def __init__(self, conanfile, arch_names=None):
         super(Apt, self).__init__(conanfile)
@@ -64,33 +98,22 @@ class Apt(SystemPackageManagerTool):
 
         self._arch_separator = ":"
 
-    @SystemPackageManagerTool.decorator
-    def install(self, packages, recommends=False, update=False):
-        if update:
-            self.update()
+    @SystemPackageManagerTool.method_decorator
+    def install(self, packages, recommends=False):
         recommends_str = '' if recommends else '--no-install-recommends '
         packages_arch = [self.get_package_name(package) for package in packages]
-        command = "{}{} install -y {}{}".format(self.sudo_str, self.tool_name, recommends_str,
-                                                " ".join(packages_arch))
+        command = self.install_command.format(sudo=self.sudo_str,
+                                              tool=self.tool_name,
+                                              recommends=recommends_str,
+                                              packages=" ".join(packages_arch))
         return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def update(self):
-        command = "{}{} update".format(self.sudo_str, self.tool_name)
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def check(self, packages):
-        not_installed = [pkg for pkg in packages if self.check_package(self.get_package_name(pkg)) != 0]
-        return not_installed
-
-    def check_package(self, package):
-        command = "dpkg-query -W -f='${{Status}}' {} | grep -q \"ok installed\"".format(package)
-        return self._conanfile.run(command, ignore_errors=True)
 
 
 class Yum(SystemPackageManagerTool):
     tool_name = "yum"
+    install_command = "{sudo}{tool} install -y {packages}"
+    update_command = "{sudo}{tool} check-update -y"
+    check_command = "{tool} -q"
 
     def __init__(self, conanfile, arch_names=None):
         super(Yum, self).__init__(conanfile)
@@ -104,27 +127,6 @@ class Yum(SystemPackageManagerTool):
                             "s390x": "s390x"} if arch_names is None else arch_names
         self._arch_separator = "."
 
-    @SystemPackageManagerTool.decorator
-    def install(self, packages):
-        packages_arch = [self.get_package_name(package) for package in packages]
-        command = "{}{} install -y {}".format(self.sudo_str, self.tool_name, " ".join(packages_arch))
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def update(self):
-        command = "{}{} check-update -y".format(self.sudo_str, self.tool_name)
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def check(self, packages):
-        not_installed = [self.get_package_name(pkg) for pkg in packages if
-                         self.check_package(self.get_package_name(pkg)) != 0]
-        return not_installed
-
-    def check_package(self, package):
-        command = "rpm -q {}".format(package)
-        return self._conanfile.run(command, ignore_errors=True)
-
 
 class Dnf(Yum):
     tool_name = "dnf"
@@ -132,154 +134,47 @@ class Dnf(Yum):
 
 class Brew(SystemPackageManagerTool):
     tool_name = "brew"
-
-    @SystemPackageManagerTool.decorator
-    def install(self, packages):
-        command = "{}{} install {}".format(self.sudo_str, self.tool_name, " ".join(packages))
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def update(self):
-        command = "{}{} update".format(self.sudo_str, self.tool_name)
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def check(self, packages):
-        not_installed = [pkg for pkg in packages if self.check_package(pkg) != 0]
-        return not_installed
-
-    def check_package(self, package):
-        command = 'test -n "$({} ls --versions {})"'.format(self.tool_name, package)
-        return self._conanfile.run(command, ignore_errors=True)
+    install_command = "{sudo}{tool} install {packages}"
+    update_command = "{sudo}{tool} update}"
+    check_command = 'test -n "$({tool} ls --versions {package})"'
 
 
 class Pkg(SystemPackageManagerTool):
     tool_name = "pkg"
-
-    @SystemPackageManagerTool.decorator
-    def install(self, packages):
-        command = "{}{} install -y {}".format(self.sudo_str, self.tool_name, " ".join(packages))
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def update(self):
-        command = "{}{} update".format(self.sudo_str, self.tool_name)
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def check(self, packages):
-        not_installed = [pkg for pkg in packages if self.check_package(pkg) != 0]
-        return not_installed
-
-    def check_package(self, package):
-        command = "{} info {}".format(self.tool_name, package)
-        return self._conanfile.run(command, ignore_errors=True)
+    install_command = "{sudo}{tool} install -y {packages}"
+    update_command = "{sudo}{tool} update"
+    check_command = "{tool} info {package}"
 
 
 class PkgUtil(SystemPackageManagerTool):
     tool_name = "pkgutil"
-
-    @SystemPackageManagerTool.decorator
-    def install(self, packages):
-        command = "{}{} --install --yes {}".format(self.sudo_str, self.tool_name, " ".join(packages))
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def update(self):
-        command = "{}{} --catalog".format(self.sudo_str, self.tool_name)
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def check(self, packages):
-        not_installed = [pkg for pkg in packages if self.check_package(pkg) != 0]
-        return not_installed
-
-    def check_package(self, package):
-        command = 'test -n "`{} --list {}`"'.format(self.tool_name, package)
-        return self._conanfile.run(command, ignore_errors=True)
+    install_command = "{sudo}{tool} --install --yes {packages}"
+    update_command = "{sudo}{tool} --catalog"
+    check_command = 'test -n "`{tool} --list {package}`"'
 
 
 class Chocolatey(SystemPackageManagerTool):
     tool_name = "choco"
-
-    @SystemPackageManagerTool.decorator
-    def install(self, packages):
-        command = "{} --install --yes {}".format(self.tool_name, " ".join(packages))
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def update(self):
-        command = "{} outdated".format(self.tool_name)
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def check(self, packages):
-        not_installed = [pkg for pkg in packages if self.check_package(pkg) != 0]
-        return not_installed
-
-    def check_package(self, package):
-        command = '{} search --local-only --exact {} | ' \
-                  'findstr /c:"1 packages installed."'.format(self.tool_name, package)
-        return self._conanfile.run(command, ignore_errors=True)
+    install_command = "{tool} --install --yes {package}"
+    update_command = "{tool} outdated"
+    check_command = '{tool} search --local-only --exact {package} | ' \
+                    'findstr /c:"1 packages installed."'
 
 
 class PacMan(SystemPackageManagerTool):
     tool_name = "pacman"
+    install_command = "{sudo}{tool} -S --noconfirm {packages}"
+    update_command = "{sudo}{tool} -Syyu --noconfirm"
+    check_command = "{tool} -Qi {package}"
 
     def __init__(self, conanfile, arch_names=None):
         super(PacMan, self).__init__(conanfile)
         self._arch_names = {"x86": "lib32"} if arch_names is None else arch_names
         self._arch_separator = "-"
 
-    @SystemPackageManagerTool.decorator
-    def install(self, packages):
-        packages_arch = [self.get_package_name(pkg) for pkg in packages]
-        command = "{}{} -S --noconfirm {}".format(self.sudo_str, self.tool_name,
-                                                  " ".join(packages_arch))
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def update(self):
-        command = "{}{} -Syyu --noconfirm".format(self.sudo_str, self.tool_name)
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def check(self, packages):
-        not_installed = [self.get_package_name(pkg) for pkg in packages if
-                         self.check_package(self.get_package_name(pkg)) != 0]
-        return not_installed
-
-    def check_package(self, package):
-        command = "{} -Qi {}".format(self.tool_name, package)
-        return self._conanfile.run(command, ignore_errors=True)
-
 
 class Zypper(SystemPackageManagerTool):
     tool_name = "zypper"
-
-    def __init__(self, conanfile, arch_names=None):
-        super(Zypper, self).__init__(conanfile)
-        self._arch_names = {"x86": "i586"} if arch_names is None else arch_names
-        self._arch_separator = "."
-
-    @SystemPackageManagerTool.decorator
-    def install(self, packages):
-        packages_arch = [self.get_package_name(pkg) for pkg in packages]
-        command = "{}{}  --non-interactive in {}".format(self.sudo_str, self.tool_name,
-                                                         " ".join(packages_arch))
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def update(self):
-        command = "{}{} --non-interactive ref".format(self.sudo_str, self.tool_name)
-        return self._conanfile.run(command)
-
-    @SystemPackageManagerTool.decorator
-    def check(self, packages):
-        not_installed = [self.get_package_name(pkg) for pkg in packages if
-                         self.check_package(self.get_package_name(pkg)) != 0]
-        return not_installed
-
-    def check_package(self, package):
-        command = "{} -Qi {}".format(self.tool_name, package)
-        return self._conanfile.run(command, ignore_errors=True)
+    install_command = "{sudo}{tool}  --non-interactive in {packages}"
+    update_command = "{sudo}{tool} --non-interactive ref"
+    check_command = "rpm -q {package}"
