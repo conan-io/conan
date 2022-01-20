de71ffcc474cc74336a4f2ec155aa80640ceca8d
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-20 17:19:26 +0100

    force CMP0091 policy to NEW in CMakeToolchain if msvc
    Even with CMake >= 3.15, if the toolchain is used with a CMakeLists whose cmake_minimum_required() is lower than 3.15, CMP0091 is set to OLD by default, breaking the conan logic of vc runtime injection through CMakeToolchain.
    Therefore the toolchain must ensures that CMP0091 is set to NEW.

--- conan/tools/cmake/toolchain.py
+++ conan/tools/cmake/toolchain.py
@@ -99,6 +99,9 @@ class VSRuntimeBlock(Block):
             {% set genexpr.str = genexpr.str +
                                   '$<$<CONFIG:' + config + '>:' + value|string + '>' %}
         {% endfor %}
+        if(POLICY CMP0091)
+            cmake_policy(SET CMP0091 NEW)
+        endif()
         set(CMAKE_MSVC_RUNTIME_LIBRARY "{{ genexpr.str }}")
         """)
 
