ef3485c427a50dda3f8fb2cf5cafe65813dbdc2f
czoido <mrgalleta@gmail.com>
2022-01-20 15:03:31 +0100

    test brew check

--- conans/test/integration/tools/system/package_manager.py
+++ conans/test/integration/tools/system/package_manager.py
@@ -177,8 +177,26 @@ def test_apt_check():
             def system_requirements(self):
                 apt = Apt(self)
                 not_installed = apt.check(["non-existing1", "non-existing2"])
-                print(not_installed)
+                print("missing:", not_installed)
         """)})
     client.run("create . test/1.0@ -c tools.system.package_manager:tool=apt-get -s:b arch=armv8 "
                "-s:h arch=x86")
-    assert "['non-existing1:i386', 'non-existing2:i386']" in client.out
+    assert "missing: ['non-existing1:i386', 'non-existing2:i386']" in client.out
+
+
+@pytest.mark.tool_brew
+@pytest.mark.skipif(platform.system() != "Darwin", reason="Requires brew")
+def test_brew_check():
+    client = TestClient()
+    client.save({"conanfile.py": textwrap.dedent("""
+        from conans import ConanFile
+        from conan.tools.system import Brew
+        class MyPkg(ConanFile):
+            settings = "arch"
+            def system_requirements(self):
+                brew = Brew(self)
+                not_installed = brew.check(["non-existing1", "non-existing2"])
+                print("missing:", not_installed)
+        """)})
+    client.run("create . test/1.0@")
+    assert "missing: ['non-existing1', 'non-existing2']" in client.out
