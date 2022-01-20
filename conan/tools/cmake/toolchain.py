c77f110c8f4ae686c4e7d9dc367527012ca1a94c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-20 19:13:22 +0100

    external injection of policy must use CMAKE_POLICY_DEFAULT_CMPNNNN
    see https://cmake.org/cmake/help/latest/variable/CMAKE_POLICY_DEFAULT_CMPNNNN.html

--- conan/tools/cmake/toolchain.py
+++ conan/tools/cmake/toolchain.py
@@ -99,9 +99,7 @@ class VSRuntimeBlock(Block):
             {% set genexpr.str = genexpr.str +
                                   '$<$<CONFIG:' + config + '>:' + value|string + '>' %}
         {% endfor %}
-        if(POLICY CMP0091)
-            cmake_policy(SET CMP0091 NEW)
-        endif()
+        set(CMAKE_POLICY_DEFAULT_CMP0091 NEW)
         set(CMAKE_MSVC_RUNTIME_LIBRARY "{{ genexpr.str }}")
         """)
 
