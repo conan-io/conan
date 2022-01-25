a6ef6c9970f60b3782bcdc539018f03bd31cd8c7
memsharded <james@conan.io>
2022-01-25 13:56:48 +0100

    fixing tests

--- conans/test/integration/package_id/test_validate.py
+++ conans/test/integration/package_id/test_validate.py
@@ -69,7 +69,8 @@ class TestValidate(unittest.TestCase):
         client.run("create . --name=pkg --version=0.1 -s os=Windows", assert_error=True)
         self.assertIn("pkg/0.1: Invalid: Windows not supported", client.out)
         print(client.out)
-        client.assert_listed_binary({"pkg/0.1": ("INVALID", "Invalid")})
+        client.assert_listed_binary({"pkg/0.1": ("cf2e4ff978548fafd099ad838f9ecb8858bf25cb",
+                                                 "Invalid")})
         client.run("graph info --reference=pkg/0.1@ -s os=Windows")
         self.assertIn("pkg/0.1: Main binary package 'cf2e4ff978548fafd099ad838f9ecb8858bf25cb' "
                       "missing. Using compatible package '02145fcd0a1e750fb6e1d2f119ecdf21d2adaac8'",
@@ -99,7 +100,8 @@ class TestValidate(unittest.TestCase):
 
         client.run("create . --name=pkg --version=0.1 -s os=Windows", assert_error=True)
         self.assertIn("pkg/0.1: Invalid: Windows not supported", client.out)
-        client.assert_listed_binary({"pkg/0.1": ("INVALID", "Invalid")})
+        client.assert_listed_binary({"pkg/0.1": ("357add7d387f11a959f3ee7d4fc9c2487dbaa604",
+                                                 "Invalid")})
 
         client.run("graph info --reference=pkg/0.1@ -s os=Windows")
         self.assertIn("package_id: {}".format(NO_SETTINGS_PACKAGE_ID), client.out)
@@ -276,8 +278,10 @@ class TestValidate(unittest.TestCase):
         client.save({"conanfile.py": GenConanfile().with_requires("dep/0.1")})
         error = client.run("create . --name=pkg --version=0.1 -s os=Windows", assert_error=True)
         self.assertEqual(error, ERROR_INVALID_CONFIGURATION)
-        client.assert_listed_binary({"dep/0.1": ("INVALID", "Invalid")})
-        client.assert_listed_binary({"pkg/0.1": ("INVALID", "Invalid")})
+        client.assert_listed_binary({"dep/0.1": ("cf2e4ff978548fafd099ad838f9ecb8858bf25cb",
+                                                 "Invalid")})
+        client.assert_listed_binary({"pkg/0.1": ("a1097c99904cb5a20e9033e9c5a3c2cb6c53d35d",
+                                                 "Build")})
         self.assertIn("ERROR: There are invalid packages (packages that cannot "
                       "exist for this configuration):", client.out)
         self.assertIn("dep/0.1: Invalid: Windows not supported", client.out)
