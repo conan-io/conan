e3e11f9bd1ac9a3e05517b5d36b42b68ff15fec7
czoido <mrgalleta@gmail.com>
2022-01-27 17:16:50 +0100

    review

--- conans/client/graph/range_resolver.py
+++ conans/client/graph/range_resolver.py
@@ -66,8 +66,7 @@ class RangeResolver(object):
 
     def _search_and_resolve_remotes(self, search_ref, version_range):
         results = []
-        remotes = self._conan_app.enabled_remotes
-        for remote in remotes:
+        for remote in self._conan_app.selected_remotes:
             remote_results = self._search_remote_recipes(remote, search_ref)
             remote_results = [ref for ref in remote_results
                               if ref.user == search_ref.user
