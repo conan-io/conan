6b9b2c61da7fab31aea11e6e46190a16fbc1092f
memsharded <james@conan.io>
2022-02-14 17:50:33 +0100

    fix test

--- conans/test/integration/toolchains/cmake/test_cmake.py
+++ conans/test/integration/toolchains/cmake/test_cmake.py
@@ -17,7 +17,7 @@ def test_configure_args():
             def build(self):
                 cmake = CMake(self)
                 cmake.configure(variables={"MYVAR": "MYVALUE"})
-                cmake.build(cli_args=["--verbose"], build_tool_args=["-something"])
+                cmake.build(cli_args=["--verbosebuild"], build_tool_args=["-something"])
                 cmake.test(cli_args=["--testverbose"], build_tool_args=["-testok"])
 
             def run(self, *args, **kwargs):
@@ -25,6 +25,10 @@ def test_configure_args():
             """)
     client.save({"conanfile.py": conanfile})
     client.run("create . ")
+    # TODO: This check is ugly, because the command line is so different in each platform,
+    #  and args_to_string() is doing crazy stuff
     assert '-DMYVAR=MYVALUE' in client.out
-    assert "--verbose -- -something" in client.out
-    assert "--testverbose -- -testok" in client.out
+    assert "--verbosebuild" in client.out
+    assert "-something" in client.out
+    assert "--testverbose" in client.out
+    assert "-testok" in client.out
