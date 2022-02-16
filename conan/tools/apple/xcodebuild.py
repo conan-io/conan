1baf84876f7ecd9457339a5ecb8374de6b45c747
czoido <mrgalleta@gmail.com>
2022-02-16 12:17:47 +0100

    add deployment_target

--- conan/tools/apple/xcodebuild.py
+++ conan/tools/apple/xcodebuild.py
@@ -6,15 +6,16 @@ from conans.errors import ConanException
 
 class XcodeBuild(object):
     def __init__(self, conanfile):
-        self._conanfile = conanfile
+        self.conanfile = conanfile
         self.build_type = conanfile.settings.get_safe("build_type")
         self.arch = to_apple_arch(conanfile.settings.get_safe("arch"))
-        self._sdk = conanfile.settings.get_safe("os.sdk") or ""
-        self._sdk_version = conanfile.settings.get_safe("os.sdk_version") or ""
+        self.sdk = conanfile.settings.get_safe("os.sdk") or ""
+        self.sdk_version = conanfile.settings.get_safe("os.sdk_version") or ""
+        self.os_version = conanfile.settings.get_safe("os.version") or ""
 
     @property
     def verbosity(self):
-        verbosity = self._conanfile.conf["tools.apple.xcodebuild:verbosity"]
+        verbosity = self.conanfile.conf["tools.apple.xcodebuild:verbosity"]
         if not verbosity:
             return ""
         elif verbosity == "quiet" or verbosity == "verbose":
@@ -23,20 +24,22 @@ class XcodeBuild(object):
             raise ConanException("Value {} for 'tools.apple.xcodebuild:verbosity' is not valid".format(verbosity))
 
     @property
-    def sdk(self):
+    def sdkroot(self):
         # TODO: check if we want move this logic to tools
         # User's sdk_path has priority, then if specified try to compose sdk argument
         # with sdk/sdk_version settings, leave blank otherwise and the sdk will be automatically
         # chosen by the build system
-        sdk = self._conanfile.conf["tools.apple:sdk_path"]
-        if not sdk and self._sdk:
-            sdk = "{}{}".format(self._sdk, self._sdk_version)
+        sdk = self.conanfile.conf["tools.apple:sdk_path"]
+        if not sdk and self.sdk:
+            sdk = "{}{}".format(self.sdk, self.sdk_version)
         return "SDKROOT={}".format(sdk) if sdk else ""
 
+    @property
+    def deployment_target(self):
+        return "MACOSX_DEPLOYMENT_TARGET={}".format(self.os_version) if self.os_version else ""
+
     def build(self, xcodeproj):
-        cmd = "xcodebuild -project {} -configuration {} -arch {} {} {}".format(xcodeproj,
-                                                                               self.build_type,
-                                                                               self.arch,
-                                                                               self.sdk,
-                                                                               self.verbosity)
-        self._conanfile.run(cmd)
+        cmd = "xcodebuild -project {} -configuration {} -arch " \
+              "{} {} {} {}".format(xcodeproj, self.build_type, self.arch, self.sdkroot,
+                                   self.verbosity, self.deployment_target)
+        self.conanfile.run(cmd)
