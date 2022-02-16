260dde0f76b48eb9511df67b19d5aaa77674ace2
czoido <mrgalleta@gmail.com>
2022-02-16 19:32:57 +0100

    basic test

---
+++ conans/test/integration/toolchains/apple/test_xcodetoolchain.py
@@ -0,0 +1,20 @@
+import platform
+
+import pytest
+
+from conans.test.utils.tools import TestClient
+
+
+@pytest.mark.skipif(platform.system() != "Darwin", reason="Only for MacOS")
+def test_generator_files():
+    client = TestClient()
+    client.save({"conanfile.txt": "[generators]\nXcodeToolchain\n"})
+    client.run("install . -s build_type=Release")
+    client.run("install . -s build_type=Debug")
+    toolchain_all = client.load("conantoolchain.xcconfig")
+    toolchain_vars_release = client.load("conantoolchain_release_x86_64.xcconfig")
+    toolchain_vars_debug = client.load("conantoolchain_debug_x86_64.xcconfig")
+    assert '#include "conantoolchain_release_x86_64.xcconfig"' in toolchain_all
+    assert '#include "conantoolchain_debug_x86_64.xcconfig"' in toolchain_all
+    assert 'CLANG_CXX_LIBRARY[config=Debug][arch=x86_64][sdk=*]=libc++' in toolchain_vars_debug
+    assert 'CLANG_CXX_LIBRARY[config=Release][arch=x86_64][sdk=*]=libc++' in toolchain_vars_release
