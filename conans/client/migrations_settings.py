e0d454edeb95f34fb6b49f8d863366cdc8545701
czoido <mrgalleta@gmail.com>
2022-02-18 15:44:19 +0100

    conan 1.43.4

--- conans/client/migrations_settings.py
+++ conans/client/migrations_settings.py
@@ -2965,3 +2965,4 @@ cppstd: [None, 98, gnu98, 11, gnu11, 14, gnu14, 17, gnu17, 20, gnu20, 23, gnu23]
 settings_1_43_1 = settings_1_43_0
 settings_1_43_2 = settings_1_43_1
 settings_1_43_3 = settings_1_43_2
+settings_1_43_4 = settings_1_43_3
