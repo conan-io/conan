aa4fc7dee2dedab512904135867165db4aa295f4
czoido <mrgalleta@gmail.com>
2022-02-28 17:25:28 +0100

    private

--- conan/tools/apple/xcodebuild.py
+++ conan/tools/apple/xcodebuild.py
@@ -7,13 +7,13 @@ from conans.errors import ConanException
 class XcodeBuild(object):
     def __init__(self, conanfile):
         self._conanfile = conanfile
-        self.build_type = conanfile.settings.get_safe("build_type")
-        self.arch = to_apple_arch(conanfile.settings.get_safe("arch"))
-        self.sdk = conanfile.settings.get_safe("os.sdk") or ""
-        self.sdk_version = conanfile.settings.get_safe("os.sdk_version") or ""
+        self._build_type = conanfile.settings.get_safe("build_type")
+        self._arch = to_apple_arch(conanfile.settings.get_safe("arch"))
+        self._sdk = conanfile.settings.get_safe("os.sdk") or ""
+        self._sdk_version = conanfile.settings.get_safe("os.sdk_version") or ""
 
     @property
-    def verbosity(self):
+    def _verbosity(self):
         verbosity = self._conanfile.get("tools.apple.xcodebuild:verbosity", default="", check_type=str)
         if verbosity == "quiet" or verbosity == "verbose":
             return "-{}".format(verbosity)
@@ -26,11 +26,11 @@ class XcodeBuild(object):
         # with sdk/sdk_version settings, leave blank otherwise and the sdk will be automatically
         # chosen by the build system
         sdk = self._conanfile.conf["tools.apple:sdk_path"]
-        if not sdk and self.sdk:
-            sdk = "{}{}".format(self.sdk, self.sdk_version)
+        if not sdk and self._sdk:
+            sdk = "{}{}".format(self._sdk, self._sdk_version)
         return "SDKROOT={}".format(sdk) if sdk else ""
 
     def build(self, xcodeproj):
         cmd = "xcodebuild -project {} -configuration {} -arch {} " \
-              "{} {}".format(xcodeproj, self.build_type, self.arch, self._sdkroot, self.verbosity)
+              "{} {}".format(xcodeproj, self._build_type, self._arch, self._sdkroot, self._verbosity)
         self._conanfile.run(cmd)
