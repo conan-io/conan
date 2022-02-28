64d5c1ddab7d33d14fd193f9514b5fbbf7e822c6
Luis Martinez <lasote@gmail.com>
2022-02-28 09:47:01 +0100

    refactor toolchain (#10665)

--- conan/tools/cmake/toolchain/blocks.py
+++ conan/tools/cmake/toolchain/blocks.py
@@ -3,53 +3,47 @@ import re
 import textwrap
 from collections import OrderedDict
 
-import six
 from jinja2 import Template
 
-from conan.tools._check_build_profile import check_using_build_profile
-from conan.tools._compilers import architecture_flag, use_win_mingw
+from conan.tools._compilers import architecture_flag
 from conan.tools.apple.apple import is_apple_os
 from conan.tools.build import build_jobs
+from conan.tools.cmake.toolchain import CONAN_TOOLCHAIN_FILENAME
 from conan.tools.cmake.utils import is_multi_configuration
 from conan.tools.cross_building import cross_building
-from conan.tools.files.files import save_toolchain_args
 from conan.tools.intel import IntelCC
-from conan.tools.microsoft import VCVars
-from conan.tools.microsoft.visual import vs_ide_version, is_msvc
+from conan.tools.microsoft.visual import is_msvc
 from conans.errors import ConanException
-from conans.util.files import load, save
+from conans.util.files import load
 
 
-class Variables(OrderedDict):
-    _configuration_types = None  # Needed for py27 to avoid infinite recursion
+class ToolchainBlocks:
+    def __init__(self, conanfile, toolchain, items=None):
+        self._blocks = OrderedDict()
+        self._conanfile = conanfile
+        self._toolchain = toolchain
+        if items:
+            for name, block in items:
+                self._blocks[name] = block(conanfile, toolchain)
 
-    def __init__(self):
-        super(Variables, self).__init__()
-        self._configuration_types = {}
+    def remove(self, name):
+        del self._blocks[name]
 
-    def __getattribute__(self, config):
-        try:
-            return super(Variables, self).__getattribute__(config)
-        except AttributeError:
-            return self._configuration_types.setdefault(config, OrderedDict())
+    def __setitem__(self, name, block_type):
+        # Create a new class inheriting Block with the elements of the provided one
+        block_type = type('proxyUserBlock', (Block,), dict(block_type.__dict__))
+        self._blocks[name] = block_type(self._conanfile, self._toolchain)
 
-    @property
-    def configuration_types(self):
-        # Reverse index for the configuration_types variables
-        ret = OrderedDict()
-        for conf, definitions in self._configuration_types.items():
-            for k, v in definitions.items():
-                ret.setdefault(k, []).append((conf, v))
-        return ret
+    def __getitem__(self, name):
+        return self._blocks[name]
 
-    def quote_preprocessor_strings(self):
-        for key, var in self.items():
-            if isinstance(var, six.string_types):
-                self[key] = '"{}"'.format(var)
-        for config, data in self._configuration_types.items():
-            for key, var in data.items():
-                if isinstance(var, six.string_types):
-                    data[key] = '"{}"'.format(var)
+    def process_blocks(self):
+        result = []
+        for b in self._blocks.values():
+            content = b.get_rendered_content()
+            if content:
+                result.append(content)
+        return result
 
 
 class Block(object):
@@ -111,8 +105,8 @@ class VSRuntimeBlock(Block):
             return
 
         config_dict = {}
-        if os.path.exists(CMakeToolchain.filename):
-            existing_include = load(CMakeToolchain.filename)
+        if os.path.exists(CONAN_TOOLCHAIN_FILENAME):
+            existing_include = load(CONAN_TOOLCHAIN_FILENAME)
             msvc_runtime_value = re.search(r"set\(CMAKE_MSVC_RUNTIME_LIBRARY \"([^)]*)\"\)",
                                            existing_include)
             if msvc_runtime_value:
@@ -736,206 +730,3 @@ class GenericSystemBlock(Block):
                 "cmake_system_name": system_name,
                 "cmake_system_version": system_version,
                 "cmake_system_processor": system_processor}
-
-
-class ToolchainBlocks:
-    def __init__(self, conanfile, toolchain, items=None):
-        self._blocks = OrderedDict()
-        self._conanfile = conanfile
-        self._toolchain = toolchain
-        if items:
-            for name, block in items:
-                self._blocks[name] = block(conanfile, toolchain)
-
-    def remove(self, name):
-        del self._blocks[name]
-
-    def __setitem__(self, name, block_type):
-        # Create a new class inheriting Block with the elements of the provided one
-        block_type = type('proxyUserBlock', (Block,), dict(block_type.__dict__))
-        self._blocks[name] = block_type(self._conanfile, self._toolchain)
-
-    def __getitem__(self, name):
-        return self._blocks[name]
-
-    def process_blocks(self):
-        result = []
-        for b in self._blocks.values():
-            content = b.get_rendered_content()
-            if content:
-                result.append(content)
-        return result
-
-
-class CMakeToolchain(object):
-    filename = "conan_toolchain.cmake"
-
-    _template = textwrap.dedent("""
-        {% macro iterate_configs(var_config, action) %}
-            {% for it, values in var_config.items() %}
-                {% set genexpr = namespace(str='') %}
-                {% for conf, value in values -%}
-                    {% set genexpr.str = genexpr.str +
-                                          '$<IF:$<CONFIG:' + conf + '>,' + value|string + ',' %}
-                    {% if loop.last %}{% set genexpr.str = genexpr.str + '""' -%}{%- endif -%}
-                {% endfor %}
-                {% for i in range(values|count) %}{% set genexpr.str = genexpr.str + '>' %}
-                {% endfor %}
-                {% if action=='set' %}
-                set({{ it }} {{ genexpr.str }} CACHE STRING
-                    "Variable {{ it }} conan-toolchain defined")
-                {% elif action=='add_definitions' %}
-                add_definitions(-D{{ it }}={{ genexpr.str }})
-                {% endif %}
-            {% endfor %}
-        {% endmacro %}
-
-        # Conan automatically generated toolchain file
-        # DO NOT EDIT MANUALLY, it will be overwritten
-
-        # Avoid including toolchain file several times (bad if appending to variables like
-        #   CMAKE_CXX_FLAGS. See https://github.com/android/ndk/issues/323
-        include_guard()
-
-        message(STATUS "Using Conan toolchain: ${CMAKE_CURRENT_LIST_FILE}")
-
-        if(${CMAKE_VERSION} VERSION_LESS "3.15")
-            message(FATAL_ERROR "The 'CMakeToolchain' generator only works with CMake >= 3.15")
-        endif()
-
-        {% for conan_block in conan_blocks %}
-        {{ conan_block }}
-        {% endfor %}
-
-        # Variables
-        {% for it, value in variables.items() %}
-        set({{ it }} {{ value|cmake_value }} CACHE STRING "Variable {{ it }} conan-toolchain defined")
-        {% endfor %}
-        # Variables  per configuration
-        {{ iterate_configs(variables_config, action='set') }}
-
-        # Preprocessor definitions
-        {% for it, value in preprocessor_definitions.items() %}
-        # add_compile_definitions only works in cmake >= 3.12
-        add_definitions(-D{{ it }}={{ value }})
-        {% endfor %}
-        # Preprocessor definitions per configuration
-        {{ iterate_configs(preprocessor_definitions_config, action='add_definitions') }}
-        """)
-
-    def __init__(self, conanfile, generator=None, namespace=None):
-        self._conanfile = conanfile
-        self.generator = self._get_generator(generator)
-        self._namespace = namespace
-        self.variables = Variables()
-        self.preprocessor_definitions = Variables()
-
-        self.blocks = ToolchainBlocks(self._conanfile, self,
-                                      [("user_toolchain", UserToolchain),
-                                       ("generic_system", GenericSystemBlock),
-                                       ("android_system", AndroidSystemBlock),
-                                       ("apple_system", AppleSystemBlock),
-                                       ("fpic", FPicBlock),
-                                       ("arch_flags", ArchitectureBlock),
-                                       ("libcxx", GLibCXXBlock),
-                                       ("vs_runtime", VSRuntimeBlock),
-                                       ("cppstd", CppStdBlock),
-                                       ("parallel", ParallelBlock),
-                                       ("cmake_flags_init", CMakeFlagsInitBlock),
-                                       ("try_compile", TryCompileBlock),
-                                       ("find_paths", FindFiles),
-                                       ("rpath", SkipRPath),
-                                       ("shared", SharedLibBock)])
-
-        check_using_build_profile(self._conanfile)
-
-    def _context(self):
-        """ Returns dict, the context for the template
-        """
-        self.preprocessor_definitions.quote_preprocessor_strings()
-        blocks = self.blocks.process_blocks()
-        ctxt_toolchain = {
-            "variables": self.variables,
-            "variables_config": self.variables.configuration_types,
-            "preprocessor_definitions": self.preprocessor_definitions,
-            "preprocessor_definitions_config": self.preprocessor_definitions.configuration_types,
-            "conan_blocks": blocks
-        }
-
-        return ctxt_toolchain
-
-    @property
-    def content(self):
-        context = self._context()
-        content = Template(self._template, trim_blocks=True, lstrip_blocks=True).render(**context)
-        return content
-
-    def generate(self):
-        toolchain_file = self._conanfile.conf.get("tools.cmake.cmaketoolchain:toolchain_file")
-        if toolchain_file is None:  # The main toolchain file generated only if user dont define
-            save(self.filename, self.content)
-        # If we're using Intel oneAPI, we need to generate the environment file and run it
-        if self._conanfile.settings.get_safe("compiler") == "intel-cc":
-            IntelCC(self._conanfile).generate()
-        # Generators like Ninja or NMake requires an active vcvars
-        elif self.generator is not None and "Visual" not in self.generator:
-            VCVars(self._conanfile).generate()
-        self._writebuild(toolchain_file)
-
-    def _writebuild(self, toolchain_file):
-        result = {}
-        # TODO: Lets do it compatible with presets soon
-        if self.generator is not None:
-            result["cmake_generator"] = self.generator
-
-        result["cmake_toolchain_file"] = toolchain_file or self.filename
-
-        if result:
-            save_toolchain_args(result, namespace=self._namespace)
-
-    def _get_generator(self, recipe_generator):
-        # Returns the name of the generator to be used by CMake
-        conanfile = self._conanfile
-
-        # Downstream consumer always higher priority
-        generator_conf = conanfile.conf.get("tools.cmake.cmaketoolchain:generator")
-        if generator_conf:
-            return generator_conf
-
-        # second priority: the recipe one:
-        if recipe_generator:
-            return recipe_generator
-
-        # if not defined, deduce automatically the default one
-        compiler = conanfile.settings.get_safe("compiler")
-        compiler_version = conanfile.settings.get_safe("compiler.version")
-
-        cmake_years = {'8': '8 2005',
-                       '9': '9 2008',
-                       '10': '10 2010',
-                       '11': '11 2012',
-                       '12': '12 2013',
-                       '14': '14 2015',
-                       '15': '15 2017',
-                       '16': '16 2019',
-                       '17': '17 2022'}
-
-        if compiler == "msvc":
-            if compiler_version is None:
-                raise ConanException("compiler.version must be defined")
-            vs_version = vs_ide_version(self._conanfile)
-            return "Visual Studio %s" % cmake_years[vs_version]
-
-        compiler_base = conanfile.settings.get_safe("compiler.base")
-        compiler_base_version = conanfile.settings.get_safe("compiler.base.version")
-
-        if compiler == "Visual Studio" or compiler_base == "Visual Studio":
-            version = compiler_base_version or compiler_version
-            major_version = version.split('.', 1)[0]
-            base = "Visual Studio %s" % cmake_years[major_version]
-            return base
-
-        if use_win_mingw(conanfile):
-            return "MinGW Makefiles"
-
-        return "Unix Makefiles"
