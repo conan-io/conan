97becd442e83bc5a98c2bcb40b8e6174a26106cd
memsharded <james@conan.io>
2022-02-28 20:01:44 +0100

    what to do with local priority in cli?

--- conans/test/integration/configuration/test_profile_priority.py
+++ conans/test/integration/configuration/test_profile_priority.py
@@ -1,33 +1,34 @@
 import os
+import textwrap
 
 import pytest
 
 from conans.test.utils.tools import TestClient
-from conans.util.files import save
+from conans.util.files import save, mkdir, rmdir
 
 
 @pytest.mark.parametrize("local_path", ["", "./"])
 def test_profile_local_folder_priority(local_path):
     """ The local include(profile) must have priority over a file with same name in cache
     """
-    client = TestClient()
-    client.save({"profiles/default": f"include({local_path}otherprofile)",
-                 "profiles/otherprofile": "[settings]\nos=AIX",
-                 "conanfile.txt": ""})
-    save(os.path.join(client.cache.profiles_path, "otherprofile"), "[settings]\nos=FreeBSD")
+    c = TestClient()
+    c.save({"profiles/default": f"include({local_path}otherprofile)",
+            "profiles/otherprofile": "[settings]\nos=AIX",
+            "conanfile.txt": ""})
+    save(os.path.join(c.cache.profiles_path, "otherprofile"), "[settings]\nos=FreeBSD")
 
-    client.run("install . -pr=profiles/default")
-    assert "os=AIX" in client.out
+    c.run("install . -pr=profiles/default")
+    assert "os=AIX" in c.out
 
 
 def test_profile_cache_folder_priority():
     """ The cache include(profile) must have priority over a file with same name in local
     """
-    client = TestClient()
-    client.save({"otherprofile": "[settings]\nos=FreeBSD",
-                 "conanfile.txt": ""})
-    save(os.path.join(client.cache.profiles_path, "default"), "include(./otherprofile)")
-    save(os.path.join(client.cache.profiles_path, "otherprofile"), "[settings]\nos=AIX")
+    c = TestClient()
+    c.save({"otherprofile": "[settings]\nos=FreeBSD",
+            "conanfile.txt": ""})
+    save(os.path.join(c.cache.profiles_path, "default"), "include(./otherprofile)")
+    save(os.path.join(c.cache.profiles_path, "otherprofile"), "[settings]\nos=AIX")
 
-    client.run("install . -pr=default")
-    assert "os=AIX" in client.out
+    c.run("install . -pr=default")
+    assert "os=AIX" in c.out
