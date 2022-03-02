48b15daf5eb91b44c9b698a226e46ba5b4ba37c5
Luis Martinez <lasote@gmail.com>
2022-03-02 10:00:44 +0100

    basic_layout and external_sources for cmake_layout, removed meson_layout (#10659)
    * basic_layout and external_sources for cmake_layout, removed meson_layout
    
    * skipped py2 tests
    
    * fix test
    
    * src_folder argument
    
    * Adjust layout of conan new template according new cmake_layout
    
    * Fix test

--- conan/tools/meson/layout.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def meson_layout(conanfile):
-    conanfile.folders.build = "build-{}".format(str(conanfile.settings.build_type).lower())
-    conanfile.folders.generators = os.path.join(conanfile.folders.build, "conan")
-    conanfile.cpp.build.bindirs = ["."]
-    conanfile.cpp.build.libdirs = ["."]
