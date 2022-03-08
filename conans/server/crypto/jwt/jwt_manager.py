960173c4f4f7397e30a8ea5886b625040d7abeb7
Uilian Ries <uilianries@gmail.com>
2022-03-08 10:04:38 +0100

    Move JWT to server requirements
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/server/crypto/jwt/jwt_manager.py
+++ conans/server/crypto/jwt/jwt_manager.py
@@ -1,8 +1,6 @@
 from datetime import datetime
-from calendar import timegm
 
 import jwt
-import six
 
 
 class JWTManager(object):
@@ -18,24 +16,13 @@ class JWTManager(object):
 
     def get_token_for(self, profile_fields=None):
         """Generates a token with the provided fields.
-           if exp is True, expiration time will be used
-
-           This method supports both JWT 1.x and 2.x
-           Read https://github.com/conan-io/conan/pull/8952 for more details
-        """
+        if exp is True, expiration time will be used"""
         profile_fields = profile_fields or {}
 
         if self.expire_time:
             profile_fields["exp"] = datetime.utcnow() + self.expire_time
 
-        # profile_fields.exp no longer is converted to integer on JWT 2.x, we need to do it manually
-        if self.expire_time and isinstance(profile_fields.get("exp"), datetime):
-            profile_fields["exp"] = timegm(profile_fields.get("exp").utctimetuple())
-        encoded = jwt.encode(profile_fields, self.secret, algorithm="HS256")
-        # JWT 2,x returns decoded string, but 1.x returns encoded bytes
-        if isinstance(encoded, six.text_type):
-            encoded = encoded.encode('utf-8')
-        return encoded
+        return jwt.encode(profile_fields, self.secret, algorithm="HS256")
 
     def get_profile(self, token):
         """Gets the user from credentials object. None if no credentials.
