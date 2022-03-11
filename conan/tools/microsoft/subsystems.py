6e1b7139f80dd89cae482df7616ea249db6ea42d
Luis <lasote@gmail.com>
2022-03-11 11:29:24 +0100

    Removed argument

--- conan/tools/microsoft/subsystems.py
+++ conan/tools/microsoft/subsystems.py
@@ -1,8 +1,8 @@
 from conans.client.subsystems import deduce_subsystem, subsystem_path
 
 
-def unix_path(conanfile, path, subsystem=None):
+def unix_path(conanfile, path):
     if not conanfile.win_bash:
         return path
-    subsystem = subsystem or deduce_subsystem(conanfile, scope="build")
+    subsystem = deduce_subsystem(conanfile, scope="build")
     return subsystem_path(subsystem, path)
