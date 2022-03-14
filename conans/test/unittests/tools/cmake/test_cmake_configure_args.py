6d6866bd9058560b78d1ffbff27a91685b98d3d2
Luis <lasote@gmail.com>
2022-03-14 14:27:42 +0100

    Test configure

---
+++ conans/test/unittests/tools/cmake/test_cmake_configure_args.py
@@ -0,0 +1,46 @@
+from unittest.mock import Mock
+
+import pytest
+from mock import mock
+
+from conan.tools.cmake import CMake
+from conan.tools.files.files import save_toolchain_args
+from conans import ConanFile, Settings
+from conans.model.conf import Conf
+from conans.model.env_info import EnvValues
+from tools import temp_folder
+
+
+@pytest.fixture(scope="module")
+def conanfile():
+    c = ConanFile(Mock(), None)
+    c.settings = "os", "compiler", "build_type", "arch"
+    c.initialize(Settings({"os": ["Windows"],
+                           "compiler": {"gcc": {"libcxx": ["libstdc++"]}},
+                           "build_type": ["Release"],
+                           "arch": ["x86"]}), EnvValues())
+    c.settings.build_type = "Release"
+    c.settings.arch = "x86"
+    c.settings.compiler = "gcc"
+    c.settings.compiler.libcxx = "libstdc++"
+    c.settings.os = "Windows"
+    c.conf = Conf()
+    tmp_folder = temp_folder()
+    c.folders.set_base_generators(tmp_folder)
+    c.folders.generators = "."
+    c.folders.set_base_build(tmp_folder)
+    return c
+
+
+def test_cmake_make_program(conanfile):
+    save_toolchain_args({"cmake_generator": "MinGW Makefiles"},
+                        generators_folder=conanfile.folders.generators_folder)
+
+    def run(command):
+        assert '-DCMAKE_MAKE_PROGRAM="C:/mymake.exe"' in command
+
+    conanfile.run = run
+    conanfile.conf.define("tools.gnu:make_program", "C:\\mymake.exe")
+    with mock.patch("platform.system", mock.MagicMock(return_value='Windows')):
+        cmake = CMake(conanfile)
+        cmake.configure()
