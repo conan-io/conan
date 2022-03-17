c02bd0caf0ffc6d7d1effa31f99b31b832e5f80d
Francisco Ramirez de Anton <franchuti688@gmail.com>
2022-03-17 17:43:31 +0100

    Simplified and added new extra flags block

--- conan/tools/cmake/toolchain/toolchain.py
+++ conan/tools/cmake/toolchain/toolchain.py
@@ -8,9 +8,9 @@ from conan.tools._check_build_profile import check_using_build_profile
 from conan.tools._compilers import use_win_mingw
 from conan.tools.cmake.toolchain import CONAN_TOOLCHAIN_FILENAME
 from conan.tools.cmake.toolchain.blocks import ToolchainBlocks, UserToolchain, GenericSystemBlock, \
-    AndroidSystemBlock, AppleSystemBlock, FPicBlock, GLibCXXBlock, VSRuntimeBlock, \
-    CppStdBlock, CMakeFlagsInitBlock, TryCompileBlock, FindFiles, SkipRPath, \
-    SharedLibBock, OutputDirsBlock
+    AndroidSystemBlock, AppleSystemBlock, FPicBlock, ArchitectureBlock, GLibCXXBlock, VSRuntimeBlock, \
+    CppStdBlock, ParallelBlock, CMakeFlagsInitBlock, TryCompileBlock, FindFiles, SkipRPath, \
+    SharedLibBock, OutputDirsBlock, ExtraFlagsBlock
 from conan.tools.files.files import save_toolchain_args
 from conan.tools.intel import IntelCC
 from conan.tools.microsoft import VCVars
@@ -121,9 +121,12 @@ class CMakeToolchain(object):
                                        ("android_system", AndroidSystemBlock),
                                        ("apple_system", AppleSystemBlock),
                                        ("fpic", FPicBlock),
+                                       ("arch_flags", ArchitectureBlock),
                                        ("libcxx", GLibCXXBlock),
                                        ("vs_runtime", VSRuntimeBlock),
                                        ("cppstd", CppStdBlock),
+                                       ("parallel", ParallelBlock),
+                                       ("extra_flags", ExtraFlagsBlock),
                                        ("cmake_flags_init", CMakeFlagsInitBlock),
                                        ("try_compile", TryCompileBlock),
                                        ("find_paths", FindFiles),
