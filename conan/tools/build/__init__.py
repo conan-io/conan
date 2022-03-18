fc035dae17ac304c69fb6696443d3bd1eba99122
memsharded <james@conan.io>
2022-03-18 18:49:49 +0100

    [develop2] remove legacy conans.tools

--- conan/tools/build/__init__.py
+++ conan/tools/build/__init__.py
@@ -3,6 +3,7 @@ import sys
 
 from conan.tools.build.cpu import build_jobs
 from conan.tools.build.cross_building import cross_building
+from conan.tools.build.settings import check_min_cppstd, valid_min_cppstd
 
 
 def use_win_mingw(conanfile):
