f9d23661b995ea45720dcd3d6ec57cbe7767b3ef
Francisco Ramirez de Anton <franchuti688@gmail.com>
2022-03-30 11:59:09 +0200

    Added NDK env variable

--- conans/test/functional/toolchains/android/test_using_cmake.py
+++ conans/test/functional/toolchains/android/test_using_cmake.py
@@ -1,10 +1,12 @@
-import textwrap
+import os
 import platform
+import textwrap
 
 import pytest
+
 from conan.tools.cmake import CMakeToolchain
-from conans.test.utils.tools import TestClient
 from conans.test.functional.toolchains.android._utils import create_library
+from conans.test.utils.tools import TestClient
 
 
 @pytest.fixture
@@ -46,15 +48,14 @@ def client():
             compiler.libcxx=c++_shared
             build_type=Release
             [conf]
-            tools.android:ndk_path=/usr/local/share/android-ndk
-        """)
-        # FIXME: The conf ndk_path is hardcoded, could be received from the conftest?
+            tools.android:ndk_path={ndk_path}
+        """.format(ndk_path=os.getenv("TEST_CONAN_ANDROID_NDK")))
     })
     return t
 
 
 @pytest.mark.tool_cmake
-@pytest.mark.tool_ndk
+@pytest.mark.tool_android_ndk
 @pytest.mark.skipif(platform.system() != "Darwin", reason="Requires Xcode")
 def test_use_cmake_toolchain(client):
     """ This is the naive approach, we follow instruction from CMake in its documentation
