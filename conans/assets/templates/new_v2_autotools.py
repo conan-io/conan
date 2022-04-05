6e327a2538b8464a96662b2bdba19e20d0f0e84c
czoido <mrgalleta@gmail.com>
2022-04-05 17:39:55 +0200

    wip

--- conans/assets/templates/new_v2_autotools.py
+++ conans/assets/templates/new_v2_autotools.py
@@ -1,6 +1,6 @@
 import textwrap
 
-conanfile_sources_v2 = textwrap.dedent("""
+conanfile_lib = textwrap.dedent("""
     import os
 
     from conan import ConanFile
@@ -212,11 +212,11 @@ test_conanfile = textwrap.dedent("""
 
         def test(self):
             if not cross_building(self):
-                cmd = os.path.join(self.cpp.build.bindirs[0], "example")
+                cmd = os.path.join(self.cpp.build.bindirs[0], "main")
                 self.run(cmd, env="conanrun")
     """)
 
-test_example = textwrap.dedent("""
+main = textwrap.dedent("""
     #include "{name}.h"
 
     int main() {{
@@ -225,7 +225,7 @@ test_example = textwrap.dedent("""
     """)
 
 test_configure_ac = textwrap.dedent("""
-    AC_INIT([example], [1.0], [])
+    AC_INIT([main], [1.0], [])
     AM_INIT_AUTOMAKE([-Wall -Werror foreign])
     AC_PROG_CXX
     AC_PROG_RANLIB
@@ -235,14 +235,14 @@ test_configure_ac = textwrap.dedent("""
     """)
 
 test_makefile_am = textwrap.dedent("""
-    bin_PROGRAMS = example
-    example_SOURCES = example.cpp
+    bin_PROGRAMS = main
+    main_SOURCES = main.cpp
     """)
 
 
 def get_autotools_lib_files(name, version, package_name="Pkg"):
-    files = {"conanfile.py": conanfile_sources_v2.format(name=name, version=version,
-                                                         package_name=package_name),
+    files = {"conanfile.py": conanfile_lib.format(name=name, version=version,
+                                                  package_name=package_name),
              "src/Makefile.am": makefile_am_lib.format(name=name, version=version),
              "src/{}.cpp".format(name): source_cpp.format(name=name, version=version),
              "src/{}.h".format(name): source_h.format(name=name, version=version),
@@ -250,8 +250,111 @@ def get_autotools_lib_files(name, version, package_name="Pkg"):
              "Makefile.am": makefile_am.format(name=name, version=version),
              "test_package/conanfile.py": test_conanfile.format(name=name, version=version,
                                                                 package_name=package_name),
-             "test_package/example.cpp": test_example.format(name=name),
+             "test_package/main.cpp": main.format(name=name),
              "test_package/configure.ac": test_configure_ac.format(name=name, version=version),
-             "test_package/Makefile.am": test_makefile_am.format(name=name, version=version),
-             }
+             "test_package/Makefile.am": test_makefile_am.format(name=name, version=version)}
     return files
+
+
+conanfile_exe = textwrap.dedent("""
+    import os
+
+    from conan import ConanFile
+    from conan.tools.gnu import AutotoolsToolchain, Autotools
+    from conan.tools.gnu.layout import autotools_layout
+    from conan.tools.files import chdir
+
+
+    class {package_name}Conan(ConanFile):
+        name = "{name}"
+        version = "{version}"
+
+        # Optional metadata
+        license = "<Put the package license here>"
+        author = "<Put your name here> <And your email here>"
+        url = "<Package recipe repository url here, for issues about the package>"
+        description = "<Description of {package_name} here>"
+        topics = ("<Put some tag here>", "<here>", "<and here>")
+
+        # Binary configuration
+        settings = "os", "compiler", "build_type", "arch"
+
+        # Sources are located in the same place as this recipe, copy them to the recipe
+        exports_sources = "configure.ac", "Makefile.am", "src/*"
+
+        def layout(self):
+            autotools_layout(self)
+
+        def generate(self):
+            at_toolchain = AutotoolsToolchain(self)
+            at_toolchain.generate()
+
+        def build(self):
+            with chdir(self, self.source_folder):
+                self.run("aclocal")
+                self.run("autoconf")
+                self.run("automake --add-missing --foreign")
+            autotools = Autotools(self)
+            autotools.configure(self.source_folder)
+            autotools.make()
+
+        def package(self):
+            autotools = Autotools(self)
+            autotools.install()
+        """)
+
+test_conanfile_exe = textwrap.dedent("""
+    import os
+    from conan import ConanFile
+    from conan.tools.build import cross_building
+    from conan.tools.layout import basic_layout
+
+
+    class {package_name}TestConan(ConanFile):
+        settings = "os", "compiler", "build_type", "arch"
+        # VirtualRunEnv can be avoided if "tools.env.virtualenv:auto_use" is defined
+        # (it will be defined in Conan 2.0)
+        generators = "VirtualRunEnv"
+        apply_env = False
+        test_type = "explicit"
+
+        def requirements(self):
+            self.requires(self.tested_reference_str)
+
+        def layout(self):
+            basic_layout(self)
+
+        def test(self):
+            if not cross_building(self):
+                self.run("{name}", env="conanrun")
+    """)
+
+configure_ac_exe = textwrap.dedent("""
+    AC_INIT([{name}], [{version}], [])
+    AM_INIT_AUTOMAKE([-Wall -Werror foreign])
+    AC_PROG_CXX
+    AC_PROG_RANLIB
+    AM_PROG_AR
+    AC_CONFIG_FILES([Makefile src/Makefile])
+    AC_OUTPUT
+    """)
+
+makefile_am_exe = textwrap.dedent("""
+    bin_PROGRAMS = {name}
+    {name}_SOURCES = main.cpp {name}.cpp {name}.h
+    """)
+
+
+def get_autotools_exe_files(name, version, package_name="Pkg"):
+    files = {"conanfile.py": conanfile_exe.format(name=name, version=version,
+                                                  package_name=package_name),
+             "src/Makefile.am": makefile_am_exe.format(name=name, version=version),
+             "src/main.cpp": main.format(name=name),
+             "src/{}.cpp".format(name): source_cpp.format(name=name, version=version),
+             "src/{}.h".format(name): source_h.format(name=name, version=version),
+             "configure.ac": configure_ac_exe.format(name=name, version=version),
+             "Makefile.am": makefile_am.format(name=name, version=version),
+             "test_package/conanfile.py": test_conanfile_exe.format(name=name, version=version,
+                                                                    package_name=package_name)}
+    return files
+
