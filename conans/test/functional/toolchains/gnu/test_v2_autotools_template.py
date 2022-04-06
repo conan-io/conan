eee7493ea9dcebf323b8b143af87ebf72da0df07
czoido <mrgalleta@gmail.com>
2022-04-06 07:07:04 +0200

    skip tests

--- conans/test/functional/toolchains/gnu/test_v2_autotools_template.py
+++ conans/test/functional/toolchains/gnu/test_v2_autotools_template.py
@@ -1,10 +1,12 @@
-import os
-import re
+import platform
 
-from conans.model.ref import ConanFileReference, PackageReference
+import pytest
+
 from conans.test.utils.tools import TestClient
 
 
+@pytest.mark.skipif(platform.system() not in ["Linux", "Darwin"], reason="Requires Autotools")
+@pytest.mark.tool_autotools()
 def test_autotools_lib_template():
     client = TestClient(path_with_spaces=False)
     client.run("new hello/0.1 --template=autotools_lib")
@@ -23,6 +25,8 @@ def test_autotools_lib_template():
     # assert "hello/0.1: Hello World Release!" in client.out
 
 
+@pytest.mark.skipif(platform.system() not in ["Linux", "Darwin"], reason="Requires Autotools")
+@pytest.mark.tool_autotools()
 def test_autotools_exe_template():
     client = TestClient(path_with_spaces=False)
     client.run("new greet/0.1 --template=autotools_exe")
