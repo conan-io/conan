6b3c73a92bdd30960ff4c4fcf2d954e514ffccad
James <james@conan.io>
2022-04-11 09:23:17 +0200

    graph info should be able to print dependency graph so conflicts can be debugged (#10994)

--- conans/cli/commands/export_pkg.py
+++ conans/cli/commands/export_pkg.py
@@ -40,6 +40,7 @@ def export_pkg(conan_api, parser, *args, **kwargs):
                                             lockfile=lockfile,
                                             remotes=None,
                                             update=None)
+    deps_graph.report_graph_error()
     conan_api.graph.analyze_binaries(deps_graph, build_mode=[ref.name], lockfile=lockfile)
     deps_graph.report_graph_error()
 
