72618eba5dd2f3f10d714fd05ac37f4ecddcc5d1
czoido <mrgalleta@gmail.com>
2022-04-27 10:00:41 +0200

    update test

--- conans/test/functional/toolchains/gnu/test_v2_autotools_template.py
+++ conans/test/functional/toolchains/gnu/test_v2_autotools_template.py
@@ -71,7 +71,8 @@ def test_autotools_relocatable_libs_darwin():
     client.run("new autotools_lib -d name=hello -d version=0.1")
     client.run("create . -o hello/*:shared=True")
 
-    package_id = re.search(r"Packaging to (\S+)", str(client.out)).group(1)
+    package_id = re.search(r"Package (\S+) created", str(client.out)).group(1)
+    package_id = package_id.replace("'", "")
     ref = RecipeReference.loads("hello/0.1")
     pref = client.get_latest_package_reference(ref, package_id)
     package_folder = client.get_latest_pkg_layout(pref).package()
@@ -80,26 +81,26 @@ def test_autotools_relocatable_libs_darwin():
     if platform.system() == "Darwin":
         client.run_command("otool -l {}".format(dylib))
         assert "@rpath/libhello.0.dylib" in client.out
-        client.run_command("otool -l {}".format("test_package/build-release/main"))
+        client.run_command("otool -l {}".format("test_package/test_output/build-release/main"))
         assert package_folder in client.out
         assert "@executable_path" in client.out
 
     # will work because rpath set
-    client.run_command("test_package/build-release/main")
+    client.run_command("test_package/test_output/build-release/main")
     assert "hello/0.1: Hello World Release!" in client.out
 
     # move to another location so that the path set in the rpath does not exist
     # then the execution should fail
     shutil.move(os.path.join(package_folder, "lib"), os.path.join(client.current_folder, "tempfolder"))
     # will fail because rpath does not exist
-    client.run_command("test_package/build-release/main", assert_error=True)
+    client.run_command("test_package/test_output/build-release/main", assert_error=True)
     assert "Library not loaded: @rpath/libhello.0.dylib" in client.out
 
     # move the dylib to the folder where the executable is
     # should work because the @executable_path set in the rpath
     shutil.move(os.path.join(client.current_folder, "tempfolder", "libhello.0.dylib"),
-                os.path.join(client.current_folder, "test_package", "build-release"))
+                os.path.join(client.current_folder, "test_package", "test_output", "build-release"))
     shutil.move(os.path.join(client.current_folder, "tempfolder", "libhello.dylib"),
-                os.path.join(client.current_folder, "test_package", "build-release"))
-    client.run_command("test_package/build-release/main")
+                os.path.join(client.current_folder, "test_package", "test_output", "build-release"))
+    client.run_command("test_package/test_output/build-release/main")
     assert "hello/0.1: Hello World Release!" in client.out
