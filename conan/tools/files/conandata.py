ecbb2aef41e146ab2d5ceec0bf0e310ffeb25197
Luis <lasote@gmail.com>
2022-05-11 17:17:32 +0200

    Review

--- conan/tools/files/conandata.py
+++ conan/tools/files/conandata.py
@@ -8,6 +8,12 @@ from conans.util.files import load, save
 
 def update_conandata(conanfile, data):
     """
+    Tool to modify the ``conandata.yml`` once it is exported. It can be used, for example:
+
+       - To add additional data like the "commit" and "url" for the scm.
+       - To modify the contents cleaning the data that belong to other versions (different
+         from the exported) to avoid changing the recipe revision when the changed data doesn't
+         belong to the current version.
 
     :param conanfile: The current recipe object. Always use ``self``.
     :param data: (Required) A dictionary (can be nested), of values to update
