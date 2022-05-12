4d4f8ba5ce24680c47857ed54f46720d52cb6303
czoido <mrgalleta@gmail.com>
2022-05-12 12:04:33 +0200

    wip

--- conans/test/functional/toolchains/apple/test_xcodebuild.py
+++ conans/test/functional/toolchains/apple/test_xcodebuild.py
@@ -54,6 +54,7 @@ def client():
 
 @pytest.mark.skipif(platform.system() != "Darwin", reason="Only for MacOS")
 @pytest.mark.tool_xcodebuild
+@pytest.mark.tool_xcodegen
 def test_project_xcodebuild(client):
 
     conanfile = textwrap.dedent("""
@@ -97,6 +98,7 @@ def test_project_xcodebuild(client):
 
 @pytest.mark.skipif(platform.system() != "Darwin", reason="Only for MacOS")
 @pytest.mark.tool_xcodebuild
+@pytest.mark.tool_xcodegen
 @pytest.mark.skip(reason="Different sdks not installed in CI")
 def test_xcodebuild_test_different_sdk(client):
 
@@ -132,6 +134,7 @@ def test_xcodebuild_test_different_sdk(client):
 
 @pytest.mark.skipif(platform.system() != "Darwin", reason="Only for MacOS")
 @pytest.mark.tool_xcodebuild
+@pytest.mark.tool_xcodegen
 def test_missing_sdk(client):
 
     conanfile = textwrap.dedent("""
