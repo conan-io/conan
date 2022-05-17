626f787c34393b42412e8d0781397d86dad20af2
Francisco Ramirez de Anton <franchuti688@gmail.com>
2022-05-17 18:15:10 +0200

    Added more docstrings

--- conan/tools/microsoft/msbuild.py
+++ conan/tools/microsoft/msbuild.py
@@ -17,8 +17,16 @@ def msbuild_arch(arch):
 
 
 class MSBuild(object):
+    """
+    MSBuild build helper class
+    """
+
     def __init__(self, conanfile):
+        """
+        :param conanfile: ``< ConanFile object >`` The current recipe object. Always use ``self``.
+        """
         self._conanfile = conanfile
+        #: Defines the build type. By default, ``settings.build_type``.
         self.build_type = conanfile.settings.get_safe("build_type")
         # if platforms:
         #    msvc_arch.update(platforms)
@@ -26,9 +34,17 @@ class MSBuild(object):
         msvc_arch = msbuild_arch(arch)
         if conanfile.settings.get_safe("os") == "WindowsCE":
             msvc_arch = conanfile.settings.get_safe("os.platform")
+        #: Defines the platform name, e.g., ``ARM`` if ``settings.arch == "armv7"``.
         self.platform = msvc_arch
 
     def command(self, sln):
+        """
+        Gets the ``msbuild`` command line. For instance,
+        :command:`msbuild "MyProject.sln" /p:Configuration=<conf> /p:Platform=<platform>`.
+
+        :param sln: ``str`` name of Visual Studio ``*.sln`` file
+        :return: ``str`` msbuild command line.
+        """
         # TODO: Enable output_binary_log via config
         cmd = ('msbuild "%s" /p:Configuration=%s /p:Platform=%s'
                % (sln, self.build_type, self.platform))
@@ -45,6 +61,11 @@ class MSBuild(object):
         return cmd
 
     def build(self, sln):
+        """
+        Runs the ``msbuild`` command line obtained from ``self.command(sln)``.
+
+        :param sln: ``str`` name of Visual Studio ``*.sln`` file
+        """
         cmd = self.command(sln)
         self._conanfile.run(cmd)
 
