b96c4ea14151af97a99cdc8ecc4b724e42a8eacf
czoido <mrgalleta@gmail.com>
2022-05-17 18:22:01 +0200

    add test

--- conans/test/functional/toolchains/gnu/autotools/test_basic.py
+++ conans/test/functional/toolchains/gnu/autotools/test_basic.py
@@ -245,3 +245,56 @@ def test_autotools_with_pkgconfigdeps():
     assert re.search("I.*hello.*1.0.*include", str(client.out))
     assert "-lhello" in client.out
     assert re.search("L.*hello.*1.0.*package", str(client.out))
+
+
+def test_autotools_option_checking():
+    client = TestClient(path_with_spaces=False)
+    client.run("new mylib/1.0@ -m autotools_lib")
+    conanfile = textwrap.dedent("""
+        import os
+
+        from conan import ConanFile
+        from conan.tools.gnu import AutotoolsToolchain, Autotools
+        from conan.tools.layout import basic_layout
+        from conan.tools.build import cross_building
+        from conan.tools.files import chdir
+
+
+        class MylibTestConan(ConanFile):
+            settings = "os", "compiler", "build_type", "arch"
+            # VirtualBuildEnv and VirtualRunEnv can be avoided if "tools.env.virtualenv:auto_use" is defined
+            # (it will be defined in Conan 2.0)
+            generators = "AutotoolsDeps", "VirtualBuildEnv", "VirtualRunEnv"
+            apply_env = False
+            test_type = "explicit"
+
+            def requirements(self):
+                self.requires(self.tested_reference_str)
+
+            def generate(self):
+                at_toolchain = AutotoolsToolchain(self)
+                at_toolchain.configure_args = ['--enable-option-checking=fatal']
+                at_toolchain.generate()
+
+            def build(self):
+                autotools = Autotools(self)
+                autotools.autoreconf()
+                autotools.configure()
+                autotools.make()
+
+            def layout(self):
+                basic_layout(self)
+
+            def test(self):
+                if not cross_building(self):
+                    cmd = os.path.join(self.cpp.build.bindirs[0], "main")
+                    self.run(cmd, env="conanrun")
+            """)
+
+    client.save({"test_package/conanfile.py": conanfile})
+    client.run("create . -tf=None")
+
+    # will fail because using the unrecognized options: --disable-shared, --enable-static, --with-pic
+    client.run("test test_package mylib/1.0@")
+    assert "configure: error: unrecognized options: --disable-shared, --enable-static, --with-pic" \
+           not in client.out
