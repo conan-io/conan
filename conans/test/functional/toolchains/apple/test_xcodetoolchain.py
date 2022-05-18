b063021b16463a39bb68c15440e4a01bf2c053b9
Carlos Zoido <mrgalleta@gmail.com>
2022-05-18 19:32:54 +0200

    Add components support in XcodeDeps (#11233)
    * add components to xcodedeps
    
    * update tests
    
    * wip
    
    * wip
    
    * wip
    
    * update test
    
    * update test
    
    * wip
    
    * fix format
    
    * update test
    
    * update test
    
    * wip
    
    * update test
    
    * update
    
    * update test
    
    * wip
    
    * update test
    
    * Update conans/test/integration/toolchains/apple/test_xcodedeps.py
    
    Co-authored-by: Francisco Ramírez <franchuti688@gmail.com>
    
    * document test a bit
    
    Co-authored-by: James <james@conan.io>
    Co-authored-by: Francisco Ramírez <franchuti688@gmail.com>

--- conans/test/functional/toolchains/apple/test_xcodetoolchain.py
+++ conans/test/functional/toolchains/apple/test_xcodetoolchain.py
@@ -6,319 +6,6 @@ import pytest
 from conans.test.assets.sources import gen_function_cpp
 from conans.test.utils.tools import TestClient
 
-pbxproj = textwrap.dedent("""
-    // !$*UTF8*$!
-    {
-        archiveVersion = 1;
-        classes = {
-        };
-        objectVersion = 55;
-        objects = {
-
-    /* Begin PBXBuildFile section */
-            07879B4027219EE500B6FB51 /* main.cpp in Sources */ = {isa = PBXBuildFile; fileRef = 07879B3F27219EE500B6FB51 /* main.cpp */; };
-    /* End PBXBuildFile section */
-
-    /* Begin PBXCopyFilesBuildPhase section */
-            07879B3A27219EE500B6FB51 /* CopyFiles */ = {
-                isa = PBXCopyFilesBuildPhase;
-                buildActionMask = 2147483647;
-                dstPath = /usr/share/man/man1/;
-                dstSubfolderSpec = 0;
-                files = (
-                );
-                runOnlyForDeploymentPostprocessing = 1;
-            };
-    /* End PBXCopyFilesBuildPhase section */
-
-    /* Begin PBXFileReference section */
-            07879B3C27219EE500B6FB51 /* app */ = {isa = PBXFileReference; explicitFileType = "compiled.mach-o.executable"; includeInIndex = 0; path = app; sourceTree = BUILT_PRODUCTS_DIR; };
-            07879B3F27219EE500B6FB51 /* main.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; path = main.cpp; sourceTree = "<group>"; };
-            4130DB5F27BE8D0300BDEE84 /* conan_hello_release_x86_64_macosx_12_1.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = conan_hello_release_x86_64_macosx_12_1.xcconfig; sourceTree = "<group>"; };
-            4130DB6027BE8D0300BDEE84 /* conan_hello_vars_debug_x86_64_macosx_12_1.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = conan_hello_vars_debug_x86_64_macosx_12_1.xcconfig; sourceTree = "<group>"; };
-            4130DB6127BE8D0300BDEE84 /* conantoolchain_debug_x86_64_macosx_12_1.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = conantoolchain_debug_x86_64_macosx_12_1.xcconfig; sourceTree = "<group>"; };
-            4130DB6227BE8D0300BDEE84 /* conantoolchain_release_x86_64_macosx_12_1.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = conantoolchain_release_x86_64_macosx_12_1.xcconfig; sourceTree = "<group>"; };
-            4130DB6327BE8D0300BDEE84 /* conan_hello.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = conan_hello.xcconfig; sourceTree = "<group>"; };
-            4130DB6427BE8D0300BDEE84 /* conan_hello_vars_release_x86_64_macosx_12_1.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = conan_hello_vars_release_x86_64_macosx_12_1.xcconfig; sourceTree = "<group>"; };
-            4130DB6527BE8D0300BDEE84 /* conan_hello_debug_x86_64_macosx_12_1.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = conan_hello_debug_x86_64_macosx_12_1.xcconfig; sourceTree = "<group>"; };
-            4130DB6627BE8D0300BDEE84 /* conan_config.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = conan_config.xcconfig; sourceTree = "<group>"; };
-            4130DB6727BE8D0300BDEE84 /* conantoolchain.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = conantoolchain.xcconfig; sourceTree = "<group>"; };
-            4130DB6827BE8D0300BDEE84 /* conandeps.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = conandeps.xcconfig; sourceTree = "<group>"; };
-            416ED66527F1FFAE00664526 /* conan_global_flags.xcconfig */ = {isa = PBXFileReference; lastKnownFileType = text.xcconfig; path = conan_global_flags.xcconfig; sourceTree = "<group>"; };
-    /* End PBXFileReference section */
-
-    /* Begin PBXFrameworksBuildPhase section */
-            07879B3927219EE500B6FB51 /* Frameworks */ = {
-                isa = PBXFrameworksBuildPhase;
-                buildActionMask = 2147483647;
-                files = (
-                );
-                runOnlyForDeploymentPostprocessing = 0;
-            };
-    /* End PBXFrameworksBuildPhase section */
-
-    /* Begin PBXGroup section */
-            07879B3327219EE500B6FB51 = {
-                isa = PBXGroup;
-                children = (
-                    4130DB6927BE8D0D00BDEE84 /* conan */,
-                    07879B3E27219EE500B6FB51 /* app */,
-                    07879B3D27219EE500B6FB51 /* Products */,
-                );
-                sourceTree = "<group>";
-            };
-            07879B3D27219EE500B6FB51 /* Products */ = {
-                isa = PBXGroup;
-                children = (
-                    07879B3C27219EE500B6FB51 /* app */,
-                );
-                name = Products;
-                sourceTree = "<group>";
-            };
-            07879B3E27219EE500B6FB51 /* app */ = {
-                isa = PBXGroup;
-                children = (
-                    07879B3F27219EE500B6FB51 /* main.cpp */,
-                );
-                path = app;
-                sourceTree = "<group>";
-            };
-            4130DB6927BE8D0D00BDEE84 /* conan */ = {
-                isa = PBXGroup;
-                children = (
-                    416ED66527F1FFAE00664526 /* conan_global_flags.xcconfig */,
-                    4130DB6627BE8D0300BDEE84 /* conan_config.xcconfig */,
-                    4130DB6527BE8D0300BDEE84 /* conan_hello_debug_x86_64_macosx_12_1.xcconfig */,
-                    4130DB5F27BE8D0300BDEE84 /* conan_hello_release_x86_64_macosx_12_1.xcconfig */,
-                    4130DB6027BE8D0300BDEE84 /* conan_hello_vars_debug_x86_64_macosx_12_1.xcconfig */,
-                    4130DB6427BE8D0300BDEE84 /* conan_hello_vars_release_x86_64_macosx_12_1.xcconfig */,
-                    4130DB6327BE8D0300BDEE84 /* conan_hello.xcconfig */,
-                    4130DB6827BE8D0300BDEE84 /* conandeps.xcconfig */,
-                    4130DB6127BE8D0300BDEE84 /* conantoolchain_debug_x86_64_macosx_12_1.xcconfig */,
-                    4130DB6227BE8D0300BDEE84 /* conantoolchain_release_x86_64_macosx_12_1.xcconfig */,
-                    4130DB6727BE8D0300BDEE84 /* conantoolchain.xcconfig */,
-                );
-                name = conan;
-                sourceTree = "<group>";
-            };
-    /* End PBXGroup section */
-
-    /* Begin PBXNativeTarget section */
-            07879B3B27219EE500B6FB51 /* app */ = {
-                isa = PBXNativeTarget;
-                buildConfigurationList = 07879B4327219EE500B6FB51 /* Build configuration list for PBXNativeTarget "app" */;
-                buildPhases = (
-                    07879B3827219EE500B6FB51 /* Sources */,
-                    07879B3927219EE500B6FB51 /* Frameworks */,
-                    07879B3A27219EE500B6FB51 /* CopyFiles */,
-                );
-                buildRules = (
-                );
-                dependencies = (
-                );
-                name = app;
-                productName = app;
-                productReference = 07879B3C27219EE500B6FB51 /* app */;
-                productType = "com.apple.product-type.tool";
-            };
-    /* End PBXNativeTarget section */
-
-    /* Begin PBXProject section */
-            07879B3427219EE500B6FB51 /* Project object */ = {
-                isa = PBXProject;
-                attributes = {
-                    BuildIndependentTargetsInParallel = 1;
-                    LastUpgradeCheck = 1320;
-                    TargetAttributes = {
-                        07879B3B27219EE500B6FB51 = {
-                            CreatedOnToolsVersion = 13.0;
-                        };
-                    };
-                };
-                buildConfigurationList = 07879B3727219EE500B6FB51 /* Build configuration list for PBXProject "app" */;
-                compatibilityVersion = "Xcode 13.0";
-                developmentRegion = en;
-                hasScannedForEncodings = 0;
-                knownRegions = (
-                    en,
-                    Base,
-                );
-                mainGroup = 07879B3327219EE500B6FB51;
-                productRefGroup = 07879B3D27219EE500B6FB51 /* Products */;
-                projectDirPath = "";
-                projectRoot = "";
-                targets = (
-                    07879B3B27219EE500B6FB51 /* app */,
-                );
-            };
-    /* End PBXProject section */
-
-    /* Begin PBXSourcesBuildPhase section */
-            07879B3827219EE500B6FB51 /* Sources */ = {
-                isa = PBXSourcesBuildPhase;
-                buildActionMask = 2147483647;
-                files = (
-                    07879B4027219EE500B6FB51 /* main.cpp in Sources */,
-                );
-                runOnlyForDeploymentPostprocessing = 0;
-            };
-    /* End PBXSourcesBuildPhase section */
-
-    /* Begin XCBuildConfiguration section */
-            07879B4127219EE500B6FB51 /* Debug */ = {
-                isa = XCBuildConfiguration;
-                baseConfigurationReference = 4130DB6627BE8D0300BDEE84 /* conan_config.xcconfig */;
-                buildSettings = {
-                    ALWAYS_SEARCH_USER_PATHS = NO;
-                    ARCHS = x86_64;
-                    CLANG_ANALYZER_NONNULL = YES;
-                    CLANG_ANALYZER_NUMBER_OBJECT_CONVERSION = YES_AGGRESSIVE;
-                    CLANG_ENABLE_MODULES = YES;
-                    CLANG_ENABLE_OBJC_ARC = YES;
-                    CLANG_ENABLE_OBJC_WEAK = YES;
-                    CLANG_WARN_BLOCK_CAPTURE_AUTORELEASING = YES;
-                    CLANG_WARN_BOOL_CONVERSION = YES;
-                    CLANG_WARN_COMMA = YES;
-                    CLANG_WARN_CONSTANT_CONVERSION = YES;
-                    CLANG_WARN_DEPRECATED_OBJC_IMPLEMENTATIONS = YES;
-                    CLANG_WARN_DIRECT_OBJC_ISA_USAGE = YES_ERROR;
-                    CLANG_WARN_DOCUMENTATION_COMMENTS = YES;
-                    CLANG_WARN_EMPTY_BODY = YES;
-                    CLANG_WARN_ENUM_CONVERSION = YES;
-                    CLANG_WARN_INFINITE_RECURSION = YES;
-                    CLANG_WARN_INT_CONVERSION = YES;
-                    CLANG_WARN_NON_LITERAL_NULL_CONVERSION = YES;
-                    CLANG_WARN_OBJC_IMPLICIT_RETAIN_SELF = YES;
-                    CLANG_WARN_OBJC_LITERAL_CONVERSION = YES;
-                    CLANG_WARN_OBJC_ROOT_CLASS = YES_ERROR;
-                    CLANG_WARN_QUOTED_INCLUDE_IN_FRAMEWORK_HEADER = YES;
-                    CLANG_WARN_RANGE_LOOP_ANALYSIS = YES;
-                    CLANG_WARN_STRICT_PROTOTYPES = YES;
-                    CLANG_WARN_SUSPICIOUS_MOVE = YES;
-                    CLANG_WARN_UNGUARDED_AVAILABILITY = YES_AGGRESSIVE;
-                    CLANG_WARN_UNREACHABLE_CODE = YES;
-                    CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
-                    COPY_PHASE_STRIP = NO;
-                    DEBUG_INFORMATION_FORMAT = dwarf;
-                    ENABLE_STRICT_OBJC_MSGSEND = YES;
-                    ENABLE_TESTABILITY = YES;
-                    GCC_C_LANGUAGE_STANDARD = gnu11;
-                    GCC_DYNAMIC_NO_PIC = NO;
-                    GCC_NO_COMMON_BLOCKS = YES;
-                    GCC_OPTIMIZATION_LEVEL = 0;
-                    GCC_PREPROCESSOR_DEFINITIONS = (
-                        "DEBUG=1",
-                        "$(inherited)",
-                    );
-                    GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
-                    GCC_WARN_ABOUT_RETURN_TYPE = YES_ERROR;
-                    GCC_WARN_UNDECLARED_SELECTOR = YES;
-                    GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
-                    GCC_WARN_UNUSED_FUNCTION = YES;
-                    GCC_WARN_UNUSED_VARIABLE = YES;
-                    MTL_ENABLE_DEBUG_INFO = INCLUDE_SOURCE;
-                    MTL_FAST_MATH = YES;
-                    ONLY_ACTIVE_ARCH = YES;
-                    SDKROOT = macosx12.1;
-                };
-                name = Debug;
-            };
-            07879B4227219EE500B6FB51 /* Release */ = {
-                isa = XCBuildConfiguration;
-                baseConfigurationReference = 4130DB6627BE8D0300BDEE84 /* conan_config.xcconfig */;
-                buildSettings = {
-                    ALWAYS_SEARCH_USER_PATHS = NO;
-                    ARCHS = x86_64;
-                    CLANG_ANALYZER_NONNULL = YES;
-                    CLANG_ANALYZER_NUMBER_OBJECT_CONVERSION = YES_AGGRESSIVE;
-                    CLANG_ENABLE_MODULES = YES;
-                    CLANG_ENABLE_OBJC_ARC = YES;
-                    CLANG_ENABLE_OBJC_WEAK = YES;
-                    CLANG_WARN_BLOCK_CAPTURE_AUTORELEASING = YES;
-                    CLANG_WARN_BOOL_CONVERSION = YES;
-                    CLANG_WARN_COMMA = YES;
-                    CLANG_WARN_CONSTANT_CONVERSION = YES;
-                    CLANG_WARN_DEPRECATED_OBJC_IMPLEMENTATIONS = YES;
-                    CLANG_WARN_DIRECT_OBJC_ISA_USAGE = YES_ERROR;
-                    CLANG_WARN_DOCUMENTATION_COMMENTS = YES;
-                    CLANG_WARN_EMPTY_BODY = YES;
-                    CLANG_WARN_ENUM_CONVERSION = YES;
-                    CLANG_WARN_INFINITE_RECURSION = YES;
-                    CLANG_WARN_INT_CONVERSION = YES;
-                    CLANG_WARN_NON_LITERAL_NULL_CONVERSION = YES;
-                    CLANG_WARN_OBJC_IMPLICIT_RETAIN_SELF = YES;
-                    CLANG_WARN_OBJC_LITERAL_CONVERSION = YES;
-                    CLANG_WARN_OBJC_ROOT_CLASS = YES_ERROR;
-                    CLANG_WARN_QUOTED_INCLUDE_IN_FRAMEWORK_HEADER = YES;
-                    CLANG_WARN_RANGE_LOOP_ANALYSIS = YES;
-                    CLANG_WARN_STRICT_PROTOTYPES = YES;
-                    CLANG_WARN_SUSPICIOUS_MOVE = YES;
-                    CLANG_WARN_UNGUARDED_AVAILABILITY = YES_AGGRESSIVE;
-                    CLANG_WARN_UNREACHABLE_CODE = YES;
-                    CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
-                    COPY_PHASE_STRIP = NO;
-                    DEBUG_INFORMATION_FORMAT = dwarf;
-                    ENABLE_NS_ASSERTIONS = NO;
-                    ENABLE_STRICT_OBJC_MSGSEND = YES;
-                    GCC_C_LANGUAGE_STANDARD = gnu11;
-                    GCC_NO_COMMON_BLOCKS = YES;
-                    GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
-                    GCC_WARN_ABOUT_RETURN_TYPE = YES_ERROR;
-                    GCC_WARN_UNDECLARED_SELECTOR = YES;
-                    GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
-                    GCC_WARN_UNUSED_FUNCTION = YES;
-                    GCC_WARN_UNUSED_VARIABLE = YES;
-                    MTL_ENABLE_DEBUG_INFO = NO;
-                    MTL_FAST_MATH = YES;
-                    ONLY_ACTIVE_ARCH = YES;
-                    SDKROOT = macosx12.1;
-                };
-                name = Release;
-            };
-            07879B4427219EE500B6FB51 /* Debug */ = {
-                isa = XCBuildConfiguration;
-                buildSettings = {
-                    CODE_SIGN_IDENTITY = "-";
-                    CODE_SIGN_STYLE = Automatic;
-                    PRODUCT_NAME = "$(TARGET_NAME)";
-                };
-                name = Debug;
-            };
-            07879B4527219EE500B6FB51 /* Release */ = {
-                isa = XCBuildConfiguration;
-                buildSettings = {
-                    CODE_SIGN_IDENTITY = "-";
-                    CODE_SIGN_STYLE = Automatic;
-                    PRODUCT_NAME = "$(TARGET_NAME)";
-                };
-                name = Release;
-            };
-    /* End XCBuildConfiguration section */
-
-    /* Begin XCConfigurationList section */
-            07879B3727219EE500B6FB51 /* Build configuration list for PBXProject "app" */ = {
-                isa = XCConfigurationList;
-                buildConfigurations = (
-                    07879B4127219EE500B6FB51 /* Debug */,
-                    07879B4227219EE500B6FB51 /* Release */,
-                );
-                defaultConfigurationIsVisible = 0;
-                defaultConfigurationName = Release;
-            };
-            07879B4327219EE500B6FB51 /* Build configuration list for PBXNativeTarget "app" */ = {
-                isa = XCConfigurationList;
-                buildConfigurations = (
-                    07879B4427219EE500B6FB51 /* Debug */,
-                    07879B4527219EE500B6FB51 /* Release */,
-                );
-                defaultConfigurationIsVisible = 0;
-                defaultConfigurationName = Release;
-            };
-    /* End XCConfigurationList section */
-        };
-        rootObject = 07879B3427219EE500B6FB51 /* Project object */;
-    }
-    """)
 
 test = textwrap.dedent("""
     import os
@@ -346,7 +33,9 @@ def test_project_xcodetoolchain(cppstd, cppstd_output, min_version):
     client.run("export .")
 
     conanfile = textwrap.dedent("""
-        from conans import ConanFile
+        import os
+        from conan import ConanFile
+        from conan.tools.files import copy
         from conan.tools.apple import XcodeBuild
         class MyApplicationConan(ConanFile):
             name = "myapplication"
@@ -361,16 +50,33 @@ def test_project_xcodetoolchain(cppstd, cppstd_output, min_version):
                 self.run("otool -l build/{}/app".format(self.settings.build_type))
 
             def package(self):
-                self.copy("*/app", dst="bin", src=".", keep_path=False)
+                copy(self, "build/{}/app".format(self.settings.build_type), self.build_folder,
+                     os.path.join(self.package_folder, "bin"), keep_path=False)
 
             def package_info(self):
                 self.cpp_info.bindirs = ["bin"]
         """)
 
+    xcode_project = textwrap.dedent("""
+        name: app
+        targets:
+          app:
+            type: tool
+            platform: macOS
+            sources:
+              - app
+            configFiles:
+              Debug: conan_config.xcconfig
+              Release: conan_config.xcconfig
+        """)
+
     client.save({"conanfile.py": conanfile,
                  "test_package/conanfile.py": test,
                  "app/main.cpp": gen_function_cpp(name="main", includes=["hello"], calls=["hello"]),
-                 "app.xcodeproj/project.pbxproj": pbxproj}, clean_first=True)
+                 "project.yml": xcode_project,
+                 "conan_config.xcconfig": ""}, clean_first=True)
+
+    client.run_command("xcodegen generate")
 
     sdk_version = "11.3"
     settings = "-s arch=x86_64 -s os.sdk=macosx -s os.sdk_version={} -s compiler.cppstd={} " \
--- conans/test/integration/toolchains/apple/test_xcodedeps.py
+++ conans/test/integration/toolchains/apple/test_xcodedeps.py
@@ -8,39 +8,39 @@ from conans.test.integration.toolchains.apple.test_xcodetoolchain import _get_fi
 from conans.test.utils.tools import TestClient
 
 _expected_dep_xconfig = [
-    "HEADER_SEARCH_PATHS = $(inherited) $(HEADER_SEARCH_PATHS_{name})",
-    "GCC_PREPROCESSOR_DEFINITIONS = $(inherited) $(GCC_PREPROCESSOR_DEFINITIONS_{name})",
-    "OTHER_CFLAGS = $(inherited) $(OTHER_CFLAGS_{name})",
-    "OTHER_CPLUSPLUSFLAGS = $(inherited) $(OTHER_CPLUSPLUSFLAGS_{name})",
-    "FRAMEWORK_SEARCH_PATHS = $(inherited) $(FRAMEWORK_SEARCH_PATHS_{name})",
-    "LIBRARY_SEARCH_PATHS = $(inherited) $(LIBRARY_SEARCH_PATHS_{name})",
-    "OTHER_LDFLAGS = $(inherited) $(OTHER_LDFLAGS_{name})",
+    "HEADER_SEARCH_PATHS = $(inherited) $(HEADER_SEARCH_PATHS_{name}_{name})",
+    "GCC_PREPROCESSOR_DEFINITIONS = $(inherited) $(GCC_PREPROCESSOR_DEFINITIONS_{name}_{name})",
+    "OTHER_CFLAGS = $(inherited) $(OTHER_CFLAGS_{name}_{name})",
+    "OTHER_CPLUSPLUSFLAGS = $(inherited) $(OTHER_CPLUSPLUSFLAGS_{name}_{name})",
+    "FRAMEWORK_SEARCH_PATHS = $(inherited) $(FRAMEWORK_SEARCH_PATHS_{name}_{name})",
+    "LIBRARY_SEARCH_PATHS = $(inherited) $(LIBRARY_SEARCH_PATHS_{name}_{name})",
+    "OTHER_LDFLAGS = $(inherited) $(OTHER_LDFLAGS_{name}_{name})",
 ]
 
 _expected_vars_xconfig = [
-    "CONAN_{name}_BINARY_DIRECTORIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
-    "CONAN_{name}_C_COMPILER_FLAGS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
-    "CONAN_{name}_CXX_COMPILER_FLAGS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
-    "CONAN_{name}_LINKER_FLAGS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
-    "CONAN_{name}_PREPROCESSOR_DEFINITIONS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
-    "CONAN_{name}_INCLUDE_DIRECTORIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
-    "CONAN_{name}_RESOURCE_DIRECTORIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
-    "CONAN_{name}_LIBRARY_DIRECTORIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
-    "CONAN_{name}_LIBRARIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] = -l{name}",
-    "CONAN_{name}_SYSTEM_LIBS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
-    "CONAN_{name}_FRAMEWORKS_DIRECTORIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
-    "CONAN_{name}_FRAMEWORKS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] = -framework framework_{name}"
+    "CONAN_{name}_{name}_BINARY_DIRECTORIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
+    "CONAN_{name}_{name}_C_COMPILER_FLAGS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
+    "CONAN_{name}_{name}_CXX_COMPILER_FLAGS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
+    "CONAN_{name}_{name}_LINKER_FLAGS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
+    "CONAN_{name}_{name}_PREPROCESSOR_DEFINITIONS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
+    "CONAN_{name}_{name}_INCLUDE_DIRECTORIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
+    "CONAN_{name}_{name}_RESOURCE_DIRECTORIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
+    "CONAN_{name}_{name}_LIBRARY_DIRECTORIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
+    "CONAN_{name}_{name}_LIBRARIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] = -l{name}",
+    "CONAN_{name}_{name}_SYSTEM_LIBS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
+    "CONAN_{name}_{name}_FRAMEWORKS_DIRECTORIES[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] =",
+    "CONAN_{name}_{name}_FRAMEWORKS[config={configuration}][arch={architecture}][sdk={sdk}{sdk_version}] = -framework framework_{name}"
 ]
 
 _expected_conf_xconfig = [
     "#include \"{vars_name}\"",
-    "HEADER_SEARCH_PATHS_{name} = $(CONAN_{name}_INCLUDE_DIRECTORIES",
-    "GCC_PREPROCESSOR_DEFINITIONS_{name} = $(CONAN_{name}_PREPROCESSOR_DEFINITIONS",
-    "OTHER_CFLAGS_{name} = $(CONAN_{name}_C_COMPILER_FLAGS",
-    "OTHER_CPLUSPLUSFLAGS_{name} = $(CONAN_{name}_CXX_COMPILER_FLAGS",
-    "FRAMEWORK_SEARCH_PATHS_{name} = $(CONAN_{name}_FRAMEWORKS_DIRECTORIES",
-    "LIBRARY_SEARCH_PATHS_{name} = $(CONAN_{name}_LIBRARY_DIRECTORIES",
-    "OTHER_LDFLAGS_{name} = $(CONAN_{name}_LINKER_FLAGS) $(CONAN_{name}_LIBRARIES) $(CONAN_{name}_SYSTEM_LIBS) $(CONAN_{name}_FRAMEWORKS"
+    "HEADER_SEARCH_PATHS_{name}_{name} = $(CONAN_{name}_{name}_INCLUDE_DIRECTORIES",
+    "GCC_PREPROCESSOR_DEFINITIONS_{name}_{name} = $(CONAN_{name}_{name}_PREPROCESSOR_DEFINITIONS",
+    "OTHER_CFLAGS_{name}_{name} = $(CONAN_{name}_{name}_C_COMPILER_FLAGS",
+    "OTHER_CPLUSPLUSFLAGS_{name}_{name} = $(CONAN_{name}_{name}_CXX_COMPILER_FLAGS",
+    "FRAMEWORK_SEARCH_PATHS_{name}_{name} = $(CONAN_{name}_{name}_FRAMEWORKS_DIRECTORIES",
+    "LIBRARY_SEARCH_PATHS_{name}_{name} = $(CONAN_{name}_{name}_LIBRARY_DIRECTORIES",
+    "OTHER_LDFLAGS_{name}_{name} = $(CONAN_{name}_{name}_LINKER_FLAGS) $(CONAN_{name}_{name}_LIBRARIES) $(CONAN_{name}_{name}_SYSTEM_LIBS) $(CONAN_{name}_{name}_FRAMEWORKS"
 ]
 
 
@@ -49,17 +49,17 @@ def expected_files(current_folder, configuration, architecture, sdk, sdk_version
     name = _get_filename(configuration, architecture, sdk, sdk_version)
     deps = ["hello", "goodbye"]
     files.extend(
-        [os.path.join(current_folder, "conan_{}{}.xcconfig".format(dep, name)) for dep in deps])
+        [os.path.join(current_folder, "conan_{dep}_{dep}{name}.xcconfig".format(dep=dep, name=name)) for dep in deps])
     files.extend(
-        [os.path.join(current_folder, "conan_{}_vars{}.xcconfig".format(dep, name)) for dep in deps])
+        [os.path.join(current_folder, "conan_{dep}_{dep}_vars{name}.xcconfig".format(dep=dep, name=name)) for dep in deps])
     files.append(os.path.join(current_folder, "conandeps.xcconfig"))
     return files
 
 
 def check_contents(client, deps, configuration, architecture, sdk, sdk_version):
     for dep_name in deps:
-        dep_xconfig = client.load("conan_{}.xcconfig".format(dep_name))
-        conf_name = "conan_{}{}.xcconfig".format(dep_name,
+        dep_xconfig = client.load("conan_{dep}_{dep}.xcconfig".format(dep=dep_name))
+        conf_name = "conan_{}_{}{}.xcconfig".format(dep_name, dep_name,
                                                  _get_filename(configuration, architecture, sdk, sdk_version))
 
         assert '#include "{}"'.format(conf_name) in dep_xconfig
@@ -67,7 +67,7 @@ def check_contents(client, deps, configuration, architecture, sdk, sdk_version):
             line = var.format(name=dep_name)
             assert line in dep_xconfig
 
-        vars_name = "conan_{}_vars{}.xcconfig".format(dep_name,
+        vars_name = "conan_{}_{}_vars{}.xcconfig".format(dep_name, dep_name,
                                                       _get_filename(configuration, architecture, sdk, sdk_version))
         conan_vars = client.load(vars_name)
         for var in _expected_vars_xconfig:
