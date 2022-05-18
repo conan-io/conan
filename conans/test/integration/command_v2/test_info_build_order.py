dc429ad386a056af9a2cd7df8a31fc57ea60d98e
James <james@conan.io>
2022-05-18 16:39:15 +0200

    Feature/develop2 build requires self (#11244)
    * checking build_requires to self in other arch for cross-build
    
    * wip
    
    * review

--- conans/test/integration/command_v2/test_info_build_order.py
+++ conans/test/integration/command_v2/test_info_build_order.py
@@ -20,16 +20,17 @@ def test_info_build_order():
             {
                 "ref": "dep/0.1#4d670581ccb765839f2239cc8dff8fbd",
                 "depends": [],
-                "packages": [
+                "packages": [[
                     {
                         "package_id": "da39a3ee5e6b4b0d3255bfef95601890afd80709",
                         'prev': None,
                         'filenames': [],
                         "context": "host",
+                        'depends': [],
                         "binary": "Build",
                         "options": []
                     }
-                ]
+                ]]
             }
         ],
         [
@@ -38,16 +39,17 @@ def test_info_build_order():
                 "depends": [
                     "dep/0.1#4d670581ccb765839f2239cc8dff8fbd"
                 ],
-                "packages": [
+                "packages": [[
                     {
                         "package_id": "59205ba5b14b8f4ebc216a6c51a89553021e82c1",
                         'prev': None,
                         'filenames': [],
                         "context": "host",
+                        'depends': [],
                         "binary": "Build",
                         "options": []
                     }
-                ]
+                ]]
             }
         ]
     ]
@@ -69,16 +71,17 @@ def test_info_build_order_build_require():
             {
                 "ref": 'dep/0.1#4d670581ccb765839f2239cc8dff8fbd',
                 "depends": [],
-                "packages": [
+                "packages": [[
                     {
                         "package_id": "da39a3ee5e6b4b0d3255bfef95601890afd80709",
                         'prev': None,
                         'filenames': [],
                         "context": "build",
+                        'depends': [],
                         "binary": "Build",
                         "options": []
                     }
-                ]
+                ]]
             }
         ],
         [
@@ -87,16 +90,17 @@ def test_info_build_order_build_require():
                 "depends": [
                     "dep/0.1#4d670581ccb765839f2239cc8dff8fbd"
                 ],
-                "packages": [
+                "packages": [[
                     {
                         "package_id": "da39a3ee5e6b4b0d3255bfef95601890afd80709",
                         'prev': None,
                         'filenames': [],
                         "context": "host",
+                        'depends': [],
                         "binary": "Build",
                         "options": []
                     }
-                ]
+                ]]
             }
         ]
     ]
@@ -124,29 +128,31 @@ def test_info_build_order_options():
         [
             {'ref': 'tool/0.1#b4c19a1357b43877a2019dd2804336a9',
              'depends': [],
-             'packages': [
+             'packages': [[
                  {'package_id': '1124b99dc8cd3c8bbf79121c7bf86ce40c725a40', 'prev': None,
-                  'context': 'build',
+                  'context': 'build', 'depends': [],
                   'binary': 'Build', 'options': ['tool/0.1:myopt=2'], 'filenames': []},
                  {'package_id': 'a9035d84c5880b26c4b44acf70078c9a7dd37412', 'prev': None,
-                  'context': 'build',
+                  'context': 'build', 'depends': [],
                   'binary': 'Build', 'options': ['tool/0.1:myopt=1'],
                   'filenames': []}
-             ]}
+             ]]}
         ],
         [
             {'ref': 'dep1/0.1#7f0d80f9cb8c6bab06def7f6fb8f3b86',
              'depends': ['tool/0.1#b4c19a1357b43877a2019dd2804336a9'],
-             'packages': [
+             'packages': [[
                  {'package_id': 'da39a3ee5e6b4b0d3255bfef95601890afd80709', 'prev': None,
-                  'context': 'host', 'binary': 'Build', 'options': [], 'filenames': []}
-             ]},
+                  'context': 'host', 'depends': [], 'binary': 'Build', 'options': [],
+                  'filenames': []}
+             ]]},
             {'ref': 'dep2/0.1#23c789d2b36f0461e52cd6f139f97f5e',
              'depends': ['tool/0.1#b4c19a1357b43877a2019dd2804336a9'],
-             'packages': [
+             'packages': [[
                  {'package_id': 'da39a3ee5e6b4b0d3255bfef95601890afd80709', 'prev': None,
-                  'context': 'host', 'binary': 'Build', 'options': [], 'filenames': []}
-             ]}
+                  'context': 'host', 'depends': [], 'binary': 'Build', 'options': [],
+                  'filenames': []}
+             ]]}
         ]
     ]
     assert bo_json == result
@@ -173,16 +179,17 @@ def test_info_build_order_merge_multi_product():
             {
                 "ref": "dep/0.1#4d670581ccb765839f2239cc8dff8fbd",
                 "depends": [],
-                "packages": [
+                "packages": [[
                     {
                         "package_id": "da39a3ee5e6b4b0d3255bfef95601890afd80709",
                         'prev': None,
                         'filenames': ["bo1", "bo2"],
                         "context": "host",
+                        'depends': [],
                         "binary": "Build",
                         "options": []
                     }
-                ]
+                ]]
             }
         ],
         [
@@ -191,32 +198,34 @@ def test_info_build_order_merge_multi_product():
                 "depends": [
                     "dep/0.1#4d670581ccb765839f2239cc8dff8fbd"
                 ],
-                "packages": [
+                "packages": [[
                     {
                         "package_id": "59205ba5b14b8f4ebc216a6c51a89553021e82c1",
                         'prev': None,
                         'filenames': ["bo1"],
                         "context": "host",
+                        'depends': [],
                         "binary": "Build",
                         "options": []
                     }
-                ]
+                ]]
             },
             {
                 "ref": "pkg/0.2#1ac8dd17c0f9f420935abd3b6a8fa032",
                 "depends": [
                     "dep/0.1#4d670581ccb765839f2239cc8dff8fbd"
                 ],
-                "packages": [
+                "packages": [[
                     {
                         "package_id": "59205ba5b14b8f4ebc216a6c51a89553021e82c1",
                         'prev': None,
                         'filenames': ["bo2"],
                         "context": "host",
+                        'depends': [],
                         "binary": "Build",
                         "options": []
                     }
-                ]
+                ]]
             }
         ]
     ]
@@ -256,30 +265,32 @@ def test_info_build_order_merge_conditionals():
             {
                 "ref": "depwin/0.1#4d670581ccb765839f2239cc8dff8fbd",
                 "depends": [],
-                "packages": [
+                "packages": [[
                     {
                         "package_id": "da39a3ee5e6b4b0d3255bfef95601890afd80709",
                         'prev': None,
                         'filenames': ["bo_win"],
                         "context": "host",
+                        'depends': [],
                         "binary": "Build",
                         "options": []
                     }
-                ]
+                ]]
             },
             {
                 "ref": "depnix/0.1#4d670581ccb765839f2239cc8dff8fbd",
                 "depends": [],
-                "packages": [
+                "packages": [[
                     {
                         "package_id": "da39a3ee5e6b4b0d3255bfef95601890afd80709",
                         'prev': None,
                         'filenames': ["bo_nix"],
                         "context": "host",
+                        'depends': [],
                         "binary": "Build",
                         "options": []
                     }
-                ]
+                ]]
             }
         ],
         [
@@ -289,12 +300,13 @@ def test_info_build_order_merge_conditionals():
                     "depwin/0.1#4d670581ccb765839f2239cc8dff8fbd",
                     "depnix/0.1#4d670581ccb765839f2239cc8dff8fbd"
                 ],
-                "packages": [
+                "packages": [[
                     {
                         "package_id": "b23846b9b10455081d89a9dfacd01f7712d04b95",
                         'prev': None,
                         'filenames': ["bo_win"],
                         "context": "host",
+                        'depends': [],
                         "binary": "Build",
                         "options": []
                     },
@@ -303,10 +315,11 @@ def test_info_build_order_merge_conditionals():
                         'prev': None,
                         'filenames': ["bo_nix"],
                         "context": "host",
+                        'depends': [],
                         "binary": "Build",
                         "options": []
                     }
-                ]
+                ]]
             }
         ]
     ]
