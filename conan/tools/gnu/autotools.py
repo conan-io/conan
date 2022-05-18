e4ee626c7d9b23e9ba6ac23361af69ea991e0239
czoido <mrgalleta@gmail.com>
2022-05-18 15:32:49 +0200

    remove second configure

--- conan/tools/gnu/autotools.py
+++ conan/tools/gnu/autotools.py
@@ -28,7 +28,7 @@ class Autotools(object):
         https://gcc.gnu.org/onlinedocs/gccint/Configure-Terms.html
         """
         configure_args = []
-        if self.default_configure_install_args and self._conanfile.package_folder:
+        if self.default_configure_install_args:
             def _get_argument(argument_name, cppinfo_name):
                 elements = getattr(self._conanfile.cpp.package, cppinfo_name)
                 return "--{}=${{prefix}}/{}".format(argument_name, elements[0]) if elements else ""
@@ -95,9 +95,6 @@ class Autotools(object):
                     _fix_install_name(shared_lib, full_folder)
 
     def install(self):
-        # FIXME: we have to run configure twice because the local flow won't work otherwise
-        #  because there's no package_folder until the package step
-        self.configure()
         self.make(target="install", args=["DESTDIR={}".format(self._conanfile.package_folder)])
         if self._conanfile.settings.get_safe("os") == "Macos" and self._conanfile.options.get_safe("shared", False):
             self._fix_osx_shared_install_name()
