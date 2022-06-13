cdbebf920cb72f6bc15b06f98c9c1862a1a254ca
Luis Martinez <lasote@gmail.com>
2022-06-13 12:06:59 +0200

    New rm by pattern tool (#11443)

--- conan/tools/files/__init__.py
+++ conan/tools/files/__init__.py
@@ -1,5 +1,5 @@
-from conan.tools.files.files import load, save, mkdir, rmdir, ftp_download, download, get, rename, \
-    chdir, unzip, replace_in_file, collect_libs, check_md5, check_sha1, check_sha256
+from conan.tools.files.files import load, save, mkdir, rmdir, rm, ftp_download, download, get, \
+    rename, chdir, unzip, replace_in_file, collect_libs, check_md5, check_sha1, check_sha256
 
 from conan.tools.files.patches import patch, apply_conandata_patches
 from conan.tools.files.cpp_package import CppPackage
