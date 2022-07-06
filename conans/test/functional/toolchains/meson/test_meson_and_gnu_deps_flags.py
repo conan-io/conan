b0e8b329f3b004467a9dc547ba0f956c1d5d0643
Francisco Ramirez de Anton <franchuti688@gmail.com>
2022-07-06 10:58:21 +0200

    Added new tests to check MesonDeps flags addtion

--- conans/test/functional/toolchains/meson/test_meson_and_gnu_deps_flags.py
+++ conans/test/functional/toolchains/meson/test_meson_and_gnu_deps_flags.py
@@ -1,5 +1,7 @@
+import os
 import textwrap
 
+from conan.tools.files import load
 from conans.test.assets.sources import gen_function_cpp
 from conans.test.functional.toolchains.meson._base import TestMesonBase
 from conans.test.utils.tools import TestClient
@@ -7,7 +9,7 @@ from conans.test.utils.tools import TestClient
 
 class TestMesonToolchainAndGnuFlags(TestMesonBase):
 
-    def test_mesontoolchain_using_gnu_deps_flags(self):
+    def test_mesondeps(self):
         client = TestClient(path_with_spaces=False)
         client.run("new hello/0.1 -s")
         client.run("create . hello/0.1@ %s" % self._settings_str)
@@ -46,3 +48,85 @@ class TestMesonToolchainAndGnuFlags(TestMesonBase):
         client.run("install . %s" % self._settings_str)
         client.run("build .")
         assert "[2/2] Linking target demo" in client.out
+
+    def test_mesondeps_flags_are_being_appended_and_not_replacing_toolchain_ones(self):
+        """
+        Test MesonDeps and MesonToolchain are keeping all the flags/definitions defined
+        from both generators and nothing is being messed up.
+        """
+        client = TestClient(path_with_spaces=False)
+        # Dependency - hello/0.1
+        conanfile_py = textwrap.dedent("""
+        from conans import ConanFile
+
+        class HelloConan(ConanFile):
+            name = "hello"
+            version = "0.1"
+
+            def package_info(self):
+                self.cpp_info.libs = ["hello"]
+                self.cpp_info.cxxflags = ["-Wpedantic", "-Werror"]
+                self.cpp_info.defines = ['DEF1=one_string', 'DEF2=other_string']
+        """)
+        client.save({"conanfile.py": conanfile_py})
+        client.run("create . %s" % self._settings_str)
+        # Dependency - other/0.2
+        conanfile_py = textwrap.dedent("""
+        from conans import ConanFile
+
+        class OtherConan(ConanFile):
+            name = "other"
+            version = "0.1"
+
+            def package_info(self):
+                self.cpp_info.libs = ["other"]
+                self.cpp_info.defines = ['DEF3=simple_string']
+        """)
+        client.save({"conanfile.py": conanfile_py}, clean_first=True)
+        client.run("create . %s" % self._settings_str)
+
+        # Consumer using MesonDeps and MesonToolchain
+        conanfile_py = textwrap.dedent("""
+        from conan import ConanFile
+        from conan.tools.meson import Meson, MesonDeps, MesonToolchain
+
+        class App(ConanFile):
+            settings = "os", "arch", "compiler", "build_type"
+            requires = "hello/0.1", "other/0.1"
+
+            def layout(self):
+                self.folders.build = "build"
+
+            def generate(self):
+                tc = MesonDeps(self)
+                tc.generate()
+                tc = MesonToolchain(self)
+                tc.preprocessor_definitions["VAR"] = "VALUE"
+                tc.preprocessor_definitions["VAR2"] = "VALUE2"
+                tc.generate()
+
+            def build(self):
+                meson = Meson(self)
+                meson.configure()
+                meson.build()
+        """)
+
+        meson_build = textwrap.dedent("""
+            project('tutorial', 'cpp')
+            cxx = meson.get_compiler('cpp')
+            # It's not needed to declare "hello/0.1" as a dependency, only interested in flags
+            executable('demo', 'main.cpp')
+        """)
+        client.save({"conanfile.py": conanfile_py,
+                     "meson.build": meson_build,
+                     "main.cpp": "int main()\n{return 0;}\n"},
+                    clean_first=True)
+
+        client.run("install . %s -c 'tools.build:cxxflags=[\"-Wall\", \"-finline-functions\"]'" % self._settings_str)
+        client.run("build .")
+
+        meson_log_path = os.path.join(client.current_folder, "build", "meson-logs", "meson-log.txt")
+        meson_log = load(None, meson_log_path)
+        assert '-Wall -finline-functions -DVAR="VALUE" -DVAR2="VALUE2" ' \
+               '-Wpedantic -Werror -DDEF3=simple_string -DDEF1=one_string ' \
+               '-DDEF2=other_string' in meson_log
