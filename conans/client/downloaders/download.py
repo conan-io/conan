c3f820ecaef24a52e010d8e4a8a4c1d901b55806
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2022-07-06 13:17:26 +0100

    Undo redundant changes in download.py

--- conans/client/downloaders/download.py
+++ conans/client/downloaders/download.py
@@ -2,8 +2,8 @@ from conans.client.downloaders.cached_file_downloader import CachedFileDownloade
 from conans.client.downloaders.file_downloader import FileDownloader
 
 
-def run_downloader(requester, output, verify, retry, retry_wait, download_cache,
-                   user_download=False, **kwargs):
+def run_downloader(requester, output, verify, retry, retry_wait, download_cache, user_download=False,
+                   **kwargs):
     downloader = FileDownloader(requester=requester, output=output, verify=verify,
                                 config_retry=retry, config_retry_wait=retry_wait)
     if download_cache:
