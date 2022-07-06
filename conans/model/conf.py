f70d68a6a936475fc5914749cd3dc14d6bb8f2c1
Francisco Ramirez de Anton <franchuti688@gmail.com>
2022-07-06 15:19:13 +0200

    added more

--- conans/model/conf.py
+++ conans/model/conf.py
@@ -38,6 +38,7 @@ BUILT_IN_CONFS = {
     "tools.build:skip_test": "Do not execute CMake.test() and Meson.test() when enabled",
     "tools.build:jobs": "Default compile jobs number -jX Ninja, Make, /MP VS (default: max CPUs)",
     "tools.build:sysroot": "Pass the --sysroot=<tools.build:sysroot> flag if available. (None by default)",
+    "tools.build.cross_building:can_run": "",
     "tools.cmake.cmaketoolchain:generator": "User defined CMake generator to use instead of default",
     "tools.cmake.cmaketoolchain:find_package_prefer_config": "Argument for the CMAKE_FIND_PACKAGE_PREFER_CONFIG",
     "tools.cmake.cmaketoolchain:toolchain_file": "Use other existing file rather than conan_toolchain.cmake one",
