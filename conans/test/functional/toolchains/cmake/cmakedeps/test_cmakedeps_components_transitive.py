82f58b75c9dbd46fc6ceb6da6753ec5174b1e0e0
czoido <mrgalleta@gmail.com>
2022-07-06 09:09:01 +0200

    faster test

--- conans/test/functional/toolchains/cmake/cmakedeps/test_cmakedeps_components_transitive.py
+++ conans/test/functional/toolchains/cmake/cmakedeps/test_cmakedeps_components_transitive.py
@@ -17,50 +17,37 @@ def test_cmakedeps_propagate_components():
             name = "top"
             version = "1.0"
             settings = "os", "compiler", "build_type", "arch"
-            exports_sources = "CMakeLists.txt", "src/*", "include/*"
+            exports_sources = "CMakeLists.txt", "include/*"
             generators = "CMakeToolchain"
 
             def layout(self):
                 cmake_layout(self)
 
-            def build(self):
-                cmake = CMake(self)
-                cmake.configure()
-                cmake.build()
-
             def package(self):
                 cmake = CMake(self)
+                cmake.configure()
                 cmake.install()
 
             def package_info(self):
-                self.cpp_info.components["cmp1"].libs = ["cmp1"]
-                self.cpp_info.components["cmp2"].libs = ["cmp2"]
-        """)
-
-    cmp_cpp = textwrap.dedent("""
-        #include <iostream>
-        #include "{cmpname}.h"
-        void {cmpname}(){{ std::cout << "{cmpname}" << std::endl; }}
+                self.cpp_info.components["cmp1"].includedirs = ["include"]
+                self.cpp_info.components["cmp2"].includedirs = ["include"]
         """)
 
     cmp_include = textwrap.dedent("""
         #pragma once
-        void {cmpname}();
+        #include <iostream>
+        void {cmpname}(){{ std::cout << "{cmpname}" << std::endl; }};
         """)
 
     cmakelist = textwrap.dedent("""
         cmake_minimum_required(VERSION 3.15)
         project(top CXX)
-
-        add_library(cmp1 src/cmp1.cpp)
-        add_library(cmp2 src/cmp2.cpp)
-
-        target_include_directories(cmp1 PUBLIC include)
-        target_include_directories(cmp2 PUBLIC include)
-
+        add_library(cmp1 INTERFACE)
+        add_library(cmp2 INTERFACE)
+        target_include_directories(cmp1 INTERFACE include)
+        target_include_directories(cmp2 INTERFACE include)
         set_target_properties(cmp1 PROPERTIES PUBLIC_HEADER "include/cmp1.h")
         set_target_properties(cmp2 PROPERTIES PUBLIC_HEADER "include/cmp2.h")
-
         install(TARGETS cmp1)
         install(TARGETS cmp2)
         """)
@@ -70,8 +57,6 @@ def test_cmakedeps_propagate_components():
         'top/CMakeLists.txt': cmakelist,
         'top/include/cmp1.h': cmp_include.format(cmpname="cmp1"),
         'top/include/cmp2.h': cmp_include.format(cmpname="cmp2"),
-        'top/src/cmp1.cpp': cmp_cpp.format(cmpname="cmp1"),
-        'top/src/cmp2.cpp': cmp_cpp.format(cmpname="cmp2"),
     })
 
     client.run("create top")
@@ -87,46 +72,35 @@ def test_cmakedeps_propagate_components():
             requires = "top/1.0"
             settings = "os", "compiler", "build_type", "arch"
             generators = "CMakeDeps", "CMakeToolchain"
-            exports_sources = "CMakeLists.txt", "src/*", "include/*"
+            exports_sources = "CMakeLists.txt", "include/*"
 
             def layout(self):
                 cmake_layout(self)
 
-            def build(self):
-                cmake = CMake(self)
-                cmake.configure()
-                cmake.build()
-
             def package(self):
                 cmake = CMake(self)
+                cmake.configure()
                 cmake.install()
 
             def package_info(self):
                 self.cpp_info.requires = ["top::cmp1"]
-                self.cpp_info.libs = ["middle"]
-        """)
-
-    middle_cpp = textwrap.dedent("""
-        #include <iostream>
-        #include "cmp1.h"
-        #include "middle.h"
-
-        void middle(){ cmp1(); }
         """)
 
     middle_include = textwrap.dedent("""
         #pragma once
-        void middle();
+        #include <iostream>
+        #include "cmp1.h"
+        void middle(){ cmp1(); };
         """)
 
     cmakelist = textwrap.dedent("""
         cmake_minimum_required(VERSION 3.15)
         project(middle CXX)
         find_package(top CONFIG REQUIRED COMPONENTS cmp1)
-        add_library(middle src/middle.cpp)
-        target_include_directories(middle PUBLIC include)
+        add_library(middle INTERFACE)
+        target_include_directories(middle INTERFACE include)
+        target_link_libraries(middle INTERFACE top::cmp1)
         set_target_properties(middle PROPERTIES PUBLIC_HEADER "include/middle.h")
-        target_link_libraries(middle top::cmp1)
         install(TARGETS middle)
         """)
 
@@ -134,7 +108,6 @@ def test_cmakedeps_propagate_components():
         'middle/conanfile.py': middle,
         'middle/CMakeLists.txt': cmakelist,
         'middle/include/middle.h': middle_include,
-        'middle/src/middle.cpp': middle_cpp,
     })
 
     client.run("create middle")
@@ -168,16 +141,7 @@ def test_cmakedeps_propagate_components():
         'consumer/main.cpp': main,
     })
 
-    with client.chdir("consumer/build"):
-        client.run("install ..")
-        if platform.system() == "Windows":
-            client.run_command('cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake '
-                               '-G "Visual Studio 15"')
-            client.run_command('cmake --build . --config Release')
-            client.run_command(r"Release\\consumer.exe")
-        else:
-            client.run_command(
-                "cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release")
-            client.run_command("cmake --build .")
-            client.run_command("./consumer")
-        assert "cmp2" not in client.out
+    client.run("install consumer")
+
+    assert "top::cmp2" not in client.load("top-release-x86_64-data.cmake")
+    assert "top::cmp2" not in client.load("top-Target-release.cmake")
