5ae26263b43d999d9ad041f3b58f99a78f6035cc
R. Yushaev <44146334+Naufragous@users.noreply.github.com>
2022-07-09 22:23:12 +0200

    Fix CMake.test() target for Ninja Multi-Config (#11594)
    * Fix bug in CMake.test() when using Ninja Multi-Config (#11405)
    
    * Add unit test for the CMake.test() helper
    
    * Fix unit test for Windows platform

---
+++ conans/test/unittests/tools/cmake/test_cmake_test.py
@@ -0,0 +1,45 @@
+import platform
+import pytest
+
+from conan.tools.cmake import CMake
+from conan.tools.cmake.presets import write_cmake_presets
+from conans.client.conf import get_default_settings_yml
+from conans.model.conf import Conf
+from conans.model.settings import Settings
+from conans.test.utils.mocks import ConanFileMock
+from conans.test.utils.test_files import temp_folder
+
+
+@pytest.mark.parametrize("generator,target", [
+    ("NMake Makefiles", "test"),
+    ("Ninja Makefiles", "test"),
+    ("Ninja Multi-Config", "test"),
+    ("Unix Makefiles", "test"),
+    ("Visual Studio 14 2015", "RUN_TESTS"),
+    ("Xcode", "RUN_TESTS"),
+])
+def test_run_tests(generator, target):
+    """
+    Testing that the proper test target is picked for different generators, especially multi-config ones.
+    Issue related: https://github.com/conan-io/conan/issues/11405
+    """
+    settings = Settings.loads(get_default_settings_yml())
+    settings.os = "Windows"
+    settings.arch = "x86"
+    settings.build_type = "Release"
+    settings.compiler = "Visual Studio"
+    settings.compiler.runtime = "MDd"
+    settings.compiler.version = "14"
+
+    conanfile = ConanFileMock()
+    conanfile.conf = Conf()
+    conanfile.folders.generators = "."
+    conanfile.folders.set_base_generators(temp_folder())
+    conanfile.settings = settings
+
+    write_cmake_presets(conanfile, "toolchain", generator, {})
+    cmake = CMake(conanfile)
+    cmake.test()
+
+    search_pattern = "--target {}" if platform.system() == "Windows" else "'--target' '{}'"
+    assert search_pattern.format(target) in conanfile.command
