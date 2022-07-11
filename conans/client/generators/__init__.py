bac87e11a365fbbd8c67842b283ae13387a687bc
James <james@conan.io>
2022-07-11 13:42:47 +0200

    Update conans/client/generators/__init__.py

--- conans/client/generators/__init__.py
+++ conans/client/generators/__init__.py
@@ -79,7 +79,6 @@ def write_generators(conanfile, hook_manager):
     new_gen_folder = conanfile.generators_folder
     _receive_conf(conanfile)
 
-    # TODO: Should this apply to the ``generate()`` method only?
     hook_manager.execute("pre_generate", conanfile=conanfile)
 
     for generator_name in set(conanfile.generators):
