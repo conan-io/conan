1a92246569ef39b11a86460b31eb0677454ae440
Francisco Ramirez de Anton <franchuti688@gmail.com>
2022-07-12 16:40:15 +0200

    comment

--- conans/test/functional/toolchains/meson/test_meson.py
+++ conans/test/functional/toolchains/meson/test_meson.py
@@ -137,5 +137,6 @@ class MesonToolchainTest(TestMesonBase):
             ext = "dylib" if platform.system() == "Darwin" else "so"
             assert os.path.exists(os.path.join(package_folder, "bin", "demo"))
             assert os.path.exists(os.path.join(package_folder, "lib", "libhello." + ext))
+        # res/tutorial -> tutorial is being added automatically by Meson
         assert os.path.exists(os.path.join(package_folder, "res", "tutorial", "file1.txt"))
         assert os.path.exists(os.path.join(package_folder, "res", "tutorial", "file2.txt"))
