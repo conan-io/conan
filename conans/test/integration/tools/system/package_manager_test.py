80a824c61df83667830bc607a55c6a02bba613f5
czoido <mrgalleta@gmail.com>
2022-07-18 13:17:05 +0200

    minor changes

--- conans/test/integration/tools/system/package_manager_test.py
+++ conans/test/integration/tools/system/package_manager_test.py
@@ -55,6 +55,7 @@ def test_msys2():
     ("opensuse-tumbleweed", "zypper"),
     ("freebsd", "pkg"),
 ])
+@pytest.mark.skipif(platform.system() != "Linux", reason="Only linux")
 def test_package_manager_distro(distro, tool):
     with mock.patch("platform.system", return_value="Linux"):
         with mock.patch("distro.id", return_value=distro):
