6a6ff6e07526b05d51998a5c5ed54936f0fd7ff9
Luis Martinez <lasote@gmail.com>
2022-07-22 11:45:10 +0200

    cmake presets with VS and ninja and improved merge (#11666)
    * tests pending
    
    * Added tests
    
    * Fix default
    
    * not cpp 20
    
    * Update conan/tools/cmake/presets.py
    
    Co-authored-by: James <james@conan.io>
    
    * fix
    
    * WIP
    
    * Fix win test
    
    * Tested win
    
    * toolset and architecture for Ninka
    
    * Fix bug duplicated
    
    * Fixed bug
    
    * comment
    
    * Test ninja cmake presets
    
    * version only for non windows
    
    * compiler 191
    
    * CMakePresets bugfixes related to the update of a configuration
    
    Co-authored-by: James <james@conan.io>

--- conans/test/integration/toolchains/cmake/test_cmaketoolchain.py
+++ conans/test/integration/toolchains/cmake/test_cmaketoolchain.py
@@ -9,6 +9,7 @@ from mock import mock
 from conan.tools.cmake.presets import load_cmake_presets
 from conans.test.assets.genconanfile import GenConanfile
 from conans.test.utils.tools import TestClient
+from conans.util.files import rmdir
 
 
 def test_cross_build():
@@ -384,7 +385,8 @@ def test_cmake_presets_multiconfig():
     assert presets["buildPresets"][0]["configuration"] == "Release"
     assert presets["buildPresets"][1]["configuration"] == "Debug"
 
-    client.run("install mylib/1.0@ -g CMakeToolchain -s build_type=RelWithDebInfo --profile:h=profile")
+    client.run("install mylib/1.0@ -g CMakeToolchain -s build_type=RelWithDebInfo "
+               "--profile:h=profile")
     client.run("install mylib/1.0@ -g CMakeToolchain -s build_type=MinSizeRel --profile:h=profile")
     presets = json.loads(client.load("CMakePresets.json"))
     assert len(presets["buildPresets"]) == 4
@@ -581,3 +583,107 @@ def test_presets_paths_correct():
     assert "build/17/generators/conan_toolchain.cmake" \
            in presets["configurePresets"][1]["cacheVariables"]["CMAKE_TOOLCHAIN_FILE"].replace("\\",
                                                                                                "/")
+
+
+def test_presets_updated():
+    """If a preset file is generated more than once, the values are always added and, in case the
+    configurePreset or buildPreset already exist, the new preset is updated """
+    client = TestClient()
+    conanfile = textwrap.dedent("""
+            from conan import ConanFile
+            from conan.tools.cmake import cmake_layout
+
+            class Conan(ConanFile):
+                settings = "os", "arch", "compiler", "build_type"
+                generators = "CMakeToolchain"
+
+                def layout(self):
+                    cmake_layout(self)
+            """)
+    client.save({"conanfile.py": conanfile, "CMakeLists.txt": "foo"})
+    configs = ["-c tools.cmake.cmaketoolchain.presets:max_schema_version=2 ",
+               "-c tools.cmake.cmake_layout:build_folder_vars='[\"settings.compiler.cppstd\"]'"]
+    client.run("install . {} -s compiler.cppstd=14".format(" ".join(configs)))
+    client.run("install . {} -s compiler.cppstd=17".format(" ".join(configs)))
+
+    presets = json.loads(client.load("CMakeUserPresets.json"))
+    assert len(presets["configurePresets"]) == 2
+    assert "FOO" not in presets["configurePresets"][0]["cacheVariables"]
+
+    # Now introduce a cache_variable FOO to see if we get it in the CMakeUserPresets.json (that
+    # at the same time, it will come from the build/xxxx/CMakePreset.json that is also updated)
+    conanfile = textwrap.dedent("""
+            from conan import ConanFile
+            from conan.tools.cmake import cmake_layout, CMakeToolchain
+
+            class Conan(ConanFile):
+                settings = "os", "arch", "compiler", "build_type"
+
+                def generate(self):
+                    tc = CMakeToolchain(self)
+                    tc.cache_variables["FOO"] = "var"
+                    tc.generate()
+
+                def layout(self):
+                    cmake_layout(self)
+            """)
+    client.save({"conanfile.py": conanfile})
+    client.run("install . {} -s compiler.cppstd=14".format(" ".join(configs)))
+    presets = json.loads(client.load("CMakeUserPresets.json"))
+    assert len(presets["configurePresets"]) == 2
+    assert "FOO" in presets["configurePresets"][0]["cacheVariables"]
+
+
+@pytest.mark.parametrize("arch, arch_toolset", [("x86", "x86_64"), ("x86_64", "x86_64")])
+def test_presets_ninja_msvc(arch, arch_toolset):
+    client = TestClient()
+    conanfile = textwrap.dedent("""
+            from conan import ConanFile
+            from conan.tools.cmake import cmake_layout
+
+            class Conan(ConanFile):
+                settings = "os", "arch", "compiler", "build_type"
+                generators = "CMakeToolchain"
+
+                def layout(self):
+                    cmake_layout(self)
+            """)
+    client.save({"conanfile.py": conanfile, "CMakeLists.txt": "foo"})
+    configs = ["-c tools.cmake.cmaketoolchain:toolset_arch={}".format(arch_toolset),
+               "-c tools.cmake.cmake_layout:build_folder_vars='[\"settings.compiler.cppstd\"]'",
+               "-c tools.cmake.cmaketoolchain:generator=Ninja"]
+    msvc = " -s compiler=msvc -s compiler.version=191 -s compiler.runtime=static " \
+           "-s compiler.runtime_type=Release"
+    client.run("install . {} -s compiler.cppstd=14 {} -s arch={}".format(" ".join(configs), msvc, arch))
+
+    presets = json.loads(client.load("build/14/generators/CMakePresets.json"))
+
+    toolset_value = {"x86_64": "host=x86_64", "x86": "x86"}.get(arch_toolset)
+    arch_value = {"x86_64": "x64", "x86": "x86"}.get(arch)
+
+    assert presets["configurePresets"][0]["architecture"]["value"] == arch_value
+    assert presets["configurePresets"][0]["architecture"]["strategy"] == "external"
+    assert presets["configurePresets"][0]["toolset"]["value"] == toolset_value
+    assert presets["configurePresets"][0]["toolset"]["strategy"] == "external"
+
+    # Only for Ninja, no ninja, no values
+    rmdir(os.path.join(client.current_folder, "build"))
+    configs = ["-c tools.cmake.cmaketoolchain:toolset_arch={}".format(arch_toolset),
+               "-c tools.cmake.cmake_layout:build_folder_vars='[\"settings.compiler.cppstd\"]'"]
+    client.run(
+        "install . {} -s compiler.cppstd=14 {} -s arch={}".format(" ".join(configs), msvc, arch))
+
+    presets = json.loads(client.load("build/14/generators/CMakePresets.json"))
+    assert "architecture" not in presets["configurePresets"][0]
+    assert "toolset" not in presets["configurePresets"][0]
+
+    # No toolset defined in conf, no value
+    rmdir(os.path.join(client.current_folder, "build"))
+    configs = ["-c tools.cmake.cmake_layout:build_folder_vars='[\"settings.compiler.cppstd\"]'",
+               "-c tools.cmake.cmaketoolchain:generator=Ninja"]
+
+    client.run(
+        "install . {} -s compiler.cppstd=14 {} -s arch={}".format(" ".join(configs), msvc, arch))
+    presets = json.loads(client.load("build/14/generators/CMakePresets.json"))
+    assert "architecture" in presets["configurePresets"][0]
+    assert "toolset" not in presets["configurePresets"][0]
