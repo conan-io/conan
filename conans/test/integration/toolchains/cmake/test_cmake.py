caec658189ced147e9eaf1b0875aead94fcb2f49
Luis Martinez de Bartolome <lasote@gmail.com>
2022-07-29 12:26:10 +0200

    Fail if no 'os' is specified in build profile and want to run an external command

--- conans/test/integration/toolchains/cmake/test_cmake.py
+++ conans/test/integration/toolchains/cmake/test_cmake.py
@@ -1,6 +1,41 @@
 import textwrap
+import platform
+import pytest
 
 from conans.test.utils.tools import TestClient
+
+
+@pytest.mark.skipif(platform.system() != "Windows", reason="Fails on windows")
+def test_crossbuild_windows_incomplete():
+    client = TestClient()
+    conanfile = textwrap.dedent("""
+        from conan import ConanFile
+        from conan.tools.cmake import CMake
+        class Pkg(ConanFile):
+            name = "pkg"
+            version = "0.1"
+            settings = "os", "compiler", "build_type", "arch"
+            generators = "CMakeToolchain"
+            def build(self):
+                self.run("dir c:")
+            """)
+    build_profile = textwrap.dedent("""
+        [settings]
+        arch=x86_64
+    """)
+    client.save({"conanfile.py": conanfile, "build_profile": build_profile})
+    client.run("create . -pr:b=build_profile", assert_error=True)
+    assert "'.' is not recognized as an internal or external command" not in client.out
+    assert "ERROR: The 'build' profile must have a 'os' declared" in client.out
+
+    build_profile = textwrap.dedent("""
+            [settings]
+            os=Windows
+            arch=x86_64
+        """)
+    client.save({"conanfile.py": conanfile, "build_profile": build_profile})
+    client.run("create . -pr:b=build_profile")
+    assert "<DIR>          ." in client.out
 
 
 def test_configure_args():
