1eff01f9fb3cf31a5de5815b6f93be103c59dfc3
Francisco Ramirez de Anton <franchuti688@gmail.com>
2022-08-05 14:38:50 +0200

    Clearer comment

--- conans/client/installer.py
+++ conans/client/installer.py
@@ -681,7 +681,7 @@ class BinaryInstaller(object):
                         conanfile.cpp_info = CppInfo(conanfile.name, package_folder,
                                                      default_values=CppInfoDefaultValues())
                         # Note: Remember that this is not needed for Conan 2.x
-                        # Let's avoid losing this information already processed before.
+                        # Let's avoid losing this information.
                         conanfile.cpp_info.version = conanfile.version
                         conanfile.cpp_info.description = conanfile.description
                         conanfile.cpp_info.public_deps = public_deps
