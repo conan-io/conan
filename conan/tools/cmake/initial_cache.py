755b4fdb333460bcb8f77f994d21c2f6c24ad235
Francisco Ramirez de Anton <franchuti688@gmail.com>
2022-09-05 10:58:53 +0200

    wip

--- conan/tools/cmake/initial_cache.py
+++ conan/tools/cmake/initial_cache.py
@@ -22,7 +22,42 @@ class CMakeInitialCache:
         return {}
 
     def generate(self):
-        cache_cmake_paath = os.path.join(self._conanfile.generators_folder, "CMakeInitialCache.cmake")
-        if not os.path.exists(cache_cmake_paath):
+        cache_cmake_path = os.path.join(self._conanfile.generators_folder, "CMakeInitialCache.cmake")
+        if not os.path.exists(cache_cmake_path):
             content = self._content()
-            save(cache_cmake_paath, content)
+            save(cache_cmake_path, content)
+
+    def configure(self, variables=None, build_script_folder=None):
+        cmakelist_folder = self._conanfile.source_folder
+        if build_script_folder:
+            cmakelist_folder = os.path.join(self._conanfile.source_folder, build_script_folder)
+
+        build_folder = self._conanfile.build_folder
+        generator_folder = self._conanfile.generators_folder
+
+        mkdir(self._conanfile, build_folder)
+
+        arg_list = [self._cmake_program]
+        if self._generator:
+            arg_list.append('-G "{}"'.format(self._generator))
+        if self._toolchain_file:
+            if os.path.isabs(self._toolchain_file):
+                toolpath = self._toolchain_file
+            else:
+                toolpath = os.path.join(generator_folder, self._toolchain_file)
+            arg_list.append('-DCMAKE_TOOLCHAIN_FILE="{}"'.format(toolpath.replace("\\", "/")))
+        if self._conanfile.package_folder:
+            pkg_folder = self._conanfile.package_folder.replace("\\", "/")
+            arg_list.append('-DCMAKE_INSTALL_PREFIX="{}"'.format(pkg_folder))
+
+        if not variables:
+            variables = {}
+        self._cache_variables.update(variables)
+
+        arg_list.extend(['-D{}="{}"'.format(k, v) for k, v in self._cache_variables.items()])
+        arg_list.append('"{}"'.format(cmakelist_folder))
+
+        command = " ".join(arg_list)
+        self._conanfile.output.info("CMake command: %s" % command)
+        with chdir(self, build_folder):
+            self._conanfile.run(command)
