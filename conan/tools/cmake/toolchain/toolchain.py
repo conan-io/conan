45d2f85c09d3ab91618787911b39965ee4e17db1
Uilian Ries <uilianries@gmail.com>
2022-09-13 16:28:52 +0200

    [hotfix] CMakeToolchain.cache_variables should parse PackageOption (#12086)
    * cache variables parse config value
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * parse digit
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/tools/cmake/toolchain/toolchain.py
+++ conan/tools/cmake/toolchain/toolchain.py
@@ -17,6 +17,7 @@ from conan.tools.intel import IntelCC
 from conan.tools.microsoft import VCVars
 from conan.tools.microsoft.visual import vs_ide_version
 from conans.errors import ConanException
+from conans.model.options import PackageOption
 from conans.util.files import save
 
 
@@ -182,6 +183,13 @@ class CMakeToolchain(object):
         for name, value in self.cache_variables.items():
             if isinstance(value, bool):
                 cache_variables[name] = "ON" if value else "OFF"
+            elif isinstance(value, PackageOption):
+                if str(value).lower() in ["true", "false", "none"]:
+                    cache_variables[name] = "ON" if bool(value) else "OFF"
+                elif str(value).isdigit():
+                    cache_variables[name] = int(value)
+                else:
+                    cache_variables[name] = str(value)
             else:
                 cache_variables[name] = value
 
