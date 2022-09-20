3a79b0f64138c08736b5361132988c804e513d9b
memsharded <james@conan.io>
2022-09-21 00:27:19 +0200

    review

--- conans/test/integration/package_id/compatible_test.py
+++ conans/test/integration/package_id/compatible_test.py
@@ -89,52 +89,50 @@ class CompatibleIDsTest(unittest.TestCase):
 
     def test_compatible_setting_debug_release(self):
         # From issue https://github.com/conan-io/conan/issues/9398
-        client = TestClient(servers={"server1": TestServer()},
-                            users={"server1": [("lasote", "mypass")]})
+        client = TestClient(default_server_user=True)
         conanfile = textwrap.dedent("""\
-        from conans import ConanFile
-        class Pkg(ConanFile):
-            settings = "build_type"
+            from conan import ConanFile
+            class Pkg(ConanFile):
+                name = "pkga"
+                version = "0.1"
+                settings = "build_type"
 
-            def package_id(self):
-                compatible_pkg = self.info.clone()
-                compatible_pkg.settings.build_type = "Release"
-                self.compatible_packages.append(compatible_pkg)
-        """)
+                def package_id(self):
+                    compatible_pkg = self.info.clone()
+                    compatible_pkg.settings.build_type = "Release"
+                    self.compatible_packages.append(compatible_pkg)
+            """)
         client.save({"conanfile.py": conanfile})
         # Create the recipe and upload it into the remote
-        client.run("create . pkga/0.1@lasote/testing -s build_type=Release")
-        client.run("upload pkg* -r=server1 --confirm --all")
-        # Remove all the files created
+        client.run("create . -s build_type=Release")
+        client.run("upload pkg* -r=default --confirm --all")
         client.run("remove * -f")
 
         # Install locally the uploaded package
-        client.run("install -if pkga pkga/0.1@lasote/testing --build missing")
+        client.run("install pkga/0.1@")
 
         # Create a middle package and export it
         conanfile = textwrap.dedent("""\
-        from conans import ConanFile
+            from conan import ConanFile
 
-        class PkgB(ConanFile):
-            name = "pkgb"
-            version = "0.1"
-            settings = "os", "compiler", "build_type", "arch"
-            build_requires = "pkga/0.1@lasote/testing"
-           """)
+            class PkgB(ConanFile):
+                name = "pkgb"
+                version = "0.1"
+                build_requires = "pkga/0.1"
+               """)
         client.save({"conanfile.py": conanfile}, clean_first=True)
         client.run("export .")
 
         # Create a final package and install it changing the "build_type"
         conanfile = textwrap.dedent("""\
-        from conans import ConanFile
+            from conan import ConanFile
 
-        class PkgC(ConanFile):
-           settings = "os", "compiler", "build_type", "arch"
-           requires = "pkgb/0.1"
-           build_requires = "pkga/0.1@lasote/testing"
-           """)
+            class PkgC(ConanFile):
+               requires = "pkgb/0.1"
+               build_requires = "pkga/0.1"
+               """)
         client.save({"conanfile.py": conanfile}, clean_first=True)
-        client.run("install . -if build -s build_type=Debug --build=missing")
+        client.run("install . -s build_type=Debug --build=missing")
 
     def test_compatible_setting_no_user_channel(self):
         client = TestClient()
