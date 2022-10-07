f1f0d860ac0d601581956f6c3d74654bb3852149
Carlos Zoido <mrgalleta@gmail.com>
2022-10-07 16:33:29 +0200

    remote subcommand (#12263)

--- conan/cli/commands/inspect.py
+++ conan/cli/commands/inspect.py
@@ -1,9 +1,10 @@
 import inspect as python_inspect
-
+import os
 
 from conan.api.output import cli_out_write
 from conan.cli.command import conan_command, COMMAND_GROUPS, conan_subcommand
 from conan.cli.commands import default_json_formatter
+from conan.cli.commands.install import _get_conanfile_path
 
 
 def inspect_text_formatter(data):
@@ -18,22 +19,18 @@ def inspect_text_formatter(data):
             cli_out_write("{}: {}".format(name, value))
 
 
-@conan_command(group=COMMAND_GROUPS['consumer'])
+@conan_command(group=COMMAND_GROUPS['consumer'], formatters={"text": inspect_text_formatter, "json": default_json_formatter})
 def inspect(conan_api, parser, *args):
     """
     Inspect a conanfile.py to return the public fields
     """
-
-
-@conan_subcommand(formatters={"text": inspect_text_formatter, "json": default_json_formatter})
-def inspect_path(conan_api, parser, subparser, *args, **kwargs):
-    """
-    Returns the specified attribute/s of a conanfile
-    """
-    subparser.add_argument("path", help="Path to a folder containing a recipe (conanfile.py)")
+    parser.add_argument("path", help="Path to a folder containing a recipe (conanfile.py)")
 
     args = parser.parse_args(*args)
-    conanfile = conan_api.graph.load_conanfile_class(args.path)
+
+    path = _get_conanfile_path(args.path, os.getcwd(), py=True)
+
+    conanfile = conan_api.graph.load_conanfile_class(path)
     ret = {}
 
     for name, value in python_inspect.getmembers(conanfile):
