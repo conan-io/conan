264686738a2453303f7a4a886e8778226f2a77e7
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2022-10-10 11:14:19 +0100

    Add docstring

--- conan/tools/build/stdcpp_library.py
+++ conan/tools/build/stdcpp_library.py
@@ -1,5 +1,9 @@
 
 def stdcpp_library(conanfile):
+    """ Returns the name of the C++ standard library that can be passed
+    to the linker, based on the current settings. Returs None if the name 
+    of the C++ standard library file is not known.
+    """
     libcxx = conanfile.settings.get_safe("compiler.libcxx")
     if libcxx in ["libstdc++", "libstdc++11"]:
         return "stdc++"
