d5d8c8279a1eb4c703e1c1efa7939468bfd1fc11
czoido <mrgalleta@gmail.com>
2022-10-26 17:14:31 +0200

    fix test

--- conans/test/unittests/tools/apple/test_apple_tools.py
+++ conans/test/unittests/tools/apple/test_apple_tools.py
@@ -1,12 +1,12 @@
 import pytest
 
-from conans.test.utils.mocks import ConanFileMock, MockSettings
+from conans.test.utils.mocks import ConanFileMock, MockSettings, MockOptions
 from conans.test.utils.test_files import temp_folder
 from conan.tools.apple import is_apple_os, to_apple_arch, fix_apple_shared_install_name
 
 def test_tools_apple_is_apple_os():
     conanfile = ConanFileMock()
-    
+
     conanfile.settings = MockSettings({"os": "Macos"})
     assert is_apple_os(conanfile) == True
 
@@ -16,20 +16,20 @@ def test_tools_apple_is_apple_os():
     conanfile.settings = MockSettings({"os": "Windows"})
     assert is_apple_os(conanfile) == False
 
-    
+
 def test_tools_apple_to_apple_arch():
     conanfile = ConanFileMock()
-    
+
     conanfile.settings = MockSettings({"arch": "armv8"})
     assert to_apple_arch(conanfile) == "arm64"
 
     conanfile.settings = MockSettings({"arch": "x86_64"})
     assert to_apple_arch(conanfile) == "x86_64"
 
+
 def test_fix_shared_install_name_no_libraries():
-    conanfile = ConanFileMock(
-        options="""{"shared": [True, False]}""",
-        options_values={"shared": True})
+    conanfile = ConanFileMock()
+    conanfile.options = MockOptions({"shared": True})
     conanfile.settings = MockSettings({"os": "Macos"})
     conanfile.folders.set_base_package(temp_folder())
 
