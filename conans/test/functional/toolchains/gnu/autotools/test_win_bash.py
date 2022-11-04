3e08841b55bd7bb83af31d212774b998add503f1
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-11-04 12:37:55 +0100

    Fix autotools install on Windows (#12193)
    * fix autotools install on Windows
    
    * extend win_bash/Autotools test with autotools installation
    
    * install in build() since test runs discrete commands
    
    Co-authored-by: memsharded <james@conan.io>

--- conans/test/functional/toolchains/gnu/autotools/test_win_bash.py
+++ conans/test/functional/toolchains/gnu/autotools/test_win_bash.py
@@ -1,3 +1,4 @@
+import os
 import platform
 import textwrap
 
@@ -47,6 +48,7 @@ def test_autotools_bash_complete():
                 autotools = Autotools(self)
                 autotools.configure()
                 autotools.make()
+                autotools.install()
         """)
 
     client.save({"conanfile.py": conanfile,
@@ -61,3 +63,6 @@ def test_autotools_bash_complete():
     bat_contents = client.load("conanbuild.bat")
     assert "conanvcvars.bat" in bat_contents
 
+    # To check that the ``autotools.install()`` has worked correctly
+    # FIXME IN CONAN 2.0 this will break, no local `package_folder`
+    assert os.path.exists(os.path.join(client.current_folder, "package", "bin", "main.exe"))
