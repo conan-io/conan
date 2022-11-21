c856eda0b4430602028182950703676995a204ee
memsharded <james@conan.io>
2022-11-21 01:33:07 +0100

    fix unittest

--- conans/test/unittests/cli/test_cli_ref_matching.py
+++ conans/test/unittests/cli/test_cli_ref_matching.py
@@ -6,8 +6,8 @@ from conan.internal.api.ref_pattern import RefPattern
 @pytest.mark.parametrize("pattern, result",
                          [("*",                  ("*", None, None, None)),
                           ("zlib/1.2.11",        ("zlib/1.2.11", None, None, None)),
-                          ("zlib/1.2.11#rev1",   ("zlib/1.2.11", "rev1", None , None)),
+                          ("zlib/1.2.11#rev1",   ("zlib/1.2.11", "rev1", None, None)),
                           ("zlib/1.2.11:pid1",   ("zlib/1.2.11", None, "pid1", None))])
 def test_cli_pattern_matching(pattern, result):
     pattern = RefPattern(pattern)
-    assert result == (pattern.ref, pattern.rrev, pattern.pid, pattern.prev)
+    assert result == (pattern.ref, pattern.rrev, pattern.package_id, pattern.prev)
