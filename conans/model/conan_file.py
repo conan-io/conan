c8b225f0f581d52e1d1d65d48cf3b5ad8b6e8844
memsharded <james@conan.io>
2022-11-22 11:10:45 +0100

    fixes

--- conans/model/conan_file.py
+++ conans/model/conan_file.py
@@ -438,7 +438,7 @@ class ConanFile(object):
                     return tools.run_in_windows_bash(self, bashcmd=cmd, cwd=cwd, subsystem=subsystem,
                                                      msys_mingw=msys_mingw, with_login=with_login)
             envfiles_folder = self.generators_folder or os.getcwd()
-            _env = [_env] if _env and isinstance(_env, str) else _env
+            _env = [_env] if _env and isinstance(_env, str) else (_env or [])
             assert isinstance(_env, list)
             wrapped_cmd = command_env_wrapper(self, cmd, _env, envfiles_folder=envfiles_folder,
                                               scope=scope)
