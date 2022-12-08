421d4bb890d2134a9de44025e65a4ec42f357fbe
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-12-08 15:57:27 +0100

    add conan.tools.gnu.is_mingw()

--- conan/tools/gnu/__init__.py
+++ conan/tools/gnu/__init__.py
@@ -1,5 +1,6 @@
 from conan.tools.gnu.autotools import Autotools
 from conan.tools.gnu.autotoolstoolchain import AutotoolsToolchain
 from conan.tools.gnu.autotoolsdeps import AutotoolsDeps
+from conan.tools.gnu.mingw import is_mingw
 from conan.tools.gnu.pkgconfig import PkgConfig
 from conan.tools.gnu.pkgconfigdeps import PkgConfigDeps
---
+++ conan/tools/gnu/mingw.py
@@ -0,0 +1,12 @@
+def is_mingw(conanfile):
+    """
+    Validate if current compiler in host setttings is related to MinGW
+    :param conanfile: ConanFile instance
+    :return: True, if the host compiler is related to MinGW, otherwise False.
+    """
+    host_os = conanfile.settings.get_safe("os")
+    host_compiler = conanfile.settings.get_safe("compiler")
+    is_mingw_gcc = host_os == "Windows" and host_compiler == "gcc"
+    is_mingw_clang = host_os == "Windows" and host_compiler == "clang" and \
+                     not conanfile.settings.get_safe("compiler.runtime")
+    return is_mingw_gcc or is_mingw_clang
