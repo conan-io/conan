e45409b5c2ceb827f7e6bd5af9980d9a90aedc6e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-12-10 14:49:07 +0100

    not mingw if host subsystem is cygwin or wsl

--- conan/tools/gnu/mingw.py
+++ conan/tools/gnu/mingw.py
@@ -5,8 +5,10 @@ def is_mingw(conanfile):
     :return: True, if the host compiler is related to MinGW, otherwise False.
     """
     host_os = conanfile.settings.get_safe("os")
+    host_subsystem = conanfile.settings.get_safe("os.subsystem")
+    is_windows_native = host_os == "Windows" and host_subsystem not in ["cygwin", "wsl"]
     host_compiler = conanfile.settings.get_safe("compiler")
-    is_mingw_gcc = host_os == "Windows" and host_compiler == "gcc"
-    is_mingw_clang = host_os == "Windows" and host_compiler == "clang" and \
+    is_mingw_gcc = is_windows_native and host_compiler == "gcc"
+    is_mingw_clang = is_windows_native and host_compiler == "clang" and \
                      not conanfile.settings.get_safe("compiler.runtime")
     return is_mingw_gcc or is_mingw_clang
