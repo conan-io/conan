c92cfdc135c141a14ec14a9affe357f88e52789a
Rubén Rincón Blanco <rubenrb@jfrog.com>
2022-12-20 16:09:33 +0100

    Check for comment only in line start

--- conans/client/loader.py
+++ conans/client/loader.py
@@ -364,9 +364,9 @@ def _get_required_conan_version_without_loading(conan_file_path):
     txt_version = None
 
     try:
-        found = re.search(r".*required_conan_version\s*=\s*(.*)", contents)
-        if found and "#" not in found.group(0):
-            txt_version = found.group(1).replace('"', "")
+        found = re.search(r"(.*)required_conan_version\s*=\s*(.*)", contents)
+        if found and "#" not in found.group(1):
+            txt_version = found.group(2).replace('"', "")
     except:
         pass
 
