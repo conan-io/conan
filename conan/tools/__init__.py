cf44fd6523bbf01d6c71c698973fe785d955f30a
Rubén Rincón Blanco <ruben@rinconblanco.es>
2022-12-20 15:26:05 +0100

    Update conan/tools/__init__.py
    Co-authored-by: James <memsharded@gmail.com>

--- conan/tools/__init__.py
+++ conan/tools/__init__.py
@@ -7,5 +7,5 @@ CONAN_TOOLCHAIN_ARGS_SECTION = "toolchain"
 def _check_duplicated_generator(generator, conanfile):
     if generator.__class__.__name__ in conanfile.generators:
         raise ConanException(f"{generator.__class__.__name__} is declared in the generators "
-                             "attribute, but was instantiated in the generate() method toos. "
+                             "attribute, but was instantiated in the generate() method too. "
                              "It should only be present in one of them.")
