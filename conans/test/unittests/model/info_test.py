19941153edb72c4656ffcb8b6e62a96860c50418
memsharded <james@conan.io>
2022-12-23 00:06:29 +0100

    [develop2] test that false values affect package_id

--- conans/test/unittests/model/info_test.py
+++ conans/test/unittests/model/info_test.py
@@ -15,10 +15,16 @@ def test_false_values_affect_none():
     build_reqs = RequirementsInfo({})
     python_reqs = PythonRequiresInfo({}, default_package_id_mode=None)
     options = Options({"shared": [True, False], "other": [None, "1"]}, {"shared": False})
-    c = ConanInfo(Settings(), options, reqs, build_reqs, python_reqs)
+    settings = Settings({"mysetting": [1, 2, 3]})
+    c = ConanInfo(settings, options, reqs, build_reqs, python_reqs)
     conaninfo = c.dumps()
     assert "shared=False" in conaninfo
     assert "other" not in conaninfo
+    assert "mysetting" not in conaninfo
+
+    settings.mysetting = 1
+    conaninfo = c.dumps()
+    assert "mysetting=1" in conaninfo
 
 
 info_text = '''[settings]
