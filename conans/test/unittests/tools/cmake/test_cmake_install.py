478cea262c3e5543f14d2c55f441ec41792a2def
Joshua Neu <neuj21@gmail.com>
2022-12-23 21:06:18 -0600

    Address comments from #12762

--- conans/test/unittests/tools/cmake/test_cmake_install.py
+++ conans/test/unittests/tools/cmake/test_cmake_install.py
@@ -10,19 +10,12 @@ from conans.test.utils.mocks import ConanFileMock
 from conans.test.utils.test_files import temp_folder
 
 
-@pytest.mark.parametrize("generator", [
-    ("NMake Makefiles"),
-    ("Ninja Makefiles"),
-    ("Ninja Multi-Config"),
-    ("Unix Makefiles"),
-    ("Visual Studio 14 2015"),
-    ("Xcode"),
-])
-def test_run_install_component(generator):
+def test_run_install_component():
     """
     Testing that the proper component is installed.
     Issue related: https://github.com/conan-io/conan/issues/10359
     """
+    # Load some generic windows settings
     settings = Settings.loads(get_default_settings_yml())
     settings.os = "Windows"
     settings.arch = "x86"
@@ -38,39 +31,9 @@ def test_run_install_component(generator):
     conanfile.settings = settings
     conanfile.folders.set_base_package(temp_folder())
 
-    write_cmake_presets(conanfile, "toolchain", generator, {})
+    # Choose generator to match generic setttings
+    write_cmake_presets(conanfile, "toolchain", "Visual Studio 14 2015", {})
     cmake = CMake(conanfile)
     cmake.install(component="foo")
 
     assert "--component foo" in conanfile.command
-
-
-@pytest.mark.parametrize("generator", [
-    ("NMake Makefiles"),
-    ("Ninja Makefiles"),
-    ("Ninja Multi-Config"),
-    ("Unix Makefiles"),
-    ("Visual Studio 14 2015"),
-    ("Xcode"),
-])
-def test_run_install_no_component(generator):
-    settings = Settings.loads(get_default_settings_yml())
-    settings.os = "Windows"
-    settings.arch = "x86"
-    settings.build_type = "Release"
-    settings.compiler = "Visual Studio"
-    settings.compiler.runtime = "MDd"
-    settings.compiler.version = "14"
-
-    conanfile = ConanFileMock()
-    conanfile.conf = Conf()
-    conanfile.folders.generators = "."
-    conanfile.folders.set_base_generators(temp_folder())
-    conanfile.settings = settings
-    conanfile.folders.set_base_package(temp_folder())
-
-    write_cmake_presets(conanfile, "toolchain", generator, {})
-    cmake = CMake(conanfile)
-    cmake.install()
-
-    assert "--component" not in conanfile.command
