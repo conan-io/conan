44aa15cdfe4d18c8aba473061ba4b21ba6849bb0
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-12-28 01:09:38 +0100

    remove wsl logic

--- conan/tools/gnu/mingw.py
+++ conan/tools/gnu/mingw.py
@@ -6,10 +6,9 @@ def is_mingw(conanfile):
     """
     host_os = conanfile.settings.get_safe("os")
     host_subsystem = conanfile.settings.get_safe("os.subsystem")
-    is_wsl = host_os == "Windows" and host_subsystem == "wsl"
     is_cygwin = host_os == "Windows" and host_subsystem == "cygwin"
     host_compiler = conanfile.settings.get_safe("compiler")
-    is_mingw_gcc = host_os == "Windows" and not (is_wsl or is_cygwin) and host_compiler == "gcc"
-    is_mingw_clang = host_os == "Windows" and not (is_wsl or is_cygwin) and host_compiler == "clang" and \
+    is_mingw_gcc = host_os == "Windows" and not is_cygwin and host_compiler == "gcc"
+    is_mingw_clang = host_os == "Windows" and not is_cygwin and host_compiler == "clang" and \
                      not conanfile.settings.get_safe("compiler.runtime")
     return is_mingw_gcc or is_mingw_clang
