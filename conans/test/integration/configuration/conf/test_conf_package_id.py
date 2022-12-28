1685d77b78594878b368eaff1746a29d90402ba5
memsharded <james@conan.io>
2022-12-28 02:48:30 +0100

    tests

--- conans/test/integration/configuration/conf/test_conf_package_id.py
+++ conans/test/integration/configuration/conf/test_conf_package_id.py
@@ -29,6 +29,6 @@ def test_package_id(client):
     client.save({"profile1": profile1,
                  "profile2": profile2})
     client.run("create . --name=pkg --version=0.1 -pr=profile1")
-    client.assert_listed_binary({"pkg/0.1": ("bf9f277ff9c5ce88c27d42e2de02d8ee3433ddd9", "Build")})
+    client.assert_listed_binary({"pkg/0.1": ("f34214fcde48b6a07353cfe1dbd452cdf01a098c", "Build")})
     client.run("create . --name=pkg --version=0.1 -pr=profile2")
-    client.assert_listed_binary({"pkg/0.1": ("aafb08585a95c8a51b765f758507cbee1e680867", "Build")})
+    client.assert_listed_binary({"pkg/0.1": ("1db44e3df6a7a86a169026273f283074d69dfa81", "Build")})
