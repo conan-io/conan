97cd27b8be8338d1c0c9a83531662ab36de8e988
Francisco Ramirez de Anton <franchuti688@gmail.com>
2022-12-28 16:57:37 +0100

    Added more tests

--- conans/test/integration/command_v2/list_test.py
+++ conans/test/integration/command_v2/list_test.py
@@ -47,6 +47,20 @@ class TestListBase:
                         f"--user={ref.user} --channel={ref.channel}")
         self.client.run("upload --force -r {} {}".format(remote, ref))
 
+    def _upload_full_recipe_without_conaninfo(self, remote, ref):
+        self.client.save({"conanfile.py": GenConanfile("pkg", "0.1").with_package_file("file.h",
+                                                                                       "0.1")})
+        self.client.run("create . --user=user --channel=channel")
+        self.client.run("upload --force -r {} {}".format(remote, "pkg/0.1@user/channel"))
+
+        self.client.save({'conanfile.py': GenConanfile()
+                          })
+        self.client.run(f"create . --name={ref.name} --version={ref.version} "
+                        f"-s os=Macos -s build_type=Release -s arch=x86_64 "
+                        f"--user={ref.user} --channel={ref.channel}")
+        self.client.run("upload --force -r {} {}".format(remote, ref))
+
+
     @staticmethod
     def _get_fake_recipe_refence(recipe_name):
         return f"{recipe_name}#fca0383e6a43348f7989f11ab8f0a92d"
@@ -329,6 +343,99 @@ class TestListUseCases(TestListBase):
                   shared=False
                 requires:
                   pkg/0.1.Z@user/channel
+        """)
+        assert bool(re.match(expected_output, output, re.MULTILINE))
+
+    def test_search_all_revisions_and_package_revisions(self):
+        """Checking if RREVs and PREVs are shown correctly"""
+        remote1 = "remote1"
+        remote2 = "remote2"
+
+        self._add_remote(remote1)
+        self._upload_full_recipe_without_conaninfo(remote1,
+                                                   RecipeReference(name="test_recipe", version="1.0",
+                                                                   user="user", channel="channel"))
+        self._add_remote(remote2)
+        self._upload_full_recipe_without_conaninfo(remote2,
+                                                   RecipeReference(name="test_recipe", version="2.1",
+                                                                   user="user", channel="channel"))
+        self.client.run(f'list *#* -r="*" -c')
+        output = str(self.client.out)
+        expected_output = textwrap.dedent("""\
+        Local Cache:
+          test_recipe
+            test_recipe/2.1@user/channel#4d670581ccb765839f2239cc8dff8fbd .*
+            test_recipe/1.0@user/channel#4d670581ccb765839f2239cc8dff8fbd .*
+          pkg
+            pkg/0.1@user/channel#44a36b797bc85fb66af6acf90cf8f539 .*
+        remote1:
+          pkg
+            pkg/0.1@user/channel#44a36b797bc85fb66af6acf90cf8f539 .*
+          test_recipe
+            test_recipe/1.0@user/channel#4d670581ccb765839f2239cc8dff8fbd .*
+        remote2:
+          pkg
+            pkg/0.1@user/channel#44a36b797bc85fb66af6acf90cf8f539 .*
+          test_recipe
+            test_recipe/2.1@user/channel#4d670581ccb765839f2239cc8dff8fbd .*
+        """)
+        assert bool(re.match(expected_output, output, re.MULTILINE))
+        self.client.run(f'list test_recipe/*:*#* -r="*" -c')
+        output = str(self.client.out)
+        expected_output = textwrap.dedent("""\
+        Local Cache:
+          test_recipe
+            test_recipe/2.1@user/channel#4d670581ccb765839f2239cc8dff8fbd .*
+              PID: da39a3ee5e6b4b0d3255bfef95601890afd80709
+                PREV: 0ba8627bd47edc3a501e8f0eb9a79e5e .*
+            test_recipe/1.0@user/channel#4d670581ccb765839f2239cc8dff8fbd .*
+              PID: da39a3ee5e6b4b0d3255bfef95601890afd80709
+                PREV: 0ba8627bd47edc3a501e8f0eb9a79e5e .*
+        remote1:
+          test_recipe
+            test_recipe/1.0@user/channel#4d670581ccb765839f2239cc8dff8fbd .*
+              PID: da39a3ee5e6b4b0d3255bfef95601890afd80709
+                PREV: 0ba8627bd47edc3a501e8f0eb9a79e5e .*
+        remote2:
+          test_recipe
+            test_recipe/2.1@user/channel#4d670581ccb765839f2239cc8dff8fbd .*
+              PID: da39a3ee5e6b4b0d3255bfef95601890afd80709
+                PREV: 0ba8627bd47edc3a501e8f0eb9a79e5e .*
+        """)
+        assert bool(re.match(expected_output, output, re.MULTILINE))
+
+    def test_search_all_revisions_given_a_package_id(self):
+        """
+        Checking if PREVs are shown correctly given a PkgID and even though that package has no
+        configuration at all.
+        """
+        remote1 = "remote1"
+        self._add_remote(remote1)
+        self._upload_full_recipe_without_conaninfo(remote1,
+                                                   RecipeReference(name="test_recipe", version="1.0",
+                                                                   user="user", channel="channel"))
+        self.client.run(f'list *:* -r=remote1')
+        output = str(self.client.out)
+        expected_output = textwrap.dedent("""\
+        remote1:
+          pkg
+            pkg/0.1@user/channel#44a36b797bc85fb66af6acf90cf8f539 .*
+              PID: da39a3ee5e6b4b0d3255bfef95601890afd80709
+                Empty package information
+          test_recipe
+            test_recipe/1.0@user/channel#4d670581ccb765839f2239cc8dff8fbd .*
+              PID: da39a3ee5e6b4b0d3255bfef95601890afd80709
+                Empty package information
+        """)
+        assert bool(re.match(expected_output, output, re.MULTILINE))
+        self.client.run(f'list test_recipe/*:da39a3ee5e6b4b0d3255bfef95601890afd80709#* -r=remote1')
+        output = str(self.client.out)
+        expected_output = textwrap.dedent("""\
+        remote1:
+          test_recipe
+            test_recipe/1.0@user/channel#4d670581ccb765839f2239cc8dff8fbd .*
+              PID: da39a3ee5e6b4b0d3255bfef95601890afd80709
+                PREV: 0ba8627bd47edc3a501e8f0eb9a79e5e .*
         """)
         assert bool(re.match(expected_output, output, re.MULTILINE))
 
