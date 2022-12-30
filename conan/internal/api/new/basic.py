96d6939118cccdd1b7c269cb99e441ff296796bb
Rubén Rincón Blanco <rubenrb@jfrog.com>
2022-12-30 15:02:50 +0100

    Add help text to some commands

--- conan/internal/api/new/basic.py
+++ conan/internal/api/new/basic.py
@@ -1,13 +1,24 @@
+def inject_get_or_else(variable, default):
+    return variable + ' = "{% if ' + variable + " is defined %}{{ " + variable + " }}{% else %}" + default + '{% endif %}"'
+
+_conanfile_header = f'''\
+    {inject_get_or_else("name", "pkg")}
+    {inject_get_or_else("version", "1.0")}
+    {inject_get_or_else("description", "A basic recipe")}
+    {inject_get_or_else("license", "<Your project license goes here>")}
+    {inject_get_or_else("homepage", "<Your project homepage goes here>")}
+'''
+
 _conanfile = '''\
 from conan import ConanFile
 
 class BasicConanfile(ConanFile):
-    name = "{% if name is defined %}{{ name }}{% else %}pkg{% endif %}"
-    version = "{% if version is defined %}{{ version }}{% else %}1.0{% endif %}"
-    description = "{% if description is defined %}{{ description }}{% else %}A bare-bones recipe{% endif %}"
+''' + _conanfile_header + '''\
 
-    # Text about the requirements() method
+    # The requirements method allows you to define the dependencies of your recipe
     def requirements(self):
+        # Each call to self.requires() will add the corresponding requirement
+        # to the current list of requirements
         {% if requires is defined -%}
         {% for require in as_iterable(requires) -%}
         self.requires("{{ require }}")
@@ -18,12 +29,24 @@ class BasicConanfile(ConanFile):
         pass
         {%- endif %}
 
-    # Text about the build() method
-    def build(self):
+    # The purpose of generate() is to prepare the build, generating the necessary files, such as
+    # Files containing information to locate the dependencies, environment activation scripts,
+    # and specific build system files among others
+    def generate(self):
         pass
 
-    # Text about the package() method
+    # This method is used to build the source code of the recipe using the desired commands.
+    def build(self):
+        # You can use your command line tools to invoke your build system
+        # or any of the build helpers provided with Conan in conan.tools
+        # self.run("g++ ...")
+        pass
+
+    # The actual creation of the package, once it's built, is done in the package() method.
+    # Using the copy() method from tools.files, artifacts are copied
+    # from the build folder to the package folder
     def package(self):
+        # copy(self, "*.h", self.source_folder, join(self.package_folder, "include"), keep_path=False)
         pass
 '''
 
