4a5fe644473c5bde476b6005e35934c52aec13ec
Rubén Rincón Blanco <rubenrb@jfrog.com>
2022-12-30 14:08:34 +0100

    Revert unwanted file

--- conan/internal/api/new/alias_new.py
+++ conan/internal/api/new/alias_new.py
@@ -3,7 +3,7 @@ from conan import ConanFile
 
 class AliasConanfile(ConanFile):
     name = "{{name}}"
-    {% if version is defined %}version = "{{version}}"{%endif%}
+    {% if version %}version = "{{version}}"{%endif%}
     alias = "{{name}}/{{target}}"
     revision_mode = "{{revision_mode|default('hash')}}"
 '''
