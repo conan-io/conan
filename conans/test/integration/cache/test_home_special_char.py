7248137f9cbee2e3a70f8f62707eccfda2d25721
Markus Rännare <mackyman@gmail.com>
2023-01-08 09:25:33 +0100

    Added test that fails with special chars in the pathname

--- conans/test/integration/cache/test_home_special_char.py
+++ conans/test/integration/cache/test_home_special_char.py
@@ -1,16 +1,29 @@
 import os
 
-from conans.test.assets.genconanfile import GenConanfile
 from conans.test.utils.test_files import temp_folder
 from conans.test.utils.tools import TestClient
 
+import textwrap
 
 def test_home_special_chars():
     path_chars = "päthñç$"
-    cache_folder = os.path.join(temp_folder(), path_chars)
-    current_folder = os.path.join(temp_folder(), path_chars)
+    cache_folder = os.path.join("C:\\Tmp", path_chars)
+    current_folder = os.path.join("C:\\Tmp", path_chars)
     c = TestClient(cache_folder, current_folder)
-    c.save({"conanfile.py": GenConanfile("pkg", "0.1")})
-    c.run("install .")
+
+    conan_file = textwrap.dedent("""
+        from conans import ConanFile
+
+        class App(ConanFile):
+            name="Failure"
+            version="0.1"
+            settings = 'os', 'arch', 'compiler', 'build_type'
+            generators = "MSBuildToolchain"
+
+            def build(self):
+                self.run("git --version")
+    """)
+
+    c.save({"conanfile.py": conan_file})
     c.run("create .")
     assert path_chars in c.out
