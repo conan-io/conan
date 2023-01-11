a42412598f767df7b1b33926a350bcf5ad884290
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-01-11 14:13:52 +0000

    Add unix_path_package_info_legacy compatibility function (#12887)
    * Add unix_path_legacy_compat for compatibility
    
    * Add unix_path_package_info_legacy

--- conan/tools/microsoft/subsystems.py
+++ conan/tools/microsoft/subsystems.py
@@ -1,6 +1,12 @@
 from conans.client.subsystems import deduce_subsystem, subsystem_path
+from conans.client.tools.win import unix_path as unix_path_legacy_tools
 
 
 def unix_path(conanfile, path, scope="build"):
     subsystem = deduce_subsystem(conanfile, scope=scope)
     return subsystem_path(subsystem, path)
+
+def unix_path_package_info_legacy(conanfile, path, path_flavor=None):
+    # Call legacy implementation, which has different logic
+    # to autodeduce the subsystem type for the conversion.
+    return unix_path_legacy_tools(path, path_flavor)
