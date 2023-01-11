2389de71178cef728493ba473c074bf39e47004f
Rubén Rincón Blanco <rubenrb@jfrog.com>
2023-01-11 11:32:32 +0100

    Math->Mathematics to fix Windows

--- conans/test/functional/command/new_test.py
+++ conans/test/functional/command/new_test.py
@@ -7,17 +7,17 @@ from conans.test.utils.tools import TestClient
 @pytest.mark.tool("cmake")
 def test_diamonds_cmake_with_new():
     tc = TestClient()
-    tc.run("new cmake_lib -d name=math -d version=3.14")
+    tc.run("new cmake_lib -d name=mathematics -d version=3.14")
     tc.run("create .")
-    tc.run("new cmake_lib -d name=ai -d version=0.1 -d requires=math/3.14 -f")
+    tc.run("new cmake_lib -d name=ai -d version=0.1 -d requires=mathematics/3.14 -f")
     tc.run("create .")
-    assert "math/3.14: Hello World" in tc.out
-    tc.run("new cmake_lib -d name=ui -d version=1.0 -d requires=math/3.14 -f")
+    assert "mathematics/3.14: Hello World" in tc.out
+    tc.run("new cmake_lib -d name=ui -d version=1.0 -d requires=mathematics/3.14 -f")
     tc.run("create .")
-    assert "math/3.14: Hello World" in tc.out
+    assert "mathematics/3.14: Hello World" in tc.out
     tc.run("new cmake_exe -d name=game -d version=0.0 -d requires=ai/0.1 -d requires=ui/1.0 -f")
     tc.run("create .")
-    assert "math/3.14: Hello World" in tc.out
+    assert "mathematics/3.14: Hello World" in tc.out
     assert "ai/0.1: Hello World" in tc.out
     assert "ui/1.0: Hello World" in tc.out
 
