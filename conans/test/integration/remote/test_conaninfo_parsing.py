a44c57c5bdbe87f6c134d4000c2b3ec78f0eb984
memsharded <james@conan.io>
2023-01-13 02:25:01 +0100

    fix tests

--- conans/test/integration/remote/test_conaninfo_parsing.py
+++ conans/test/integration/remote/test_conaninfo_parsing.py
@@ -29,5 +29,5 @@ def test_conaninfo_special_chars():
 
     t.run("upload * -c -r default")
     # TODO: I have struggled with this, it was not accepting "latest", revision needs explicit one
-    t.run('list weird_info/1.0#8c9e59246220eef8ca3bd4ac4f39ceb3 -r default')
+    t.run('list weird_info/1.0#8c9e59246220eef8ca3bd4ac4f39ceb3:* -r default')
     assert 'ññ¨¨&是: ][{}"是是是' in t.out
