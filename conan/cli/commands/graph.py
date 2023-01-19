b7c012d3e17a52102c1b9f9db6242047952eec83
James <james@conan.io>
2023-01-19 15:42:24 +0100

    [develop2] Improving system_requirements (#12912)
    * [develop2] Improving system_requirements
    
    * fix tests
    
    * review
    
    * wip
    
    * added report-installed

--- conan/cli/commands/graph.py
+++ conan/cli/commands/graph.py
@@ -156,6 +156,10 @@ def graph_info(conan_api, parser, subparser, *args):
         conan_api.graph.analyze_binaries(deps_graph, args.build, remotes=remotes, update=args.update,
                                          lockfile=lockfile)
         print_graph_packages(deps_graph)
+        # TODO: Refactor magic strings and use _SystemPackageManagerTool.mode_xxx ones
+        if profile_host.conf.get("tools.system.package_manager:mode") \
+                in ("check", "report", "report-installed"):
+            conan_api.install.install_system_requires(deps_graph)
 
         lockfile = conan_api.lockfile.update_lockfile(lockfile, deps_graph, args.lockfile_packages,
                                                       clean=args.lockfile_clean)
