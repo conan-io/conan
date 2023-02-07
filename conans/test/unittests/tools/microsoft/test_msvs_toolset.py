44155d67bbfc570f7cf52f013a3042861d86f1c0
Uilian Ries <uilianries@gmail.com>
2023-02-07 14:59:06 +0100

    add more test to validate intel as compiler
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/test/unittests/tools/microsoft/test_msvs_toolset.py
+++ conans/test/unittests/tools/microsoft/test_msvs_toolset.py
@@ -1,5 +1,6 @@
 import pytest
 from conan.tools.microsoft import msvs_toolset
+from conan.errors import ConanException
 from conans.test.utils.mocks import ConanFileMock, MockSettings
 
 
@@ -19,7 +20,7 @@ def test_visual_studio_default(compiler_version, expected_toolset):
     """
     conanfile = ConanFileMock()
     conanfile.settings = MockSettings({"compiler": "Visual Studio", "compiler.version": compiler_version})
-    assert expected_toolset == msvs_toolset(conanfile)
+    assert msvs_toolset(conanfile) == expected_toolset
 
 
 @pytest.mark.parametrize("compiler_version,default_toolset,expected_toolset", [
@@ -36,7 +37,7 @@ def test_visual_studio_custom(compiler_version, default_toolset, expected_toolse
     conanfile.settings = MockSettings({"compiler": "Visual Studio",
                              "compiler.version": compiler_version,
                              "compiler.toolset": expected_toolset})
-    assert expected_toolset == msvs_toolset(conanfile)
+    assert msvs_toolset(conanfile) == expected_toolset
 
 
 def test_invalid_compiler():
@@ -62,7 +63,7 @@ def test_msvc_default(compiler_version, expected_toolset):
     """
     conanfile = ConanFileMock()
     conanfile.settings = MockSettings({"compiler": "msvc", "compiler.version": compiler_version})
-    assert expected_toolset == msvs_toolset(conanfile)
+    assert msvs_toolset(conanfile) == expected_toolset
 
 
 @pytest.mark.parametrize("compiler_version,default_toolset,expected_toolset", [
@@ -80,4 +81,61 @@ def test_msvc_custom(compiler_version, default_toolset, expected_toolset):
     conanfile.settings = MockSettings({"compiler": "msvc",
                              "compiler.version": compiler_version,
                              "compiler.toolset": expected_toolset})
-    assert expected_toolset == msvs_toolset(conanfile)
+    assert msvs_toolset(conanfile) == expected_toolset
+
+
+@pytest.mark.parametrize("compiler_version,expected_toolset", [
+    ("19.1", "Intel C++ Compiler 19.1"),
+    ("19", "Intel C++ Compiler 19.0"),
+    ("18", "Intel C++ Compiler 18.0"),
+    ("17", "Intel C++ Compiler 17.0")])
+def test_intel_default(compiler_version, expected_toolset):
+    """ When running intel as compiler, and there is a compiler version configured,
+        msvs_toolset must return the compiler version as its toolset
+    """
+    conanfile = ConanFileMock()
+    conanfile.settings = MockSettings({"compiler": "intel", "compiler.version": compiler_version})
+    assert msvs_toolset(conanfile) == expected_toolset
+
+
+def test_intel_invalid():
+    """ When running intel as compiler, and there is no compiler version configured,
+        msvs_toolset must return None
+    """
+    conanfile = ConanFileMock()
+    conanfile.settings = MockSettings({"compiler": "intel"})
+    assert msvs_toolset(conanfile) is None
+
+
+def test_intel_cc_old_compiler():
+    """ When running intel-cc as compiler, and the compiler version configured is older than 2021
+        msvs_toolset must raise an ConanExpection
+    """
+    conanfile = ConanFileMock()
+    conanfile.settings = MockSettings({"compiler": "intel-cc", "compiler.version": "19.1"})
+    with pytest.raises(ConanException) as error:
+        msvs_toolset(conanfile)
+    assert "You have to use 'intel' compiler" in str(error.value)
+
+
+@pytest.mark.parametrize("compiler_version,compiler_mode,expected_toolset", [
+    ("2021.3", "classic", "Intel C++ Compiler 19.2"),
+    ("2021.2", "classic", "Intel C++ Compiler 19.2"),
+    ("2021.1", "classic", "Intel C++ Compiler 19.2"),
+    ("2021.3", "icx", "Intel C++ Compiler 2021"),
+    ("2021.2", "icx", "Intel C++ Compiler 2021"),
+    ("2021.1", "icx", "Intel C++ Compiler 2021"),
+    ("2021.3", "dpcpp", "Intel(R) oneAPI DPC++ Compiler"),
+    ("2021.2", "dpcpp", "Intel(R) oneAPI DPC++ Compiler"),
+    ("2021.1", "dpcpp", "Intel(R) oneAPI DPC++ Compiler"),
+    ("2021.3", None, "Intel(R) oneAPI DPC++ Compiler")])
+def test_intel_cc_default(compiler_version, compiler_mode, expected_toolset):
+    """ When running intel-cc as compiler, and there is a proper compiler version configured,
+        and the compiler.mode is configured,
+        msvs_toolset must return a compiler toolset based on compiler.mode
+    """
+    conanfile = ConanFileMock()
+    conanfile.settings = MockSettings({"compiler": "intel-cc",
+                                       "compiler.version": compiler_version,
+                                       "compiler.mode": compiler_mode})
+    assert msvs_toolset(conanfile) == expected_toolset
