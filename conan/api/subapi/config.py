df521af201a9c5a6c7249a1fb53d9b317cd0eb03
James <james@conan.io>
2023-02-10 12:01:41 +0100

    [develop2] remove api_method decorator (#13091)
    * [develop2] remove api_method decorator
    
    * remove dead test

--- conan/api/subapi/config.py
+++ conan/api/subapi/config.py
@@ -1,4 +1,3 @@
-from conan.api.subapi import api_method
 from conan.internal.conan_app import ConanApp
 
 
@@ -7,11 +6,9 @@ class ConfigAPI:
     def __init__(self, conan_api):
         self.conan_api = conan_api
 
-    @api_method
     def home(self):
         return self.conan_api.cache_folder
 
-    @api_method
     def install(self, path_or_url, verify_ssl, config_type=None, args=None,
                 source_folder=None, target_folder=None):
         # TODO: We probably want to split this into git-folder-http cases?
@@ -21,7 +18,6 @@ class ConfigAPI:
                                      config_type=config_type, args=args,
                                      source_folder=source_folder, target_folder=target_folder)
 
-    @api_method
     def get(self, name, default=None, check_type=None):
         app = ConanApp(self.conan_api.cache_folder)
         return app.cache.new_config.get(name, default=default, check_type=check_type)
