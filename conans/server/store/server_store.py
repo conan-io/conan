4325f30c3893e80ff4073e8b679455e3b0103267
James <james@conan.io>
2023-02-13 23:57:34 +0100

    proposing package completition only when manifest is there (#9216)
    * proposing package completition only when manifest is there
    
    * fix tests

--- conans/server/store/server_store.py
+++ conans/server/store/server_store.py
@@ -2,6 +2,7 @@ import os
 from os.path import join, normpath, relpath
 
 from conans.errors import ConanException, PackageNotFoundException, RecipeNotFoundException
+from conans.paths import CONAN_MANIFEST
 from conans.model.package_ref import PkgReference
 from conans.model.recipe_ref import RecipeReference
 from conans.server.revision_list import RevisionList
@@ -70,12 +71,18 @@ class ServerStore(object):
     def get_recipe_file_list(self, ref):
         """Returns a  [filepath] """
         assert isinstance(ref, RecipeReference)
-        return self._get_file_list(self.export(ref))
+        files = self._get_file_list(self.export(ref))
+        if CONAN_MANIFEST not in files:
+            raise RecipeNotFoundException(ref)
+        return files
 
     def get_package_file_list(self, pref):
         """Returns a  [filepath] """
         assert isinstance(pref, PkgReference)
-        return self._get_file_list(self.package(pref))
+        files = self._get_file_list(self.package(pref))
+        if CONAN_MANIFEST not in files:
+            raise PackageNotFoundException(pref)
+        return files
 
     def _get_file_list(self, relative_path):
         file_list = self._storage_adapter.get_file_list(relative_path)
