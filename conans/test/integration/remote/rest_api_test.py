ad26da29f40688e37e6f94f1630f6be2eb8eb5f7
memsharded <james@conan.io>
2023-03-07 00:20:07 +0100

    tests

--- conans/test/integration/remote/rest_api_test.py
+++ conans/test/integration/remote/rest_api_test.py
@@ -1,5 +1,4 @@
 import os
-import platform
 import unittest
 
 import pytest
@@ -97,18 +96,17 @@ class RestApiTest(unittest.TestCase):
         # Get the package
         tmp_dir = temp_folder()
         self.api.get_package(pref, tmp_dir)
-        self.assertIn("hello.cpp", os.listdir(tmp_dir))
+        # The hello.cpp file is not downloaded!
+        self.assertNotIn("hello.cpp", os.listdir(tmp_dir))
 
-    @pytest.mark.skipif(platform.system() != "Linux", reason="only Linux")
     def test_upload_huge_conan(self):
         ref = RecipeReference.loads("conanhuge/1.0.0@private_user/testing#myreciperev")
-        files = {"file%s.cpp" % name: "File conent" for name in range(10)}
-        self._upload_recipe(ref, files)
+        self._upload_recipe(ref, {"file9.cpp": ""})
 
         tmp = temp_folder()
         files = self.api.get_recipe(ref, tmp)
         self.assertIsNotNone(files)
-        self.assertTrue(os.path.exists(os.path.join(tmp, "file9.cpp")))
+        self.assertFalse(os.path.exists(os.path.join(tmp, "file9.cpp")))
 
     def test_search(self):
         # Upload a conan1
