743f655063191eda217a65aba71feebf811a0852
Uilian Ries <uilianries@gmail.com>
2023-03-08 17:31:32 +0100

    Fix info_folder_test.py tests
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/test/integration/command/info/info_folders_test.py
+++ conans/test/integration/command/info/info_folders_test.py
@@ -1,17 +1,17 @@
 import os
 import textwrap
-import unittest
-
+import json
 import pytest
 
 from conans.paths import CONANFILE
-from conans.test.utils.tools import NO_SETTINGS_PACKAGE_ID, TestClient
+from conans.test.utils.tools import TestClient
+from conan.internal.cache.conan_reference_layout import EXPORT_FOLDER
 
 conanfile_py = """
 from conan import ConanFile
 
 class AConan(ConanFile):
-    name = "MyPackage"
+    name = "package"
     version = "0.1.0"
     short_paths=False
 """
@@ -20,127 +20,150 @@ with_deps_path_file = """
 from conan import ConanFile
 
 class BConan(ConanFile):
-    name = "MyPackage2"
+    name = "package2"
     version = "0.2.0"
-    requires = "MyPackage/0.1.0@myUser/testing"
+    requires = "package/0.1.0@user/testing"
 """
 
 deps_txt_file = """
 [requires]
-MyPackage2/0.2.0@myUser/testing
+package2/0.2.0@user/testing
 """
 
 
-@pytest.mark.xfail(reason="cache2.0 revisit tests")
-class InfoFoldersTest(unittest.TestCase):
-    def setUp(self):
-        self.user_channel = "myUser/testing"
-        self.reference1 = "MyPackage/0.1.0@%s" % self.user_channel
-        self.reference2 = "MyPackage2/0.2.0@%s" % self.user_channel
+@pytest.fixture()
+def user():
+    return "user"
 
-    def _prepare_deps(self, client):
-        client.save({CONANFILE: conanfile_py})
-        client.run("export . %s" % self.user_channel)
-        client.save({CONANFILE: with_deps_path_file}, clean_first=True)
-        client.run("export . %s" % self.user_channel)
-        client.save({'conanfile.txt': deps_txt_file}, clean_first=True)
 
-    def test_basic(self):
-        client = TestClient()
-        client.save({CONANFILE: conanfile_py})
-        client.run("export . %s" % self.user_channel)
-        client.run("info %s --paths" % self.reference1)
-        base_path = os.path.join("MyPackage", "0.1.0", "myUser", "testing")
-        output = client.out
-        self.assertIn(os.path.join(base_path, "export"), output)
-        self.assertIn(os.path.join(base_path, "source"), output)
-        self.assertIn(os.path.join(base_path, "build", NO_SETTINGS_PACKAGE_ID), output)
-        self.assertIn(os.path.join(base_path, "package", NO_SETTINGS_PACKAGE_ID), output)
+@pytest.fixture()
+def channel():
+    return "testing"
 
-    def test_build_id(self):
-        # https://github.com/conan-io/conan/issues/6915
-        client = TestClient()
-        conanfile = textwrap.dedent("""
-            from conan import ConanFile
 
-            class Pkg(ConanFile):
-                options = {"myOption": [True, False]}
-                def build_id(self):
-                    self.info_build.options.myOption = "Any"
-            """)
-        client.save({CONANFILE: conanfile})
-        client.run("export . --name=pkg --version=0.1 --user=user --channel=testing")
-        client.run("info pkg/0.1@user/testing --paths -o pkg/*:myOption=True")
-        out = str(client.out).replace("\\", "/")
-        self.assertIn("ID: 75f77640a42abb05cd36235484f164c96ace0952", out)
-        self.assertIn("BuildID: c74f42995b9d1452f1534f88b6c56ed7102212d0", out)
-        self.assertIn("pkg/0.1/user/testing/build/c74f42995b9d1452f1534f88b6c56ed7102212d0", out)
-        self.assertIn("pkg/0.1/user/testing/package/75f77640a42abb05cd36235484f164c96ace0952", out)
+@pytest.fixture()
+def user_channel(user, channel):
+    return f"{user}/{channel}"
 
-        client.run("info pkg/0.1@user/testing --paths -o pkg/*:myOption=False")
-        out = str(client.out).replace("\\", "/")
-        self.assertIn("ID: 357add7d387f11a959f3ee7d4fc9c2487dbaa604", out)
-        self.assertIn("BuildID: c74f42995b9d1452f1534f88b6c56ed7102212d0", out)
-        self.assertIn("pkg/0.1/user/testing/build/c74f42995b9d1452f1534f88b6c56ed7102212d0", out)
-        self.assertIn("pkg/0.1/user/testing/package/357add7d387f11a959f3ee7d4fc9c2487dbaa604", out)
 
-    def test_deps_basic(self):
-        client = TestClient()
-        self._prepare_deps(client)
+@pytest.fixture()
+def reference1(user_channel):
+    return f"package/0.1.0@{user_channel}"
 
-        for ref in [self.reference2, "."]:
-            client.run("info %s --paths" % ref)
-            output = client.out
 
-            base_path = os.path.join("MyPackage", "0.1.0", "myUser", "testing")
-            self.assertIn(os.path.join(base_path, "export"), output)
-            self.assertIn(os.path.join(base_path, "source"), output)
+@pytest.fixture()
+def reference2(user_channel):
+    return f"package2/0.2.0@{user_channel}"
 
-            base_path = os.path.join("MyPackage2", "0.2.0", "myUser", "testing")
-            self.assertIn(os.path.join(base_path, "export"), output)
-            self.assertIn(os.path.join(base_path, "source"), output)
 
-    @pytest.mark.xfail(reason="Info command output changed")
-    def test_deps_specific_information(self):
-        client = TestClient()
-        self._prepare_deps(client)
-        client.run("info . --paths --only package_folder --package-filter MyPackage/*")
-        output = client.out
+@pytest.fixture()
+def client(user, channel):
+    client = TestClient()
+    client.save({CONANFILE: conanfile_py})
+    client.run(f"export . --user={user} --channel={channel}")
+    client.save({CONANFILE: with_deps_path_file}, clean_first=True)
+    client.run(f"export . --user={user} --channel={channel}")
+    client.save({'conanfile.txt': deps_txt_file}, clean_first=True)
+    return client
 
-        base_path = os.path.join("MyPackage", "0.1.0", "myUser", "testing")
-        self.assertIn(os.path.join(base_path, "package"), output)
-        self.assertNotIn("build_folder", output)
-        self.assertNotIn("MyPackage2", output)
 
-        client.run("info . --paths --only package_folder --package-filter MyPackage*")
-        output = client.out
+def test_basic(user, channel, reference1):
+    client = TestClient()
+    client.save({CONANFILE: conanfile_py})
+    client.run(f"export . --user={user} --channel={channel}")
+    client.run(f"graph info --requires={reference1} --format=json")
+    nodes = json.loads(client.stdout)["nodes"]
+    assert client.cache_folder in nodes[1]["recipe_folder"]
+    assert os.path.basename(nodes[1]["recipe_folder"]).strip() == EXPORT_FOLDER
+    assert nodes[1]["source_folder"] is None
+    assert nodes[1]["build_folder"] is None
+    assert nodes[1]["package_folder"] is None
 
-        base_path = os.path.join("MyPackage", "0.1.0", "myUser", "testing")
-        self.assertIn(os.path.join(base_path, "package"), output)
-        self.assertNotIn("build_folder", output)
 
-        base_path = os.path.join("MyPackage2", "0.2.0", "myUser", "testing")
-        self.assertIn(os.path.join(base_path, "package"), output)
+def test_build_id():
+    # https://github.com/conan-io/conan/issues/6915
+    client = TestClient()
+    conanfile = textwrap.dedent("""
+        from conan import ConanFile
 
-    @pytest.mark.xfail(reason="Info command output changed")
-    def test_single_field(self):
-        client = TestClient()
-        client.save({CONANFILE: conanfile_py})
-        client.run("export . %s" % self.user_channel)
-        client.run("info %s --paths --only=build_folder" % self.reference1)
-        base_path = os.path.join("MyPackage", "0.1.0", "myUser", "testing")
-        output = client.out
-        self.assertNotIn("export", output)
-        self.assertNotIn("source", output)
-        self.assertIn(os.path.join(base_path, "build"), output)
-        self.assertNotIn("package", output)
+        class Pkg(ConanFile):
+            options = {"myOption": [True, False]}
+            def build_id(self):
+                self.info_build.options.myOption = "Any"
+        """)
+    client.save({CONANFILE: conanfile})
+    client.run("export . --name=pkg --version=0.1 --user=user --channel=testing")
+    client.run("graph info --requires=pkg/0.1@user/testing -o pkg/*:myOption=True")
+    out = str(client.out).replace("\\", "/")
+    assert "package_id: b868c8ab4ae6ddccfe19fabd62a5e180d4b18a2b" in out
+    assert "build_id: d5d6fc54af6f589e338090910ac18c848a87720d" in out
 
-    def test_direct_conanfile(self):
-        client = TestClient()
-        client.save({CONANFILE: conanfile_py})
-        client.run("info .")
-        output = client.out
-        self.assertNotIn("export_folder", output)
-        self.assertNotIn("source_folder", output)
-        self.assertNotIn("build_folder", output)
-        self.assertNotIn("package_folder", output)
+    client.run("graph info --requires=pkg/0.1@user/testing -o pkg/*:myOption=False")
+    out = str(client.out).replace("\\", "/")
+    assert "package_id: 41e2e23ac9570fd23f421bcd0cf9e5cbab49e6ee" in out
+    assert "build_id: d5d6fc54af6f589e338090910ac18c848a87720d" in out
+
+
+def test_deps_basic(client, reference1, reference2):
+    for ref in [f"--requires={reference2}", "conanfile.txt"]:
+        client.run(f"graph info {ref} --format=json")
+        nodes = json.loads(client.stdout)
+        found_ref = False
+
+        for node in nodes["nodes"]:
+            if node["ref"] == "conanfile":
+                assert node["source_folder"] is None
+            else:
+                assert client.cache_folder in node["recipe_folder"]
+                assert os.path.basename(node["recipe_folder"]).strip() == EXPORT_FOLDER
+            assert node["source_folder"] is None
+            assert node["build_folder"] is None
+            assert node["package_folder"] is None
+            found_ref = found_ref or reference1 in node["ref"]
+        assert found_ref
+
+
+def test_deps_specific_information(client, reference1, reference2):
+    client.run("graph info . --package-filter package/* --format=json")
+    nodes = json.loads(client.stdout)["nodes"]
+    assert len(nodes) == 1
+    assert reference1 in nodes[0]["ref"]
+    assert nodes[0]["source_folder"] is None
+    assert nodes[0]["build_folder"] is None
+    assert nodes[0]["package_folder"] is None
+
+    client.run("graph info . --package-filter package* --format=json")
+    nodes = json.loads(client.stdout)["nodes"]
+    assert len(nodes) == 2
+    assert reference2 in nodes[0]["ref"]
+    assert nodes[0]["source_folder"] is None
+    assert nodes[0]["build_folder"] is None
+    assert nodes[0]["package_folder"] is None
+    assert reference1 in nodes[1]["ref"]
+    assert nodes[1]["source_folder"] is None
+    assert nodes[1]["build_folder"] is None
+    assert nodes[1]["package_folder"] is None
+
+
+def test_single_field(user, channel, reference1):
+    client = TestClient()
+    client.save({CONANFILE: conanfile_py})
+    client.run(f"export . --user={user} --channel={channel}")
+    client.run(f"graph info --requires {reference1} --format=json")
+    nodes = json.loads(client.stdout)["nodes"]
+    assert len(nodes) == 2
+    assert reference1 in nodes[1]["ref"]
+    assert nodes[1]["source_folder"] is None
+    assert nodes[1]["build_folder"] is None
+    assert nodes[1]["package_folder"] is None
+
+
+def test_direct_conanfile():
+    client = TestClient()
+    client.save({CONANFILE: conanfile_py})
+    client.run("graph info .")
+    output = client.out
+    assert "export_folder" not in output
+    assert "source_folder: None" in output
+    assert "build_folder: None" in output
+    assert "package_folder: None" in output
