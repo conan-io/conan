ee13fadf6bd6f523871c61f505d397797febd4ca
Rubén Rincón Blanco <rubenrb@jfrog.com>
2023-03-13 17:25:54 +0100

    Proper conf name :facepalm:

--- conans/model/conf.py
+++ conans/model/conf.py
@@ -45,7 +45,7 @@ BUILT_IN_CONFS = {
     "tools.build:sysroot": "Pass the --sysroot=<tools.build:sysroot> flag if available. (None by default)",
     "tools.build.cross_building:can_run": "Bool value that indicates whether is possible to run a non-native "
                                           "app on the same architecture. It's used by 'can_run' tool",
-    "tools.build.verbosity": 'Verbosity of build systems. Possible values are "Quiet", "Minimal", "Normal", "Detailed" and "Diagnostic"',
+    "tools.build:verbosity": 'Verbosity of build systems. Possible values are "Quiet", "Minimal", "Normal", "Detailed" and "Diagnostic"',
     "tools.cmake.cmaketoolchain:generator": "User defined CMake generator to use instead of default",
     "tools.cmake.cmaketoolchain:find_package_prefer_config": "Argument for the CMAKE_FIND_PACKAGE_PREFER_CONFIG",
     "tools.cmake.cmaketoolchain:toolchain_file": "Use other existing file rather than conan_toolchain.cmake one",
