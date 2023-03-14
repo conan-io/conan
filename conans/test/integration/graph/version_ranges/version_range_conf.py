423d2bdadc074cf52023181126056bf473a182ba
Rubén Rincón Blanco <rubenrb@jfrog.com>
2023-03-14 21:29:30 +0100

    Use GenConanfile

--- conans/test/integration/graph/version_ranges/version_range_conf.py
+++ conans/test/integration/graph/version_ranges/version_range_conf.py
@@ -1,3 +1,4 @@
+from conans.test.assets.genconanfile import GenConanfile
 from utils.tools import TestClient
 import textwrap
 
@@ -69,25 +70,8 @@ def test_version_range_conf_explicit_expression():
     tc.run("create base/conanfile.py --version=1.5.1")
     tc.run("create base/conanfile.py --version=2.5.0-pre")
 
-    conanfilev1 = textwrap.dedent("""
-    from conan import ConanFile
-
-    class Package(ConanFile):
-        name = "pkg"
-        version = "1.0"
-        requires = "base/[>1- <2]"
-    """)
-
-    conanfilev2 = textwrap.dedent("""
-        from conan import ConanFile
-
-        class Package(ConanFile):
-            name = "pkg"
-            version = "2.0"
-            requires = "base/[>2- <3]"
-        """)
-
-    tc.save({"v1/conanfile.py": conanfilev1, "v2/conanfile.py": conanfilev2})
+    tc.save({"v1/conanfile.py": GenConanfile("pkg", "1.0").with_requires("base/[>1- <2]"),
+             "v2/conanfile.py": GenConanfile("pkg", "2.0").with_requires("base/[>2- <3]")})
 
     tc.save({"global.conf": "core.version_ranges:resolve_prereleases=False"}, path=tc.cache.cache_folder)
     tc.run("create v1/conanfile.py")
