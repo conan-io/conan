e1e8835ce9f50d0ac1bf03ef0f614078955560a4
memsharded <james@conan.io>
2023-04-18 22:59:52 +0200

    add check

--- conans/test/integration/conan_v2/test_legacy_cpp_info.py
+++ conans/test/integration/conan_v2/test_legacy_cpp_info.py
@@ -34,7 +34,7 @@ def test_legacy_names_filenames():
     c.save({"conanfile.py": conanfile})
     c.run("create .")
     for name in ["cpp_info.names", "cpp_info.filenames", "env_info", "user_info", "cpp_info.build_modules"]:
-        assert f"'{name}' used in: pkg/1.0" in c.out
+        assert f"WARN: deprecated:     '{name}' used in: pkg/1.0" in c.out
 
     save(c.cache.new_config_path, 'core:skip_warnings=["deprecated"]')
     c.run("create .")
