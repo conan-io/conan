f0a310915da246f905dfb0a0c3efac82bea6baa9
Francisco Ramirez de Anton <franchuti688@gmail.com>
2023-05-08 12:27:56 +0200

    fix test

--- conans/test/integration/command_v2/test_output_level.py
+++ conans/test/integration/command_v2/test_output_level.py
@@ -37,11 +37,11 @@ def test_output_level():
     assert "This is a warning" in t.out
     assert "This is a error" in t.out
 
-    # Print also verbose traces
+    # Check if -v argument is equal to VERBOSE level
     t.run("create . --name foo --version 1.0 -v")
     assert "This is a trace" not in t.out
     assert "This is a debug" not in t.out
-    assert "This is a verbose" not in t.out
+    assert "This is a verbose" in t.out
     assert "This is a info" in t.out
     assert "This is a highlight" in t.out
     assert "This is a success" in t.out
