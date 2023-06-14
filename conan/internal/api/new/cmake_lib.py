7a98f3cf6e5e11b2345cf769812fd0c2eaa6a3f4
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-06-14 11:11:27 +0100

    cmake_lib template: print msvc runtime based on defined macros

--- conan/internal/api/new/cmake_lib.py
+++ conan/internal/api/new/cmake_lib.py
@@ -153,6 +153,21 @@ void {{package_name}}(){
     std::cout << "  {{name}}/{{version}}: _GLIBCXX_USE_CXX11_ABI "<< _GLIBCXX_USE_CXX11_ABI << "\n";
     #endif
 
+    // MSVC runtime
+    #if defined(_DEBUG)
+        #if defined(_MT) and defined(_DLL)
+        std::cout << "  {{name}}/{{version}}: MSVC runtime: MultiThreadedDebugDLL\n";
+        #elif defined(_MT)
+        std::cout << "  {{name}}/{{version}}: MSVC runtime: MultiThreadedDebug\n";
+        #endif
+    #else
+        #if defined(_MT) and defined(_DLL)
+        std::cout << "  {{name}}/{{version}}: MSVC runtime: MultiThreadedDLL\n";
+        #elif defined(_MT)
+        std::cout << "  {{name}}/{{version}}: MSVC runtime: MultiThreaded\n";
+        #endif
+    #endif
+
     // COMPILER VERSIONS
     #if _MSC_VER
     std::cout << "  {{name}}/{{version}}: _MSC_VER" << _MSC_VER<< "\n";
