5ed457f2a7dcdebf291ac5b46d8e07d2ff081a17
Joris Putcuyps <joris.putcuyps@gmail.com>
2023-06-18 15:43:41 +0200

    Deduplicate Version comparison unittests

--- conans/test/unittests/model/version_test.py
+++ conans/test/unittests/model/version_test.py
@@ -8,51 +8,11 @@ class VersionTest(unittest.TestCase):
 
     def test_simple(self):
         v1 = Version("1.2.3")
-        self.assertTrue(v1 == "1.2.3")
-        self.assertTrue(v1 > "1.1")
-        self.assertTrue(v1 > None)
-        self.assertTrue(v1 < "1.11")
-        self.assertTrue(v1 > "1.2")
-        self.assertTrue(v1 > "1.2.2.2")
-        self.assertTrue(v1 < "1.2.3.2")
         self.assertEqual(v1.major, "1")  # 1.X.Y
         self.assertEqual(v1.minor, "2")  # 1.2.Y
         self.assertEqual(v1.patch, "3")
         self.assertEqual(v1.pre, None)
         self.assertEqual(v1.build, None)
-
-        v1 = Version("1.2-rc1")
-        self.assertTrue(v1 < "1.2.0")
-        self.assertFalse(v1 < "1.1.9")
-
-        self.assertTrue(Version("1.2.1-dev") < Version("1.2.1"))
-        self.assertTrue(Version("1.2.1-dev") < Version("1.2.2"))
-        self.assertTrue(Version("1.2.1-dev") < Version("1.3"))
-        self.assertTrue(Version("1.2.1-dev") < Version("1.3-alpha"))
-        self.assertTrue(Version("1.2.1-dev") > Version("1.2.0"))
-        self.assertTrue(Version("1.2.1-dev") > Version("1.2"))
-        self.assertTrue(Version("1.2.1-dev") > Version("1.2-alpha"))
-
-        self.assertFalse(Version("4") < Version("4.0.0"))
-        self.assertFalse(Version("4") > Version("4.0.0"))
-        self.assertFalse(Version("4") != Version("4.0.0"))
-        self.assertTrue(Version("4") == Version("4.0.0"))
-        self.assertTrue(Version("4") <= Version("4.0.0"))
-        self.assertTrue(Version("4") >= Version("4.0.0"))
-        self.assertTrue(Version("4.0") == Version("4.0.0"))
-
-        self.assertTrue(Version("4.0.0") == Version("4.0.0"))
-        self.assertTrue(Version("4.0.1") != "4")
-        self.assertFalse(Version("4.0.0.1") == "4")
-        self.assertTrue(Version("4.0.0.1") >= "4")
-
-    def test_build_metadata_is_not_equal(self):
-        # https://github.com/conan-io/conan/issues/5900
-        self.assertNotEqual(Version("4.0.0+abc"), Version("4.0.0+xyz"))
-        # Shouldn't be an "official" order for build metadata, but as they cannot be equal
-        # the order is alphabetic
-        self.assertTrue(Version("4.0.0+abc") != Version("4.0.0+xyz"))
-        self.assertTrue(Version("4.0.0+xyz") != Version("4.0.0+abc"))
 
     def test_text(self):
         v1 = Version("master+build2")
