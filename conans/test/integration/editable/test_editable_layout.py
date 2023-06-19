a6e3b7a277b0e24b563923952368d6574462e641
Rubén Rincón <rubenrb@jfrog.com>
2023-06-19 11:10:38 +0200

    Add extra sibling test

--- conans/test/integration/editable/test_editable_layout.py
+++ conans/test/integration/editable/test_editable_layout.py
@@ -43,6 +43,44 @@ def test_editable_folders_root():
     assert f"pkg/0.1: PKG source_folder {c.current_folder}" in c.out
 
 
+def test_editable_folders_sibiling_root():
+    """ Editables with self.folders.root = ".." should work too for sibling folders
+    """
+    c = TestClient()
+    pkg = textwrap.dedent("""
+        from conan import ConanFile
+
+        class Pkg(ConanFile):
+            name = "pkg"
+            version = "0.1"
+
+            def layout(self):
+                self.folders.root = ".."
+                self.folders.build = "mybuild"
+                self.cpp.build.libdirs = ["mylibdir"]
+
+            def generate(self):
+                self.output.info(f"PKG source_folder {self.source_folder}")
+        """)
+    app = textwrap.dedent("""
+        from conan import ConanFile
+
+        class Pkg(ConanFile):
+            requires = "pkg/0.1"
+            def generate(self):
+                pkg_libdir = self.dependencies["pkg"].cpp_info.libdir
+                self.output.info(f"PKG LIBDIR {pkg_libdir}")
+        """)
+    c.save({"pkg/conanfile.py": pkg,
+            "app/conanfile.py": app})
+
+    c.run("editable add pkg")
+    c.run("install app")
+    libdir = os.path.join(c.current_folder, "mybuild", "mylibdir")
+    assert f"conanfile.py: PKG LIBDIR {libdir}" in c.out
+    assert f"pkg/0.1: PKG source_folder {c.current_folder}" in c.out
+
+
 def test_editable_matching_folder_names():
     # https://github.com/conan-io/conan/issues/14091
     client = TestClient()
