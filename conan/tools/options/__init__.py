bfededfe9ea6a320afa9514a3e69c2288c51f853
danimtb <danimanzaneque@gmail.com>
2023-07-03 12:43:22 +0200

    fix import

--- conan/tools/options/__init__.py
+++ conan/tools/options/__init__.py
@@ -1,4 +1,5 @@
 from conan.tools.options.helpers import (
     handle_common_config_options,
-    handle_common_configure_options
+    handle_common_configure_options,
+    handle_common_package_id_options
 )
