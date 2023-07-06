58222bc477ca56de2fca6e39d6d8e6a6123f1591
memsharded <james@conan.io>
2023-07-07 00:53:25 +0200

    print

--- conans/test/functional/toolchains/cmake/cmakedeps/test_cmakedeps.py
+++ conans/test/functional/toolchains/cmake/cmakedeps/test_cmakedeps.py
@@ -272,6 +272,7 @@ def test_cmake_config_version_compat_rejected(policy):
                  "CMakeLists.txt": cmakelists}, clean_first=True)
     client.run("install .")
     client.run_command('cmake .', assert_error=True)
+    print(client.out)
     assert "The following configuration files were considered but not accepted" in client.out
     assert "2.2.1" in client.out
 
