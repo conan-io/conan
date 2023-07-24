5d762970dff4857bb102d855ed0579daaaf14ce7
Uilian Ries <uilianries@gmail.com>
2023-07-24 16:44:48 +0200

    fix rootify
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/tools/gnu/makedeps.py
+++ conan/tools/gnu/makedeps.py
@@ -50,9 +50,9 @@ class GlobalContentGenerator:
     template = textwrap.dedent("""\
             {%- macro format_map_values(values) -%}
             {%- for var, value in values.items() -%}
-            {% if value|length > 0 %}
+            {%- if value|length > 0 -%}
             CONAN_{{ var.upper() }} = {{ format_list_values(value) }}
-            {% endif %}
+            {%- endif -%}
             {%- endfor -%}
             {%- endmacro -%}
 
@@ -72,7 +72,7 @@ class GlobalContentGenerator:
 
             # Aggregated global variables
 
-            {{ format_map_values(dirs) }}
+            {{- format_map_values(dirs) -}}
             {{- format_map_values(flags) -}}
             """)
 
@@ -133,7 +133,7 @@ class GlobalGenerator:
         common_variables = {
             "objects": None,
             "libs": "lib",
-            "system_libs": "system_lib",
+
             "defines": "define",
             "cflags": None,
             "cxxflags": None,
@@ -141,6 +141,7 @@ class GlobalGenerator:
             "exelinkflags": None,
             "frameworks": None,
             "requires": None,
+            "system_libs": "system_lib",
         }
         flags = {}
         for var in common_variables.keys():
@@ -315,15 +316,15 @@ class DepComponentGenerator:
             if formatted_dirs:
                 self._makeinfo.dirs.append(var)
                 var = var.replace("dirs", "_dirs")
-                formatted_dirs = self._rootify(self._root, self._dep.ref.name, formatted_dirs)
+                formatted_dirs = self._rootify(self._root, self._dep.ref.name, cppinfo_value)
                 dirs[var] = [self._conan_prefix_flag(var) + it for it in formatted_dirs]
         return dirs
 
     def _rootify(self, root, root_id, path_list):
         root_len = len(root)
-        root_with_sep = root + os.path.sep
+        root_with_sep = root + os.sep
         root_var_ref = f"$(CONAN_ROOT_{_makefy(root_id)})"
-        return [root_var_ref + path[root_len:] if path.startswith(root_with_sep) else path for path in path_list]
+        return [root_var_ref + path[root_len:].replace("\\", "/") if path.startswith(root_with_sep) else path for path in path_list]
 
     def _get_component_flags(self):
         """
@@ -426,7 +427,7 @@ class DepGenerator:
                 cppinfo_value = [var.replace('"', '\\"') for var in cppinfo_value]
             if cppinfo_value:
                 self._info.flags.append(var)
-                flags[var.upper()] = [prefix_var + it for it in cppinfo_value]
+                flags[var.upper()] = [self._conan_prefix_flag(prefix_var) + it for it in cppinfo_value]
         return flags
 
     def _get_sysroot(self, root):
@@ -474,120 +475,15 @@ class MakeDeps(object):
         """
         self._conanfile = conanfile
 
-    def _var_ref(self, var):
-        return f"$({var})"
-
-    def _var_assign_single(self, var, value):
-        return f"{var} = {value}"
-
-    def _var_assign_multi(self, var, value):
-        return " \\\n\t".join([f"{var} ="] + value)
-
     def generate(self):
         check_duplicated_generator(self, self._conanfile)
-
-        line_buffer = []
-        var_aggregates = {}
-
-        def _divert():
-            buf = list(line_buffer)
-            line_buffer.clear()
-            return buf
-
-        # Convert a name to Make-variable-friendly syntax
-        def _makeify(name):
-            return re.sub(r'[^0-9A-Z_]', '_', name.upper())
-
-        # Replace common prefixes in a path list with a variable reference
-        def _rootify(root, root_id, path_list):
-            root_len = len(root)
-            root_with_sep = root + os.path.sep
-            root_var_ref = self._var_ref(f"CONAN_ROOT_{root_id}")
-            return [root_var_ref + path[root_len:]  \
-                    if path.startswith(root_with_sep) else path for path in path_list]
-
-        # Output a variable assignment, appropriately formatted
-        def _output_var(name, item, root=None, root_id=None, prefix_var=None, blank_after=True):
-            if item is None:
-                return False
-            if type(item).__name__ in ('Component', 'SettingsItem', 'Version', 'str'):
-                value = str(item)
-                if not value:
-                    return False
-                line_buffer.append(self._var_assign_single(name, value))
-            else:
-                assert type(item) is list
-                ritem = _rootify(root, root_id, item) if root else item
-                if prefix_var:
-                    ritem = [self._var_ref(prefix_var) + i for i in ritem]
-                if len(ritem) == 0:
-                    return False
-                elif len(ritem) == 1:
-                    line_buffer.append(self._var_assign_single(name, ritem[0]))
-                else:
-                    line_buffer.append(self._var_assign_multi(name, ritem))
-            if blank_after:
-                line_buffer.append("")
-            return True
-
-        def _output_cpp_info_vars(var_id, cpp_info, root=None, root_id=None, add_to_aggregates=True, dep=None):
-
-            def _var(name, item, prefix_var=None):
-                v = f"CONAN_{name}_{var_id}"
-
-                if _output_var(v, item, root, root_id, prefix_var) and add_to_aggregates:
-                    var_aggregates.setdefault(name, []).append(self._var_ref(v))
-
-            prefix_path = root.replace("\\", "/")
-
-            # INFO: sysroot is a list with an empty string as default value
-            sysroot = cpp_info.sysroot if isinstance(cpp_info.sysroot, list) else [cpp_info.sysroot]
-            if sysroot and sysroot[0]:
-                sysroot = _get_formatted_dirs(sysroot, prefix_path, _makeify(dep.ref.name))
-                _var("SYSROOT", sysroot)
-
-            for var in ['INCLUDE_DIRS', 'LIB_DIRS', 'BIN_DIRS', 'SRC_DIRS', 'BUILD_DIRS', 'RES_DIRS', 'FRAMEWORK_DIRS']:
-                cppinfo_value = getattr(cpp_info, var.replace('_', '').lower())
-                dirs = _get_formatted_dirs(cppinfo_value, prefix_path, _makeify(dep.ref.name))
-                _var(var, dirs, f"CONAN_{var}_FLAG")
-
-            # TODO: Collect from components if possible
-            common_variables = {
-                "OBJECTS": None,
-                "LIBS": "CONAN_LIB_FLAG",
-                "SYSTEM_LIBS": "CONAN_SYSTEM_LIB_FLAG",
-                "DEFINES": "CONAN_DEFINE_FLAG",
-                "CFLAGS": None,
-                "CXXFLAGS": None,
-                "SHAREDLINKFLAGS": None,
-                "EXELINKFLAGS": None,
-                "FRAMEWORKS": None,
-                "REQUIRES": None,
-            }
-
-            for var, prefix_var in common_variables.items():
-                cppinfo_value = getattr(cpp_info, var.lower())
-                # Use component cpp_info info when does not provide any value
-                if not cppinfo_value and hasattr(cpp_info, "components"):
-                    cppinfo_value = [self._var_ref(f"CONAN_{var}_{_makeify(var_id)}_{_makeify(name)}") for name, obj in cpp_info.components.items() if getattr(obj, var.lower())]
-                    # avoid repeating same prefix twice
-                    prefix_var = None
-                if "FLAGS" in var:
-                    cppinfo_value = [var.replace('"', '\\"') for var in cppinfo_value]
-                _var(var, cppinfo_value, prefix_var)
-
-        # Top-of-file banner
-        line_buffer.append(self._title)
-        preamble = _divert()
-
-        body = []
-        dep_name_list = []
 
         host_req = self._conanfile.dependencies.host
         build_req = self._conanfile.dependencies.build  # tool_requires
         test_req = self._conanfile.dependencies.test
 
-        require_list = []
+        content_buffer = self._title
+        deps_buffer = ""
 
         # Filter the build_requires not activated for any requirement
         dependencies = [tup for tup in list(host_req.items()) + list(build_req.items()) + list(test_req.items()) if not tup[0].build]
@@ -597,70 +493,15 @@ class MakeDeps(object):
         for require, dep in dependencies:
             # Require is not used at the moment, but its information could be used,
             # and will be used in Conan 2.0
-
             if require.build:
                 continue
 
-            require_list.append(require)
             dep_gen = DepGenerator(self._conanfile, require, dep)
-            content = dep_gen.generate()
-            save(self._conanfile, path=f"{require.ref.name}.mk", content=content)
-
-            dep_id = _makeify(require.ref.name)
-            dep_name_list.append(require.ref.name)
-
-            root = dep.recipe_folder if dep.package_folder is None else dep.package_folder
-
-            line_buffer.extend([
-                "",
-                f"# {require.ref.name}/{require.ref.version}",
-                f"# (" + ("direct" if require.direct else "indirect") + " dependency)",
-                ""
-            ])
-
-            _output_var(f"CONAN_NAME_{dep_id}", require.ref.name)
-            _output_var(f"CONAN_VERSION_{dep_id}", require.ref.version)
-            _output_var(f"CONAN_ROOT_{dep_id}", root)
-
-            _output_cpp_info_vars(dep_id, dep.cpp_info, root, dep_id, True, dep)
-
-            dep_section = _divert()
-            comp_name_list = []
-
-            for comp_name, component in dep.cpp_info.get_sorted_components().items():
-                line_buffer.append(f"# {require.ref.name}::{comp_name}")
-                line_buffer.append("")
-                comp_id = _makeify(comp_name)
-                comp_name_list.append(comp_name)
-                _output_cpp_info_vars(f"{dep_id}_{comp_id}", component, root, dep_id, False, dep)
-
-            comp_section = _divert()
-
-            line_buffer.extend(dep_section)
-            _output_var(f"CONAN_COMPONENTS_{dep_id}", comp_name_list)
-            line_buffer.extend(comp_section)
-
-            body.extend(_divert())
-
             make_infos.append(dep_gen.make_global_info())
+            deps_buffer += dep_gen.generate()
 
         glob_gen = GlobalGenerator(self._conanfile, make_infos)
-        content = glob_gen.deps_generate() + "\n" + glob_gen.generate()
-        save(self._conanfile, path="global.mk", content=content)
+        content_buffer += glob_gen.deps_generate() + deps_buffer + "\n" + glob_gen.generate()
 
-        assert not line_buffer
-        line_buffer.extend([
-            "",
-            "# Aggregated global values",
-            ""
-        ])
-        for var_name, var_list in var_aggregates.items():
-            _output_var(f"CONAN_{var_name}", var_list)
-        body.extend(_divert())
-
-        line_buffer.extend(preamble)
-        _output_var("CONAN_DEPS", dep_name_list)
-        line_buffer.extend(body)
-
-        save(self._conanfile, CONAN_MAKEFILE_FILENAME, "\n".join(line_buffer))
+        save(self._conanfile, CONAN_MAKEFILE_FILENAME, content_buffer)
         self._conanfile.output.info(f"Generated {CONAN_MAKEFILE_FILENAME}")
