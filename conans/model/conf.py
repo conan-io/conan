2c5da744b372aa355c8810ac64c268aacee21d55
Rubén Rincón <rubenrb@jfrog.com>
2023-08-17 14:26:53 +0200

    Add tools.files.download:verify

--- conans/model/conf.py
+++ conans/model/conf.py
@@ -69,6 +69,7 @@ BUILT_IN_CONFS = {
     "tools.deployer:symlinks": "Set to False to disable deployers copying symlinks",
     "tools.files.download:retry": "Number of retries in case of failure when downloading",
     "tools.files.download:retry_wait": "Seconds to wait between download attempts",
+    "tools.files.download:verify": "Force enable/disable of verification checks for downloaded files",
     "tools.gnu:make_program": "Indicate path to make program",
     "tools.gnu:define_libcxx11_abi": "Force definition of GLIBCXX_USE_CXX11_ABI=1 for libstdc++11",
     "tools.gnu:pkg_config": "Path to pkg-config executable used by PkgConfig build helper",
