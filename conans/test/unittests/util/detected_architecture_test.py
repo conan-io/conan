ee197b80e239cf2dfe1c45d999a28e0786eaf2ac
memsharded <james@conan.io>
2023-08-29 23:22:12 +0200

    fixes

--- conans/test/unittests/util/detected_architecture_test.py
+++ conans/test/unittests/util/detected_architecture_test.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-
-
 import mock
 import unittest
 
 from parameterized import parameterized
 
-from conans.client.conf.detect import _detected_architecture
+from conan.internal.api.detect_api import detect_architecture
 
 
 class DetectedArchitectureTest(unittest.TestCase):
@@ -36,22 +31,22 @@ class DetectedArchitectureTest(unittest.TestCase):
     def test_various(self, mocked_machine, expected_arch):
 
         with mock.patch("platform.machine", mock.MagicMock(return_value=mocked_machine)):
-            self.assertEqual(expected_arch, _detected_architecture(), "given '%s' expected '%s'" % (mocked_machine, expected_arch))
+            self.assertEqual(expected_arch, detect_architecture(), "given '%s' expected '%s'" % (mocked_machine, expected_arch))
 
     def test_aix(self):
         with mock.patch("platform.machine", mock.MagicMock(return_value='00FB91F44C00')),\
                 mock.patch("platform.processor", mock.MagicMock(return_value='powerpc')),\
                 mock.patch("platform.system", mock.MagicMock(return_value='AIX')),\
-                mock.patch("conans.client.conf.detect._get_aix_conf", mock.MagicMock(return_value='32')),\
+                mock.patch("conan.internal.api.detect_api._get_aix_conf", mock.MagicMock(return_value='32')),\
                 mock.patch('subprocess.check_output', mock.MagicMock(return_value='7.1.0.0')):
-            self.assertEqual('ppc32', _detected_architecture())
+            self.assertEqual('ppc32', detect_architecture())
 
         with mock.patch("platform.machine", mock.MagicMock(return_value='00FB91F44C00')),\
                 mock.patch("platform.processor", mock.MagicMock(return_value='powerpc')),\
                 mock.patch("platform.system", mock.MagicMock(return_value='AIX')),\
-                mock.patch("conans.client.conf.detect._get_aix_conf", mock.MagicMock(return_value='64')),\
+                mock.patch("conan.internal.api.detect_api._get_aix_conf", mock.MagicMock(return_value='64')),\
                 mock.patch('subprocess.check_output', mock.MagicMock(return_value='7.1.0.0')):
-            self.assertEqual('ppc64', _detected_architecture())
+            self.assertEqual('ppc64', detect_architecture())
 
     def test_solaris(self):
         with mock.patch("platform.machine", mock.MagicMock(return_value='sun4v')),\
@@ -59,14 +54,14 @@ class DetectedArchitectureTest(unittest.TestCase):
                 mock.patch("platform.system", mock.MagicMock(return_value='SunOS')),\
                 mock.patch("platform.architecture", mock.MagicMock(return_value=('64bit', 'ELF'))),\
                 mock.patch("platform.release", mock.MagicMock(return_value='5.11')):
-            self.assertEqual('sparcv9', _detected_architecture())
+            self.assertEqual('sparcv9', detect_architecture())
 
         with mock.patch("platform.machine", mock.MagicMock(return_value='i86pc')),\
                 mock.patch("platform.processor", mock.MagicMock(return_value='i386')),\
                 mock.patch("platform.system", mock.MagicMock(return_value='SunOS')),\
                 mock.patch("platform.architecture", mock.MagicMock(return_value=('64bit', 'ELF'))),\
                 mock.patch("platform.release", mock.MagicMock(return_value='5.11')):
-            self.assertEqual('x86_64', _detected_architecture())
+            self.assertEqual('x86_64', detect_architecture())
 
     @parameterized.expand([
         ["E1C+", "e2k-v4"],
@@ -83,4 +78,4 @@ class DetectedArchitectureTest(unittest.TestCase):
     def test_e2k(self, processor, expected_arch):
         with mock.patch("platform.machine", mock.MagicMock(return_value='e2k')), \
                 mock.patch("platform.processor", mock.MagicMock(return_value=processor)):
-            self.assertEqual(expected_arch, _detected_architecture())
+            self.assertEqual(expected_arch, detect_architecture())
