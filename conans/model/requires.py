c26ff32e4aaedd75947bb2b0b40bad2a1b7b76a5
memsharded <james@conan.io>
2023-09-06 18:57:14 +0200

    fix tests

--- conans/model/requires.py
+++ conans/model/requires.py
@@ -32,7 +32,7 @@ class Requirement:
         self.overriden_ref = None  # to store if the requirement has been overriden (store old ref)
         self.override_ref = None  # to store if the requirement has been overriden (store new ref)
         self.is_test = test  # to store that it was a test, even if used as regular requires too
-        self.skip = None
+        self.skip = False
 
     @property
     def files(self):  # require needs some files in dependency package
