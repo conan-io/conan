ff664b388526570a2c98e53e429ca16e31a40f1b
memsharded <james@conan.io>
2023-10-15 00:49:21 +0200

    wip

--- conan/api/subapi/cache.py
+++ conan/api/subapi/cache.py
@@ -2,6 +2,7 @@ import json
 import os
 import shutil
 import tarfile
+from io import BytesIO
 
 from conan.api.model import PackagesList
 from conan.api.output import ConanOutput
@@ -12,7 +13,7 @@ from conans.errors import ConanException
 from conans.model.package_ref import PkgReference
 from conans.model.recipe_ref import RecipeReference
 from conans.util.dates import revision_timestamp_now
-from conans.util.files import rmdir, gzopen_without_timestamps, save, load
+from conans.util.files import rmdir, gzopen_without_timestamps
 
 
 class CacheAPI:
@@ -117,51 +118,61 @@ class CacheAPI:
             tgz = gzopen_without_timestamps(name, mode="w", fileobj=tgz_handle)
             for ref, ref_bundle in package_list.refs().items():
                 ref_layout = app.cache.recipe_layout(ref)
-                base_folder = ref_layout.base_folder
-                folder = os.path.relpath(base_folder, cache_folder)
-                ref_bundle["folder"] = folder
-                out.info(f"Saving {ref}: {folder}")
-                tgz.add(os.path.join(cache_folder, folder), folder, recursive=True)
+                recipe_folder = os.path.relpath(ref_layout.base_folder, cache_folder)
+                ref_bundle["recipe_folder"] = recipe_folder
+                out.info(f"Saving {ref}: {recipe_folder}")
+                tgz.add(os.path.join(cache_folder, recipe_folder), recipe_folder, recursive=True)
                 for pref, pref_bundle in package_list.prefs(ref, ref_bundle).items():
                     pref_layout = app.cache.pkg_layout(pref)
                     pkg_folder = pref_layout.package()
-                    # FIXME: Missing metadata
                     folder = os.path.relpath(pkg_folder, cache_folder)
                     pref_bundle["package_folder"] = folder
                     out.info(f"Saving {pref}: {folder}")
                     tgz.add(os.path.join(cache_folder, folder), folder, recursive=True)
+                    if os.path.exists(pref_layout.metadata()):
+                        metadata_folder = os.path.relpath(pref_layout.metadata(), cache_folder)
+                        pref_bundle["metadata_folder"] = metadata_folder
+                        out.info(f"Saving {pref} metadata: {folder}")
+                        tgz.add(os.path.join(cache_folder, metadata_folder), metadata_folder,
+                                recursive=True)
             serialized = json.dumps(package_list.serialize(), indent=2)
-            manifest_path = os.path.join(cache_folder, "pkglist.json")
-            save(manifest_path, serialized)
-            tgz.add(manifest_path, "pkglist.json")
-            os.remove(manifest_path)  # remove file from the cache
+            info = tarfile.TarInfo(name="pkglist.json")
+            data = serialized.encode('utf-8')
+            info.size = len(data)
+            tgz.addfile(tarinfo=info, fileobj=BytesIO(data))
             tgz.close()
 
     def restore(self, path):
         with open(path, mode='rb') as file_handler:
             the_tar = tarfile.open(fileobj=file_handler)
+            fileobj = the_tar.extractfile("pkglist.json")
+            pkglist = fileobj.read()
             the_tar.extractall(path=self.conan_api.cache_folder)
             the_tar.close()
 
         out = ConanOutput()
-        manifest_path = os.path.join(self.conan_api.cache_folder, "pkglist.json")
-        package_list = PackagesList.deserialize(json.loads(load(manifest_path)))
-        os.remove(manifest_path)  # remove file from the cache
+        package_list = PackagesList.deserialize(json.loads(pkglist))
         cache = ClientCache(self.conan_api.cache_folder)
         for ref, ref_bundle in package_list.refs().items():
             ref.timestamp = revision_timestamp_now()
             recipe_layout = cache.get_or_create_ref_layout(ref)
-            folder = ref_bundle["folder"]
+            recipe_folder = ref_bundle["recipe_folder"]
             rel_path = os.path.relpath(recipe_layout.base_folder, cache.cache_folder)
-            assert rel_path == folder, f"{rel_path}!={folder}"
-            out.info(f"Put: {ref} in {folder}")
+            assert rel_path == recipe_folder, f"{rel_path}!={recipe_folder}"
+            out.info(f"Put: {ref} in {recipe_folder}")
             for pref, pref_bundle in package_list.prefs(ref, ref_bundle).items():
                 pref.timestamp = revision_timestamp_now()
                 pkg_layout = cache.get_or_create_pkg_layout(pref)
-                folder = pref_bundle["package_folder"]
-                out.info(f"Restore: {pref} in {folder}")
-                shutil.move(os.path.join(cache.cache_folder, folder),
-                            os.path.join(cache.cache_folder, pkg_layout.package()))
+                pkg_folder = pref_bundle["package_folder"]
+                out.info(f"Restore: {pref} in {pkg_folder}")
+                # We need to put the package in the final location in the cache
+                shutil.move(os.path.join(cache.cache_folder, pkg_folder), pkg_layout.package())
+                metadata_folder = pref_bundle.get("metadata_folder")
+                if metadata_folder:
+                    out.info(f"Restore: {pref} metadata in {metadata_folder}")
+                    # We need to put the package in the final location in the cache
+                    shutil.move(os.path.join(cache.cache_folder, metadata_folder),
+                                pkg_layout.metadata())
         return package_list
 
 
--- conans/test/integration/command_v2/test_cache_save_restore.py
+++ conans/test/integration/command_v2/test_cache_save_restore.py
@@ -3,6 +3,7 @@ import shutil
 
 from conans.test.assets.genconanfile import GenConanfile
 from conans.test.utils.tools import TestClient
+from conans.util.files import save, load
 
 
 def test_cache_save_restore():
@@ -14,7 +15,29 @@ def test_cache_save_restore():
     c.run("cache save cache.tgz pkg/*:* ")
     cache_path = os.path.join(c.current_folder, "cache.tgz")
     assert os.path.exists(cache_path)
+    _validate_restore(cache_path)
 
+
+def test_cache_save_downloaded_restore():
+    """ what happens if we save packages downloaded from server, not
+    created
+    """
+    c = TestClient(default_server_user=True)
+    c.save({"conanfile.py": GenConanfile().with_settings("os")})
+    c.run("create . --name=pkg --version=1.0 -s os=Linux")
+    c.run("create . --name=pkg --version=1.1 -s os=Linux")
+    c.run("create . --name=other --version=2.0 -s os=Linux")
+    c.run("upload * -r=default -c")
+    c.run("remove * -c")
+    c.run("download *:* -r=default --metadata=*")
+    c.run("cache save cache.tgz pkg/*:* ")
+    cache_path = os.path.join(c.current_folder, "cache.tgz")
+    assert os.path.exists(cache_path)
+
+    _validate_restore(cache_path)
+
+
+def _validate_restore(cache_path):
     c2 = TestClient()
     # Create a package in the cache to check put doesn't interact badly
     c2.save({"conanfile.py": GenConanfile().with_settings("os")})
@@ -26,3 +49,44 @@ def test_cache_save_restore():
     assert "pkg/1.0" in c2.out
     assert "pkg/1.1" in c2.out
     assert "other/2.0" not in c2.out
+
+    # Restore again, just in case
+    c2.run("cache restore cache.tgz")
+    c2.run("list *:*#*")
+    assert "pkg2/3.0" in c2.out
+    assert "pkg/1.0" in c2.out
+    assert "pkg/1.1" in c2.out
+    assert "other/2.0" not in c2.out
+
+
+def test_cache_save_restore_metadata():
+    c = TestClient()
+    c.save({"conanfile.py": GenConanfile().with_settings("os")})
+    c.run("create . --name=pkg --version=1.0 -s os=Linux")
+    pid = c.created_package_id("pkg/1.0")
+    # Add some metadata
+    c.run("cache path pkg/1.0 --folder=metadata")
+    metadata_path = str(c.stdout).strip()
+    myfile = os.path.join(metadata_path, "logs", "mylogs.txt")
+    save(myfile, "mylogs!!!!")
+    c.run(f"cache path pkg/1.0:{pid} --folder=metadata")
+    pkg_metadata_path = str(c.stdout).strip()
+    myfile = os.path.join(pkg_metadata_path, "logs", "mybuildlogs.txt")
+    save(myfile, "mybuildlogs!!!!")
+
+    c.run("cache save cache.tgz pkg/*:* ")
+    cache_path = os.path.join(c.current_folder, "cache.tgz")
+    assert os.path.exists(cache_path)
+
+    # restore and check
+    c2 = TestClient()
+    shutil.copy2(cache_path, c2.current_folder)
+    c2.run("cache restore cache.tgz")
+    c2.run("cache path pkg/1.0 --folder=metadata")
+    metadata_path = str(c2.stdout).strip()
+    myfile = os.path.join(metadata_path, "logs", "mylogs.txt")
+    assert load(myfile) == "mylogs!!!!"
+    c2.run(f"cache path pkg/1.0:{pid} --folder=metadata")
+    pkg_metadata_path = str(c2.stdout).strip()
+    myfile = os.path.join(pkg_metadata_path, "logs", "mybuildlogs.txt")
+    assert load(myfile) == "mybuildlogs!!!!"
