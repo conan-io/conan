8a4889cfc1f719759bee75ab6792a7193b260730
memsharded <james@conan.io>
2023-11-03 13:38:41 +0100

    avoid overwrite user CMakePresets

--- conans/test/integration/toolchains/cmake/test_cmaketoolchain.py
+++ conans/test/integration/toolchains/cmake/test_cmaketoolchain.py
@@ -6,6 +6,7 @@ import textwrap
 import pytest
 from mock import mock
 
+import conans.test.unittests.util.output_test
 from conan.tools.cmake.presets import load_cmake_presets
 from conans.test.assets.genconanfile import GenConanfile
 from conans.test.utils.tools import TestClient
@@ -1176,3 +1177,13 @@ def test_extra_flags():
     assert 'string(APPEND CONAN_C_FLAGS " extra_cflags cflags")' in toolchain
     assert 'string(APPEND CONAN_SHARED_LINKER_FLAGS " extra_sharedlinkflags sharedlinkflags")' in toolchain
     assert 'string(APPEND CONAN_EXE_LINKER_FLAGS " extra_exelinkflags exelinkflags")' in toolchain
+
+
+def test_avoid_ovewrite_user_cmakepresets():
+    # https://github.com/conan-io/conan/issues/15052
+    c = TestClient()
+    c.save({"conanfile.txt": "",
+            "CMakePresets.json": "{}"})
+    c.run('install . -g CMakeToolchain', assert_error=True)
+    assert "Error in generator 'CMakeToolchain': Existing CMakePresets.json not generated" in c.out
+    assert "Use --output-folder or define a 'layout' to avoid collision" in c.out
