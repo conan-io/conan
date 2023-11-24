04a3fa13ac2302488e43119d451afd09ec6d52dd
memsharded <james@conan.io>
2023-11-24 15:39:04 +0100

    proposal for deploy() feature

--- conan/internal/deploy.py
+++ conan/internal/deploy.py
@@ -5,6 +5,7 @@ from conan.internal.cache.home_paths import HomePaths
 from conan.api.output import ConanOutput
 from conans.client.loader import load_python_file
 from conans.errors import ConanException
+from conans.model.recipe_ref import ref_matches
 from conans.util.files import rmdir, mkdir
 
 
@@ -37,8 +38,18 @@ def _find_deployer(d, cache_deploy_folder):
     raise ConanException(f"Cannot find deployer '{d}'")
 
 
-def do_deploys(conan_api, graph, deploy, deploy_folder):
+def do_deploys(conan_api, graph, deploy, deploy_pkg, deploy_folder):
     mkdir(deploy_folder)
+    # handle the recipe deploy()
+    if deploy_pkg:
+        for node in graph.ordered_iterate():
+            conanfile = node.conanfile
+            if not conanfile.ref or not any(ref_matches(conanfile.ref, p, None) for p in deploy_pkg):
+                continue
+            if hasattr(conanfile, "deploy"):
+                conanfile.output.info("Executing deploy()")
+                conanfile.deploy_folder = deploy_folder
+                conanfile.deploy()
     # Handle the deploys
     cache = HomePaths(conan_api.cache_folder)
     for d in deploy or []:
