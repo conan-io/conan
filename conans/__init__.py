1dba8e0af2f676ec58f4dec3ca232566bad2c3ef
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2023-11-28 20:44:42 +0000

    Read version from conans.__init__

--- conans/__init__.py
+++ conans/__init__.py
@@ -1,7 +1,5 @@
-import importlib.metadata
-
 CHECKSUM_DEPLOY = "checksum_deploy"  # Only when v2
 REVISIONS = "revisions"  # Only when enabled in config, not by default look at server_launcher.py
 OAUTH_TOKEN = "oauth_token"
 
-__version__ = importlib.metadata.version("conan")
+__version__ = '2.0.15-dev'
