1227f3da56d327e7619c78716386664278c97db0
czoido <mrgalleta@gmail.com>
2023-11-30 08:00:26 +0100

    test ctest

--- conans/test/functional/toolchains/cmake/test_cmake_toolchain.py
+++ conans/test/functional/toolchains/cmake/test_cmake_toolchain.py
@@ -1364,23 +1364,36 @@ def test_add_buildenv_to_configure_preset():
         from conan import ConanFile
         from conan.tools.files import chdir, save
         class Tool(ConanFile):
-            name = "mytool"
             version = "0.1"
             settings = "os", "compiler", "arch", "build_type"
             def package(self):
                 with chdir(self, self.package_folder):
-                    save(self, "bin/mytool.bat", f"@echo off\n running: {self.name}/{self.version} {self.settings.build_type}")
-                    save(self, "bin/mytool.sh", f"echo running: {self.name}/{self.version} {self.settings.build_type}")
-                    os.chmod("bin/mytool.sh", 0o777)
+                    save(self, f"bin/{{self.name}}.bat", f"@echo off\n running: {{self.name}}/{{self.version}} {{self.settings.build_type}}")
+                    save(self, f"bin/{{self.name}}.sh", f"echo running: {{self.name}}/{{self.version}} {{self.settings.build_type}}")
+                    os.chmod(f"bin/{{self.name}}.sh", 0o777)
             def package_info(self):
-                self.buildenv_info.define("MYCUSTOMVAR", "MYCUSTOMVARVALUE")
+                self.buildenv_info.define("MY_BUILD_VAR", "MY_BUILDVAR_VALUE")
+                {}
         """)
 
     consumer = textwrap.dedent("""
         [tool_requires]
         mytool/0.1
+        [test_requires]
+        mytesttool/0.1
         [layout]
         cmake_layout
+    """)
+
+    test_env = textwrap.dedent("""
+        #include <cstdlib>
+        #include <iostream>
+        int main() {
+            if (const char* env_var = std::getenv("MY_RUNVAR")) {
+                return 0; // passes
+            }
+            return 1; // fails
+        }
     """)
 
     cmakelists = textwrap.dedent("""
@@ -1392,24 +1405,56 @@ def test_add_buildenv_to_configure_preset():
             set(MYTOOL_SCRIPT "mytool.sh")
         endif()
         add_custom_target(run_mytool COMMAND ${MYTOOL_SCRIPT})
-        message("MYCUSTOMVAR:$ENV{MYCUSTOMVAR}")
+        # build var should be available at configure
+        set(MY_BUILD_VAR $ENV{MY_BUILD_VAR})
+        if (MY_BUILD_VAR)
+            message("MY_BUILD_VAR:${MY_BUILD_VAR}")
+        else()
+            message("MY_BUILD_VAR NOT FOUND")
+        endif()
+        # run var should not be available at configure, just when testing
+        set(MY_RUNVAR $ENV{MY_RUNVAR})
+        if (MY_RUNVAR)
+            message("MY_RUNVAR:${MY_RUNVAR}")
+        else()
+            message("MY_RUNVAR NOT FOUND")
+        endif()
+        enable_testing()
+        add_executable(test_env test_env.cpp)
+        add_test(NAME TestRunEnv COMMAND test_env)
     """)
 
-    c.save({"tool.py": tool,
+    c.save({"tool.py": tool.format(""),
+            "test_tool.py": tool.format('self.runenv_info.define("MY_RUNVAR", "MY_RUNVAR_VALUE")'),
             "conanfile.txt": consumer,
-            "CMakeLists.txt": cmakelists})
+            "CMakeLists.txt": cmakelists,
+            "test_env.cpp": test_env})
 
-    c.run("create tool.py")
-    c.run("create tool.py -s build_type=Debug")
+    c.run("create tool.py --name=mytool")
+    c.run("create tool.py --name=mytool -s build_type=Debug")
+
+    c.run("create test_tool.py --name=mytesttool")
+    c.run("create test_tool.py --name=mytesttool -s build_type=Debug")
+
     c.run("install . -g CMakeToolchain -g CMakeDeps")
     c.run("install . -g CMakeToolchain -g CMakeDeps -s:a build_type=Debug")
 
+    # test that the buildenv is correctly injected to configure and build steps
+    # that the runenv is not injected to configure, but it is when running tests
     c.run_command("cmake --preset conan-release")
-    assert "MYCUSTOMVAR:MYCUSTOMVARVALUE" in c.out
-    c.run_command("cmake --build --preset conan-release --target run_mytool")
+    assert "MY_BUILD_VAR:MY_BUILDVAR_VALUE" in c.out
+    assert "MY_RUNVAR NOT FOUND" in c.out
+    c.run_command("cmake --build --preset conan-release --target run_mytool --target test_env")
     assert "running: mytool/0.1 Release" in c.out
 
+    c.run_command("ctest --preset conan-release")
+    assert "tests passed" in c.out
+
     c.run_command("cmake --preset conan-debug")
-    assert "MYCUSTOMVAR:MYCUSTOMVARVALUE" in c.out
-    c.run_command("cmake --build --preset conan-debug --target run_mytool")
+    assert "MY_BUILD_VAR:MY_BUILDVAR_VALUE" in c.out
+    assert "MY_RUNVAR NOT FOUND" in c.out
+    c.run_command("cmake --build --preset conan-debug --target run_mytool --target test_env")
     assert "running: mytool/0.1 Debug" in c.out
+
+    c.run_command("ctest --preset conan-debug")
+    assert "tests passed" in c.out
