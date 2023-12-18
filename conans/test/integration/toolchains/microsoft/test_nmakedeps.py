18e1f844a93966658ece778f0631f31e606fcdbe
Rubén Rincón Blanco <rubenrb@jfrog.com>
2023-12-18 11:16:04 +0100

    Backport NMakeDeps quoting bugfix (#15288)
    Try to address NMakeDeps quoting issues (#15140)
    
    * Better spacing and quotes for defines in NMakeDeps
    
    * No need to escape parenthesis in defines
    
    * Fix tests
    
    * Follow proper syntax definition
    
    * Add numeric test
    
    * Ensure functional tests cover the quoting
    
    * Add test for space in values
    
    * fix values with whitespace in nmaketoolchain defines
    
    * fix nmakedeps and test
    
    * simplify
    
    * review
    
    ---------
    
    Co-authored-by: danimtb <danimanzaneque@gmail.com>

--- conans/test/integration/toolchains/microsoft/test_nmakedeps.py
+++ conans/test/integration/toolchains/microsoft/test_nmakedeps.py
@@ -28,7 +28,10 @@ def test_nmakedeps():
                 self.cpp_info.components["pkg-3"].defines = ["TEST_DEFINITION3="]
                 self.cpp_info.components["pkg-3"].requires = ["pkg-1", "pkg-2"]
                 self.cpp_info.components["pkg-4"].libs = ["pkg-4"]
-                self.cpp_info.components["pkg-4"].defines = ["TEST_DEFINITION4=foo"]
+                self.cpp_info.components["pkg-4"].defines = ["TEST_DEFINITION4=foo",
+                                                            "TEST_DEFINITION5=__declspec(dllexport)",
+                                                            "TEST_DEFINITION6=foo bar",
+                                                            "TEST_DEFINITION7=7"]
     """)
     client.save({"conanfile.py": conanfile})
     client.run("create . -s arch=x86_64")
@@ -37,8 +40,11 @@ def test_nmakedeps():
     bat_file = client.load("conannmakedeps.bat")
     # Checking that defines are added to CL
     for flag in (
-        r"/DTEST_DEFINITION1", r"/DTEST_DEFINITION2#0",
-        r"/DTEST_DEFINITION3#", r'/DTEST_DEFINITION4#\\"foo\\"',
+        '/D"TEST_DEFINITION1"', '/D"TEST_DEFINITION2#0"',
+        '/D"TEST_DEFINITION3#"', '/D"TEST_DEFINITION4#"foo""',
+        '/D"TEST_DEFINITION5#"__declspec\(dllexport\)""',
+        '/D"TEST_DEFINITION6#"foo bar""',
+        '/D"TEST_DEFINITION7#7"'
     ):
         assert re.search(fr'set "CL=%CL%.*\s{flag}(?:\s|")', bat_file)
     # Checking that libs and system libs are added to _LINK_
