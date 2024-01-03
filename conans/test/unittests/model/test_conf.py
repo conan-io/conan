7e8c43a27e400b1dd8ff14335261a0ccee57f371
czoido <mrgalleta@gmail.com>
2024-01-03 11:59:03 +0100

    raise if bool on int check

--- conans/test/unittests/model/test_conf.py
+++ conans/test/unittests/model/test_conf.py
@@ -220,6 +220,7 @@ def test_conf_get_check_type_and_default():
         zlib:user.company.check:shared_str="False"
         zlib:user.company.check:static_str=off
         user.company.list:newnames+=myname
+        core.download:parallel=True
     """)
     c = ConfDefinition()
     c.loads(text)
@@ -238,6 +239,10 @@ def test_conf_get_check_type_and_default():
     assert c.get("zlib:user.company.check:static_str") == "off"
     assert c.get("zlib:user.company.check:static_str", check_type=bool) is False  # smart conversion
     assert c.get("user.company.list:newnames") == ["myname"]  # Placeholder is removed
+    with pytest.raises(ConanException) as exc_info:
+        c.get("core.download:parallel", check_type=int)
+    assert ("[conf] core.download:parallel must be a int-like object. "
+            "The value 'True' introduced is a bool object") in str(exc_info.value)
 
 
 def test_conf_pop():
