56e981c7ef31c3c8ee1f41b0a79f1f687c5d4e42
Rubén Rincón Blanco <rubenrb@jfrog.com>
2024-01-10 13:44:34 +0100

    Bugfix libcxx detection when using CC/CXX (#15418)
    * Bugfix libxx detection when using CC/CXX
    
    * Fix tests
    
    * Fix tests
    
    * Fix tests
    
    * Fix tests
    
    * Make compiler_exe in detect_libcxx optional
    
    * Create new version not to break existing users
    
    * Prettier error when jinja2 rendering fails
    
    * Update conan/internal/api/detect_api.py
    
    Co-authored-by: Carlos Zoido <mrgalleta@gmail.com>
    
    * Add exception msg and profile path to output
    
    ---------
    
    Co-authored-by: Carlos Zoido <mrgalleta@gmail.com>

--- conans/test/unittests/client/conf/detect/test_gcc_compiler.py
+++ conans/test/unittests/client/conf/detect/test_gcc_compiler.py
@@ -13,6 +13,7 @@ class GCCCompilerTestCase(unittest.TestCase):
     def test_detect_gcc_10(self, version):
         with mock.patch("platform.system", return_value="Linux"):
             with mock.patch("conan.internal.api.detect_api.detect_runner", return_value=(0, version)):
-                compiler, installed_version = _gcc_compiler()
+                compiler, installed_version, compiler_exe = _gcc_compiler()
         self.assertEqual(compiler, 'gcc')
         self.assertEqual(installed_version, version)
+        self.assertEqual(compiler_exe, 'gcc')
