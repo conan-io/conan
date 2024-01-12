2ba740417e166dbc9dd674da890d60ca9139cea1
memsharded <james@conan.io>
2024-01-12 18:31:32 +0100

    wip

--- conans/test/functional/test_oss_recipes_repo.py
+++ conans/test/functional/test_oss_recipes_repo.py
@@ -2,8 +2,10 @@ import os
 import textwrap
 
 from conans.test.assets.cmake import gen_cmakelists
+from conans.test.assets.genconanfile import GenConanfile
 from conans.test.assets.sources import gen_function_cpp, gen_function_h
 from conans.test.utils.file_server import TestFileServer
+from conans.test.utils.scm import create_local_git_repo, git_add_changes_commit
 from conans.test.utils.test_files import temp_folder
 from conans.test.utils.tools import TestClient, zipdir
 from conans.util.files import save_files, sha256sum
@@ -110,3 +112,43 @@ class TestInRepo:
         path = os.path.join(repo_folder, "recipes/pkg/all")
         c.run(f'create "{path}" --version=0.1')
         assert "pkg/0.1: Created package" in c.out
+
+
+class TestOssRecipeGit:
+    def test_conan_new_oss_recipe(self):
+        # save repo
+        config_yml = textwrap.dedent("""\
+            versions:
+              "0.1":
+                folder: all
+            """)
+        conanfile = GenConanfile("pkg")
+        files = {"recipes/pkg/config.yml": config_yml,
+                 "recipes/pkg/all/conanfile.py": str(conanfile)}
+        url_folder, commit = create_local_git_repo(files)
+
+        c = TestClient()
+        c.run(f'remote add local "{url_folder}" --type=oss-recipes-git')
+        c.run("list *:* -r=local")
+        assert "Remote local not cloned yet" in c.out
+        assert "pkg/0.1" in c.out
+        c.run("list *:* -r=local")
+        assert "Remote local not cloned yet" not in c.out
+        assert "pkg/0.1" in c.out
+
+        config_yml = textwrap.dedent("""\
+            versions:
+              "0.1":
+                folder: all
+              "0.2":
+                folder: all
+            """)
+        save_files(url_folder, {"recipes/pkg/config.yml": config_yml})
+        git_add_changes_commit(url_folder)
+        c.run("list *:* -r=local")
+        assert "pkg/0.1" in c.out
+        assert "pkg/0.2" not in c.out
+        c.run("remote pull local")
+        c.run("list *:* -r=local")
+        assert "pkg/0.1" in c.out
+        assert "pkg/0.2" in c.out
--- conans/test/integration/remote/test_oss_recipes_repo.py
+++ conans/test/integration/remote/test_oss_recipes_repo.py
@@ -5,7 +5,6 @@ import textwrap
 import pytest
 
 from conans.test.assets.genconanfile import GenConanfile
-from conans.test.utils.scm import create_local_git_repo, git_add_changes_commit
 from conans.test.utils.test_files import temp_folder
 from conans.test.utils.tools import TestClient
 from conans.util.files import mkdir, save, save_files
@@ -258,43 +257,3 @@ class TestErrorsUx:
         c.run(f"remote add local '{folder}' --type=oss-recipes")
         c.run("install --requires=zlib/[*] --build missing", assert_error=True)
         assert "NameError: name 'ConanFile' is not defined" in c.out
-
-
-class TestOssRecipeGit:
-    def test_conan_new_oss_recipe(self):
-        # save repo
-        config_yml = textwrap.dedent("""\
-            versions:
-              "0.1":
-                folder: all
-            """)
-        conanfile = GenConanfile("pkg")
-        files = {"recipes/pkg/config.yml": config_yml,
-                 "recipes/pkg/all/conanfile.py": str(conanfile)}
-        url_folder, commit = create_local_git_repo(files)
-
-        c = TestClient()
-        c.run(f'remote add local "{url_folder}" --type=oss-recipes-git')
-        c.run("list *:* -r=local")
-        assert "Remote local not cloned yet" in c.out
-        assert "pkg/0.1" in c.out
-        c.run("list *:* -r=local")
-        assert "Remote local not cloned yet" not in c.out
-        assert "pkg/0.1" in c.out
-
-        config_yml = textwrap.dedent("""\
-            versions:
-              "0.1":
-                folder: all
-              "0.2":
-                folder: all
-            """)
-        save_files(url_folder, {"recipes/pkg/config.yml": config_yml})
-        git_add_changes_commit(url_folder)
-        c.run("list *:* -r=local")
-        assert "pkg/0.1" in c.out
-        assert "pkg/0.2" not in c.out
-        c.run("remote pull local")
-        c.run("list *:* -r=local")
-        assert "pkg/0.1" in c.out
-        assert "pkg/0.2" in c.out
