e650aa54b840d86ad1850b6069f47d821521a014
memsharded <james@conan.io>
2024-01-24 12:56:07 +0100

    tests

--- conans/test/integration/remote/test_offline.py
+++ conans/test/integration/remote/test_offline.py
@@ -1,3 +1,5 @@
+import re
+
 from conans.test.assets.genconanfile import GenConanfile
 from conans.test.utils.tools import TestClient
 
@@ -29,6 +31,18 @@ def test_offline_uploaded():
 
 
 def test_offline_build_requires():
+    """
+    When there are tool-requires that are not installed locally, Conan will try to check
+    its existence in the remote, because that package might be needed later. Even if some
+    packages can be marked later as "skip" and not fail, that cannot be known a priori, so if the
+    package is not in the cache, it will be checked in servers
+    https://github.com/conan-io/conan/issues/15339
+
+    Approaches:
+    - conan remote disable <remotes>
+    - conan install ... -nr (--no-remotes)
+    - prepopulate the cache with all tool-requires with `-c:a tools.graph:skip_binaries=False`
+    """
     c = TestClient(default_server_user=True)
     c.save({"tool/conanfile.py": GenConanfile("tool", "0.1"),
             "lib/conanfile.py": GenConanfile("pkg", "0.1").with_tool_requires("tool/0.1")})
@@ -44,6 +58,14 @@ def test_offline_build_requires():
     # is not locally installed, and Conan will check for it. Later it might decide that it can
     # be skipped, but initially it will look for it, and as it is not in the cache, it will look
     # in the remotes.
+    assert "ERROR: 'NoneType' object has no attribute 'fake_url'. [Remote: default]" in c.out
     # Explicitly telling that no-remotes, works
     c.run("install --requires=pkg/0.1 -nr")
     assert "Install finished successfully" in c.out
+
+    # graph info also breaks
+    c.run("graph info --requires=pkg/0.1", assert_error=True)
+    assert "ERROR: 'NoneType' object has no attribute 'fake_url'. [Remote: default]" in c.out
+
+    c.run("graph info --requires=pkg/0.1 -nr")
+    assert re.search(r"Skipped binaries(\s*)tool/0.1", c.out)
