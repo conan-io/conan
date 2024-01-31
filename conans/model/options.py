c09d6bc1334043fcf8638aa35ac2be0448fe11ab
memsharded <james@conan.io>
2024-01-31 16:58:58 +0100

    wip

--- conans/model/options.py
+++ conans/model/options.py
@@ -14,10 +14,10 @@ def option_not_exist_msg(option_name, existing_options):
 
 
 class _PackageOption:
-    def __init__(self, name, value, possible_values=None, strong=False):
+    def __init__(self, name, value, possible_values=None):
         self._name = name
         self._value = value  # Value None = not defined
-        self.strong = strong
+        self.strong = False
         # possible_values only possible origin is recipes
         if possible_values is None:
             self._possible_values = None
@@ -32,7 +32,7 @@ class _PackageOption:
         if scope:
             return "%s:%s%s=%s" % (scope, self._name, strong, self._value)
         else:
-            return "%s=%s%s" % (self._name, strong, self._value)
+            return "%s%s=%s" % (self._name, strong, self._value)
 
     def copy_conaninfo_option(self):
         # To generate a copy without validation, for package_id info.options value
@@ -192,7 +192,7 @@ class _PackageOptions:
             raise ConanException(f"Incorrect attempt to modify option '{item}' "
                                  f"from '{current_value}' to '{value}'")
         self._ensure_exists(item)
-        v = self._data.setdefault(item, _PackageOption(item, None, strong=strong))
+        v = self._data.setdefault(item, _PackageOption(item, None))
         new_value_strong = strong or (isinstance(value, _PackageOption) and value.strong)
         if new_value_strong or not v.strong:
             v.value = value
