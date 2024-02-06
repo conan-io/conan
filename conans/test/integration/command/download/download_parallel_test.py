6a69300af5827fa86c1ad06a41f1199a37cfa2a0
Rubén Rincón Blanco <git@rinconblanco.es>
2024-02-06 03:59:09 +0100

    Download multiple packages

--- conans/test/integration/command/download/download_parallel_test.py
+++ conans/test/integration/command/download/download_parallel_test.py
@@ -4,16 +4,18 @@ from conans.test.utils.tools import GenConanfile, TestClient
 def test_basic_parallel_download():
     client = TestClient(light=True, default_server_user=True)
     threads = 2
-    counter = 4
+    packages = 2
+    per_package = 4
     client.save({"global.conf": f"core.download:parallel={threads}"},
                 path=client.cache.cache_folder)
     client.save({"conanfile.py": GenConanfile().with_option("myoption", '["ANY"]')})
 
     package_ids = []
-    for i in range(counter):
-        client.run(f"create . --name=pkg{i} --version=0.1 --user=user --channel=testing -o pkg{i}/*:myoption={i}")
-        package_id = client.created_package_id(f"pkg{i}/0.1@user/testing")
-        package_ids.append((i, package_id))
+    for i in range(packages):
+        for n in range(per_package):
+            client.run(f"create . --name=pkg{i} --version=0.1 --user=user --channel=testing -o pkg{i}/*:myoption={n}")
+            package_id = client.created_package_id(f"pkg{i}/0.1@user/testing")
+            package_ids.append((i, package_id))
     client.run("upload * --confirm -r default")
     client.run("remove * -c")
 
