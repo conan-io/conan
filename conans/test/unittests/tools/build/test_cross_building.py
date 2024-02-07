0093b591c532705ccd0b3b8215ffe25aa234be68
Francisco Ramirez de Anton <franchuti688@gmail.com>
2024-02-07 11:30:46 +0100

    Renamed

--- conans/test/unittests/tools/build/test_cross_building.py
+++ conans/test/unittests/tools/build/test_cross_building.py
@@ -4,13 +4,13 @@ from conan.tools.build import cross_building
 from conans.test.utils.mocks import ConanFileMock
 
 
-@pytest.mark.parametrize("force", (True, False))
-def test_using_force_conf(force):
+@pytest.mark.parametrize("cross_build", (True, False))
+def test_using_cross_build_conf(cross_build):
     """
     Tests cross_building function is using the conf variable to force or not.
 
     Issue related: https://github.com/conan-io/conan/issues/15392
     """
     conanfile = ConanFileMock()
-    conanfile.conf.define("tools.build.cross_building:force", force)
-    assert cross_building(conanfile) == force
+    conanfile.conf.define("tools.build.cross_building:cross_build", cross_build)
+    assert cross_building(conanfile) == cross_build
