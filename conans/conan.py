d93b4ac092c55d9cef854eedcd178474e51d7eb8
Francisco Ramírez <franchuti688@gmail.com>
2024-02-12 11:05:17 +0100

    Added macOS workaround to avoid tqdm failing (#15628)

--- conans/conan.py
+++ conans/conan.py
@@ -1,5 +1,11 @@
 import sys
 import os
+import platform
+
+# https://github.com/icloud-photos-downloader/icloud_photos_downloader/commit/5450392a6d7c28c6926207b03e063f1ec049da2f#
+if platform.system() == "Darwin":
+    from multiprocessing import freeze_support
+    freeze_support() # fixing tqdm on macos
 
 if os.getenv("CONAN_V2_CLI"):
     from conans.cli.cli import main
