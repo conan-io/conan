ac572da98fc783b1e7caba5fe6e27d2cfba99b04
Uilian Ries <uilianries@gmail.com>
2024-02-21 09:07:58 +0100

    Update method description
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/tools/build/flags.py
+++ conan/tools/build/flags.py
@@ -167,9 +167,13 @@ def build_type_flags(settings):
 
 def cppstd_flag(conanfile) -> str:
     """ Returns flags specific to the C++ standard based on the compiler, its version and cppstd.
+        It considers when using GNU extension in cppstd, reflecting it in the flag.
+        Currently, it supports GCC, Clang, AppleClang, MSVC, Intel, MCST-LCC.
 
-    :param conanfile: ConanFile instance
-    :return: string with the flag (-std=c++11, -std=gnu++11, etc.)
+        In case there is no compiler or cppstd in the profile, the result will be an empty string.
+
+    :param conanfile: The current recipe object. Always use ``self``.
+    :return: The standard C++ flag used by the compiler. e.g. -std=c++11, /std:c++latest
     """
     compiler = conanfile.settings.get_safe("compiler")
     compiler_version = conanfile.settings.get_safe("compiler.version")
