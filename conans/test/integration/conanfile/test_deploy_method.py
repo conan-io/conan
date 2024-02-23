17610ce2391172c5848dc1f9db377a0f9aa5c4ed
James <james@conan.io>
2024-02-23 10:43:01 +0100

    fixed --deployer-package for local (#15737)
    * fixed --deployer-package for local
    
    * fix test

--- conans/test/integration/conanfile/test_deploy_method.py
+++ conans/test/integration/conanfile/test_deploy_method.py
@@ -44,3 +44,35 @@ def test_deploy_method():
     c.run("install --requires=pkg/0.1 --deployer-package=pkg/* --deployer-folder=mydeploy")
     assert "dep/0.1: Executing deploy()" not in c.out
     assert "pkg/0.1: Executing deploy()" in c.out
+
+
+def test_deploy_local():
+    c = TestClient()
+    conanfile = textwrap.dedent("""
+        from conan import ConanFile
+        from conan.tools.files import copy
+        class Pkg(ConanFile):
+            name = "pkg"
+            version = "0.1"
+            def deploy(self):
+                copy(self, "*", src=self.package_folder, dst=self.deploy_folder)
+        """)
+    c.save({"conanfile.py": conanfile})
+    c.run("install . --deployer-package=*", assert_error=True)
+    assert "ERROR: conanfile.py (pkg/0.1): Error in deploy() method, line 8" in c.out
+    assert "copy() received 'src=None' argument" in c.out
+
+    # Without name/version same error
+    conanfile = textwrap.dedent("""
+        from conan import ConanFile
+        class Pkg(ConanFile):
+            def deploy(self):
+                assert self.package_folder  # will fail if None
+        """)
+    c.save({"conanfile.py": conanfile})
+    c.run("install . --deployer-package=*", assert_error=True)
+    assert "ERROR: conanfile.py: Error in deploy() method, line 5" in c.out
+
+    # I can exclude the current consumer, it won't fail
+    c.run("install . --deployer-package=!& --deployer-package=*")
+    assert "Install finished successfully" in c.out
