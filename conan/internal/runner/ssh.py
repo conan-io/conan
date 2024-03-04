acc000cd991700d242fb9c350851412e2923389c
David Sanchez <davidsf@jfrog.com>
2024-03-04 18:15:46 +0100

    docker runner updated

--- conan/internal/runner/ssh.py
+++ conan/internal/runner/ssh.py
@@ -1,3 +1,11 @@
 class SSHRunner:
-    def __init__(self, conan_api, profile, args, raw_args):
+
+    def __init__(self, conan_api, command, profile, args, raw_args):
+        self.conan_api = conan_api
+        self.command = command
+        self.profile = profile
+        self.args = args
+        self.raw_args = raw_args
+
+    def run(self, use_cache=True):
         pass
