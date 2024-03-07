99a72b14e67a4fcf3bce66492b3cb6f60c572590
David Sanchez <davidsf@jfrog.com>
2024-03-07 18:03:15 +0100

    new test added

--- conans/test/integration/command/runner_test.py
+++ conans/test/integration/command/runner_test.py
@@ -1,25 +1,47 @@
 import textwrap
+import os
+from typing import Literal
 import pytest
 import docker
 from conans.test.utils.tools import TestClient
+from conans.test.assets.cmake import gen_cmakelists
+from conans.test.assets.sources import gen_function_h, gen_function_cpp
 
 
-def test_create_docker_runner():
+
+def docker_skip(test_image=None):
+    try:
+        docker_client = docker.from_env()
+        if test_image:
+            docker_client.images.get(test_image)
+    except docker.errors.DockerException:
+        return True
+    except docker.errors.ImageNotFound:
+        return True
+    except docker.errors.APIError:
+        return True
+    return False
+
+
+def conan_base_path():
+    import conans
+    return os.path.dirname(os.path.dirname(conans.__file__))
+
+
+def dockerfile_path(name=None):
+    path = os.path.join(os.path.dirname(__file__), "dockerfiles")
+    if name:
+        path = os.path.join(path, name)
+    return path
+
+
+@pytest.mark.skipif(docker_skip(), reason="Only docker running")
+def test_create_docker_runner_dockerfile_folder_path():
     """
     Tests the ``conan create . ``
     """
-    try:
-        docker_client = docker.from_env()
-        docker_client.images.get('conan-runner-default')
-    except docker.errors.DockerException:
-        pytest.skip("docker client not found")
-    except docker.errors.ImageNotFound:
-        pytest.skip("docker 'conan-runner-default' image doesn't exist")
-    except docker.errors.APIError:
-        pytest.skip("docker server returns an error")
-    
     client = TestClient()
-    profile_build = textwrap.dedent("""\
+    profile_build = textwrap.dedent(f"""\
     [settings]
     arch=x86_64
     build_type=Release
@@ -30,11 +52,13 @@ def test_create_docker_runner():
     os=Linux
     [runner]
     type=docker
-    image=conan-runner-default
+    dockerfile={dockerfile_path()}
+    docker_build_path={conan_base_path()}
+    image=conan-runner-default-test
     cache=copy
-    remove=1
+    remove=True
     """)
-    profile_host = textwrap.dedent("""\
+    profile_host = textwrap.dedent(f"""\
     [settings]
     arch=x86_64
     build_type=Release
@@ -45,9 +69,11 @@ def test_create_docker_runner():
     os=Linux
     [runner]
     type=docker
-    image=conan-runner-default
+    dockerfile={dockerfile_path()}
+    docker_build_path={conan_base_path()}
+    image=conan-runner-default-test
     cache=copy
-    remove=1
+    remove=True
     """)
     conanfile = textwrap.dedent("""
         from conan import ConanFile
@@ -73,3 +99,136 @@ def test_create_docker_runner():
     assert "Restore: pkg/0.2:a0826e5ee3b340fcc7a8ccde40224e3562316307" in client.out
     assert "Restore: pkg/0.2:a0826e5ee3b340fcc7a8ccde40224e3562316307 metadata" in client.out
     assert "Removing container" in client.out
+
+
+@pytest.mark.skipif(docker_skip(), reason="Only docker running")
+def test_create_docker_runner_dockerfile_file_path():
+    """
+    Tests the ``conan create . ``
+    """
+    client = TestClient()
+    profile_build = textwrap.dedent(f"""\
+    [settings]
+    arch=x86_64
+    build_type=Release
+    compiler=gcc
+    compiler.cppstd=gnu17
+    compiler.libcxx=libstdc++11
+    compiler.version=11
+    os=Linux
+    [runner]
+    type=docker
+    dockerfile={dockerfile_path("Dockerfile_test")}
+    docker_build_path={conan_base_path()}
+    image=conan-runner-default-test
+    cache=copy
+    remove=True
+    """)
+    profile_host = textwrap.dedent(f"""\
+    [settings]
+    arch=x86_64
+    build_type=Release
+    compiler=gcc
+    compiler.cppstd=gnu17
+    compiler.libcxx=libstdc++11
+    compiler.version=11
+    os=Linux
+    [runner]
+    type=docker
+    dockerfile={dockerfile_path("Dockerfile_test")}
+    docker_build_path={conan_base_path()}
+    image=conan-runner-default-test
+    cache=copy
+    remove=True
+    """)
+    conanfile = textwrap.dedent("""
+        from conan import ConanFile
+
+        class MyTest(ConanFile):
+            name = "pkg"
+            version = "0.2"
+            settings = "build_type", "compiler"
+            author = "John Doe"
+            license = "MIT"
+            url = "https://foo.bar.baz"
+            homepage = "https://foo.bar.site"
+            topics = "foo", "bar", "qux"
+            provides = "libjpeg", "libjpg"
+            deprecated = "other-pkg"
+            options = {"shared": [True, False], "fPIC": [True, False]}
+            default_options = {"shared": False, "fPIC": True}
+        """)
+    client.save({"conanfile.py": conanfile, "host": profile_host, "build": profile_build})
+    client.run("create . -pr:h host -pr:b build")
+
+    assert "Restore: pkg/0.2" in client.out
+    assert "Restore: pkg/0.2:a0826e5ee3b340fcc7a8ccde40224e3562316307" in client.out
+    assert "Restore: pkg/0.2:a0826e5ee3b340fcc7a8ccde40224e3562316307 metadata" in client.out
+    assert "Removing container" in client.out
+
+
+@pytest.mark.skipif(docker_skip(), reason="Only docker running")
+@pytest.mark.parametrize("build_type,shared", [("Release", False), ("Debug", True)])
+@pytest.mark.tool("ninja")
+def test_create_docker_runner_with_ninja(build_type: Literal['Release', 'Debug'], shared: bool):
+    conanfile = textwrap.dedent("""
+    import os
+    from conan import ConanFile
+    from conan.tools.cmake import CMake, CMakeToolchain
+
+    class Library(ConanFile):
+        name = "hello"
+        version = "1.0"
+        settings = 'os', 'arch', 'compiler', 'build_type'
+        exports_sources = 'hello.h', '*.cpp', 'CMakeLists.txt'
+        options = {'shared': [True, False]}
+        default_options = {'shared': False}
+
+        def generate(self):
+            tc = CMakeToolchain(self, generator="Ninja")
+            tc.generate()
+
+        def build(self):
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
+            self.run(os.sep.join([".", "myapp"]))
+
+        def package(self):
+            cmake = CMake(self)
+            cmake.install()
+    """)
+
+    client = TestClient(path_with_spaces=False)
+    client.save({'conanfile.py': conanfile,
+                "CMakeLists.txt": gen_cmakelists(libsources=["hello.cpp"],
+                                                appsources=["main.cpp"],
+                                                install=True),
+                "hello.h": gen_function_h(name="hello"),
+                "hello.cpp": gen_function_cpp(name="hello", includes=["hello"]),
+                "main.cpp": gen_function_cpp(name="main", includes=["hello"],
+                                            calls=["hello"])})
+    profile = textwrap.dedent(f"""\
+    [settings]
+    arch=x86_64
+    build_type=Release
+    compiler=gcc
+    compiler.cppstd=gnu17
+    compiler.libcxx=libstdc++11
+    compiler.version=11
+    os=Linux
+    [runner]
+    type=docker
+    image=conan-runner-ninja-test
+    dockerfile={dockerfile_path("Dockerfile_ninja")}
+    docker_build_path={conan_base_path()}
+    cache=copy
+    remove=True
+    """)
+    client.save({"profile": profile})
+    settings = "-s os=Linux -s arch=x86_64 -s build_type={} -o hello/*:shared={}".format(build_type, shared)
+    # create should also work
+    client.run("create . --name=hello --version=1.0 {} -pr:h profile -pr:b profile".format(settings))
+    print(client.out)
+    assert 'cmake -G "Ninja"' in client.out
+    assert "main: {}!".format(build_type) in client.out
