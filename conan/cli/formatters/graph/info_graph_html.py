dd7f338c706c4f7c16ebc3ab1838295471330c20
memsharded <james@conan.io>
2024-03-11 18:29:05 +0100

    wip

--- conan/cli/formatters/graph/info_graph_html.py
+++ conan/cli/formatters/graph/info_graph_html.py
@@ -6,12 +6,7 @@ graph_info_html = """
 
     <body>
         <script type="text/javascript">
-            function showhideclass(id) {
-                var elements = document.getElementsByClassName(id)
-                for (var i = 0; i < elements.length; i++) {
-                    elements[i].style.display = (elements[i].style.display != 'none') ? 'none' : 'block';
-                }
-            }
+
         </script>
         <style>
             @media print {
@@ -31,16 +26,18 @@ graph_info_html = """
             }
         </style>
 
-        <script type="application/json" id="graph_data">
-            {{ deps_graph }}
-        </script>
-
         <div style="width: 100%;">
             <div id="mynetwork" style="float:left; width: 75%;"></div>
-                <button onclick="javascript:switchBuild()" class="button noPrint">
-                    Show / hide build-requires
-                </button>
-                <div style="float:right;width:25%;">
+            <div style="float:right;width:25%;">
+                <div>
+                    <input type="checkbox" onchange="switchBuild()" id="show_build_requires" checked />
+                    <label for="show_build_requires">Show build-requires</label>
+                </div>
+                <div>
+                    <input type="checkbox" onchange="collapseBuild()" id="group_build_requires"/>
+                    <label for="group_build_requires">Group build-requires</label>
+                </div>
+
                 <div id="details"  style="padding:10;" class="noPrint">Package info: no package selected</div>
                 <button onclick="javascript:showhideclass('controls')" class="button noPrint">
                     Show / hide graph controls
@@ -51,31 +48,49 @@ graph_info_html = """
         <div style="clear:both"></div>
 
         <script type="text/javascript">
-            var graph_data = JSON.parse(document.getElementById('graph_data').innerHTML);
-            var hide_build = false;
-            var nodes = [];
-            var edges = [];
-            for (const [node_id, node] of Object.entries(graph_data["nodes"])) {
-                const shape = node.context == "build" ? "ellipse" : node.test ? "hexagon" : "box";
-                nodes.push({
-                    id: node_id,
-                    label: node["name"],
-                    shape: shape
-                });
-                for (const [dep_id, dep] of Object.entries(node["dependencies"])) {
-                    if (dep.direct)
-                        edges.push({from: node_id, to: dep_id});
-                }
-            }
 
-            var nodes = new vis.DataSet(nodes);
-            var edges = new vis.DataSet(edges);
+            const graph_data = {{ deps_graph }};
+            var hide_build = false;
+            var collapse_build = false;
+
+            function define_data(){
+                var nodes = [];
+                var edges = [];
+                var collapsed_build = {};
+                var targets = {};
+                for (const [node_id, node] of Object.entries(graph_data["nodes"])) {
+                    const shape = node.context == "build" ? "ellipse" : node.test ? "hexagon" : "box";
+                    var label =  node["name"] + "/" + node["version"];
+                    if (collapse_build) {
+                        var existing = collapsed_build[label];
+                        targets[node_id] = existing;
+                        if (existing) continue;
+                        collapsed_build[label] = node_id;
+                    }
+                    nodes.push({
+                        id: node_id,
+                        label: label,
+                        shape: shape
+                    });
+
+                }
+                for (const [node_id, node] of Object.entries(graph_data["nodes"])) {
+                    for (const [dep_id, dep] of Object.entries(node["dependencies"])) {
+                        if (dep.direct){
+                            var target_id = targets[dep_id] || dep_id;
+                            edges.push({from: node_id, to: target_id});
+                        }
+                    }
+                }
+                var nodes = new vis.DataSet(nodes);
+                var edges = new vis.DataSet(edges);
+                var data = {nodes: nodes, edges: edges};
+                return data;
+            };
+
             var container = document.getElementById('mynetwork');
             var controls = document.getElementById('controls');
-            var data = {
-                nodes: nodes,
-                edges: edges
-            };
+
             var options = {
                 autoResize: true,
                 locale: 'en',
@@ -90,8 +105,10 @@ graph_info_html = """
                     "hierarchical": {
                         "enabled": true,
                         "sortMethod": "directed",
-                        "direction": "UD",
-                        nodeSpacing: 200
+                        "direction": "DU",
+                        nodeSpacing: 170,
+                        blockShifting: true,
+                        edgeMinimization: true
                     }
                 },
                 physics: {
@@ -99,21 +116,13 @@ graph_info_html = """
                 },
                 configure: {
                     enabled: true,
-                    filter: 'layout physics',
+                    filter: 'layout',
                     showButton: false,
                     container: controls
                 }
             };
-            function switchBuild() {
-                hide_build = !hide_build;
-                nodes_update = [];
-                for (const [node_id, node] of Object.entries(graph_data["nodes"])) {
-                    if (node.context == "build")
-                        nodes_update.push({id: node_id, hidden: hide_build})
-                }
-                nodes.update(nodes_update);
-                draw();
-            }
+
+            var data = define_data();
             var network = new vis.Network(container, data, options);
             network.on('click', function (properties) {
                 var ids = properties.nodes;
@@ -125,9 +134,11 @@ graph_info_html = """
                     selected_node = graph_data["nodes"][ids[0]]
                     let ul = document.createElement('ul');
                     for (const [key, value] of Object.entries(selected_node)) {
-                        let li = document.createElement('li');
-                        li.innerHTML = "<b>"+ key +"</b>: " + value;
-                        ul.appendChild(li);
+                        if (value) {
+                            let li = document.createElement('li');
+                            li.innerHTML = "<b>"+ key +"</b>: " + value;
+                            ul.appendChild(li);
+                        }
                     }
                     control.appendChild(ul);
                 }
@@ -138,8 +149,31 @@ graph_info_html = """
             function draw() {
                 network.redraw();
             }
+            function switchBuild() {
+                hide_build = !hide_build;
+                nodes_update = [];
+                for (const [node_id, node] of Object.entries(graph_data["nodes"])) {
+                    if (node.context == "build")
+                        nodes_update.push({id: node_id, hidden: hide_build})
+                }
+                data.nodes.update(nodes_update);
+                draw();
+            }
+            function collapseBuild() {
+                collapse_build = !collapse_build;
+                console.log("collapsing build");
+                data = define_data();
+                network.setData(data);
+                draw();
+            }
+            function showhideclass(id) {
+                var elements = document.getElementsByClassName(id)
+                for (var i = 0; i < elements.length; i++) {
+                    elements[i].style.display = (elements[i].style.display != 'none') ? 'none' : 'block';
+                }
+            }
             window.addEventListener("load", () => {
-              draw();
+               draw();
             });
         </script>
     </body>
