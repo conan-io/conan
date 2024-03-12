c9135702ddfcbd41b85f5287f168462dc1c31ffc
David Sanchez <davidsf@jfrog.com>
2024-03-12 21:35:38 +0100

    docker conan version check added

--- conan/internal/runner/__init__.py
+++ conan/internal/runner/__init__.py
@@ -1,6 +1,6 @@
-class RunnerExection(Exception):
+class RunnerException(Exception):
     def __init__(self, *args, **kwargs):
         self.command = kwargs.pop("command", None)
         self.stdout_log = kwargs.pop("stdout_log", None)
         self.stderr_log = kwargs.pop("stderr_log", None)
-        super(RunnerExection, self).__init__(*args, **kwargs)
+        super(RunnerException, self).__init__(*args, **kwargs)
