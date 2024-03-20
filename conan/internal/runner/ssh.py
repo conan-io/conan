09d41b78a766a554b32a61d11917075d97d610ef
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2024-03-20 20:52:05 +0000

    Run remote create command

--- conan/internal/runner/ssh.py
+++ conan/internal/runner/ssh.py
@@ -61,12 +61,19 @@ class SSHRunner:
         raw_args = " ".join(raw_args)
         command = f"{self.remote_conan} create {raw_args}"
 
-        # ssh_info(f"Remote command: {command}")
+        ssh_info(f"Remote command: {command}")
     
-        _stdin, _stdout,_stderr = self.client.exec_command(command, get_pty=True)
-
-        # TODO: achieve printing continuous output
-
+        stdout, _ = self._run_command(command)
+        first_line = True
+        while not stdout.channel.exit_status_ready():
+            line = stdout.channel.recv(1024)
+            if first_line and self.remote_is_windows:
+                # Avoid clearing and moving the cursor when the remote server is Windows
+                # https://github.com/PowerShell/Win32-OpenSSH/issues/1738#issuecomment-789434169
+                line = line.replace(b"\x1b[2J\x1b[m\x1b[H",b"")
+            sys.stdout.buffer.write(line)
+            sys.stdout.buffer.flush()
+            first_line = False
 
         self.client.close()
 
@@ -220,3 +227,21 @@ class SSHRunner:
                 dst = _Path(self.remote_create_dir).joinpath(relative_root).joinpath(file).as_posix()
                 sftp.put(orig, dst)
         sftp.close()
+
+    def _run_command(self, command):
+        ''' Run a command in an SSH session.
+            When requesting a pseudo-terminal from the server,
+            ensure we pass width and height that matches the current
+            terminal
+        '''
+        channel = self.client.get_transport().open_session()
+        if sys.stdout.isatty():
+            width, height = os.get_terminal_size()
+            channel.get_pty(width=width, height=height)
+
+        channel.exec_command(command)
+        
+        stdout = channel.makefile("r")
+        stderr = channel.makefile("r")
+        return stdout, stderr
+        
