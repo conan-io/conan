e703b076bd30f994d0704c7baba51e92efa10432
Uilian Ries <uilianries@gmail.com>
2024-04-09 16:22:12 +0200

    test: consume tool requires using fish
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/test/functional/toolchains/env/test_virtualenv_fish.py
+++ conans/test/functional/toolchains/env/test_virtualenv_fish.py
@@ -1,6 +1,8 @@
 import os
+import platform
 
 import pytest
+import textwrap
 
 from conans.test.assets.genconanfile import GenConanfile
 from conans.test.utils.test_files import temp_folder
@@ -41,3 +43,45 @@ def test_virtualenv_fish():
 
     client.run_command("fish -c 'source conanbuildenv.fish && set && source deactivate_conanbuildenv.fish && set'")
     assert str(client.out).count('MYPATH1 /path/to/ar') == 1
+
+
+@pytest.mark.tool("fish")
+@pytest.mark.parametrize("fish_value", [True, False])
+def test_transitive_tool_requires(fish_value):
+    """Generate a tool require package, which provides and binary and a custom environment variable.
+    Using fish, the binary should be available in the path, and the environment variable too.
+    """
+    client = TestClient()
+    save(client.cache.new_config_path, f"tools.env.virtualenv:fish={fish_value}\n")
+
+    # Generate the tool package with pkg-echo-tool binary that prints the value of LADY env var
+    cmd_line = "echo ${LADY}" if platform.system() != "Windows" else "echo %LADY%"
+    conanfile = str(GenConanfile("tool", "0.1.0")
+                    .with_package_file("bin/pkg-echo-tool", cmd_line))
+    package_info = """
+        os.chmod(os.path.join(self.package_folder, "bin", "pkg-echo-tool"), 0o777)
+
+    def package_info(self):
+        self.buildenv_info.define("LADY", "Dulcinea-del-Toboso")
+    """
+    conanfile += package_info
+    client.save({"tool/conanfile.py": conanfile})
+    client.run("create tool")
+
+    assert "tool/0.1.0: package(): Packaged 1 file: pkg-echo-tool" in client.out
+
+    # Generate the app package that uses the tool package. It should be able to run the binary and
+    # access the environment variable as well.
+    conanfile = str(GenConanfile("app", "0.1.0")
+                 .with_tool_requires("tool/0.1.0")
+                 .with_generator("VirtualBuildEnv"))
+    build = """
+    def build(self):
+        self.run("pkg-echo-tool", env="conanbuild")
+    """
+    conanfile += build
+
+    client.save({"app/conanfile.py": conanfile})
+    client.run("create app")
+
+    assert "Dulcinea-del-Toboso" in client.out
