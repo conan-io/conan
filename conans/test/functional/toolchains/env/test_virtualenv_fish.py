2d202ed3fb18850a7b29110150d2f5bace8fcb22
Uilian Ries <uilianries@gmail.com>
2024-04-18 10:53:14 +0200

    Skip fish test in Windows
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conans/test/functional/toolchains/env/test_virtualenv_fish.py
+++ conans/test/functional/toolchains/env/test_virtualenv_fish.py
@@ -6,67 +6,25 @@ import pytest
 
 from conans.test.assets.genconanfile import GenConanfile
 from conans.test.utils.test_files import temp_folder
-from conans.test.utils.tools import TestClient
+from conans.test.utils.tools import TestClient, load
 from conans.util.files import save
+
+# INFO: Fish only supports Cygwin and WSL https://github.com/fish-shell/fish-shell?tab=readme-ov-file#windows
+pytestmark = pytest.mark.skipif(platform.system() not in ("Darwin", "Linux"), reason="Fish is well supported only in Linux and MacOS")
 
 
 @pytest.mark.tool("fish")
-def test_define_new_vars():
+@pytest.mark.parametrize("fake_path", ["/path/to/fake/folder", "/path/with space/to/folder"])
+@pytest.mark.parametrize("my_var1", ["myvalue", "my value"])
+@pytest.mark.parametrize("my_var2", ["varwithspaces", "var with spaces"])
+@pytest.mark.parametrize("my_path", ["/path/to/ar", "/path/to space/ar"])
+def test_buildenv_define_new_vars(fake_path, my_var1, my_var2, my_path):
     """Test when defining new path and new variable in buildenv_info.
 
     Variables should be available as environment variables in the buildenv script.
     And, should be deleted when deactivate_conanbuildenv is called.
 
-    Avoided pytest.mark.parametrize to check how it works when having multiple definitions at same time.
-    """
-    cache_folder = os.path.join(temp_folder(), "[sub] folder")
-    client = TestClient(cache_folder)
-    conanfile = str(GenConanfile("pkg", "0.1"))
-    conanfile += """
-
-    def package_info(self):
-        self.buildenv_info.define_path("MYPATH1", "/path/to/ar")
-        self.buildenv_info.define("MYVAR1", "myvalue")
-        self.buildenv_info.define("MYVAR2", "var with spaces")
-    """
-    client.save({"conanfile.py": conanfile})
-    client.run("create .")
-    save(client.cache.new_config_path, "tools.env.virtualenv:fish=True\n")
-    client.save({"conanfile.py": GenConanfile("app", "0.1").with_requires("pkg/0.1")})
-    client.run("install . -s:a os=Linux")
-
-    for ext in ["*.sh", "*.bat"]:
-        not_expected_files = glob.glob(os.path.join(client.current_folder, ext))
-        assert not not_expected_files
-
-    assert os.path.exists(os.path.join(client.current_folder, "conanbuildenv.fish"))
-    assert not os.path.exists(os.path.join(client.current_folder, "conanrunenv.fish"))
-
-    with open(os.path.join(client.current_folder, "conanbuildenv.fish"), "r") as f:
-        buildenv = f.read()
-        assert 'set -gx MYPATH1 "/path/to/ar"' in buildenv
-        assert 'set -gx MYVAR1 "myvalue"' in buildenv
-        assert 'set -gx MYVAR2 "var with spaces"' in buildenv
-
-    client.run_command("fish -c 'source conanbuildenv.fish && set'")
-    assert 'MYPATH1 /path/to/ar' in client.out
-    assert 'MYVAR1 myvalue' in client.out
-    assert "MYVAR2 'var with spaces'" in client.out
-
-    client.run_command('fish -c ". conanbuildenv.fish && set && deactivate_conanbuildenv && set"')
-    assert str(client.out).count('MYPATH1 /path/to/ar') == 1
-    assert str(client.out).count('MYVAR1 myvalue') == 1
-    assert str(client.out).count("MYVAR2 'var with spaces'") == 1
-
-
-@pytest.mark.tool("fish")
-@pytest.mark.parametrize("fake_path", ["/path/to/fake/folder", "/path/with space/to/folder"])
-def test_prepend_path(fake_path):
-    """Test when appending to an existing path in buildenv_info.
-
-    Path should be available as environment variable in the buildenv script, including the new value
-    as first element.
-    Once deactivate_conanbuildenv is called, the path should be restored as before.
+    Variable with spaces should be wrapped by single quotes when exported.
     """
     cache_folder = os.path.join(temp_folder(), "[sub] folder")
     client = TestClient(cache_folder)
@@ -74,6 +32,9 @@ def test_prepend_path(fake_path):
     conanfile += f"""
 
     def package_info(self):
+        self.buildenv_info.define_path("MYPATH1", "{my_path}")
+        self.buildenv_info.define("MYVAR1", "{my_var1}")
+        self.buildenv_info.define("MYVAR2", "{my_var2}")
         self.buildenv_info.prepend_path("PATH", "{fake_path}")
     """
     client.save({"conanfile.py": conanfile})
@@ -82,30 +43,39 @@ def test_prepend_path(fake_path):
     client.save({"conanfile.py": GenConanfile("app", "0.1").with_requires("pkg/0.1")})
     client.run("install . -s:a os=Linux")
 
+    # Only generated fish scripts when virtualenv:fish=True
     for ext in ["*.sh", "*.bat"]:
         not_expected_files = glob.glob(os.path.join(client.current_folder, ext))
         assert not not_expected_files
 
-    assert os.path.exists(os.path.join(client.current_folder, "conanbuildenv.fish"))
-    assert not os.path.exists(os.path.join(client.current_folder, "conanrunenv.fish"))
+    # It does not generate conanrun because there is no variables to be exported
+    expected_files = glob.glob(os.path.join(client.current_folder, "*.fish"))
+    assert [os.path.join(client.current_folder, "conanbuild.fish"),
+            os.path.join(client.current_folder, "conanbuildenv.fish")] == expected_files
 
-    script_path = os.path.join(client.current_folder, "conanbuildenv.fish")
-    with open(script_path, "r") as f:
-        buildenv = f.read()
-        assert f'set -pgx PATH "{fake_path}"' in buildenv
+    buildenv = load(os.path.join(client.current_folder, "conanbuildenv.fish"))
+    assert f'set -gx MYPATH1 "{my_path}"' in buildenv
+    assert f'set -gx MYVAR1 "{my_var1}"' in buildenv
+    assert f'set -gx MYVAR2 "{my_var2}"' in buildenv
+    assert f'set -pgx PATH "{fake_path}"' in buildenv
 
-    client.run_command(f'fish -c ". conanbuildenv.fish; set"')
-    assert f"PATH '{fake_path}'" in client.out
-
-    client.run_command(f'fish -c ". conanbuildenv.fish; deactivate_conanbuildenv; set"')
-    assert f"PATH '{fake_path}'" not in client.out
+    wrap_with_quotes = lambda s: f"'{s}'" if ' ' in s else s
+    client.run_command('fish -c ". conanbuild.fish; and set; and deactivate_conanbuildenv; and set"')
+    # Define variables only once and remove after running deactivate_conanbuildenv
+    assert str(client.out).count(f"\nMYPATH1 {wrap_with_quotes(my_path)}") == 1
+    assert str(client.out).count(f"\nMYVAR1 {wrap_with_quotes(my_var1)}") == 1
+    assert str(client.out).count(f"\nMYVAR2 {wrap_with_quotes(my_var2)}") == 1
+    assert str(client.out).count(f"\nPATH '{fake_path}'") == 1
+    # Temporary variables to store names should be removed as well
+    assert str(client.out).count(f"_PATH {wrap_with_quotes(fake_path)}") == 1
+    assert str(client.out).count("_del 'MYPATH1'  'MYVAR1'  'MYVAR2'") == 1
 
 
 @pytest.mark.tool("fish")
 @pytest.mark.parametrize("fish_value", [True, False])
 @pytest.mark.parametrize("path_with_spaces", [True, False])
 @pytest.mark.parametrize("value", ["Dulcinea del Toboso", "Dulcinea-Del-Toboso"])
-def test_transitive_tool_requires(fish_value, path_with_spaces, value):
+def test_buildenv_transitive_tool_requires(fish_value, path_with_spaces, value):
     """Generate a tool require package, which provides and binary and a custom environment variable.
     Using fish, the binary should be available in the path, and the environment variable too.
     """
@@ -144,7 +114,68 @@ def test_transitive_tool_requires(fish_value, path_with_spaces, value):
 
     assert value in client.out
 
-    # TODO: Check why is generating when running Conan create:
-    # - deactivate_conanbuildenv-release-armv8.fish
-    # - conanrunenv-release-armv8.fish
-    # No variables listed, only script_folder. Should not generate these files.
+
+@pytest.mark.tool("fish")
+@pytest.mark.parametrize("fake_path", ["/path/to/fake/folder", "/path/with space/to/folder"])
+@pytest.mark.parametrize("fake_define", ["FOOBAR", "FOO BAR"])
+def test_runenv_buildenv_define_new_vars(fake_path, fake_define):
+    """Test when defining new path and new variable using both buildenvinfo and runenvinfo.
+
+    Variables should be available as environment variables in the buildenv and runenv scripts.
+    And, should be deleted when deactivate_conanbuildenv and deactivate_conanrunenv are called.
+    """
+    cache_folder = os.path.join(temp_folder(), "[sub] folder")
+    client = TestClient(cache_folder)
+    conanfile = str(GenConanfile("pkg", "0.1"))
+    conanfile += f"""
+
+    def package_info(self):
+        self.buildenv_info.define("FAKE_DEFINE", "{fake_define}")
+        self.runenv_info.define_path("FAKE_PATH", "{fake_path}")
+    """
+    client.save({"conanfile.py": conanfile})
+    client.run("create .")
+    save(client.cache.new_config_path, "tools.env.virtualenv:fish=True\n")
+    client.save({"conanfile.py": GenConanfile("app", "0.1").with_requires("pkg/0.1")})
+    client.run("install . -s:a os=Linux")
+
+    # Only generated fish scripts when virtualenv:fish=True
+    for ext in ["*.sh", "*.bat"]:
+        not_expected_files = glob.glob(os.path.join(client.current_folder, ext))
+        assert not not_expected_files
+
+    expected_files = glob.glob(os.path.join(client.current_folder, "*.fish"))
+    assert [os.path.join(client.current_folder, "conanrunenv.fish"),
+            os.path.join(client.current_folder, "conanrun.fish"),
+            os.path.join(client.current_folder, "conanbuild.fish"),
+            os.path.join(client.current_folder, "conanbuildenv.fish")] == expected_files
+
+    # Do not mix buildenv and runenv variables
+    script_path = os.path.join(client.current_folder, "conanbuildenv.fish")
+    script_content = load(script_path)
+    assert f'set -gx FAKE_DEFINE "{fake_define}"' in script_content
+    assert 'FAKE_PATH' not in script_content
+
+    script_path = os.path.join(client.current_folder, "conanrunenv.fish")
+    script_content = load(script_path)
+    assert f'set -pgx FAKE_PATH "{fake_path}"' in script_content
+    assert 'FAKE_DEFINE' not in script_content
+
+    # Check if generated wrappers are sourcing the env scripts
+    for group in ['build', 'run']:
+        script_path = os.path.join(client.current_folder, f"conan{group}.fish")
+        script_content = load(script_path)
+        assert f'. "{os.path.join(client.current_folder, f"conan{group}env.fish")}"' in script_content
+
+    # Check if the variables are available in the environment
+    client.run_command(f'fish -c ". conanbuild.fish; . conanrun.fish; set; deactivate_conanbuildenv; deactivate_conanrunenv; set"')
+    wrap_with_quotes = lambda s: f"'{s}'" if ' ' in s else s
+    assert f"FAKE_PATH {wrap_with_quotes(fake_path)}" in client.out
+    assert f"FAKE_DEFINE {wrap_with_quotes(fake_define)}" in client.out
+    # It finds 2 because there is a variable with all values names to be deleted
+    assert client.out.count('FAKE_PATH') == 2
+    assert client.out.count('FAKE_DEFINE') == 2
+
+    deactivated_content = client.out[client.out.index("echo Restoring environment"):]
+    assert deactivated_content.count('FAKE_PATH') == 0
+    assert deactivated_content.count('FAKE_DEFINE') == 0
