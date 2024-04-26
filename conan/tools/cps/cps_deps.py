b5c89c1b1b6781b764a8ad459a89f091af91d7a5
memsharded <james@conan.io>
2024-04-26 12:22:05 +0200

    wip

--- conan/tools/cps/cps_deps.py
+++ conan/tools/cps/cps_deps.py
@@ -70,8 +70,14 @@ class CPSDeps:
     def generate(self):
         self.conanfile.output.info(f"[CPSDeps] generators folder {self.conanfile.generators_folder}")
         deps = self.conanfile.dependencies.host.items()
+        mapping = {}
         for _, dep in deps:
             self.conanfile.output.info(f"[CPSDeps]: dep {dep.ref.name}")
+
+            cps_in_package = os.path.join(dep.package_folder, f"{dep.ref.name}.cps")
+            if os.path.exists(cps_in_package):
+                mapping[dep.ref.name] = cps_in_package
+                continue
 
             cps = {"Cps-Version": "0.8.1",
                    "Name": dep.ref.name,
@@ -100,3 +106,12 @@ class CPSDeps:
             output_file = os.path.join(self.conanfile.generators_folder, f"{dep.ref.name}.cps")
             cps_json = json.dumps(cps, indent=4)
             save(self.conanfile, output_file, cps_json)
+            mapping[dep.ref.name] = output_file
+
+        name = ["cpsmap",
+                self.conanfile.settings.get_safe("arch"),
+                self.conanfile.settings.get_safe("build_type"),
+                self.conanfile.options.get_safe("shared")]
+        name = "-".join([f for f in name if f]) + ".json"
+        self.conanfile.output.info(f"Generating CPS mapping file: {name}")
+        save(self.conanfile, name, json.dumps(mapping, indent=2))
