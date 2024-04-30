e3143b611af4dfbb39684fe9b6f5ca87ad9e5bbc
memsharded <james@conan.io>
2024-04-30 10:10:37 +0200

    fix unit test

--- conans/test/unittests/tools/cmake/test_cmake_presets_definitions.py
+++ conans/test/unittests/tools/cmake/test_cmake_presets_definitions.py
@@ -45,6 +45,7 @@ def test_cmake_make_program(conanfile):
         assert '-DCMAKE_MAKE_PROGRAM="C:/mymake.exe"' in command
 
     conanfile.run = run
+    conanfile.folders.set_base_source(temp_folder())
     conanfile.conf.define("tools.gnu:make_program", "C:\\mymake.exe")
 
     with mock.patch("platform.system", mock.MagicMock(return_value='Windows')):
