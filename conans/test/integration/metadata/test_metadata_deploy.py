49b39e79e46201341f8e2b6fb2a5a57ace08fd43
memsharded <james@conan.io>
2024-05-20 14:07:45 +0200

    more tests

--- conans/test/integration/metadata/test_metadata_deploy.py
+++ conans/test/integration/metadata/test_metadata_deploy.py
@@ -1,9 +1,10 @@
 import os.path
 import textwrap
+from collections import OrderedDict
 
 import pytest
 
-from conans.test.utils.tools import TestClient
+from conans.test.utils.tools import TestClient, TestServer
 
 
 class TestMetadataDeploy:
@@ -42,7 +43,8 @@ class TestMetadataDeploy:
                                                                              d.ref.name))
            """)
 
-        c = TestClient(default_server_user=True, light=True)
+        servers = OrderedDict([("default", TestServer()), ("remote2", TestServer())])
+        c = TestClient(servers=servers, inputs=2 * ["admin", "password"], light=True)
         c.save({"conanfile.py": conanfile,
                 "deploy.py": deploy})
         c.run("create . --name=pkg1")
@@ -73,6 +75,34 @@ class TestMetadataDeploy:
         c.run("list -g=graph.json -f=json", redirect_stdout="mylist.json")
         c.run("pkglist find-remote mylist.json -f=json", redirect_stdout="pkg_remotes.json")
         c.run("download --list=pkg_remotes.json -r=default --metadata=*")
+
+        # Now we will have the metadata in cache and we can deploy it
+        c.run("install --requires=pkg1/0.1 --requires=pkg2/0.1 --deployer=deploy")
+        assert "some logs pkg1!!!" in c.load("pkgs/pkg1/logs/mylogs.txt")
+        assert "some logs pkg2!!!" in c.load("pkgs/pkg2/logs/mylogs.txt")
+        assert "srclog pkg1!!!" in c.load("recipes/pkg1/logs/src.log")
+        assert "srclog pkg2!!!" in c.load("recipes/pkg2/logs/src.log")
+
+    def test_multi_remote(self, client):
+        # But the remote story is more complex, metadata is not retrieved by default,
+        # we need to iterate the remotes if data coming from multiple remotes
+        c = client
+        c.run("upload pkg1* -c -r=default")
+        c.run("upload pkg2* -c -r=remote2")
+        c.run("remove * -c")
+        # First install without metadata
+        c.run("install --requires=pkg1/0.1 --requires=pkg2/0.1")
+        # So this will not deploy metadata
+        c.run("install --requires=pkg1/0.1 --requires=pkg2/0.1 --deployer=deploy -f=json",
+              redirect_stdout="graph.json")
+        assert not os.path.exists(os.path.join(c.current_folder, "pkgs"))
+
+        # We can obtain the pkg-list for the graph, then "find-remote" and download the metadata
+        c.run("list -g=graph.json -f=json", redirect_stdout="mylist.json")
+        c.run("pkglist find-remote mylist.json -f=json", redirect_stdout="pkg_remotes.json")
+        # we need to ITERATE the remotes
+        c.run("download --list=pkg_remotes.json -r=default --metadata=*")
+        c.run("download --list=pkg_remotes.json -r=remote2 --metadata=*")
 
         # Now we will have the metadata in cache and we can deploy it
         c.run("install --requires=pkg1/0.1 --requires=pkg2/0.1 --deployer=deploy")
