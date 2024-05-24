a856dd076251d32e91831aeed34fc26b914ead0e
Ivan Komissarov <abbapoh@gmail.com>
2024-05-24 10:58:45 +0300

    Fix issue with infinite loop in tests

--- test/unittests/tools/qbs/test_qbsdeps.py
+++ test/unittests/tools/qbs/test_qbsdeps.py
@@ -4,6 +4,7 @@ from mock import Mock
 from conan.tools.qbs import QbsDeps
 from conan import ConanFile
 from conans.model.conanfile_interface import ConanFileInterface
+from conans.model.conf import ConfDefinition
 from conans.model.dependencies import ConanFileDependencies
 from conans.model.build_info import CppInfo
 from conans.model.recipe_ref import RecipeReference
@@ -24,6 +25,8 @@ def test_content_qbsdeps():
     conanfile.settings.arch = "x86"
     conanfile.settings.compiler = "gcc"
     conanfile.settings.os = "Windows"
+    conanfile.settings_build = conanfile.settings
+    conanfile.conf = ConfDefinition()
     from conan.tools.env import Environment
     conanfile._conan_buildenv = Environment()
     conanfile._conan_buildenv.define("MyVar", "MyValue")
@@ -36,35 +39,34 @@ def test_content_qbsdeps():
     conanfile_dep._conan_node.ref = RecipeReference.loads("OriginalDepName/1.0")
     conanfile_dep._conan_node.context = "host"
     conanfile_dep.settings = conanfile.settings
+    conanfile_dep._conan_dependencies = ConanFileDependencies({})
+    conanfile_dep._conan_node.transitive_deps = {}
 
-    # TODO: this loops e.g. with VirtualBuildEnv
-    with mock.patch('conan.ConanFile.dependencies', new_callable=mock.PropertyMock) as mock_deps:
-        req = Requirement(RecipeReference.loads("OriginalDepName/1.0"))
-        mock_deps.return_value = ConanFileDependencies({req: ConanFileInterface(conanfile_dep)})
+    req = Requirement(RecipeReference.loads("OriginalDepName/1.0"))
+    conanfile._conan_dependencies = ConanFileDependencies({req: ConanFileInterface(conanfile_dep)})
+    conanfile._conan_node.transitive_deps = {}
 
-        qbsdeps = QbsDeps(conanfile)
-        files = qbsdeps.content
+    qbsdeps = QbsDeps(conanfile)
+    files = qbsdeps.content
 
-        assert 'common.json' in files
-        assert 'modules/OriginalDepName.json' in files
-        # common_content = files['common.json'].get_content()
-        # assert 'MyValue' == common_content.get('build_env').get('MyVar')
-        # assert 1 == common_content.get('format_version')
-        module_content = files['modules/OriginalDepName.json'].get_content()
+    assert 'common.json' in files
+    assert 'modules/OriginalDepName.json' in files
+    common_content = files['common.json'].get_content()
+    assert 'MyValue' == common_content.get('build_env').get('MyVar')
+    assert 1 == common_content.get('format_version')
+    module_content = files['modules/OriginalDepName.json'].get_content()
 
-        assert 'OriginalDepName' == module_content.get('package_name')
-        assert '1.0' == module_content.get('version')
-        assert 'settings' in module_content
-        assert 'Release' == module_content.get('settings').get('build_type')
-        assert 'gcc' == module_content.get('settings').get('compiler')
-        assert 'x86' == module_content.get('settings').get('arch')
-        assert 'Windows' == module_content.get('settings').get('os')
-        # we patch all instances, can we avoid that in the dep?
-        module_dep = {'name': 'OriginalDepName', 'version': '1.0'}
-        assert [module_dep] == module_content.get('dependencies')
-        assert 'cpp_info' in module_content
-        assert 'includedirs' in module_content['cpp_info']
-        assert 'libdirs' in module_content['cpp_info']
+    assert 'OriginalDepName' == module_content.get('package_name')
+    assert '1.0' == module_content.get('version')
+    assert 'settings' in module_content
+    assert 'Release' == module_content.get('settings').get('build_type')
+    assert 'gcc' == module_content.get('settings').get('compiler')
+    assert 'x86' == module_content.get('settings').get('arch')
+    assert 'Windows' == module_content.get('settings').get('os')
+    assert [] == module_content.get('dependencies')
+    assert 'cpp_info' in module_content
+    assert 'includedirs' in module_content['cpp_info']
+    assert 'libdirs' in module_content['cpp_info']
 
 
 def test_cpp_info_name_qbsdeps():
@@ -89,12 +91,15 @@ def test_cpp_info_name_qbsdeps():
     conanfile_dep._conan_node.context = "host"
     conanfile_dep.settings = conanfile.settings
 
-    with mock.patch('conan.ConanFile.dependencies', new_callable=mock.PropertyMock) as mock_deps:
-        req = Requirement(RecipeReference.loads("OriginalDepName/1.0"))
-        mock_deps.return_value = ConanFileDependencies({req: ConanFileInterface(conanfile_dep)})
+    conanfile_dep._conan_dependencies = ConanFileDependencies({})
+    conanfile_dep._conan_node.transitive_deps = {}
 
-        qbsdeps = QbsDeps(conanfile)
-        files = qbsdeps.content
+    req = Requirement(RecipeReference.loads("OriginalDepName/1.0"))
+    conanfile._conan_dependencies = ConanFileDependencies({req: ConanFileInterface(conanfile_dep)})
+    conanfile._conan_node.transitive_deps = {}
 
-        assert 'common.json' in files
-        assert 'modules/MySuperPkg1.json' in files
+    qbsdeps = QbsDeps(conanfile)
+    files = qbsdeps.content
+
+    assert 'common.json' in files
+    assert 'modules/MySuperPkg1.json' in files
