42fdd12c5039ba7b913e0811514fcc43f26cbe52
Francisco Ramirez de Anton <franchuti688@gmail.com>
2024-05-30 13:28:44 +0200

    docstrings

--- conan/tools/google/bazeldeps.py
+++ conan/tools/google/bazeldeps.py
@@ -270,9 +270,9 @@ class _BazelDependenciesBZLGenerator:
     repository_rules_content = textwrap.dedent("""\
         # This bazel repository rule is used to load Conan dependencies into the Bazel workspace.
         # It's used by a generated module file that provides information about the conan packages.
-        # Each conan package is loaded into a bazel repository rule, with having the name of the package.
-        # The whole method is based on symlinks to not copy the whole package into the Bazel workspace,
-        # which is expensive.
+        # Each conan package is loaded into a bazel repository rule, with having the name of the
+        # package. The whole method is based on symlinks to not copy the whole package into the
+        # Bazel workspace, which is expensive.
         def _conan_dependency_repo(rctx):
             package_path = rctx.workspace_root.get_child(rctx.attr.package_path)
 
