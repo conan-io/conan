1a7561892073408664ffe35808e017b288376854
Ivan Komissarov <ABBAPOH@gmail.com>
2024-06-09 21:03:36 +0300

    Qbs: Add test for multiple configurations (#16435)
    * qbs: fix install
    
    In the previous PR, the --install-root was accidentally removed.
    Bring it back now.
    
    * qbs: add multiple configuration test
    
    * Remove build_type

--- test/functional/toolchains/qbs/test_qbs.py
+++ test/functional/toolchains/qbs/test_qbs.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 import textwrap
 
@@ -100,3 +101,46 @@ def test_qbs_specific_products():
 
     client.run("create .")
     assert "--products hello,hello" in client.out
+
+
+@pytest.mark.tool("qbs")
+def test_qbs_multiple_configurations():
+    client = TestClient()
+
+    context = {
+        "name": "hello",
+        "version": "2.0",
+        "package_name": "hello"
+    }
+
+    conanfile = textwrap.dedent('''
+    import os
+
+    from conan import ConanFile
+    from conan.tools.qbs import Qbs
+
+    class Recipe(ConanFile):
+        name = "hello"
+        version = "1.0"
+
+        exports_sources = "*.cpp", "*.h", "*.qbs"
+        settings = "os", "compiler", "arch"
+
+        def build(self):
+            qbs = Qbs(self)
+            qbs.add_configuration("release", {"qbs.debugInformation": False})
+            qbs.add_configuration("debug", {"qbs.debugInformation": True})
+            qbs.build()
+        ''')
+
+    client.save({
+        "conanfile.py": conanfile,
+        "hello.cpp": gen_file(source_cpp, **context),
+        "hello.h": gen_file(source_h, **context),
+        "hello.qbs": gen_file(qbs_lib_file, **context),
+    }, clean_first=True)
+
+    client.run("create .")
+    build_folder = client.created_layout().build()
+    assert os.path.exists(os.path.join(build_folder, "release"))
+    assert os.path.exists(os.path.join(build_folder, "debug"))
