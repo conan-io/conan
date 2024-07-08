812d15f43aca27ad84f0003e3be61f7826904ce4
memsharded <james@conan.io>
2024-07-08 13:48:12 +0200

    new test

--- test/functional/revisions_test.py
+++ test/functional/revisions_test.py
@@ -699,3 +699,39 @@ def test_reupload_older_revision():
     c.run(f"remove pkg/0.1#{rrev1} -r=default -c")
     c.run("upload * -r=default -c --force")
     check_order(inverse=True)
+
+
+@pytest.mark.artifactory_ready
+def test_reupload_older_revision_new_binaries():
+    """ upload maintains the server history
+        https://github.com/conan-io/conan/pull/16621
+    """
+    c = TestClient(default_server_user=True)
+    c.save({"conanfile.py": GenConanfile("pkg", "0.1")})
+    c.run("export .")
+    rrev1 = c.exported_recipe_revision()
+    c.run("upload * -r=default -c")
+    c.save({"conanfile.py": GenConanfile("pkg", "0.1").with_class_attribute("potato = 42")})
+    c.run("export .")
+    rrev2 = c.exported_recipe_revision()
+    c.run("upload * -r=default -c")
+
+    def check_order(inverse=False):
+        c.run("list pkg/0.1#* -r=default")
+        out = str(c.out)
+        assert rrev1 in out
+        assert rrev2 in out
+        if inverse:
+            assert out.find(rrev1) > out.find(rrev2)
+        else:
+            assert out.find(rrev1) < out.find(rrev2)
+
+    check_order()
+
+    # If we create the same older revision, and upload, still the same order
+    c.save({"conanfile.py": GenConanfile("pkg", "0.1")})
+    c.run("export .")
+    rrev3 = c.exported_recipe_revision()
+    assert rrev3 == rrev1
+    c.run(f"upload pkg*#{rrev3} -r=default -c --force")
+    check_order()
