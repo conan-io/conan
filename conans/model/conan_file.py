73f5167aff5456d4126b520705ad026b397518de
Abril Rincón Blanco <git@rinconblanco.es>
2024-07-22 17:25:37 +0200

    Add access to immutable_package_folder from conanfile/conanfileinterfaze

--- conans/model/conan_file.py
+++ conans/model/conan_file.py
@@ -319,6 +319,10 @@ class ConanFile:
         return self.folders.base_package
 
     @property
+    def immutable_package_folder(self):
+        return self.folders.immutable_package_folder
+
+    @property
     def install_folder(self):
         return self.folders.install_folder
 
