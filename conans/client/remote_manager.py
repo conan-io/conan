6a109f4cce3db46d1759e58763f2e3a2cc2afd02
Adam Gross <grossag@vmware.com>
2024-07-22 11:36:36 -0400

    Fix typo in parameter which broke tests

--- conans/client/remote_manager.py
+++ conans/client/remote_manager.py
@@ -287,7 +287,7 @@ def uncompress_file(src_path, dest_folder, scope=None):
 
         with open(src_path, mode='rb') as file_handler:
             tar_extract(file_handler, dest_folder,
-                        is_tar_zstd=src_path.endswith(".tar.zst"))
+                        is_tar_zst=src_path.endswith(".tar.zst"))
     except Exception as e:
         error_msg = "Error while extracting downloaded file '%s' to %s\n%s\n"\
                     % (src_path, dest_folder, str(e))
