d72b085e12ca2a54606b6d19ac2ab1f574dd761c
memsharded <james@conan.io>
2024-09-23 22:35:04 +0200

    fix test

--- test/integration/test_source_download_password.py
+++ test/integration/test_source_download_password.py
@@ -1,5 +1,6 @@
 import json
 import os
+import platform
 import sys
 import textwrap
 from shutil import copy
@@ -85,12 +86,16 @@ def test_source_credentials_only_download():
         c.run("download pkg/0.1 -r=default")
 
 
-@pytest.mark.skipif(sys.version_info.minor < 12, reason="Extraction filters only Python 3.12")
+@pytest.mark.skipif(sys.version_info.minor < 12 or platform.system() == "Windows",
+                    reason="Extraction filters only Python 3.12, using symlinks (not Windows)")
 def test_blocked_malicius_tgz():
     folder = temp_folder()
     f = os.path.join(folder, "myfile.txt")
     save(f, "The contents")
-    tgz_path = compress_files({f: f}, "myfiles.tgz", dest_dir=folder)
+    s = os.path.join(folder, "mylink.txt")
+    os.symlink(f, s)
+    tgz_path = compress_files({f: f, s: s}, "myfiles.tgz", dest_dir=folder)
+    os.remove(f)
 
     conan_file = textwrap.dedent("""
         from conan import ConanFile
@@ -109,7 +114,7 @@ def test_blocked_malicius_tgz():
             copy(tgz_path, os.getcwd())
         mock_download.side_effect = download_zip
         client.run("create . -c tools.files.unzip:filter=data", assert_error=True)
-        assert "OutsideDestinationError" in client.out
+        assert "AbsoluteLinkError" in client.out
         client.save({"conanfile.py": conan_file.format("extract_filter='fully_trusted'")})
         client.run("create . ")  # Doesn't fail now
         # user conf has precedence
