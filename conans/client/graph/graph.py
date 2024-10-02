e327c67a09a2eb274799910004426369b6889340
memsharded <james@conan.io>
2024-10-02 20:47:42 +0200

    poc for package manifests and SBOM

--- conans/client/graph/graph.py
+++ conans/client/graph/graph.py
@@ -69,6 +69,23 @@ class Node(object):
         self.is_conf = False
         self.replaced_requires = {}  # To track the replaced requires for self.dependencies[old-ref]
 
+    def subgraph(self):
+        nodes = [self]
+        opened = [self]
+        while opened:
+            new_opened = []
+            for o in opened:
+                for n in o.neighbors():
+                    if n not in nodes:
+                        nodes.append(n)
+                    if n not in opened:
+                        new_opened.append(n)
+            opened = new_opened
+
+        graph = DepsGraph()
+        graph.nodes = nodes
+        return graph
+
     def __lt__(self, other):
         """
         @type other: Node
