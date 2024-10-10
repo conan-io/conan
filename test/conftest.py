c521dd43e626a3ceaf4146d28aad4077c37a7810
czoido <mrgalleta@gmail.com>
2024-10-10 08:00:56 +0200

    get android ndk from env

--- test/conftest.py
+++ test/conftest.py
@@ -178,7 +178,7 @@ tools_locations = {
         "exe": "ndk-build",
         "default": "system",
         "system": {
-            "path": {'Darwin': '/Users/runner/Library/Android/sdk'}
+            "path": {'Darwin': os.getenv("ANDROID_NDK")}
         }
     },
     "qbs": {"disabled": True},
