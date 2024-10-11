7af6812fd7941e4588a69e0ee2898e4339d4a293
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2024-10-11 11:35:21 +0200

    test register_env_script and minor changes

--- test/integration/generators/test_generators_from_br.py
+++ test/integration/generators/test_generators_from_br.py
@@ -1,4 +1,3 @@
-import re
 import textwrap
 
 from conan.test.assets.genconanfile import GenConanfile
@@ -36,13 +35,16 @@ def test_inject_vars():
     import os
     tc = TestClient(light=True)
     tc.save({
-    "tool/envars_generator.sh": textwrap.dedent("""
+    "tool/envars_generator1.sh": textwrap.dedent("""
     export VAR1="value1"
     export PATH="$PATH:/additional/path"
     """),
+    "tool/envars_generator2.sh": textwrap.dedent("""
+    export VAR2="value2"
+    """),
     "tool/conanfile.py": textwrap.dedent("""
     from conan import ConanFile
-    from conan.tools.env import create_env_script
+    from conan.tools.env import create_env_script, register_env_script
     import os
 
     class GeneratorSet:
@@ -53,6 +55,7 @@ def test_inject_vars():
             content = 'call envars_generator.sh'
             name = f"conantest.sh"
             create_env_script(self.conanfile, content, name)
+            register_env_script(self.conanfile, "tool/envars_generator2.sh")
 
     class ToolConan(ConanFile):
         name = "tool"
@@ -64,11 +67,8 @@ def test_inject_vars():
              "conanfile.py": GenConanfile("app", "1.0").with_tool_requires("tool/0.1")})
 
     tc.run("create tool")
-    tc.run(f"create . ")
+    tc.run(f"install . ")
 
-    regex = r'Build folder (.+)'
-    out = tc.out
-    build_folder = re.findall(regex, out)[0]
-    file_path = os.path.join(build_folder, "conanbuild.sh")
-    with open(file_path, 'r') as file:
-         assert "conantest.sh" in file.read()
+    content = tc.load("conanbuild.sh")
+    assert "conantest.sh" in content
+    assert "envars_generator2.sh" in content
