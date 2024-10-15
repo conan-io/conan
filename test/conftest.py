38bee4306069aa544ee7ff21431415576fd88d05
Francisco Ramirez de Anton <franchuti688@gmail.com>
2024-10-15 14:03:38 +0200

    To powershell

--- test/conftest.py
+++ test/conftest.py
@@ -24,12 +24,12 @@ tools_locations = {
         "3.15": {},
         "3.16": {"disabled": True},
         "3.17": {"disabled": True},
-        "3.19": {"path": {"Windows": "C:/ws/cmake/cmake-3.19.7-win64-x64/bin"}},
+        "3.19": {"path": {"Windows": "C:/ws/cmake/cmake-3.19.7-windows-x86_64/bin"}},
         # To explicitly skip one tool for one version, define the path as 'skip-tests'
         # if you don't define the path for one platform it will run the test with the
         # tool in the path. For example here it will skip the test with CMake in Darwin but
         # in Linux it will run with the version found in the path if it's not specified
-        "3.23": {"path": {"Windows": "C:/ws/cmake/cmake-3.19.7-win64-x64/bin",
+        "3.23": {"path": {"Windows": "C:/ws/cmake/cmake-3.19.7-windows-x86_64/bin",
                           "Darwin": "skip-tests"}},
     },
     'ninja': {
@@ -67,17 +67,17 @@ tools_locations = {
     'cmake': {
         "default": "3.15",
         "3.15": {
-            "path": {'Windows': f'C:/Tools/CMake/3.15.7/cmake-3.15.7-win64-x64/bin',
+            "path": {'Windows': 'C:/Tools/CMake/3.15.7/cmake-3.15.7-windows-x86_64/bin',
                      'Darwin': '/Users/jenkins/cmake/cmake-3.15.7/bin',
                      'Linux': '/usr/share/cmake-3.15.7/bin'}
         },
         "3.19": {
-            "path": {'Windows': f'C:/Tools/CMake/3.19.7/cmake-3.19.7-win64-x64/bin',
+            "path": {'Windows': 'C:/Tools/CMake/3.19.7/cmake-3.19.7-windows-x86_64/bin',
                      'Darwin': '/Users/jenkins/cmake/cmake-3.19.7/bin',
                      'Linux': '/usr/share/cmake-3.19.7/bin'}
         },
         "3.23": {
-            "path": {'Windows': f'C:/Tools/CMake/3.23.5/cmake-3.23.5-win64-x64/bin',
+            "path": {'Windows': 'C:/Tools/CMake/3.23.5/cmake-3.23.5-windows-x86_64/bin',
                      'Darwin': '/Users/jenkins/cmake/cmake-3.23.1/bin',
                      'Linux': "/usr/share/cmake-3.23.5/bin"}
         },
@@ -147,10 +147,10 @@ tools_locations = {
     'bazel': {
         "default": "6.3.2",
         "6.3.2": {"path": {'Linux': '/usr/share/bazel-6.3.2/bin',
-                           'Windows': 'C:/Tools/bazel/6.3.2',
+                           'Windows': 'C:/Tools/bazel/6.3.2/bazel-6.3.2-windows-x86_64',
                            'Darwin': '/Users/jenkins/bazel-6.3.2/bin'}},
         "7.1.2": {"path": {'Linux': '/usr/share/bazel-7.1.2/bin',
-                           'Windows': 'C:/Tools/bazel/7.1.2',
+                           'Windows': 'C:/Tools/bazel/7.1.2/bazel-7.1.2-windows-x86_64',
                            'Darwin': '/Users/jenkins/bazel-7.1.2/bin'}},
     },
     'premake': {
@@ -248,7 +248,7 @@ def _get_individual_tool(name, version):
         tool_path = tool_path.replace("/", "\\") if tool_platform == "Windows" and tool_path is not None else tool_path
         # To allow to skip for a platform, we can put the path to None
         # "cmake": { "3.23": {
-        #               "path": {'Windows': 'C:/cmake/cmake-3.23.1-win64-x64/bin',
+        #               "path": {'Windows': 'C:/cmake/cmake-3.23.1-windows-x86_64/bin',
         #                        'Darwin': '/Users/jenkins/cmake/cmake-3.23.1/bin',
         #                        'Linux': None}}
         #          }
