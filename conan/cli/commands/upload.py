8cd93a45db2b51b260c79671e6f2ee2f09413df5
memsharded <james@conan.io>
2024-10-17 01:52:17 +0200

    refactor conans-conan userio

--- conan/cli/commands/upload.py
+++ conan/cli/commands/upload.py
@@ -4,7 +4,7 @@ from conan.api.output import ConanOutput
 from conan.cli import make_abs_path
 from conan.cli.command import conan_command, OnceArgument
 from conan.cli.commands.list import print_list_json, print_serial
-from conans.client.userio import UserInput
+from conan.api.input import UserInput
 from conan.errors import ConanException
 
 
