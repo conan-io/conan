6e806a67faf78dee0a87c0a62ee55507088eaa28
memsharded <james@conan.io>
2024-11-29 19:16:03 +0100

    new

--- test/functional/toolchains/cmake/test_presets_inherit.py
+++ test/functional/toolchains/cmake/test_presets_inherit.py
@@ -91,3 +91,73 @@ def test_cmake_presets_with_user_presets_file():
         c.run_command("build\\Release\\foo")
 
     assert "Hello World Release!" in c.out
+
+
+@pytest.mark.tool("cmake", "3.23")
+def test_inherit_custom_folders():
+    # https://github.com/conan-io/conan/issues/17324
+    conanfile = textwrap.dedent("""\
+        from conan import ConanFile
+        from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout
+        from conan.tools.files import load, save
+        import os
+
+        class CompressorRecipe(ConanFile):
+            # Binary configuration
+            settings = "os", "compiler", "build_type", "arch"
+
+            def layout(self):
+                self.folders.build_folder_vars = ["settings.compiler"]
+                cmake_layout(self)
+
+            def generate(self):
+                deps = CMakeDeps(self)
+                deps.generate()
+                tc = CMakeToolchain(self)
+                tc.user_presets_path = "ConanCMakePresets.json"
+                tc.generate()
+        """)
+    presets = textwrap.dedent("""\
+        {
+            "version": 4,
+            "include": ["ConanCMakePresets.json"],
+            "configurePresets": [
+                {
+                    "name": "gcc",
+                    "generator": "Ninja Multi-Config",
+                    "inherits": ["conan-gcc"],
+                    "binaryDir": "${sourceDir}/build"
+                }
+            ],
+            "buildPresets": [
+                {
+                    "name": "gcc-release",
+                    "configuration": "Release",
+                    "configurePreset": "gcc",
+                    "inherits": ["conan-gcc-release"]
+                },
+                {
+                    "name": "gcc-debug",
+                    "configuration": "Debug",
+                    "configurePreset": "gcc",
+                    "inherits": ["conan-gcc-debug"]
+                }
+            ]
+        }
+        """)
+    c = TestClient()
+    c.save({"conanfile.py": conanfile,
+            "CMakeLists.txt": "",  # Irrelevant, only needed for Conan to generate the presets
+            "CMakePresets.json": presets})
+    conf = '-s compiler=gcc -s compiler.version=11 -s compiler.libcxx=libstdc++ ' \
+           '-c tools.cmake.cmaketoolchain:generator="Ninja Multi-Config"'
+    c.run(f"install . --build=missing -s build_type=Release {conf}")
+    print(c.out)
+    print(c.load("build/gcc/generators/CMakePresets.json"))
+    c.run(f"install . --build=missing -s build_type=Debug {conf}")
+    print(c.out)
+    print(c.load("build/gcc/generators/CMakePresets.json"))
+    c.run_command("cmake --list-presets")
+    print(c.out)
+    assert "gcc" in c.out
+    assert "conan-gcc" in c.out
