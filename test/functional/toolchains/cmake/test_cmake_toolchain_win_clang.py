89ea6fe2210affb24b977b1c2644135123e54074
czoido <mrgalleta@gmail.com>
2024-11-29 07:19:47 +0100

    revert toolset

--- test/functional/toolchains/cmake/test_cmake_toolchain_win_clang.py
+++ test/functional/toolchains/cmake/test_cmake_toolchain_win_clang.py
@@ -157,7 +157,7 @@ class TestVSClangCL:
         # My local is 17, but CI ClangCL still 16
         assert "main __clang_major__18" in client.out
         # Check this! Clang compiler in Windows is reporting MSC_VER and MSVC_LANG!
-        assert "main _MSC_VER1942" in client.out
+        assert "main _MSC_VER1938" in client.out
         assert "main _MSVC_LANG201703" in client.out
         assert "main _M_X64 defined" in client.out
         assert "main __x86_64__ defined" in client.out
