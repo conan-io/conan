82119cb7ace654477d2e4dd7848dda5c674b3645
memsharded <james@conan.io>
2024-12-18 00:16:07 +0100

    fix missing is_test fix of 2.9.3

--- conan/__init__.py
+++ conan/__init__.py
@@ -1,5 +1,5 @@
 from conans.model.conan_file import ConanFile
 from conan.tools.scm import Version as _Version
 
-__version__ = '2.10.2'
+__version__ = '2.10.3'
 conan_version = _Version(__version__)
