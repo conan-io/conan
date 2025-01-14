1d8c20e5c15d0d0f9e08ecc3be65c81c1baae29c
maitrey <maitreymishra23@gmail.com>
2025-01-14 16:30:03 +0100

    Updated to use maxsplit=1 (#17571)
    * Updated to use maxsplit=1
    
    * Add tests, unquote paths with spaces
    
    ---------
    
    Co-authored-by: Abril Rincón Blanco <git@rinconblanco.es>

--- conan/tools/scm/git.py
+++ conan/tools/scm/git.py
@@ -137,7 +137,9 @@ class Git:
             return bool(status)
         # Parse the status output, line by line, and match it with "_excluded"
         lines = [line.strip() for line in status.splitlines()]
-        lines = [line.split()[1] for line in lines if line]
+        # line is of the form STATUS PATH, get the path by splitting
+        # (Taking into account that STATUS is one word, PATH might be many)
+        lines = [line.split(maxsplit=1)[1].strip('"') for line in lines if line]
         lines = [line for line in lines if not any(fnmatch.fnmatch(line, p) for p in self._excluded)]
         self._conanfile.output.debug(f"Filtered git status: {lines}")
         return bool(lines)
