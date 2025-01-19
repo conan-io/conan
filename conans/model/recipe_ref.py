9a320cc9867b16f98d6088f2ec79a8f31c2dc29a
memsharded <james@conan.io>
2025-01-19 16:48:49 +0100

    review

--- conans/model/recipe_ref.py
+++ conans/model/recipe_ref.py
@@ -3,9 +3,10 @@ from conan.internal.model.recipe_ref import RecipeReference as _RecipeReference
 
 _msg = """
 *******************************************************************
-private '{}'
+Private '{}'
 detected in user code (custom commands, extensions, recipes, etc).
-Stop using it, use only public documented APIs.
+Please stop using it, use only public documented APIs, as this
+will break in next releases.
 *******************************************************************
 """
 
