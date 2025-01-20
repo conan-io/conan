ac018fb003bd1da03d0f75575ea8445a174cf883
memsharded <james@conan.io>
2025-01-20 11:34:08 +0100

    refactoring model

---
+++ conan/api/model/__init__.py
@@ -0,0 +1,3 @@
+from conan.api.model.remote import Remote
+from conan.api.model.refs import RecipeReference, PkgReference
+from conan.api.model.list import PackagesList, MultiPackagesList, ListPattern
