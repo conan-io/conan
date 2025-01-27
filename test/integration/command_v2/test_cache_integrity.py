26f943efdda5945cc409a14a08767fdfa00a9ccc
memsharded <james@conan.io>
2025-01-27 10:16:03 +0100

    more checks in tests

--- test/integration/command_v2/test_cache_integrity.py
+++ test/integration/command_v2/test_cache_integrity.py
@@ -47,14 +47,19 @@ def test_cache_integrity_missing_recipe_manifest():
     layout = t.exported_layout()
     manifest = os.path.join(layout.export(), "conanmanifest.txt")
     os.remove(manifest)
+    t.run("create . --name pkg3 --version=3.0")
 
     t.run("cache check-integrity *", assert_error=True)
     assert "pkg1/1.0: Integrity checked: ok" in t.out
     assert "ERROR: pkg2/2.0#4d670581ccb765839f2239cc8dff8fbd: Manifest missing" in t.out
+    assert "pkg3/3.0: Integrity checked: ok" in t.out
+    assert "ERROR: There are corrupted artifacts, check the error logs" in t.out
 
     t.run("remove pkg2* -c")
     t.run("cache check-integrity *")
     assert "pkg1/1.0:da39a3ee5e6b4b0d3255bfef95601890afd80709: Integrity checked: ok" in t.out
+    assert "pkg3/3.0: Integrity checked: ok" in t.out
+    assert "Integrity check: ok" in t.out
 
 
 def test_cache_integrity_missing_package_manifest():
@@ -65,16 +70,21 @@ def test_cache_integrity_missing_package_manifest():
     layout = t.created_layout()
     manifest = os.path.join(layout.package(), "conanmanifest.txt")
     os.remove(manifest)
+    t.run("create . --name pkg3 --version=3.0")
 
     t.run("cache check-integrity *", assert_error=True)
     assert "pkg1/1.0: Integrity checked: ok" in t.out
     assert "ERROR: pkg2/2.0#4d670581ccb765839f2239cc8dff8fbd" \
            ":da39a3ee5e6b4b0d3255bfef95601890afd80709" \
            "#0ba8627bd47edc3a501e8f0eb9a79e5e: Manifest missing" in t.out
+    assert "pkg3/3.0: Integrity checked: ok" in t.out
+    assert "ERROR: There are corrupted artifacts, check the error logs" in t.out
 
     t.run("remove pkg2* -c")
     t.run("cache check-integrity *")
     assert "pkg1/1.0:da39a3ee5e6b4b0d3255bfef95601890afd80709: Integrity checked: ok" in t.out
+    assert "pkg3/3.0: Integrity checked: ok" in t.out
+    assert "Integrity check: ok" in t.out
 
 
 def test_cache_integrity_missing_package_conaninfo():
