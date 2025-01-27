fd65f82b34ea58c16a7b616d68d0679e8e43ed1b
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-01-27 13:00:57 +0100

    Less docs for remote

--- conan/api/model/remote.py
+++ conan/api/model/remote.py
@@ -3,21 +3,16 @@ LOCAL_RECIPES_INDEX = "local-recipes-index"
 
 class Remote:
     """
-    The Remote class represents a remote registry of packages. It's a read-only opaque object that
+    The ``Remote`` class represents a remote registry of packages. It's a read-only opaque object that
     should not be created directly, but obtained from the relevant ``RemotesAPI`` subapi methods.
     """
     def __init__(self, name, url, verify_ssl=True, disabled=False, allowed_packages=None,
                  remote_type=None):
         self.name = name  # Read only, is the key
-        """User-facing remote name"""
         self.url = url
-        """URL endpoint of the Conan remote"""
         self.verify_ssl = verify_ssl
-        #"""Whether to verify SSL certificates"""
         self.disabled = disabled
-        #"""If this remote is disabled, it won't be taken into account for Conan operations"""
         self.allowed_packages = allowed_packages
-        #"""Which packages this remote is allowed to fetch, or None if all packages are allowed"""
         self.remote_type = remote_type
         self._caching = {}
 
@@ -41,7 +36,4 @@ class Remote:
         return str(self)
 
     def invalidate_cache(self):
-        """
-        Clears the package existence cache for this remote
-        """
         self._caching = {}
