83f6390f3d5eb4e94a689cef5d9741ddd1804e7e
memsharded <james@conan.io>
2025-02-07 20:19:08 +0100

    fix test

--- test/integration/toolchains/cmake/test_cmaketoolchain.py
+++ test/integration/toolchains/cmake/test_cmaketoolchain.py
@@ -806,7 +806,7 @@ def test_variables_types():
     client.run("install . --name=mylib --version=1.0")
 
     toolchain = client.load("conan_toolchain.cmake")
-    assert 'set(FOO ON CACHE BOOL "Variable FOO conan-toolchain defined")' in toolchain
+    assert 'set(FOO ON")' in toolchain
 
 
 def test_android_c_library():
