bd13470213c7a9f7cc73d831b138408957dbcd14
memsharded <james@conan.io>
2025-02-12 14:50:58 +0100

    fix docker

--- test/conftest.py
+++ test/conftest.py
@@ -163,7 +163,7 @@ tools_locations = {
         "exe": "premake5",
         "default": "5.0.0",
         "5.0.0": {
-            "path": {'Linux': '/usr/share/bin'}
+            "path": {'Linux': '/usr/share/premake'}
         }
     },
     'xcodegen': {"platform": "Darwin"},
