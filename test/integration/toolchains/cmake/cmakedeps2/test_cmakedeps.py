0b2accaefaf0f6530ef88df274bef9f4257fd9c8
memsharded <james@conan.io>
2025-03-12 00:10:11 +0100

    just the test

--- test/integration/toolchains/cmake/cmakedeps2/test_cmakedeps.py
+++ test/integration/toolchains/cmake/cmakedeps2/test_cmakedeps.py
@@ -200,3 +200,92 @@ def test_autolink_pragma():
     c.run("create . --name=pkg --version=0.1 -c tools.cmake.cmakedeps:new=will_break_next")
     assert "CMakeConfigDeps: cmake_set_interface_link_directories deprecated and invalid. " \
            "The package 'package_info()' must correctly define the (CPS) information" in c.out
+
+
+def test_cmakeconfigdeps_components():
+    gtest = textwrap.dedent("""
+    from conan import ConanFile
+    from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+    from conan.tools.files import copy, get
+    from conan.tools.scm import Version
+
+    class GtestConan(ConanFile):
+        name = "gtest"
+        version = "1.16.0"
+        package_type = "static-library"
+        settings = "os", "compiler", "build_type", "arch"
+
+        options = {
+            "shared": [True, False],
+            "build_gmock": [True, False],
+            "no_main": [True, False],
+            "debug_postfix": ["ANY"],
+        }
+        default_options = {
+            "shared": True,
+            "build_gmock": True,
+            "no_main": False,
+            "debug_postfix": "d",
+        }
+
+        def package(self):
+            pass
+
+        def package_info(self):
+            postfix = self.options.get_safe("debug_prefix", "")
+            self.cpp_info.set_property("cmake_file_name", "GTest")
+
+            self.cpp_info.components["gtest"].set_property("cmake_target_name", "GTest::gtest")
+            self.cpp_info.components["gtest"].set_property("cmake_target_aliases", ["GTest::GTest"])
+            #self.cpp_info.components["gtest"].set_property("pkg_config_name", "gtest")
+            self.cpp_info.components["gtest"].libs = [f"gtest{postfix}"]
+
+            self.cpp_info.components["gtest"].type = self.package_type
+            self.cpp_info.components["gtest"].location = f"bin/gtest{postfix}.dll" if self.options.shared else f"lib/gtest{postfix}.lib"
+            self.cpp_info.components["gtest"].link_location = f"lib/gtest{postfix}.lib" if self.options.shared else None
+            self.cpp_info.components["gtest"].languages = ["C++"]
+
+            if self.options.shared:
+                self.cpp_info.components["gtest"].defines.append("GTEST_LINKED_AS_SHARED_LIBRARY=1")
+
+            if not self.options.no_main:
+                self.cpp_info.components["gtest_main"].set_property("cmake_target_name", "GTest::gtest_main")
+                self.cpp_info.components["gtest_main"].set_property("cmake_target_aliases", ["GTest::Main"])
+                #self.cpp_info.components["gtest_main"].set_property("pkg_config_name", "gtest_main")
+                self.cpp_info.components["gtest_main"].libs = [f"gtest_main{postfix}"]
+                self.cpp_info.components["gtest_main"].requires = ["gtest"]
+
+                self.cpp_info.components["gtest_main"].type = self.package_type
+                self.cpp_info.components["gtest_main"].location = f"bin/gtest_main{postfix}.dll" if self.options.shared else f"lib/gtest_main{postfix}.lib"
+                self.cpp_info.components["gtest_main"].link_location = f"lib/gtest_main{postfix}.lib" if self.options.shared else None
+                self.cpp_info.components["gtest_main"].languages = ["C++"]
+
+            # gmock
+            if self.options.build_gmock:
+                self.cpp_info.components["gmock"].set_property("cmake_target_name", "GTest::gmock")
+                #self.cpp_info.components["gmock"].set_property("pkg_config_name", "gmock")
+                self.cpp_info.components["gmock"].libs = [f"gmock{postfix}"]
+                self.cpp_info.components["gmock"].requires = ["gtest"]
+
+                self.cpp_info.components["gmock"].type = self.package_type
+                self.cpp_info.components["gmock"].location = f"bin/gmock{postfix}.dll" if self.options.shared else f"lib/gmock{postfix}.lib"
+                self.cpp_info.components["gmock"].link_location = f"lib/gmock{postfix}.lib" if self.options.shared else None
+                self.cpp_info.components["gmock"].languages = ["C++"]
+
+
+                # gmock_main
+                if not self.options.no_main:
+                    self.cpp_info.components["gmock_main"].set_property("cmake_target_name", "GTest::gmock_main")
+                    #self.cpp_info.components["gmock_main"].set_property("pkg_config_name", "gmock_main")
+                    self.cpp_info.components["gmock_main"].libs = [f"gmock_main{postfix}"]
+                    self.cpp_info.components["gmock_main"].requires = ["gmock"]
+
+                    self.cpp_info.components["gmock_main"].type = self.package_type
+                    self.cpp_info.components["gmock_main"].location = f"bin/gmock_main{postfix}.dll" if self.options.shared else f"lib/gmock_main{postfix}.lib"
+                    self.cpp_info.components["gmock_main"].link_location = f"lib/gmock_main{postfix}.lib" if self.options.shared else None
+                    self.cpp_info.components["gmock_main"].languages = ["C++"]
+        """)
+    c = TestClient()
+    c.save({"conanfile.py": gtest,
+            "test_package/conanfile.py": GenConanfile().with_test("pass")})
+    c.run("create .")
