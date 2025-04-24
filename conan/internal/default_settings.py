4cef45f9296ea9339072210683cea7c332d85ff7
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-04-24 21:24:26 +0200

    Add gcc 15.1

--- conan/internal/default_settings.py
+++ conan/internal/default_settings.py
@@ -112,7 +112,8 @@ compiler:
                     "11", "11.1", "11.2", "11.3", "11.4", "11.5",
                     "12", "12.1", "12.2", "12.3",  "12.4",
                     "13", "13.1", "13.2", "13.3",
-                    "14", "14.1", "14.2"]
+                    "14", "14.1", "14.2",
+                    "15.1"]
         libcxx: [libstdc++, libstdc++11]
         threads: [null, posix, win32, mcf]  # Windows MinGW
         exception: [null, dwarf2, sjlj, seh]  # Windows MinGW
