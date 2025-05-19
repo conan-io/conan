98e697dfc882edaba5c67a2ab429982b5424de81
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-05-19 18:13:49 +0200

    Tests

--- test/functional/command/compare_test.py
+++ test/functional/command/compare_test.py
@@ -14,7 +14,7 @@ from conan.test.utils.tools import TestClient
 ])
 @pytest.mark.parametrize("new_args", [
     "-np=v2 -nr=pkg/2.0",
-    "-nr=pkg/2.0#16fad934f9a90b1da6740cbac9590b53",
+    "-nr=pkg/2.0#{revision}",
 ])
 @pytest.mark.parametrize("formatter", [
     "-f=json --out-file=output.json",
@@ -60,22 +60,27 @@ def test_compare_paths(old_args, new_args, formatter):
     tc.save({
         "v1/conanfile.py": conanfile.format(version="v1"),
         "v1/conandata.yml": conandata_yml.format(version="1.0"),
-        "v1/1.0.txt": "1.0",
+        "v1/v1.txt": "1.0",
         "v1/patches/patch.patch": patch_file.format(version="1.0"),
 
         "v2/conanfile.py": conanfile.format(version="v2"),
         "v2/conandata.yml": conandata_yml.format(version="2.0"),
-        "v2/2.0.txt": "2.0",
+        "v2/v2.txt": "2.0",
         "v2/patches/patch.patch": patch_file.format(version="2.0"),
     })
 
     tc.run("create v1 --version=1.0")
-    v1_path = tc.exported_layout().export()
+    v1_layout = tc.exported_layout()
+    v1_path = v1_layout.export()
+    v1_revision = v1_layout.reference.revision
 
     tc.run("create v2 --version=2.0")
+    v2_revision = tc.exported_layout().reference.revision
 
     tc.run("upload * -r=default -c")
     tc.run("remove * -c")
+
+    new_args = new_args.format(revision=v2_revision)
 
     tc.run(f'compare {old_args} {new_args} {formatter}')
 
@@ -86,5 +91,5 @@ def test_compare_paths(old_args, new_args, formatter):
 
     elif "html" in formatter:
         output_html = tc.load("output.html")
-        assert """<span class="context">--- a(pkg/1.0#147b6df93bdb64119aa4208cb4825184)/es/patches/patch.patch	</span>""" in output_html
+        assert f"""<span class="context">--- a(pkg/1.0#{v1_revision})/es/patches/patch.patch	</span>""" in output_html
 
