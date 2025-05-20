452f7746b5d793cdd32e3f66e3c26dcaf73d45fa
PerseoGI <perseog@jfrog.com>
2025-05-20 16:34:52 +0200

    Restore previous tar_extract fixing tests

--- conan/internal/rest/remote_manager.py
+++ conan/internal/rest/remote_manager.py
@@ -294,7 +294,8 @@ def uncompress_file(src_path, dest_folder, scope=None, compression_plugin=None):
         if compression_plugin:
             compression_plugin.tar_extract(src_path, dest_folder)
         else:
-            tar_extract(src_path, dest_folder)
+            with open(src_path, mode='rb') as file_handler:
+                tar_extract(file_handler, dest_folder)
     except Exception as e:
         error_msg = "Error while extracting downloaded file '%s' to %s\n%s\n"\
                     % (src_path, dest_folder, str(e))
