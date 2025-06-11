263f69e683c513243239d5d3fa5f052b4bbd18a5
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2025-06-11 12:01:23 +0200

    test and use subproject path

--- conan/tools/layout/__init__.py
+++ conan/tools/layout/__init__.py
@@ -6,7 +6,7 @@ def basic_layout(conanfile, src_folder=".", build_folder=None):
 
     conanfile.folders.source = src_folder if not subproject else os.path.join(subproject, src_folder)
     if build_folder:
-        conanfile.folders.build = build_folder
+        conanfile.folders.build = build_folder if not subproject else os.path.join(subproject, build_folder)
     else:
         conanfile.folders.build = "build" if not subproject else os.path.join(subproject, "build")
         if conanfile.settings.get_safe("build_type"):
