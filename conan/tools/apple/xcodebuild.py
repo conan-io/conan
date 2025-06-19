81b0515fdffdeab7022798adb15bdad27e3179d4
Andrey Filipenkov <decapitator@ukr.net>
2025-06-19 13:18:19 +0300

    allow passing custom build config
    # Conflicts:
    #	conan/tools/apple/xcodebuild.py

--- conan/tools/apple/xcodebuild.py
+++ conan/tools/apple/xcodebuild.py
@@ -28,7 +28,7 @@ class XcodeBuild(object):
             sdk = "{}{}".format(self._sdk, self._sdk_version)
         return "SDKROOT={}".format(sdk) if sdk else ""
 
-    def build(self, xcodeproj, target=None):
+    def build(self, xcodeproj, target=None, configuration=None):
         """
         Call to ``xcodebuild`` to build a Xcode project.
 
@@ -36,11 +36,13 @@ class XcodeBuild(object):
         :param target: the target to build, in case this argument is passed to the ``build()``
                        method it will add the ``-target`` argument to the build system call. If not passed, it
                        will build all the targets passing the ``-alltargets`` argument instead.
+        :param configuration: the configuration to build, defaults to profile's ``settings.build_type``.
         :return: the return code for the launched ``xcodebuild`` command.
         """
         target = "-target '{}'".format(target) if target else "-alltargets"
+        build_config = configuration or self._build_type
         cmd = "xcodebuild -project '{}' -configuration {} -arch {} " \
-              "{} {} {}".format(xcodeproj, self._build_type, self._arch, self._sdkroot,
+              "{} {} {}".format(xcodeproj, build_config, self._arch, self._sdkroot,
                                 self._verbosity, target)
 
         deployment_target_key = xcodebuild_deployment_target_key(self._os)
