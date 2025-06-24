30b760b2ad5d7eb235c92aa53468d66ebe7c68a6
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2025-06-24 14:47:07 +0200

    Add build_folder parameter in basic_layout (#18442)
    * add build_folder
    
    * test and use subproject path

--- conan/tools/layout/__init__.py
+++ conan/tools/layout/__init__.py
@@ -1,13 +1,16 @@
 import os
 
 
-def basic_layout(conanfile, src_folder="."):
+def basic_layout(conanfile, src_folder=".", build_folder=None):
     subproject = conanfile.folders.subproject
 
     conanfile.folders.source = src_folder if not subproject else os.path.join(subproject, src_folder)
-    conanfile.folders.build = "build" if not subproject else os.path.join(subproject, "build")
-    if conanfile.settings.get_safe("build_type"):
-        conanfile.folders.build += "-{}".format(str(conanfile.settings.build_type).lower())
+    if build_folder:
+        conanfile.folders.build = build_folder if not subproject else os.path.join(subproject, build_folder)
+    else:
+        conanfile.folders.build = "build" if not subproject else os.path.join(subproject, "build")
+        if conanfile.settings.get_safe("build_type"):
+            conanfile.folders.build += "-{}".format(str(conanfile.settings.build_type).lower())
     conanfile.folders.generators = os.path.join(conanfile.folders.build, "conan")
     conanfile.cpp.build.bindirs = ["."]
     conanfile.cpp.build.libdirs = ["."]
