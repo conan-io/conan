336078f7ade38067f1f701aea86313c2b52971b6
Carlos Zoido <mrgalleta@gmail.com>
2025-06-26 16:46:46 +0200

    wip

--- conan/tools/cmake/cmake.py
+++ conan/tools/cmake/cmake.py
@@ -27,7 +27,7 @@ def _cmake_cmd_line_args(conanfile, generator):
         verbosity = msbuild_verbosity_cmd_line_arg(conanfile)
         if verbosity:
             # trying to avoid issues with powershell and - : characters preceded by --
-            # -verbosity → /verbosity
+            # -verbosity -> /verbosity
             # https://github.com/PowerShell/PowerShell/issues/17399
             if conanfile.conf.get("tools.env.virtualenv:powershell"):
                 verbosity = verbosity.replace('-', '/', 1)
