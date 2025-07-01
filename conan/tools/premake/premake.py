85478d3657011ee92347c8e4569c1e89fda5aaf1
PerseoGI <perseo.gi98@gmail.com>
2025-07-01 10:02:58 +0200

    Preamke generator: fix some issues with windows paths and MSBuild (#18572)
    * Preamke generator: fix some issues with windows paths and MSBuild
    
    * Added integration test case for msbuild_platform

--- conan/tools/premake/premake.py
+++ conan/tools/premake/premake.py
@@ -66,7 +66,7 @@ class Premake:
         """
         if self._premake_conan_toolchain.exists():
             content = Template(self._premake_file_template).render(
-                premake_conan_toolchain=self._premake_conan_toolchain, luafile=self.luafile
+                premake_conan_toolchain=self._premake_conan_toolchain.as_posix(), luafile=self.luafile
             )
             conan_luafile = Path(self._conanfile.build_folder) / self.filename
             save(self._conanfile, conan_luafile, content)
@@ -98,7 +98,7 @@ class Premake:
         # --verbose does not print compilation commands but internal Makefile progress logic
         return " verbose=1" if verbosity == "verbose" else ""
 
-    def build(self, workspace, targets=None):
+    def build(self, workspace, targets=None, msbuild_platform=None):
         """
         Depending on the action, this method will run either ``msbuild`` or ``make`` with ``N_JOBS``.
         You can specify ``N_JOBS`` through the configuration line ``tools.build:jobs=N_JOBS``
@@ -106,11 +106,14 @@ class Premake:
 
         :param workspace: ``str`` Specifies the solution to be compiled (only used by ``MSBuild``).
         :param targets: ``List[str]`` Declare the projects to be built (None to build all projects).
+        :param msbuild_platform: ``str`` Specify the platform for the internal MSBuild generator (only used by ``MSBuild``).
         """
         if not self._premake_conan_toolchain.exists():
-            raise ConanException(f"Premake.build() method requires PrmakeToolchain to work properly")
+            raise ConanException("Premake.build() method requires PrmakeToolchain to work properly")
         if self.action.startswith("vs"):
             msbuild = MSBuild(self._conanfile)
+            if msbuild_platform:
+                msbuild.platform = msbuild_platform
             msbuild.build(sln=f"{workspace}.sln", targets=targets)
         else:
             build_type = str(self._conanfile.settings.build_type)
