7369d11f63ff9f7f1ba025cbde98382d68d90735
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-07-07 14:50:11 +0200

    Skip build information for gcc if present

--- conan/internal/api/detect/detect_api.py
+++ conan/internal/api/detect/detect_api.py
@@ -488,7 +488,8 @@ def _cc_compiler(compiler_exe="cc"):
         compiler = "clang" if "clang" in out else "gcc"
         # clang and gcc have version after a space, first try to find that to skip extra numbers
         # that might appear in the first line of the output before the version
-        installed_version = re.search(r" ([0-9]+(\.[0-9]+)+)", out)
+        # There might be a leading parenthesis that contains build information, so we try to skip it
+        installed_version = re.search(r"(?:\(.*\))? ([0-9]+(\.[0-9]+)+)", out)
         # Try only major but with spaces next
         installed_version = installed_version or re.search(r" ([0-9]+(\.[0-9]+)?)", out)
         # Fallback to the first number we find optionally followed by other version fields
