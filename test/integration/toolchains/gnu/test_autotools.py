26c3cb75f63d8a12f5ca16027ebf0cebc5b614e0
PerseoGI <perseog@jfrog.com>
2025-07-07 09:24:57 +0200

    Use long option instead

--- test/integration/toolchains/gnu/test_autotools.py
+++ test/integration/toolchains/gnu/test_autotools.py
@@ -24,5 +24,5 @@ def test_autotools_make_parameters():
 
     client.save({"conanfile.py": conanfile})
     client.run("build .")
-    assert "make -f MyMake test -j4 VERBOSE=1" in client.out
-    assert "make -f OtherMake install" in client.out
+    assert "make --file=MyMake test -j4 VERBOSE=1" in client.out
+    assert "make --file=OtherMake install" in client.out
