9dc2989bf314072dcfa5ddc2e481df5fc3132b07
memsharded <james@conan.io>
2025-07-29 16:38:07 +0200

    hotfix: remove definition of CMAKE_TRY_COMPILE_CONFIGURATION for legacy code

--- conan/__init__.py
+++ conan/__init__.py
@@ -2,5 +2,5 @@ from conan.internal.model.conan_file import ConanFile
 from conan.internal.model.workspace import Workspace
 from conan.internal.model.version import Version
 
-__version__ = '2.19.0'
+__version__ = '2.19.1'
 conan_version = Version(__version__)
