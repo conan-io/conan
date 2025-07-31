07ab78d4b5fd14e02475ff27624825293aef87ad
memsharded <james@conan.io>
2025-07-31 17:21:14 +0200

    relax risk warning for visible conflicts only to non-test

--- conan/cli/printers/graph.py
+++ conan/cli/printers/graph.py
@@ -99,10 +99,14 @@ def print_graph_basic(graph):
         output.warning("There are deprecated packages in the graph", warn_tag="risk")
 
     if graph.visibility_conflicts:
-        msg = ["Packages required both with visible=True and visible=False"]
+        msg = []
         for ref, consumers in graph.visibility_conflicts.items():
-            msg.append(f"    {ref}: Required by {', '.join(str(c) for c in consumers)}")
-        output.warning("\n".join(msg), warn_tag="risk")
+            consumers = [str(c[0]) for c in consumers if not c[1]]  # Filter test ones
+            if consumers:
+                msg.append(f"    {ref}: Required by {', '.join(consumers)}")
+        if msg:
+            msg.insert(0, "Packages required both with visible=True and visible=False")
+            output.warning("\n".join(msg), warn_tag="risk")
 
 
 def print_graph_packages(graph):
