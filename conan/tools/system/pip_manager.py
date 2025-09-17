370d78be0c9b1e8d2a1049c800320f2d8eb22a3f
David Sanchez <davidsf@jfrog.com>
2025-09-17 09:13:53 +0200

    wip

--- conan/tools/system/pip_manager.py
+++ conan/tools/system/pip_manager.py
@@ -13,28 +13,32 @@ def _conanfile_run(conanfile, command, accepted_returns):
     return ret
 
 
-def _check(conanfile, env_dir, python_exe, name, version, accepted_returns):
-    if os.path.exists(env_dir):
+def _check(conanfile, env_dir, python_exe, packages, accepted_returns):
+    # FIXME: Local pip python package path
+    packages_to_install = []
+    if not os.path.exists(env_dir):
+        return packages
+    for package in packages:
+        name, version = (package.split("=")[0], package.split("=")[1]) if "=" in package else (package, "")
         command = f'"{python_exe}" -m pip show {name} | grep "{name}"'
         if version:
             command = f'"{python_exe}" -m pip show {name} | grep "{version}"'
-        installed = _conanfile_run(conanfile, command, accepted_returns) != 0
-    else:
-        installed = False
-    return installed
+        if not _conanfile_run(conanfile, command, accepted_returns) != 0:
+            packages_to_install.append(package)
+    return packages_to_install
 
 
-def _install(conanfile, env_dir, python_exe, base_dir, package, name, accepted_returns):
+def _install(conanfile, env_dir, python_exe, base_dir, packages, accepted_returns):
     venv.EnvBuilder(clear=True, with_pip=True).create(env_dir)
-    command = f'"{python_exe}" -m pip install {package}'
+    command = f'"{python_exe}" -m pip install {" ".join(packages)}'
     _conanfile_run(conanfile, command, accepted_returns)
     env = Environment()
     env.prepend_path("PATH", base_dir)
-    env.vars(conanfile).save_script(f"conan_pip_{name}")
+    env.vars(conanfile).save_script(f"conan_pip_{conanfile.name}")
     return base_dir
 
 
-def pip_tool_requires(conanfile, package, output_forlder):
+def pip_tool_requires(conanfile, packages, output_forlder):
     mode_check = "check"  # Check if installed, fail if not
     mode_install = "install"
     mode_report = "report"  # Only report what would be installed, no check (can run in any system)
@@ -43,30 +47,29 @@ def pip_tool_requires(conanfile, package, output_forlder):
     accepted_check_codes = [0, 1]
 
     mode = conanfile.conf.get("tools.system.package_manager:mode", default=mode_check)
-    name, version = (package.split("=")[0], package.split("=")[1]) if "=" in package else (package, "")
 
-    env_dir = os.path.join(output_forlder or conanfile.folders.generators_folder, f"pip_venv_{name}")
+    env_dir = os.path.join(output_forlder or conanfile.folders.generators_folder, f"pip_venv_{conanfile.name}")
     base_dir = os.path.join(env_dir, "Scripts" if platform.system() == "Windows" else "bin")
     python_exe = os.path.join(base_dir, "python.exe" if platform.system() == "Windows" else "python")
 
     if mode == mode_report:
         return
 
-    installed = _check(conanfile, env_dir, python_exe, name, version, accepted_check_codes)
+    packages_to_install = _check(conanfile, env_dir, python_exe, packages, accepted_check_codes)
 
     if mode == mode_report_installed:
         return
 
-    if mode == mode_check and not installed:
+    if mode == mode_check and packages_to_install:
         raise ConanException("Pip requirement: '{0}' are missing but can't install "
                              "because tools.system.package_manager:mode is '{1}'."
                              "Please set 'tools.system.package_manager:mode' "
                              "to '{2}' in the [conf] section of the profile, "
                              "or in the command line using "
-                             "'-c tools.system.package_manager:mode={2}'".format(package,
+                             "'-c tools.system.package_manager:mode={2}'".format(" ".join(packages),
                                                                                  mode_check,
                                                                                  mode_install))
-    elif not installed:
-        return _install(conanfile, env_dir, python_exe, base_dir, package, name, accepted_install_codes)
+    elif packages_to_install:
+        return _install(conanfile, env_dir, python_exe, base_dir, packages_to_install, accepted_install_codes)
     else:
-        conanfile.output.info(f"Pip requirements: {package} already installed")
+        conanfile.output.info(f"Pip requirements: {' '.join(packages)} already installed")
--- test/functional/tools/system/pip_manager_test.py
+++ test/functional/tools/system/pip_manager_test.py
@@ -1,12 +1,32 @@
-import os
-import platform
 import textwrap
-
 from conan.test.utils.tools import TestClient
+from conan.internal.util.files import save_files
+from conan.test.utils.test_files import temp_folder
 
 
 def test_pip_manager():
-    conanfile_meson = textwrap.dedent("""
+
+    setup_py = textwrap.dedent("""
+        from setuptools import setup, find_packages
+
+        setup(
+            name='hello',
+            version='0.1.0',
+            packages=find_packages(include=['hello', 'hello.*']),
+            entry_points={'console_scripts': ['hello-world = hello:hello']}
+        )
+        """)
+    hello_py = textwrap.dedent("""
+        def hello():
+            print("Hello Test World!")
+        """)
+
+    pip_package_folder = temp_folder(path_with_spaces=False)
+    save_files(pip_package_folder,
+               {"setup.py": setup_py,
+                "hello/__init__.py": hello_py})
+
+    conanfile_pip = textwrap.dedent(f"""
         from conan import ConanFile
         from conan.tools.system.pip_manager import pip_tool_requires
         from conan.tools.files import copy
@@ -15,18 +35,18 @@ def test_pip_manager():
         import os
 
 
-        class MesonPackage(ConanFile):
-            name = "pip_meson_test"
+        class PipPackage(ConanFile):
+            name = "pip_hello_test"
             version = "0.1"
 
             def layout(self):
                 basic_layout(self)
 
             def generate(self):
-                self._venv_dir = pip_tool_requires(self, "meson==1.7.1", output_forlder=self.package_folder)
+                self._venv_dir = pip_tool_requires(self, ["{pip_package_folder}"], output_forlder=self.package_folder)
 
             def build(self):
-                self.run("meson --version")
+                self.run("hello-world")
 
             def finalize(self):
                 copy(self, "*", src=self.immutable_package_folder, dst=self.package_folder)
@@ -34,9 +54,9 @@ def test_pip_manager():
             def package_info(self):
                 self.cpp_info.includedirs = []
                 self.cpp_info.libdirs = []
-                meson_root = os.path.join(self.package_folder, "pip_venv_meson", "Scripts" if platform.system() == "Windows" else "bin")
-                self.buildenv_info.prepend_path("PATH", meson_root)
-                self.runenv_info.prepend_path("PATH", meson_root)
+                python_root = os.path.join(self.package_folder, "pip_venv_pip_hello_test", "Scripts" if platform.system() == "Windows" else "bin")
+                self.buildenv_info.prepend_path("PATH", python_root)
+                self.runenv_info.prepend_path("PATH", python_root)
         """)
 
     conanfile = textwrap.dedent("""
@@ -48,21 +68,20 @@ def test_pip_manager():
             version = "0.1"
 
             def requirements(self):
-                self.requires("pip_meson_test/0.1")
+                self.requires("pip_hello_test/0.1")
 
             def build(self):
-                self.run("meson --version")
+                self.run("hello-world")
         """)
 
     client = TestClient()
-    client.save({"pip_meson/conanfile.py": conanfile_meson, "consumer/conanfile.py": conanfile})
+    client.save({"pip/conanfile.py": conanfile_pip, "consumer/conanfile.py": conanfile})
 
-    client.run("create pip_meson/conanfile.py --version=0.1 -c tools.system.package_manager:mode=install")
-
-    assert "RUN: meson --version" in client.out
-    assert "1.7.1" in client.out
+    client.run("create pip/conanfile.py --version=0.1 -c tools.system.package_manager:mode=install")
+    assert "RUN: hello-world" in client.out
+    assert "Hello Test World!" in client.out
 
     client.run("build consumer/conanfile.py")
 
-    assert "RUN: meson --version" in client.out
-    assert "1.7.1" in client.out
+    assert "RUN: hello-world" in client.out
+    assert "Hello Test World!" in client.out
