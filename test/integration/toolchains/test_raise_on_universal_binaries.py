0bacf932ece97194a1364bd3d27760be7eed52af
Carlos Zoido <mrgalleta@gmail.com>
2025-09-25 16:41:09 +0200

    add support for universal binaries

--- test/integration/toolchains/test_raise_on_universal_binaries.py
+++ test/integration/toolchains/test_raise_on_universal_binaries.py
@@ -6,7 +6,7 @@ from conan.test.assets.genconanfile import GenConanfile
 from conan.test.utils.tools import TestClient
 
 
-@parameterized.expand(["AutotoolsToolchain", "MesonToolchain", "BazelToolchain"])
+@parameterized.expand(["MesonToolchain", "BazelToolchain"])
 def test_create_universal_binary(toolchain):
     client = TestClient()
     conanfile = (GenConanfile().with_settings("os", "arch", "compiler", "build_type")
@@ -17,6 +17,33 @@ def test_create_universal_binary(toolchain):
                assert_error=True)
     assert (f"Error in generator '{toolchain}': "
             f"Universal binaries not supported by toolchain.") in client.out
+
+
+def test_autotoolstoolchain_universal_binary_support():
+    """Test that AutotoolsToolchain now supports universal binaries on macOS"""
+    client = TestClient()
+    conanfile = (GenConanfile().with_settings("os", "arch", "compiler", "build_type")
+                 .with_generator("AutotoolsToolchain"))
+    client.save({"conanfile.py": conanfile})
+
+    client.run('install . --name=foo --version=1.0 -s="os=Macos" -s="arch=armv8|x86_64" -s="compiler=apple-clang"')
+
+    toolchain = client.load("conanautotoolstoolchain.sh")
+    assert "-arch arm64" in toolchain
+    assert "-arch x86_64" in toolchain
+
+
+def test_autotoolstoolchain_universal_binary_non_macos():
+    """Test that AutotoolsToolchain still raises error for universal binaries on non-macOS"""
+    client = TestClient()
+    conanfile = (GenConanfile().with_settings("os", "arch", "compiler", "build_type")
+                 .with_generator("AutotoolsToolchain"))
+    client.save({"conanfile.py": conanfile})
+
+    # This should still raise an error on non-macOS platforms
+    client.run('create . --name=foo --version=1.0 -s="os=Linux" -s="arch=armv8|x86_64"',
+               assert_error=True)
+    assert "Universal arch 'armv8|x86_64' is only supported in Apple OSes" in client.out
 
 
 def test_create_universal_binary_test_package_folder():
