d08ef8e5ba9174a4c1827c1d853e93c448a3b217
David Sanchez <davidsf@jfrog.com>
2025-09-25 11:38:41 +0200

    wip

--- conan/tools/system/pip_manager.py
+++ conan/tools/system/pip_manager.py
@@ -3,12 +3,11 @@ import platform
 import os
 
 from conan.errors import ConanException
+from conan.tools.build import cmd_args_to_string
 from conan.tools.env.environment import Environment
 
 
 class PipEnv:
-    accepted_install_codes = [0]
-    accepted_check_codes = [0, 1]
 
     def __init__(self, conanfile, folder=None):
         self._conanfile = conanfile
@@ -16,12 +15,6 @@ class PipEnv:
         self._env_dir = os.path.abspath(os.path.join(folder or self._conanfile.build_folder, self.env_name))
         self.bin_dir = os.path.join(self._env_dir, "Scripts" if platform.system() == "Windows" else "bin")
         self._python_exe = os.path.join(self.bin_dir, "python.exe" if platform.system() == "Windows" else "python")
-
-    def _conanfile_run(self, command, accepted_returns):
-        ret = self._conanfile.run(command, ignore_errors=True, quiet=True)
-        if ret not in accepted_returns:
-            raise ConanException("Command '%s' failed" % command)
-        return ret
 
     def generate(self):
         """
@@ -32,14 +25,25 @@ class PipEnv:
         env.prepend_path("PATH", self.bin_dir)
         env.vars(self._conanfile).save_script(self.env_name)
 
-    def install(self, packages):
+    def install(self, packages, pip_args=None):
         """
         Will try to install the list of pip packages passed as a parameter.
 
         :param packages: try to install the list of pip packages passed as a parameter.
+        :param pip_args: additional argument list to be passed to the 'pip install' command,
+                         for example: ['--no-cache-dir', '--index-url', 'https://my.pypi.org/simple'].
+                         Defaults to ``None``.
+
         :return: the return code of the executed pip command.
         """
-        packages = [f'"{p}"' for p in packages]
+
         venv.EnvBuilder(clear=True, with_pip=True).create(self._env_dir)
-        command = f'"{self._python_exe}" -m pip install {" ".join(packages)}'
-        return self._conanfile_run(command, self.accepted_install_codes)
+        pip_args = list(pip_args) if pip_args else []
+        args = [self._python_exe, "-m", "pip", "install", "--disable-pip-version-check"] + pip_args
+        args += list(packages)
+        command = cmd_args_to_string(args)
+
+        ret = self._conanfile.run(command, ignore_errors=True, quiet=True)
+        if ret == 0:
+            return ret
+        raise ConanException("Command '%s' failed" % command)
