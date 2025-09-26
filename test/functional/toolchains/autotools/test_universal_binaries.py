527c2d9bf71e540a38a26634d0b321a2a23a331f
Carlos Zoido <mrgalleta@gmail.com>
2025-09-26 07:21:47 +0200

    remove test_package

--- test/functional/toolchains/autotools/test_universal_binaries.py
+++ test/functional/toolchains/autotools/test_universal_binaries.py
@@ -9,7 +9,6 @@ from conan.test.utils.tools import TestClient
 
 @pytest.mark.skipif(platform.system() != "Darwin", reason="Only OSX")
 def test_autotools_universal_binary():
-    """Test that AutotoolsToolchain works with universal binaries on macOS"""
     client = TestClient(path_with_spaces=False)
 
     conanfile = textwrap.dedent("""
@@ -59,47 +58,10 @@ def test_autotools_universal_binary():
 
     """)
 
-    test_conanfile = textwrap.dedent("""
-        import os
-
-        from conan import ConanFile
-        from conan.tools.gnu import AutotoolsToolchain, Autotools, AutotoolsDeps
-        from conan.tools.layout import basic_layout
-        from conan.tools.build import can_run
-
-
-        class mylibraryTestConan(ConanFile):
-            settings = "os", "compiler", "build_type", "arch"
-            generators = "AutotoolsDeps", "AutotoolsToolchain"
-
-            def requirements(self):
-                self.requires(self.tested_reference_str)
-
-            def build(self):
-                autotools = Autotools(self)
-                autotools.autoreconf()
-                autotools.configure()
-                autotools.make()
-
-            def layout(self):
-                basic_layout(self)
-
-            def test(self):
-                exe = os.path.join(self.cpp.build.bindir, "main")
-                self.run(f"lipo {exe} -info", env="conanrun")
-            """)
-
-
     client.run('new autotools_lib -d name=mylibrary -d version=0.1')
 
-    client.save({"conanfile.py": conanfile, "test_package/conanfile.py": test_conanfile})
+    client.save({"conanfile.py": conanfile})
 
-    # Test universal binary creation
     client.run('create . -s="arch=armv8|x86_64" -tf=""')
 
-    # Verify the library contains both architectures
     assert "libmylibrary.a are: x86_64 arm64" in client.out
-
-    client.run('test test_package mylibrary/1.0 -s="arch=armv8|x86_64"')
-
-    assert "./main are: x86_64 arm64" in client.out
