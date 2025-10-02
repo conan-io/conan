57efa02c1e33081ff68d69a967f69061bdb809ec
David Sanchez <davidsf@jfrog.com>
2025-10-02 14:33:45 +0200

    pyinstaller support added to PipEnv

--- conan/tools/system/pip_manager.py
+++ conan/tools/system/pip_manager.py
@@ -1,9 +1,11 @@
-import venv
 import platform
 import os
+import shutil
+import sys
 
 from conan.tools.build import cmd_args_to_string
 from conan.tools.env.environment import Environment
+from conan.errors import ConanException
 
 
 class PipEnv:
@@ -23,6 +25,39 @@ class PipEnv:
         env.prepend_path("PATH", self.bin_dir)
         env.vars(self._conanfile).save_script(self.env_name)
 
+    def _create_venv(self):
+        print("=" * 20)
+        python_executable = None
+        if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+            if platform.system() == "Windows":
+                candidate_names = ['python.exe', 'pythonw.exe']
+            else:
+                candidate_names = ['python', 'python3']
+
+            for name in candidate_names:
+                expected_path = os.path.join(sys._MEIPASS, name)
+                print(expected_path)
+                if os.path.exists(expected_path):
+                    python_executable = expected_path
+                    break
+            if not python_executable:
+                if platform.system() == "Windows":
+                    system_py = shutil.which('python')
+                else:
+                    system_py = shutil.which('python3') or shutil.which('python')
+                if system_py:
+                    python_executable = system_py
+        else:
+            python_executable = sys.executable
+        print("=" * 20)
+        print(python_executable)
+        print(getattr(sys, 'frozen', False))
+        print(getattr(sys, '_MEIPASS', False))
+        print("=" * 20)
+        if not python_executable:
+            raise ConanException("PipEnv could not find a Python executable path.")
+        self._conanfile.run(cmd_args_to_string([python_executable, '-m', 'venv', self._env_dir]))
+
     def install(self, packages, pip_args=None):
         """
         Will try to install the list of pip packages passed as a parameter.
@@ -34,7 +69,7 @@ class PipEnv:
         :return: the return code of the executed pip command.
         """
 
-        venv.EnvBuilder(clear=True, with_pip=True).create(self._env_dir)
+        self._create_venv()
         args = [self._python_exe, "-m", "pip", "install", "--disable-pip-version-check"]
         if pip_args:
             args += list(pip_args)
