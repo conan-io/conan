f9545e147906a9c3309ceb77257731517060ef2a
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-12-05 13:37:55 +0100

    Update conan/internal/model/recipe_ref.py
    Co-authored-by: James <memsharded@gmail.com>

--- conan/internal/model/recipe_ref.py
+++ conan/internal/model/recipe_ref.py
@@ -6,7 +6,7 @@ def ref_matches(ref, pattern, is_consumer):
     if not ref or not str(ref):
         assert is_consumer
         ref = RecipeReference.loads("*/*")  # FIXME: ugly
-    if "[" in pattern or "]" in pattern:
+    if pattern.startswith("["):
         ConanOutput().warning(f"Pattern {pattern} contains a version range, which has no effect. "
                               f"Only '&' for consumer and '*' as wildcard are supported in this context.",
                               warn_tag="risk")
