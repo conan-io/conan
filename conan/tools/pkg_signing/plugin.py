bbec2b800bb4b0ab640510facf19dd5a0808aa7f
danimtb <danimanzaneque@gmail.com>
2025-12-30 12:28:19 +0100

    fix docstrings

--- conan/tools/pkg_signing/plugin.py
+++ conan/tools/pkg_signing/plugin.py
@@ -15,8 +15,8 @@ PKGSIGN_SIGNATURES = "pkgsign-signatures.json"
 def get_manifest_filepath(signature_folder):
     """
     Gets the path of the summary file path
-    @param signature_folder: Signature folder path
-    @return: Path of the summary file
+    :param signature_folder: Signature folder path
+    :return: Path of the summary file
     """
     return os.path.join(signature_folder, PKGSIGN_MANIFEST)
 
@@ -24,8 +24,8 @@ def get_manifest_filepath(signature_folder):
 def load_manifest(signature_folder):
     """
     Loads the summary file from the signature folder
-    @param signature_folder: Signature folder path
-    @return: Dictionary object with the content of the summary
+    :param signature_folder: Signature folder path
+    :return: Dictionary object with the content of the summary
     """
     return json.loads(load(get_manifest_filepath(signature_folder)))
 
@@ -64,7 +64,7 @@ def _create_manifest_content(artifacts_folder):
 def _save_manifest(artifacts_folder, signature_folder):
     """
     Saves the content of manifest file in the signature folder
-    @param signature_folder: Signature folder path
+    :param signature_folder: Signature folder path
     """
     content = _create_manifest_content(artifacts_folder)
     save(get_manifest_filepath(signature_folder), json.dumps(content, indent=2))
@@ -73,8 +73,8 @@ def _save_manifest(artifacts_folder, signature_folder):
 def get_signatures_filepath(signature_folder):
     """
     Gets the path of the signatures file path
-    @param signature_folder: Signature folder path
-    @return: Path of the signatures file
+    :param signature_folder: Signature folder path
+    :return: Path of the signatures file
     """
     return os.path.join(signature_folder, PKGSIGN_SIGNATURES)
 
@@ -82,8 +82,8 @@ def get_signatures_filepath(signature_folder):
 def load_signatures(signature_folder):
     """
     Loads the signatures file pkgsign-signatures.json from the signature folder
-    @param signature_folder: Signature folder path
-    @return: dict of {filename: signature_value}
+    :param signature_folder: Signature folder path
+    :return: dict of {filename: signature_value}
     """
     return json.loads(load(get_signatures_filepath(signature_folder)))
 
@@ -91,8 +91,8 @@ def load_signatures(signature_folder):
 def _save_signatures(signature_folder, signatures):
     """
     Saves the content of signatures file in the signature folder
-    @param signature_folder: Signature folder path
-    @param signatures: dict of {filename: signature_value}
+    :param signature_folder: Signature folder path
+    :param signatures: dict of {filename: signature_value}
     """
     assert isinstance(signatures, list),\
         "The signature plugin function must return a list of signatures values"
@@ -115,7 +115,7 @@ def verify_files_checksums(signature_folder, files):
     """
     Verifies that the files' checksums match those stored in the summary.
 
-    @param files: dict of {filename: filepath} of files in artifact folder to verify
+    :param files: dict of {filename: filepath} of files in artifact folder to verify
     """
     expected_list = load_manifest(signature_folder).get("files", [])
     expected_files = {item["file"]: item["sha256"] for item in expected_list}
