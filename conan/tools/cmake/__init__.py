8832c12fcdef813bfa2ea281d34b77a52c088c66
memsharded <james@conan.io>
2026-01-08 11:53:05 +0100

    improve msg

--- conan/tools/cmake/__init__.py
+++ conan/tools/cmake/__init__.py
@@ -7,9 +7,11 @@ from conan.tools.cmake.layout import cmake_layout
 def CMakeDeps(conanfile):  # noqa
     if conanfile.conf.get("tools.cmake.cmakedeps:new",
                           choices=["will_break_next", "recipe_will_break"]) == "will_break_next":
-        conanfile.output.warning("Using the new CMakeConfigDeps generator, behind the "
-                                 "'tools.cmake.cmakedeps:new' gate conf. This conf will change "
-                                 "next release, breaking, so use it only for testing and dev")
+        conanfile.output.warning("On the fly replacement of CMakeDeps by CMakeConfigDeps generator, "
+                                 "because 'tools.cmake.cmakedeps:new' incubating conf activated. "
+                                 "This conf is incubating and will break in next releases. "
+                                 "CMakeConfigDeps is now experimental and can be used as such in "
+                                 "recipes.")
         return CMakeConfigDeps(conanfile)
     from conan.tools.cmake.cmakedeps.cmakedeps import CMakeDeps as _CMakeDeps
     return _CMakeDeps(conanfile)
