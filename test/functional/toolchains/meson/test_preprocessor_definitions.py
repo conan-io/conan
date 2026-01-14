152e00e20ea4c83496230b2ddfd914ead3d2dde4
Uilian Ries <uilianr@jfrog.com>
2026-01-14 16:15:07 +0100

    Update preprocessor test to validate #19463
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- test/functional/toolchains/meson/test_preprocessor_definitions.py
+++ test/functional/toolchains/meson/test_preprocessor_definitions.py
@@ -28,8 +28,7 @@ class TestMesonPreprocessorDefinitionsTest:
 
         def generate(self):
             tc = MesonToolchain(self)
-            tc.preprocessor_definitions["TEST_DEFINITION1"] = "TestPpdValue1"
-            tc.preprocessor_definitions["TEST_DEFINITION2"] = "TestPpdValue2"
+    preprocessor_definitions
             tc.generate()
 
         def build(self):
@@ -44,16 +43,28 @@ class TestMesonPreprocessorDefinitionsTest:
     executable('demo', 'main.cpp', link_with: hello)
     """)
 
+    def _inject_definitions(self, content: str, defines: dict) -> str:
+        lines = []
+        for define, value in defines.items():
+            lines.append(f"        tc.preprocessor_definitions['{define}'] = {value}")
+        return content.replace("preprocessor_definitions", "\n".join(lines))
+
     @pytest.mark.tool("ninja")
     @pytest.mark.tool("meson")
-    def test_build(self):
+    @pytest.mark.parametrize("defines", [{"TEST_DEFINITION1": '"TestPpdValue1"', "TEST_DEFINITION2": '"TestPpdValue2"'},
+                                         {"DEADBEEF": 3735928495, "CAFED00D": 3405697037},
+                                         {"DEAD10CC": None},
+                                         {"BAADF00D": True, "BAD22222": False}])
+    def test_build(self, defines):
         hello_h = gen_function_h(name="hello")
         hello_cpp = gen_function_cpp(name="hello",
-                                     preprocessor=["TEST_DEFINITION1", "TEST_DEFINITION2"])
+                                     preprocessor=defines.keys())
         app = gen_function_cpp(name="main", includes=["hello"], calls=["hello"])
 
+        conanfile = self._inject_definitions(self._conanfile_py, defines)
+
         t = TestClient()
-        t.save({"conanfile.py": self._conanfile_py,
+        t.save({"conanfile.py": conanfile,
                 "meson.build": self._meson_build,
                 "hello.h": hello_h,
                 "hello.cpp": hello_cpp,
@@ -66,11 +77,33 @@ class TestMesonPreprocessorDefinitionsTest:
         assert "[built-in options]" in content
         assert "buildtype = 'release'" in content
 
+        definitions = []
+        for define, value in defines.items():
+            if value is None:
+                definitions.append(f"'-D{define}'")
+            elif isinstance(value, bool):
+                expected_value = "1" if value else "0"
+                definitions.append(f"'-D{define}={expected_value}'")
+            elif isinstance(value, str):
+                definitions.append(f"'-D{define}={value}'")
+            elif isinstance(value, int):
+                definitions.append(f"'-D{define}={value}'")
+        assert f"preprocessor_definitions = [{', '.join(definitions)}]" in content
+
         t.run("build .")
         t.run_command(os.path.join("build", "demo"))
 
         assert "hello: Release!" in t.out
-        assert "TEST_DEFINITION1: TestPpdValue1" in t.out
-        assert "TEST_DEFINITION2: TestPpdValue2" in t.out
+        for define, value in defines.items():
+            if value is None:
+                assert f"{define}" in t.out
+            elif isinstance(value, bool):
+                expected_value = "1" if value else "0"
+                assert f"{define}={expected_value}" in t.out
+            elif isinstance(value, str):
+                value = value.replace('"', '')
+                assert f"{define}: {value}" in t.out
+            elif isinstance(value, int):
+                assert f"{define}: {value}" in t.out
 
         check_binary(t)
