fb658115a5ea831eff4464b361b3cae7e27a3c95
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-01-29 12:20:31 +0100

    Add failing test for CMakeConfigDeps casing fallback

--- test/functional/toolchains/cmake/cmakedeps2/test_cmakeconfigdeps_new.py
+++ test/functional/toolchains/cmake/cmakedeps2/test_cmakeconfigdeps_new.py
@@ -1620,11 +1620,13 @@ def test_find_package_casing():
     """)
     consumer = textwrap.dedent("""
        from conan import ConanFile
-       from conan.tools.cmake import CMake
+       from conan.tools.cmake import CMake, cmake_layout
        class Pkg(ConanFile):
            requires = "hello/1.0"
            generators = "CMakeToolchain", "CMakeDeps"
            settings = "os", "compiler", "build_type", "arch"
+           def layout(self):
+               cmake_layout(self)
            def build(self):
                cmake = CMake(self)
                cmake.configure()
