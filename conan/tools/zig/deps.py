cf43af3132254ddfa52ba9827275dc01ec2fe1d5
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-02-16 14:41:08 +0100

    Sketch what a zigdeps might look like

---
+++ conan/tools/zig/deps.py
@@ -0,0 +1,115 @@
+import json
+import textwrap
+import os
+from jinja2 import Template, StrictUndefined
+
+from conan.errors import ConanException
+from conan.internal.model.pkg_type import PackageType
+
+
+class DepsTemplate:
+    def __init__(self, zigdeps, conanfile):
+        self._zigdeps = zigdeps
+        self._conanfile = conanfile  # The dependency conanfile, not the consumer one
+
+    def content(self):
+        t = Template(self._template, trim_blocks=True, lstrip_blocks=True,
+                     undefined=StrictUndefined)
+        return t.render(self._context)
+
+    @property
+    def filename(self):
+        return f"conan_deps.zig"
+
+    def _get_libs(self, cpp_info, pkg_name, pkg_folder=None):
+        libs = {}
+        if cpp_info.has_components:
+            for name, component in cpp_info.components.items():
+                target = self._get_lib(component, cpp_info.components, pkg_folder, comp_name=name)
+                if target is not None:
+                    libs[name] = target
+        else:
+            target = self._get_lib(cpp_info, None, pkg_folder)
+            if target is not None:
+                libs[pkg_name] = target
+        return libs
+
+    def _get_lib(self, info, components, pkg_folder, comp_name=None):
+        if info.exe or not (info.package_framework or info.frameworks or info.includedirs or info.libs
+                            or info.system_libs or info.defines):
+            return {}
+        target = {}
+        if info.libs:
+            if len(info.libs) != 1:
+                raise ConanException(f"ZigDeps only allows 1 lib per component:\n"
+                                     f"{self._conanfile}: {info.libs}")
+            assert info.location, "info.location missing for .libs, it should have been deduced"
+            location = info.location
+            link_location = info.link_location or None
+            lib_type = "SHARED" if info.type is PackageType.SHARED else \
+                "STATIC" if info.type is PackageType.STATIC else None
+            assert lib_type, f"Unknown package type {info.type}"
+            target["type"] = lib_type
+            target["location"] = location
+            target["link_location"] = link_location
+        return target
+
+    def _defines(self, defines):
+        result = {}
+        for define in defines:
+            if "=" in define:
+                name, value = define.split("=", 1)
+            else:
+                name, value = define, "1"
+            result[name] = value
+        return result
+
+    @property
+    def _context(self):
+        result = {"deps": {}}
+        for require, dep in self._conanfile.dependencies.host.items():
+            full_cpp_info = dep.cpp_info.deduce_full_cpp_info(dep)
+
+            result["deps"][dep.ref.name] = {
+                "include_paths": full_cpp_info.includedirs,
+                "lib_paths": full_cpp_info.libdirs,
+                "libs": self._get_libs(full_cpp_info, dep.ref.name),
+                "defines": self._defines(full_cpp_info.defines),
+                "dependencies": [],#[d.ref.name for d in dep.dependencies.host],
+            }
+        return result
+
+    @property
+    def _template(self):
+        return textwrap.dedent("""\
+            const std = @import("std");
+
+            const Dep = struct {
+                include_paths: []const []const u8,
+                lib_paths: []const []const u8,
+                libs: []const []const u8,
+                defines: []const struct { name: []const u8, value: []const u8 },
+                dependencies: []const []const u8,
+                // c_sources: []const u8,
+                // link_libc: bool,
+                // link_libcpp: bool,
+            };
+
+            // Now, fill the conan_deps struct with the information from Conan
+
+            // initialize directly at compile time
+            pub const conan_deps = std.StaticStringMap(Dep).initComptime(.{
+                {% for dep_name, dep_info in deps.items() %}
+                .{
+                    "{{ dep_name }}",
+                    Dep{
+                        .include_paths = &.{ {% for path in dep_info.include_paths %}"{{ path }}",{% endfor %} },
+                        .lib_paths = &.{ {% for path in dep_info.lib_paths %}"{{ path }}",{% endfor %} },
+                        .libs = &.{ {% for lib in dep_info.libs.values() %}{% if "location" in lib %}"{{ lib["location"] }}",{% endif %}{% endfor %} },
+                        .defines = &.{ {% for define, value in dep_info.defines.items() %}.{ .name="{{ define }}", .value="{{ value }}"},{% endfor %} },
+                        .dependencies = &.{ {% for d in dep_info.dependencies %}"{{ d }}",{% endfor %} },
+                    }
+                },
+                {% endfor %}
+            });
+            """)
---
+++ conan/tools/zig/setup.py
@@ -0,0 +1,73 @@
+import textwrap
+from jinja2 import Template, StrictUndefined
+
+
+class SetupTemplate:
+    def __init__(self, zigdeps, conanfile):
+        self._zigdeps = zigdeps
+        self._conanfile = conanfile  # The dependency conanfile, not the consumer one
+
+    def content(self):
+        t = Template(self._template, trim_blocks=True, lstrip_blocks=True,
+                     undefined=StrictUndefined)
+        return t.render(self._context)
+
+    @property
+    def filename(self):
+        return "conan_setup.zig"
+
+    @property
+    def _context(self):
+        result = {}
+        # for dep in self._conanfile.dependencies.host:
+        #     result[dep.ref.name] = {
+        #
+        #     }
+        return result
+
+    @property
+    def _template(self):
+        return textwrap.dedent("""\
+            const std = @import("std");
+            const deps = @import("conan_deps.zig");
+
+            pub fn linkDependency(step: *std.Build.Step.Compile, dep_name: []const u8) void {
+                //std.debug.print("Checking dependency: {s}\\n", .{dep_name});
+                if (deps.conan_deps.get(dep_name)) |dep_info| {
+                    for (dep_info.include_paths) |include_path| {
+                        // std.debug.print("Adding include path: {s}\\n", .{include_path});
+                        step.addIncludePath(std.Build.LazyPath{ .cwd_relative = include_path });
+                    }
+                    // for (dep_info.lib_paths) |lib_path| {
+                    //     std.debug.print("Adding library path: {s}\\n", .{lib_path});
+                    //     step.addLibraryPath(std.Build.LazyPath{ .cwd_relative = lib_path});
+                    // }
+                    for (dep_info.libs) |lib| {
+                        // std.debug.print("Linking library: {s}\\n", .{lib});
+                        //step.linkSystemLibrary(lib);
+                        step.addObjectFile(std.Build.LazyPath{ .cwd_relative = lib});
+                    }
+                    // for (c_source in dep_info.c_sources) {
+                    //     step.addCSourceFile(c_source);
+                    // }
+                    for (dep_info.defines) |define| {
+                        //std.debug.print("Adding define: {s}={s}\\n", .{define.name, define.value});
+                        step.root_module.addCMacro(define.name, define.value);
+
+                    }
+                    // if (dep_info.link_libc) {
+                    //     step.linkLibC();
+                    // }
+                    // if (dep_info.link_libcpp) {
+                    //     step.linkLibCpp();
+                    // }
+                }
+            }
+
+            pub fn linkDependencies(step: *std.Build.Step.Compile) void {
+                for (deps.conan_deps.keys()) |dep_name| {
+                    linkDependency(step, dep_name);
+                }
+            }
+            """)
+
