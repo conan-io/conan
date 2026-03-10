cc4f1245c6b11e1fda882afb414d237a00669237
memsharded <james@conan.io>
2026-03-10 19:47:47 +0100

    improve source creds msg

--- test/integration/cache/backup_sources_test.py
+++ test/integration/cache/backup_sources_test.py
@@ -424,8 +424,8 @@ class TestDownloadCacheBackupSources:
         client.run("create .")
         assert "CONTENT: Hello, world!" in client.out
         client.run("upload * -c -r=default", assert_error=True)
-        assert f"The source backup server '{http_server.fake_url}" \
-               f"/uploader/' needs authentication" in client.out
+        assert f"Authentication to source backup server '{http_server.fake_url}" \
+               f"/uploader/' failed" in client.out
         content = {"credentials": [
             {"url": f"{http_server.fake_url}", "token": "myuploadtoken"}
         ]}
