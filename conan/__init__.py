a4a7f77f3ea79f39e1a085b925c26ff72b5a7ae2
Carlos Zoido <mrgalleta@gmail.com>
2026-03-25 15:52:35 +0100

    Conan 2.28.0 dev

--- conan/__init__.py
+++ conan/__init__.py
@@ -2,5 +2,5 @@ from conan.internal.model.conan_file import ConanFile
 from conan.internal.model.workspace import Workspace
 from conan.internal.model.version import Version
 
-__version__ = '2.27.0'
+__version__ = '2.28.0-dev'
 conan_version = Version(__version__)
