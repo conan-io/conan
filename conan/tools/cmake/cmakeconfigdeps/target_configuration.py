0048f223c1e17574a5277adc68d66264becaa6c8
memsharded <james@conan.io>
2026-04-06 09:48:34 +0200

    trying if 'req.libs' is enough

--- conan/tools/cmake/cmakeconfigdeps/target_configuration.py
+++ conan/tools/cmake/cmakeconfigdeps/target_configuration.py
@@ -58,8 +58,7 @@ class TargetConfigurationTemplate2:
                 dep_target = self._cmakedeps.get_property("cmake_target_name", d)
                 dep_target = dep_target or f"{d.ref.name}::{d.ref.name}"
                 link_feature = self._cmakedeps.get_property("cmake_link_feature", d)
-                link = (req.libs or
-                        not (pkg_type is PackageType.SHARED and d.package_type is PackageType.SHARED))
+                link = req.libs
                 result[dep_target] = {
                     "link": link,
                     "link_feature": link_feature
@@ -109,7 +108,7 @@ class TargetConfigurationTemplate2:
                         default_target = f"{required_pkg}::{required_comp}"
                         link = not (pkg_type is PackageType.SHARED and
                                     dep_comp.type is PackageType.SHARED)
-                    link = req.libs or link
+                    link = req.libs
                     dep_target = self._cmakedeps.get_property("cmake_target_name", dep, comp)
                     dep_target = dep_target or default_target
                     link_feature = self._cmakedeps.get_property("cmake_link_feature", dep, comp)
