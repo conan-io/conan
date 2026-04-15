1c09c83f7ac63077bc8e7f4d8e26d095e158f3c9
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-04-15 18:11:41 +0200

    Remove deprecated message for order-by in conan graph info build-order

--- conan/cli/commands/graph.py
+++ conan/cli/commands/graph.py
@@ -67,14 +67,13 @@ def graph_build_order(conan_api, parser, subparser, *args):
     """
     common_graph_args(subparser)
     subparser.add_argument("--order-by", choices=['recipe', 'configuration'],
+                           default="recipe",
                            help='Select how to order the output, "recipe" by default if not set.')
     subparser.add_argument("--reduce", action='store_true', default=False,
                            help='Reduce the build order, output only those to build. Use this '
                                 'only if the result will not be merged later with other build-order')
     args = parser.parse_args(*args)
     validate_common_graph_args(args)
-    if args.order_by is None:
-        ConanOutput().warning("Please specify --order-by argument", warn_tag="deprecated")
 
     cwd = os.getcwd()
     path = conan_api.local.get_conanfile_path(args.path, cwd, py=None) if args.path else None
@@ -109,8 +108,6 @@ def graph_build_order(conan_api, parser, subparser, *args):
     install_graph = conan_api.graph.build_order(deps_graph, args.order_by, args.reduce,
                                                 profile_args=args)
     install_order_serialized = install_graph.install_build_order()
-    if args.order_by is None:  # legacy
-        install_order_serialized = install_order_serialized["order"]
 
     lockfile = conan_api.lockfile.update_lockfile(lockfile, deps_graph, args.lockfile_packages,
                                                   clean=args.lockfile_clean)
