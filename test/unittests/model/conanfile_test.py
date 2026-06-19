c7ad9acc95c9c14ffd595548398857c068ee889d
memsharded <james@conan.io>
2026-06-19 23:58:27 +0200

    test

--- test/unittests/model/conanfile_test.py
+++ test/unittests/model/conanfile_test.py
@@ -6,6 +6,7 @@ from conan.test.utils.tools import TestClient
 
 class TestConanFile:
     def test_conanfile_naming(self):
+        # simple non breaking change
         for member in vars(ConanFile):
             if member.startswith('_') and not member.startswith("__"):
                 assert member.startswith('_conan')
