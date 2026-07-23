70fcb3c799779b52ddcd22bcce76bb4f34c3dd13
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-07-24 00:19:16 +0200

    Simplify diff

--- conan/api/subapi/remotes.py
+++ conan/api/subapi/remotes.py
@@ -114,6 +114,8 @@ class RemotesAPI:
           the remote in that position instead of the last one
         """
         remotes = _load(self._remotes_file)
+        if remote.remote_type != LOCAL_RECIPES_INDEX:
+            _validate_url(remote.url)
         current = {r.name: r for r in remotes}.get(remote.name)
         if current:  # same name remote existing!
             if not force:
@@ -123,8 +125,6 @@ class RemotesAPI:
             if current.url != remote.url:
                 ConanOutput().warning("Updating existing remote with new url")
         add_local_recipes_index_remote(self._home_folder, remote)
-        if remote.remote_type != LOCAL_RECIPES_INDEX:
-            _validate_url(remote.url)
 
         _check_urls(remotes, remote.url, force, current)
         if index is None:  # append or replace in place
