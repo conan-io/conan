ae87639cffbf72902e42a4b04229d8d5cac9e6a9
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-08-26 10:37:49 +0200

    Comment

--- conan/internal/model/conan_file.py
+++ conan/internal/model/conan_file.py
@@ -48,7 +48,7 @@ class ConanFile:
     default_options = None
     default_build_options = None
     package_type = None
-    # For packages that
+    # For packages that need an override of the inferred requirement traits based on package_type
     package_type_overrides = {}
     vendor = False
     languages = []
