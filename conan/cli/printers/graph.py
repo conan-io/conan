094d2688db39d3d8c084966ab192a473c715e312
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-09-09 11:01:43 +0200

    Rename to header

--- conan/cli/printers/graph.py
+++ conan/cli/printers/graph.py
@@ -130,7 +130,7 @@ def print_graph_packages(graph):
         if existing[0] == "Skip":
             existing[0] = node.binary
 
-    def _format_requires(title, reqs_to_print, context):
+    def _format_requires(title, reqs_to_print, header):
         if not reqs_to_print:
             return
         output.info(title, Color.BRIGHT_YELLOW)
@@ -138,7 +138,7 @@ def print_graph_packages(graph):
             name = pref.repr_notime() if status != "Platform" else str(pref.ref)
             msg = f"{tab}{name} - "
             if status == "Skip":
-                skipped_requires.setdefault(context, []).append(str(pref.ref))
+                skipped_requires.setdefault(header, []).append(str(pref.ref))
                 output.verbose(f"{msg}{status}", Color.BRIGHT_CYAN)
             elif status == "Missing" or status == "Invalid":
                 output.write(msg, Color.BRIGHT_CYAN)
@@ -162,7 +162,7 @@ def print_graph_packages(graph):
     _format_requires("Build requirements", build_requires, "build")
 
     if not output.level_allowed(LEVEL_VERBOSE):
-        for context, names in skipped_requires.items():
-            output.info(f"Skipped {context} binaries", Color.BRIGHT_YELLOW)
+        for header, names in skipped_requires.items():
+            output.info(f"Skipped {header} binaries", Color.BRIGHT_YELLOW)
             output.info(tab, newline=False)
             output.info(", ".join(names), Color.BRIGHT_CYAN)
