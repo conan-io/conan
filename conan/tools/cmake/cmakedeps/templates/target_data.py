d8a0f5c07d1747f47196c4a9eef4a0cecbef7813
memsharded <memsharded@gmail.com>
2026-09-18 16:04:02 +0200

    fixed tests

--- conan/tools/cmake/cmakedeps/templates/target_data.py
+++ conan/tools/cmake/cmakedeps/templates/target_data.py
@@ -147,20 +147,20 @@ class ConfigDataTemplate(CMakeDepsFileTemplate):
 
               # COMPOUND VARIABLES
               set({{ pkg_name }}_COMPILE_OPTIONS{{ config_suffix }})
-              if ("{{ pkg_var(pkg_name, 'COMPILE_OPTIONS_CXX', config_suffix) }}")
+              if (NOT "{{ pkg_var(pkg_name, 'COMPILE_OPTIONS_CXX', config_suffix) }}" STREQUAL "")
                   list(APPEND {{ pkg_name }}_COMPILE_OPTIONS{{ config_suffix }}
                       "$<$<COMPILE_LANGUAGE:CXX>:{{ pkg_var(pkg_name, 'COMPILE_OPTIONS_CXX', config_suffix) }}>")
               endif ()
-              if ("{{ pkg_var(pkg_name, 'COMPILE_OPTIONS_C', config_suffix) }}")
+              if (NOT "{{ pkg_var(pkg_name, 'COMPILE_OPTIONS_C', config_suffix) }}" STREQUAL "")
                   list(APPEND {{ pkg_name }}_COMPILE_OPTIONS{{ config_suffix }}
                       "$<$<COMPILE_LANGUAGE:C>:{{ pkg_var(pkg_name, 'COMPILE_OPTIONS_C', config_suffix) }}>")
               endif ()
               set({{ pkg_name }}_LINKER_FLAGS{{ config_suffix }})
-              if ("{{ pkg_var(pkg_name, 'SHARED_LINK_FLAGS', config_suffix) }}")
+              if (NOT "{{ pkg_var(pkg_name, 'SHARED_LINK_FLAGS', config_suffix) }}" STREQUAL "")
                   list(APPEND {{ pkg_name }}_LINKER_FLAGS{{ config_suffix }}
                       "$<$<OR:$<STREQUAL{{ ':$' }}<TARGET_PROPERTY:TYPE>,SHARED_LIBRARY>,$<STREQUAL{{ ':$' }}<TARGET_PROPERTY:TYPE>,MODULE_LIBRARY>>:{{ pkg_var(pkg_name, 'SHARED_LINK_FLAGS', config_suffix) }}>")
               endif ()
-              if ("{{ pkg_var(pkg_name, 'EXE_LINK_FLAGS', config_suffix) }}")
+              if (NOT "{{ pkg_var(pkg_name, 'EXE_LINK_FLAGS', config_suffix) }}" STREQUAL "")
                   list(APPEND {{ pkg_name }}_LINKER_FLAGS{{ config_suffix }}
                       "$<$<STREQUAL{{ ':$' }}<TARGET_PROPERTY:TYPE>,EXECUTABLE>:{{ pkg_var(pkg_name, 'EXE_LINK_FLAGS', config_suffix) }}>")
               endif ()
@@ -193,20 +193,20 @@ class ConfigDataTemplate(CMakeDepsFileTemplate):
 
               # COMPOUND VARIABLES
               set({{ pkg_name }}_{{ comp_variable_name }}_LINKER_FLAGS{{ config_suffix }})
-              if ("{{ comp_var(pkg_name, comp_variable_name, 'SHARED_LINK_FLAGS', config_suffix) }}")
+              if (NOT "{{ comp_var(pkg_name, comp_variable_name, 'SHARED_LINK_FLAGS', config_suffix) }}" STREQUAL "")
                   list(APPEND {{ pkg_name }}_{{ comp_variable_name }}_LINKER_FLAGS{{ config_suffix }}
                       $<$<OR:$<STREQUAL:$<TARGET_PROPERTY:TYPE>,SHARED_LIBRARY>,$<STREQUAL:$<TARGET_PROPERTY:TYPE>,MODULE_LIBRARY>>:{{ comp_var(pkg_name, comp_variable_name, 'SHARED_LINK_FLAGS', config_suffix) }}>)
               endif ()
-              if ("{{ comp_var(pkg_name, comp_variable_name, 'SHARED_LINK_FLAGS', config_suffix) }}")
+              if (NOT "{{ comp_var(pkg_name, comp_variable_name, 'EXE_LINK_FLAGS', config_suffix) }}" STREQUAL "")
                   list(APPEND {{ pkg_name }}_{{ comp_variable_name }}_LINKER_FLAGS{{ config_suffix }}
                       $<$<STREQUAL:$<TARGET_PROPERTY:TYPE>,EXECUTABLE>:{{ comp_var(pkg_name, comp_variable_name, 'EXE_LINK_FLAGS', config_suffix) }}>)
               endif ()
               set({{ pkg_name }}_{{ comp_variable_name }}_COMPILE_OPTIONS{{ config_suffix }})
-              if ("{{ comp_var(pkg_name, comp_variable_name, 'COMPILE_OPTIONS_CXX', config_suffix) }}")
+              if (NOT "{{ comp_var(pkg_name, comp_variable_name, 'COMPILE_OPTIONS_CXX', config_suffix) }}" STREQUAL "")
                   list(APPEND {{ pkg_name }}_{{ comp_variable_name }}_COMPILE_OPTIONS{{ config_suffix }}
                       "$<$<COMPILE_LANGUAGE:CXX>:{{ comp_var(pkg_name, comp_variable_name, 'COMPILE_OPTIONS_CXX', config_suffix) }}>")
               endif ()
-              if ("{{ comp_var(pkg_name, comp_variable_name, 'COMPILE_OPTIONS_C', config_suffix) }}")
+              if (NOT "{{ comp_var(pkg_name, comp_variable_name, 'COMPILE_OPTIONS_C', config_suffix) }}" STREQUAL "")
                   list(APPEND {{ pkg_name }}_{{ comp_variable_name }}_COMPILE_OPTIONS{{ config_suffix }}
                       "$<$<COMPILE_LANGUAGE:C>:{{ comp_var(pkg_name, comp_variable_name, 'COMPILE_OPTIONS_C', config_suffix) }}>")
               endif ()
