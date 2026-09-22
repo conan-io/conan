dc796bab2cac2aa362b3a1e38f7d32960bbd0ee9
Carlos Zoido <mrgalleta@gmail.com>
2026-09-22 15:55:27 +0200

    better naming

--- conan/internal/hook_manager.py
+++ conan/internal/hook_manager.py
@@ -28,19 +28,20 @@ class HookManager:
         hooks = self.hooks.get(method_name)
         if hooks is None:
             return
-        previous_scope = conanfile._conan_scope_override  # noqa
-        scope = conanfile.output.scope
+        previous_override = conanfile._conan_scope_override  # noqa
+        current_scope = conanfile.output.scope
         for name, method in hooks:
             hook_scope = f"[HOOK - {name}] {method_name}()"
             try:
-                conanfile._conan_scope_override = f"{scope}: {hook_scope}" if scope else hook_scope
+                override = f"{current_scope}: {hook_scope}" if current_scope else hook_scope
+                conanfile._conan_scope_override = override
                 method(conanfile)
             except ConanInvalidConfiguration:
                 raise
             except Exception as e:
                 raise ConanException("[HOOK - %s] %s(): %s" % (name, method_name, str(e)))
             finally:
-                conanfile._conan_scope_override = previous_scope
+                conanfile._conan_scope_override = previous_override
 
     def _load_hooks(self):
         hooks = {}
